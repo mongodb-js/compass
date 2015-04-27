6efdd4819bf0f77ee32af3824440efc75f2b9423
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-04-27 15:52:03 -0400

    fix(ui): statusbar jsfmt

--- scout-ui/src/statusbar.js
+++ scout-ui/src/statusbar.js
@@ -12,14 +12,19 @@ var StatusbarView = AmpersandView.extend({
     width: [
       {
         hook: 'bar',
-        type: function (el, value, previousValue) {
-          if(!this.el) return;
-          if(value === 0){
-            $(this.el).find('.progress').css({height: '0px'});
-          }
-          else {
-            $(this.el).find('.progress-bar').css({width: value + '%'});
-            $(this.el).find('.progress').css({height: '4px'});
+        type: function(el, value, previousValue) {
+          if (!this.el) return;
+          if (value === 0) {
+            $(this.el).find('.progress').css({
+              height: '0px'
+            });
+          } else {
+            $(this.el).find('.progress-bar').css({
+              width: value + '%'
+            });
+            $(this.el).find('.progress').css({
+              height: '4px'
+            });
           }
         }
       },
@@ -30,25 +35,25 @@ var StatusbarView = AmpersandView.extend({
       }
     ]
   },
-  watch: function(view, collection){
+  watch: function(view, collection) {
     view.listenTo(collection, 'sync', this.hide.bind(this));
     view.listenTo(collection, 'request', this.show.bind(this));
     return this;
   },
-  unwatch: function(view, collection){
+  unwatch: function(view, collection) {
     view.stopListening(collection, 'sync', this.hide.bind(this));
     view.stopListening(collection, 'request', this.show.bind(this));
     return this;
   },
-  show: function(){
+  show: function() {
     this.$el = $('#statusbar');
     this.el = this.$el.get(0);
     this.width = 100;
   },
-  hide: function(){
+  hide: function() {
     this.width = 0;
   },
-  render: function(){
+  render: function() {
     this.show();
   }
 });
