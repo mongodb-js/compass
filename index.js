f015a93d06d71c3fd8fafdcbeb091eef59519c8e
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-04-27 15:29:59 -0400

    fix(project): whitespace

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('events').EventEmitter.prototype._maxListeners = 100;
 
-if(process.env.NODE_ENV !== 'development'){
+if (process.env.NODE_ENV !== 'development') {
   var server = require('./scout-server');
 
-  server.listen(server.config.get('port'), server.config.get('hostname'), function(){
+  server.listen(server.config.get('port'), server.config.get('hostname'), function() {
     console.log('   ');
     console.log('   scout ready to get to work');
     console.log('   open ' + server.config.get('listen'));
