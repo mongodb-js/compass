4b8f8ed716980049eec114cde4f3a54832183e34
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-05-27 14:31:42 -0400

    unique charts now show actually random values from the sample set.

--- scout-ui/src/minicharts/unique.js
+++ scout-ui/src/minicharts/unique.js
@@ -5,32 +5,16 @@ var debug = require('debug')('scout-ui:minicharts:unique');
 module.exports = VizView.extend({
   template: require('./unique.jade'),
   derived: {
-    orderedValues: {
-      deps: ['model.values'],
-      cache: true,
-      fn: function() {
-        return this.model.values.toJSON().sort();
-      }
-    },
-    minValue: {
-      deps: ['orderedValues'],
-      cache: true,
-      fn: function() {
-        return this.orderedValues[0];
-      }
-    },
-    maxValue: {
-      deps: ['orderedValues'],
-      cache: true,
-      fn: function() {
-        return this.orderedValues[this.orderedValues.length - 1];
-      }
-    },
     randomValues: {
       deps: ['orderedValues'],
       cache: false,
       fn: function() {
-        return this.orderedValues.slice(1, 15);
+        return _(this.model.values.sample(15))
+          .map(function(x) {
+            return x.value;
+          })
+          .sort()
+          .value();
       }
     }
   },
