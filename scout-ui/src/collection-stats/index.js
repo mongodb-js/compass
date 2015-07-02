df1afb2257e8a22c993a1bb982867da890d1a4b1
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-07-02 11:53:28 -0400

    fix(ui): dynamically set precision for col stats

--- scout-ui/src/collection-stats/index.js
+++ scout-ui/src/collection-stats/index.js
@@ -23,43 +23,55 @@ var CollectionStatsView = AmpersandView.extend({
     },
     index_size_average: {
       hook: 'index_size_average'
-    },
+    }
+  },
+  /**
+   * Use [numeral.js][numeral.js] to format a collection stat as a nice string.
+   * @param {String} propertyName of `this.model` to format as a string.
+   * @return {String} Nicely formatted value.
+   * [numeral.js]: http://numeraljs.com/
+   */
+  format: function(propertyName) {
+    var value = this.model.get(propertyName);
+    var precision = value <= 1000 ? '0' : '0.0';
+    var format = propertyName.indexOf('_size') > -1 ? 'b' : 'a';
+    return numeral(value).format(precision + format);
   },
   derived: {
     document_count: {
       deps: ['model.document_count'],
       fn: function() {
-        return numeral(this.model.document_count).format('0.0a');
+        return this.format('document_count');
       }
     },
     document_size: {
       deps: ['model.document_size'],
       fn: function() {
-        return numeral(this.model.document_size).format('0.0b');
+        return this.format('document_size');
       }
     },
     document_size_average: {
       deps: ['model.document_size_average'],
       fn: function() {
-        return numeral(this.model.document_size_average).format('0.0b');
+        return this.format('document_size_average');
       }
     },
     index_count: {
       deps: ['model.index_count'],
       fn: function() {
-        return numeral(this.model.index_count).format('0.0a');
+        return this.format('index_count');
       }
     },
     index_size: {
       deps: ['model.index_size'],
       fn: function() {
-        return numeral(this.model.index_size).format('0.0b');
+        return this.format('index_size');
       }
     },
     index_size_average: {
       deps: ['model.index_size_average'],
       fn: function() {
-        return numeral(this.model.index_size_average).format('0.0b');
+        return this.format('index_size_average');
       }
     }
   },
