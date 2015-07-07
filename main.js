d0c82df25bdad3dc9b3f9d22453dd324ae234758
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-07-07 13:00:28 -0400

    main.js will now start livereload
    instead of it living in the server

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 var server = require('scout-server');
+var watch = require('watch');
+var tinyLR = require('tiny-lr');
+
+var NODE_MODULES_REGEX = /node_modules/;
+var opts = {
+  port: 35729,
+  host: '127.0.0.1'
+};
+
+var livereload = tinyLR();
+livereload.listen(opts.port, opts.host);
+
+watch.watchTree(__dirname, {
+  filter: function(filename) {
+    return !NODE_MODULES_REGEX.test(filename);
+  },
+  ignoreDotFiles: true
+}, function(files) {
+  console.log('File change detected!  Sending reload message');
+  livereload.changed({
+    body: {
+      files: files
+    }
+  });
+});
 
 server.start({
   static: __dirname
