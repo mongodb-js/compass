8f426d9a952b93786ca8dabbb8bd4317570a037a
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-07-09 18:27:29 +0200

    make query bar more user friendly
    - empty input is turned into {}
    - single quotes are replaced with double quotes
    - missing quotes around key names are added

--- src/refine-view/index.js
+++ src/refine-view/index.js
@@ -1,5 +1,7 @@
 var AmpersandView = require('ampersand-view');
 var EJSON = require('mongodb-extended-json');
+var _ = require('lodash');
+var debug = require('debug')('scout:refine-view:index');
 
 module.exports = AmpersandView.extend({
   template: require('./index.jade'),
@@ -36,9 +38,21 @@ module.exports = AmpersandView.extend({
     'input [data-hook=refine-input]': 'inputChanged',
     'submit form': 'submit'
   },
+  _cleanupInput: function (input) {
+    var output = input;
+    // accept whitespace-only input as empty query
+    if (_.trim(output) === '') {
+      output = '{}';
+    }
+    // replace single quotes with double quotes
+    output = output.replace(/'/g, '"');
+    // wrap field names in double quotes
+    output = output.replace(/([{,])\s*([^,{\s\'"]+)\s*:/g, ' $1 "$2" : ');
+    return output;
+  },
   inputChanged: function() {
     // validate user input on the fly
-    var queryStr = this.queryByHook('refine-input').value;
+    var queryStr = this._cleanupInput(this.queryByHook('refine-input').value);
     try {
       EJSON.parse(queryStr);
     } catch (e) {
@@ -48,10 +62,9 @@ module.exports = AmpersandView.extend({
     this.valid = true;
   },
   buttonClicked: function() {
-    var queryStr = this.queryByHook('refine-input').value;
+    var queryStr = this._cleanupInput(this.queryByHook('refine-input').value);
     var queryObj = EJSON.parse(queryStr);
     this.model.query = queryObj;
-
     // Modifying the query will reset field-list#schema and because we're using
     // good ampersand, outgoing views will be removed for us automatically.
   },
