6cf498b9e2c5e03d5f40552fd8e85b7ad9006b32
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-07-10 21:13:56 -0400

    serve frontend via file:// instead of http://
    - puts much less pressure on the renderer processes
    - think of this like making nginx serve your static content instead of
    the application

--- main.js
+++ main.js
@@ -2,10 +2,7 @@ var server = require('scout-server');
 var debug = require('debug')('scout:main');
 
 var STATIC = process.env.WATCH_DIRECTORY || __dirname;
-debug('pointing scout-server static serving at `%s`', STATIC);
-server.start({
-  static: STATIC
-});
+server.start({});
 
 if (process.env.WATCH_DIRECTORY) {
   var watch = require('watch');
@@ -18,14 +15,14 @@ if (process.env.WATCH_DIRECTORY) {
 
   var livereload = tinyLR();
   livereload.listen(opts.port, opts.host);
-  console.log('Watching %s for changes', STATIC);
+  debug('Watching %s for changes', STATIC);
   watch.watchTree(STATIC, {
     filter: function(filename) {
       return !NODE_MODULES_REGEX.test(filename);
     },
     ignoreDotFiles: true
   }, function(files) {
-    console.log('File change detected!  Sending reload message');
+    debug('File change detected!  Sending reload message');
     livereload.changed({
       body: {
         files: files
