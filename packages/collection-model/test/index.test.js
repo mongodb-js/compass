047d334c5c863cfa3537ad35d7643473a971d438
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-08-12 08:24:58 -0400

    initial commit

---
+++ packages/collection-model/test/index.test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var Collection = require('../');
+
+describe('mongodb-collection-model', function() {
+  it('should work', function(){
+    assert(Collection);
+  });
+});
