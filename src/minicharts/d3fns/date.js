65b4c9dd8e722c18193b50c3fec6ecba7626b6ce
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-08-25 17:56:17 +1000

    better mousedown handling on existing selection

--- src/minicharts/d3fns/date.js
+++ src/minicharts/d3fns/date.js
@@ -113,13 +113,16 @@ var minicharts_d3fns_date = function() {
     };
     options.view.trigger('querybuilder', evt);
   };
+
   function handleMouseDown() {
     var line = this;
     var parent = $(this).closest('.minichart');
     var background = parent.find('g.brush > rect.background')[0];
     var brushNode = parent.find('g.brush')[0];
     var start = barcodeX.invert(d3.mouse(background)[0]);
-    brushstart.call(brushNode, line);
+    var brushstartOnce = _.once(function() {
+      brushstart.call(brushNode, line);
+    });
 
     var w = d3.select(window)
       .on('mousemove', mousemove)
@@ -128,6 +131,7 @@ var minicharts_d3fns_date = function() {
     d3.event.preventDefault(); // disable text dragging
 
     function mousemove() {
+      brushstartOnce();
       var extent = [start, barcodeX.invert(d3.mouse(background)[0])];
       d3.select(brushNode).call(brush.extent(_.sortBy(extent)));
       brushed.call(brushNode);
