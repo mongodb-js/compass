dcfa1bafb0d60273bc179ab55bb2a4eb4c84d1b4
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-09-01 14:48:48 +1000

    INT-407 round down probability for type bars
    to provent line break in type distribution bars, e.g. with a distribution of
    
    [50, 16.666, 16.666, 16.666] which would round to [50, 17, 17, 17] and sum up to 101%.

--- src/field-list/type-list-item.js
+++ src/field-list/type-list-item.js
@@ -38,7 +38,8 @@ module.exports = View.extend(tooltipMixin, {
     probability_percentage: {
       deps: ['model.probability'],
       fn: function() {
-        return numeral(this.model.probability).format('0.00%');
+        // round down probability so that the bar widths can never sum up to > 100%
+        return numeral(Math.floor(this.model.probability * 100.0) / 100.0).format('0.00%');
       }
     },
     tooltip_message: {
