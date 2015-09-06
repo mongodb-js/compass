ce9ecfebce79cc9ad61dac2ee7fc82489e754522
Yonatan Schreiber <yonatanmn@gmail.com>
2015-09-06 15:04:27 +0300

    bracket error

--- packages/reflux-state-mixin/index.js
+++ packages/reflux-state-mixin/index.js
@@ -26,43 +26,33 @@ module.exports = function stateMixin(Reflux) {
   }
 
   return {
-
-    setState: function (newState) {
+    setState: function (state) {
       var changed = false;
       var prevState = update({}, {$merge: this.state});
-      var statesToTrigger = [];
-      for (var key in newState) {
-        if (newState.hasOwnProperty(key)) {
-          if (this.state[key] !== newState[key]) {
-            this.state[key] = newState[key];
-            statesToTrigger.push(key);
+
+      for (var key in state) {
+        if (state.hasOwnProperty(key)) {
+          if (this.state[key] !== state[key]) {
+            this[key].trigger(state[key]);
             changed = true;
           }
         }
       }
 
       if (changed) {
-        if (utils.isFunction(this.shouldStoreUpdate) && !this.shouldStoreUpdate(prevState)) {
-            return;
-          }
-        }
+        this.state = update(this.state, {$merge: state});
 
-        for (var index in statesToTrigger) {
-          key = statesToTrigger[index];
-          this[key].trigger(newState[key]);
+        if (_.isFunction(this.storeDidUpdate)) {
+          this.storeDidUpdate(prevState);
         }
 
         this.trigger(this.state);
-
-        if (utils.isFunction(this.storeDidUpdate)) {
-          this.storeDidUpdate(prevState);
-        }
       }
 
     },
 
     init: function () {
-      if (utils.isFunction(this.getInitialState)) {
+      if (_.isFunction(this.getInitialState)) {
         this.state = this.getInitialState();
         for (var key in this.state) {
           if (this.state.hasOwnProperty(key)) {
@@ -75,21 +65,21 @@ module.exports = function stateMixin(Reflux) {
     connect: function (store, key) {
       return {
         getInitialState: function () {
-          if (!utils.isFunction(store.getInitialState)) {
+          if (!_.isFunction(store.getInitialState)) {
             return {};
           } else if (key === undefined) {
             return store.state;
           } else {
-            return utils.object([key], [store.state[key]]);
+            return _.object([key], [store.state[key]]);
           }
         },
         componentDidMount: function () {
-          utils.extend(this, Reflux.ListenerMethods);
+          _.extend(this, Reflux.ListenerMethods);
           var noKey = key === undefined;
           var me = this,
               cb = (noKey ? this.setState : function (v) {
                 if (typeof me.isMounted === "undefined" || me.isMounted() === true) {
-                  me.setState(utils.object([key], [v]));
+                  me.setState(_.object([key], [v]));
                 }
               }),
               listener = noKey ? store : store[key];
