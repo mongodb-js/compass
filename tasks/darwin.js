mongodb-js/compass

commit ed5004cbf1eccc2b31afd997b8819b9ca7125d90

INT-198 tidy up darwin build logic

tasks/darwin.js
@@ -2,6 +2,8 @@ var path = require('path');
 var pkg = require(path.resolve(__dirname, '../package.json'));
 var fs = require('fs');
 var cp = require('child_process');
+var series = require('run-series');
+var _ = require('lodash');
 
 var debug = require('debug')('scout:tasks:darwin');
 
@@ -54,21 +56,18 @@ var CONFIG = module.exports = {
   ]
 };
 
-debug('packager config: ', JSON.stringify(CONFIG, null, 2));
+// Adjust config via environment variables
+if (process.env.SCOUT_INSTALLER_UNSIGNED !== undefined) {
+  CONFIG.sign = null;
+}
 
-var doCodeSign = function() {
-  return process.env.SCOUT_INSTALLER_UNSIGNED === undefined;
-};
+debug('packager config: ', JSON.stringify(CONFIG, null, 2));
 
 module.exports.build = function(done) {
   fs.exists(APP_PATH, function(exists) {
     if (exists) {
       debug('.app already exists.  skipping packager run.');
       return done();
-    }
-
-    if (!doCodeSign()) {
-      CONFIG.sign = null;
     }
 
     debug('running packager...');
@@ -85,24 +84,17 @@ var verify = function(done) {
 module.exports.installer = function(done) {
   debug('running packager...');
 
-  // TODO: clean up with https://www.npmjs.com/package/run-series ?
-  if (!doCodeSign()) {
-    CONFIG.sign = null;
+  var tasks = [
+    _.partial(packager, CONFIG)
+  ];
 
-    packager(CONFIG, function(err) {
-      if (err) return done(err);
-
-      createDMG(CONFIG, done);
-    });
-  } else {
-    packager(CONFIG, function(err) {
-      if (err) return done(err);
-
-      verify(function(err) {
-        if (err) return done(err);
-
-        createDMG(CONFIG, done);
-      });
-    });
+  if (CONFIG.sign) {
+    tasks.push(_.partial(verify, CONFIG));
   }
+
+  tasks.push(_.partial(createDMG, CONFIG));
+
+  series(tasks, function(err) {
+    if (err) return done(err);
+  });
 };

commit dcd78c9bbee6adada20184b86a79e22e1411a787

fix(osx) signed installer build bug
Fix bug introduced by yours truly in ed5004c

tasks/darwin.js
@@ -89,7 +89,7 @@ module.exports.installer = function(done) {
   ];
 
   if (CONFIG.sign) {
-    tasks.push(_.partial(verify, CONFIG));
+    tasks.push(verify);
   }
 
   tasks.push(_.partial(createDMG, CONFIG));