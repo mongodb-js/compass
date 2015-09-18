412f98798a1710477bd04aa99d23322b5f6c8d26
Thomas Rueckstiess <thomas@mongodb.com>
2015-09-18 12:41:55 +0200

    added google maps, no interaction yet

--- src/minicharts/d3fns/mapstyle.js
+++ src/minicharts/d3fns/mapstyle.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module.exports = [
   {
     featureType: 'administrative',
@@ -5,10 +6,27 @@ module.exports = [
     stylers: [
       {
         visibility: 'simplified'
+=======
+/*eslint quote-props: 0*/
+module.exports = [
+  {
+    'featureType': 'all',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'saturation': 36
+      },
+      {
+        'color': '#333333'
+      },
+      {
+        'lightness': 40
+>>>>>>> added google maps, no interaction yet
       }
     ]
   },
   {
+<<<<<<< HEAD
     featureType: 'landscape',
     elementType: 'geometry',
     stylers: [
@@ -17,10 +35,33 @@ module.exports = [
       },
       {
         color: '#fcfcfc'
+=======
+    'featureType': 'all',
+    'elementType': 'labels.text.stroke',
+    'stylers': [
+      {
+        'visibility': 'on'
+      },
+      {
+        'color': '#ffffff'
+      },
+      {
+        'lightness': 16
       }
     ]
   },
   {
+    'featureType': 'all',
+    'elementType': 'labels.icon',
+    'stylers': [
+      {
+        'visibility': 'off'
+>>>>>>> added google maps, no interaction yet
+      }
+    ]
+  },
+  {
+<<<<<<< HEAD
     featureType: 'poi',
     elementType: 'geometry',
     stylers: [
@@ -29,10 +70,282 @@ module.exports = [
       },
       {
         color: '#fcfcfc'
+=======
+    'featureType': 'administrative',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'off'
       }
     ]
   },
   {
+    'featureType': 'administrative',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'color': '#fefefe'
+      },
+      {
+        'lightness': 20
+      }
+    ]
+  },
+  {
+    'featureType': 'administrative',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#fefefe'
+      },
+      {
+        'lightness': 17
+      },
+      {
+        'weight': 1.2
+      }
+    ]
+  },
+  {
+    'featureType': 'landscape',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'lightness': 20
+      },
+      {
+        'color': '#ececec'
+      }
+    ]
+  },
+  {
+    'featureType': 'landscape.man_made',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      },
+      {
+        'color': '#f0f0ef'
+      }
+    ]
+  },
+  {
+    'featureType': 'landscape.man_made',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'visibility': 'on'
+      },
+      {
+        'color': '#f0f0ef'
+      }
+    ]
+  },
+  {
+    'featureType': 'landscape.man_made',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'visibility': 'on'
+      },
+      {
+        'color': '#d4d4d4'
+      }
+    ]
+  },
+  {
+    'featureType': 'landscape.natural',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      },
+      {
+        'color': '#ececec'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'lightness': 21
+      },
+      {
+        'visibility': 'off'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'visibility': 'on'
+      },
+      {
+        'color': '#d4d4d4'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#303030'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'labels.icon',
+    'stylers': [
+      {
+        'saturation': '-100'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.attraction',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.business',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.government',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.medical',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.park',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.park',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dedede'
+      },
+      {
+        'lightness': 21
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.place_of_worship',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.school',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.school',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'lightness': '-61'
+      },
+      {
+        'gamma': '0.00'
+      },
+      {
+        'visibility': 'off'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.sports_complex',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'visibility': 'on'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'color': '#ffffff'
+      },
+      {
+        'lightness': 17
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#ffffff'
+      },
+      {
+        'lightness': 29
+      },
+      {
+        'weight': 0.2
+>>>>>>> added google maps, no interaction yet
+      }
+    ]
+  },
+  {
+<<<<<<< HEAD
     featureType: 'road.highway',
     elementType: 'geometry',
     stylers: [
@@ -41,10 +354,21 @@ module.exports = [
       },
       {
         color: '#dddddd'
+=======
+    'featureType': 'road.arterial',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#ffffff'
+      },
+      {
+        'lightness': 18
+>>>>>>> added google maps, no interaction yet
       }
     ]
   },
   {
+<<<<<<< HEAD
     featureType: 'road.arterial',
     elementType: 'geometry',
     stylers: [
@@ -53,10 +377,21 @@ module.exports = [
       },
       {
         color: '#dddddd'
+=======
+    'featureType': 'road.local',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#ffffff'
+      },
+      {
+        'lightness': 16
+>>>>>>> added google maps, no interaction yet
       }
     ]
   },
   {
+<<<<<<< HEAD
     featureType: 'road.local',
     elementType: 'geometry',
     stylers: [
@@ -65,10 +400,21 @@ module.exports = [
       },
       {
         color: '#eeeeee'
+=======
+    'featureType': 'transit',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#f2f2f2'
+      },
+      {
+        'lightness': 19
+>>>>>>> added google maps, no interaction yet
       }
     ]
   },
   {
+<<<<<<< HEAD
     featureType: 'water',
     elementType: 'geometry',
     stylers: [
@@ -81,3 +427,17 @@ module.exports = [
     ]
   }
 ]
+=======
+    'featureType': 'water',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'visibility': 'simplified'
+      },
+      {
+        'saturation': -60
+      }
+    ]
+  }
+];
+>>>>>>> added google maps, no interaction yet
