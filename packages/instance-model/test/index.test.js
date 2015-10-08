fa8f1d3fbcf89d80a7d70a487ff0e3f4de803fab
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-10-08 11:41:42 -0400

    muscle: add another case for instance id

--- packages/instance-model/test/index.test.js
+++ packages/instance-model/test/index.test.js
@@ -13,5 +13,8 @@ describe('mongodb-instance-model', function() {
     it('should treat a number param as the port', function() {
       assert.equal(Instance.getId(27017), 'localhost:27017');
     });
+    it('should remove mongodb://', function() {
+      assert.equal(Instance.getId('mongodb://localhost:27017'), 'localhost:27017');
+    });
   });
 });
