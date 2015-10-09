6f816794e652f551acff35177d836ce27e7e3743
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-10-09 14:38:20 -0400

    :zap: Default NODE_ENV to production
    `npm start` will always explicitly set NODE_ENV=development.
    
    So if it's not set, the app is being run outside of the
    active dev context.
    
    This way we'll be able to start filtering exception
    reports more easily from stuff we should really pay attention to.

--- src/electron/index.js
+++ src/electron/index.js
@@ -1,3 +1,6 @@
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'production';
+}
 // Start crash-reporter asap so if anything goes
 // wrong, we can at least view the crash reports
 // locally.  When a crash occurs, a `.dmp` file
