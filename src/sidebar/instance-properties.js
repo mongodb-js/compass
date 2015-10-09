e2c6165acc2132b0a39edb28c17cef182c7ad09f
Thomas Rueckstiess <thomas@mongodb.com>
2015-10-09 18:27:36 -0400

    reload collections, and spin button

--- src/sidebar/instance-properties.js
+++ src/sidebar/instance-properties.js
@@ -1,6 +1,7 @@
 var View = require('ampersand-view');
 var app = require('ampersand-app');
 // var debug = require('debug')('scout:sidebar:instace-properties');
+var _ = require('lodash');
 
 var InstancePropertiesView = module.exports = View.extend({
   template: require('./instance-properties.jade'),
@@ -9,7 +10,11 @@ var InstancePropertiesView = module.exports = View.extend({
     version: 'string',
     numDatabases: 'number',
     numCollections: 'number',
-    instance: 'state'
+    instance: 'state',
+    is_fetching: {
+      type: 'boolean',
+      default: true
+    }
   },
   bindings: {
     'instance.host.hostname': {
@@ -27,14 +32,31 @@ var InstancePropertiesView = module.exports = View.extend({
     numDatabases: {
       type: 'text',
       hook: 'num-databases'
+    },
+    is_fetching: {
+      type: 'booleanClass',
+      hook: 'refresh-icon',
+      yes: ['fa-refresh', 'fa-spin'],
+      no: 'fa-repeat'
     }
+  },
+  events: {
+    'click button[data-hook=refresh-button]': 'onRefreshButtonClicked'
   },
   initialize: function() {
     this.listenTo(app.instance, 'sync', this.onInstanceFetched);
   },
   onInstanceFetched: function() {
+    // delay switching the spinner back to static for 500ms, otherwise the reload is not noticable
+    _.delay(function() {
+      this.is_fetching = false;
+    }.bind(this), 500);
     this.numDatabases = app.instance.databases.length;
     this.numCollections = app.instance.collections.length;
+  },
+  onRefreshButtonClicked: function() {
+    app.instance.fetch();
+    this.is_fetching = true;
   }
 });
 
