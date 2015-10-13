79e04948cf4c615835b8c7cdc19aaf77360cad67
Thomas Rueckstiess <thomas@mongodb.com>
2015-10-13 15:27:19 -0400

    shiftkey handling, language model bump, geojson

--- src/minicharts/index.js
+++ src/minicharts/index.js
@@ -43,6 +43,22 @@ module.exports = AmpersandView.extend(QueryBuilderMixin, {
     });
     this.listenTo(app.volatileQueryOptions, 'change:query', this.handleVolatileQueryChange);
   },
+  _mangleGeoCoordinates: function(values) {
+    debug('mangle values', values)
+    // now check value bounds
+    var lons = values.filter(function(val, idx) {
+      return idx % 2 === 0;
+    });
+    var lats = values.filter(function(val, idx) {
+      return idx % 2 === 1;
+    });
+    if (_.min(lons) >= -180 && _.max(lons) <= 180 && _.min(lats) >= -90 && _.max(lats) <= 90) {
+
+      // attach the zipped up coordinates to the model where VizView would expect it
+      return new ArrayCollection(_.zip(lons, lats));
+    }
+    return false;
+  },
   render: function() {
     this.renderWithTemplate(this);
 
@@ -54,28 +70,52 @@ module.exports = AmpersandView.extend(QueryBuilderMixin, {
       this.viewOptions.className = 'minichart unique';
       this.subview = new UniqueMinichartView(this.viewOptions);
     } else if (this.model.name === 'Document') {
-      // nested objects get a div-based DocumentRootMinichart
-      this.viewOptions.height = 55;
-      this.subview = new DocumentRootMinichartView(this.viewOptions);
+      // are these coordinates? Do a basic check for now, until we support semantic schema types
+      // here we check for GeoJSON form: { loc: {type: "Point", "coordinates": [47.80, 9.63] } }
+      var isGeo = false;
+      if (app.isFeatureEnabled('Geo Minicharts')) {
+        if (this.model.fields.length === 2
+          && this.model.fields.get('type')
+          && this.model.fields.get('type').type === 'String'
+          && this.model.fields.get('type').types.get('String').unique === 1
+          && this.model.fields.get('type').types.get('String').values.at(0).value === 'Point'
+          && this.model.fields.get('coordinates').types.get('Array').count
+             === this.model.fields.get('coordinates').count
+          && this.model.fields.get('coordinates').types.get('Array').average_length === 2
+        ) {
+          var coords =this._mangleGeoCoordinates(
+            this.model.fields.get('coordinates').types.get('Array')
+            .types.get('Number').values.serialize());
+          if (coords) {
+            this.model.values = coords;
+            this.model.fields.reset();
+            isGeo = true;
+          }
+        }
+      }
+      if (isGeo) {
+        // coordinates get an HTML-based d3 VizView with `coordinates` vizFn
+        this.viewOptions.renderMode = 'html';
+        this.viewOptions.height = 250;
+        this.viewOptions.vizFn = vizFns.geo;
+        this.subview = new VizView(this.viewOptions);
+      } else {
+        // nested objects get a div-based DocumentRootMinichart
+        this.viewOptions.height = 55;
+        this.subview = new DocumentRootMinichartView(this.viewOptions);
+      }
     } else if (this.model.name === 'Array') {
       var isGeo = false;
       if (app.isFeatureEnabled('Geo Minicharts')) {
         // are these coordinates? Do a basic check for now, until we support semantic schema types
+        // here we check for legacy coordinates in array form: { loc: [47.80, 9.63] }
         var lengths = this.model.lengths;
         if (_.min(lengths) === 2 && _.max(lengths) === 2) {
-          // now check value bounds
-          var values = this.model.types.get('Number').values.serialize();
-          var lons = values.filter(function(val, idx) {
-            return idx % 2 === 0;
-          });
-          var lats = values.filter(function(val, idx) {
-            return idx % 2 === 1;
-          });
-          if (_.min(lons) >= -180 && _.max(lons) <= 180 && _.min(lats) >= -90 && _.max(lats) <= 90) {
+          var coords = this._mangleGeoCoordinates(
+            this.model.types.get('Number').values.serialize());
+          if (coords) {
+            this.model.values = coords;
             isGeo = true;
-            // attach the zipped up coordinates to the model where VizView would expect it
-            this.model.values = new ArrayCollection(_.zip(lons, lats));
-            debug('model.values', this.model.values);
           }
         }
       }
