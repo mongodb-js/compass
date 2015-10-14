c554673c913157fd35819199f89083fa76f604e5
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-10-14 17:11:58 -0400

    Yay!  An actual test

--- test/specs/connect/index.js
+++ test/specs/connect/index.js
@@ -1,12 +1,37 @@
+var assert = require('assert');
+
+function* connect(model){
+  if(model.hostname){
+    yield browser.setValue('[name=hostname]', model.hostname);
+  }
+  if(model.port){
+    yield browser.setValue('[name=port]', model.port);
+  }
+  if(model.name){
+    yield browser.setValue('[name=name]', model.name);
+  }
+
+  yield browser.click('[data-hook=submit-button]');
+}
+
 describe('Connect', function() {
-  it('should work', function(done) {
-    return browser
-      .getTitle().then(function(v) {
-      console.log('title', v);
-    })
-      .call(function() {
-        console.log('done', arguments);
-        done();
-      });
+  it('should open the connect dialog', function* () {
+    var title = yield browser.getTitle();
+    assert.equal(title, 'Connect to MongoDB');
+  });
+
+  it('should fail to connect to localhost:27017', function *(){
+    yield connect({
+      hostname: 'localhost',
+      port: 27017,
+      name: 'My MongoDB'
+    });
+    yield browser.pause(1000);
+    var handles = yield browser.windowHandles();
+    console.log('Window handles', handles);
+
+    assert.equal(handles.value.length, 1);
+    var isErrorVisible = yield browser.isVisible('.message.alert');
+    assert.equal(isErrorVisible, true);
   });
 });
