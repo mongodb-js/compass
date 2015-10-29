da110aa1ccd68338c6d7b108cfe99eeff9627f02
Waley <waleycz@gmail.com>
2015-10-29 11:57:44 -0400

    revert showing menu to Windows, story is stalled

--- src/electron/menu.js
+++ src/electron/menu.js
@@ -3,147 +3,149 @@ var Menu = require('menu');
 var debug = require('debug')('scout-electron:menu');
 
 function getTemplate(_window) {
-  return [
-    {
-      label: 'MongoDB Compass',
-      submenu: [
-        {
-          label: 'About Compass',
-          selector: 'orderFrontStandardAboutPanel:'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Hide',
-          accelerator: 'Command+H',
-          selector: 'hide:'
-        },
-        {
-          label: 'Hide Others',
-          accelerator: 'Command+Shift+H',
-          selector: 'hideOtherApplications:'
-        },
-        {
-          label: 'Show All',
-          selector: 'unhideAllApplications:'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Quit',
-          accelerator: 'Command+Q',
-          click: function() {
-            app.quit();
+  if (process.platform === 'darwin') {
+    return [
+      {
+        label: 'MongoDB Compass',
+        submenu: [
+          {
+            label: 'About Compass',
+            selector: 'orderFrontStandardAboutPanel:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Hide',
+            accelerator: 'Command+H',
+            selector: 'hide:'
+          },
+          {
+            label: 'Hide Others',
+            accelerator: 'Command+Shift+H',
+            selector: 'hideOtherApplications:'
+          },
+          {
+            label: 'Show All',
+            selector: 'unhideAllApplications:'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function() {
+              app.quit();
+            }
           }
-        }
-      ]
-    },
-    {
-      label: 'Connect',
-      submenu: [
-        {
-          label: 'Connect to...',
-          accelerator: 'Command+N',
-          click: function() {
-            app.emit('show connect dialog');
+        ]
+      },
+      {
+        label: 'Connect',
+        submenu: [
+          {
+            label: 'Connect to...',
+            accelerator: 'Command+N',
+            click: function() {
+              app.emit('show connect dialog');
+            }
           }
-        }
-      ]
-    },
-    {
-      label: 'Edit',
-      submenu: [
-        {
-          label: 'Undo',
-          accelerator: 'Command+Z',
-          role: 'undo'
-        },
-        {
-          label: 'Redo',
-          accelerator: 'Shift+Command+Z',
-          role: 'redo'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Cut',
-          accelerator: 'Command+X',
-          role: 'cut'
-        },
-        {
-          label: 'Copy',
-          accelerator: 'Command+C',
-          role: 'copy'
-        },
-        {
-          label: 'Paste',
-          accelerator: 'Command+V',
-          role: 'paste'
-        },
-        {
-          label: 'Select All',
-          accelerator: 'Command+A',
-          role: 'selectall'
-        }
-      ]
-    },
-    {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'Command+R',
-          click: function() {
-            _window.restart();
+        ]
+      },
+      {
+        label: 'Edit',
+        submenu: [
+          {
+            label: 'Undo',
+            accelerator: 'Command+Z',
+            role: 'undo'
+          },
+          {
+            label: 'Redo',
+            accelerator: 'Shift+Command+Z',
+            role: 'redo'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Cut',
+            accelerator: 'Command+X',
+            role: 'cut'
+          },
+          {
+            label: 'Copy',
+            accelerator: 'Command+C',
+            role: 'copy'
+          },
+          {
+            label: 'Paste',
+            accelerator: 'Command+V',
+            role: 'paste'
+          },
+          {
+            label: 'Select All',
+            accelerator: 'Command+A',
+            role: 'selectall'
           }
-        },
-        {
-          label: 'Toggle DevTools',
-          accelerator: 'Alt+Command+I',
-          click: function() {
-            _window.toggleDevTools();
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'Reload',
+            accelerator: 'Command+R',
+            click: function() {
+              _window.restart();
+            }
+          },
+          {
+            label: 'Toggle DevTools',
+            accelerator: 'Alt+Command+I',
+            click: function() {
+              _window.toggleDevTools();
+            }
           }
-        }
-      ]
-    },
-    {
-      label: 'Share',
-      submenu: [
-        {
-          label: 'Share Schema as JSON',
-          accelerator: 'Alt+Command+S',
-          click: function() {
-            _window.webContents.send('message', 'menu-share-schema-json');
+        ]
+      },
+      {
+        label: 'Share',
+        submenu: [
+          {
+            label: 'Share Schema as JSON',
+            accelerator: 'Alt+Command+S',
+            click: function() {
+              _window.webContents.send('message', 'menu-share-schema-json');
+            }
           }
-        }
-      ]
-    },
-    {
-      label: 'Window',
-      submenu: [
-        {
-          label: 'Minimize',
-          accelerator: 'Command+M',
-          role: 'minimize'
-        },
-        {
-          label: 'Close',
-          accelerator: 'Command+W',
-          role: 'close'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Bring All to Front',
-          selector: 'arrangeInFront:'
-        }
-      ]
-    }
-  ];
+        ]
+      },
+      {
+        label: 'Window',
+        submenu: [
+          {
+            label: 'Minimize',
+            accelerator: 'Command+M',
+            role: 'minimize'
+          },
+          {
+            label: 'Close',
+            accelerator: 'Command+W',
+            role: 'close'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Bring All to Front',
+            selector: 'arrangeInFront:'
+          }
+        ]
+      }
+    ];
+  }
 
   return [
     {
