b72db05dd4e2aa0f3ec17e9ff01bd307f46e5309
Marc Schäffner-Gurney <mws8726@gmail.com>
2015-11-09 16:46:38 -0500

    tour updates

--- src/tour/index.js
+++ src/tour/index.js
@@ -3,25 +3,9 @@ var View = require('ampersand-view');
 
 var TourView = View.extend({
   props: {
-    isAnimating: {
-      type: 'boolean',
-      default: true
-    },
     tourCount: {
       type: 'number',
       default: 0
-    },
-    tourImages: {
-      type: 'array',
-      default: function() {
-        return [
-          { file: 'f1.gif', duration: 4000 },
-          { file: 'f2.gif', duration: 9000 },
-          { file: 'f3.gif', duration: 9000 },
-          { file: 'f4.gif', duration: 6000 },
-          { file: 'f5.gif', duration: 9000 }
-        ];
-      }
     },
     tourImagesFolder: {
       type: 'string',
@@ -31,50 +15,66 @@ var TourView = View.extend({
   template: require('./index.jade'),
   events: {
     'click #features ul': 'showFeature',
+    'click .previous-slide': 'showPreviousFeature',
+    'click .next-slide': 'showNextFeature',
     'click #tour-remove': 'tourRemove',
     'click #tour-bg': 'tourRemove'
   },
   render: function() {
-    var that = this;
     this.renderWithTemplate(this);
     this.$featuresUL = this.query('#features ul');
     this.$featuresLI = this.queryAll('#features li');
     this.$animationGIF = this.query('#animation-gif');
     this.$tourRemove = this.query('#tour-remove');
-    function showAnimation() {
-      var duration = that.tourImages[that.tourCount].duration;
-      // deselect old
-      $('#features li.selected').removeClass('selected');
-      // select new
-      that.$featuresLI[that.tourCount].className = 'selected';
-      that.$animationGIF.src = that.tourImagesFolder + that.tourImages[that.tourCount].file;
-      if (that.tourCount === 4) {
-        that.tourCount = 0;
-      } else {
-        that.tourCount++;
-      }
-      if (that.isAnimating) {
-        setTimeout(showAnimation, duration);
-      }
-    }
-    showAnimation();
   },
   showFeature: function(ev) {
-    this.isAnimating = false;
     var nCLick = ev.target.getAttribute('data-n');
     if (nCLick === null) {
       return false;
     }
     var nFeature = parseInt(nCLick, 10);
+    
     // deselect old
     $('#features li.selected').removeClass('selected');
+    $('.feature-content.active').removeClass('active');
+    
     // select new
     ev.target.className = 'selected';
     this.$animationGIF.src = this.tourImagesFolder + ev.target.id + '.gif';
+    $('.feature-content#f' + nFeature + '-content').addClass('active');
     this.tourCount = nFeature;
   },
+  showPreviousFeature: function(ev) {
+    var currentFeature = this.tourCount;
+    var previousFeature = this.tourCount - 1;
+    
+    // deselect old
+    $('#features li.selected').removeClass('selected');
+    $('.feature-content.active').removeClass('active');
+    
+    // select new
+    $('#features li#f' + previousFeature).addClass('selected');
+    this.$animationGIF.src = this.tourImagesFolder + 'f' + previousFeature + '.gif';
+    $('.feature-content#f' + previousFeature + '-content').addClass('active');
+
+    this.tourCount = previousFeature;
+  },
+  showNextFeature: function(ev) {
+    var currentFeature = this.tourCount;
+    var nextFeature = this.tourCount + 1;
+    
+    // deselect old
+    $('#features li.selected').removeClass('selected');
+    $('.feature-content.active').removeClass('active');
+    
+    // select new
+    $('#features li#f' + nextFeature).addClass('selected');
+    this.$animationGIF.src = this.tourImagesFolder + 'f' + nextFeature + '.gif';
+    $('.feature-content#f' + nextFeature + '-content').addClass('active');
+
+    this.tourCount = nextFeature;
+  },
   tourRemove: function() {
-    this.isAnimating = false;
     this.remove();
   }
 });
