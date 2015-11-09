fccccc609a179acb919534ed6a89550acc74228c
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-11-08 21:39:40 -0500

    zap: test all the things.

--- packages/instance-model/fetch.js
+++ packages/instance-model/fetch.js
@@ -1,30 +1,135 @@
+var _ = require('lodash');
 var async = require('async');
+var toNS = require('mongodb-ns');
+var isNotAuthorized = require('mongodb-js-errors').isNotAuthorized;
 var debug = require('debug')('mongodb-instance:fetch');
 var ReadPreference = require('mongodb-read-preference');
 
+function rollupAllDatabaseStats(databases, fn) {
+  var keys = [
+    'document_count',
+    'storage_size',
+    'index_count',
+    'index_size'
+  ];
+  var stats = {};
+
+  keys.map(function(k) {
+    stats[k] = 0;
+  });
+
+  databases.map(function(db) {
+    keys.map(function(k) {
+      stats[k] += db[k];
+    });
+  });
+  fn(null, stats);
+}
+
+/**
+ * @example
+ *
+ * @param {Object} resp - Result of `db.admin().command({buildInfo: 1})`.
+ * @return {Object}
+ */
+function parseBuildInfo(resp) {
+  var res = {
+    version: resp.version,
+    commit: resp.gitVersion,
+    commit_url: 'https://github.com/mongodb/mongo/commit/' + resp.gitVersion,
+    flags_loader: resp.loaderFlags,
+    flags_compiler: resp.compilerFlags,
+    allocator: resp.allocator,
+    javascript_engine: resp.javascriptEngine,
+    debug: resp.debug,
+    for_bits: resp.bits,
+    max_bson_object_size: resp.maxBsonObjectSize,
+    enterprise_module: false
+  };
+  if (resp.modules && resp.modules.indexOf('enterprise') !== -1) {
+    res.enterprise_module = true;
+  }
+  return res;
+}
+
 function getBuildInfo(db, done) {
   debug('checking we can get buildInfo...');
-  db.admin().buildInfo(function(err) {
+  db.admin().buildInfo(function(err, res) {
     if (err) {
       debug('buildInfo failed!', err);
       err.command = 'buildInfo';
       return done(err);
     }
-    done();
+    done(null, parseBuildInfo(res));
   });
 }
 
+/**
+ * @param {Object} resp - Result of `db.admin().command({hostInfo: 1})`.
+ * @return {Object}
+ */
+function parseHostInfo(resp) {
+  return {
+    system_time: resp.system.currentTime,
+    hostname: resp.system.hostname,
+    os: resp.os.name,
+    os_family: resp.os.type.toLowerCase(),
+    kernel_version: resp.os.version,
+    kernel_version_string: resp.extra.versionString,
+    memory_bits: resp.system.memSizeMB * 1024 * 1024,
+    memory_page_size: resp.extra.pageSize,
+    arch: resp.system.cpuArch,
+    cpu_cores: resp.system.numCores,
+    cpu_cores_physical: resp.extra.physicalCores,
+    cpu_scheduler: resp.extra.scheduler,
+    cpu_frequency: resp.extra.cpuFrequencyMHz * 1000000,
+    cpu_string: resp.extra.cpuString,
+    cpu_bits: resp.system.cpuAddrSize,
+    machine_model: resp.extra.model,
+    feature_numa: resp.system.numaEnabled,
+    /* `alwaysFullSync` seen as synchronous :p */
+    /* eslint no-sync: 0 */
+    feature_always_full_sync: resp.extra.alwaysFullSync,
+    feature_nfs_async: resp.extra.nfsAsync
+  };
+}
+
+/**
+ * @param {mongo.Db} db
+ * @param {Function} done
+ *
+ * @example
+ *   { system_time: Sun Nov 08 2015 19:40:59 GMT-0500 (EST),
+ *     hostname: 'lucas.local',
+ *     os: 'Mac OS X',
+ *     os_family: 'darwin',
+ *     kernel_version: '14.5.0\u0000',
+ *     kernel_version_string: 'Darwin Kernel Version 14.5.0: Tue Sep  1 21:23:09 PDT 2015; root:xnu-2782.50.1~1/RELEASE_X86_64\u0000',
+ *     memory_bits: 17179869184,
+ *     memory_page_size: 4096,
+ *     arch: 'x86_64\u0000',
+ *     cpu_cores: 4,
+ *     cpu_cores_physical: 2,
+ *     cpu_scheduler: 'multiq\u0000',
+ *     cpu_frequency: 2800000000,
+ *     cpu_string: 'Intel(R) Core(TM) i7-4558U CPU @ 2.80GHz\u0000',
+ *     cpu_bits: 64,
+ *     machine_model: 'MacBookPro11,1\u0000',
+ *     feature_numa: false,
+ *     feature_always_full_sync: 0,
+ *     feature_nfs_async: 0 }
+ */
 function getHostInfo(db, done) {
   debug('checking we can get hostInfo...');
   var spec = {
     hostInfo: 1
   };
   var options = {};
-  db.admin().command(spec, options, function(err) {
+  db.admin().command(spec, options, function(err, res) {
     if (err) {
       if (/^not authorized/.test(err.message)) {
         debug('hostInfo unavailable for this user and thats ok!');
-        done();
+        done(null, {});
         return;
       }
       debug('driver error', err);
@@ -33,7 +138,7 @@ function getHostInfo(db, done) {
       return;
     }
     debug('got hostInfo successully!');
-    done();
+    done(null, parseHostInfo(res));
   });
 }
 
@@ -49,19 +154,29 @@ function getDatabaseNames(db, done) {
 
   db.admin().command(spec, options, function(err, res) {
     if (err) {
+      if (isNotAuthorized(err)) {
+        debug('list databases failed.  Falling back to single database.');
+        done(null, [db.databaseName]);
+        return;
+      }
+
       debug('list database names failed', err);
       err.command = 'listDatabases';
-      return done(err);
+      done(err);
+      return;
     }
 
-    var names = res.databases.map(function(d) {
-      return d.name;
-    }).filter(function(name) {
-      if (name === 'admin') {
-        return false;
-      }
-      return true;
-    });
+    var names = res.databases
+      .map(function(d) {
+        return d.name;
+      });
+      // .filter(function(name) {
+      //   if (name === 'admin') {
+      //     return false;
+      //   }
+      //   return true;
+      // });
+
     debug('list database names succeeded!', {
       res: res,
       names: names
@@ -70,21 +185,59 @@ function getDatabaseNames(db, done) {
   });
 }
 
+function parseDatabase(resp) {
+  return {
+    _id: resp.db,
+    name: resp.db,
+    document_count: resp.objects,
+    storage_size: resp.storageSize,
+    index_count: resp.indexes,
+    index_size: resp.indexSize
+  };
+}
+
+/**
+ * @param {mongo.Db} db
+ * @param {String} [name]
+ * @param {Function} done
+
+ * @example
+ *   {
+ *     _id: 'yelp',
+ *     name: 'yelp',
+ *     document_count: 656617,
+ *     storage_size: 200200192,
+ *     index_count: 3,
+ *     index_size: 5496832
+ *   }
+ */
 function getDatabase(db, name, done) {
+  if (typeof name === 'function') {
+    done = name;
+    name = db.databaseName;
+  }
+
   var spec = {
     dbStats: 1
   };
+
   var options = {};
   debug('running dbStats for `%s`...', name);
-  db.db(name).command(spec, options, function(err) {
+  db.db(name).command(spec, options, function(err, res) {
     if (err) {
+      if (isNotAuthorized(err)) {
+        debug('dbStats failed.  Falling back to no stats.');
+        done(null, {});
+        return;
+      }
+
       debug('failed to get dbStats for `%s`', name, err);
       err.command = 'dbStats';
       done(err);
       return;
     }
     debug('got dbStats for `%s`', name);
-    done();
+    done(null, parseDatabase(res));
   });
 }
 
@@ -94,12 +247,34 @@ function getDatabases(db, done) {
     if (err) {
       return done(err);
     }
-    async.parallel(names.map(function(name) {
-      return getDatabase.bind(null, db, name);
+
+    async.parallel(_.map(names, function(name) {
+      return _.partial(getDatabase, db, name);
     }), done);
   });
 }
 
+function parseCollection(resp) {
+  var ns = toNS(resp.db + '.' + resp.name);
+  return {
+    _id: ns.toString(),
+    name: ns.collection,
+    database: ns.database
+  };
+}
+
+/**
+ * @param {mongo.Db} db
+ * @param {Function} done
+ * @example
+ *   [
+ *     { _id: 'test.INT_407', name: 'INT_407', database: 'test' },
+ *     { _id: 'test.test', name: 'test', database: 'test' },
+ *     { _id: 'yelp.business', name: 'business', database: 'yelp' },
+ *     { _id: 'yelp.checkin', name: 'checkin', database: 'yelp' },
+ *     { _id: 'yelp.user', name: 'user', database: 'yelp' }
+ *   ]
+ */
 function getDatabaseCollections(db, done) {
   debug('getDatabaseCollections...');
 
@@ -109,12 +284,15 @@ function getDatabaseCollections(db, done) {
 
   var spec = {};
 
-  db.listCollections(spec, options).toArray(function(err) {
+  db.listCollections(spec, options).toArray(function(err, res) {
     if (err) {
       err.command = 'listCollections';
       return done(err);
     }
-    done();
+    done(null, _.map(res, function(d) {
+      d.db = db.databaseName;
+      return parseCollection(d);
+    }));
   });
 }
 
@@ -129,27 +307,45 @@ function getAllCollections(db, done) {
       return getDatabaseCollections.bind(null, db.db(name));
     });
 
-    async.parallel(tasks, function(_err) {
+    async.parallel(tasks, function(_err, res) {
       if (_err) {
         debug('getCollections failed', _err);
         return done(_err);
       }
       debug('getCollections succeeded!');
-      done();
+      done(null, _.flatten(res));
     });
   });
 }
 
 
-module.exports = function(db, done) {
-  async.series([
-    getDatabaseNames.bind(null, db),
-    getDatabases.bind(null, db),
-    getAllCollections.bind(null, db),
-    getBuildInfo.bind(null, db),
-    getHostInfo.bind(null, db)
-  ], done);
-};
+function getInstanceDetail(db, fn) {
+  var tasks = {
+    databases: getDatabases.bind(null, db),
+    collections: getAllCollections.bind(null, db),
+    host: getHostInfo.bind(null, db),
+    build: getBuildInfo.bind(null, db)
+  };
+
+  async.parallel(tasks, function(err, res) {
+    if (err) {
+      return fn(err);
+    }
+
+    rollupAllDatabaseStats(res.databases, function(_err, stats) {
+      if (_err) {
+        return fn(_err);
+      }
+
+      res.stats = stats;
+
+      debug('instance detail is `%j`', res);
+      fn(null, res);
+    });
+  });
+}
+
+module.exports = getInstanceDetail;
 
 module.exports.getDatabaseNames = getDatabaseNames;
 module.exports.getAllCollections = getAllCollections;
---
+++ packages/instance-model/test/fetch.test.js
@@ -0,0 +1,171 @@
+var _ = require('lodash');
+var assert = require('assert');
+var Connection = require('mongodb-connection-model');
+var connect = Connection.connect;
+var format = require('util').format;
+var fetch = require('../fetch');
+var debug = require('debug')('mongodb-instance-model:test:fetch');
+
+var fixtures = require('mongodb-connection-fixture').MATRIX.map(function(model) {
+  return new Connection(model);
+});
+describe('mongodb-instance-model#fetch', function() {
+  describe('local', function() {
+    var db;
+    before(require('mongodb-runner/mocha/before')());
+    after(function() {
+      if (db) {
+        db.close();
+      }
+    });
+    it('should connect to `localhost:27017`', function(done) {
+      var model = Connection.from('mongodb://localhost:27017');
+      connect(model, function(err, _db) {
+        if (err) {
+          return done(err);
+        }
+        db = _db;
+        done();
+      });
+    });
+    it('should list collections', function(done) {
+      assert(db);
+      fetch.getAllCollections(db, function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        debug('list collections', JSON.stringify(res, null, 2));
+        done();
+      });
+    });
+
+    it('should list databases', function(done) {
+      assert(db);
+      fetch.getDatabases(db, function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        debug('list databases', JSON.stringify(res, null, 2));
+        done();
+      });
+    });
+
+    it('should get build info', function(done) {
+      assert(db);
+      fetch.getBuildInfo(db, function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        debug('build info', JSON.stringify(res, null, 2));
+        done();
+      });
+    });
+
+    it('should get host info', function(done) {
+      assert(db);
+      fetch.getHostInfo(db, function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        debug('host info', JSON.stringify(res, null, 2));
+        done();
+      });
+    });
+
+    it('should get instance details', function(done) {
+      assert(db);
+      fetch(db, function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        debug('instance details', JSON.stringify(res, null, 2));
+        done();
+      });
+    });
+  });
+
+  if (fixtures.length > 0) {
+    describe('functional #slow', function() {
+      _.map(_.groupBy(fixtures, 'authentication'), function(models, authentication) {
+        describe(format('Using authentication `%s`', authentication), function() {
+          _.each(models, function(model) {
+            describe(model.name, function() {
+              var db;
+              it('should connect', function(done) {
+                if (process.env.dry) {
+                  this.skip();
+                  return;
+                }
+                this.slow(5000);
+                this.timeout(20000);
+
+                connect(model, function(err, _db) {
+                  if (err) {
+                    return done(err);
+                  }
+                  db = _db;
+                  done();
+                });
+              });
+
+              // it('should list databases', function(done) {
+              //   if (process.env.dry ) {
+              //     this.skip();
+              //     return;
+              //   }
+              //   this.slow(5000);
+              //   this.timeout(10000);
+              //   assert(db, 'requires successful connection');
+              //
+              //   fetch.getDatabases(db, function(err, res) {
+              //     if (err) return done(err);
+              //
+              //     assert(Array.isArray(res));
+              //     assert(res.length > 0, 'Database list is empty');
+              //     done();
+              //   });
+              // });
+              //
+              // it('should list collections', function(done) {
+              //   if (process.env.dry ) {
+              //     this.skip();
+              //     return;
+              //   }
+              //   this.slow(5000);
+              //   this.timeout(10000);
+              //   assert(db, 'requires successful connection');
+              //
+              //   fetch.getAllCollections(db, function(err, res) {
+              //     if (err) return done(err);
+              //
+              //     assert(Array.isArray(res));
+              //     assert(res.length > 0, 'Collection list is empty');
+              //     done();
+              //   });
+              // });
+
+
+              it('should get instance details', function(done) {
+                if (process.env.dry) {
+                  this.skip();
+                  return;
+                }
+
+                this.slow(5000);
+                this.timeout(10000);
+                assert(db, 'requires successful connection');
+                fetch(db, done);
+              });
+
+              after(function() {
+                if (db) {
+                  db.close();
+                }
+              });
+            });
+          });
+        });
+      });
+    });
+  }
+});
