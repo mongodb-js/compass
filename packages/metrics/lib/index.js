fa127e87734e65940fe422cdf81c20d708a81cfc
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-11-11 14:05:34 -0500

    tada: wire up to bugsnag + add debug messages

--- packages/metrics/lib/index.js
+++ packages/metrics/lib/index.js
@@ -1,9 +1,24 @@
 exports.bugsnag = require('./bugsnag');
 exports.ga = require('./ga');
 exports.intercom = require('./intercom');
+var debug = require('debug')('mongodb-js-metrics');
 
-exports.track = function() {};
-exports.error = function() {};
+exports.track = function(title, meta) {
+  debug('track', {
+    title: title,
+    meta: meta
+  });
+};
+
+exports.error = function(err, title, meta) {
+  debug('error', {
+    err: err,
+    title: title,
+    meta: meta
+  });
+
+  exports.bugsnag.notifyException(err, title, meta);
+};
 
 exports.listen = function(app) {
   if (!app || !app.isFeatureEnabled) {
