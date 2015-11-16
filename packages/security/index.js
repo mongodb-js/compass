d3397439a652cfc688a3058200d8f7dc7087efa6
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-11-16 17:58:11 -0500

    shirt:

--- packages/security/index.js
+++ packages/security/index.js
@@ -1,39 +1,41 @@
-var actions = require('./res/actions.json'),
-  roles = require('./res/roles.json');
+var actions = require('./res/actions.json');
+var roles = require('./res/roles.json');
 
-function inflateAction(name){
+var debug = require('debug')('mongodb-security');
+
+function inflateAction(name) {
   var def = actions[name];
   def._id = name;
   return def;
 }
 
 var special = {
-  db: function(s){
+  db: function(s) {
     return ['local', 'config'].indexOf(s) > -1;
   },
-  collection: function(s){
+  collection: function(s) {
     return ['system.profile', 'system.indexes', 'system.js', 'system.namespaces'].indexOf(s) > -1;
   }
 };
 
-module.exports.inflate = function(userOrRole){
+exports.inflate = function(userOrRole) {
   // merge `privileges` and `inheritedPrivileges` into one array.
   var privileges = [];
   privileges.push.apply(privileges, userOrRole.privileges, userOrRole.inheritedPrivileges);
-  privileges.sort(function(a, b){
+  privileges.sort(function(a, b) {
     return a.actions.length - b.actions.length;
   });
 
   var res = userOrRole;
   res.grants = [];
 
-  privileges.map(function(privilege){
-    if(special.db(privilege.resource.db)){
+  privileges.map(function(privilege) {
+    if (special.db(privilege.resource.db)) {
       debug('skip special db privilege', privilege.resource);
       return false;
     }
 
-    if(special.collection(privilege.resource.collection)){
+    if (special.collection(privilege.resource.collection)) {
       debug('skip special collection grant', privilege.resource);
       return false;
     }
@@ -43,10 +45,15 @@ module.exports.inflate = function(userOrRole){
       actions: {}
     };
 
-    privilege.actions.map(function(name){
-      grant.actions[name] = expandAction(name);
+    privilege.actions.map(function(name) {
+      grant.actions[name] = inflateAction(name);
     });
     res.grants.push(grant);
   });
   return res;
 };
+
+exports.actions = actions;
+exports.roles = roles;
+
+module.exports = exports;
