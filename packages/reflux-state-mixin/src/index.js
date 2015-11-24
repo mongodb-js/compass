60a439832d6764ec1930e4a9a97b4e1782268325
Yonatan Schreiber <yonatanmn@gmail.com>
2015-11-24 21:22:02 +0200

    index is now compatible with 0.5 and below

--- packages/reflux-state-mixin/src/index.js
+++ packages/reflux-state-mixin/src/index.js
@@ -1,13 +1,21 @@
 'use strict';
 
-var storeMixin = require('./storeMixin');
-var connectMixin = require('./connectMixin');
+import storeMixin from './storeMixin';
+import connectMixin from './connectMixin';
 import connectDecorator from './decorator';
 
-module.exports = {
-  store: storeMixin,
-  connect: connectMixin,
-  connectDecorator: connectDecorator
+//compatibility with versions below 0.6.0
+let port = () => {
+  storeMixin.connect = connectMixin;
+  return storeMixin;
 };
 
+//^0.6.0 index
+port.store = storeMixin;
+port.connect = connectMixin;
+port.connector = connectDecorator;
+
+//don't remove `module.exports = port`, to enable common-js-non-es6 way of importing : import {x} from '..'
+module.exports = port;
+export default port;
 
