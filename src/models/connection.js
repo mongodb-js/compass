026fe0c9ffeb127bcb05f39ba828ba59f6f131fa
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-11-24 17:19:14 -0500

    :tada: make connection startup much faster
    can now just use `test()` and avoid the extra ~5 seconds now that
    instance-model and connection-model handle all sorts of crazy edge
    cases for us.

--- src/models/connection.js
+++ src/models/connection.js
@@ -38,18 +38,6 @@ module.exports = Connection.extend({
   },
   test: function(done) {
     var model = this.serialize();
-
-    var onInstanceFetched = function(err, res) {
-      if (!err) {
-        debug('woot.  all gravy!  able to see %s collections', res.collections.length);
-        done(null, this);
-        return;
-      }
-      debug('could not get collection list :( sending to bugsnag for follow up...');
-      metrics.error(err, 'collection list failed');
-      done(err);
-    }.bind(this);
-
     var onTested = function(err) {
       if (err) {
         metrics.error(err, 'connection test failed');
@@ -57,9 +45,8 @@ module.exports = Connection.extend({
       }
 
       debug('test worked!');
-      debug('making sure we can get collection list...');
-      client(app.endpoint, model).instance(onInstanceFetched);
-    };
+      done(null, this);
+    }.bind(this);
 
     debug('Testing connection to `%j`...', model);
     client.test(app.endpoint, model, onTested);
