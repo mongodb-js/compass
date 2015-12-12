2a83b0bd723c9acb65c9c24451f8e6054f5536b6
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-12-12 13:14:27 -0500

    INT-923: handle maxTimeMS errors
    Merge pull request #279 from 10gen/INT-923-catch-maxTimeMS
    
    (cherry picked from commit b36d1161b4fa4a9cdbd2abbc79f7007bac4b8374)

--- src/home/collection.js
+++ src/home/collection.js
@@ -4,9 +4,11 @@ var FieldListView = require('../field-list');
 var DocumentListView = require('../document-list');
 var RefineBarView = require('../refine-view');
 var MongoDBCollection = require('../models/mongodb-collection');
+var QueryOptions = require('../models/query-options');
 var SampledSchema = require('../models/sampled-schema');
 var app = require('ampersand-app');
 var _ = require('lodash');
+var ms = require('ms');
 var remote = window.require('remote');
 var dialog = remote.require('dialog');
 var BrowserWindow = remote.require('browser-window');
@@ -110,7 +112,20 @@ var MongoDBCollectionView = View.extend({
     debug('updating namespace to `%s`', ns);
     this.schema.reset();
     this.schema.fetch(_.assign({}, app.volatileQueryOptions.serialize(), {
-      message: 'Analyzing documents...'
+      message: 'Analyzing documents...',
+      error: function(model, err) {
+        debug('Fetching schema had an error:', err);
+        var msg;
+        if (/operation exceeded time limit/.test(err.message)) {
+          msg = 'Your query took more than ' + ms(QueryOptions.DEFAULT_MAX_TIME_MS, {long: true}) +
+            ' to complete on the database. ' +
+            'As a safety measure, Compass aborts long-running queries. ' +
+            'This limit is currently not configurable.';
+        } else {
+          msg = err.message;
+        }
+        alert(msg);
+      }
     }));
     this.model.fetch();
   },
