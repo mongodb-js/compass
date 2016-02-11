b268bef7c91b7e1bf45e0433a047243ee4569c65
Durran Jordan <durran@gmail.com>
2016-02-11 13:18:29 -0500

    Adding find tests

--- packages/data-service/lib/native-client.js
+++ packages/data-service/lib/native-client.js
@@ -1,5 +1,54 @@
+var MongoClient = require('mongodb').MongoClient;
+
+/**
+ * Instantiate a new NativeClient object.
+ */
 function NativeClient(connection) {
   this.connection = connection;
+  var client = this;
+  MongoClient.connect(this.connection.driver_url).then(function(database) {
+    client.setDatabase(database);
+  });
 }
+
+/**
+ * The NativeClient API.
+ */
+NativeClient.prototype = (function() {
+  /**
+   * Get the collection name from a namespace.
+   */
+  var collectionName = function(ns) {
+    return ns.split('.')[1];
+  };
+
+  /**
+   * Get the database name from a namespace.
+   */
+  var databaseName = function(ns) {
+    return ns.split('.')[0];
+  };
+
+  return {
+    constructor: NativeClient,
+
+    /**
+     * Find documents for the provided filter and options on the collection.
+     */
+    find: function(ns, filter, options) {
+      return this.database
+        .db(databaseName(ns))
+        .collection(collectionName(ns))
+        .find(filter, options);
+    },
+
+    /**
+     * Set the database on the client once the connection is made.
+     */
+    setDatabase: function(database) {
+      this.database = database;
+    }
+  };
+})();
 
 module.exports = NativeClient;
--- packages/data-service/test/native-client.test.js
+++ packages/data-service/test/native-client.test.js
@@ -1,5 +1,6 @@
 var chai = require('chai');
-chai.should();
+var assert = require('assert');
+var expect = chai.expect;
 
 var NativeClient = require('../lib/native-client');
 var Connection = require('mongodb-connection-model');
@@ -8,49 +9,53 @@ describe('DataService', function() {
   before(require('mongodb-runner/mocha/before')({ port: 27018 }));
   after(require('mongodb-runner/mocha/after')());
 
-  var connection = new Connection({ hostname: '127.0.0.1', port: 27018 });
+  var connection = new Connection({ hostname: '127.0.0.1', port: 27018, ns: 'data-service' });
   var client = new NativeClient(connection);
 
   describe('#new', function() {
     it('sets the connection on the instance', function() {
-      return client.connection.should.equal(connection);
-    });
-  });
-
-  describe('#collections', function() {
-    it('returns all the collections for the database', function() {
-      // return service.collections('test').should.eventually.have.length(0);
-    });
-  });
-
-  describe('#databases', function() {
-    it('returns all the databases for the connection', function() {
-      // return service.databases().should.eventually.have.property('databases');
+      expect(client.connection).to.equal(connection);
     });
   });
 
   describe('#find', function() {
+    before(function() {
+      var collection = client.database.collection('test');
+      collection.insertMany([{ a: 1 }, { a: 2 }]);
+    });
+
+    after(function() {
+      var collection = client.database.collection('test');
+      collection.deleteMany();
+    });
+
     context('when a filter is provided', function() {
-      it('returns a cursor for the matching documents', function() {
-        // return service.find('compass-test.test', { 'name' : 'test' }).then(function(result) {
-          // return result.count().should.eventually.equal(0);
-        // });
+      it('returns a cursor for the matching documents', function(done) {
+        client.find('data-service.test', { a: 1 }).toArray(function(error, docs) {
+          assert.equal(null, error);
+          expect(docs.length).to.equal(1);
+          done();
+        });
       });
     });
 
     context('when no filter is provided', function() {
-      it('returns a cursor for all documents', function() {
-        // return service.find('compass-test.test').then(function(result) {
-          // return result.count().should.eventually.equal(0);
-        // });
+      it('returns a cursor for all documents', function(done) {
+        client.find('data-service.test').toArray(function(error, docs) {
+          assert.equal(null, error);
+          expect(docs.length).to.equal(2);
+          done();
+        });
       });
     });
 
     context('when options are provided', function() {
-      it('returns a cursor for the documents', function() {
-        // return service.find('compass-test.test', {}, { 'skip' : 5 }).then(function(result) {
-          // return result.count().should.eventually.equal(0);
-        // });
+      it('returns a cursor for the documents', function(done) {
+        client.find('data-service.test', {}, { skip: 1 }).toArray(function(error, docs) {
+          assert.equal(null, error);
+          expect(docs.length).to.equal(1);
+          done();
+        });
       });
     });
   });
