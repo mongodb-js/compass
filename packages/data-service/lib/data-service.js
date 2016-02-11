3f8936e4ea9336ffaad928e57c33bbb2b68a958b
Durran Jordan <durran@gmail.com>
2016-02-11 13:27:04 -0500

    Don't need chai as promised, yet.

--- packages/data-service/lib/data-service.js
+++ packages/data-service/lib/data-service.js
@@ -2,6 +2,9 @@ var NativeClient = require('./native-client');
 
 /**
  * Instantiate a new DataService object.
+ *
+ * @constructor
+ * @param {Connection} connection - The Connection model.
  */
 function DataService(connection) {
   this.client = new NativeClient(connection);
