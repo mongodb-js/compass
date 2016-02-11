67145774b973b52f61a61cfc15ca1af15d03418a
Durran Jordan <durran@gmail.com>
2016-02-11 15:20:53 -0500

    Flushing out DataService API with count/find/databases

--- packages/data-service/lib/data-service.js
+++ packages/data-service/lib/data-service.js
@@ -10,4 +10,47 @@ function DataService(connection) {
   this.client = new NativeClient(connection);
 }
 
+/**
+ * The DataService API.
+ */
+DataService.prototype = (function() {
+  return {
+    constructor: DataService,
+
+    /**
+     * Count the number of documents in the collection for the provided filter
+     * and options.
+     *
+     * @param {string} ns - The namespace to search on.
+     * @param {object} filter - The filter.
+     * @param {object} options - The query options.
+     * @returns {Promise} The count.
+     */
+    count: function(ns, filter, options) {
+      return this.client.count(ns, filter, options);
+    },
+
+    /**
+     * Get a list of databases for the server.
+     *
+     * @returns {Promise} The list of databases.
+     */
+    databases: function() {
+      return this.client.databases();
+    },
+
+    /**
+     * Find documents for the provided filter and options on the collection.
+     *
+     * @param {string} ns - The namespace to search on.
+     * @param {object} filter - The filter.
+     * @param {object} options - The query options.
+     * @returns {Cursor} The cursor.
+     */
+    find: function(ns, filter, options) {
+      return this.client.find(ns, filter, options);
+    }
+  };
+})();
+
 module.exports = DataService;
