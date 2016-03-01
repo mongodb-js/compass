c186457ce6c085ec5bbc11f72872815ca6b7a55b
Durran Jordan <durran@gmail.com>
2016-03-01 17:55:48 +0100

    Make wrapper es6

--- packages/data-service/lib/data-service.js
+++ packages/data-service/lib/data-service.js
@@ -1,4 +1,6 @@
-var NativeClient = require('./native-client');
+'use strict';
+
+const NativeClient = require('./native-client');
 
 /**
  * Instantiate a new DataService object.
@@ -6,51 +8,51 @@ var NativeClient = require('./native-client');
  * @constructor
  * @param {Connection} connection - The Connection model.
  */
-function DataService(connection) {
-  this.client = new NativeClient(connection);
+class DataService {
+
+  /**
+   * Instantiate a new DataService object.
+   *
+   * @constructor
+   * @param {Connection} connection - The Connection model.
+   */
+  constructor(connection) {
+    this.client = new NativeClient(connection);
+  }
+
+  /**
+   * Count the number of documents in the collection for the provided filter
+   * and options.
+   *
+   * @param {string} ns - The namespace to search on.
+   * @param {object} filter - The filter.
+   * @param {object} options - The query options.
+   * @returns {Promise} The count.
+   */
+  count(ns, filter, options) {
+    return this.client.count(ns, filter, options);
+  }
+
+  /**
+   * Get a list of databases for the server.
+   *
+   * @returns {Promise} The list of databases.
+   */
+  databases() {
+    return this.client.databases();
+  }
+
+  /**
+   * Find documents for the provided filter and options on the collection.
+   *
+   * @param {string} ns - The namespace to search on.
+   * @param {object} filter - The filter.
+   * @param {object} options - The query options.
+   * @returns {Cursor} The cursor.
+   */
+  find(ns, filter, options) {
+    return this.client.find(ns, filter, options);
+  }
 }
-
-/**
- * The DataService API.
- */
-DataService.prototype = (function() {
-  return {
-    constructor: DataService,
-
-    /**
-     * Count the number of documents in the collection for the provided filter
-     * and options.
-     *
-     * @param {string} ns - The namespace to search on.
-     * @param {object} filter - The filter.
-     * @param {object} options - The query options.
-     * @returns {Promise} The count.
-     */
-    count: function(ns, filter, options) {
-      return this.client.count(ns, filter, options);
-    },
-
-    /**
-     * Get a list of databases for the server.
-     *
-     * @returns {Promise} The list of databases.
-     */
-    databases: function() {
-      return this.client.databases();
-    },
-
-    /**
-     * Find documents for the provided filter and options on the collection.
-     *
-     * @param {string} ns - The namespace to search on.
-     * @param {object} filter - The filter.
-     * @param {object} options - The query options.
-     * @returns {Cursor} The cursor.
-     */
-    find: function(ns, filter, options) {
-      return this.client.find(ns, filter, options);
-    }
-  };
-})();
 
 module.exports = DataService;
