b4cb3b9bc56992fcf162b7f71e13e683f6d1421f
Thomas Rueckstiess <thomas@rueckstiess.net>
2016-03-04 16:00:58 +1100

    moved files into new folder structure

--- src/app/statusbar/schema-subview.js
+++ src/app/statusbar/schema-subview.js
@@ -1,14 +1,18 @@
 var View = require('ampersand-view');
 var ms = require('ms');
 var app = require('ampersand-app');
+var jade = require('jade');
+var path = require('path');
 
 // var debug = require('debug')('mongodb-compass:statusbar:schema-subview');
+var subviewTemplate = jade.compileFile(path.resolve(__dirname, 'schema-subview.jade'));
+
 
 var SHOW_STEPS_MS = 3000;
 var SHOW_ANALYZING_BUTTONS_MS = 10000;
 
 module.exports = View.extend({
-  template: require('./schema-subview.jade'),
+  template: subviewTemplate,
   props: {
     schema: {
       type: 'state',
