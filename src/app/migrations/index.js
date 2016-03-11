62c85a1dedff424edfcb9f0686d7ac497e0cac55
Thomas Rueckstiess <thomas@rueckstiess.net>
2016-03-11 12:47:47 +1100

    :green_heart: fix migrations tests

--- src/app/migrations/index.js
+++ src/app/migrations/index.js
@@ -30,45 +30,51 @@ function getPreviousVersion(done) {
   miniPrefs.fetch();
 }
 
-function migrate(done) {
+function migrateFromTo(previousVersion, currentVersion, done) {
   var tasks;
+  if (semver.lt(previousVersion, currentVersion)) {
+    // pick migration tasks for upgrade
+    debug('upgrading schema from version', previousVersion, 'to version', currentVersion);
+    tasks = _.pick(module.exports.migrations, function(fn, version) {
+      return semver.gt(version, previousVersion) &&
+             semver.lte(version, currentVersion);
+    });
+    tasks = _.mapValues(tasks, function(fn) {
+      return fn.bind(null, previousVersion, currentVersion);
+    });
+    debug('executing migration steps for versions %j', _.keys(tasks));
+    return async.series(tasks, done);
+  }
+  if (semver.gt(previousVersion, currentVersion)) {
+    // check if the downgrade is compatible with the schema
+    tasks = _.pick(module.exports.migrations, function(fn, version) {
+      return semver.gt(version, currentVersion) &&
+             semver.lte(version, previousVersion);
+    });
+    if (_.keys(tasks).length > 0) {
+      // schema incompatible, return error
+      return done(new Error(format('Downgrade from version %s to %s'
+        + ' not possible due to schema incompatibilities.', previousVersion,
+        currentVersion)));
+    }
+  }
+  done(null, {});
+}
+
+function migrate(done) {
   getPreviousVersion(function(err, previousVersion) {
     if (err) {
       done(err);
     }
     // strip any prerelease parts off
     var currentVersion = pkg.version.split('-')[0];
-    if (semver.lt(previousVersion, currentVersion)) {
-      // pick migration tasks for upgrade
-      debug('upgrading schema from version', previousVersion, 'to version', currentVersion);
-      tasks = _.pick(module.exports.migrations, function(fn, version) {
-        return semver.gt(version, previousVersion) &&
-               semver.lte(version, currentVersion);
-      });
-      tasks = _.mapValues(tasks, function(fn) {
-        return fn.bind(null, previousVersion, currentVersion);
-      });
-      debug('executing migration steps for versions %j', _.keys(tasks));
-      return async.series(tasks, done);
-    }
-    if (semver.gt(previousVersion, currentVersion)) {
-      // check if the downgrade is compatible with the schema
-      tasks = _.pick(module.exports.migrations, function(fn, version) {
-        return semver.gt(version, currentVersion) &&
-               semver.lte(version, previousVersion);
-      });
-      if (_.keys(tasks).length > 0) {
-        // schema incompatible, return error
-        return done(new Error(format('Downgrade from version %s to %s'
-          + ' not possible due to schema incompatibilities.', previousVersion,
-          currentVersion)));
-      }
-    }
-    done(null, {});
+    migrateFromTo(previousVersion, currentVersion, done);
   });
 }
 
 module.exports = migrate;
+module.exports.migrateFromTo = migrateFromTo;
+
 module.exports.migrations = {
   '1.1.2': require('./1.1.2')
 };
