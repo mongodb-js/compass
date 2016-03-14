7e700510d5f430bf9104afa06c11c6fce036a96c
Thomas Rueckstiess <thomas@rueckstiess.net>
2016-03-14 14:50:24 +1100

    remove obsolete line.

--- src/app/models/sampled-schema.js
+++ src/app/models/sampled-schema.js
@@ -140,9 +140,6 @@ module.exports = Schema.extend({
       schemaStatusSubview.error = true;
       schemaStatusSubview.showButtons();
 
-      // // destroy the stream
-      // this.destroy();
-
       // track error
       metrics.error(err);
 
