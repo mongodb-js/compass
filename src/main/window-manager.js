mongodb-js/compass

commit 8fd3378a8c5c30fe0ae0cb25300f54da9bf1a340

:fire: INT-998 INT-1248 Remove node-notifier
dead code

src/main/window-manager.js
@@ -5,7 +5,6 @@
 var electron = require('electron');
 var AppMenu = require('./menu');
 var BrowserWindow = electron.BrowserWindow;
-var Notifier = require('node-notifier');
 
 var _ = require('lodash');
 var app = electron.app;
@@ -253,24 +252,6 @@ app.on('renderer ready', function(arg, event) {
 app.on('before-quit', function() {
   debug('sending `app-quit` msg');
   BrowserWindow.getAllWindows()[0].webContents.send('message', 'app-quit');
-});
-
-app.on('show bugsnag OS notification', function(errorMsg) {
-  if (_.contains(['development', 'testing'], process.env.NODE_ENV)) {
-    Notifier.notify({
-      'icon': COMPASS_ICON_PATH,
-      'message': errorMsg,
-      'title': 'MongoDB Compass Exception',
-      'wait': true
-    }, function(err, resp) {
-      if (err) {
-        debug(err);
-      }
-      if (resp === 'Activate\n') {
-        openDevTools();
-      }
-    });
-  }
 });
 
 /**

commit c26cc70e817c730051e84252f8868ef41599a11d

:shirt: fix linter error introduced in 8fd3378

src/main/window-manager.js
@@ -19,11 +19,6 @@ var path = require('path');
 var RESOURCES = path.resolve(__dirname, '../app/');
 
 /**
- * TODO (imlucas) Missing this PNG.
- */
-var COMPASS_ICON_PATH = RESOURCES + '/images/mongodb-compass.png';
-
-/**
  * The app's HTML shell which is the output of `./src/index.html`
  * created by the `build:pages` gulp task.
  */
@@ -78,13 +73,6 @@ function isSingleInstance(_window) {
   });
 
   return !isNotSingle;
-}
-
-function openDevTools() {
-  debug('openDevTools()');
-  AppMenu.lastFocusedWindow.openDevTools({
-    detach: true
-  });
 }
 
 /**