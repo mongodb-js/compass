36bd6b3ac812ded52059f800b3078e50a347e9b3
Durran Jordan <durran@gmail.com>
2016-03-21 15:57:30 +0100

    Adding package class

--- src/export/compass-core.js
+++ src/export/compass-core.js
@@ -6,4 +6,4 @@
 module.exports.Action = require('../app/action');
 // module.exports.ComponentRegistry = require('../app/component-registry');
 // module.exports.PackageManager = require('../app/package-manager');
-// module.exports.Package = require('../app/package');
+module.exports.Package = require('../app/package');
