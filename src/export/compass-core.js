83ffdb814143e3277e8b06874fcd6446225085fa
Durran Jordan <durran@gmail.com>
2016-03-31 17:33:46 +0200

    Use package manager module

--- src/export/compass-core.js
+++ src/export/compass-core.js
@@ -5,5 +5,3 @@
  */
 module.exports.Action = require('../app/action');
 module.exports.ComponentRegistry = require('../app/component-registry');
-module.exports.PackageManager = require('../app/package-manager');
-module.exports.Package = require('../app/package');
