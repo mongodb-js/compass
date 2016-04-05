db130e81a365abee094d47ea86c56712e873c59c
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-04-05 15:49:05 -0400

    :racehorse: Inline main application router

--- src/app/index.js
+++ src/app/index.js
@@ -42,13 +42,48 @@ var Connection = require('./models/connection');
 var MongoDBInstance = require('./models/mongodb-instance');
 var Preferences = require('./models/preferences');
 var User = require('./models/user');
-var Router = require('./router');
 var Statusbar = require('./statusbar');
 var migrateApp = require('./migrations');
 var metricsSetup = require('./metrics');
 var metrics = require('mongodb-js-metrics')();
 
 var addInspectElementMenu = require('debug-menu').install;
+
+var AmpersandRouter = require('ampersand-router');
+
+var Router = AmpersandRouter.extend({
+  routes: {
+    '': 'index',
+    schema: 'index',
+    connect: 'connect',
+    'schema/:ns': 'schema',
+    help: 'help',
+    'help/:entryId': 'help',
+    '(*path)': 'catchAll'
+  },
+  index: function() {
+    this.schema();
+  },
+  schema: function(ns) {
+    var HomePage = require('./home');
+    this.trigger('page', new HomePage({
+      ns: ns
+    }));
+  },
+  help: function(entryId) {
+    var HelpPage = require('./help');
+    this.trigger('page', new HelpPage({
+      entryId: entryId
+    }));
+  },
+  catchAll: function() {
+    this.redirectTo('');
+  },
+  connect: function() {
+    var ConnectPage = require('./connect');
+    this.trigger('page', new ConnectPage({}));
+  }
+});
 
 function getConnection(model, done) {
   function _fetch(fn) {
