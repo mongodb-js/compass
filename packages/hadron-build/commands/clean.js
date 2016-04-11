bfd2b5368b94d031fcacc6d0e81421309785c41c
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-04-11 11:58:51 -0400

    bug: :art: Clean reuses options from ui
    Clean now also removes the template cache.

--- packages/hadron-build/commands/clean.js
+++ packages/hadron-build/commands/clean.js
@@ -2,23 +2,22 @@
 /**
  * @see [Atom's clean-task.coffee](https://git.io/vaZLw)
  */
-const path = require('path');
+const _ = require('lodash');
 const del = require('del');
-const async = require('async');
 const abortIfError = require('../lib/abort-if-error');
-
-var COMPILED_LESS = path.join('src', 'app', 'compiled-less');
+const ui = require('./ui');
 
 exports.command = 'clean';
 
 exports.describe = 'Remove generated directories.';
 
-exports.builder = {};
+exports.builder = _.clone(ui.builder);
 
-exports.handler = function() {
-  async.parallel(['dist/', 'node_modules/', COMPILED_LESS].map(function(p) {
-    return function(cb) {
-      del(p).then(cb.bind(null, null));
-    };
-  }), abortIfError);
+exports.handler = function(argv) {
+  del([
+    'dist/',
+    'node_modules/',
+    argv.less_cache,
+    argv.template_cache
+  ]).catch(abortIfError);
 };
