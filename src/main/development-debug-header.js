da8a06e84b3d2a7ea474ce472115c032dfb339f7
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-04-15 11:07:17 -0400

    migrating to new main application

--- src/main/development-debug-header.js
+++
@@ -1,5 +0,0 @@
-// Injected into the dom before the app for setting up
-// global state if `NODE_ENV === 'development'`.
-
-// Enable debugging so messages show up in the devtools console.
-localStorage.setItem('debug', 'mon*,sco*');
