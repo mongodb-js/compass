3287c9b8ec27ea31b4e662c419fd053b5eff05d4
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-04-24 18:06:28 -0400

    art: refactoring

---
+++ packages/hadron-ipc/lib/main.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const getResponseChannel = require('./common').getResponseChannel;
+const _ = require('lodash');
+const isPromise = require('is-promise');
+const electron = require('electron');
+const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
+const debug = require('debug')('hadron-ipc:main');
+
+exports.respondTo = (methodName, handler) => {
+  if (_.isPlainObject(methodName)) {
+    _.forIn(methodName, (methodHandler, name) => {
+      exports.respondTo(name, methodHandler);
+    });
+    return exports;
+  }
+
+  const responseChannel = getResponseChannel(methodName);
+  const errorResponseChannel = `${responseChannel}-error`;
+
+  ipcMain.on(methodName, (event, ...args) => {
+    const browserWindow = BrowserWindow.fromWebContents(event.sender);
+    const resolve = (result) => {
+      debug(`responding with result for ${methodName}`, result);
+      event.sender.send(responseChannel, result);
+    };
+    const reject = (err) => {
+      debug(`responding with error for ${methodName}`, err);
+      event.sender.send(errorResponseChannel, err);
+    };
+
+    debug(`calling ${methodName} handler with args`, args);
+    const res = handler(browserWindow, ...args);
+    if (isPromise(res)) {
+      res.then(resolve).catch(reject);
+    } else if (res instanceof Error) {
+      reject(res);
+    } else {
+      resolve(res);
+    }
+  });
+  return exports;
+};
+
+exports.broadcast = (methodName, ...args) => {
+  BrowserWindow.getAllWindows().forEach((_win) => {
+    if (_win.webContents) {
+      _win.webContents.send(methodName, ...args);
+    }
+  });
+};
---
+++ packages/hadron-ipc/lib/renderer.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const getResponseChannel = require('./common').getResponseChannel;
+const electron = require('electron');
+const ipcRenderer = electron.ipcRenderer;
+const debug = require('debug')('hadron-ipc:main');
+
+exports.call = function(methodName, ...args) {
+  debug(`calling ${methodName} with args`, args);
+  const responseChannel = getResponseChannel(methodName);
+  const errorResponseChannel = `${responseChannel}-error`;
+
+  return new Promise(function(resolve, reject) {
+    ipcRenderer.on(responseChannel, function(event, result) {
+      debug(`got response for ${methodName} from main`, result);
+      ipcRenderer.removeAllListeners(responseChannel);
+      ipcRenderer.removeAllListeners(errorResponseChannel);
+      resolve(result);
+    });
+
+    ipcRenderer.on(errorResponseChannel, function(event, err) {
+      debug(`error for ${methodName} from main`, err);
+      ipcRenderer.removeAllListeners(responseChannel);
+      ipcRenderer.removeAllListeners(errorResponseChannel);
+      reject(err);
+    });
+
+    ipcRenderer.send(methodName, ...args);
+  });
+};
