96b6870187400aaacc815e1952bef4a70c0c18af
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-05-06 14:50:49 -0400

    checkpoint railcars scripting

--- packages/hadron-build/lib/railcars.js
+++ packages/hadron-build/lib/railcars.js
@@ -1,92 +1,97 @@
-// var fs = require('fs')
-// var exec = require('child_process').exec
-// var series = require('async').series
-// var semver = require('semver')
+// 'use strict';
 //
-// function run (command) {
-//   return function (next) {
-//     console.log('>', command)
-//     exec(command, next)
-//   }
-// }
+// const fs = require('fs');
+// const exec = require('child_process').exec;
+// const semver = require('semver');
+// const cli = require('mongodb-js-cli')('hadron-build:railcars');
 //
-// function section (message) {
-//   return function (next) {
-//     console.log()
-//     console.log(message)
-//     next()
-//   }
-// }
+// const getCurrentVersion = () => {
+//   /* eslint no-sync: 0 */
+//   return JSON.parse(fs.readFileSync(require.resolve('../package.json'))).version;
+// };
 //
-// function checkCleanWorkingTree (next) {
-//   run('git status --porcelain')(function (error, output) {
-//     if (error) return next(error)
-//     if (output.trim().length > 0) return next(new Error('Cannot run the railcars with a dirty working tree'))
-//     next()
-//   })
-// }
+// const run = (bin, args) => {
+//   return new Promise((resolve, reject) => {
+//     cli.info(`> ${bin} ${args.join(' ')}`);
+//     exec(bin, args, (err, stdout, stderr) => {
+//       if (err) {
+//         return reject(err);
+//       }
+//       resolve({
+//         stdout: stdout,
+//         stderr: stderr
+//       });
+//     });
+//   });
+// };
 //
-// function bumpStableVersion (next) {
-//   var newVersion = getCurrentVersion().replace(/-beta.*$/, '')
-//   run('npm version ' + newVersion)(next)
-// }
+// const section = (message) => {
+//   return new Promise((resolve) => {
+//     cli.info(message);
+//     resolve(message);
+//   });
+// };
 //
-// function bumpBetaVersion (next) {
-//   var newVersion = getCurrentVersion().replace(/-dev$/, '-beta0')
-//   run('npm version ' + newVersion)(next)
-// }
+// const checkCleanWorkingTree = () => {
+//   return run('git', ['status', '--porcelain'])
+//     .then((res) => {
+//       if (res.stdout.trim().length > 0) {
+//         throw new Error('Cannot run the railcars with a dirty working tree');
+//       }
+//     });
+// };
 //
-// function bumpDevVersion (next) {
-//   var newVersion = semver.inc(getCurrentVersion(), 'preminor', 'dev').replace(/\.0$/, '')
-//   series([
-//     run('npm --no-git-tag-version version ' + newVersion),
-//     run('git commit -am "' + newVersion + '"')
-//   ], next)
-// }
+// const bumpStableVersion = () => {
+//   const newVersion = getCurrentVersion().replace(/-beta.*$/, '');
+//   return run('npm', ['version', newVersion]);
+// };
 //
-// function finish (error) {
-//   if (error) {
-//     console.log('Error: ' + error.message)
-//     process.exit(1)
-//   }
+// const bumpBetaVersion = () => {
+//   const newVersion = getCurrentVersion().replace(/-dev$/, '-beta0');
+//   return run('npm', ['version', newVersion]);
+// };
 //
-//   console.log('OK, now just wait for all CI builds to pass on beta and stable')
-// }
+// const bumpDevVersion = () => {
+//   const newVersion = semver.inc(getCurrentVersion(), 'preminor', 'dev').replace(/\.0$/, '');
+//   return run('npm', ['--no-git-tag-version', 'version', newVersion])
+//     .then(run('git', ['commit', '-am', newVersion]));
+// };
 //
-// function getCurrentVersion () {
-//   return JSON.parse(fs.readFileSync(require.resolve('../package.json'))).version
-// }
+// section('Preparing to roll the railcars')
+//   .then(checkCleanWorkingTree)
+//   .then(run('git', ['checkout', 'master']))
+//   .then(run('git', ['pull', '--ff-only', 'origin', 'master']))
+//   .then(run('git', ['fetch', 'origin', 'beta:beta', 'stable:stable']))
+//   .then(run('git', ['fetch', 'origin', '--tags']))
 //
-// series([
-//   section('Preparing to roll the railcars'),
-//   checkCleanWorkingTree,
-//   run('git checkout master'),
-//   run('git pull --ff-only origin master'),
-//   run('git fetch origin beta:beta stable:stable'),
-//   run('git fetch origin --tags'),
-//
-//   section('Checking that merges will be fast-forwards'),
-//   run('git branch --contains beta | grep master'),
-//   run('git branch --contains stable | grep beta'),
-//
-//   section('Updating stable branch'),
-//   run('git checkout stable'),
-//   run('git merge --ff-only origin/beta'),
-//   bumpStableVersion,
-//
-//   section('Updating beta branch'),
-//   run('git checkout beta'),
-//   run('git merge --ff-only origin/master'),
-//   run('git merge --strategy ours origin/stable'),
-//   bumpBetaVersion,
-//
-//   section('Updating master branch'),
-//   run('git checkout master'),
-//   run('git merge --ff-only origin/master'),
-//   run('git merge --strategy ours origin/beta'),
-//   bumpDevVersion,
-//
-//   section('Pushing changes upstream'),
-//   run('git push origin master:master beta:beta stable:stable'),
-//   run('git push origin --tags')
-// ], finish)
+//   .then(section('Checking that merges will be fast-forwards'))
+//   // run('git branch --contains beta | grep master'),
+//   // run('git branch --contains stable | grep beta'),
+//   //
+//   // section('Updating stable branch'),
+//   // run('git checkout stable'),
+//   // run('git merge --ff-only origin/beta'),
+//   // bumpStableVersion,
+//   //
+//   // section('Updating beta branch'),
+//   // run('git checkout beta'),
+//   // run('git merge --ff-only origin/master'),
+//   // run('git merge --strategy ours origin/stable'),
+//   // bumpBetaVersion,
+//   //
+//   // section('Updating master branch'),
+//   // run('git checkout master'),
+//   // run('git merge --ff-only origin/master'),
+//   // run('git merge --strategy ours origin/beta'),
+//   // bumpDevVersion,
+//   //
+//   // section('Pushing changes upstream'),
+//   // run('git push origin master:master beta:beta stable:stable'),
+//   // run('git push origin --tags')
+// .then( () => {
+//   /**
+//    * TODO (imlucas) Use evergreen api to show status of builds.
+//    */
+//   cli.ok('now just wait for all CI builds to pass on beta and stable');
+// })
+// .catch( (err) => cli.abortIfError(err));
