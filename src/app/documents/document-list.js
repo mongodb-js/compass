f36439e1b897535c14bf834a6081bcd4359ed7ee
Durran Jordan <durran@gmail.com>
2016-05-15 13:08:15 +0200

    Remove scroll handler

--- src/app/documents/document-list.js
+++ src/app/documents/document-list.js
@@ -26,38 +26,14 @@ var DocumentListView = View.extend({
   },
   render: function() {
     ReactDOM.render(React.createElement(this.documentList), this.el.parentNode);
-    // this.renderCollection(this.documents, DocumentListItemView,
-      // this.queryByHook('document-list-container'));
-
-    // var scrollContainer = this.el.parentNode;
-    // scrollContainer.addEventListener('scroll', function() {
-      // this.onViewerScroll(scrollContainer);
-    // }.bind(this));
-
     return this;
-  },
-  /**
-   * When the DOM element scrolls, check to see if it's within 100px of the bottom.
-   * If it is, load more documents.
-   *
-   * @param {Element} docListEl The element in the DOM that was scrolled.
-   */
-  onViewerScroll: function(docListEl) {
-    // scrollHeight: total height including off-screen content.
-    // scrollTop:    total distance between the top and the visible content.
-    // clientHeight: total height of the element on screen.
-    // if (docListEl.scrollHeight - docListEl.scrollTop <= docListEl.clientHeight + 100) {
-      // this.loadDocuments();
-    // }
   },
   reset: function() {
     Action.filterChanged({});
     this.loading = false;
   },
   remove: function() {
-    // var scrollContainer = this.el.parentNode;
-    // scrollContainer.removeEventListener('scroll');
-    // View.prototype.remove.call(this);
+    View.prototype.remove.call(this);
   }
 });
 module.exports = DocumentListView;
