41eca9c79b75db0998fe47b97c54dc2ca13992fa
Durran Jordan <durran@gmail.com>
2016-05-16 22:25:19 +0200

    Refactor element components to component registry

--- src/internal-packages/crud/index.js
+++ src/internal-packages/crud/index.js
@@ -3,21 +3,11 @@
 const app = require('ampersand-app');
 const DocumentList = require('./lib/component/document-list');
 
-const DocumentListItem = DocumentList.DocumentListItem;
-const ArrayElement = DocumentListItem.ArrayElement;
-const DateElement = DocumentListItem.DateElement;
-const ObjectElement = DocumentListItem.ObjectElement;
-const StringElement = DocumentListItem.StringElement;
-
 /**
  * Activate all the components in the CRUD package.
  */
 function activate() {
   app.componentRegistry.register(DocumentList, { role: 'Collection:DocumentList' });
-  app.componentRegistry.register(ArrayElement, { role: 'DocumentListItem:Type:Array' });
-  app.componentRegistry.register(DateElement, { role: 'DocumentListItem:Type:Date' });
-  app.componentRegistry.register(ObjectElement, { role: 'DocumentListItem:Type:Object' });
-  app.componentRegistry.register(StringElement, { role: 'DocumentListItem:Type:String' });
 }
 
 /**
@@ -25,10 +15,6 @@ function activate() {
  */
 function deactivate() {
   app.componentRegistry.deregister(DocumentList);
-  app.componentRegistry.deregister(ArrayElement);
-  app.componentRegistry.deregister(DateElement);
-  app.componentRegistry.deregister(ObjectElement);
-  app.componentRegistry.deregister(StringElement);
 }
 
 module.exports.activate = activate;
--- src/internal-packages/crud/lib/component/document-list.jsx
+++ src/internal-packages/crud/lib/component/document-list.jsx
@@ -1,43 +1,12 @@
 'use strict';
 
 const _ = require('lodash');
-const moment = require('moment');
 const React = require('react');
 const ReactDOM = require('react-dom');
 const app = require('ampersand-app');
-const component = require('hadron-component-registry');
+const ElementFactory = require('hadron-component-registry').ElementFactory;
 const Action = require('hadron-action');
-const Element = component.Element;
-const ExpandableElement = component.ExpandableElement;
-const Waypoint = require('react-waypoint');
-const TypeChecker = require('../model/type-checker');
 const DocumentListStore = require('../store/document-list-store');
-
-/**
- * Get an array of elements for the provided object.
- *
- * @param {Object} object - The object to get the elements from.
- *
- * @returns {Array} An array of element React components.
- */
-function elements(object) {
-  return _.map(object, (value, field) => {
-    var type = TypeChecker.type(value);
-    var elementProps = { field: field, value: value, type: type, key: `${object._id}_${field}` };
-    return React.createElement(elementComponent(type), elementProps);
-  });
-}
-
-/**
- * Get the element component for the type.
- *
- * @param {String} type - The type of the value.
- *
- * @returns {React.Component} The component for the type.
- */
-function elementComponent(type) {
-  return app.componentRegistry.findByRole(`DocumentListItem:Type:${type}`)[0] || Element;
-}
 
 /**
  * The full document list container class.
@@ -196,86 +165,9 @@ class DocumentListItem extends React.Component {
     return (
       <li className={LIST_ITEM_CLASS}>
         <ol className={DOCUMENT_CLASS}>
-          {elements(this.props.doc)}
+          {ElementFactory.elements(this.props.doc)}
         </ol>
       </li>
-    );
-  }
-}
-/**
- * Component for array types.
- */
-class ArrayElement extends React.Component {
-
-  /**
-   * Render an array element.
-   */
-  render() {
-    return (
-      <ExpandableElement
-        elements={elements(this.props.value)}
-        field={this.props.field}
-        value={this.props.value}
-        type={this.props.type}
-        label={`${this.props.type}[${this.props.value.length}]`} />
-    );
-  }
-}
-
-/**
- * Component for date types.
- */
-class DateElement extends React.Component {
-
-  /**
-   * Render a date element.
-   */
-  render() {
-    var date = moment(this.props.value).format('LLL');
-    return (
-      <Element field={this.props.field} value={date} type={this.props.type} />
-    );
-  }
-}
-
-/**
- * Component for object types.
- */
-class ObjectElement extends React.Component {
-
-  /**
-   * Render an object element.
-   */
-  render() {
-    return (
-      <ExpandableElement
-        elements={elements(this.props.value)}
-        field={this.props.field}
-        value={this.props.value}
-        type={this.props.type}
-        label={this.props.type} />
-    );
-  }
-}
-
-/**
- * The elipsis constant.
- */
-const ELIPSIS = '...';
-
-/**
- * Component for string types.
- */
-class StringElement extends React.Component {
-
-  /**
-   * Render a string element.
-   */
-  render() {
-    var string = this.props.value.length > 500 ?
-      this.props.value.substring(0, 500) + ELIPSIS : this.props.value;
-    return (
-      <Element field={this.props.field} value={string} type={this.props.type} />
     );
   }
 }
@@ -283,20 +175,12 @@ class StringElement extends React.Component {
 /**
  * Set the display names for all components.
  */
-ArrayElement.displayName = 'ArrayElement';
-DateElement.displayName = 'DateElement';
 DocumentList.displayName = 'DocumentList';
 DocumentListItem.displayName = 'DocumentListItem';
-ObjectElement.displayName = 'ObjectElement';
-StringElement.displayName = 'StringElement';
 
 /**
  * Set the child components.
  */
 DocumentList.DocumentListItem = DocumentListItem;
-DocumentListItem.ArrayElement = ArrayElement;
-DocumentListItem.DateElement = DateElement;
-DocumentListItem.ObjectElement = ObjectElement;
-DocumentListItem.StringElement = StringElement;
 
 module.exports = DocumentList;
