f6df7f33de40c2d180fc6d05804eedf946bfa2ad
Durran Jordan <durran@gmail.com>
2016-05-16 22:22:44 +0200

    Adding more exports

--- packages/hadron-app-registry/index.js
+++ packages/hadron-app-registry/index.js
@@ -8,6 +8,11 @@ require('babel-register')({
 const ComponentRegistry = require('./lib/component-registry');
 module.exports = ComponentRegistry;
 module.exports.ComponentRegistry = ComponentRegistry;
+module.exports.ArrayElement = require('./lib/component/element/array-element');
+module.exports.DateElement = require('./lib/component/element/date-element');
+module.exports.ObjectElement = require('./lib/component/element/object-element');
+module.exports.StringElement = require('./lib/component/element/string-element');
+module.exports.ElementFactory = require('./lib/component/element/factory');
 module.exports.Element = require('./lib/component/element');
 module.exports.ExpandableElement = require('./lib/component/expandable-element');
 module.exports.Field = require('./lib/component/field');
