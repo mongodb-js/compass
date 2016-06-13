9e785f98c0c4d79168b1218e6fea365eb71ed926
Durran Jordan <durran@gmail.com>
2016-06-13 15:58:12 +0200

    Export type checker

--- packages/hadron-app-registry/index.js
+++ packages/hadron-app-registry/index.js
@@ -21,3 +21,4 @@ module.exports.Form = require('./lib/component/form');
 module.exports.GroupItem = require('./lib/component/group-item');
 module.exports.Group = require('./lib/component/group');
 module.exports.GroupedList = require('./lib/component/grouped-list');
+module.exports.TypeChecker = require('./lib/component/element/type-checker');
