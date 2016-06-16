f09022e2a1c7595fc991cf1133eb1d28ec36c2ff
Durran Jordan <durran@gmail.com>
2016-06-16 13:06:26 +0200

    INT-1160: Don't allow _id edits

--- src/internal-packages/crud/lib/component/editable-key.jsx
+++ src/internal-packages/crud/lib/component/editable-key.jsx
@@ -52,22 +52,32 @@ class EditableKey extends React.Component {
    * @param {Event} evt - The event.
    */
   handleChange(evt) {
-    this.element.rename(evt.target.value);
-    this.setState({ key: this.element.currentKey });
+    if (this.isEditable()) {
+      this.element.rename(evt.target.value);
+      this.setState({ key: this.element.currentKey });
+    }
   }
 
   /**
    * Handle focus on the key.
    */
   handleFocus() {
-    this.setState({ editing: true });
+    if (this.isEditable()) {
+      this.setState({ editing: true });
+    }
   }
 
   /**
    * Handle blur from the key.
    */
   handleBlur() {
-    this.setState({ editing: false });
+    if (this.isEditable()) {
+      this.setState({ editing: false });
+    }
+  }
+
+  isEditable() {
+    return this.element.key !== '_id'
   }
 
   /**
