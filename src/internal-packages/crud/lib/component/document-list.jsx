761ae4e1ad86aefb75e48bf212217e8cc87b5854
Durran Jordan <durran@gmail.com>
2016-06-22 14:01:13 -0400

    INT-1160: Splitting up document list global stores

--- src/internal-packages/crud/lib/component/document-list.jsx
+++ src/internal-packages/crud/lib/component/document-list.jsx
@@ -6,7 +6,8 @@ const ReactDOM = require('react-dom');
 const app = require('ampersand-app');
 const Action = require('hadron-action');
 const Document = require('./document');
-const DocumentListStore = require('../store/document-list-store');
+const ResetDocumentListStore = require('../store/reset-document-list-store');
+const LoadMoreDocumentsStore = require('../store/load-more-documents-store');
 
 /**
  * The full document list container class.
@@ -24,39 +25,31 @@ const SCROLL_EVENT = 'scroll';
 class DocumentList extends React.Component {
 
   /**
+   * Attach the scroll event to the parent container.
+   */
+  attachScrollEvent() {
+    this.documentListNode = ReactDOM.findDOMNode(this);
+    this.documentListNode.parentNode.addEventListener(
+      SCROLL_EVENT,
+      this.handleScroll.bind(this)
+    );
+  }
+
+  /**
    * Fetch the state when the component mounts.
    */
   componentDidMount() {
-    this._attachScrollEvent();
-    this.unsubscribe = DocumentListStore.listen((documents, reset, count) => {
-      if (reset) {
-        // If resetting, then we need to go back to page one with
-        // the documents as the filter changed. The loaded count and
-        // total count are reset here as well.
-        this.setState({
-          docs: this._documentListItems(documents),
-          currentPage: 1,
-          count: count,
-          loadedCount: documents.length
-        });
-      } else {
-        // If not resetting we append the documents to the existing
-        // list and increment the page. The loaded count is incremented
-        // by the number of new documents.
-        this.setState({
-          docs: this.state.docs.concat(this._documentListItems(documents)),
-          currentPage: (this.state.currentPage + 1),
-          loadedCount: (this.state.loadedCount + documents.length)
-        });
-      }
-    });
+    this.attachScrollEvent();
+    this.unsubscribeReset = ResetDocumentListStore.listen(this.handleReset.bind(this));
+    this.unsubscribeLoadMore = LoadMoreDocumentsStore.listen(this.handleLoadMore.bind(this));
   }
 
   /**
    * Unsibscribe from the document list store when unmounting.
    */
   componentWillUnmount() {
-    this.unsubscribe();
+    this.unsubscribeReset();
+    this.unsubscribeLoadMore();
   }
 
   /**
@@ -67,6 +60,64 @@ class DocumentList extends React.Component {
   constructor(props) {
     super(props);
     this.state = { docs: [], currentPage: 0 };
+  }
+
+  /**
+   * Handle the loading of more documents.
+   *
+   * @param {Array} documents - The next batch of documents.
+   */
+  handleLoadMore(documents) {
+    // If not resetting we append the documents to the existing
+    // list and increment the page. The loaded count is incremented
+    // by the number of new documents.
+    this.setState({
+      docs: this.state.docs.concat(this.renderDocuments(documents)),
+      currentPage: (this.state.currentPage + 1),
+      loadedCount: (this.state.loadedCount + documents.length)
+    });
+  }
+
+  /**
+   * Handle the reset of the document list.
+   *
+   * @param {Array} documents - The documents.
+   * @param {Integer} count - The count.
+   */
+  handleReset(documents, count) {
+    // If resetting, then we need to go back to page one with
+    // the documents as the filter changed. The loaded count and
+    // total count are reset here as well.
+    this.setState({
+      docs: this.renderDocuments(documents),
+      currentPage: 1,
+      count: count,
+      loadedCount: documents.length
+    });
+  }
+
+  /**
+   * Handle the scroll event of the parent container.
+   *
+   * @param {Event} evt - The scroll event.
+   */
+  handleScroll(evt) {
+    var container = evt.srcElement;
+    if (container.scrollTop > (this.documentListNode.offsetHeight - this._scrollDelta())) {
+      // If we are scrolling downwards, and have hit the distance to initiate a scroll
+      // from the end of the list, we will fire the event to load more documents.
+      this.loadMore();
+    }
+  }
+
+  /**
+   * Get the next batch of documents. Will only fire if there are more documents
+   * in the collection to load.
+   */
+  loadMore() {
+    if (this.state.loadedCount < this.state.count) {
+      Action.fetchNextDocuments(this.state.currentPage);
+    }
   }
 
   /**
@@ -83,53 +134,16 @@ class DocumentList extends React.Component {
   }
 
   /**
-   * Attach the scroll event to the parent container.
-   */
-  _attachScrollEvent() {
-    this.documentListNode = ReactDOM.findDOMNode(this);
-    this.documentListNode.parentNode.addEventListener(
-      SCROLL_EVENT,
-      this._handleScroll.bind(this)
-    );
-  }
-
-  /**
    * Get the document list item components.
    *
    * @param {Array} docs - The raw documents.
    *
    * @return {Array} The document list item components.
    */
-  _documentListItems(docs) {
+  renderDocuments(docs) {
     return _.map(docs, (doc) => {
-      return (
-        <Document doc={doc} key={doc._id} />
-      );
+      return (<Document doc={doc} key={doc._id} />);
     });
-  }
-
-  /**
-   * Handle the scroll event of the parent container.
-   *
-   * @param {Event} evt - The scroll event.
-   */
-  _handleScroll(evt) {
-    var container = evt.srcElement;
-    if (container.scrollTop > (this.documentListNode.offsetHeight - this._scrollDelta())) {
-      // If we are scrolling downwards, and have hit the distance to initiate a scroll
-      // from the end of the list, we will fire the event to load more documents.
-      this._nextBatch();
-    }
-  }
-
-  /**
-   * Get the next batch of documents. Will only fire if there are more documents
-   * in the collection to load.
-   */
-  _nextBatch() {
-    if (this.state.loadedCount < this.state.count) {
-      Action.fetchNextDocuments(this.state.currentPage);
-    }
   }
 
   /**
---
+++ src/internal-packages/crud/lib/store/reset-document-list-store.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const Reflux = require('reflux');
+const app = require('ampersand-app');
+const NamespaceStore = require('hadron-reflux-store').NamespaceStore;
+const Action = require('hadron-action');
+
+/**
+ * The reflux store for resetting the document list.
+ */
+const ResetDocumentListStore = Reflux.createStore({
+
+  /**
+   * Initialize the reset document list store.
+   */
+  init: function() {
+    this.listenTo(Action.filterChanged, this.reset);
+  },
+
+  /**
+   * This function is called when the collection filter changes.
+   *
+   * @param {Object} filter - The query filter.
+   */
+  reset: function(filter) {
+    app.dataService.count(NamespaceStore.ns, filter, {}, (err, count) => {
+      var options = { limit: 20, sort: [[ '_id', 1 ]] };
+      app.dataService.find(NamespaceStore.ns, filter, options, (error, documents) => {
+        this.trigger(documents, count);
+      });
+    });
+  },
+});
+
+module.exports = ResetDocumentListStore;
