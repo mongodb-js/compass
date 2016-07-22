1623440abdf6a33a77598aed67dd7da107b09ff9
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-07-22 18:32:50 -0400

    INT-1590: Add CRUD and geo slides to the first-run tour (#440)
    * INT-1590 First run tour needs updating
    
    - Updated gifs with new tab options
    - Added CRUD gif
    - Fixed some copy
    
    * :construction: Fix explain plan image path

--- src/app/tour/features.js
+++ src/app/tour/features.js
@@ -2,63 +2,63 @@ module.exports = [
   {
     title: 'Choose a Collection',
     description: 'See a list of collections in the left sidebar. Select a collection and Compass will instantly start analyzing the schema data. Use the search filter at the top to narrow your list of collections.',
-    image: 'f0.gif',
+    image: 'choose-collection.gif',
     version: '1.0.0',
     initial: true
   },
   {
     title: 'Browse the Schema',
-    description: 'Once a collection is loaded Compass will visualize the collection schema. Field are listed as rows in the main view. The left side of the row displays the field name and datatype distribution, the right side displays a visualization of the data.',
-    image: 'f1.gif',
+    description: 'Once a collection is loaded, Compass will visualize the collection schema. Field are listed as rows in the main view. The left side of the row displays the field name and datatype distribution, the right side displays a visualization of the data.',
+    image: 'browse-schema.gif',
     version: '1.0.0',
     initial: true
   },
   {
     title: 'View Data Distribution',
     description: 'View the charts in the right-hand column of each row to see data distribution at a high level. Hover over charts to see more detail.',
-    image: 'f2.gif',
+    image: 'data-distribution.gif',
     version: '1.0.0',
     initial: false
   },
   {
     title: 'Build Queries',
     description: 'Click on charts to build MongoDB queries. Click and drag within bar charts to select multiple values. Edit your query by typing directly into the query bar.',
-    image: 'f3.gif',
+    image: 'build-queries.gif',
     version: '1.0.0',
     initial: true
   },
   {
     title: 'View Documents',
     description: 'Click the Documents tab to view the raw JSON documents in the result set.',
-    image: 'f4.gif',
+    image: 'documents.gif',
     version: '1.0.0',
     initial: true
   },
   {
     title: 'View Indexes',
     description: 'Click the Index tab to view the definition, attributes, size, and usage of your indexes.',
-    image: 'f5.gif',
+    image: 'indexes.gif',
     version: '1.2.0',
     initial: true
   },
   {
-    title: 'Examine Query Performance with Explain Plans',
+    title: 'Examine Query Performance with the Explain Plan',
     description: 'Click the Explain Plan tab to view a performance summary of the executed query and get access to the detailed explain plan report.',
-    image: 'f7.gif',
+    image: 'explain-plan.gif',
     version: '1.2.0',
-    initial: false
+    initial: true
   },
   {
-    title: 'Show Explain plans as tree charts',
-    description: 'In the Explain Plan tab, click on "Show as: Visual Tree" to see a tree chart of the explain plan.',
-    image: 'f8.gif',
+    title: 'Find using the Location Map',
+    description: 'Maps will display in collections with location data. Click and drag across the map for a geographic query. Click "Apply" to resample with the coordinates.',
+    image: 'geo.gif',
     version: '1.3.0-beta.1',
-    initial: false
+    initial: true
   },
   {
-    title: 'Geo Location Queries',
-    description: 'Collections with location data can be used as a filter. Click and drag across the map for a geographic query. Click "Apply" to resample with the coordinates.',
-    image: 'f6.gif',
+    title: 'CRUD Support',
+    description: 'From documents tab, make changes to your dataset. Click insert to add a document. Each document has clone or delete actions, along with the ability to edit the fields.',
+    image: 'crud.gif',
     version: '1.3.0-beta.1',
     initial: true
   }
