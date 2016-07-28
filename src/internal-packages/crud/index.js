7ceb961eeadb777c08e3a2d205dbf735f9a0b4ce
Durran Jordan <durran@gmail.com>
2016-07-28 15:23:25 +0200

    INT-1686: Use new app registry

--- src/internal-packages/crud/index.js
+++ src/internal-packages/crud/index.js
@@ -2,19 +2,24 @@
 
 const app = require('ampersand-app');
 const DocumentList = require('./lib/component/document-list');
+const Actions = require('./lib/actions');
 
 /**
  * Activate all the components in the CRUD package.
  */
 function activate() {
-  app.componentRegistry.register(DocumentList, { role: 'Collection:DocumentList' });
+  app.appRegistry.registerComponent('Component::CRUD::DocumentList', DocumentList);
+  app.appRegistry.registerAction('Action::CRUD::DocumentRemoved', Actions.documentRemoved);
+  app.appRegistry.registerAction('Action::CRUD::InsertDocument', Actions.insertDocument);
 }
 
 /**
  * Deactivate all the components in the CRUD package.
  */
 function deactivate() {
-  app.componentRegistry.deregister(DocumentList);
+  app.appRegistry.deregisterComponent('Component::CRUD::DocumentList');
+  app.appRegistry.deregisterAction('Action::CRUD::DocumentRemoved');
+  app.appRegistry.deregisterAction('Action::CRUD::InsertDocument');
 }
 
 module.exports.activate = activate;
