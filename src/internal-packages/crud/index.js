54b1aefdc5f47dacee0453faae2b809aa818f100
Durran Jordan <durran@gmail.com>
2016-07-28 15:52:07 +0200

    INT-1686: Register stores from CRUD

--- src/internal-packages/crud/index.js
+++ src/internal-packages/crud/index.js
@@ -3,6 +3,7 @@
 const app = require('ampersand-app');
 const DocumentList = require('./lib/component/document-list');
 const Actions = require('./lib/actions');
+const InsertDocumentStore = require('./lib/store/insert-document-store');
 
 /**
  * Activate all the components in the CRUD package.
@@ -10,7 +11,7 @@ const Actions = require('./lib/actions');
 function activate() {
   app.appRegistry.registerComponent('Component::CRUD::DocumentList', DocumentList);
   app.appRegistry.registerAction('Action::CRUD::DocumentRemoved', Actions.documentRemoved);
-  app.appRegistry.registerAction('Action::CRUD::InsertDocument', Actions.insertDocument);
+  app.appRegistry.registerStore('Store::CRUD::InsertDocumentStore', InsertDocumentStore);
 }
 
 /**
@@ -19,7 +20,7 @@ function activate() {
 function deactivate() {
   app.appRegistry.deregisterComponent('Component::CRUD::DocumentList');
   app.appRegistry.deregisterAction('Action::CRUD::DocumentRemoved');
-  app.appRegistry.deregisterAction('Action::CRUD::InsertDocument');
+  app.appRegistry.deregisterStore('Store::CRUD::InsertDocumentStore');
 }
 
 module.exports.activate = activate;
