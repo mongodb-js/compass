9f192c0f6070db2e0262f99958fa713fe7d96087
Durran Jordan <durran@gmail.com>
2016-08-24 23:59:16 +0200

    Adding server stats package

---
+++ src/internal-packages/server-stats/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Activate all the components in the CRUD package.
+ */
+function activate() {
+}
+
+/**
+ * Deactivate all the components in the CRUD package.
+ */
+function deactivate() {
+}
+
+module.exports.activate = activate;
+module.exports.deactivate = deactivate;
