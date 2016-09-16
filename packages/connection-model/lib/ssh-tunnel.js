6d4a0a220f57124d7a04177fb9f0f8b65d5b8a87
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2016-09-16 18:33:08 -0400

    INT-1771: Fix UncaughtException when not providing a required passphrase (#110)
    * INT-1771: Fix UncaughtException when not providing a required passphrase
    
    Workaround a bug in `tunnel-ssh` where the error event is not handled.
    This causes an UncaughtException in cases such as attempting to use a
    key which is passphrase protected but not providing the passphrase.
    
    * Fix typo
    
    * Fix typo

--- packages/connection-model/lib/ssh-tunnel.js
+++ packages/connection-model/lib/ssh-tunnel.js
@@ -21,23 +21,43 @@ SSHTunnel.prototype.listen = function(done) {
     message: 'Create SSH Tunnel',
     pending: true
   });
+  /**
+   * Workaround a bug in `tunnel-ssh` where the error event is not
+   * handled.  This causes an UncaughtException in cases such as
+   * attempting to use a key which is passphrase protected.
+   *
+   * @see INT-1771
+   */
 
-  this._tunnel = createTunnel(this.model.ssh_tunnel_options, function(err) {
-    if (err) {
-      debug('error setting up tunnel', err);
-      this.emit('status', {
-        message: 'Create SSH Tunnel',
-        error: err
+  this._tunnel = createTunnel(this.model.ssh_tunnel_options, function() {
+    debug('tunnel-ssh called callback');
+  });
+
+  this._tunnel.on('netConnection', function(netConnection) {
+    var onError = this._tunnel.emit.bind(this._tunnel, 'error');
+    netConnection.on('sshStream', function(sshStream) {
+      sshStream.on('error', function(err) {
+        onError(err);
       });
+    });
+  }.bind(this));
 
-      return done(err);
-    }
+  this._tunnel.on('listening', function() {
     this.emit('status', {
       message: 'Create SSH Tunnel',
       complete: true
     });
 
     done(null, true);
+  }.bind(this));
+
+  this._tunnel.on('error', function(err) {
+    debug('error setting up tunnel', err);
+    this.emit('status', {
+      message: 'Create SSH Tunnel',
+      error: err
+    });
+    done(err);
   }.bind(this));
 
   return this;
