mongodb-js/compass

commit 224fa14aac7be6610dc03b06bb616ac743f3323d

The great eslint internal package cleanup. (#486)
* The great eslint internal package cleanup

* Fix small errors

* Fixing more errors after osx scan

* Fix complexity errors

src/internal-packages/schema/lib/d3/coordinates.js
@@ -55,7 +55,7 @@ const minicharts_d3fns_geo = function() {
     // const project = d3.geo.mercator();
     // const unproject = d3.geo.mercator().invert;
 
-    let update;
+    const update = null;
 
     function querybuilder() {
       if (circleCenter && circleOuter) {

src/internal-packages/schema/lib/d3/d3-tip.js
@@ -20,13 +20,13 @@
   //
   // Returns a tip
   return function() {
-    var direction = d3_tip_direction;
-    var offset = d3_tip_offset;
-    var html = d3_tip_html;
-    var node = initNode();
-    var svg = null;
-    var point = null;
-    var target = null;
+    const direction = d3_tip_direction;
+    const offset = d3_tip_offset;
+    const html = d3_tip_html;
+    const node = initNode();
+    let svg = null;
+    let point = null;
+    let target = null;
 
     function tip(vis) {
       svg = getSVGNode(vis);
@@ -41,17 +41,17 @@
     //
     // Returns a tip
     tip.show = function() {
-      var args = Array.prototype.slice.call(arguments);
+      const args = Array.prototype.slice.call(arguments);
       if (args[args.length - 1] instanceof SVGElement) {
         target = args.pop();
       }
 
-      var content = html.apply(this, args),
+      const content = html.apply(this, args),
         poffset = offset.apply(this, args),
         dir = direction.apply(this, args),
         nodel = getNodeEl(),
         i = directions.length,
-        coords,
+        coords = null,
         scrollTop = document.documentElement.scrollTop || document.body.scrollTop,
         scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
 
@@ -77,7 +77,7 @@
     //
     // Returns a tip
     tip.hide = function() {
-      var nodel = getNodeEl();
+      const nodel = getNodeEl();
       nodel.style({
         opacity: 0,
         'pointer-events': 'none'
@@ -95,7 +95,7 @@
       if (arguments.length < 2 && typeof n === 'string') {
         return getNodeEl().attr(n);
       } else {
-        var args = Array.prototype.slice.call(arguments);
+        const args = Array.prototype.slice.call(arguments);
         d3.selection.prototype.attr.apply(getNodeEl(), args);
       }
 
@@ -112,7 +112,7 @@
       if (arguments.length < 2 && typeof n === 'string') {
         return getNodeEl().style(n);
       } else {
-        var args = Array.prototype.slice.call(arguments);
+        const args = Array.prototype.slice.call(arguments);
         d3.selection.prototype.style.apply(getNodeEl(), args);
       }
 
@@ -183,7 +183,7 @@
       return ' ';
     }
 
-    var direction_callbacks = d3.map({
+    const direction_callbacks = d3.map({
         n: direction_n,
         s: direction_s,
         e: direction_e,
@@ -197,7 +197,7 @@
       directions = direction_callbacks.keys();
 
     function direction_n() {
-      var bbox = getScreenBBox();
+      const bbox = getScreenBBox();
       return {
         top: bbox.n.y - node.offsetHeight,
         left: bbox.n.x - node.offsetWidth / 2
@@ -205,7 +205,7 @@
     }
 
     function direction_s() {
-      var bbox = getScreenBBox();
+      const bbox = getScreenBBox();
       return {
         top: bbox.s.y,
         left: bbox.s.x - node.offsetWidth / 2
@@ -213,7 +213,7 @@
     }
 
     function direction_e() {
-      var bbox = getScreenBBox();
+      const bbox = getScreenBBox();
       return {
         top: bbox.e.y - node.offsetHeight / 2,
         left: bbox.e.x
@@ -221,7 +221,7 @@
     }
 
     function direction_w() {
-      var bbox = getScreenBBox();
+      const bbox = getScreenBBox();
       return {
         top: bbox.w.y - node.offsetHeight / 2,
         left: bbox.w.x - node.offsetWidth
@@ -229,7 +229,7 @@
     }
 
     function direction_nw() {
-      var bbox = getScreenBBox();
+      const bbox = getScreenBBox();
       return {
         top: bbox.nw.y - node.offsetHeight,
         left: bbox.nw.x - node.offsetWidth
@@ -237,7 +237,7 @@
     }
 
     function direction_ne() {
-      var bbox = getScreenBBox();
+      const bbox = getScreenBBox();
       return {
         top: bbox.ne.y - node.offsetHeight,
         left: bbox.ne.x
@@ -245,7 +245,7 @@
     }
 
     function direction_sw() {
-      var bbox = getScreenBBox();
+      const bbox = getScreenBBox();
       return {
         top: bbox.sw.y,
         left: bbox.sw.x - node.offsetWidth
@@ -253,7 +253,7 @@
     }
 
     function direction_se() {
-      var bbox = getScreenBBox();
+      const bbox = getScreenBBox();
       return {
         top: bbox.se.y,
         left: bbox.e.x
@@ -261,7 +261,7 @@
     }
 
     function initNode() {
-      var node = d3.select(document.createElement('div'));
+      const node = d3.select(document.createElement('div'));
       node.style({
         position: 'absolute',
         top: 0,
@@ -308,13 +308,13 @@
     //
     // Returns an Object {n, s, e, w, nw, sw, ne, se}
     function getScreenBBox() {
-      var targetel = target || d3.event.target;
+      const targetel = target || d3.event.target;
 
       while ('undefined' === typeof targetel.getScreenCTM && 'undefined' === targetel.parentNode) {
         targetel = targetel.parentNode;
       }
 
-      var bbox = {},
+      const bbox = {},
         matrix = targetel.getScreenCTM(),
         tbbox = targetel.getBBox(),
         width = tbbox.width,

src/internal-packages/indexes/lib/component/index.jsx
@@ -1,5 +1,3 @@
-'use strict';
-
 const React = require('react');
 const NameColumn = require('./name-column');
 const TypeColumn = require('./type-column');

src/internal-packages/crud/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const app = require('ampersand-app');
 const Document = require('./lib/component/document');
 const DocumentList = require('./lib/component/document-list');

src/internal-packages/crud/lib/actions.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const Reflux = require('reflux');
 
 const Actions = Reflux.createActions([

src/internal-packages/crud/lib/component/binary-value.jsx
@@ -1,5 +1,3 @@
-'use strict';
-
 const React = require('react');
 const truncate = require('hadron-app-registry').truncate;
 
@@ -35,7 +33,21 @@ class BinaryValue extends React.Component {
    */
   constructor(props) {
     super(props);
-    this.value = props.element.currentValue;
+    this.element = props.element;
+  }
+
+  /**
+   * Render the value.
+   *
+   * @returns {Component} The component.
+   */
+  renderValue() {
+    const type = this.element.currentValue.sub_type;
+    const buffer = this.element.currentValue.buffer;
+    if (type === UUID || type === UUID_OLD) {
+      return `Binary('${truncate(buffer.toString())}')`;
+    }
+    return `Binary('${truncate(buffer.toString(BASE_64))}')`;
   }
 
   /**
@@ -50,22 +62,12 @@ class BinaryValue extends React.Component {
       </div>
     );
   }
-
-  /**
-   * Render the value.
-   *
-   * @returns {Component} The component.
-   */
-  renderValue() {
-    var type = this.value.sub_type;
-    var buffer = this.value.buffer;
-    if (type === UUID || type === UUID_OLD) {
-      return `Binary('${truncate(buffer.toString())}')`;
-    }
-    return `Binary('${truncate(buffer.toString(BASE_64))}')`;
-  }
 }
 
 BinaryValue.displayName = 'BinaryValue';
+
+BinaryValue.propTypes = {
+  element: React.PropTypes.object.isRequired
+};
 
 module.exports = BinaryValue;

src/internal-packages/crud/lib/component/code-value.jsx
@@ -1,5 +1,3 @@
-'use strict';
-
 const React = require('react');
 const truncate = require('hadron-app-registry').truncate;
 
@@ -20,7 +18,7 @@ class CodeValue extends React.Component {
    */
   constructor(props) {
     super(props);
-    this.value = props.element.currentValue;
+    this.element = props.element;
   }
 
   /**
@@ -31,12 +29,16 @@ class CodeValue extends React.Component {
   render() {
     return (
       <div className={VALUE_CLASS}>
-        {truncate(this.value.code)}
+        {truncate(this.element.currentValue.code)}
       </div>
     );
   }
 }
 
 CodeValue.displayName = 'CodeValue';
+
+CodeValue.propTypes = {
+  element: React.PropTypes.object.isRequired
+};
 
 module.exports = CodeValue;

src/internal-packages/crud/lib/component/document-actions.jsx
@@ -1,5 +1,3 @@
-'use strict';
-
 const React = require('react');
 const IconButton = require('hadron-app-registry').IconButton;
 
@@ -9,33 +7,24 @@ const IconButton = require('hadron-app-registry').IconButton;
 class DocumentActions extends React.Component {
 
   /**
-   * The component constructor.
-   *
-   * @param {Object} props - The properties.
-   */
-  constructor(props) {
-    super(props);
-  }
-
-  /**
    * Render the actions.
    *
    * @returns {Component} The actions component.
    */
   render() {
     return (
-      <div className='document-actions'>
+      <div className="document-actions">
         <IconButton
-          title='Edit Document'
-          iconClassName='fa fa-pencil'
+          title="Edit Document"
+          iconClassName="fa fa-pencil"
           clickHandler={this.props.edit} />
         <IconButton
-          title='Delete Document'
-          iconClassName='fa fa-trash-o'
+          title="Delete Document"
+          iconClassName="fa fa-trash-o"
           clickHandler={this.props.remove} />
         <IconButton
-          title='Clone Document'
-          iconClassName='fa fa-clone'
+          title="Clone Document"
+          iconClassName="fa fa-clone"
           clickHandler={this.props.clone} />
       </div>
     );
@@ -43,5 +32,11 @@ class DocumentActions extends React.Component {
 }
 
 DocumentActions.displayName = 'DocumentActions';
+
+DocumentActions.propTypes = {
+  edit: React.PropTypes.func.isRequired,
+  remove: React.PropTypes.func.isRequired,
+  clone: React.PropTypes.func.isRequired
+};
 
 module.exports = DocumentActions;

commit a3a8445e7d486bf4c5ab2458042e28113d6394e7

Fix "Assignment to const" errors introduced by #486 (#491)
* fix assignment to constants errors
* storeDidChange should not JSON.stringify its contents with %j.

src/internal-packages/query/lib/store/index.jsx
@@ -359,7 +359,7 @@ const QueryStore = Reflux.createStore({
   },
 
   storeDidUpdate(prevState) {
-    debug('query store changed from %j to %j', prevState, this.state);
+    debug('query store changed from', prevState, 'to', this.state);
   }
 
 });

src/internal-packages/schema/lib/d3/coordinates.js
@@ -55,7 +55,7 @@ const minicharts_d3fns_geo = function() {
     // const project = d3.geo.mercator();
     // const unproject = d3.geo.mercator().invert;
 
-    const update = null;
+    let update = null;
 
     function querybuilder() {
       if (circleCenter && circleOuter) {

src/internal-packages/schema/lib/d3/d3-tip.js
@@ -20,10 +20,10 @@
   //
   // Returns a tip
   return function() {
-    const direction = d3_tip_direction;
-    const offset = d3_tip_offset;
-    const html = d3_tip_html;
-    const node = initNode();
+    let direction = d3_tip_direction;
+    let offset = d3_tip_offset;
+    let node = initNode();
+    let html = d3_tip_html;
     let svg = null;
     let point = null;
     let target = null;
@@ -50,10 +50,11 @@
         poffset = offset.apply(this, args),
         dir = direction.apply(this, args),
         nodel = getNodeEl(),
-        i = directions.length,
-        coords = null,
         scrollTop = document.documentElement.scrollTop || document.body.scrollTop,
         scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+
+      let i = directions.length,
+        coords = null;
 
       nodel.html(content)
         .style({

src/internal-packages/schema/lib/store/index.jsx
@@ -195,7 +195,7 @@ const SchemaStore = Reflux.createStore({
   },
 
   storeDidUpdate(prevState) {
-    debug('schema store changed from %j to %j', prevState, this.state);
+    debug('schema store changed from', prevState, 'to', this.state);
   }
 
 });

src/internal-packages/status/lib/store/index.jsx
@@ -196,7 +196,7 @@ const StatusStore = Reflux.createStore({
   },
 
   storeDidUpdate(prevState) {
-    debug('status store changed from %j to %j', prevState, this.state);
+    debug('status store changed from', prevState, 'to', this.state);
   }
 });
 