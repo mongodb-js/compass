f7fabdcf8c8d66ab42d747e2f646a99ab6ef48a3
Durran Jordan <durran@gmail.com>
2016-09-26 16:38:16 +0200

    Create index definition component. (#494)
    * Create index definition component
    
    * Lodash not used in name column

--- src/internal-packages/indexes/lib/component/name-column.jsx
+++ src/internal-packages/indexes/lib/component/name-column.jsx
@@ -1,5 +1,5 @@
-const _ = require('lodash');
 const React = require('react');
+const IndexDefinition = require('./index-definition');
 
 /**
  * Component for the name column.
@@ -7,70 +7,14 @@ const React = require('react');
 class NameColumn extends React.Component {
 
   /**
-   * Render the direction of the index field.
-   *
-   * @param {Object} field - The field.
-   *
-   * @returns {React.Component} The field component.
-   */
-  renderDirection(field) {
-    if (field.value === 1) {
-      return (
-        <span className="sort">
-          <i className="fa fa-arrow-circle-up fa-lg" />
-        </span>
-      );
-    } else if (field.value === -1) {
-      return (
-        <span className="sort">
-          <i className="fa fa-arrow-circle-down fa-lg" />
-        </span>
-      );
-    }
-    return (
-      <span className="type">
-        {field.value}
-      </span>
-    );
-  }
-
-  /**
-   * Render a field in an index.
-   *
-   * @param {Object} field - The field.
-   *
-   * @returns {React.Component} The field component.
-   */
-  renderField(field) {
-    return (
-      <span key={field.field} className="pair">
-        <span className="field">
-          {field.field}
-          {this.renderDirection(field)}
-        </span>
-      </span>
-    );
-  }
-
-  /**
    * Render the name column.
    *
    * @returns {React.Component} The name column.
    */
   render() {
-    const fields = _.map(this.props.index.fields.serialize(), (field) => {
-      return this.renderField(field);
-    });
     return (
       <td className="name-column">
-        <div className="name">
-          {this.props.index.name}
-        </div>
-        <div>
-          <p className="definition">
-            {fields}
-          </p>
-        </div>
+        <IndexDefinition index={this.props.index} />
       </td>
     );
   }
