3913c2e89e43714479a40b579ec52c9b32351dad
Thomas Rueckstiess <thomas@mongodb.com>
2016-10-06 18:46:33 -0400

    uncomment index.test.js

--- src/internal-packages/query/test/index.test.js
+++ src/internal-packages/query/test/index.test.js
@@ -1,9 +1,9 @@
 /* eslint no-var: 0 */
-// var QueryBar = require('../lib/component');
-// var assert = require('assert');
-//
-// describe('QueryBar', function() {
-//   it('should work', function() {
-//     assert.ok(QueryBar);
-//   });
-// });
+var QueryBar = require('../lib/component');
+var assert = require('assert');
+
+describe('QueryBar', function() {
+  it('should work', function() {
+    assert.ok(QueryBar);
+  });
+});
