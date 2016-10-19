94c6c8702f6dc23a9453120d8664a3e41bd9059a
Durran Jordan <durran@gmail.com>
2016-10-19 11:59:06 +0200

    SW: Allow style manager to append css at build time

--- packages/hadron-style-manager/lib/style-manager.js
+++ packages/hadron-style-manager/lib/style-manager.js
@@ -2,6 +2,7 @@
 
 /* eslint no-sync: 0 */
 const LessCache = require('less-cache');
+const fs = require('fs');
 
 /**
  * The style tag constant.
@@ -12,6 +13,31 @@ const STYLE = 'style';
  * Manas styles for a hadron application.
  */
 class StyleManager {
+
+  /**
+   * For use in a build system when one wants to generate the css
+   * at build time and insert them into the document <head>.
+   *
+   * This is only for use at build time.
+   *
+   * @param {String} file - The path to the file.
+   * @param {String} stylesheet - The path to the stylesheet.
+   * @param {Function} done - The callback to execute.
+   */
+  build(file, stylesheet, done) {
+    fs.readFile(file, (error, data) => {
+      if (error) {
+        return done(error);
+      }
+      const html = this._buildHtml(data, stylesheet);
+      fs.writeFile(file, html, (err) => {
+        if (err) {
+          return done(err);
+        }
+        done(null, html);
+      });
+    });
+  }
 
   /**
    * The style manager loads styles from the less cache.
@@ -38,6 +64,13 @@ class StyleManager {
     const styles = doc.createElement(STYLE);
     styles.textContent = this.cache.readFileSync(stylesheet);
     doc.head.appendChild(styles);
+  }
+
+  _buildHtml(data, stylesheet) {
+    const cheerio = require('cheerio');
+    const $ = cheerio.load(data);
+    $('head').append(`<style>\n${this.cache.readFileSync(stylesheet)}</style>\n`);
+    return $.html();
   }
 }
 
--- packages/hadron-style-manager/test/style-manager.test.js
+++ packages/hadron-style-manager/test/style-manager.test.js
@@ -1,24 +1,50 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const expect = require('chai').expect;
 const StyleManager = require('../lib/style-manager');
 
-describe('StyleManager', function() {
-  describe('#constructor', function() {
+describe('StyleManager', () => {
+  describe('#build', () => {
+    const cachePath = path.join(__dirname, '.compiled-less');
+    const resourcePath = path.join(__dirname);
+    const htmlTemplatePath = path.join(__dirname, 'test.html.template');
+    const htmlPath = path.join(__dirname, 'test.html');
+    const lessPath = path.join(__dirname, 'test.less');
+    const manager = new StyleManager(cachePath, resourcePath);
+
+    before(() => {
+      fs.createReadStream(htmlTemplatePath).pipe(fs.createWriteStream(htmlPath));
+    });
+
+    after((done) => {
+      fs.unlink(htmlPath, done());
+    });
+
+    it('writes the css into the head of the html', (done) => {
+      manager.build(htmlPath, lessPath, (error, html) => {
+        expect(error).to.equal(null);
+        expect(html).to.include('color: red;');
+        done();
+      });
+    });
+  });
+
+  describe('#constructor', () => {
     const cachePath = path.join(__dirname, '.compiled-less');
     const resourcePath = path.join(__dirname);
     const manager = new StyleManager(cachePath, resourcePath);
 
-    it('sets the cache directory', function() {
+    it('sets the cache directory', () => {
       expect(manager.cache.cacheDir).to.equal(cachePath);
     });
 
-    it('sets the resource path', function() {
+    it('sets the resource path', () => {
       expect(manager.cache.resourcePath).to.equal(resourcePath);
     });
 
-    it('sets the import path', function() {
+    it('sets the import path', () => {
       expect(manager.cache.importPaths[0]).to.equal(resourcePath);
     });
   });
