f0bfb6d6829b76370af672f730fe3a9ea1fa8c1f
Durran Jordan <durran@gmail.com>
2016-10-20 22:08:57 +0200

    Skunkworks: Adding drag & drop of JSON:
    The user can now drag and drop a file into the Compass collection view
    if the file contains a single valid JSON object. Compass will open the
    insert document modal with the file contents.

--- src/internal-packages/crud/lib/actions.js
+++ src/internal-packages/crud/lib/actions.js
@@ -1,9 +1,31 @@
+const fs = require('fs');
 const Reflux = require('reflux');
+const debug = require('debug')('mongodb-compass:crud:actions');
 
 const Actions = Reflux.createActions([
   'documentRemoved',
   'openInsertDocumentDialog',
-  'insertDocument'
+  'insertDocument',
+  'fileDropped'
 ]);
+
+document.ondragover = document.ondrop = (ev) => {
+  ev.preventDefault();
+};
+
+document.body.ondrop = (ev) => {
+  ev.preventDefault();
+  const file = ev.dataTransfer.files[0].path;
+  fs.readFile(file, 'utf-8', (error, data) => {
+    if (error) {
+      debug(`Error opening file '${file}': ${error.message}`);
+    }
+    try {
+      Actions.openInsertDocumentDialog(JSON.parse(data), false);
+    } catch (e) {
+      debug(`File ${file} is not a single parseable JSON document: ${e.message}`);
+    }
+  })
+};
 
 module.exports = Actions;
