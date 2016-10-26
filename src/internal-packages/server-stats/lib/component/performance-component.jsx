34f2989c1a8667336ea11cc66aa262565ec1d809
Thomas Rückstieß <rueckstiess@users.noreply.github.com>
2016-10-27 02:38:08 +1100

    add Databases tab, refactoring (#540)

--- src/internal-packages/server-stats/lib/component/performance-component.jsx
+++ src/internal-packages/server-stats/lib/component/performance-component.jsx
@@ -4,18 +4,22 @@ const React = require('react');
 const GraphsComponent = require('./server-stats-graphs-component');
 const ListsComponent = require('./server-stats-lists-component');
 const DBErrorComponent = require('./dberror-component');
+const TimeAndPauseButton = require('./time-and-pause-button');
 const DBErrorStore = require('../store/dberror-store');
 
 class PerformanceComponent extends React.Component {
   render() {
     return (
-      <section className="rt-perf">
-        <DBErrorComponent store={DBErrorStore} />
-        <section className="rt__graphs-out">
-          <GraphsComponent interval={this.props.interval} />
-        </section>
-        <section className="rt__lists-out">
-          <ListsComponent interval={this.props.interval} />
+      <section>
+        <TimeAndPauseButton paused={false} />
+        <section className="rt-perf">
+          <DBErrorComponent store={DBErrorStore} />
+          <section className="rt__graphs-out">
+            <GraphsComponent interval={this.props.interval} />
+          </section>
+          <section className="rt__lists-out">
+            <ListsComponent interval={this.props.interval} />
+          </section>
         </section>
       </section>
     );
