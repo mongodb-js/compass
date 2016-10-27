bee2ba2a33fa634a334f308e91ceb9252aebe2cf
Thomas Rueckstiess <thomas@mongodb.com>
2016-10-27 21:40:52 +1100

    clean up and remove commented out code.

--- test/validation.range.test.js
+++ test/validation.range.test.js
@@ -1,31 +1,21 @@
-// @todo: Not sure how to move this test into a sub-directory and
-// @todo: ... have it run via `npm run test-unit` or `npm run ci`
+/* eslint no-unused-vars: 0 */
 const chai = require('chai');
 const chaiEnzyme = require('chai-enzyme');
 const expect = chai.expect;
-// https://github.com/babel/babel-eslint/issues/6
-const React = require('react');  // eslint-disable-line no-unused-vars
-// const _ = require('lodash');
+const React = require('react');
 
 const shallow = require('enzyme').shallow;
 const bootstrap = require('react-bootstrap');
 const ControlLabel = bootstrap.ControlLabel;
 const FormControl = bootstrap.FormControl;
-// const Rule = require('../src/internal-packages/validation/lib/components/rule');
-// const RuleCategorySelector = require('../src/internal-packages/validation/lib/components/rule-category-selector');
-const RangeInput = require('../src/internal-packages/validation/lib/components/common/range-input');  // eslint-disable-line no-unused-vars
+const RangeInput = require('../src/internal-packages/validation/lib/components/common/range-input');
 
-// const debug = require('debug')('compass:validation:test');
-
-// use chai-enzyme assertions, see https://github.com/producthunt/chai-enzyme
 chai.use(chaiEnzyme());
-
 
 describe('<RangeInput />', () => {
   context('when rendering the default control', () => {
     it('has label `LOWER BOUND`', () => {
       const component = shallow(<RangeInput />);
-      // console.log(component.debug());
       const labelText = component.find(ControlLabel).dive().text();
       expect(labelText).to.be.equal('LOWER BOUND');
     });
@@ -49,25 +39,3 @@ describe('<RangeInput />', () => {
     });
   });
 });
-
-
-// @todo: Might be useful for COMPASS-156
-// describe('<Rule />', function() {
-//   let component;
-//   const ruleTemplate = {
-//     id: 'my-new-rule',
-//     field: 'created_at',
-//     category: 'range',
-//     parameters: {type: 9},  // type "date"
-//     nullable: false
-//   };
-//
-//   it('has a category of "range" if category "range" is supplied', function() {
-//     const rule = _.assign(ruleTemplate, {});
-//     component = shallow(<table><tbody><Rule {...rule} /></tbody></table>);
-//     const ruleCategory = component.find(Rule).dive().find(RuleCategorySelector);
-//     expect(ruleCategory.props().category).to.be.equal("range");
-//   });
-//
-//   it('has a lower bound rendered')
-// });
