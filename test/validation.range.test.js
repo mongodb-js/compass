1169102da195aafc070ae14393825f2e5018625b
Peter Schmidt <peter@peterjs.com>
2016-10-31 14:33:28 +1100

    Add RuleCategoryRange unit and Rule integration tests

--- test/validation.range.test.js
+++ test/validation.range.test.js
@@ -3,12 +3,14 @@ const chai = require('chai');
 const chaiEnzyme = require('chai-enzyme');
 const expect = chai.expect;
 const React = require('react');
+const _ = require('lodash');
 
 const shallow = require('enzyme').shallow;
 const bootstrap = require('react-bootstrap');
 const ControlLabel = bootstrap.ControlLabel;
 const FormControl = bootstrap.FormControl;
 const RangeInput = require('../src/internal-packages/validation/lib/components/common/range-input');
+const RuleCategoryRange = require('../src/internal-packages/validation/lib/components/rule-categories/range');
 
 chai.use(chaiEnzyme());
 
@@ -37,5 +39,26 @@ describe('<RangeInput />', () => {
       const placeholderText = component.find(FormControl).props().placeholder;
       expect(placeholderText).to.be.equal('enter upper bound');
     });
+  });
+});
+
+describe('<RuleCategoryRange />', function() {
+  let component;
+  const propsTemplate = {
+    id: 'my-new-rule',
+    field: 'created_at',
+    category: 'range',
+    parameters: {
+      lowerBoundValue: -5,
+      upperBoundValue: 5
+    },
+    nullable: false
+  };
+
+  it('has two child <RangeInput /> components', function() {
+    const props = _.assign(propsTemplate, {});
+    component = shallow(<RuleCategoryRange {...props} />);
+    const ranges = component.dive().find(RangeInput);
+    expect(ranges).to.have.length(2);
   });
 });
