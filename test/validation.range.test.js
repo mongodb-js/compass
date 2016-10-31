61219ebf3f1d3b32f2ce795b96d2821d8b1b7203
Peter Schmidt <peter@peterjs.com>
2016-10-31 11:38:58 +1100

    Fix RangeInput test warnings about missing non-default required "value"

--- test/validation.range.test.js
+++ test/validation.range.test.js
@@ -15,12 +15,12 @@ chai.use(chaiEnzyme());
 describe('<RangeInput />', () => {
   context('when rendering the default control', () => {
     it('has label `LOWER BOUND`', () => {
-      const component = shallow(<RangeInput />);
+      const component = shallow(<RangeInput value={5} />);
       const labelText = component.find(ControlLabel).dive().text();
       expect(labelText).to.be.equal('LOWER BOUND');
     });
     it('has placeholder text of `enter lower bound`', () => {
-      const component = shallow(<RangeInput />);
+      const component = shallow(<RangeInput value={5} />);
       const placeholderText = component.find(FormControl).props().placeholder;
       expect(placeholderText).to.be.equal('enter lower bound');
     });
@@ -28,12 +28,12 @@ describe('<RangeInput />', () => {
 
   context('when rendering an upperBound control', () => {
     it('has label `UPPER BOUND`', () => {
-      const component = shallow(<RangeInput upperBound />);
+      const component = shallow(<RangeInput upperBound value={5} />);
       const labelText = component.find(ControlLabel).dive().text();
       expect(labelText).to.be.equal('UPPER BOUND');
     });
     it('has placeholder text of `enter upper bound`', () => {
-      const component = shallow(<RangeInput upperBound />);
+      const component = shallow(<RangeInput upperBound value={5} />);
       const placeholderText = component.find(FormControl).props().placeholder;
       expect(placeholderText).to.be.equal('enter upper bound');
     });
