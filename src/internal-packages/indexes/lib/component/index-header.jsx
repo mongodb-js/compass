335b7757de2057e36b12e1eb3e5b006dc3f848d9
Durran Jordan <durran@gmail.com>
2016-11-01 18:44:27 +0100

    COMPASS-143: Don't show DDL ops on secondaries

--- src/internal-packages/indexes/lib/component/index-header.jsx
+++ src/internal-packages/indexes/lib/component/index-header.jsx
@@ -59,7 +59,7 @@ class IndexHeader extends React.Component {
           <IndexHeaderColumn hook="th-size" name="Size" sortOrder={this.state.sortOrder} />
           <IndexHeaderColumn hook="th-usage" name="Usage" sortOrder={this.state.sortOrder} />
           <IndexHeaderColumn hook="th-properties" name="Properties" sortOrder={this.state.sortOrder} />
-          {app.preferences.isFeatureEnabled('indexDDL') ?
+          {app.dataService.isWritable() ?
             <IndexHeaderColumn hook="th-drop" name="Drop" sortOrder={this.state.sortOrder}/>
             : null}
         </tr>
