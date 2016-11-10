bdf66a11ceee82292da9b98d4b6a37bece471f24
Durran Jordan <durran@gmail.com>
2016-11-10 20:35:14 +0100

    COMPASS-213: Readonly view indexes

--- src/internal-packages/indexes/lib/component/indexes.jsx
+++ src/internal-packages/indexes/lib/component/indexes.jsx
@@ -2,6 +2,7 @@ const React = require('react');
 const IndexHeader = require('./index-header');
 const IndexList = require('./index-list');
 const CreateIndexButton = require('./create-index-button');
+const LoadIndexesStore = require('../store/load-indexes-store');
 const app = require('ampersand-app');
 
 /**
@@ -10,25 +11,76 @@ const app = require('ampersand-app');
 class Indexes extends React.Component {
 
   /**
+   * The component constructor.
+   *
+   * @param {Object} props - The properties.
+   */
+  constructor(props) {
+    super(props);
+    this.CollectionStore = app.appRegistry.getStore('App.CollectionStore');
+    this.state = this.determineState();
+  }
+
+  /**
+   * Subscribe on mount.
+   */
+  componentWillMount() {
+    this.unsubscribeLoad = LoadIndexesStore.listen(this.handleLoad.bind(this));
+  }
+
+  /**
+   * Unsubscribe on unmount.
+   */
+  componentWillUnmount() {
+    this.unsubscribeLoad();
+  }
+
+  determineState() {
+    const writable = app.dataService.isWritable() && !this.CollectionStore.readonly;
+    return { writable: writable, readonly: this.CollectionStore.readonly };
+  }
+
+  handleLoad() {
+    this.setState(this.determineState());
+  }
+
+  shouldComponentupdate(nextProps, nextState) {
+    return nextState.writable !== this.state.writable ||
+      nextState.readonly !== this.state.readonly;
+  }
+
+  renderComponent() {
+    return (
+      <div className="column-container">
+        <div className="column main">
+          {this.state.writable ? <CreateIndexButton /> : null}
+          <table>
+            <IndexHeader />
+            <IndexList />
+          </table>
+        </div>
+      </div>
+    );
+  }
+
+  renderReadonly() {
+    return (
+      <div className="index-container-notice">
+        Readonly views may not contain indexes.
+      </div>
+    );
+  }
+
+  /**
    * Render the indexes.
    *
    * @returns {React.Component} The indexes.
    */
   render() {
-    console.log('#######################################');
-    console.log('RENDERING INDEXES');
     return (
       <div className="index-container header-margin">
         <div className="flexbox-fix"></div>
-        <div className="column-container">
-          <div className="column main">
-            {app.dataService.isWritable() ? <CreateIndexButton /> : null}
-            <table>
-              <IndexHeader />
-              <IndexList />
-            </table>
-          </div>
-        </div>
+        {this.state.readonly ? this.renderReadonly() : this.renderComponent()}
       </div>
     );
   }
