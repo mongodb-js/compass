bc84bb6eb6cf8009dedc52a91fcc8407dd5c1e20
Durran Jordan <durran@gmail.com>
2016-11-11 09:52:36 +0100

    COMPASS-137: Reaonly view support in coll stats:
    This allows the collection and index stats are in the top right of the
    collection view to handle readonly views. They now display an "N/A" when
    the user is viewing a readonly view.
    
    This also removes the previous ampersand view for this component and
    replaces it with a react component. The old collection stats ampersand
    code has been removed.
    
    The collection model no longer syncs on namespace change as this
    component was the last part of the collection view that depended on the
    amersand model.
    
    This pull request has a dependency on:
    https://github.com/10gen/compass/pull/578
    
    That pull request MUST go in first.

---
+++ src/internal-packages/collection-stats/README.md
@@ -0,0 +1,13 @@
+# Compass Collection Stats Package
+
+Provides functionality shown in the stats header area in the collection view.
+
+## Available Resources in the App Registry
+
+### Components
+
+#### Definitions
+
+| Key                               | Description                                   |
+|-----------------------------------|-----------------------------------------------|
+| `CollectionStats.CollectionStats` | Renders the collection and index stats        |
---
+++ src/internal-packages/collection-stats/lib/component/collection-stats-item.jsx
@@ -0,0 +1,60 @@
+const React = require('react');
+
+/**
+ * The base class.
+ */
+const BASE_CLASS = 'collection-stats-list-item';
+
+/**
+ * The primary label class.
+ */
+const PRIMARY_LABEL = `${BASE_CLASS}-primary-label`;
+
+/**
+ * The primary value class.
+ */
+const PRIMARY_VALUE = `${BASE_CLASS}-primary-value`;
+
+/**
+ * The label class.
+ */
+const LABEL = `${BASE_CLASS}-label`;
+
+/**
+ * The value class.
+ */
+const VALUE = `${BASE_CLASS}-value`;
+
+/**
+ * Component for a single collection stats item.
+ */
+class CollectionStatsItem extends React.Component {
+
+  /**
+   * Render the component.
+   *
+   * @returns {React.Component} The component.
+   */
+  render() {
+    return (
+      <li className={BASE_CLASS}>
+        <div className={this.props.primary ? PRIMARY_LABEL : LABEL}>
+          {this.props.label}
+        </div>
+        <div className={this.props.primary ? PRIMARY_VALUE : VALUE}>
+          {this.props.value}
+        </div>
+      </li>
+    );
+  }
+}
+
+CollectionStatsItem.displayName = 'CollectionStatsItem';
+
+CollectionStatsItem.propTypes = {
+  label: React.PropTypes.string.isRequired,
+  value: React.PropTypes.any.isRequired,
+  primary: React.PropTypes.bool
+};
+
+module.exports = CollectionStatsItem;
