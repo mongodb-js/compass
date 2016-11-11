8560ca96baf0275136198276457c0f457ba8622e
Thomas Rueckstiess <thomas@mongodb.com>
2016-11-11 11:34:22 +1100

    use a : instead of a . to separate host and port.

--- src/internal-packages/sidebar/lib/components/sidebar-instance-properties.jsx
+++ src/internal-packages/sidebar/lib/components/sidebar-instance-properties.jsx
@@ -11,7 +11,7 @@ class SidebarInstanceProperties extends React.Component {
       return '';
     }
 
-    return `${instance.hostname}.${instance.port}`;
+    return `${instance.hostname}:${instance.port}`;
   }
 
   getVersionName() {
