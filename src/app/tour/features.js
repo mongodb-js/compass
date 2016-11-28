286cf0238f5e3423c224fb9ff1253baf478236a4
Thomas Rückstieß <rueckstiess@users.noreply.github.com>
2016-11-29 01:53:30 +1100

    COMPASS-441 new feature tour for 1.5 (#639)
    * COMPASS-441 new feature tour for 1.5
    
    * performance image was cropped at bottom.
    
    * add geo queries tour point.
    
    * remove old feature definitions.
    
    * reduce feature tour height slightly to avoid scroll bars.

--- src/app/tour/features.js
+++ src/app/tour/features.js
@@ -1,65 +1,58 @@
 module.exports = [
   {
-    title: 'Choose a Collection',
-    description: 'See a list of collections in the left sidebar. Select a collection and Compass will instantly start analyzing the schema data. Use the search filter at the top to narrow your list of collections.',
-    image: 'choose-collection.gif',
-    version: '1.0.0',
+    title: 'Performance Charts.',
+    description: 'Real-time server statistics let you view key server metrics and database operations. Drill down into database operations easily and understand your most active collections.',
+    image: 'performance.png',
+    version: '1.5.0',
     initial: true
   },
   {
-    title: 'Browse the Schema',
-    description: 'Once a collection is loaded, Compass will visualize the collection schema. Field are listed as rows in the main view. The left side of the row displays the field name and datatype distribution, the right side displays a visualization of the data.',
-    image: 'browse-schema.gif',
-    version: '1.0.0',
+    title: 'Sidebar. Redesigned.',
+    description: 'See with one glance what server you are connected to. Navigate between instance, database and collection level, with powerful filtering of your namespaces.',
+    image: 'sidebar.png',
+    version: '1.5.0',
     initial: true
   },
   {
-    title: 'View Data Distribution',
-    description: 'View the charts in the right-hand column of each row to see data distribution at a high level. Hover over charts to see more detail.',
-    image: 'data-distribution.gif',
-    version: '1.0.0',
-    initial: false
-  },
-  {
-    title: 'Build Queries',
-    description: 'Click on charts to build MongoDB queries. Click and drag within bar charts to select multiple values. Edit your query by typing directly into the query bar.',
-    image: 'build-queries.gif',
-    version: '1.0.0',
+    title: 'Visualize your Schema.',
+    description: 'MongoDB Compass analyzes your documents and displays rich structures within your collections through an intuitive GUI. It allows you to quickly visualize and explore your schema to understand the frequency, types and ranges of fields in your data set.',
+    image: 'schema.png',
+    version: '1.5.0',
     initial: true
   },
   {
-    title: 'View Documents',
-    description: 'Click the Documents tab to view the raw JSON documents in the result set.',
-    image: 'documents.gif',
-    version: '1.0.0',
+    title: 'Build Geo Queries.',
+    description: 'Visualize, understand, and work with your geospatial data. Point and click to construct sophisticated queries, execute them with the push of a button and Compass will display your results both graphically and as sets of JSON documents.',
+    image: 'geo.png',
+    version: '1.5.0',
     initial: true
   },
   {
-    title: 'View Indexes',
-    description: 'Click the Index tab to view the definition, attributes, size, and usage of your indexes.',
-    image: 'indexes.gif',
-    version: '1.2.0',
+    title: 'Interactive Document Editor.',
+    description: 'Modify existing documents with greater confidence using the intuitive visual editor, or insert new documents and clone or delete existing ones in just a few clicks.',
+    image: 'crud.png',
+    version: '1.5.0',
     initial: true
   },
   {
-    title: 'Examine Query Performance with the Explain Plan',
-    description: 'Click the Explain Plan tab to view a performance summary of the executed query and get access to the detailed explain plan report.',
-    image: 'explain-plan.gif',
-    version: '1.2.0',
+    title: 'Visual Explain Plans.',
+    description: 'Know how queries are running through an easy-to-understand GUI that helps you identify and resolve performance issues.',
+    image: 'explain.png',
+    version: '1.5.0',
     initial: true
   },
   {
-    title: 'Find using the Location Map',
-    description: 'Maps will display in collections with location data. Click and drag across the map for a geographic query. Click "Apply" to resample with the coordinates.',
-    image: 'geo.gif',
-    version: '1.3.0-beta.1',
+    title: 'Index Management.',
+    description: 'Understand the type and size of your indexes, their utilization and special properties. Add and remove indexes at the click of a button.',
+    image: 'indexes.png',
+    version: '1.5.0',
     initial: true
   },
   {
-    title: 'CRUD Support',
-    description: 'From documents tab, make changes to your dataset. Click insert to add a document. Each document has clone or delete actions, along with the ability to edit the fields.',
-    image: 'crud.gif',
-    version: '1.3.0-beta.1',
+    title: 'Document Validation.',
+    description: 'Create and modify rules that validate your data using a simple point and click interface. CRUD support lets you fix data quality issues easily in individual documents.',
+    image: 'validation.png',
+    version: '1.5.0',
     initial: true
   }
 ];
