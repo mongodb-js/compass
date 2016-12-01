8e31162c72c8c9a6838d01aa195a4825a5e1b356
Durran Jordan <durran@gmail.com>
2016-12-01 23:16:03 +0000

    COMPASS-63: Visual indicator on hotspot (#655)
    * COMPASS-63: Visual indicator on hotspot
    
    * COMPASS-63: Missing semicolon
    
    * COMPASS-63: More reasonable input sizes
    
    * COMPASS-63: Move icons left, make green

--- src/internal-packages/crud/lib/component/utils.js
+++ src/internal-packages/crud/lib/component/utils.js
@@ -1,3 +1,20 @@
+const moment = require('moment');
+
+/**
+ * The date format.
+ */
+const FORMAT = 'YYYY-MM-DD HH:mm:ss.SSS';
+
+/**
+ * Get the size for the string value.
+ *
+ * @return {Number} The size.
+ */
+function size(value) {
+  const length = String(value).length;
+  return length === 0 ? 1 : length;
+}
+
 /**
  * Get the size value for an input field when editing.
  *
@@ -5,9 +22,19 @@
  *
  * @returns {Integer} The size.
  */
-function inputSize(value) {
-  const length = String(value).length;
-  return length === 0 ? 1 : length;
+function inputSize(value, type) {
+  switch (type) {
+    case 'Int32':
+      return size(value.valueOf());
+    case 'Int64':
+      return size(value);
+    case 'Double':
+      return size(value.value);
+    case 'Date':
+      return size(moment(value).format(FORMAT));
+    default:
+      return size(value);
+  }
 }
 
 module.exports.inputSize = inputSize;
