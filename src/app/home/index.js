a87e8fd4c7b937e0c0984eb61d79c0e759305686
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2016-12-14 11:18:12 +1100

    moved oninstancefetch to src/app/index.js

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -1,7 +1,5 @@
 var View = require('ampersand-view');
 var app = require('ampersand-app');
-var metrics = require('mongodb-js-metrics')();
-var _ = require('lodash');
 var debug = require('debug')('mongodb-compass:home');
 var React = require('react');
 var ReactDOM = require('react-dom');
@@ -11,7 +9,6 @@ var indexTemplate = require('./index.jade');
 var HomeView = View.extend({
   screenName: 'Schema',
   initialize: function() {
-    this.listenTo(app.instance, 'sync', this.onInstanceFetched);
     this.homeView = app.appRegistry.getComponent('Home.Home');
 
     this.once('change:rendered', this.onRendered);
@@ -24,28 +21,6 @@ var HomeView = View.extend({
     ReactDOM.render(
         React.createElement(this.homeView),
         this.queryByHook('home-content'));
-  },
-  onInstanceFetched: function() {
-    // TODO: Remove this line
-    // Instead, set the instance inside InstanceStore.refreshInstance
-    app.appRegistry.getAction('App.InstanceActions').setInstance(app.instance);
-    debug('app.instance fetched', app.instance.serialize());
-    metrics.track('Deployment', 'detected', {
-      'databases count': app.instance.databases.length,
-      'namespaces count': app.instance.collections.length,
-      'mongodb version': app.instance.build.version,
-      'enterprise module': app.instance.build.enterprise_module,
-      'longest database name length': _.max(app.instance.databases.map(function(db) {
-        return db._id.length;
-      })),
-      'longest collection name length': _.max(app.instance.collections.map(function(col) {
-        return col._id.split('.')[1].length;
-      })),
-      'server architecture': app.instance.host.arch,
-      'server cpu cores': app.instance.host.cpu_cores,
-      'server cpu frequency (mhz)': app.instance.host.cpu_frequency / 1000 / 1000,
-      'server memory size (gb)': app.instance.host.memory_bits / 1024 / 1024 / 1024
-    });
   },
   template: indexTemplate
 });
