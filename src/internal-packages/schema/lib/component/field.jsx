d3f8ca72cd0fd03bf9eba0eb39f0839d2e5c21ec
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2016-12-15 13:37:52 +1100

    sort types on mount rather than during render phase

--- src/internal-packages/schema/lib/component/field.jsx
+++ src/internal-packages/schema/lib/component/field.jsx
@@ -36,9 +36,18 @@ const Field = React.createClass({
   },
 
   componentWillMount() {
+    // sort the types
+    const types = _.sortBy(this.props.types, (type) => {
+      if (type.name === 'Undefined') {
+        return -Infinity;
+      }
+      return type.probability;
+    }).reverse();
+
     // sets the active type to the first type in the props.types array
     this.setState({
-      activeType: this.props.types.length > 0 ? this.props.types[0] : null
+      types: types,
+      activeType: types.length > 0 ? types[0] : null
     });
   },
 
@@ -139,13 +148,7 @@ const Field = React.createClass({
     const cls = FIELD_CLASS + ' ' + (this.state.collapsed ? 'collapsed' : 'expanded');
 
     // types represented as horizontal bars with labels
-    const types = _.sortBy(this.props.types, (type) => {
-      if (type.name === 'Undefined') {
-        return -Infinity;
-      }
-      return type.probability;
-    }).reverse();
-    const typeList = types.map((type) => {
+    const typeList = this.state.types.map((type) => {
       // allow for semantic types and convert the type, e.g. geo coordinates
       type = this.getSemanticType(type);
       return (
