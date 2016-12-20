1dfd99d07128b7445439d77b739b2442c532fafb
Peter Schmidt <peter@peterjs.com>
2016-12-20 11:00:40 +1100

    Explicitly backport COMPASS-317 COMPASS-382 COMPASS-383 COMPASS-503 (#667)
    * COMPASS-317 :arrow_up: mongodb-data-service@2.1.7 (#630)
    
    * COMPASS-382 and COMPASS-383 - Improve RTSS error handling (#638)
    
    * COMPASS-382 and COMPASS-383 - Improve RTSS error handling
    
    * Update mongodb-data-service dependency to 2.2.0
    
    * COMPASS 503 :arrow_up: mongodb-connection-model@6.3.4 && mongodb-data-service@2.2.1

--- src/internal-packages/server-stats/lib/store/dberror-store.js
+++ src/internal-packages/server-stats/lib/store/dberror-store.js
@@ -1,5 +1,6 @@
 const Reflux = require('reflux');
 const Actions = require('../action');
+const translate = require('mongodb-js-errors').translate;
 // const debug = require('debug')('mongodb-compass:server-stats:dberror-store');
 
 const DBErrorStore = Reflux.createStore({
@@ -11,27 +12,47 @@ const DBErrorStore = Reflux.createStore({
   },
 
   restart: function() {
-    this.ops = {'top': null, 'currentOp': null, 'serverStatus': null};
-    this.errors = {};
+    this.ops = {top: null, currentOp: null, serverStatus: null};
+    this.srcName = {top: 'Hottest Collections', currentOp: 'Slowest Operations', serverStatus: 'Stats Charts'};
+  },
+
+  /**
+   * Translates the error message to something human readable. From data-service.
+   *
+   * @param {Error} error - The error.
+   *
+   * @returns {Error} The error with message translated.
+   */
+  _translateMessage(error) {
+    const mapping = translate(error);
+    if (mapping) {
+      error.message = mapping.message;
+    }
+    return error;
+  },
+
+  publish: function() {
+    const msg = [];
+    for (let key in this.ops) { // eslint-disable-line prefer-const
+      if (this.ops.hasOwnProperty( key ) ) {
+        if (this.ops[key] !== null) {
+          msg.push({errorMsg: this._translateMessage(this.ops[key]).message, ops: key, type: this.ops[key].name, srcName: this.srcName[key]});
+        }
+      }
+    }
+    this.trigger(msg);
   },
 
   dbError: function(data) {
-    // New or different error
-    const err = data.error.name + ': ' + data.error.message;
-    if (this.ops[data.op] !== err) {
-      this.ops[data.op] = err;
-      if (err in this.errors) {
-        this.errors[err] = this.errors[err] + ', ' + data.op;
-      } else {
-        this.errors[err] = data.op;
-      }
-      const msg = [];
-      for (let key in this.errors) { // eslint-disable-line prefer-const
-        if (this.errors.hasOwnProperty( key ) ) {
-          msg.push({'errorMsg': key, 'ops': this.errors[key], 'type': this.errors});
-        }
-      }
-      this.trigger(msg);
+    // Remove previous error
+    if (this.ops[data.op] !== null && data.error === null) {
+      this.ops[data.op] = data.error;
+      this.publish();
+    }
+    // New error
+    if (data.error !== null && this.ops[data.op] !== data.error) {
+      this.ops[data.op] = data.error;
+      this.publish();
     }
   }
 
