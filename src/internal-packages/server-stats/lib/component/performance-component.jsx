d4b16bea9516c8cdfd83b6640767156449869c9d
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2017-01-17 14:25:03 +1100

    added top command not available in mongos message to perf tab

--- src/internal-packages/server-stats/lib/component/performance-component.jsx
+++ src/internal-packages/server-stats/lib/component/performance-component.jsx
@@ -6,12 +6,24 @@ const ListsComponent = require('./server-stats-lists-component');
 const DBErrorComponent = require('./dberror-component');
 const TimeAndPauseButton = require('./time-and-pause-button');
 const DBErrorStore = require('../store/dberror-store');
+const app = require('ampersand-app');
+const StatusRow = app.appRegistry.getComponent('App.StatusRow');
 
 class PerformanceComponent extends React.Component {
+
+  renderTopMessage() {
+    return (
+        <StatusRow style="warning">
+          Top command is not available for mongos, some charts may not show any data.
+        </StatusRow>
+    );
+  }
+
   render() {
     return (
       <section className="rt-perf">
         <TimeAndPauseButton paused={false} />
+        {app.dataService.isMongos() ? this.renderTopMessage() : null}
         <DBErrorComponent store={DBErrorStore} />
         <section className="rt__graphs-out">
           <GraphsComponent interval={this.props.interval} />
