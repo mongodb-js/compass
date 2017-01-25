6dc621c285558b211ff9e59dd5cd458fcf47ac9a
Peter Schmidt <peter@peterjs.com>
2017-01-25 17:57:24 +1100

    assert.equals => assert.equal

--- test/unit/icon.test.js
+++ test/unit/icon.test.js
@@ -3,6 +3,6 @@ var icon = require('../../src/icon');
 
 describe('icon', function() {
   it('should be a non-empty nativeImage', function() {
-    assert.equals(icon.isEmpty(), false);
+    assert.equal(icon.isEmpty(), false);
   });
 });
