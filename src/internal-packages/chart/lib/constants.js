65e25b959a415290385d8293dbc7eb99d5901439
Peter Schmidt <peter@peterjs.com>
2017-02-01 17:02:23 +1100

    Review: SPEC_TYPE_ENUM: Use UPPER_SNAKE_CASE keys and kebab-case values

--- src/internal-packages/chart/lib/constants.js
+++ src/internal-packages/chart/lib/constants.js
@@ -68,9 +68,9 @@ const MEASUREMENT_ENUM = Object.freeze({
  * if we end up not using them or not finding much similarity then nuke this.
  */
 const SPEC_TYPE_ENUM = Object.freeze({
-  VEGA: 'VEGA',
-  VEGA_LITE: 'VEGA_LITE',
-  CUSTOM: 'CUSTOM'
+  VEGA: 'vega',
+  VEGA_LITE: 'vega-lite',
+  CUSTOM: 'custom'
 });
 
 const DEFAULTS = Object.freeze({
