93d654781b459907aa659988bf0e4af5335d18df
Peter Schmidt <peter@peterjs.com>
2017-02-03 00:53:16 +1100

    Expand the --renderer test so it can handle integration tests
    Tried several times to get this into the --unit tests, but our internal-packages tend to depend on “window” and “document” so that is not possible. Both those are genuinely available in the renderer process so it’s definitely a better home for these tests for now. We can decide later when we have more if they can or should be decoupled further.

---
+++ test/renderer/_setup.js
@@ -0,0 +1,10 @@
+require('babel-register')();
+
+require('../../src/app/reflux-listen-to-external-store');
+
+// Would be nice not to need this jQuery being present
+window.jQuery = require('jquery');
+
+// Require our internal-packages so we can integration-test things fast,
+// i.e. without requiring a full functional test
+require('../../src/app/setup-package-manager');
