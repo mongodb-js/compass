250b1458bfc30975b825a9ae9cd6198c8ad07851
Thomas Rueckstiess <thomas@mongodb.com>
2017-02-08 07:40:09 +1100

    add dependencies, tests, pluralize components dir

--- src/internal-packages/chart/index.js
+++ src/internal-packages/chart/index.js
@@ -1,6 +1,7 @@
 const app = require('ampersand-app');
 const ChartActions = require('./lib/actions');
 const ChartStore = require('./lib/store');
+const Chart = require('./lib/components/chart');
 
 /**
  * Activate all the components in the Chart package.
@@ -8,6 +9,7 @@ const ChartStore = require('./lib/store');
 function activate() {
   app.appRegistry.registerAction('Chart.Actions', ChartActions);
   app.appRegistry.registerStore('Chart.Store', ChartStore);
+  app.appRegistry.registerComponent('Chart.Chart', Chart);
 }
 
 /**
@@ -16,6 +18,7 @@ function activate() {
 function deactivate() {
   app.appRegistry.deregisterAction('Chart.Actions');
   app.appRegistry.deregisterStore('Chart.Store');
+  app.appRegistry.deregisterComponent('Chart.Chart');
 }
 
 module.exports.activate = activate;
