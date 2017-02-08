17a3b073451fa7fd9d52312931ceb15e32b8a985
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2017-02-08 18:28:51 +1100

    added a clippy reflux store + constants

---
+++ src/internal-packages/clippy/constants.js
@@ -0,0 +1,75 @@
+const clippings = Object.freeze({
+  /**
+   * Query messages
+   */
+  'Query': {
+    'bad': {
+      'message': [
+        'Ok seriously what are you doing?, use oid',
+        'Please stop, go back and use oid',
+        'I\'m really upset :(',
+        'Please no more!'
+      ],
+      'animation': [
+        'Acknowledge',
+        'Save',
+        'GetTechy',
+        'GestureUp'
+      ]
+    }
+  }
+});
+
+module.exports = clippings;
+
+/**
+ clippy actions!
+[
+  "DeepIdle1",
+  "Congratulate",
+  "Idle(1)",
+  "Hide",
+  "SendMail",
+  "Thinking",
+  "Idle(3)",
+  "Explain",
+  "Idle(5)",
+  "Print",
+  "LookRight",
+  "GetAttention",
+  "Save",
+  "GetTechy",
+  "GestureUp",
+  "Idle1_1",
+  "Processing",
+  "Alert",
+  "LookUpRight",
+  "Idle(9)",
+  "Idle(7)",
+  "GestureDown",
+  "LookLeft",
+  "Idle(2)",
+  "LookUpLeft",
+  "CheckingSomething",
+  "Hearing_1",
+  "GetWizardy",
+  "GestureLeft",
+  "Wave",
+  "Goodbye",
+  "GestureRight",
+  "Writing",
+  "LookDownRight",
+  "GetArtsy",
+  "Show",
+  "LookDown",
+  "Searching",
+  "Idle(4)",
+  "EmptyTrash",
+  "Greeting",
+  "LookUp",
+  "Idle(6)",
+  "RestPose",
+  "Idle(8)",
+  "LookDownLeft"
+]
+**/
---
+++ src/internal-packages/clippy/store/index.js
@@ -0,0 +1,25 @@
+const Reflux = require('reflux');
+const app = require('ampersand-app');
+const clippings = require('../constants');
+const _ = require('lodash');
+
+const ClippyStore = Reflux.createStore({
+  init: function() {
+    this.listenToExternalStore('Query.Store', this.clipQuery.bind(this));
+  },
+
+  /**
+   * @param {Reflux.store} state - state of the QueryStore
+   */
+  clipQuery: function(state) {
+    // do a regex test for {"objectID": ......}
+    console.log('query is changing: ', state);
+    const messages = clippings.Query.bad.message;
+    const animations = clippings.Query.bad.animation;
+    app.clippy.stopCurrent();
+    app.clippy.animate(animations[_.random(0, animations.length - 1)]);
+    app.clippy.speak(messages[_.random(0, messages.length - 1)]);
+  }
+});
+
+module.exports = ClippyStore;
