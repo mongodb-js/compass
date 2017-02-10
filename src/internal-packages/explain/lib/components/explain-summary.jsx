beca2765f7bba2516738618da065795733c78438
Fred Truman <ftruman@gmail.com>
2017-02-10 15:36:26 +1100

    explain plan info sprinkle links in quert summary

--- src/internal-packages/explain/lib/components/explain-summary.jsx
+++ src/internal-packages/explain/lib/components/explain-summary.jsx
@@ -15,6 +15,16 @@ class ExplainSummary extends React.Component {
    */
   render() {
     const inMemorySort = this.props.inMemorySort ? 'yes' : 'no';
+    const BASE_URL = 'https://docs.mongodb.com/master/reference/explain-results/';
+
+    const HELP_URLS = {
+      NRETURNED: BASE_URL + '#explain.executionStats.nReturned',
+      KEYS_EXAMINED: BASE_URL + '#explain.executionStats.totalKeysExamined',
+      DOCS_EXAMINED: BASE_URL + '#explain.executionStats.totalDocsExamined',
+      EXECUTION_TIME: BASE_URL + '#explain.executionStats.executionTimeMillis',
+      SORT_STAGE: BASE_URL + '#sort-stage',
+      INDEX_USED: BASE_URL + '#collection-scan-vs-index-use'
+    };
 
     return (
       <div className="explain-summary">
@@ -22,19 +32,19 @@ class ExplainSummary extends React.Component {
         <FlexBox alignItems="flex-start">
           <div className="summary-stats">
             <SummaryStat
-              dataLink="nReturned"
+              dataLink={HELP_URLS.NRETURNED}
               dataTestId="explain-returned-count"
               label="Documents Returned:"
               value={this.props.nReturned}
             />
             <SummaryStat
-              dataLink="totalKeysExamined"
+              dataLink={HELP_URLS.KEYS_EXAMINED}
               dataTestId="explain-examined-keys-count"
               label="Index Keys Examined:"
               value={this.props.totalKeysExamined}
             />
             <SummaryStat
-              dataLink="totalDocsExamined"
+              dataLink={HELP_URLS.DOCS_EXAMINED}
               dataTestId="explain-examined-count"
               label="Documents Examined:"
               value={this.props.totalDocsExamined}
@@ -42,16 +52,17 @@ class ExplainSummary extends React.Component {
           </div>
           <div className="summary-stats">
             <SummaryStat
-              dataLink="executionTimeMillis"
+              dataLink={HELP_URLS.EXECUTION_TIME}
               label="Actual Query Execution Time (ms):"
               value={this.props.executionTimeMillis}
             />
             <SummaryStat
-              dataLink="sortStage"
+              dataLink={HELP_URLS.SORT_STAGE}
               label="Sorted in Memory:"
               value={inMemorySort}
             />
             <SummaryIndexStat
+              dataLink={HELP_URLS.INDEX_USED}
               indexType={this.props.indexType}
               index={this.props.index}
             />
