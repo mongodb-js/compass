b02a26f73c5e861f20311f5c4ebf9a818264cced
aherlihy <anna.herlihy@10gen.com>
2017-02-16 15:32:44 +1100

    WIP merge shared rtss module into server-stats: stores

--- src/internal-packages/server-stats/lib/stores/top-store.js
+++ src/internal-packages/server-stats/lib/stores/top-store.js
@@ -5,8 +5,6 @@ const toNS = require('mongodb-ns');
 const { DataServiceActions } = require('mongodb-data-service');
 const _ = require('lodash');
 
-const debug = require('debug')('mongodb-compass:server-stats:top-store');
-
 /* eslint complexity:0 */
 
 /**
