5b4c205225c98f9b451bcca4ddf24f7c8720e974
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2017-02-20 17:09:03 +1100

    fixed enzyme test for new instance-header component

---
+++ test/enzyme/instance-header.test.js
@@ -0,0 +1,104 @@
+/* eslint no-unused-vars: 0, no-unused-expressions: 0 */
+const app = require('hadron-app');
+const chai = require('chai');
+const chaiEnzyme = require('chai-enzyme');
+const expect = chai.expect;
+const React = require('react');
+const sinon = require('sinon');
+const AppRegistry = require('hadron-app-registry');
+const { shallow } = require('enzyme');
+const InstanceHeader = require('../../src/internal-packages/instance-header/lib/components/instance-header');
+
+chai.use(chaiEnzyme());
+
+const appRegistry = app.appRegistry;
+const dataService = app.dataService;
+
+describe('<InstanceHeader />', () => {
+  beforeEach(function() {
+    app.appRegistry = new AppRegistry();
+    this.DatabaseDDLActionSpy = sinon.spy();
+    app.appRegistry.registerAction(
+      'DatabaseDDL.Actions',
+      {openCreateDatabaseDialog: this.DatabaseDDLActionSpy}
+    );
+    app.dataService = {
+      isWritable: () => {
+        return true;
+      }
+    };
+  });
+  afterEach(() => {
+    app.dataService = dataService;
+    app.appRegistry = appRegistry;
+  });
+
+  context('when rendering with no SSH Tunnel', () => {
+    beforeEach(function() {
+      app.connection = {
+        ssh_tunnel_hostname: 'ip-1-2-3-4-mongod.com',
+        ssh_tunnel_options: {
+          dstPort: 27000
+        },
+        ssh_tunnel: 'NONE'
+      };
+      this.instance = {
+        build: {version: '3.4.0-rc2'},
+        collections: [],
+        databases: []
+      };
+      this.component = shallow(
+        <InstanceHeader hostname="localhost" port={27017} activeNamespace={''}
+          versionNumber={this.instance.build.version} versionDistro="Enterprise"/>
+      );
+    });
+
+    it('renders the endpoint host name and port as text', function() {
+      const element = this.component.find('.instance-header-hostname-suffix');
+      expect(element.text()).to.be.equal('localhost');
+    });
+    it('renders instance build version', function() {
+      const distro = this.component.find('.instance-header-version-distro');
+      const number = this.component.find('.instance-header-version-number');
+      expect(distro.text()).to.be.equal('Enterprise version ');
+      expect(number.text()).to.be.equal(this.instance.build.version);
+    });
+  });
+
+  context('when rendering with an SSH Tunnel', () => {
+    beforeEach(function() {
+      app.connection = {
+        hostname: 'ip-1-2-3-4-secret-mongod.com',
+        port: 27017,
+        ssh_tunnel: 'IDENTITY_FILE',
+        ssh_tunnel_hostname: 'my-jump-box.com',
+        ssh_tunnel_options: {
+          dstPort: 2222
+        }
+      };
+      this.instance = {
+        build: {version: '3.2.10'},
+        collections: [],
+        databases: []
+      };
+      this.component = shallow(
+        <InstanceHeader hostname={app.connection.hostname}
+          port={app.connection.port} activeNamespace={''}
+          versionNumber={this.instance.build.version} versionDistro="Community"/>
+      );
+    });
+
+    it('renders the endpoint host name and port as prefix and suffix', function() {
+      const prefix = this.component.find('.instance-header-hostname-prefix');
+      const suffix = this.component.find('.instance-header-hostname-suffix');
+      expect(prefix.text()).to.be.equal('ip-1-2-3-4-secret-m');
+      expect(suffix.text()).to.be.equal('ongod.com');
+    });
+    it('renders instance build version', function() {
+      const distro = this.component.find('.instance-header-version-distro');
+      const number = this.component.find('.instance-header-version-number');
+      expect(distro.text()).to.be.equal('Community version ');
+      expect(number.text()).to.be.equal(this.instance.build.version);
+    });
+  });
+});
