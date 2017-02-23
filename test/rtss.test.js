d9e0860bdb25ac31111185927c3234a3cfa5ad5d
Peter Schmidt <peter@peterjs.com>
2017-02-23 14:14:00 +1100

    COMPASS-804 Backport COMPASS-515 RTSS mongos data-service (#793)
    * COMPASS-515: View Real-Time Server Stats when connected to a mongos (#747)
    
    * added top command not available in mongos message to perf tab
    
    * added enzyme test
    
    * fix lint error
    
    * fix indentation
    
    * Reduce network connections in functional test
    
    Not sure why, but it should now pass on Travis…
    
    * Revert "Reduce network connections in functional test"
    
    This reverts commit 65fc0745ab1b673e5c7c111d61fcba3d891b0dee.
    
    * Skip 'renders the network connections' functional test

---
+++ test/rtss.test.js
@@ -0,0 +1,48 @@
+/* eslint no-unused-vars: 0, no-unused-expressions: 0 */
+const app = require('ampersand-app');
+const chai = require('chai');
+const chaiEnzyme = require('chai-enzyme');
+const expect = chai.expect;
+const React = require('react');
+const {shallow} = require('enzyme');
+const AppRegistry = require('hadron-app-registry');
+const StatusRow = require('../src/internal-packages/app/lib/components/status-row');
+
+chai.use(chaiEnzyme());
+
+describe('rtss', () => {
+  const appRegistry = app.appRegistry;
+  const appDataService = app.dataService;
+  const appInstance = app.instance;
+
+  beforeEach(() => {
+    // Mock the AppRegistry with a new one so tests don't complain about
+    // appRegistry.getComponent (i.e. appRegistry being undefined)
+    app.appRegistry = new AppRegistry();
+    app.appRegistry.registerComponent('App.StatusRow', StatusRow);
+    this.performance = require('../src/internal-packages/server-stats/lib/component/performance-component');
+  });
+  afterEach(() => {
+    // Restore properties on the global app object,
+    // so they don't affect other tests
+    app.appRegistry = appRegistry;
+    app.dataService = appDataService;
+    app.instance = appInstance;
+  });
+
+  context('when connected to a mongos', () => {
+    beforeEach(() => {
+      app.dataService = {
+        isMongos: () => {
+          return true;
+        }
+      };
+      this.component = shallow(<this.performance interval={1000} />);
+    });
+    it('displays the top not available in mongos message', () => {
+      const state = this.component.find(StatusRow);
+      expect(state.dive()).to.have
+        .text('Top command is not available for mongos, some charts may not show any data.');
+    });
+  });
+});
