9be7c48fbbab7878c97851589b135f89681eac34
Peter Schmidt <peter@peterjs.com>
2017-02-27 17:30:00 +1100

    COMPASS-812: Update connection-model tests for connection-fixture changes (#160)
    * Skip connection-fixture tests
    
    * Drop unnecessary format call
    
    String concatenation is faster.

--- packages/connection-model/test/connect.test.js
+++ packages/connection-model/test/connect.test.js
@@ -11,7 +11,7 @@ var shouldGetInstanceDetails = function(db, done) {
   Instance.fetch(db, done);
 };
 
-var format = require('util').format;
+// TODO: These instances are now turned off
 var data = require('mongodb-connection-fixture');
 
 describe('mongodb-connection#connect', function() {
@@ -69,7 +69,7 @@ describe('mongodb-connection#connect', function() {
     this.timeout(10000);
 
     data.MATRIX.map(function(d) {
-      it(format('should connect to `%s`', d.name), function(done) {
+      it.skip('should connect to ' + d.name, function(done) {
         connect(d, function(err, _db) {
           if (err) {
             return done(err);
@@ -81,7 +81,7 @@ describe('mongodb-connection#connect', function() {
     });
 
     data.SSH_TUNNEL_MATRIX.map(function(d) {
-      it('connects via the ssh_tunnel to ' + d.ssh_tunnel_hostname, function(done) {
+      it.skip('connects via the ssh_tunnel to ' + d.ssh_tunnel_hostname, function(done) {
         connect(d, function(err, _db) {
           if (err) {
             return done(err);
