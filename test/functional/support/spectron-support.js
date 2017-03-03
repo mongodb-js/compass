6fcb12191de70d9f96ced71e4b04cafeb3970681
Peter Schmidt <peter@peterjs.com>
2017-03-03 22:25:55 +1100

    Add TODO for COMPASS 635
    This test fails sometimes, would be nice to figure out a fix, especially as this looks suspiciously like a culprit.

--- test/functional/support/spectron-support.js
+++ test/functional/support/spectron-support.js
@@ -155,6 +155,7 @@ function addWaitCommands(client) {
    * Wait for the home screen to finish loading.
    */
   client.addCommand('waitForHomeView', function() {
+    // TODO: Looks like COMPASS-635, what should this selector be?
     return this.waitForVisibleInCompass(selector('instance-sidebar'));
   });
 
