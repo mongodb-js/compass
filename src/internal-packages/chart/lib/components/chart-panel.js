3d0f7756ff98c61dcb85da640809558cba3652d8
Peter Schmidt <peter@peterjs.com>
2017-03-09 16:26:56 +1100

    Initial ChartPanel commit
    Lots of assumptions, most should be explicitly pointed out with TODOs.
    
    In any case let's get this paired up with reviews and COMPASS 887 (Create layout for chart builder components) as soon as possible :)

---
+++ src/internal-packages/chart/lib/components/chart-panel.js
@@ -0,0 +1,86 @@
+const app = require('hadron-app');
+const React = require('react');
+const { FormGroup } = require('react-bootstrap');
+
+const Actions = require('../actions');
+const {
+  CHART_TYPE_CHANNELS,
+  CHART_TYPE_ENUM
+} = require('../constants');
+const EncodingChannel = require('./encoding-channel');
+
+
+/**
+ * Represents a the chart type the user has chosen, and how the user
+ * has encoded fields onto it.
+ */
+class ChartPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.OptionSelector = app.appRegistry.getComponent('App.OptionSelector');
+  }
+
+  static onChartTypeSelect(event) {
+    // TODO: Test this action once we have COMPASS-887,
+    // TODO: ... it's probably `event.target.value` or similar
+    Actions.selectChartType(event);
+  }
+
+  renderChartTypeChoice() {
+    return (
+      <this.OptionSelector
+        id="chart-type-selector"
+        bsSize="xs"
+        options={CHART_TYPE_ENUM}
+        title={this.props.chartType}
+        onSelect={this.constructor.onChartTypeSelect}
+        disabled={!this.props.isWritable}
+      />
+    );
+  }
+
+  renderEncodingChannels() {
+    const availableChannels = CHART_TYPE_CHANNELS[this.props.chartType];
+    return Object.keys(availableChannels).map((channel) => {
+      return (
+        <EncodingChannel
+          key={channel}
+          fieldName={channel}
+          encodedChannel={this.props.encodedChannels[channel]}
+        />
+      );
+    });
+  }
+
+  render() {
+    const chartType = this.renderChartTypeChoice();
+    const encodingChannels = this.renderEncodingChannels();
+    return (
+      <FormGroup>
+        {chartType}
+        {encodingChannels}
+      </FormGroup>
+    );
+  }
+}
+
+ChartPanel.propTypes = {
+  // TODO: COMPASS-700 Spec has title but invision wireframe does not. Do we need it?
+  // https://mongodb.invisionapp.com/share/6S9X3XDQ5#/screens/215032493_200
+  // title: React.PropTypes.string,
+
+  chartType: React.PropTypes.string.isRequired,
+  encodedChannels: React.PropTypes.object,
+
+  // Not yet sure whether we want to disable charts, e.g. on a secondary,
+  // if we can't save and share them?
+  isWritable: React.PropTypes.bool
+};
+
+ChartPanel.defaultProps = {
+  isWritable: true
+};
+
+ChartPanel.displayName = 'ChartPanel';
+
+module.exports = ChartPanel;
---
+++ src/internal-packages/chart/lib/components/encoding-channel.js
@@ -0,0 +1,53 @@
+const React = require('react');
+
+const Actions = require('../actions');
+
+
+/**
+ * Represents a Vega or Vega Lite channel for a particular chart type,
+ * and whether the user has encoded a specific field into it.
+ */
+class EncodingChannel extends React.Component {
+
+  static onDropField(maybeSource) {
+    // TODO: Implement Drag'N'Drop and this transform, COMPASS-709 ...
+    const channel = maybeSource;
+    const field = maybeSource;
+    Actions.mapFieldToChannel(channel, field);
+    // TODO: Swap fields if source DraggableField is the ChartPanel, like http://vega.github.io/polestar/
+  }
+
+  render() {
+    // TODO: required/optional CSS className
+    // const cssOptional = this.props.optional === 'required' ? CSS : CSS;
+    const cssOptional = '';
+    const chartChannelId = `chart-panel-channel-${this.props.fieldName}`;
+    const placeholder = 'drop a field here';
+    return (
+      <div>
+        <label className={cssOptional} htmlFor={chartChannelId}>
+          {this.props.fieldName}
+        </label>
+        <span id={chartChannelId}>{this.props.encodedChannel || placeholder}</span>
+      </div>
+    );
+  }
+}
+
+EncodingChannel.propTypes = {
+  encodedChannel: React.PropTypes.string,
+  fieldName: React.PropTypes.string.isRequired,
+  optional: React.PropTypes.string,
+
+  // Not yet sure whether we want to disable charts, e.g. on a secondary,
+  // if we can't save and share them?
+  isWritable: React.PropTypes.bool
+};
+
+EncodingChannel.defaultProps = {
+  isWritable: true
+};
+
+EncodingChannel.displayName = 'EncodingChannel';
+
+module.exports = EncodingChannel;
