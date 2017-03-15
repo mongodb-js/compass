cfacd448d49675a1a7f81eef720a4bb2fc57a455
Durran Jordan <durran@gmail.com>
2017-03-15 14:06:01 +0100

    COMPASS-758: Pass HADRON_DISTRIBUTION to package manager

--- src/app/setup-package-manager.js
+++ src/app/setup-package-manager.js
@@ -1,8 +1,22 @@
 const app = require('hadron-app');
+const pkg = require('../../package.json');
 const path = require('path');
 const AppRegistry = require('hadron-app-registry');
 const { PackageManager } = require('hadron-package-manager');
+const debug = require('debug')('mongodb-compass:setup-package-manager');
 
 app.appRegistry = new AppRegistry();
-app.packageManager = new PackageManager(path.join(__dirname, '..', 'internal-packages'));
+
+/**
+ * @note: The 2nd and 3rd arguments are the root directory and an array
+ *   of packages for the distribution and their relative paths from the
+ *   root directory.
+ */
+app.packageManager = new PackageManager(
+  path.join(__dirname, '..', 'internal-packages'),
+  path.join(__dirname, '..', '..'),
+  pkg.config.hadron.distributions[process.env.HADRON_DISTRIBUTION].packages
+);
 app.packageManager.activate();
+
+debug(`Package manager activated with distribution ${process.env.HADRON_DISTRIBUTION}.`);
