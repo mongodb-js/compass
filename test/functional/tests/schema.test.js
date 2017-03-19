abc1b81cbe942bdef79ce402de04366113adc8bf
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2017-03-19 20:47:50 +1100

    enable and fix connection & create + go to collection in schema test

--- test/functional/tests/schema.test.js
+++ test/functional/tests/schema.test.js
@@ -9,8 +9,19 @@ context('Schema', function() {
     launchCompass().then(function(application) {
       app = application;
       client = application.client;
-      client.connectToCompass({ hostname: 'localhost', port: 27018 });
-      done();
+      client
+        .connectToCompass({ hostname: 'localhost', port: 27018 })
+        .waitForWindowTitle('MongoDB Compass - localhost:27018')
+        .createDatabaseCollection('music', 'artists')
+        .goToCollection('music', 'artists')
+        .insertDocument({
+          'name': 'Aphex Twin',
+          'genre': 'Electronic',
+          'location': 'London'
+        }, 1)
+        .then(() => {
+          done();
+        });
     });
   });
 
@@ -20,13 +31,36 @@ context('Schema', function() {
 
   context('when applying a filter', function() {
     const filter = '{"name":"Bonobo"}';
+    const expectedZeroDoc = 'Query returned 0 documents.';
+    const expectedOneDoc = 'Query returned 1 document.';
+    const expectedZeroReport = 'This report is based on a sample of 0 documents (0.00%).';
 
-    it('updates the schema view', function() {
-      const expected = 'This report is based on a sample of 0 documents (0.00%).';
+    it('shows a blank schema view', function() {
       return client
         .clickSchemaTab()
         .getSamplingMessageFromSchemaTab()
-        .should.eventually.include(expected);
+        .should.eventually.include(`${expectedOneDoc}`);
+    });
+
+    it('shows a schema on refresh', function() {
+      return client
+        .clickDatabaseInSidebar('music')
+        .waitForDatabaseView()
+        .goToCollection('music', 'artists')
+        .getSamplingMessageFromSchemaTab()
+        .should
+        .eventually
+        .include(`${expectedOneDoc} This report is based on a sample of 1 document (100.00%).`);
+    });
+
+    it('applies the filter from schema view', function() {
+      return client
+        .inputFilterFromSchemaTab(filter)
+        .waitForStatusBar()
+        .clickApplyFilterButtonFromSchemaTab()
+        .waitForStatusBar()
+        .getSamplingMessageFromSchemaTab()
+        .should.eventually.include(`${expectedZeroDoc} ${expectedZeroReport}`);
     });
 
     it('checks the collections table', function() {
@@ -39,16 +73,13 @@ context('Schema', function() {
 
     it('applies the filter again while on schema tab', function() {
       return client
-        .waitForStatusBar()
-        .clickCollectionInSidebar('music.artists')
-        .waitForStatusBar()
+        .goToCollection('music', 'artists')
         .inputFilterFromSchemaTab(filter)
         .waitForStatusBar()
         .clickApplyFilterButtonFromSchemaTab()
+        .waitForStatusBar()
         .getSamplingMessageFromSchemaTab()
-        .should
-        .eventually
-        .equal('Query returned 0 documents. This report is based on a sample of 0 documents (0.00%).');
+        .should.eventually.equal(`${expectedZeroDoc} ${expectedZeroReport}`);
     });
   });
 });
