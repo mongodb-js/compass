74c82112702ca54cda087648d88171dea5fcd0d4
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2017-03-24 14:58:03 +1100

    replace waiting for window title with instance header

--- test/functional/support/packages/spectron-workflow.js
+++ test/functional/support/packages/spectron-workflow.js
@@ -8,12 +8,11 @@ function addWorkflowCommands(client) {
    * Connect to Compass
    */
   client.addCommand('connectToCompass', function(connection) {
-    const title = `MongoDB Compass - ${connection.hostname}:${connection.port}`;
+    // const title = `MongoDB Compass - ${connection.hostname}:${connection.port}`;
     return this
       .inputConnectionDetails(connection)
       .clickConnectButton()
-      .waitForStatusBar()
-      .waitForWindowTitle(title);
+      .waitForInstanceHeader();
   });
 
   /**
