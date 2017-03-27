d35ce906e5bf0bee93c5baef186c4b6f573fa0b6
anna herlihy <anna.herlihy@10gen.com>
2017-03-27 12:54:07 +0200

    Merge pull request #13 from 10gen/activate
    Add activate/deactivate to package

--- packages/compass-serverstats/index.js
+++ packages/compass-serverstats/index.js
@@ -1,1 +1,19 @@
 module.exports.d3 = require('./lib/d3/');
+const app = require('hadron-app');
+const RTSSComponent = require('./lib/components');
+/**
+ * Activate all the components in the RTSS package.
+ */
+function activate() {
+  app.appRegistry.registerComponent('RTSS.ServerStats', RTSSComponent);
+}
+
+/**
+ * Deactivate all the components in the RTSS package.
+ */
+function deactivate() {
+  app.appRegistry.deregisterComponent('RTSS.ServerStats');
+}
+
+module.exports.activate = activate;
+module.exports.deactivate = deactivate;
