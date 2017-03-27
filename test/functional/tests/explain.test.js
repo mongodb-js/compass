31264ba29811a03ddb45fe67706fea00e646e154
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2017-03-27 12:31:22 +1100

    add extra context level

--- test/functional/tests/explain.test.js
+++ test/functional/tests/explain.test.js
@@ -9,6 +9,12 @@ context('Explain', function() {
     launchCompass().then(function(application) {
       app = application;
       client = application.client;
+      done();
+    });
+  });
+
+  context('run the tests', function() {
+    before(function(done) {
       client
         .connectToCompass({ hostname: 'localhost', port: 27018 })
         .createDatabaseCollection('music', 'artists')
@@ -17,71 +23,71 @@ context('Explain', function() {
           done();
         });
     });
-  });
 
-  after(function(done) {
-    client
-      .teardownTest('music').then(() => {
-        quitCompass(app, done);
-      });
-  });
-
-  context('when applying a filter', function() {
-    const filter = '{"name":"Bonobo"}';
-
-    before(function(done) {
+    after(function(done) {
       client
-        .insertDocument({
-          'name': 'Aphex Twin',
-          'genre': 'Electronic',
-          'location': 'London'
-        }, 1)
-        .then(() => {
-          done();
+        .teardownTest('music').then(() => {
+          quitCompass(app, done);
         });
     });
 
-    context('when viewing the explain plan view', function() {
-      it('applies the filter in the explain plan tab', function() {
+    context('when applying a filter', function() {
+      const filter = '{"name":"Bonobo"}';
+
+      before(function(done) {
+        client
+          .insertDocument({
+            'name': 'Aphex Twin',
+            'genre': 'Electronic',
+            'location': 'London'
+          }, 1)
+          .then(() => {
+            done();
+          });
+      });
+
+      context('when viewing the explain plan view', function() {
+        it('applies the filter in the explain plan tab', function() {
+          return client
+            .clickExplainPlanTab()
+            .inputFilterFromExplainPlanTab(filter)
+            .clickApplyFilterButtonFromExplainPlanTab()
+            .waitForStatusBar()
+            .getExplainDocumentsReturned()
+            .should.eventually.equal('0');
+        });
+
+        it('updates the documents examined', function() {
+          return client
+            .getExplainDocumentsExamined()
+            .should.eventually.equal('1');
+        });
+      });
+    });
+
+    context('when resetting a filter', function() {
+      it('updates the explain plan', function() {
         return client
-          .clickExplainPlanTab()
-          .inputFilterFromExplainPlanTab(filter)
-          .clickApplyFilterButtonFromExplainPlanTab()
+          .clickResetFilterButtonFromExplainPlanTab()
           .waitForStatusBar()
-          .getExplainDocumentsReturned()
-          .should.eventually.equal('0');
+          .getExplainPlanStatusMessage()
+          .should.eventually.include('To prevent unintended collection scans');
       });
 
-      it('updates the documents examined', function() {
+      it('updates the document list', function() {
         return client
-          .getExplainDocumentsExamined()
-          .should.eventually.equal('1');
+          .clickDocumentsTab()
+          .getSamplingMessageFromDocumentsTab()
+          .should.eventually.include('Query returned 1 document.');
       });
-    });
-  });
 
-  context('when resetting a filter', function() {
-    it('updates the explain plan', function() {
-      return client
-        .clickResetFilterButtonFromExplainPlanTab()
-        .waitForStatusBar()
-        .getExplainPlanStatusMessage()
-        .should.eventually.include('To prevent unintended collection scans');
-    });
-
-    it('updates the document list', function() {
-      return client
-        .clickDocumentsTab()
-        .getSamplingMessageFromDocumentsTab()
-        .should.eventually.include('Query returned 1 document.');
-    });
-
-    it('updates the schema view', function() {
-      const expected = 'This report is based on a sample of 1 document (100.00%).';
-      return client
-        .clickSchemaTab()
-        .getSamplingMessageFromSchemaTab()
-        .should.eventually.include(expected);
+      it('updates the schema view', function() {
+        const expected = 'This report is based on a sample of 1 document (100.00%).';
+        return client
+          .clickSchemaTab()
+          .getSamplingMessageFromSchemaTab()
+          .should.eventually.include(expected);
+      });
     });
   });
 });
