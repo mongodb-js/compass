bde6878fd756ab6f939484e76b50a63cef427637
Peter Schmidt <peter@peterjs.com>
2017-03-28 15:17:38 +1100

    Add overflow and margin to chart draggable field labels (#885)
    * Add overflow and padding to chart draggable field labels
    
    So you can click the aggregation dropdown for fields longer than 20 characters, and the `text-overflow: ellipsis` actually does something:
    https://css-tricks.com/almanac/properties/t/text-overflow/
    
    * chart-draggable-field: Update label => title
    
    * Update padding to margin
    
    So you don't see the fields above and below when dragging a DraggableField from the FieldPanel to the ChartPanel.
    
    * Add fieldPath to the default HTML DraggableField title tooltip
    
    For consistency with other areas, e.g.
    https://github.com/10gen/compass/blob/b4a5a26/src/internal-packages/sidebar/lib/components/sidebar-collection.jsx#L77

--- src/internal-packages/chart/lib/components/draggable-field.jsx
+++ src/internal-packages/chart/lib/components/draggable-field.jsx
@@ -124,9 +124,9 @@ class DraggableField extends React.Component {
   render() {
     const connectDragSource = this.props.connectDragSource;
     return connectDragSource(
-      <div className="chart-draggable-field">
+      <div className="chart-draggable-field" title={this.props.fieldPath}>
         {this.props.enableMenus ? this.renderMeasurementMenu() : <div></div>}
-        <div className="chart-draggable-field-label">
+        <div className="chart-draggable-field-title">
           {this.props.fieldName}
         </div>
         {this.props.enableMenus ? this.renderAggregationMenu() : <div></div>}
