28a3ff505fc5a5494791845d80d75f9bba8215f7
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2017-03-30 17:00:54 +1100

    include schema test at the top level

--- test/functional/schema.test.js
+++ test/functional/schema.test.js
@@ -1,33 +1,33 @@
-const { launchCompass, quitCompass} = require('../support/spectron-support');
+const { launchCompass, quitCompass} = require('./support/spectron-support');
 
-context('Schema', function() {
+context('#schema Schema', function() {
   this.slow(30000);
   this.timeout(60000);
   let app = null;
   let client = null;
-  before(function(done) {
-    launchCompass().then(function(application) {
-      app = application;
-      client = application.client;
-      client
-        .connectToCompass({ hostname: 'localhost', port: 27018 })
-        .createDatabaseCollection('music', 'artists')
-        .goToCollection('music', 'artists')
-        .insertDocument({
-          'name': 'Aphex Twin',
-          'genre': 'Electronic',
-          'location': 'London'
-        }, 1)
-        .then(() => {
-          done();
-        });
-    });
+
+  before(function() {
+    return launchCompass()
+      .then(function(application) {
+        app = application;
+        client = application.client;
+        return client
+          .connectToCompass({ hostname: 'localhost', port: 27018 })
+          .createDatabaseCollection('music', 'artists')
+          .goToCollection('music', 'artists')
+          .insertDocument({
+            'name': 'Aphex Twin',
+            'genre': 'Electronic',
+            'location': 'London'
+          }, 1);
+      });
   });
 
-  after(function(done) {
-    client
-      .teardownTest('music').then(() => {
-        quitCompass(app, done);
+  after(function() {
+    return client
+      .teardownTest('music')
+      .then(() => {
+        return quitCompass(app);
       });
   });
 
