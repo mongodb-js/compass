5fe100db30678c7ea058da9870653e8c499ea12b
Peter Schmidt <peter@peterjs.com>
2017-04-06 16:31:25 +1000

    Revert "Introduce more Fibonacci-ish timeouts"
    This reverts commit f5f04f430a766c5af5652e63bcc2cc8b00f39208.

--- test/functional/support/spectron-support.js
+++ test/functional/support/spectron-support.js
@@ -60,10 +60,6 @@ const ELECTRON_EXECUTABLE = path.join(ELECTRON, fs.readFileSync(ELECTRON_PATH, {
  */
 const TIMEOUT_MAX = 3000;
 const TIMEOUTS = [
-  100,
-  200,
-  300,
-  500,
   1000,
   2000,
   TIMEOUT_MAX
