c2e055f7b7b9c711d16791355948197810b5aa68
Peter Schmidt <peter@peterjs.com>
2017-04-06 16:14:38 +1000

    Revert "Drop the 2000ms timeout"
    Got what I expected, so we will want to keep at least the 2s timeout:
    https://travis-ci.com/10gen/compass/jobs/71235527

--- test/functional/support/spectron-support.js
+++ test/functional/support/spectron-support.js
@@ -58,12 +58,13 @@ const ELECTRON_EXECUTABLE = path.join(ELECTRON, fs.readFileSync(ELECTRON_PATH, {
 /**
  * The progressive timeouts when searching for elements.
  */
-const TIMEOUT_MAX = 1000;
+const TIMEOUT_MAX = 2000;
 const TIMEOUTS = [
   100,
   200,
   300,
   500,
+  1000,
   TIMEOUT_MAX
 ];
 
