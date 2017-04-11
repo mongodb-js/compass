41f721dbe790f71cb5f6c9bfbdbbc1f0d8169f61
anna herlihy <anna.herlihy@10gen.com>
2017-04-11 15:28:27 +0200

    COMPASS-1007 - Add CollectionStats Role (#929)

--- src/internal-packages/collection-stats/index.js
+++ src/internal-packages/collection-stats/index.js
@@ -1,10 +1,26 @@
 const app = require('hadron-app');
 const CollectionStats = require('./lib/component/');
+const DocumentsStatsItem = require('./lib/component/documents-stats-item');
+const IndexesStatsItem = require('./lib/component/indexes-stats-item');
+
+const DOCUMENTS_STATS_ITEM_ROLE = {
+  component: DocumentsStatsItem,
+  name: 'DOC STATS',
+  order: 1
+};
+
+const INDEXES_STATS_ITEM_ROLE = {
+  component: IndexesStatsItem,
+  name: 'INDEX STATS',
+  order: 2
+};
 
 /**
  * Activate all the components in the collection stats package.
  */
 function activate() {
+  app.appRegistry.registerRole('CollectionStats.CollectionStatsItem', DOCUMENTS_STATS_ITEM_ROLE);
+  app.appRegistry.registerRole('CollectionStats.CollectionStatsItem', INDEXES_STATS_ITEM_ROLE);
   app.appRegistry.registerComponent('CollectionStats.CollectionStats', CollectionStats);
 }
 
@@ -12,7 +28,9 @@ function activate() {
  * Deactivate all the components in the collection stats package.
  */
 function deactivate() {
-  app.appRegistry.deregisterComponent('CollectionStats.CollectionStats');
+  app.appRegistry.deregisterRole('CollectionStats.CollectionItem', DOCUMENTS_STATS_ITEM_ROLE);
+  app.appRegistry.deregisterRole('CollectionStats.CollectionStatsItem', INDEXES_STATS_ITEM_ROLE);
+  app.appRegistry.deregisterComponent('CollectionStats.CollectionStats', CollectionStats);
 }
 
 module.exports.activate = activate;
---
+++ src/internal-packages/collection-stats/lib/component/indexes-stats-item.jsx
@@ -0,0 +1,96 @@
+const React = require('react');
+const CollectionStatsItem = require('./collection-stats-item');
+const CollectionStatsStore = require('../store');
+
+/**
+ * The list class.
+ */
+const LIST_CLASS = 'collection-stats-list';
+
+/**
+ * The invalid text.
+ */
+const INVALID = 'N/A';
+
+/**
+ * Indexes constant.
+ */
+const INDEXES = 'Indexes';
+
+/**
+ * Total size constant.
+ */
+const TOTAL_SIZE = 'total size';
+
+/**
+ * Average size constant.
+ */
+const AVG_SIZE = 'avg. size';
+
+/**
+ * The default stats state.
+ */
+const DEFAULT_STATS = {
+  indexCount: INVALID,
+  totalIndexSize: INVALID,
+  avgIndexSize: INVALID
+};
+
+/**
+ * The collection indexes stats component.
+ */
+class IndexesStatsItem extends React.Component {
+
+  /**
+   * Instantiate the component.
+   *
+   * @param {Object} props - The properties.
+   */
+  constructor(props) {
+    super(props);
+    this.state = DEFAULT_STATS;
+  }
+
+  /**
+   * Subscribe on mount.
+   */
+  componentWillMount() {
+    this.unsubscribeLoad = CollectionStatsStore.listen(this.handleStatsLoad.bind(this));
+  }
+
+  /**
+   * Unsubscribe on unmount.
+   */
+  componentWillUnmount() {
+    this.unsubscribeLoad();
+  }
+
+  /**
+   * Handle the loading of the collection stats.
+   *
+   * @param {Object} stats - The stats.
+   */
+  handleStatsLoad(stats) {
+    this.setState(stats || DEFAULT_STATS);
+  }
+
+  /**
+   * Render the component.
+   *
+   * @returns {React.Component} The component.
+   *
+   */
+  render() {
+    return (
+      <ul className={LIST_CLASS}>
+        <CollectionStatsItem label={INDEXES} value={this.state.indexCount} primary />
+        <CollectionStatsItem label={TOTAL_SIZE} value={this.state.totalIndexSize} />
+        <CollectionStatsItem label={AVG_SIZE} value={this.state.avgIndexSize} />
+      </ul>
+    );
+  }
+}
+
+IndexesStatsItem.displayName = 'IndexesStatsItem';
+
+module.exports = IndexesStatsItem;
