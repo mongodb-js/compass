dca7e24d9e9f975e7b1b99135fbcf7cc9c7afc50
Durran Jordan <durran@gmail.com>
2017-04-27 11:36:39 -0400

    COMPASS-1057: Read plugins from .compass as well (#960)

--- src/app/setup-package-manager.js
+++ src/app/setup-package-manager.js
@@ -1,6 +1,7 @@
 const app = require('hadron-app');
 const pkg = require('../../package.json');
 const path = require('path');
+const os = require('os');
 const AppRegistry = require('hadron-app-registry');
 const { PackageManager } = require('hadron-package-manager');
 const debug = require('debug')('mongodb-compass:setup-package-manager');
@@ -8,15 +9,31 @@ const debug = require('debug')('mongodb-compass:setup-package-manager');
 app.appRegistry = new AppRegistry();
 
 /**
+ * Location of the internal packages.
+ */
+const INTERNAL_PACKAGES = path.join(__dirname, '..', 'internal-packages');
+
+/**
+ * Location of the dev packages.
+ */
+const DEV_PACKAGES = path.join(os.homedir(), '.compass');
+
+/**
+ * The root dir.
+ */
+const ROOT = path.join(__dirname, '..', '..');
+
+/**
  * @note: The 2nd and 3rd arguments are the root directory and an array
  *   of packages for the distribution and their relative paths from the
  *   root directory.
  */
 app.packageManager = new PackageManager(
-  path.join(__dirname, '..', 'internal-packages'),
-  path.join(__dirname, '..', '..'),
+  [ INTERNAL_PACKAGES, DEV_PACKAGES ],
+  ROOT,
   pkg.config.hadron.distributions[process.env.HADRON_DISTRIBUTION].packages
 );
+
 app.packageManager.activate();
 
 debug(`Package manager activated with distribution ${process.env.HADRON_DISTRIBUTION}.`);
