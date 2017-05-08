17151ff6cc08f888bb092891cae32fbc45acf4a9
Fred Truman <fred@mongodb.com>
2017-05-08 15:47:38 -0400

    add new modules beside collection stats

--- src/internal-packages/collection/lib/components/index.jsx
+++ src/internal-packages/collection/lib/components/index.jsx
@@ -85,19 +85,17 @@ class Collection extends React.Component {
     return (
       <div className="collection-view clearfix">
         <header>
-          <div className="row">
-            <div className="col-md-6">
-              <h1>
-                <a onClick={this.onDBClick.bind(this)}>{database}</a>.
-                <span>{collection}</span>
-                {this.renderReadonly()}
-              </h1>
-            </div>
-            <div className="col-md-6">
-              <this.Stats />
-            </div>
+          <div className="collection-view-header-item">
+            <h1 className="collection-view-title">
+              <span className="collection-view-database-name"><a className="collection-view-database-name-link" title={database} onClick={this.onDBClick.bind(this)}>{database}</a></span>
+              <span>.</span>
+              <span className="collection-view-collection-name" title={collection}>{collection}</span>
+              {this.renderReadonly()}
+            </h1>
           </div>
+          <this.Stats />
         </header>
+
         <TabNavBar
           theme="light"
           tabs={this.tabs}
