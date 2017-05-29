35b403da51f4210371ca1363281c6457c8348568
Durran Jordan <durran@gmail.com>
2017-05-29 15:16:10 +0200

    Bumping dependencies, removing reflux

--- packages/data-service/index.js
+++ packages/data-service/index.js
@@ -1,3 +1,1 @@
 module.exports = require('./lib/data-service');
-module.exports.DataServiceActions = require('./lib/actions');
-module.exports.DataServiceStore = require('./lib/data-service-store');
