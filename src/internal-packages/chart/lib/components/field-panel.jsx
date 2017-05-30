be7fc813b628fe478ca93bc49515f7fae2df82ca
Fred Truman <fred@mongodb.com>
2017-05-30 17:34:36 -0400

    remove search for now; switch in new add calucalted field button icon; encoding channel styles

--- src/internal-packages/chart/lib/components/field-panel.jsx
+++ src/internal-packages/chart/lib/components/field-panel.jsx
@@ -4,12 +4,8 @@ const FieldPanelItem = require('./field-panel-item');
 
 class FieldPanel extends React.Component {
 
-  renderSearchIcon() {
-    return 'fa fa-search';
-  }
-
   renderAddIcon() {
-    return 'fa fa-plus';
+    return 'mms-icon-add-circle chart-builder-field-panel-controls-action chart-builder-field-panel-controls-item';
   }
 
   renderFields() {
@@ -38,8 +34,7 @@ class FieldPanel extends React.Component {
       >
         <div className="chart-builder-field-panel-controls-row">
           <h5 className="chart-builder-field-panel-controls-item chart-builder-field-panel-field-count">72 Fields</h5>
-          <button className="chart-builder-field-panel-controls-action chart-builder-field-panel-controls-item btn btn-default btn-xs" title="Add calculated field"><i className={this.renderAddIcon()} aria-hidden="true"></i></button>
-          <button className="chart-builder-field-panel-controls-action chart-builder-field-panel-controls-item btn btn-default btn-xs" title="Search fields"><i className={this.renderSearchIcon()} aria-hidden="true"></i></button>
+          <i className={this.renderAddIcon()} aria-hidden="true"></i>
         </div>
         {this.renderFields()}
       </div>
