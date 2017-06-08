e9dc9d3720cea3d96aa9988a7a0f1fad7be227f7
Sean Oh <sean.oh.1994@gmail.com>
2017-06-08 17:04:27 -0400

    Missing button classnames for sidebar expand

--- src/internal-packages/sidebar/lib/components/sidebar.jsx
+++ src/internal-packages/sidebar/lib/components/sidebar.jsx
@@ -227,12 +227,12 @@ class Sidebar extends React.Component {
         className={this.getSidebarClasses()}
         data-test-id="instance-sidebar"
         onClick={this.handleExpand.bind(this)}>
-        <div className="compass-sidebar-toggle"
+        <button className="compass-sidebar-toggle btn btn-default btn-sm"
           onClick={this.handleCollapse.bind(this)}
           data-test-id="toggle-sidebar"
         >
           <i className={this.getToggleClasses()}></i>
-        </div>
+        </button>
         <SidebarInstanceProperties
           instance={this.props.instance}
           activeNamespace={this.props.activeNamespace}
