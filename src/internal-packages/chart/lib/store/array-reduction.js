2b6a21abe910be6425e203d06968515fadfaaeb9
Thomas Rückstieß <rueckstiess@users.noreply.github.com>
2017-06-09 14:17:37 +1000

    COMPASS-1225 agg builder for array reductions (#1045)
    * move store to stores/index.js, add array-reduction
    
    * add mixed reduction use case (unwind / min)
    
    * better documentation.
    
    * remove `field` from main function signature
    
    * betterer comments.
    
    * add todo for array->doc->array case.
    
    * addressing peters comments in code review.
    
    * use $strLenCP and reverse reductions array order
    
    the order is now passed as outside->inside.

---
+++ src/internal-packages/chart/lib/store/array-reduction.js
@@ -0,0 +1,157 @@
+const _ = require('lodash');
+
+/**
+ * map wrapper around aggregation framework $map. Applies `expr` function
+ * to each element in the array `arr`. Returns the agg framework operators
+ * to represent the mapping.
+ *
+ * @param       {Array} arr       array to map over
+ * @param       {Function} expr   function to apply to each element in the array
+ * @return      {Array}           resulting array
+ */
+function _map(arr, expr) {
+  return {
+    $map: {
+      input: arr,
+      as: 'value',
+      in: expr
+    }
+  };
+}
+
+/**
+ * Array reduction operators wrapped as javascript functions
+ */
+const REDUCTIONS = Object.freeze({
+  length: function(arr) {
+    return {$size: arr};
+  },
+  max: function(arr) {
+    return {$max: arr};
+  },
+  min: function(arr) {
+    return {$min: arr};
+  },
+  mean: function(arr) {
+    return {$avg: arr};
+  },
+  index: function(arr, args) {
+    return {$arrayElemAt: [arr, args[0]]};
+  },
+  maxStringLength: function(arr) {
+    return {$max: {$map: {input: arr, as: 'str', in: {$strLenCP: '$$str'}}}};
+  },
+  minStringLength: function(arr) {
+    return {$min: {$map: {input: arr, as: 'str', in: {$strLenCP: '$$str'}}}};
+  }
+});
+
+/**
+ * Filters out all unwind reductions from the reductions array and builds
+ * $unwind stages for the respective fields.
+ *
+ * @param  {Array} reductions   array of reductions, following the following
+ *
+ * @return {Array}              array of $unwind stages, might be empty
+ */
+function constructUnwindStages(reductions) {
+  return _(reductions)
+    .filter((reduction) => {
+      return reduction.type === 'unwind';
+    })
+    .map((reduction) => {
+      return {$unwind: reduction.field};
+    })
+    .value();
+}
+
+/**
+ * Takes an array of reductions and creates an aggregation pipeline stage
+ * to reduce (possibly nested) arrays to a single scalar value according to
+ * the provided reduction functions
+ *
+ * @todo (thomas) I'm pretty sure the use case of array with sub docs with
+ * nested arrays is not yet working, as all but the first array to map over
+ * only use `$$value` so the other field names are not used anywhere yet.
+ *
+ * @param  {Array} reductions    an array of reductions as defined by the
+ *                               ChartStore, with the following format:
+ *
+ *             [
+ *               { field: "foo", type: "max", arguments: [] },
+ *               { field: "bar.baz" , type: "index", arguments: [3] }
+ *             ]
+ *
+ *                               The reductions are applied outside inwards, above
+ *                               example would result in `max(index(field, 3))`.
+ *
+ * @return {Object}              an $addFields aggregation stage that converts
+ *                               the given field array into a scalar value.
+ */
+function constructAccumulatorStage(reductions) {
+  let arr;
+  let expr;
+
+  reductions = _.filter(reductions, (reduction) => {
+    return reduction.type !== 'unwind';
+  });
+
+  // reverse the array (without modifying original), below code assumes inside->out order
+  reductions = reductions.slice().reverse();
+  const lastReduction = reductions[reductions.length - 1];
+
+  if (reductions.length === 0) {
+    // if no reductions are present, return empty array
+    return null;
+  }
+
+  if (reductions.length === 1) {
+    // with only one reduction, return the reduction applied to the field
+    // directly.
+    arr = `$${lastReduction.field}`;
+  } else {
+    // first (inner-most) reduction has no map and applies the reducer expression directly
+    arr = '$$value';
+    expr = REDUCTIONS[reductions[0].type](arr);
+
+    // second to second last reductions use a map but pass $$value down
+    reductions.slice(1, -1).forEach((reduction) => {
+      arr = _map('$$value', expr);
+      expr = REDUCTIONS[reduction.type](arr);
+    });
+
+    // last (outer-most) reduction uses the actual field name with a map
+    arr = _map(`$${lastReduction.field}`, expr);
+  }
+  expr = REDUCTIONS[lastReduction.type](arr);
+
+  // we use $addFields to overwrite the original field name
+  return {$addFields: {[lastReduction.field]: expr}};
+}
+
+/**
+ * helper to create aggregation pipeline stages to reduce arrays, based on
+ * the given reductions array.
+ *
+ * @param  {Array} reductions  array of reductions, following the following
+ *                             format:
+ *             [
+ *               { field: "foo", type: "$unwind", arguments: [] },
+ *               { field: "bar.baz" , type: "index", arguments: [3] }
+ *             ]
+ *
+ * @return {Array}            pipeline stages to reduce all arrays
+ */
+module.exports = function arrayReductionAggBuilder(reductions) {
+  const pipeline = [];
+
+  const unwindStages = constructUnwindStages(reductions);
+  const accumulatorStage = constructAccumulatorStage(reductions);
+
+  // combine pipeline
+  pipeline.push.apply(pipeline, unwindStages);
+  if (accumulatorStage) {
+    pipeline.push(accumulatorStage);
+  }
+  return pipeline;
+};
---
+++ test/unit/chart.array-reduction.test.js
@@ -0,0 +1,157 @@
+/* eslint no-unused-expressions: 0 */
+
+const { expect } = require('chai');
+const aggBuilder = require('../../src/internal-packages/chart/lib/store/array-reduction');
+
+describe('Array Reduction', function() {
+  context('no reductions are present', function() {
+    const reductions = [];
+    it('returns `null`', function() {
+      const result = aggBuilder(reductions);
+      expect(result).to.be.an('array');
+      expect(result).to.be.empty;
+    });
+  });
+  context('one reduction is present', function() {
+    const reductions = [
+      {field: 'myField', type: 'min'}
+    ];
+    it('builds the correct agg pipeline', function() {
+      const result = aggBuilder(reductions);
+      expect(result).to.be.an('array');
+      expect(result[0]).to.be.deep.equal({
+        $addFields: {
+          myField: {
+            $min: '$myField'
+          }
+        }
+      });
+    });
+  });
+  context('two reductions are present', function() {
+    const reductions = [
+      {field: 'myField', type: 'max'},
+      {field: 'myField', type: 'min'}
+    ];
+    it('builds the correct agg pipeline', function() {
+      const result = aggBuilder(reductions);
+      expect(result).to.be.an('array');
+      expect(result[0]).to.be.deep.equal({
+        $addFields: {
+          myField: {
+            $max: {
+              $map: {
+                input: '$myField',
+                as: 'value',
+                in: {
+                  $min: '$$value'
+                }
+              }
+            }
+          }
+        }
+      });
+    });
+  });
+  context('three reductions are present', function() {
+    const reductions = [
+      {field: 'myField', type: 'mean'},
+      {field: 'myField', type: 'min'},
+      {field: 'myField', type: 'length'}
+    ];
+    it('builds the correct agg pipeline', function() {
+      const result = aggBuilder(reductions);
+      expect(result).to.be.an('array');
+      expect(result[0]).to.be.deep.equal({
+        $addFields: {
+          myField: {
+            $avg: {
+              $map: {
+                input: '$myField',
+                as: 'value',
+                in: {
+                  $min: {
+                    $map: {
+                      input: '$$value',
+                      as: 'value',
+                      in: {
+                        $size: '$$value'
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      });
+    });
+  });
+  context('$unwind reduction', function() {
+    it('creates a unwind stage for a single unwind reduction', function() {
+      const reductions = [
+        {field: 'foo', type: 'unwind'}
+      ];
+      const result = aggBuilder(reductions);
+      expect(result).to.be.an('array');
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.be.deep.equal({
+        $unwind: 'foo'
+      });
+    });
+    it('creates multiple unwind stages one for each reduction', function() {
+      const reductions = [
+        {field: 'foo', type: 'unwind'},
+        {field: 'foo.bar.baz', type: 'unwind'}
+      ];
+      const result = aggBuilder(reductions);
+      expect(result).to.be.an('array');
+      expect(result).to.have.lengthOf(2);
+      expect(result[0]).to.be.deep.equal({
+        $unwind: 'foo'
+      });
+      expect(result[1]).to.be.deep.equal({
+        $unwind: 'foo.bar.baz'
+      });
+    });
+    it('creates an unwind stage and an addField stage for mixed reductions', function() {
+      const reductions = [
+        {field: 'foo', type: 'unwind'},
+        {field: 'foo.bar.baz', type: 'min'}
+      ];
+      const result = aggBuilder(reductions);
+      expect(result).to.be.an('array');
+      expect(result).to.have.lengthOf(2);
+      expect(result[0]).to.be.deep.equal({
+        $unwind: 'foo'
+      });
+      expect(result[1]).to.be.deep.equal({
+        $addFields: {
+          'foo.bar.baz': {
+            $min: '$foo.bar.baz'
+          }
+        }
+      });
+    });
+  });
+  context('Reduction Operators', function() {
+    const reductions = [
+      {field: 'foo', type: 'maxStringLength'}
+    ];
+    it('calculates the maximum string length', function() {
+      const result = aggBuilder(reductions);
+      expect(result).to.be.an('array');
+      expect(result[0].$addFields.foo).to.be.deep.equal({
+        $max: {
+          $map: {
+            input: '$foo',
+            as: 'str',
+            in: {
+              $strLenCP: '$$str'
+            }
+          }
+        }
+      });
+    });
+  });
+});
