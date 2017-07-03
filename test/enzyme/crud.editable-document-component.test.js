8ac86d66ecdae28d3baa010c4aba5c95cf758109
Peter Schmidt <peter@peterjs.com>
2017-07-03 14:32:20 +1000

    :art::white_check_mark: Test for the expanded flag independent of edit mode
    Which editing mode does not matter here.

--- test/enzyme/crud.editable-document-component.test.js
+++ test/enzyme/crud.editable-document-component.test.js
@@ -31,32 +31,40 @@ describe('<EditableDocument />', function() {
   const doc = {a: {b: {c: {d: 1}}}};
   it('if expandAll is true, renders children', () => {
     const component = mount(<EditableDocument doc={doc} />);
-    component.setState({ expandAll: true });
+    component.setState({
+      expandAll: true
+    });
     const children = component.find(EditableElement);
     expect(children).to.be.of.length(4);
   });
   context('COMPASS_1306, if expandAll is false', () => {
-    it('if not in editing mode, does not render children', () => {
+    it('if expandAll is false, does not render children', () => {
       const component = mount(
         <EditableDocument doc={doc} />
       );
       component.setState({
-        editing: false,
         expandAll: false
       });
       const children = component.find(EditableElement);
       expect(children).to.be.of.length(1);
     });
-    it('if in editing mode, render children', () => {
-      const component = mount(
-        <EditableDocument doc={doc} />
-      );
-      component.setState({
-        editing: true,
-        expandAll: false
-      });
-      const children = component.find(EditableElement);
-      expect(children).to.be.of.length(4);
+    it('if expandAll is false but expand true, renders another child', () => {
+      // Work around https://github.com/airbnb/enzyme/issues/361
+      // by mounting the EditableElement rather than the EditableDocument,
+      // note this requires the HadronDocument constructor
+      const document = EditableDocument.loadDocument(doc);
+      for (const element of document.elements) {
+        const component = mount(
+          <EditableElement element={element} />
+        );
+        let children = component.find(EditableElement);
+        expect(children).to.be.of.length(1);
+        component.setState({
+          expanded: true
+        });
+        children = component.find(EditableElement);
+        expect(children).to.be.of.length(2);
+      }
     });
   });
 });
