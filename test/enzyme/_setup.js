1b84826e4fcac80edb87a9466755eb2150d09f7b
Peter Schmidt <peter@peterjs.com>
2017-07-04 11:26:45 +1000

    COMPASS-1312: Speed up docs tab for array party dataset (#1118)
    * :white_check_mark: Add enzyme test to cover expandAll children
    
    * Make enzyme tests work with the CRUD jQuery dropdown
    
    Feels a little hacky.
    
    * :art: Allow EditableDocument state.expandAll to be initally set from props
    
    * :fast_forward: COMPASS-1312: Speed up docs tab for array party dataset
    
    So Compass doesn't render hundreds of fields for 20 documents,
    just the one document that user cares about.
    
    Yields a far better performance trade-off of around 2 seconds until
    Documents tab loads and 2 seconds to click and expand all fields,
    instead of 20 seconds to load the Documents tab.
    
    Tested on the array_party data set, see COMPASS-1235.
    
    * :white_check_mark: Add enzyme test for edit expand :bug:
    
    * :art: Switch to component.setState()
    
    * :bug: Fix expanding individual arrows in editing mode
    
    * :art::white_check_mark: Test for the expanded flag independent of edit mode
    
    Which editing mode does not matter here.
    
    * :bug: Fix EditableElement expanded state regression for all modes
    
    i.e. both view and edit mode.

--- test/enzyme/_setup.js
+++ test/enzyme/_setup.js
@@ -9,6 +9,7 @@ const exposedProperties = ['window', 'navigator', 'document'];
 
 global.document = jsdom('');
 global.window = document.defaultView;
+global.window.jQuery = require('jquery');
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
