53ef1b8ca9e102b78f69720fe9e735cbbf091f1f
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2017-07-12 15:12:34 +1000

    move reduction constants to reductions.js

--- src/internal-packages/chart/lib/constants/index.js
+++ src/internal-packages/chart/lib/constants/index.js
@@ -1,61 +1,7 @@
 const channels = require('./channels');
-const types = require('./types');
+const reductions = require('./reductions');
 const spec = require('./spec');
-
-
-const TOOL_TIP_ARRAY_REDUCE = Object.freeze({
-  'data-for': 'array-reduce-tooltip',
-  'data-tip': 'In order to use fields or values<br/>'
-    + 'inside an array, the array has to<br/>'
-    + 'be reduced to a scalar value.<br/>'
-    + 'Choose from the array reduction<br/>'
-    + 'methods.'
-});
-
-/**
- * A list of the general array reduction types.
- */
-const ARRAY_GENERAL_REDUCTIONS = Object.freeze({
-  UNWIND: 'Unwind array',
-  LENGTH: 'Array length',
-  INDEX: 'Array element by index'  // TODO: Note is args not implemented in React <ArrayReductionPicker>
-});
-
-/**
- * A list of the numeric array reduction types, or accumulates.
- */
-const ARRAY_NUMERIC_REDUCTIONS = Object.freeze({
-  MIN: 'min',
-  MAX: 'max',
-  MEAN: 'mean',
-  SUM: 'sum'
-});
-
-/**
- * A list of the string array reduction types, or accumulates.
- */
-const ARRAY_STRING_REDUCTIONS = Object.freeze({
-  CONCAT: 'concat',
-  MIN_LENGTH: 'min length',
-  MAX_LENGTH: 'max length',
-  LONGEST: 'longest',
-  SHORTEST: 'shortest'
-});
-
-/**
- * A list of all the array reduction types available.
- */
-const ARRAY_REDUCTION_TYPES = Object.freeze(Object.assign(
-  {},
-  ARRAY_GENERAL_REDUCTIONS,
-  ARRAY_NUMERIC_REDUCTIONS,
-  ARRAY_STRING_REDUCTIONS
-));
+const types = require('./types');
 
 module.exports = Object.assign({
-  TOOL_TIP_ARRAY_REDUCE,
-  ARRAY_GENERAL_REDUCTIONS,
-  ARRAY_NUMERIC_REDUCTIONS,
-  ARRAY_STRING_REDUCTIONS,
-  ARRAY_REDUCTION_TYPES
-}, channels, spec, types);
+}, channels, reductions, spec, types);
