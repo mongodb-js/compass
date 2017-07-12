c8bf47f8f50916841aae4a7bb1e1f27d0c3a9ff8
KeyboardTsundoku <satyendra.n.sinha@gmail.com>
2017-07-12 14:48:33 +1000

    move channel related enums to channels.js

---
+++ src/internal-packages/chart/lib/constants/channels.js
@@ -0,0 +1,93 @@
+/**
+ * An enumeration of the subset of Vega-Lite aggregates we support.
+ * For a full list of things we could add, see:
+ *
+ * @see https://vega.github.io/vega-lite/docs/aggregate.html
+ */
+const AGGREGATE_FUNCTION_ENUM = Object.freeze({
+  NONE: '(none)',
+  COUNT: 'count',
+  DISTINCT: 'distinct',
+  SUM: 'sum',
+  MEAN: 'mean',
+  VARIANCE: 'variance',
+  VARIANCEP: 'variancep',
+  STDEV: 'stdev',
+  STDEVP: 'stdevp',
+  // MEDIAN: 'median',
+  // Q1: 'q1',           not supported in agg framework, comment out for now
+  // Q3: 'q3',
+  MIN: 'min',
+  MAX: 'max'
+});
+
+/**
+ * An enumeration of valid Mark Properties Channels.
+ *
+ * @see https://vega.github.io/vega-lite/docs/encoding.html#props-channels
+ */
+const MARK_PROPERTY_CHANNEL_ENUM = Object.freeze({
+  X: 'x',
+  Y: 'y',
+  COLOR: 'color',
+  OPACITY: 'opacity',
+  SHAPE: 'shape',
+  SIZE: 'size',
+  TEXT: 'text'
+  // Not doing row/column faceting yet
+});
+
+/**
+ * An enumeration for the Detail Channel.
+ *
+ * @see https://vega.github.io/vega-lite/docs/encoding.html#additional-level-of-detail-channel
+ */
+const DETAIL_CHANNEL_ENUM = Object.freeze({
+  DETAIL: 'detail'
+});
+
+// Omit for V1 unless we find a compelling use case :)
+// https://vega.github.io/vega-lite/docs/encoding.html#mark-order-channels
+// const ORDER_CHANNEL_ENUM = ...
+// https://vega.github.io/vega-lite/docs/encoding.html#facet
+// const FACET_CHANNEL_ENUM = ...
+
+/**
+ * Bundle of all valid Vega-Lite channel types we plan to support in Compass.
+ *
+ * @see https://vega.github.io/vega-lite/docs/encoding.html#channels
+ */
+const CHART_CHANNEL_ENUM = Object.freeze(Object.assign({},
+    MARK_PROPERTY_CHANNEL_ENUM,
+    DETAIL_CHANNEL_ENUM
+));
+
+/**
+ * An enumeration of the valid Data Type, or Measurement values.
+ *
+ * @see https://vega.github.io/vega-lite/docs/encoding.html#data-type
+ */
+const MEASUREMENT_ENUM = Object.freeze({
+  NOMINAL: 'nominal',
+  ORDINAL: 'ordinal',
+  QUANTITATIVE: 'quantitative',
+  TEMPORAL: 'temporal'
+});
+
+/**
+ * Icons to measurement mapping
+ * @type {Class} font-awesome icons
+ */
+const MEASUREMENT_ICON_ENUM = Object.freeze({
+  [MEASUREMENT_ENUM.NOMINAL]: 'font',
+  [MEASUREMENT_ENUM.ORDINAL]: 'sort-amount-asc',
+  [MEASUREMENT_ENUM.QUANTITATIVE]: 'hashtag',
+  [MEASUREMENT_ENUM.TEMPORAL]: 'calendar'
+});
+
+module.exports = {
+  AGGREGATE_FUNCTION_ENUM,
+  CHART_CHANNEL_ENUM,
+  MEASUREMENT_ENUM,
+  MEASUREMENT_ICON_ENUM
+};
--- src/internal-packages/chart/lib/constants/index.js
+++ src/internal-packages/chart/lib/constants/index.js
@@ -1,89 +1,4 @@
-/**
- * An enumeration of the subset of Vega-Lite aggregates we support.
- * For a full list of things we could add, see:
- *
- * @see https://vega.github.io/vega-lite/docs/aggregate.html
- */
-const AGGREGATE_FUNCTION_ENUM = Object.freeze({
-  NONE: '(none)',
-  COUNT: 'count',
-  DISTINCT: 'distinct',
-  SUM: 'sum',
-  MEAN: 'mean',
-  VARIANCE: 'variance',
-  VARIANCEP: 'variancep',
-  STDEV: 'stdev',
-  STDEVP: 'stdevp',
-  // MEDIAN: 'median',
-  // Q1: 'q1',           not supported in agg framework, comment out for now
-  // Q3: 'q3',
-  MIN: 'min',
-  MAX: 'max'
-});
-
-/**
- * An enumeration of valid Mark Properties Channels.
- *
- * @see https://vega.github.io/vega-lite/docs/encoding.html#props-channels
- */
-const MARK_PROPERTY_CHANNEL_ENUM = Object.freeze({
-  X: 'x',
-  Y: 'y',
-  COLOR: 'color',
-  OPACITY: 'opacity',
-  SHAPE: 'shape',
-  SIZE: 'size',
-  TEXT: 'text'
-  // Not doing row/column faceting yet
-});
-
-/**
- * An enumeration for the Detail Channel.
- *
- * @see https://vega.github.io/vega-lite/docs/encoding.html#additional-level-of-detail-channel
- */
-const DETAIL_CHANNEL_ENUM = Object.freeze({
-  DETAIL: 'detail'
-});
-
-// Omit for V1 unless we find a compelling use case :)
-// https://vega.github.io/vega-lite/docs/encoding.html#mark-order-channels
-// const ORDER_CHANNEL_ENUM = ...
-// https://vega.github.io/vega-lite/docs/encoding.html#facet
-// const FACET_CHANNEL_ENUM = ...
-
-/**
- * Bundle of all valid Vega-Lite channel types we plan to support in Compass.
- *
- * @see https://vega.github.io/vega-lite/docs/encoding.html#channels
- */
-const CHART_CHANNEL_ENUM = Object.freeze(Object.assign({},
-    MARK_PROPERTY_CHANNEL_ENUM,
-    DETAIL_CHANNEL_ENUM
-));
-
-/**
- * An enumeration of the valid Data Type, or Measurement values.
- *
- * @see https://vega.github.io/vega-lite/docs/encoding.html#data-type
- */
-const MEASUREMENT_ENUM = Object.freeze({
-  NOMINAL: 'nominal',
-  ORDINAL: 'ordinal',
-  QUANTITATIVE: 'quantitative',
-  TEMPORAL: 'temporal'
-});
-
-/**
- * Icons to measurement mapping
- * @type {Class} font-awesome icons
- */
-const MEASUREMENT_ICON_ENUM = Object.freeze({
-  [MEASUREMENT_ENUM.NOMINAL]: 'font',
-  [MEASUREMENT_ENUM.ORDINAL]: 'sort-amount-asc',
-  [MEASUREMENT_ENUM.QUANTITATIVE]: 'hashtag',
-  [MEASUREMENT_ENUM.TEMPORAL]: 'calendar'
-});
+const channels = require('./channels');
 
 const CHART_COLORS = Object.freeze({
   CHART0: '#43B1E5',
@@ -215,11 +130,7 @@ const LITE_SPEC_GLOBAL_SETTINGS = {
   }
 };
 
-module.exports = {
-  AGGREGATE_FUNCTION_ENUM,
-  CHART_CHANNEL_ENUM,
-  MEASUREMENT_ENUM,
-  MEASUREMENT_ICON_ENUM,
+module.exports = Object.assign({
   SPEC_TYPE_ENUM,
   VIEW_TYPE_ENUM,
   TOOL_TIP_ARRAY_REDUCE,
@@ -233,4 +144,4 @@ module.exports = {
   MIN_CHART_HEIGHT,
   MIN_CHART_WIDTH,
   LITE_SPEC_GLOBAL_SETTINGS
-};
+}, channels);
