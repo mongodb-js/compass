1a0816762bccf27855dd92c1cf5463a92828bd69
aherlihy <anna.herlihy@10gen.com>
2017-07-13 20:47:16 +0200

    including story

--- packages/compass-query-history/stories/QueryHistory.js
+++ packages/compass-query-history/stories/QueryHistory.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import QueryHistorySidebarComponent from '../src/components/sidebar-component';
-import ConnectedQueryHistoryComponent from '../src/components/';
+import SidebarComponent from '../src/components/sidebar-component';
+import QueryHistoryComponent from '../src/components/';
 
 storiesOf('SidebarComponent', module)
-  .add('connected to store', () => <ConnectedQueryHistoryComponent />)
-  .add('enabled', () => <QueryHistorySidebarComponent status="enabled" />)
-  .add('disabled', () => <QueryHistorySidebarComponent status="disabled" />);
+  .add('connected to store', () => <QueryHistoryComponent />)
+  .add('enabled', () => <SidebarComponent status="enabled" />)
+  .add('disabled', () => <SidebarComponent status="disabled" />);
