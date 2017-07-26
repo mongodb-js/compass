aec5e85b2c09d9c7cde39b81ec8e5a680003a770
Sean Oh <sean.oh.1994@gmail.com>
2017-07-26 01:04:25 -0400

    Initial progress on making query history button a hide/show toggle

--- packages/compass-query-history/src/components/show-query-history-button.jsx
+++ packages/compass-query-history/src/components/show-query-history-button.jsx
@@ -2,19 +2,23 @@ const React = require('react');
 const Actions = require('../actions');
 const FontAwesome = require('react-fontawesome');
 
-
 class ShowQueryHistoryButton extends React.Component {
   constructor(props) {
     super(props);
-    this.unCollapse = this.unCollapse.bind(this);
+    this.handleCollapse = this.handleCollapse.bind(this);
+    this.state = {
+      collapsed: false
+    };
   }
 
-  unCollapse() {
-    Actions.unCollapse();
-  }
-
-  collapse() {
-    Actions.collapse();
+  handleCollapse() {
+    if (!this.state.collapsed) {
+      Actions.unCollapse();
+      this.setState({ collapsed: !this.state.collapsed });
+    } else {
+      Actions.collapse();
+      this.setState({ collapsed: !this.state.collapsed });
+    }
   }
 
   /**
@@ -30,7 +34,7 @@ class ShowQueryHistoryButton extends React.Component {
         className="btn btn-default btn-sm query-history-button query-history-button-expand"
         data-test-id="query-history-button"
         type="button"
-        onClick={this.unCollapse}
+        onClick={this.handleCollapse}
       >
         <FontAwesome name="history" className="query-history-button-icon"/>
       </button>
