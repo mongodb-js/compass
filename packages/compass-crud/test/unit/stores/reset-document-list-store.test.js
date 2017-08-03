7fc8a4a8563b49f4db389bcb20da226a906b0aa9
Jessica Lord <jessica.lord@mongodb.com>
2017-08-03 15:20:29 -0400

    Add reset document list store tests

---
+++ packages/compass-crud/test/unit/stores/reset-document-list-store.test.js
@@ -0,0 +1,71 @@
+const { expect } = require('chai');
+const Connection = require('mongodb-connection-model');
+const DataService = require('mongodb-data-service');
+const ResetDocumentListStore = require('../../../lib/stores/reset-document-list-store');
+
+const CONNECTION = new Connection({
+  hostname: '127.0.0.1',
+  port: 27018,
+  ns: 'compass-crud',
+  mongodb_database_name: 'admin'
+});
+
+describe('ResetDocumentListStore', () => {
+  const dataService = new DataService(CONNECTION);
+
+  before((done) => {
+    global.hadronApp.dataService = dataService;
+    dataService.connect(() => {
+      done();
+    });
+  });
+
+  describe('#init', () => {
+    it('sets the default filter', () => {
+      expect(ResetDocumentListStore.filter).to.deep.equal({});
+    });
+
+    it('sets the default sort', () => {
+      expect(ResetDocumentListStore.sort).to.deep.equal([[ '_id', 1 ]]);
+    });
+
+    it('sets the default limit', () => {
+      expect(ResetDocumentListStore.limit).to.equal(0);
+    });
+
+    it('sets the default skip', () => {
+      expect(ResetDocumentListStore.skip).to.equal(0);
+    });
+
+    it('sets the default project', () => {
+      expect(ResetDocumentListStore.project).to.equal(null);
+    });
+
+    it('sets the default namespace', () => {
+      expect(ResetDocumentListStore.ns).to.equal('');
+    });
+  });
+
+  describe('#onQueryChanged', () => {
+    const query = {
+      ns: 'compass-crud.test',
+      filter: { name: 'test' },
+      sort: { name: 1 },
+      limit: 10,
+      skip: 5,
+      project: { name: 1 }
+    };
+
+    it('tiggers with the reset documents', (done) => {
+      const unsubscribe = ResetDocumentListStore.listen((error, docs, count) => {
+        expect(error).to.equal(null);
+        expect(docs).to.deep.equal([]);
+        expect(count).to.equal(0);
+        unsubscribe();
+        done();
+      });
+
+      ResetDocumentListStore.onQueryChanged(query);
+    });
+  });
+});
