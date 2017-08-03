b6d49fbd8af73a081a938f803581099f2f241a58
Durran Jordan <durran@gmail.com>
2017-08-03 09:18:57 -0400

    Adding actions

---
+++ packages/compass-crud/src/actions/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const Reflux = require('reflux');
+const debug = require('debug')('mongodb-compass:crud:actions');
+
+const Actions = Reflux.createActions([
+  'documentRemoved',
+  'openInsertDocumentDialog',
+  'closeInsertDocumentDialog',
+  'insertDocument',
+  'fileDropped',
+  'refreshDocuments',
+  'closeAllMenus',
+  'fetchNextDocuments',
+  'elementInvalid',
+  'elementValid'
+]);
+
+document.ondragover = document.ondrop = (ev) => {
+  ev.preventDefault();
+};
+
+document.body.ondrop = (ev) => {
+  ev.preventDefault();
+  const file = ev.dataTransfer.files[0];
+  if (file) {
+    const path = file.path;
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error) {
+        debug(`Error opening file '${path}': ${error.message}`);
+      }
+      try {
+        Actions.openInsertDocumentDialog(JSON.parse(data), false);
+      } catch (e) {
+        debug(`File ${path} is not a single parseable JSON document: ${e.message}`);
+      }
+    });
+  }
+};
+
+module.exports = Actions;
