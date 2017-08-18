62c151920301518c990fce40cec74c9ce32478bb
aherlihy <anna.herlihy@10gen.com>
2017-08-18 13:31:19 +0200

    Sample table

--- packages/compass-crud/src/components/document-list-table-view.jsx
+++ packages/compass-crud/src/components/document-list-table-view.jsx
@@ -3,6 +3,7 @@ const PropTypes = require('prop-types');
 const BreadcrumbComponent = require('./breadcrumb');
 const BreadcrumbStore = require('../stores/breadcrumb-store');
 const { StoreConnector } = require('hadron-react-components');
+const {AgGridReact} = require('ag-grid-react');
 
 /**
  * Represents the table view of the documents tab.
@@ -14,17 +15,63 @@ class DocumentListTableView extends React.Component {
     this.NamespaceStore = appRegistry.getStore('App.NamespaceStore');
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columnDefs: this.createColumnDefs(),
+      rowData: this.createRowData()
+    };
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.columnApi = params.columnApi;
+
+    this.gridApi.sizeColumnsToFit();
+  }
+
+  createColumnDefs() {
+    return [
+      {headerName: 'Make', field: 'make'},
+      {headerName: 'Model', field: 'model'},
+      {headerName: 'Price', field: 'price'}
+    ];
+  }
+
+  createRowData() {
+    return [
+      {make: 'Toyota', model: 'Celica', price: 35000},
+      {make: 'Ford', model: 'Mondeo', price: 32000},
+      {make: 'Porsche', model: 'Boxter', price: 72000}
+    ];
+  }
+
   /**
    * Render the document table view.
    *
    * @returns {React.Component} The component.
    */
   render() {
+    const containerStyle = {
+      height: 115,
+      width: 500
+    };
+
     return (
       <div>
         <StoreConnector store={BreadcrumbStore}>
           <BreadcrumbComponent/>
         </StoreConnector>
+        <div style={containerStyle} className="ag-fresh">
+          <AgGridReact
+            // properties
+            columnDefs={this.state.columnDefs}
+            rowData={this.state.rowData}
+
+            // events
+            onGridReady={this.onGridReady}/>
+        </div>
       </div>);
   }
 }
