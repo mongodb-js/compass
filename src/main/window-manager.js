4fbfa3198f3db3a606b3dcf5bfde3f4e85c8e53d
Peter Schmidt <peter@peterjs.com>
2017-08-30 16:44:20 +1000

    COMPASS 1743 :bug: Open all links with the system browser
    Includes intercom links.
    Also means we can drop the shell.openExternal overrides, used in many <InfoSprinkle> cases, at a later date.

--- src/main/window-manager.js
+++ src/main/window-manager.js
@@ -181,6 +181,14 @@ var createWindow = module.exports.create = function(opts) {
       detach: true
     });
   }
+
+  /**
+   * Open all external links in the system's web browser.
+   */
+  _window.webContents.on('new-window', function(e, url) {
+    e.preventDefault();
+    electron.shell.openExternal(url);
+  });
   return _window;
 };
 
