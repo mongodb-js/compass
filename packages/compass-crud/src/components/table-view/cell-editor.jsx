973b4144f7be29069252db58940235f93d7313e0
aherlihy <anna.herlihy@10gen.com>
2017-08-31 15:14:09 +0200

    styles for individual cells

--- packages/compass-crud/src/components/table-view/cell-editor.jsx
+++ packages/compass-crud/src/components/table-view/cell-editor.jsx
@@ -6,7 +6,17 @@ const ReactDOM = require('react-dom');
 const initEditors = require('../editor/');
 const Types = require('../types');
 const FontAwesome = require('react-fontawesome');
+const { Tooltip } = require('hadron-react-components');
 
+/**
+ * The document value class.
+ */
+const VALUE_CLASS = 'editable-element-value';
+
+/**
+ * Invalid type class.
+ */
+const INVALID = `${VALUE_CLASS}-is-invalid-type`;
 
 /**
  * The custom cell editor for the table view.
@@ -15,11 +25,6 @@ class CellEditor extends React.Component {
   constructor(props) {
     super(props);
     this.element = props.value;
-
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleAddField = this.handleAddField.bind(this);
-    this.handleRemoveField = this.handleRemoveField.bind(this);
 
     this._editors = initEditors(props.value);
   }
@@ -38,22 +43,6 @@ class CellEditor extends React.Component {
   }
 
   /**
-   * This is only required if you are preventing event propagation.
-   * @param {Object} event
-   */
-  onKeyDown(event) {
-  }
-
-  getValue() {
-    this.editor().complete();
-    return this.editor().value();
-  }
-
-  isPopup() {
-    return true;
-  }
-
-  /**
    * Get the editor for the current type.
    *
    * @returns {Editor} The editor.
@@ -62,18 +51,39 @@ class CellEditor extends React.Component {
     return this._editors[this.element.currentType] || this._editors.Standard;
   }
 
+  /**
+   * This is only required if you are preventing event propagation.
+   * @param {Object} event
+   */
+  handleKeyDown(event) {
+  }
+
+  /**
+   * Edit the field value when using copy/paste.
+   *
+   * @param {String} value - The value to paste in.
+   */
+  _pasteEdit(value) {
+    try {
+      this.editor().paste(value);
+    } catch (e) {
+      this.editor().edit(value);
+    } finally {
+      this._pasting = false;
+    }
+  }
+
+  handlePaste() {
+    this._pasting = true;
+  }
+
   handleChange(event) {
-    // TODO: change event is triggered when enter is pressed/editing is done
-    this.editor().edit(event.target.value);
+    if (this._pasting) {
+      this._pasteEdit(value);
+    } else {
+      this.editor().edit(event.target.value);
+    }
     this.forceUpdate();
-  }
-
-  handleAddField(event) {
-    console.log("add field");
-  }
-
-  handleRemoveField(event) {
-    console.log("remove field");
   }
 
   focus() {
@@ -88,6 +98,23 @@ class CellEditor extends React.Component {
     });
   }
 
+  handleAddField(event) {
+    console.log("add field");
+  }
+
+  handleRemoveField(event) {
+    console.log("remove field");
+  }
+
+  getValue() {
+    this.editor().complete();
+    return this.editor().value();
+  }
+
+  isPopup() {
+    return true;
+  }
+
   /**
    * Render the types column.
    *
@@ -99,14 +126,57 @@ class CellEditor extends React.Component {
     );
   }
 
+  /**
+   * Get the style for the value of the element.
+   *
+   * @returns {String} The value style.
+   */
+  style() {
+    let typeClass = `${VALUE_CLASS}-is-${this.element.currentType.toLowerCase()}`;
+    if (!this.element.isCurrentTypeValid()) {
+      typeClass = `${typeClass} ${INVALID}`;
+    }
+    return `${VALUE_CLASS} ${VALUE_CLASS}-is-editing ${typeClass}`;
+  }
+
+  /**
+   * Get the style for the input wrapper.
+   *
+   * @returns {String} The class name.
+   */
+  wrapperStyle() {
+    return `${VALUE_CLASS}-wrapper ${VALUE_CLASS}-wrapper-is-${this.element.currentType.toLowerCase()}`;
+  }
+
+  renderInput() {
+    const length = 120; // TODO: styles
+    return (
+      <span className={this.wrapperStyle()}>
+        <Tooltip
+          id={this.element.uuid}
+          className="editable-element-value-tooltip"
+          border
+          getContent={() => { return this.element.invalidTypeMessage; }}/>
+        <input
+          data-tip=""
+          data-for={this.element.uuid}
+          ref={(c) => this._node = c}
+          type="text"
+          style={{ width: `${length}px` }}
+          className={this.style()}
+          onChange={this.handleChange.bind(this)}
+          onKeyDown={this.handleKeyDown.bind(this)}
+          onPaste={this.handlePaste.bind(this)}
+          value={this.editor().value(false)} />
+      </span>
+     );
+
+  }
+
   render() {
     return (
       <div className="table-view-cell-editor">
-        <input className="table-view-cell-editor-input"
-               ref="input"
-               value={this.editor().value(true)}
-               onChange={this.handleChange}
-        />
+        {this.renderInput()}
         {this.renderTypes()}
         <button
           className="table-view-cell-editor-button"
--- packages/compass-crud/src/components/table-view/cell-renderer.jsx
+++ packages/compass-crud/src/components/table-view/cell-renderer.jsx
@@ -2,10 +2,50 @@ const React = require('react');
 const PropTypes = require('prop-types');
 // const _ = require('lodash');
 const util = require('util');
-const EditableValue = require('../editable-value');
 const getComponent = require('hadron-react-bson');
 const { Element } = require('hadron-document');
 const initEditors = require('../editor/');
+const { Tooltip } = require('hadron-react-components');
+
+/**
+ * The BEM base style name for the element.
+ */
+const BEM_BASE = 'editable-element';
+/**
+ * The document value class.
+ */
+const VALUE_CLASS = 'editable-element-value';
+
+/**
+ * Invalid type class.
+ */
+const INVALID = `${VALUE_CLASS}-is-invalid-type`;
+
+/**
+ * The added constant.
+ */
+const ADDED = 'is-added';
+
+/**
+ * The edited constant.
+ */
+const EDITED = 'is-edited';
+
+/**
+ * The editing constant.
+ */
+const EDITING = 'is-editing';
+
+/**
+ * The field limit.
+ */
+const FIELD_LIMIT = 30;
+
+/**
+ * The removed constant.
+ */
+const REMOVED = 'is-removed';
+
 
 /**
  * The custom cell renderer that renders a cell in the table view.
@@ -70,18 +110,53 @@ class CellRenderer extends React.Component {
     this.props.node.data.state = 'modified';
   }
 
-  renderReadOnly() {
-    const component = getComponent(this.props.value.currentType);
-    return React.createElement(
-      component,
-      { type: this.props.value.currentType, value: this.props.value.currentValue }
-    );
+  /**
+   * Get the style for the element component.
+   *
+   * @param {String} base - The base style.
+   *
+   * @returns {String} The element style.
+   */
+  style(base = BEM_BASE) {
+    let style = base;
+    if (this.element.isAdded()) {
+      style = style.concat(` ${base}-${ADDED}`);
+    } else if (this.element.isEdited()) {
+      style = style.concat(` ${base}-${EDITED}`);
+    } else if (this.element.isRemoved()) {
+      style = style.concat(` ${base}-${REMOVED}`);
+    }
+    return style;
+  }
+
+  renderInvalidCell() {
+    let valueClass = `${VALUE_CLASS}-is-${this.element.currentType.toLowerCase()}`;
+    valueClass = `${valueClass} ${INVALID}`;
+    return (
+      <div className="table-cell">
+        <div className={valueClass}>
+          {this.element.currentValue}
+        </div>
+      </div>
+    )
   }
 
   render() {
+    if (!this.element.isCurrentTypeValid()) {
+      return this.renderInvalidCell();
+    }
+
+    const component = getComponent(this.element.currentType);
+    const element = React.createElement(
+      component,
+      { type: this.props.value.currentType, value: this.element.currentValue }
+    );
+
     return (
       <div className="table-cell">
-        {this.renderReadOnly()}
+        <div className={this.style()}>
+          {element}
+        </div>
       </div>
     );
   }
