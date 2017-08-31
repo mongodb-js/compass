6f7e0d110263e120e6cf227013e8f8c0afa2a898
Peter Schmidt <peter@peterjs.com>
2017-08-31 13:43:25 +1000

    :white_check_mark: Add some basic unit tests

--- test/unit/namespace-store.test.js
+++ test/unit/namespace-store.test.js
@@ -1,8 +1,28 @@
+const app = require('hadron-app');
+const AppRegistry = require('hadron-app-registry');
 const { expect } = require('chai');
+const Reflux = require('reflux');
 const NamespaceStore = require('../../src/internal-packages/app/lib/stores/namespace-store');
 
-
+/**
+ * Useful background information on namespaces:
+ * https://docs.mongodb.com/manual/reference/limits/#naming-restrictions
+ */
 describe('NamespaceStore', () => {
+  const initialDatabase = 'foo';
+  const initialCollection = 'bar.baz';
+  let appRegistry;
+
+  beforeEach(() => {
+    NamespaceStore.ns = `${initialDatabase}.${initialCollection}`;
+    appRegistry = app.appRegistry;
+    app.appRegistry = new AppRegistry();
+  });
+
+  afterEach(() => {
+    app.appRegistry = appRegistry;
+  });
+
   describe('#set ns', () => {
     it('triggers a store event', (done) => {
       const unsubscribe = NamespaceStore.listen((ns) => {
@@ -11,6 +31,30 @@ describe('NamespaceStore', () => {
         done();
       });
       NamespaceStore.ns = 'database.collection';
+    });
+
+    it('when collection changes, calls onCollectionChanged', (done) => {
+      const newNamespace = `${initialDatabase}.change.the-collection.please`;
+      const CollectionSubscriberStore = Reflux.createStore({
+        onCollectionChanged(namespace) {
+          expect(namespace).to.be.equal(newNamespace);
+          done();
+        }
+      });
+      app.appRegistry.registerStore('CollectionSubscriber.Store', CollectionSubscriberStore);
+      NamespaceStore.ns = newNamespace;
+    });
+
+    it('when database changes, calls onDatabaseChanged', (done) => {
+      const newNamespace = `changeTheDB.${initialCollection}`;
+      const DatabaseSubscriberStore = Reflux.createStore({
+        onDatabaseChanged(namespace) {
+          expect(namespace).to.be.equal(newNamespace);
+          done();
+        }
+      });
+      app.appRegistry.registerStore('DatabaseSubscriber.Store', DatabaseSubscriberStore);
+      NamespaceStore.ns = newNamespace;
     });
   });
 });
