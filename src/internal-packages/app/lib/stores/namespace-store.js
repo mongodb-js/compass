801c564d9639e39a9f1aa21535ac9d4077e5b382
Peter Schmidt <peter@peterjs.com>
2017-09-04 11:37:40 +1000

    [Backport 1.8] COMPASS 1747 Fire onCollectionChanged in more cases (#1227) (#1231)
    * :hammer: Refactor NamespaceStore test
    
    * :white_check_mark: Add some basic unit tests
    
    * :white_check_mark: Add test to detect the :bug:
    
    * :bug: onCollectionChange should fire on either of DB or Collection changed
    
    Example: db1.coll => db2.coll, the collection component is identical, but it is a different collection because it is on a different database.
    
    * :bug: Reuse mongodb-ns
    
    It handles the '.' in collection names correctly.
    
    * :art: Use contexts for test readability
    
    * :white_check_mark: Add test to detect the second :bug:
    
    * :bug: Initialize to the default namespace of ''
    
    Reproduced on local with:
    npm test -- --functional
    
    https://travis-ci.com/10gen/compass/jobs/88742497

--- src/internal-packages/app/lib/stores/namespace-store.js
+++ src/internal-packages/app/lib/stores/namespace-store.js
@@ -1,29 +1,31 @@
 const Reflux = require('reflux');
 const app = require('hadron-app');
-const _ = require('lodash');
+const toNS = require('mongodb-ns');
 const debug = require('debug')('mongodb-compass:namespace-store');
+
+/**
+ * The default namespace when the Compass user connects to a MongoDB instance.
+ */
+const DEFAULT_NAMESPACE = '';
 
 /**
  * The store holds the source of truth for the namespace being worked on.
  */
 const NamespaceStore = Reflux.createStore({
+
+  /**
+   * Initializing the store should set up the default namespace.
+   */
+  init() {
+    this._ns = DEFAULT_NAMESPACE;
+  },
+
   /**
    * Gets the current namespace being worked with in the application.
    */
   get ns() {
     debug('getting ns:', this._ns);
     return this._ns;
-  },
-
-  __nsHelper: function(ns) {
-    if (!ns) {
-      return ['', ''];
-    }
-    if (_.includes(ns, '.')) {
-      return ns.split('.');
-    }
-
-    return [ns, ''];
   },
 
   /**
@@ -35,17 +37,17 @@ const NamespaceStore = Reflux.createStore({
     debug('setting ns: from', this._ns, 'to', ns);
     const registry = app.appRegistry;
     if (registry) {
-      const oldNns = this.__nsHelper(this._ns);
-      const newNs = this.__nsHelper(ns);
+      const oldNs = toNS(this._ns);
+      const newNs = toNS(ns);
 
-      if (oldNns[0] !== newNs[0]) {
+      if (oldNs.database !== newNs.database) {
         registry.callOnStores(function(store) {
           if (store.onDatabaseChanged) {
             store.onDatabaseChanged(ns);
           }
         });
       }
-      if (oldNns[1] !== newNs[1]) {
+      if (oldNs.database !== newNs.database || oldNs.collection !== newNs.collection) {
         registry.callOnStores(function(store) {
           if (store.onCollectionChanged) {
             store.onCollectionChanged(ns);
