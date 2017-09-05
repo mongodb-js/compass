5ad4efcce21fee959e82e1f329f7b02fe54d2ade
Durran Jordan <durran@gmail.com>
2017-09-05 17:05:30 +0200

    COMPASS-1697: Add Kerberos Plugin in dev

--- packages/compass-connect/electron/renderer/index.js
+++ packages/compass-connect/electron/renderer/index.js
@@ -4,6 +4,7 @@ const app = require('hadron-app');
 const React = require('react');
 const ReactDOM = require('react-dom');
 const AppRegistry = require('hadron-app-registry');
+const kerberos = require('@mongodb-js/compass-auth-kerberos');
 
 const entryPoint = require('../../');
 const appRegistry = new AppRegistry();
@@ -12,6 +13,9 @@ const ConnectComponent = require('../../lib/components');
 global.hadronApp = app;
 global.hadronApp.appRegistry = appRegistry;
 entryPoint.activate(appRegistry);
+kerberos.activate(appRegistry);
+
+appRegistry.onActivated();
 
 ReactDOM.render(
   React.createElement(ConnectComponent),
