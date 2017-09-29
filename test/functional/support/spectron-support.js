c72fa92a4cf073cc41f0ac899c8b51daec665534
Peter Schmidt <peter@peterjs.com>
2017-09-29 12:15:30 +1000

    :bug: Handle app null as well as undefined
    https://travis-ci.com/10gen/compass/jobs/92556220

--- test/functional/support/spectron-support.js
+++ test/functional/support/spectron-support.js
@@ -69,7 +69,7 @@ function launchCompass() {
  * @returns {Promise}    Promise that resolves when app stops or is undefined.
  */
 function quitCompass(app) {
-  if (app === undefined) {
+  if (app === undefined || app === null) {
     return Promise.resolve();
   }
   return app.quit();
