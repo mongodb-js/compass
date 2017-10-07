8a78862885c7d2b56dbb8ed81dda6e733d667c52
Durran Jordan <durran@gmail.com>
2017-10-07 12:56:20 +0200

    Revert "Make hide action sync"
    This reverts commit 3934d62cc57ff137c4663825a3a403dd9223a0c7.

--- packages/compass-status/src/actions/actions.js
+++ packages/compass-status/src/actions/actions.js
@@ -1,89 +1,89 @@
 import Reflux from 'reflux';
 
-const StatusActions = Reflux.createActions({
+const StatusActions = Reflux.createActions([
   /**
    * shows the progress bar.
    */
-  showProgressBar: { sync: false },
+  'showProgressBar',
   /**
    * shows an indeterminate progress bar at 100 percent.
    */
-  showIndeterminateProgressBar: { sync: false },
+  'showIndeterminateProgressBar',
   /**
    * hides the progress bar.
    */
-  hideProgressBar: { sync: false },
+  'hideProgressBar',
   /**
    * sets the value of the progres bar.
    *
    * @param {Number} value   the value, must be between 0 and 100.
    */
-  setProgressValue: { sync: false },
+  'setProgressValue',
   /**
    * increases the value of the progres bar.
    *
    * @param {Number} value   increase by value.
    */
-  incProgressValue: { sync: false },
+  'incProgressValue',
   /**
    * enable trickle: progress bar randomly advances a few percentage points
    * every second to indicate progress.
    */
-  enableProgressTrickle: { sync: false },
+  'enableProgressTrickle',
   /**
    * disable trickle.
    */
-  disableProgressTrickle: { sync: false },
+  'disableProgressTrickle',
   /**
    * sets a message that is shown on the screen above the loading animation.
    *
    * @param {String} message    the message to show
    */
-  setMessage: { sync: false },
+  'setMessage',
   /**
    * clears and removes the message.
    */
-  clearMessage: { sync: false },
+  'clearMessage',
   /**
    * shows loading animation in the center of the screen.
    */
-  showAnimation: { sync: false },
+  'showAnimation',
   /**
    * hides loading animation.
    */
-  hideAnimation: { sync: false },
+  'hideAnimation',
   /**
    * shows a static gray sidebar in the background. This is useful when
    * no other content is on the screen yet (e.g. when connecting to a mongod)
    * so that the message/loading animation look centered.
    */
-  showStaticSidebar: { sync: false },
+  'showStaticSidebar',
   /**
    * hide static gray sidebar.
    */
-  hideStaticSidebar: { sync: false },
+  'hideStaticSidebar',
   /**
    * set a custom subview that is shown below the loading animation. For example,
    * the schema view sets a subview to indicate longer than usual parsing.
    *
    * @param {View} subview    the subview to render.
    */
-  setSubview: { sync: false },
+  'setSubview',
   /**
    * clears the custom subview.
    */
-  clearSubview: { sync: false },
+  'clearSubview',
   /**
    * when enabled, overlays the screen with a transparent div, so that no other
    * interaction can take place.
    */
-  enableModal: { sync: false },
+  'enableModal',
   /**
    * TODO: This modal flag does not currently work, it needs to change the style
    * into say a popup box so it's clear what the user can/cannot click on.
    * disables the modal transparent div.
    */
-  disableModal: { sync: false },
+  'disableModal',
   /**
    * custom configuration to set all the options above in a single call.
    *
@@ -98,18 +98,18 @@ const StatusActions = Reflux.createActions({
    * @param {View}    options.subview       subview to show, or `null`
    * @param {Boolean} options.sidebar       show/hide static sidebar
    */
-  configure: { sync: false },
+  'configure',
   /**
    * hide all status components (progress bar, message, animation, sidebar).
    * Use when loading was interrupted.
    */
-  hide: { sync: true },
+  'hide',
   /**
    * like `hide()` but animates the progress bar to 100% before hiding, so that
    * the user gets feedback of success. Use when loading is complete.
    */
-  done: { sync: false }
-});
+  'done'
+]);
 
 export default StatusActions;
 export { StatusActions };
