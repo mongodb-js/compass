953c0b7aebdcbd678a67a28a421f07df07e906fb
Durran Jordan <durran@gmail.com>
2017-10-10 18:25:38 -0400

    Flushing out enzyme tests

--- packages/compass-license/src/components/license/license.spec.js
+++ packages/compass-license/src/components/license/license.spec.js
@@ -16,12 +16,28 @@ describe('License [Component]', () => {
       component = null;
     });
 
-    it('renders the correct root classname', () => {
-      expect(component.find(`.${styles.modal}`)).to.be.present();
+    it('renders the visible modal', () => {
+      expect(component.find(`.${styles['modal-is-visible']}`)).to.be.present();
     });
   });
 
   context('when the license is not visible', () => {
+    let component;
 
+    beforeEach(() => {
+      component = mount(<License isVisible={false} />);
+    });
+
+    afterEach(() => {
+      component = null;
+    });
+
+    it('renders the modal as hidden', () => {
+      expect(component.find(`.${styles['modal-is-visible']}`)).to.not.be.present();
+    });
+
+    it('renders the default modal class', () => {
+      expect(component.find(`.${styles.modal}`)).to.be.present();
+    });
   });
 });
