cfa82091a0ee19bfcbcb754464f33fe242c0dfa1
Durran Jordan <durran@gmail.com>
2017-10-12 15:24:17 -0400

    Filter out community features

--- src/app/tour/features.js
+++ src/app/tour/features.js
@@ -4,55 +4,63 @@ module.exports = [
     description: 'Real-time server statistics let you view key server metrics and database operations. Drill down into database operations easily and understand your most active collections.',
     image: 'performance.png',
     version: '1.5.0',
-    initial: true
+    initial: true,
+    community: false
   },
   {
     title: 'Sidebar. Redesigned.',
     description: 'See with one glance what server you are connected to. Navigate between instance, database and collection level, with powerful filtering of your namespaces.',
     image: 'sidebar.png',
     version: '1.5.0',
-    initial: true
+    initial: true,
+    community: true
   },
   {
     title: 'Visualize your Schema.',
     description: 'MongoDB Compass analyzes your documents and displays rich structures within your collections through an intuitive GUI. It allows you to quickly visualize and explore your schema to understand the frequency, types and ranges of fields in your data set.',
     image: 'schema.png',
     version: '1.5.0',
-    initial: true
+    initial: true,
+    community: false
   },
   {
     title: 'Build Geo Queries.',
     description: 'Visualize, understand, and work with your geospatial data. Point and click to construct sophisticated queries, execute them with the push of a button and Compass will display your results both graphically and as sets of JSON documents.',
     image: 'geo.png',
     version: '1.5.0',
-    initial: true
+    initial: true,
+    community: false
   },
   {
     title: 'Interactive Document Editor.',
     description: 'Modify existing documents with greater confidence using the intuitive visual editor, or insert new documents and clone or delete existing ones in just a few clicks.',
     image: 'crud.png',
     version: '1.5.0',
-    initial: true
+    initial: true,
+    community: true
   },
   {
     title: 'Visual Explain Plans.',
     description: 'Know how queries are running through an easy-to-understand GUI that helps you identify and resolve performance issues.',
     image: 'explain.png',
     version: '1.5.0',
-    initial: true
+    initial: true,
+    community: true
   },
   {
     title: 'Index Management.',
     description: 'Understand the type and size of your indexes, their utilization and special properties. Add and remove indexes at the click of a button.',
     image: 'indexes.png',
     version: '1.5.0',
-    initial: true
+    initial: true,
+    community: true
   },
   {
     title: 'Document Validation.',
     description: 'Create and modify rules that validate your data using a simple point and click interface. CRUD support lets you fix data quality issues easily in individual documents.',
     image: 'validation.png',
     version: '1.5.0',
-    initial: true
+    initial: true,
+    community: false
   }
 ];
