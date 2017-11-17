044320a07af578055f54214f099932a44aff8ca1
Felix Rieseberg <felix@felixrieseberg.com>
2017-11-16 19:05:09 -0800

    wrench: Teach walker about paths and folders

---
+++ packages/electron-wix-msi/__tests__/utils/array-to-tree-spec.ts
@@ -0,0 +1,127 @@
+import { addFilesToTree, arrayToTree, isChild, isDirectChild, shortestString } from '../../lib/utils/array-to-tree';
+import { defaultsDeep, cloneDeep } from 'lodash';
+
+const mockFolders = [
+  'slack\\resources',
+  'slack\\resources\\app.asar.unpacked',
+  'slack\\resources\\app.asar.unpacked\\node_modules',
+  'slack\\resources\\app.asar.unpacked\\src',
+  'slack\\locales'
+];
+
+const mockFiles = [
+  'slack\\slack.exe',
+  'slack\\resources\\text.txt',
+  'slack\\resources\\app.asar.unpacked\\image.png',
+  'slack\\resources\\app.asar.unpacked\\node_modules\\package.json',
+  'slack\\resources\\app.asar.unpacked\\src\\package.json',
+  'slack\\locales\\de-DE.json',
+  'slack\\locales\\en-US.json',
+];
+
+const mockFolderTree = {
+  __ELECTRON_WIX_MSI_PATH__: 'slack',
+  __ELECTRON_WIX_MSI_FILES__: [],
+  resources: {
+    __ELECTRON_WIX_MSI_PATH__: 'slack\\resources',
+    __ELECTRON_WIX_MSI_FILES__: [],
+    'app.asar.unpacked': {
+      __ELECTRON_WIX_MSI_PATH__: 'slack\\resources\\app.asar.unpacked',
+      __ELECTRON_WIX_MSI_FILES__: [],
+      node_modules: {
+        __ELECTRON_WIX_MSI_PATH__: 'slack\\resources\\app.asar.unpacked\\node_modules',
+        __ELECTRON_WIX_MSI_FILES__: []
+      },
+      src: {
+        __ELECTRON_WIX_MSI_PATH__: 'slack\\resources\\app.asar.unpacked\\src',
+        __ELECTRON_WIX_MSI_FILES__: []
+      }
+    }
+  },
+  locales: {
+    __ELECTRON_WIX_MSI_PATH__: 'slack\\locales',
+    __ELECTRON_WIX_MSI_FILES__: []
+  }
+};
+
+const mockFolderFileTree = defaultsDeep(cloneDeep(mockFolderTree), {
+  __ELECTRON_WIX_MSI_FILES__: [{ name: 'slack.exe', path: 'slack\\slack.exe' }],
+  resources: {
+    __ELECTRON_WIX_MSI_FILES__: [{ name: 'text.txt', path: 'slack\\resources\\text.txt' }],
+    'app.asar.unpacked': {
+      __ELECTRON_WIX_MSI_FILES__: [
+        { name: 'image.png', path: 'slack\\resources\\app.asar.unpacked\\image.png' },
+      ],
+      node_modules: {
+        __ELECTRON_WIX_MSI_FILES__: [
+          { name: 'package.json', path: 'slack\\resources\\app.asar.unpacked\\node_modules\\package.json' }
+        ]
+      },
+      src: {
+        __ELECTRON_WIX_MSI_FILES__: [
+          { name: 'package.json', path: 'slack\\resources\\app.asar.unpacked\\src\\package.json' }
+        ]
+      }
+    }
+  },
+  locales: {
+    __ELECTRON_WIX_MSI_FILES__: [
+      { name: 'de-DE.json', path: 'slack\\locales\\de-DE.json' },
+      { name: 'en-US.json', path: 'slack\\locales\\en-US.json' }
+    ]
+  }
+});
+
+test('isChild returns true for a child and parent', () => {
+  const a = 'C:\\my\\path';
+  const b = 'C:\\my\\path\\child';
+
+  expect(isChild(a, b)).toBe(true);
+});
+
+test('isChild returns false for a child and non-parent', () => {
+  const a = 'C:\\my\\path';
+  const b = 'C:\\my\\other\\path\\child';
+
+  expect(isChild(a, b)).toBe(false);
+});
+
+test('isDirectChild returns true for a child and direct parent', () => {
+  const a = 'C:\\my\\path';
+  const b = 'C:\\my\\path\\child';
+
+  expect(isDirectChild(a, b)).toBe(true);
+});
+
+test('isDirectChild returns false for a child and non-direct parent', () => {
+  const a = 'C:\\my\\path';
+  const b = 'C:\\my\\path\\child\\ren';
+
+  expect(isDirectChild(a, b)).toBe(false);
+});
+
+test('isDirectChild returns false for a child and non-parent', () => {
+  const a = 'C:\\my\\path';
+  const b = 'C:\\my\\other\\path\\child';
+
+  expect(isDirectChild(a, b)).toBe(false);
+});
+
+test('shortestString returns the shortest string in an array', () => {
+  const input = [
+    'aaaaaa',
+    'aa',
+    'aaaaaaaaaa',
+    'aaaaaa'
+  ]
+
+  expect(shortestString(input)).toBe('aa');
+});
+
+test('arrayToTree creates a tree structure', () => {
+  expect(arrayToTree(mockFolders, 'slack')).toEqual(mockFolderTree);
+});
+
+test('addFilesToTree adds files to a tree structure', () => {
+  expect(addFilesToTree(mockFolderTree, mockFiles, 'slack')).toEqual(mockFolderFileTree);
+})
--- packages/electron-wix-msi/src/utils/array-to-tree.ts
+++ packages/electron-wix-msi/src/utils/array-to-tree.ts
@@ -1,5 +1,6 @@
-import { StringMap } from '../interfaces';
+import { File, FileFolderTree, StringMap } from '../interfaces';
 import * as path from 'path';
+import { cloneDeep } from 'lodash';
 
 /**
  * Are two paths in a direct parent/child relationship?
@@ -76,23 +77,26 @@ export function shortestString(input: Array<string>): string {
  *   ];
  *
  *  output = {
+ *    __ELECTRON_WIX_MSI_FILES__: [],
  *    resources: {
+ *      __ELECTRON_WIX_MSI_FILES__: [],
  *      'app.asar.unpacked': {
- *        node_modules: {},
- *        src: {}
+ *        __ELECTRON_WIX_MSI_FILES__: [],
+ *        node_modules: { __ELECTRON_WIX_MSI_FILES__: [] },
+ *        src: { __ELECTRON_WIX_MSI_FILES__: [] }
  *      }
  *    },
- *    locales: {}
+ *    locales: { __ELECTRON_WIX_MSI_FILES__: [] }
  *  }
  *
  * @export
  * @param {Array<string>} input
  * @param {string} [inputRoot]
- * @returns
+ * @returns {FileFolderTree}
  */
-export function arrayToTree(input: Array<string>, inputRoot?: string) {
+export function arrayToTree(input: Array<string>, inputRoot?: string): FileFolderTree {
   const root = inputRoot || shortestString(input);
-  const output: StringMap<any> = {};
+  const output: FileFolderTree = { __ELECTRON_WIX_MSI_FILES__: [], __ELECTRON_WIX_MSI_PATH__: root };
 
   const children: Array<string> = input.filter((e) => isChild(root, e));
   const directChildren: Array<string> = children.filter((e) => isDirectChild(root, e));
@@ -100,6 +104,33 @@ export function arrayToTree(input: Array<string>, inputRoot?: string) {
   directChildren.forEach((directChild) => {
     output[path.basename(directChild)] = arrayToTree(children, directChild)
   });
+
+  return output;
+}
+
+export function addFilesToTree(tree: FileFolderTree, files: Array<string>, root: string): FileFolderTree {
+  const output: FileFolderTree = cloneDeep(tree);
+
+  files.forEach((filepath) => {
+    const file: File = { name: path.basename(filepath), path: filepath };
+    const walkingSteps = filepath.split('\\');
+    let target: FileFolderTree = output;
+
+    if (walkingSteps[0] === root) {
+      walkingSteps.splice(0, 1);
+    }
+
+    walkingSteps.forEach((step, i) => {
+      if (target[step] && i < walkingSteps.length - 1) {
+        target = target[step] as FileFolderTree;
+        return;
+      }
+
+      if (i === walkingSteps.length - 1) {
+        target.__ELECTRON_WIX_MSI_FILES__.push(file);
+      }
+    });
+  })
 
   return output;
 }
