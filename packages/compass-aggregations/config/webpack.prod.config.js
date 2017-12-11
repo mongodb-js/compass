553a46fe6a762e4a4d1d7d91e9fec7397668c346
Durran Jordan <durran@gmail.com>
2017-12-11 19:12:09 +0100

    Base64 embed the fonts

--- packages/compass-aggregations/config/webpack.prod.config.js
+++ packages/compass-aggregations/config/webpack.prod.config.js
@@ -135,35 +135,25 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
-        use: [{
-          loader: 'file-loader',
-          // In prod we need to go to $COMPASS_HOME/node_modules/<plugin>/lib or
-          // $USER_HOME/.mongodb/compasss(-community)/plugins
-          //
-          // @note This currently does not work in published plugin.
-          query: {
-            name: 'assets/images/[name]__[hash:base64:5].[ext]',
-            publicPath: function(file) {
-              return path.join(__dirname, '..', 'lib', file);
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 500000
             }
           }
-        }]
+        ]
       },
       {
         test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-          // In prod we need to go to $COMPASS_HOME/node_modules/<plugin>/lib or
-          // $USER_HOME/.mongodb/compasss(-community)/plugins
-          //
-          // @note This currently does not work in published plugin.
-          query: {
-            name: 'assets/images/[name]__[hash:base64:5].[ext]',
-            publicPath: function(file) {
-              return path.join(__dirname, '..', 'lib', file);
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 500000
             }
           }
-        }]
+        ]
       }
     ]
   },
