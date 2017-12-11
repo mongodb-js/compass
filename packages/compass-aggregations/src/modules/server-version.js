0bf1b0cbcedf51ed1ecf4a7fec78624dca37f52f
Durran Jordan <durran@gmail.com>
2017-12-11 19:00:02 +0100

    Adding server version module

---
+++ packages/compass-aggregations/src/modules/server-version.js
@@ -0,0 +1,39 @@
+/**
+ * Server version changed action.
+ */
+const SERVER_VERSION_CHANGED = 'SERVER_VERSION_CHANGED';
+
+/**
+ * The initial state.
+ */
+const INITIAL_STATE = '3.6.0';
+
+/**
+ * Reducer function for handle state changes to stages.
+ *
+ * @param {String} state - The version state.
+ * @param {Object} action - The action.
+ *
+ * @returns {String} The new state.
+ */
+const reducer = (state = INITIAL_STATE, action) => {
+  if (action.type === SERVER_VERSION_CHANGED) {
+    return action.version;
+  }
+  return state;
+};
+
+/**
+ * Action creator for server version changed events.
+ *
+ * @param {String} version - The version value.
+ *
+ * @returns {Object} The server version changed action.
+ */
+const serverVersionChanged = (version) => ({
+  type: SERVER_VERSION_CHANGED,
+  version: version
+});
+
+export default reducer;
+export { serverVersionChanged, SERVER_VERSION_CHANGED };
