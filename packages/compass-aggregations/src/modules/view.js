609db1cb10e1d321e6d807720047766032d6251e
Durran Jordan <durran@gmail.com>
2017-12-24 22:43:46 +0100

    Adding view module to reducers

---
+++ packages/compass-aggregations/src/modules/view.js
@@ -0,0 +1,46 @@
+/**
+ * View changed action.
+ */
+export const VIEW_CHANGED = 'aggregations/view/VIEW_CHANGED';
+
+/**
+ * The code view.
+ */
+export const CODE = 'Code';
+
+/**
+ * The builder view.
+ */
+export const BUILDER = 'Builder';
+
+/**
+ * The initial state.
+ */
+const INITIAL_STATE = CODE;
+
+/**
+ * Reducer function for handle state changes to the view.
+ *
+ * @param {String} state - The view state.
+ * @param {Object} action - The action.
+ *
+ * @returns {String} The new state.
+ */
+export default function reducer(state = INITIAL_STATE, action) {
+  if (action.type === VIEW_CHANGED) {
+    return action.view;
+  }
+  return state;
+}
+
+/**
+ * Action creator for view changed events.
+ *
+ * @param {String} view - The view value.
+ *
+ * @returns {Object} The view changed action.
+ */
+export const viewChanged = (view) => ({
+  type: VIEW_CHANGED,
+  view: view
+});
