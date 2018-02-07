cceff7e9fd95d3b240b3199d29ba540b1c3cbe5a
Durran Jordan <durran@gmail.com>
2018-02-07 21:40:53 +0100

    Compose stage toolbar with builder and preview toolbars

--- packages/compass-aggregations/src/components/stage-toolbar/stage-toolbar.jsx
+++ packages/compass-aggregations/src/components/stage-toolbar/stage-toolbar.jsx
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 import classnames from 'classnames';
+import StageBuilderToolbar from 'components/stage-builder-toolbar';
+import StagePreviewToolbar from 'components/stage-preview-toolbar';
 
 import styles from './stage-toolbar.less';
 
@@ -17,6 +19,8 @@ class StageToolbar extends PureComponent {
   render() {
     return (
       <div className={classnames(styles['stage-toolbar'])}>
+        <StageBuilderToolbar {...this.props} />
+        <StagePreviewToolbar {...this.props} />
       </div>
     );
   }
--- packages/compass-aggregations/src/components/stage-toolbar/stage-toolbar.spec.js
+++ packages/compass-aggregations/src/components/stage-toolbar/stage-toolbar.spec.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import StageToolbar from 'components/stage-toolbar';
+import StageBuilderToolbar from 'components/stage-builder-toolbar';
+import StagePreviewToolbar from 'components/stage-preview-toolbar';
+
 import styles from './stage-toolbar.less';
 
 describe('StageToolbar [Component]', () => {
@@ -17,5 +20,15 @@ describe('StageToolbar [Component]', () => {
 
   it('renders the wrapper div', () => {
     expect(component.find(`.${styles['stage-toolbar']}`)).to.be.present();
+  });
+
+  it('renders the stage builder toolbar', () => {
+    expect(component.find(`.${styles['stage-toolbar']}`))
+      .to.have.descendants(StageBuilderToolbar);
+  });
+
+  it('renders the stage preview toolbar', () => {
+    expect(component.find(`.${styles['stage-toolbar']}`))
+      .to.have.descendants(StagePreviewToolbar);
   });
 });
