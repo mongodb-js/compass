cb053f18c2c6cd4d0458c19158df53d498a60c74
Durran Jordan <durran@gmail.com>
2018-02-26 20:20:31 +0100

    Add reducer to emit on app registry

--- packages/compass-import-export/src/modules/app-registry.js
+++ packages/compass-import-export/src/modules/app-registry.js
@@ -9,6 +9,11 @@ const PREFIX = 'import-export/app-registry';
 export const APP_REGISTRY_ACTIVATED = `${PREFIX}/APP_REGISTRY_ACTIVATED`;
 
 /**
+ * Action for emitting on the app registry.
+ */
+export const APP_REGISTRY_EMIT = `${PREFIX}/APP_REGISTRY_EMIT`;
+
+/**
  * The initial ns state.
  */
 const INITIAL_STATE = null;
@@ -16,13 +21,27 @@ const INITIAL_STATE = null;
 /**
  * Create an app registry activated action.
  *
- * @param {String} ns - The namespace.
+ * @param {String} appRegistry - The app registry.
  *
  * @returns {Object} The action.
  */
 export const appRegistryActivated = (appRegistry) => ({
   type: APP_REGISTRY_ACTIVATED,
   appRegistry: appRegistry
+});
+
+/**
+ * Create an app registry emit action.
+ *
+ * @param {String} name - The name to emit.
+ * @param {Array} args - The arguments.
+ *
+ * @returns {Object} The action.
+ */
+export const appRegistryEmit = (name, ...args) => ({
+  type: APP_REGISTRY_EMIT,
+  name: name,
+  args: args
 });
 
 /**
@@ -36,6 +55,9 @@ export const appRegistryActivated = (appRegistry) => ({
 const reducer = (state = INITIAL_STATE, action) => {
   if (action.type === APP_REGISTRY_ACTIVATED) {
     return action.appRegistry;
+  } else if (action.type === APP_REGISTRY_EMIT) {
+    state.emit(action.name, ...action.args);
+    return state;
   }
   return state;
 };
--- packages/compass-import-export/src/modules/app-registry.spec.js
+++ packages/compass-import-export/src/modules/app-registry.spec.js
@@ -12,6 +12,17 @@ describe('app-registry [module]', () => {
       });
     });
 
+    context('when the action type is APP_REGISTRY_EMIT', () => {
+      const emitSpy = sinon.spy();
+      const registry = { emit: emitSpy };
+      const action = actions.appRegistryEmit('refresh-documents', spy);
+
+      it('returns the new state and emits the event', () => {
+        expect(reducer(registry, action)).to.equal(registry);
+        expect(emitSpy.calledWith('refresh-documents', spy)).to.equal(true);
+      });
+    });
+
     context('when the action type is not recognised', () => {
       it('returns the initial state', () => {
         expect(reducer(undefined, {})).to.deep.equal(null);
@@ -24,6 +35,16 @@ describe('app-registry [module]', () => {
       expect(actions.appRegistryActivated(spy)).to.deep.equal({
         type: actions.APP_REGISTRY_ACTIVATED,
         appRegistry: spy
+      });
+    });
+  });
+
+  describe('#appRegistryEmit', () => {
+    it('returns the action', () => {
+      expect(actions.appRegistryEmit('refresh-documents', spy)).to.deep.equal({
+        type: actions.APP_REGISTRY_EMIT,
+        name: 'refresh-documents',
+        args: [ spy ]
       });
     });
   });
