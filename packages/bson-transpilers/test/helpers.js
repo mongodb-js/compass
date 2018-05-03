72437fa6929e62e1744d625fc4a42f4127c1e6cb
Irina Shestak <shestak.irina@gmail.com>
2018-05-03 18:10:48 +0200

    bson code methods

--- packages/bson-transpilers/test/helpers.js
+++ packages/bson-transpilers/test/helpers.js
@@ -12,7 +12,7 @@ const unsupported = {
       java: {},
       python: {},
       csharp: {
-        'bson-methods': [ '*' ],
+        'bson-methods': [ 'ObjectId', 'Binary', 'DBRef', 'Int32', 'Double', 'Long', 'Decimal128', 'Timestamp', 'Symbol'],
         'bson-utils': [ '*' ]
       }
     },
