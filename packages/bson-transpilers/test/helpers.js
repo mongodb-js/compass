ac57171e15ed8f4e4ae52b55fad4594c985bd95d
Irina Shestak <shestak.irina@gmail.com>
2018-05-04 15:58:25 +0200

    dbref object methods

--- packages/bson-transpilers/test/helpers.js
+++ packages/bson-transpilers/test/helpers.js
@@ -12,7 +12,7 @@ const unsupported = {
       java: {},
       python: {},
       csharp: {
-        'bson-methods': ['DBRef', 'Int32', 'Double', 'Long', 'Decimal128', 'Timestamp', 'Symbol'],
+        'bson-methods': ['Int32', 'Double', 'Long', 'Decimal128', 'Timestamp', 'Symbol'],
         'bson-utils': [ '*' ]
       }
     },
