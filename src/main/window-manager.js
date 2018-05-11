9a9d2d54a368c177831b63c539190d907525d09a
Durran Jordan <durran@gmail.com>
2018-05-11 13:44:16 +0200

    Hide the window behind the loading screen

--- src/main/window-manager.js
+++ src/main/window-manager.js
@@ -116,7 +116,7 @@ var createWindow = module.exports.create = function(opts) {
     width: opts.width,
     height: opts.height,
     icon: opts.icon,
-    show: true,
+    show: false,
     'min-width': opts.minwidth,
     'web-preferences': {
       'subpixel-font-scaling': true,
