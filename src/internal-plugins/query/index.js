2359768e331462224fc707225ed9db488a46a44b
Durran Jordan <durran@gmail.com>
2018-06-06 21:57:45 +0200

    COMPASS-2924: Adding elipsis menu to query bar

--- src/internal-plugins/query/index.js
+++ src/internal-plugins/query/index.js
@@ -8,20 +8,20 @@ const QueryChangedStore = require('./lib/store/query-changed-store');
  * Activate all the components in the Query Bar package.
  */
 function activate(appRegistry) {
-  appRegistry.registerComponent('Query.QueryBar', QueryBarComponent);
-  appRegistry.registerAction('Query.Actions', QueryAction);
-  appRegistry.registerStore('Query.Store', QueryStore);
-  appRegistry.registerStore('Query.ChangedStore', QueryChangedStore);
+  // appRegistry.registerComponent('Query.QueryBar', QueryBarComponent);
+  // appRegistry.registerAction('Query.Actions', QueryAction);
+  // appRegistry.registerStore('Query.Store', QueryStore);
+  // appRegistry.registerStore('Query.ChangedStore', QueryChangedStore);
 }
 
 /**
  * Deactivate all the components in the Query Bar package.
  */
 function deactivate() {
-  app.appRegistry.deregisterComponent('Query.QueryBar');
-  app.appRegistry.deregisterAction('Query.Actions');
-  app.appRegistry.deregisterStore('Query.Store');
-  app.appRegistry.deregisterStore('Query.ChangedStore');
+  // app.appRegistry.deregisterComponent('Query.QueryBar');
+  // app.appRegistry.deregisterAction('Query.Actions');
+  // app.appRegistry.deregisterStore('Query.Store');
+  // app.appRegistry.deregisterStore('Query.ChangedStore');
 }
 
 module.exports.activate = activate;
