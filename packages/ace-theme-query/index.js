27dda19059b84143b7cd33a75242b378676ddfef
Durran Jordan <durran@gmail.com>
2018-06-06 22:15:38 +0200

    Fix class names

--- packages/ace-theme-query/index.js
+++ packages/ace-theme-query/index.js
@@ -2,108 +2,108 @@
 ace.define("ace/theme/mongodb-query",["require","exports","module","ace/lib/dom"], function(acequire, exports, module) {
 
 exports.isDark = false;
-exports.cssClass = "ace-mongodb";
+exports.cssClass = "ace-mongodb-query";
 exports.cssText = "\
-.ace-mongodb .ace_gutter {\
+.ace-mongodb-query .ace_gutter {\
 background: #ffffff;\
 color: #999999;\
 }\
-.ace-mongodb  {\
+.ace-mongodb-query  {\
 background: #ffffff;\
 color: #000;\
 }\
-.ace-mongodb .ace_keyword {\
+.ace-mongodb-query .ace_keyword {\
 color: #999999;\
 font-weight: normal;\
 }\
-.ace-mongodb .ace_gutter-cell {\
+.ace-mongodb-query .ace_gutter-cell {\
 padding-left: 5px;\
 padding-right: 10px;\
 }\
-.ace-mongodb .ace_string {\
+.ace-mongodb-query .ace_string {\
 color: #5b81a9;\
 }\
-.ace-mongodb .ace_boolean {\
+.ace-mongodb-query .ace_boolean {\
 color: #5b81a9;\
 font-weight: normal;\
 }\
-.ace-mongodb .ace_constant.ace_numeric {\
+.ace-mongodb-query .ace_constant.ace_numeric {\
 color: #5b81a9;\
 }\
-.ace-mongodb .ace_string.ace_regexp {\
+.ace-mongodb-query .ace_string.ace_regexp {\
 color: #5b81a9;\
 }\
-.ace-mongodb .ace_variable.ace_class {\
+.ace-mongodb-query .ace_variable.ace_class {\
 color: teal;\
 }\
-.ace-mongodb .ace_constant.ace_buildin {\
+.ace-mongodb-query .ace_constant.ace_buildin {\
 color: #0086B3;\
 }\
-.ace-mongodb .ace_support.ace_function {\
+.ace-mongodb-query .ace_support.ace_function {\
 color: #0086B3;\
 }\
-.ace-mongodb .ace_comment {\
+.ace-mongodb-query .ace_comment {\
 color: #998;\
 font-style: italic;\
 }\
-.ace-mongodb .ace_variable.ace_language  {\
+.ace-mongodb-query .ace_variable.ace_language  {\
 color: #0086B3;\
 }\
-.ace-mongodb .ace_paren {\
+.ace-mongodb-query .ace_paren {\
 font-weight: normal;\
 }\
-.ace-mongodb .ace_variable.ace_instance {\
+.ace-mongodb-query .ace_variable.ace_instance {\
 color: teal;\
 }\
-.ace-mongodb .ace_constant.ace_language {\
+.ace-mongodb-query .ace_constant.ace_language {\
 font-weight: bold;\
 }\
-.ace-mongodb .ace_cursor {\
+.ace-mongodb-query .ace_cursor {\
 color: #999999;\
 }\
-.ace-mongodb.ace_focus .ace_marker-layer .ace_active-line {\
+.ace-mongodb-query.ace_focus .ace_marker-layer .ace_active-line {\
 background: #ffffff;\
 }\
-.ace-mongodb .ace_marker-layer .ace_active-line {\
+.ace-mongodb-query .ace_marker-layer .ace_active-line {\
 background: #ffffff;\
 }\
-.ace-mongodb .ace_marker-layer .ace_selection {\
+.ace-mongodb-query .ace_marker-layer .ace_selection {\
 background: rgb(181, 213, 255);\
 }\
-.ace-mongodb.ace_multiselect .ace_selection.ace_start {\
+.ace-mongodb-query.ace_multiselect .ace_selection.ace_start {\
 box-shadow: 0 0 3px 0px white;\
 }\
-.ace-mongodb.ace_nobold .ace_line > span {\
+.ace-mongodb-query.ace_nobold .ace_line > span {\
 font-weight: normal !important;\
 }\
-.ace-mongodb .ace_marker-layer .ace_step {\
+.ace-mongodb-query .ace_marker-layer .ace_step {\
 background: rgb(252, 255, 0);\
 }\
-.ace-mongodb .ace_marker-layer .ace_stack {\
+.ace-mongodb-query .ace_marker-layer .ace_stack {\
 background: rgb(164, 229, 101);\
 }\
-.ace-mongodb .ace_marker-layer .ace_bracket {\
+.ace-mongodb-query .ace_marker-layer .ace_bracket {\
 margin: -1px 0 0 -1px;\
 border: 1px solid rgb(192, 192, 192);\
 }\
-.ace-mongodb .ace_gutter-active-line {\
+.ace-mongodb-query .ace_gutter-active-line {\
 background: #ffffff;\
 }\
-.ace-mongodb .ace_marker-layer .ace_selected-word {\
+.ace-mongodb-query .ace_marker-layer .ace_selected-word {\
 background: rgb(250, 250, 255);\
 border: 1px solid rgb(200, 200, 250);\
 }\
-.ace-mongodb .ace_invisible {\
+.ace-mongodb-query .ace_invisible {\
 color: #BFBFBF\
 }\
-.ace-mongodb .ace_print-margin {\
+.ace-mongodb-query .ace_print-margin {\
 width: 1px;\
 background: #e8e8e8;\
 }\
-.ace-mongodb .ace_hidden-cursors {\
+.ace-mongodb-query .ace_hidden-cursors {\
   opacity: 0;\
 }\
-.ace-mongodb .ace_indent-guide {\
+.ace-mongodb-query .ace_indent-guide {\
 background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bLly//BwAmVgd1/w11/gAAAABJRU5ErkJggg==\") right repeat-y;\
 }";
   var dom = acequire("../lib/dom");
