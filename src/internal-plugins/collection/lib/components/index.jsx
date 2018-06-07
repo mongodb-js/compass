2f0782c4ec1a117f3ca83bf8bdc17c65fe2a452e
Durran Jordan <durran@gmail.com>
2018-06-07 13:55:48 +0200

    COMPASS-2927: Refresh Editor on tab change

--- src/internal-plugins/collection/lib/components/index.jsx
+++ src/internal-plugins/collection/lib/components/index.jsx
@@ -38,7 +38,7 @@ class Collection extends React.Component {
   }
 
   componentDidUpdate() {
-    // @todo: Durran: this.QueryActions.refreshCodeMirror();
+    this.QueryActions.refreshEditor();
   }
 
   onTabClicked(idx) {
