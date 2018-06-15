95d3a235f1998d72250fbbe4a154e6be87efe4f6
Durran Jordan <durran@gmail.com>
2018-06-15 12:53:22 +0200

    Remove else from keywords

--- packages/ace-mode/index.js
+++ packages/ace-mode/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 ace.define("ace/mode/doc_comment_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(acequire, exports, module) {
   "use strict";
-  
+
   var oop = acequire("../lib/oop");
   var TextHighlightRules = acequire("./text_highlight_rules").TextHighlightRules;
-  
+
   var DocCommentHighlightRules = function() {
     this.$rules = {
       "start" : [ {
@@ -18,16 +18,16 @@ ace.define("ace/mode/doc_comment_highlight_rules",["require","exports","module",
         }]
     };
   };
-  
+
   oop.inherits(DocCommentHighlightRules, TextHighlightRules);
-  
+
   DocCommentHighlightRules.getTagRule = function(start) {
     return {
       token : "comment.doc.tag.storage.type",
       regex : "\\b(?:TODO|FIXME|XXX|HACK)\\b"
     };
   };
-  
+
   DocCommentHighlightRules.getStartRule = function(start) {
     return {
       token : "comment.doc", // doc comment
@@ -35,7 +35,7 @@ ace.define("ace/mode/doc_comment_highlight_rules",["require","exports","module",
       next  : start
     };
   };
-  
+
   DocCommentHighlightRules.getEndRule = function (start) {
     return {
       token : "comment.doc", // closing comment
@@ -43,20 +43,20 @@ ace.define("ace/mode/doc_comment_highlight_rules",["require","exports","module",
       next  : start
     };
   };
-  
-  
+
+
   exports.DocCommentHighlightRules = DocCommentHighlightRules;
-  
+
 });
 
 ace.define("ace/mode/javascript_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/doc_comment_highlight_rules","ace/mode/text_highlight_rules"], function(acequire, exports, module) {
   "use strict";
-  
+
   var oop = acequire("../lib/oop");
   var DocCommentHighlightRules = acequire("./doc_comment_highlight_rules").DocCommentHighlightRules;
   var TextHighlightRules = acequire("./text_highlight_rules").TextHighlightRules;
   var identifierRe = "[a-zA-Z\\$_\u00a1-\uffff][a-zA-Z\\d\\$_\u00a1-\uffff]*";
-  
+
   var JavaScriptHighlightRules = function(options) {
     var keywordMapper = this.createKeywordMapper({
       "variable.language":
@@ -65,8 +65,8 @@ ace.define("ace/mode/javascript_highlight_rules",["require","exports","module","
         "null|Infinity|NaN|undefined",
       "constant.language.boolean": "true|false"
     }, "identifier");
-    var kwBeforeRe = "case|do|else|finally|in|instanceof|return|throw|try|typeof|yield|void";
-    
+    var kwBeforeRe = "case|do|finally|in|instanceof|return|throw|try|typeof|yield|void";
+
     var escapedRe = "\\\\(?:x[0-9a-fA-F]{2}|" + // hex
       "u[0-9a-fA-F]{4}|" + // unicode
       "u{[0-9a-fA-F]{1,6}}|" + // es6 unicode
@@ -74,7 +74,7 @@ ace.define("ace/mode/javascript_highlight_rules",["require","exports","module","
       "3[0-7][0-7]?|" + // oct
       "[4-7][0-7]?|" + //oct
       ".)";
-    
+
     this.$rules = {
       "no_regex" : [
         DocCommentHighlightRules.getStartRule("doc-start"),
@@ -330,8 +330,8 @@ ace.define("ace/mode/javascript_highlight_rules",["require","exports","module","
         }
       ]
     };
-    
-    
+
+
     if (!options || !options.noES6) {
       this.$rules.no_regex.unshift({
         regex: "[{}]", onMatch: function(val, state, stack) {
@@ -366,19 +366,19 @@ ace.define("ace/mode/javascript_highlight_rules",["require","exports","module","
           defaultToken: "string.quasi"
         }]
       });
-      
+
       if (!options || options.jsx != false)
         JSX.call(this);
     }
-    
+
     this.embedRules(DocCommentHighlightRules, "doc-",
       [ DocCommentHighlightRules.getEndRule("no_regex") ]);
-    
+
     this.normalizeRules();
   };
-  
+
   oop.inherits(JavaScriptHighlightRules, TextHighlightRules);
-  
+
   function JSX() {
     var tagRegex = identifierRe.replace("\\d", "\\d\\-");
     var jsxTag = {
@@ -478,7 +478,7 @@ ace.define("ace/mode/javascript_highlight_rules",["require","exports","module","
       regex : "(?:&#[0-9]+;)|(?:&#x[0-9a-fA-F]+;)|(?:&[a-zA-Z0-9_:\\.-]+;)"
     }];
   }
-  
+
   function comments(next) {
     return [
       {
@@ -505,51 +505,51 @@ ace.define("ace/mode/javascript_highlight_rules",["require","exports","module","
 
 ace.define("ace/mode/matching_brace_outdent",["require","exports","module","ace/range"], function(acequire, exports, module) {
   "use strict";
-  
+
   var Range = acequire("../range").Range;
-  
+
   var MatchingBraceOutdent = function() {};
-  
+
   (function() {
-    
+
     this.checkOutdent = function(line, input) {
       if (! /^\s+$/.test(line))
         return false;
-      
+
       return /^\s*\}/.test(input);
     };
-    
+
     this.autoOutdent = function(doc, row) {
       var line = doc.getLine(row);
       var match = line.match(/^(\s*\})/);
-      
+
       if (!match) return 0;
-      
+
       var column = match[1].length;
       var openBracePos = doc.findMatchingBracket({row: row, column: column});
-      
+
       if (!openBracePos || openBracePos.row == row) return 0;
-      
+
       var indent = this.$getIndent(doc.getLine(openBracePos.row));
       doc.replace(new Range(row, 0, row, column-1), indent);
     };
-    
+
     this.$getIndent = function(line) {
       return line.match(/^\s*/)[0];
     };
-    
+
   }).call(MatchingBraceOutdent.prototype);
-  
+
   exports.MatchingBraceOutdent = MatchingBraceOutdent;
 });
 
 ace.define("ace/mode/folding/cstyle",["require","exports","module","ace/lib/oop","ace/range","ace/mode/folding/fold_mode"], function(acequire, exports, module) {
   "use strict";
-  
+
   var oop = acequire("../../lib/oop");
   var Range = acequire("../../range").Range;
   var BaseFoldMode = acequire("./fold_mode").FoldMode;
-  
+
   var FoldMode = exports.FoldMode = function(commentRegex) {
     if (commentRegex) {
       this.foldingStartMarker = new RegExp(
@@ -561,9 +561,9 @@ ace.define("ace/mode/folding/cstyle",["require","exports","module","ace/lib/oop"
     }
   };
   oop.inherits(FoldMode, BaseFoldMode);
-  
+
   (function() {
-    
+
     this.foldingStartMarker = /([\{\[\(])[^\}\]\)]*$|^\s*(\/\*)/;
     this.foldingStopMarker = /^[^\[\{\(]*([\}\]\)])|^[\s\*]*(\*\/)/;
     this.singleLineBlockCommentRe= /^\s*(\/\*).*\*\/\s*$/;
@@ -572,59 +572,59 @@ ace.define("ace/mode/folding/cstyle",["require","exports","module","ace/lib/oop"
     this._getFoldWidgetBase = this.getFoldWidget;
     this.getFoldWidget = function(session, foldStyle, row) {
       var line = session.getLine(row);
-      
+
       if (this.singleLineBlockCommentRe.test(line)) {
         if (!this.startRegionRe.test(line) && !this.tripleStarBlockCommentRe.test(line))
           return "";
       }
-      
+
       var fw = this._getFoldWidgetBase(session, foldStyle, row);
-      
+
       if (!fw && this.startRegionRe.test(line))
         return "start"; // lineCommentRegionStart
-      
+
       return fw;
     };
-    
+
     this.getFoldWidgetRange = function(session, foldStyle, row, forceMultiline) {
       var line = session.getLine(row);
-      
+
       if (this.startRegionRe.test(line))
         return this.getCommentRegionBlock(session, line, row);
-      
+
       var match = line.match(this.foldingStartMarker);
       if (match) {
         var i = match.index;
-        
+
         if (match[1])
           return this.openingBracketBlock(session, match[1], row, i);
-        
+
         var range = session.getCommentFoldRange(row, i + match[0].length, 1);
-        
+
         if (range && !range.isMultiLine()) {
           if (forceMultiline) {
             range = this.getSectionRange(session, row);
           } else if (foldStyle != "all")
             range = null;
         }
-        
+
         return range;
       }
-      
+
       if (foldStyle === "markbegin")
         return;
-      
+
       var match = line.match(this.foldingStopMarker);
       if (match) {
         var i = match.index + match[0].length;
-        
+
         if (match[1])
           return this.closingBracketBlock(session, match[1], row, i);
-        
+
         return session.getCommentFoldRange(row, i, -1);
       }
     };
-    
+
     this.getSectionRange = function(session, row) {
       var line = session.getLine(row);
       var startIndent = line.search(/\S/);
@@ -641,7 +641,7 @@ ace.define("ace/mode/folding/cstyle",["require","exports","module","ace/lib/oop"
         if  (startIndent > indent)
           break;
         var subRange = this.getFoldWidgetRange(session, "all", row);
-        
+
         if (subRange) {
           if (subRange.start.row <= startRow) {
             break;
@@ -653,14 +653,14 @@ ace.define("ace/mode/folding/cstyle",["require","exports","module","ace/lib/oop"
         }
         endRow = row;
       }
-      
+
       return new Range(startRow, startColumn, endRow, session.getLine(endRow).length);
     };
     this.getCommentRegionBlock = function(session, line, row) {
       var startColumn = line.search(/\s*$/);
       var maxRow = session.getLength();
       var startRow = row;
-      
+
       var re = /^\s*(?:\/\*|\/\/|--)#?(end)?region\b/;
       var depth = 1;
       while (++row < maxRow) {
@@ -669,23 +669,23 @@ ace.define("ace/mode/folding/cstyle",["require","exports","module","ace/lib/oop"
         if (!m) continue;
         if (m[1]) depth--;
         else depth++;
-        
+
         if (!depth) break;
       }
-      
+
       var endRow = row;
       if (endRow > startRow) {
         return new Range(startRow, startColumn, endRow, line.length);
       }
     };
-    
+
   }).call(FoldMode.prototype);
-  
+
 });
 
 ace.define("ace/mode/mongodb",["require","exports","module","ace/lib/oop","ace/mode/text","ace/mode/javascript_highlight_rules","ace/mode/matching_brace_outdent","ace/worker/worker_client","ace/mode/behaviour/cstyle","ace/mode/folding/cstyle"], function(acequire, exports, module) {
   "use strict";
-  
+
   var oop = acequire("../lib/oop");
   var TextMode = acequire("./text").Mode;
   var JavaScriptHighlightRules = acequire("./javascript_highlight_rules").JavaScriptHighlightRules;
@@ -693,33 +693,33 @@ ace.define("ace/mode/mongodb",["require","exports","module","ace/lib/oop","ace/m
   var WorkerClient = acequire("../worker/worker_client").WorkerClient;
   var CstyleBehaviour = acequire("./behaviour/cstyle").CstyleBehaviour;
   var CStyleFoldMode = acequire("./folding/cstyle").FoldMode;
-  
+
   var Mode = function() {
     this.HighlightRules = JavaScriptHighlightRules;
-    
+
     this.$outdent = new MatchingBraceOutdent();
     this.$behaviour = new CstyleBehaviour();
     this.foldingRules = new CStyleFoldMode();
   };
   oop.inherits(Mode, TextMode);
-  
+
   (function() {
-    
+
     this.lineCommentStart = "//";
     this.blockComment = {start: "/*", end: "*/"};
     this.$quotes = {'"': '"', "'": "'", "`": "`"};
-    
+
     this.getNextLineIndent = function(state, line, tab) {
       var indent = this.$getIndent(line);
-      
+
       var tokenizedLine = this.getTokenizer().getLineTokens(line, state);
       var tokens = tokenizedLine.tokens;
       var endState = tokenizedLine.state;
-      
+
       if (tokens.length && tokens[tokens.length-1].type == "comment") {
         return indent;
       }
-      
+
       if (state == "start" || state == "no_regex") {
         var match = line.match(/^.*(?:\bcase\b.*:|[\{\(\[])\s*$/);
         if (match) {
@@ -737,35 +737,35 @@ ace.define("ace/mode/mongodb",["require","exports","module","ace/lib/oop","ace/m
           indent += "* ";
         }
       }
-      
+
       return indent;
     };
-    
+
     this.checkOutdent = function(state, line, input) {
       return this.$outdent.checkOutdent(line, input);
     };
-    
+
     this.autoOutdent = function(state, doc, row) {
       this.$outdent.autoOutdent(doc, row);
     };
-    
+
     this.createWorker = function(session) {
       var worker = new WorkerClient(["ace"], require("./mongodb-worker"), "JavaScriptWorker");
       worker.attachToDocument(session.getDocument());
-      
+
       worker.on("annotate", function(results) {
         session.setAnnotations(results.data);
       });
-      
+
       worker.on("terminate", function() {
         session.clearAnnotations();
       });
-      
+
       return worker;
     };
-    
+
     this.$id = "ace/mode/mongodb";
   }).call(Mode.prototype);
-  
+
   exports.Mode = Mode;
 });
