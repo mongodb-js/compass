92e26074ae9beecfbbbbae3ea46a7b174fc78118
Irina Shestak <lrlna@users.noreply.github.com>
2018-07-03 11:29:17 +0200

    indent items in csharp arrays (#117)
    * Add args template for arrays
    
    * indent items in csharp arrays
    
    * move .join logic for arrays to args template

--- packages/bson-transpilers/codegeneration/javascript/Visitor.js
+++ packages/bson-transpilers/codegeneration/javascript/Visitor.js
@@ -207,7 +207,10 @@ class Visitor extends ECMAScriptVisitor {
         return arg !== ',';
       });
       if (ctx.type.argsTemplate) { // NOTE: not currently being used anywhere.
-        args = ctx.type.argsTemplate(visitedElements, ctx.indentDepth);
+        args = visitedElements.map((arg, index) => {
+          const last = !visitedElements[index + 1];
+          return ctx.type.argsTemplate(arg, ctx.indentDepth, last);
+        }).join('');
       } else {
         args = visitedElements.join(', ');
       }
