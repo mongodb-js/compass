066e0500c5164e903995fd3c9eda22f3c79f28d5
Irina Shestak <shestak.irina@gmail.com>
2018-07-10 17:43:30 +0200

    type check doubles when editing

--- packages/compass-crud/src/components/editor/double.js
+++ packages/compass-crud/src/components/editor/double.js
@@ -1,5 +1,7 @@
-import chars from 'utils';
+import TypeChecker from 'hadron-type-checker';
+import { Element } from 'hadron-document';
 import StandardEditor from './standard';
+import chars from 'utils';
 
 /**
  * CRUD editor for double values.
@@ -13,6 +15,37 @@ class DoubleEditor extends StandardEditor {
    */
   constructor(element) {
     super(element);
+  }
+
+  /**
+   * Complete the double edit by converting the valid string to a double
+   * value or leaving as invalid.
+   */
+  complete() {
+    if (this.element.isCurrentTypeValid()) {
+      this.element.edit(TypeChecker.cast(this.element.currentValue, 'Double'));
+    }
+  }
+
+  /**
+   * Edit Double element. Check if the value is a Double before setting tped
+   * up value.
+   *
+   * @param {Object} value - The new value.
+   */
+  edit(value) {
+    try {
+      const double = TypeChecker.cast(value, 'Double');
+      if (isNaN(double.value)) {
+        this.element.setInvalid(value, 'Double', `${value} is not a valid double format`);
+      } else {
+        this.element.currentValue = value;
+        this.element.setValid();
+        this.element._bubbleUp(Element.Events.Edited);
+      }
+    } catch (error) {
+      this.element.setInvalid(value, this.element.currentType, error.message);
+    }
   }
 
   /**
