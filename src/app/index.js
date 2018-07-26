7d67ee5ad6577b720ebaef269fd1d166d8e0a996
Irina Shestak <shestak.irina@gmail.com>
2018-07-26 15:59:58 +0200

    add ipc events for cmd-f; find-in-page plugin

--- src/app/index.js
+++ src/app/index.js
@@ -340,9 +340,7 @@ app.extend({
         // signal to main process that app is ready
         ipc.call('window:renderer-ready');
         // catch a data refresh coming from window-manager
-        ipc.on('app:refresh-data', function() {
-          global.hadronApp.appRegistry.emit('refresh-data');
-        });
+        ipc.on('app:refresh-data', () => global.hadronApp.appRegistry.emit('refresh-data'));
         // as soon as dom is ready, render and set up the rest
         const MongoDBInstance = require('./models/mongodb-instance');
         state.instance = new MongoDBInstance();
