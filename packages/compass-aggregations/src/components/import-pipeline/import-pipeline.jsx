65eac12267d1427be3fb2cefe9de57b5d5dd1aa9
Durran Jordan <durran@gmail.com>
2018-08-10 16:05:51 +0200

    Add note in import modal

--- packages/compass-aggregations/src/components/import-pipeline/import-pipeline.jsx
+++ packages/compass-aggregations/src/components/import-pipeline/import-pipeline.jsx
@@ -1,7 +1,18 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { Modal } from 'react-bootstrap';
 
+import styles from './import-pipeline.less';
+
+/**
+ * Note.
+ */
+const NOTE = 'Supports MongoDB Shell syntax. Pasting a pipeline will create a new pipeline.';
+
+/**
+ * Import pipeline modal.
+ */
 class ImportPipeline extends PureComponent {
   static displayName = 'ImportPipelineComponent';
 
@@ -19,9 +30,12 @@ class ImportPipeline extends PureComponent {
     return (
       <Modal show={this.props.isOpen} onHide={this.props.closeImport}>
         <Modal.Header closeButton>
-          Import Pipeline
+          <h4>New Pipeline From Plain Text</h4>
         </Modal.Header>
         <Modal.Body>
+          <div className={classnames(styles['import-pipeline-note'])}>
+            {NOTE}
+          </div>
         </Modal.Body>
         <Modal.Footer>
         </Modal.Footer>
