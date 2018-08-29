345c0f38ffc3d1a54a61b7ea683306546ad3de01
Durran Jordan <durran@gmail.com>
2018-08-29 15:26:55 +0200

    Show exception for Kerberos test

--- test/unit/kerberos.test.js
+++ test/unit/kerberos.test.js
@@ -2,8 +2,6 @@ var assert = require('assert');
 
 describe('kerberos', function() {
   it('should be requirable', function() {
-    assert.doesNotThrow(function() {
-      require('kerberos');
-    });
+    require('kerberos');
   });
 });
