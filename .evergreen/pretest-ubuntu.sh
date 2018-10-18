fe83513b7a14f88e6a38853ffb9ba6c8d71155e4
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2018-10-18 16:24:04 -0400

    Fix distribution expansion not being applied

--- .evergreen/pretest-ubuntu.sh
+++ .evergreen/pretest-ubuntu.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y xvfb
+sudo apt-get install -y x11-utils x11-xserver-utils xserver-xorg-core xvfb dbus-x11
                   
 sudo cp $(pwd)/.evergreen/etc-init.d-xvfb /etc/init.d/xvfb
 #chmod +x /etc/init.d/xvfb
