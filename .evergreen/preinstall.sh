97b5a477c44654aa80e4888cdcf44f731d4b1dee
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2018-10-22 10:50:57 -0400

    Checkpoint

--- .evergreen/preinstall.sh
+++ .evergreen/preinstall.sh
@@ -1,11 +1,39 @@
 set -ev
+export NODE_JS_VERSION="8.9.3"
+
+# OS detection
+if [ `uname` == "Darwin" ]; then
+    PLATFORM='darwin'
+    IS_OSX=true
+elif [ "$OSTYPE" == "msys" ]; then
+    PLATFORM='win32'
+    IS_WINDOWS=true
+elif [ "$OSTYPE" == "cygwin" ]; then
+    PLATFORM='win32'
+    IS_WINDOWS=trues
+else
+    PLATFORM='linux'
+    IS_LINUX=true
+    if cat /etc/*release | grep ^NAME | grep Red; then
+        IS_RHEL=true
+    elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
+        IS_UBUNTU=true
+    fi
+fi
+
+echo "NODE_JS_VERSION: $NODE_JS_VERSION"
+echo "NODE_JS_VERSION: ${NODE_JS_VERSION}"
+echo "PLATFORM: $PLATFORM"
+echo "PLATFORM: ${PLATFORM}"
+
 if [ -n "$IS_WINDOWS" ]; then
     echo "Installing nodejs v$NODE_JS_VERSION for windows..."
     curl -fs \
     -o ".deps/node-v$NODE_JS_VERSION-win-x64.zip" \
     --url "https://nodejs.org/download/release/v$NODE_JS_VERSION/node-v$NODE_JS_VERSION-win-x64.zip"
     cd .deps
-    unzip node-v$NODE_JS_VERSION-win-x64.zip
+    ls -alh
+    unzip -q node-v$NODE_JS_VERSION-win-x64.zip
     mv node-v$NODE_JS_VERSION-win-x64/* .
     rm -rf node-v$NODE_JS_VERSION-win-x64
 
@@ -17,10 +45,10 @@ if [ -n "$IS_WINDOWS" ]; then
     rm -rf node_modules/npm2/
     chmod +x npm.cmd npm
 else
-    echo "Installing nodejs v$NODE_JS_VERSION for $PLATFORM..."
+    echo "Installing nodejs v${NODE_JS_VERSION} for ${PLATFORM}..."
     curl -fs \
-        -o ".deps/node-v$NODE_JS_VERSION-$PLATFORM-x64.tar.gz" \
-        --url "https://nodejs.org/download/release/v$NODE_JS_VERSION/node-v$NODE_JS_VERSION-$PLATFORM-x64.tar.gz"
+        -o ".deps/node-v${NODE_JS_VERSION}-${PLATFORM}-x64.tar.gz" \
+        --url "https://nodejs.org/download/release/v${NODE_JS_VERSION}/node-v${NODE_JS_VERSION}-${PLATFORM}-x64.tar.gz"
     cd .deps
     tar xzf node-v$NODE_JS_VERSION-$PLATFORM-x64.tar.gz --strip-components=1
 
