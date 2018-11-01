61dc50a1072d4083124a2c1a07c8c384c2f9f542
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2018-11-01 14:38:42 -0400

    TODO for testing native module plugins in release build

--- test/unit/keytar.test.js
+++ test/unit/keytar.test.js
@@ -1,6 +1,11 @@
 var assert = require('assert');
 
-describe('keytar', function() {
+// TODO (@imlucas) Should be replaced by trying to require
+// plugins which use native modules:
+// - compass-connect
+// - compass-connect-kerberos
+// - etc.
+describe.skip('keytar', function() {
   it('should be requirable', function() {
     assert.doesNotThrow(function() {
       require('keytar');
