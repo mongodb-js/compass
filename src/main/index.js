29cc1a87f2b4a0002105492ac8892de438604d71
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2018-11-19 13:41:03 -0500

    Adding debugging for uncaught exceptions

--- src/main/index.js
+++ src/main/index.js
@@ -19,47 +19,48 @@ if (!pkg.distribution) {
 
 // TODO (@imlucas): Revisit whether to ship this at same time or not.
 //
-// const { dialog, clipboard, app } = require('electron');
-// const COMPASS_ICON = require('../icon');
-// const cleanStack = require('clean-stack');
-// const ensureError = require('ensure-error');
+const { dialog, clipboard, app } = require('electron');
+const COMPASS_ICON = require('../icon');
+const cleanStack = require('clean-stack');
+const ensureError = require('ensure-error');
 
-// process.on('uncaughtException', err => {
-//   err = ensureError(err);
-//   const stack = cleanStack(err.stack);
+process.on('uncaughtException', err => {
+  console.error('handling uncaughtException', err);
+  err = ensureError(err);
+  const stack = cleanStack(err.stack);
 
-//   var detail = '${app.getName()} version ${app.getVersion()}\n';
-//   detail += `Stacktrace:\n${stack}`;
-//   const message = `${app.getName()} has encountered an unexpected error`;
-//   console.error(`${message}: ${detail}`);
+  var detail = '${app.getName()} version ${app.getVersion()}\n';
+  detail += `Stacktrace:\n${stack}`;
+  const message = `${app.getName()} has encountered an unexpected error`;
+  console.error(`${message}: ${detail}`);
 
-//   const btnIndex = dialog.showMessageBox({
-//     type: 'error',
-//     buttons: [
-//       'OK',
-//       process.platform === 'darwin' ? 'Copy Error' : 'Copy error'
-//     ],
-//     icon: COMPASS_ICON,
-//     defaultId: 0,
-//     noLink: true,
-//     message: message,
-//     detail: detail
-//   });
-//   /**
-//    * TODO (@imlucas) Copy diagnostics to clipboard?
-//    * or prepopulated JIRA link?
-//    * https://confluence.atlassian.com/jirakb/creating-issues-via-direct-html-links-159474.html
-//    */
-//   if (btnIndex === 1) {
-//     clipboard.writeText(`${message}\n${stack}`);
-//     return;
-//   }
+  const btnIndex = dialog.showMessageBox({
+    type: 'error',
+    buttons: [
+      'OK',
+      process.platform === 'darwin' ? 'Copy Error' : 'Copy error'
+    ],
+    icon: COMPASS_ICON,
+    defaultId: 0,
+    noLink: true,
+    message: message,
+    detail: detail
+  });
+  /**
+   * TODO (@imlucas) Copy diagnostics to clipboard?
+   * or prepopulated JIRA link?
+   * https://confluence.atlassian.com/jirakb/creating-issues-via-direct-html-links-159474.html
+   */
+  if (btnIndex === 1) {
+    clipboard.writeText(`${message}\n${stack}`);
+    return;
+  }
 
-//   if (btnIndex === 0) {
-//     app.quit();
-//     return;
-//   }
-// });
+  if (btnIndex === 0) {
+    app.quit();
+    return;
+  }
+});
 
 var path = require('path');
 var resourcePath = path.join(__dirname, '..', '..');
