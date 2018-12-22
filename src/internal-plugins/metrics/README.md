e32b72da860a4ec635a1966b21067741b5ab646b
Durran Jordan <durran@gmail.com>
2018-12-23 11:23:11 +1300

    COMPASS-3331: Move Metrics Plugin to External

--- src/internal-plugins/metrics/README.md
+++
@@ -1,4 +0,0 @@
-# Compass Metrics
-
-Tracks anonymous usage metrics by listening to store changes and sending
-events to various trackers (intercom, stitch, bugsnag).
--- src/setup-hadron-distribution.js
+++ src/setup-hadron-distribution.js
@@ -11,9 +11,11 @@ if (!process.env.HADRON_DISTRIBUTION) {
 
 const config = pkg.config.hadron.distributions[process.env.HADRON_DISTRIBUTION];
 const name = config.name;
+const productName = config.productName;
 const readonly = config.readonly;
 const isolated = config.isolated;
 process.env.HADRON_PRODUCT = name || 'mongodb-compass';
+process.env.HADRON_PRODUCT_NAME = productName || 'MongoDB Compass';
 process.env.HADRON_READONLY = readonly || false;
 process.env.HADRON_ISOLATED = isolated || false;
 
