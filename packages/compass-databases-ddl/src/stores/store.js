70baef80749564702fa5997a43d71222dad9f2fc
Durran Jordan <durran@gmail.com>
2018-12-25 12:16:48 +1300

    Store listens to instance store refresh

--- packages/compass-databases-ddl/src/stores/store.js
+++ packages/compass-databases-ddl/src/stores/store.js
@@ -1,9 +1,14 @@
 import { createStore } from 'redux';
+import { sortDatabases } from 'modules/databases';
 import reducer from 'modules';
 
 const store = createStore(reducer);
 
-store.onActivated = () => {
+store.onActivated = (appRegistry) => {
+  appRegistry.getStore('App.InstanceStore').listen((state) => {
+    // @todo: Need column, direction
+    store.dispatch(sortDatabases(state.instance.databases));
+  });
 };
 
 export default store;
