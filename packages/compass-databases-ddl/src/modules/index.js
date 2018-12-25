f236d6378e34a7ee295df0b7e189d2e0d1974298
Durran Jordan <durran@gmail.com>
2018-12-25 17:30:05 +1300

    COMPASS-1100: Add data service hooks

--- packages/compass-databases-ddl/src/modules/index.js
+++ packages/compass-databases-ddl/src/modules/index.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
 import columns from 'modules/columns';
 import databases from 'modules/databases';
+import dataService from 'modules/data-service';
 
 /**
  * The main reducer.
  */
 const reducer = combineReducers({
   columns,
-  databases
+  databases,
+  dataService
 });
 
 export default reducer;
--- packages/compass-databases-ddl/src/stores/store.js
+++ packages/compass-databases-ddl/src/stores/store.js
@@ -1,13 +1,29 @@
 import { createStore } from 'redux';
+import { dataServiceConnected } from 'modules/data-service';
 import { sortDatabases } from 'modules/databases';
 import reducer from 'modules';
 
 const store = createStore(reducer);
 
 store.onActivated = (appRegistry) => {
+  /**
+   * Sort the databases once the instance is refreshed.
+   *
+   * @param {Object} state - The instance store state.
+   */
   appRegistry.getStore('App.InstanceStore').listen((state) => {
     // @todo: Need column, direction
     store.dispatch(sortDatabases(state.instance.databases));
+  });
+
+  /**
+   * Set the data service in the store when connected.
+   *
+   * @param {Error} error - The error.
+   * @param {DataService} dataService - The data service.
+   */
+  appRegistry.on('data-service-connected', (error, dataService) => {
+    store.dispatch(dataServiceConnected(error, dataService));
   });
 };
 
