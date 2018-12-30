2d5c1e93bebe14e29305d12ed1b08d8f106073f1
Durran Jordan <durran@gmail.com>
2018-12-30 14:58:31 +1300

    Add capped size to reducer

--- packages/compass-databases-ddl/src/modules/create-database/index.js
+++ packages/compass-databases-ddl/src/modules/create-database/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import dataService from 'modules/data-service';
+import cappedSize from 'modules/create-database/capped-size';
 import collectionName from 'modules/create-database/collection-name';
 import isCapped from 'modules/create-database/is-capped';
 import isVisible from 'modules/create-database/is-visible';
@@ -9,9 +10,10 @@ import name from 'modules/create-database/name';
  * The main reducer.
  */
 const reducer = combineReducers({
+  cappedSize,
+  collectionName,
   isCapped,
   isVisible,
-  collectionName,
   name,
   dataService
 });
