62afbe9226681702eb32c6a52cd7fbe1ef0968de
Durran Jordan <durran@gmail.com>
2018-12-30 14:23:41 +1300

    Adding collection name module to create database store

--- packages/compass-databases-ddl/src/modules/create-database/index.js
+++ packages/compass-databases-ddl/src/modules/create-database/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import dataService from 'modules/data-service';
 import isVisible from 'modules/create-database/is-visible';
 import name from 'modules/create-database/name';
+import collectionName from 'modules/create-database/collection-name';
 
 /**
  * The main reducer.
@@ -9,6 +10,7 @@ import name from 'modules/create-database/name';
 const reducer = combineReducers({
   isVisible,
   name,
+  collectionName,
   dataService
 });
 
