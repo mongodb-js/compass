ca44586134573a850886d9037e2be15c9cd3f08a
Durran Jordan <durran@gmail.com>
2019-01-11 14:02:49 +1300

    Add create database thunk action test skeleton

---
+++ packages/compass-databases-ddl/src/modules/create-database/index.spec.js
@@ -0,0 +1,47 @@
+// import { createDatabase } from 'modules/create-database';
+
+describe('create database module', () => {
+  describe('#createDatabase', () => {
+    context('when an error exists in the state', () => {
+
+    });
+
+    context('when no error exists in the state', () => {
+      context('when the database name is invalid', () => {
+
+      });
+
+      context('when the database name is valid', () => {
+        context('when the collection contains no special options', () => {
+          context('when the create is a success', () => {
+
+          });
+
+          context('when the create errors', () => {
+
+          });
+        });
+
+        context('when the collection is capped', () => {
+          context('when the create is a success', () => {
+
+          });
+
+          context('when the create errors', () => {
+
+          });
+        });
+
+        context('when the collection has a collation', () => {
+          context('when the create is a success', () => {
+
+          });
+
+          context('when the create errors', () => {
+
+          });
+        });
+      });
+    });
+  });
+});
