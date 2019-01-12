cd9c4047455a8b97ff203dde9a6e5319705d436c
Durran Jordan <durran@gmail.com>
2019-01-12 12:18:04 +0800

    Adding drop database name module

---
+++ packages/compass-databases-ddl/src/modules/drop-database/name.js
@@ -0,0 +1,36 @@
+/**
+ * Drop database name.
+ */
+export const CHANGE_DATABASE_NAME = 'ddl/drop-database/name/CHANGE_NAME';
+
+/**
+ * The initial state of the database name.
+ */
+export const INITIAL_STATE = '';
+
+/**
+ * Reducer function for handle state changes to drop database.
+ *
+ * @param {String} state - The drop database name state.
+ * @param {Object} action - The action.
+ *
+ * @returns {Array} The new state.
+ */
+export default function reducer(state = INITIAL_STATE, action) {
+  if (action.type === CHANGE_DATABASE_NAME) {
+    return action.name;
+  }
+  return state;
+}
+
+/**
+ * The change name action creator.
+ *
+ * @param {String} name - The database name.
+ *
+ * @returns {Object} The action.
+ */
+export const changeDatabaseName = (name) => ({
+  type: CHANGE_DATABASE_NAME,
+  name: name
+});
