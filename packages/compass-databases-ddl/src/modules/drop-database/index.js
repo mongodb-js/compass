d21ce798048041fb90e6b1f0b70ae2565836c00c
Durran Jordan <durran@gmail.com>
2019-01-12 12:16:08 +0800

    Adding in drop database module

---
+++ packages/compass-databases-ddl/src/modules/drop-database/index.js
@@ -0,0 +1,81 @@
+import { combineReducers } from 'redux';
+import dataService from 'modules/data-service';
+
+/**
+ * The reset action name.
+ */
+export const RESET = 'ddl/drop-database/RESET';
+
+/**
+ * The main reducer.
+ */
+const reducer = combineReducers({
+  dataService
+});
+
+/**
+ * The root reducer.
+ *
+ * @param {Object} state - The state.
+ * @param {Object} action - The action.
+ *
+ * @returns {Object} The new state.
+ */
+const rootReducer = (state, action) => {
+  if (action.type === RESET) {
+    return { ...state };
+  }
+  return reducer(state, action);
+};
+
+export default rootReducer;
+
+/**
+ * Reset the state of the entire store.
+ *
+ * @return {Object} The action creator.
+ */
+export const reset = () => ({
+  type: RESET
+});
+
+/**
+ * Stop progress and set the error.
+ *
+ * @param {Function} dispatch - The dispatch function.
+ * @param {Error} err - The error.
+ *
+ * @return {Object} The result.
+ */
+const stopWithError = () => {
+  // dispatch(toggleIsRunning(false));
+  // return dispatch(handleError(err));
+};
+
+/**
+ * The drop database action.
+ *
+ * @returns {Function} The thunk function.
+ */
+export const dropDatabase = () => {
+  return (dispatch, getState) => {
+    const state = getState();
+    const ds = state.dataService.dataService;
+    const dbName = state.name;
+
+    // dispatch(clearError());
+
+    try {
+      // dispatch(toggleIsRunning(true));
+      ds.dropDatabase(dbName, (e) => {
+        if (e) {
+          return stopWithError(dispatch, e);
+        }
+        global.hadronApp.appRegistry.getAction('App.InstanceActions').refreshInstance();
+        dispatch(reset());
+      });
+    } catch (e) {
+      return stopWithError(dispatch, e);
+    }
+  };
+};
---
+++ packages/compass-databases-ddl/src/modules/drop-database/index.spec.js
@@ -0,0 +1,39 @@
+import reducer, { reset, RESET } from 'modules/drop-database';
+
+describe('create database module', () => {
+  describe('#reducer', () => {
+    context('when an action is provided', () => {
+      context('when the action is reset', () => {
+        const dataService = 'data-service';
+
+        it('returns the reset state', () => {
+          expect(reducer({ dataService: dataService }, reset())).to.deep.equal({
+            dataService: 'data-service'
+          });
+        });
+      });
+    });
+  });
+
+  describe('#reset', () => {
+    it('returns the reset action', () => {
+      expect(reset()).to.deep.equal({ type: RESET });
+    });
+  });
+
+  describe('#dropDatabase', () => {
+    context('when an error exists in the state', () => {
+
+    });
+
+    context('when no error exists in the state', () => {
+      context('when the drop is a success', () => {
+
+      });
+
+      context('when the drop errors', () => {
+
+      });
+    });
+  });
+});
