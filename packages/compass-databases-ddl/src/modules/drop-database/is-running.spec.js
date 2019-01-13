6ae97dba332cd8953d7f1ac7ca4a987252bb92bb
Durran Jordan <durran@gmail.com>
2019-01-13 11:50:01 +0800

    Adding is running to drop database

---
+++ packages/compass-databases-ddl/src/modules/drop-database/is-running.spec.js
@@ -0,0 +1,30 @@
+import reducer, {
+  INITIAL_STATE,
+  toggleIsRunning,
+  TOGGLE_IS_RUNNING
+} from 'modules/drop-database/is-running';
+
+describe('drop database is running module', () => {
+  describe('#reducer', () => {
+    context('when an action is provided', () => {
+      it('returns the new state', () => {
+        expect(reducer(undefined, toggleIsRunning(true))).to.equal(true);
+      });
+    });
+
+    context('when an action is not provided', () => {
+      it('returns the default state', () => {
+        expect(reducer(undefined, {})).to.equal(INITIAL_STATE);
+      });
+    });
+  });
+
+  describe('#toggleIsRunning', () => {
+    it('returns the action', () => {
+      expect(toggleIsRunning(false)).to.deep.equal({
+        type: TOGGLE_IS_RUNNING,
+        isRunning: false
+      });
+    });
+  });
+});
