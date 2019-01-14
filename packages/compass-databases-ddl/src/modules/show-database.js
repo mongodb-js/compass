ffce03dfada9b0034b54fa5d10869119ebefae28
Durran Jordan <durran@gmail.com>
2019-01-14 13:48:09 +0800

    Adding debug

--- packages/compass-databases-ddl/src/modules/show-database.js
+++ packages/compass-databases-ddl/src/modules/show-database.js
@@ -16,17 +16,25 @@ const COLLECTION_STORE = 'App.CollectionStore';
  * @param {String} name - The database name.
  */
 export const showDatabase = (name) => {
+  console.log('show database', name);
   return (dispatch, getState) => {
     const appRegistry = getState().appRegistry;
     if (appRegistry) {
+      console.log('found app registry');
       const namespaceStore = appRegistry.getStore(NAMESPACE_STORE);
       const collectionStore = appRegistry.getStore(COLLECTION_STORE);
       if (namespaceStore && collectionStore) {
+        console.log('found ns & coll stores');
         if (namespaceStore.ns !== name) {
+          console.log('ns !== name');
           const ipc = require('hadron-ipc');
+          console.log('empty collection');
           collectionStore.setCollection({});
+          console.log('set name', name);
           namespaceStore.ns = name;
+          console.log('dispatch app registry event');
           dispatch(appRegistryEmit('database-selected', { view: 'table' }));
+          console.log('call ipc');
           ipc.call('window:hide-collection-submenu');
         }
       }
