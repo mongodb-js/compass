6dae1561c93ba343bc996296d206ae33f8c99479
Durran Jordan <durran@gmail.com>
2019-01-15 22:20:05 +0800

    Fix sidebar DDL actions

--- src/internal-plugins/sidebar/lib/components/sidebar-database.jsx
+++ src/internal-plugins/sidebar/lib/components/sidebar-database.jsx
@@ -12,7 +12,6 @@ class SidebarDatabase extends React.Component {
     const appRegistry = global.hadronApp.appRegistry;
     this.WriteStateStore = appRegistry.getStore('DeploymentAwareness.WriteStateStore');
     this.CollectionsActions = appRegistry.getAction('Database.CollectionsActions');
-    this.DatabaseDDLActions = appRegistry.getAction('DatabaseDDL.Actions');
     this.CollectionStore = appRegistry.getStore('App.CollectionStore');
     this.NamespaceStore = appRegistry.getStore('App.NamespaceStore');
     this.state = this.WriteStateStore.state;
@@ -83,7 +82,7 @@ class SidebarDatabase extends React.Component {
   handleDropDBClick(isWritable) {
     if (isWritable) {
       const databaseName = this.props._id;
-      this.DatabaseDDLActions.openDropDatabaseDialog(databaseName);
+      global.hadronApp.appRegistry.emit('open-drop-database', databaseName);
     }
   }
 
