4f17ec74b5f75d1df941e702cdbc0785c6ee8b53
Durran Jordan <durran@gmail.com>
2019-01-23 15:36:55 +0100

    Add show collection debug

--- packages/compass-collections-ddl/src/modules/show-collection.js
+++ packages/compass-collections-ddl/src/modules/show-collection.js
@@ -16,10 +16,14 @@ export const showCollection = (name) => {
     const state = getState();
     const appRegistry = state.appRegistry;
     if (appRegistry) {
+      console.log('-------------------- showCollection dbname', state.databaseName); 
+      console.log('-------------------- showCollection name', name); 
       const collectionStore = appRegistry.getStore(COLLECTION_STORE);
       const collection = find(state.collections, (coll) => {
+        console.log(coll);
         return coll._id === `${state.databaseName}.${name}`;
       });
+      console.log('collection', collection);
       collectionStore.setCollection({
         _id: collection._id,
         readonly: collection.readonly,
