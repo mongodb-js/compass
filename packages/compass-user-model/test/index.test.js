250774d2d64d090482882b6bcfcc68ad0649d3b5
aherlihy <anna.herlihy@10gen.com>
2019-02-28 14:37:31 +0100

    function

--- packages/compass-user-model/test/index.test.js
+++ packages/compass-user-model/test/index.test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var User = require('../');
 
 describe('compass-user-model', function() {
-  it('should work', () => {
+  it('should work', function() {
     assert(User);
   });
 });
