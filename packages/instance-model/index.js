87eb3a4692b9c76d8de0c3bd1d3235b0cdb45bdb
aherlihy <anna.herlihy@10gen.com>
2019-02-28 14:30:10 +0100

    only export extended model

--- packages/instance-model/index.js
+++ packages/instance-model/index.js
@@ -1,5 +1,3 @@
-var Model = require('./lib/model');
 var ExtendedModel = require('./lib/extended-model');
 
-module.exports = Model;
-module.exports.ExtendedInstanceModel = ExtendedModel;
+module.exports = ExtendedModel;
