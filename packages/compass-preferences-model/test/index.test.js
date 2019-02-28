0e593f4cd6c5dfe35665707b12fbf420ccf5c261
aherlihy <anna.herlihy@10gen.com>
2019-02-28 14:39:28 +0100

    use function

--- packages/compass-preferences-model/test/index.test.js
+++ packages/compass-preferences-model/test/index.test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var Preferences = require('../');
 
 describe('compass-preferences-model', function() {
-  it('should work', () => {
+  it('should work', function() {
     assert(Preferences);
   });
 });
