7ddf17f9cd9194a156454337c82eb371364cfa46
aherlihy <anna.herlihy@10gen.com>
2019-02-28 13:11:45 +0100

    only export extended model

--- packages/collection-model/index.js
+++ packages/collection-model/index.js
@@ -1,10 +1,5 @@
-var CollectionModel = require('./lib/collection-model');
-var CollectionModelCollection = require('./lib/collection-model').Collection;
 var ExtendedCollectionModel = require('./lib/extended-collection-model');
 var ExtendedCollectionModelCollection = require('./lib/extended-collection-model').Collection;
 
-module.export = CollectionModel;
-module.export.Collection = CollectionModelCollection;
-/* Models used by Compass */
-module.export.ExtendedCollectionModel = ExtendedCollectionModel;
-module.export.ExtendedCollectionModelCollection = ExtendedCollectionModelCollection;
+module.export = ExtendedCollectionModel;
+module.export.Collection = ExtendedCollectionModelCollection;
