850a4f689b1c76b71691a5c9cac713f6c5421361
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-03-12 20:06:20 -0400

    webpack 4 (#79)
    * webpack 4 upgrade
    
    mongodb-js/compass-plugin#22 mongodb-js/compass-plugin#21
    
    * build bundle analyzer repport for ghpages

--- packages/compass-aggregations/config/webpack.prod.config.js
+++ packages/compass-aggregations/config/webpack.prod.config.js
@@ -2,10 +2,8 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const baseWebpackConfig = require('./webpack.base.config');
 const project = require('./project');
@@ -58,6 +56,10 @@ const config = {
       }
     ]
   },
+  optimization: {
+    // Minimize and uglify the code
+    minimize: true,
+  },
   plugins: [
     // Auto-create webpack externals for any dependency listed as a peerDependency in package.json
     // so that the external vendor JavaScript is not part of our compiled bundle
@@ -77,14 +79,7 @@ const config = {
     new webpack.DefinePlugin(GLOBALS),
 
     // Creates HTML page for us at build time
-    new HtmlWebpackPlugin(),
-
-    // An ES6+ aware minifier, results in smaller output compared to UglifyJS given that
-    // Chromium in electron supports the majority of ES6 features out of the box.
-    new MinifyPlugin()
-
-    // Uncomment to Analyze the output bundle size of the plugin. Useful for optimizing the build.
-    // new BundleAnalyzerPlugin()
+    new HtmlWebpackPlugin()
   ],
   stats: {
     colors: true,
