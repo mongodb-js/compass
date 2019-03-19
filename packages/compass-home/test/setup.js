6c5f680005fe10e1a9f1871c13e7a37b8b6920e5
aherlihy <anna.herlihy@10gen.com>
2019-03-19 16:17:28 +0100

    small fixes

--- packages/compass-home/test/setup.js
+++ packages/compass-home/test/setup.js
@@ -16,34 +16,6 @@ require('jsdom-global')('', {
   }
 });
 
-// const aggregationsActivate = require('@mongodb-js/compass-aggregations').activate;
-// const appActivate = require('@mongodb-js/compass-app-stores').activate;
-// const authKerbActivate = require('@mongodb-js/compass-auth-kerberos').activate;
-// const authLdapActivate = require('@mongodb-js/compass-auth-ldap').activate;
-// const authX509Activate = require('@mongodb-js/compass-auth-x509').activate;
-// const collectionActivate = require('@mongodb-js/compass-collection').activate;
-// const collectionDDLActivate = require('@mongodb-js/compass-collections-ddl').activate;
-// const collectionStatsActivate = require('@mongodb-js/compass-collection-stats').activate;
-// const connectActivate = require('@mongodb-js/compass-connect').activate;
-// const crudActivate = require('@mongodb-js/compass-crud').activate;
-// const databaseActivate = require('@mongodb-js/compass-database').activate;
-// const databasesDDLActivate = require('@mongodb-js/compass-databases-ddl').activate;
-// const daActivate = require('@mongodb-js/compass-deployment-awareness').activate;
-// const exportToLangActivate = require('@mongodb-js/compass-export-to-language').activate;
-// const fieldStoreActivate = require('@mongodb-js/compass-field-store').activate;
-// const findInPageActivate = require('@mongodb-js/compass-find-in-page').activate;
-// const importExportActivate = require('@mongodb-js/compass-import-export').activate;
-// const indexesActivate = require('@mongodb-js/compass-indexes').activate;
-// const instanceActivate = require('@mongodb-js/compass-instance').activate;
-// const instanceHeaderActivate = require('@mongodb-js/compass-instance-header').activate;
-// const queryBarActivate = require('@mongodb-js/compass-query-bar').activate;
-// const queryHistoryActivate = require('@mongodb-js/compass-query-history').activate;
-// const schemaValidationActivate = require('@mongodb-js/compass-schema-validation').activate;
-// const serverVersionActivate = require('@mongodb-js/compass-server-version').activate;
-// const rtssActivate = require('@mongodb-js/compass-serverstats').activate;
-// const sidebarActivate = require('@mongodb-js/compass-sidebar').activate;
-// const sshTunnelStatusActivate = require('@mongodb-js/compass-ssh-tunnel-status').activate;
-// const statusActivate = require('@mongodb-js/compass-status').activate;
 const MongoDBInstance = require('mongodb-instance-model');
 
 const appRegistry = new AppRegistry();
@@ -53,35 +25,6 @@ global.expect = chai.expect;
 global.hadronApp = hadronApp;
 global.hadronApp.appRegistry = appRegistry;
 global.hadronApp.instance = new MongoDBInstance();
-
-// aggregationsActivate(appRegistry);
-// appActivate(appRegistry);
-// authKerbActivate(appRegistry);
-// authLdapActivate(appRegistry);
-// authX509Activate(appRegistry);
-// collectionActivate(appRegistry);
-// collectionDDLActivate(appRegistry);
-// collectionStatsActivate(appRegistry);
-// connectActivate(appRegistry);
-// crudActivate(appRegistry);
-// databaseActivate(appRegistry);
-// databasesDDLActivate(appRegistry);
-// daActivate(appRegistry);
-// exportToLangActivate(appRegistry);
-// fieldStoreActivate(appRegistry);
-// findInPageActivate(appRegistry);
-// importExportActivate(appRegistry);
-// indexesActivate(appRegistry);
-// instanceActivate(appRegistry);
-// instanceHeaderActivate(appRegistry);
-// queryBarActivate(appRegistry);
-// queryHistoryActivate(appRegistry);
-// schemaValidationActivate(appRegistry);
-// serverVersionActivate(appRegistry);
-// sshTunnelStatusActivate(appRegistry);
-// statusActivate(appRegistry);
-// sidebarActivate(appRegistry);
-// rtssActivate(appRegistry);
 
 appRegistry.onActivated();
 
