5a33a11f38ab2d4728e2d1310e569ca6f97fff07
Alena Khineika <alenakhineika@users.noreply.github.com>
2019-04-05 16:55:39 +0200

    COMPASS-1803 Explain plan as standalone plugin (#1682)
    * Resolving conflicts
    
    * Rebase fix try
    
    * Rebase fix context-eval
    
    * Query History and Export to language fixes

--- src/internal-plugins/explain/lib/constants.js
+++
@@ -1,9 +0,0 @@
-// Constants for width and height calculations of each stage card
-
-module.exports = {
-  DEFAULT_CARD_WIDTH: 276,     // width of a card
-  DEFAULT_CARD_HEIGHT: 132,    // height of a card without highlighted fields
-  SHARD_CARD_HEIGHT: 30,       // height of a shard label card
-  HIGHLIGHT_FIELD_HEIGHT: 41,  // height of a single 'highlighted' field
-  VERTICAL_PADDING: 50         // vertical space between two cards
-};
