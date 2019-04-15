6c7b3c16382936f3fa2e1a8fd680b44c14ebbe59
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-04-15 16:30:54 -0400

    COMPASS-3420: Add stage button not green anymore

--- packages/compass-aggregations/src/components/add-stage/add-stage.jsx
+++ packages/compass-aggregations/src/components/add-stage/add-stage.jsx
@@ -25,7 +25,7 @@ class AddStage extends PureComponent {
       <div className={classnames(styles['add-stage'])}>
         <TextButton
           text="Add Stage"
-          className="btn btn-xs btn-primary"
+          className="btn btn-xs btn-default"
           clickHandler={this.props.stageAdded} />
       </div>
     );
