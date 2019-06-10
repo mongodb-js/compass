96834a5f827d4a2d09d8c4b64fde3b24bd8d7987
Durran Jordan <durran@gmail.com>
2019-06-10 15:22:14 +0200

    User view that exists

--- packages/compass-collection/electron/renderer/index.js
+++ packages/compass-collection/electron/renderer/index.js
@@ -92,7 +92,7 @@ dataService.connect((error, ds) => {
   appRegistry.emit('data-service-connected', error, ds);
   appRegistry.emit('server-version-changed', '4.2.0');
   // appRegistry.emit('select-namespace', 'citibike.trips');
-  appRegistry.emit('open-namespace-in-new-tab', 'citibike.tripsReadonlyLongCollectionName', true, 'citibike.trips');
+  appRegistry.emit('open-namespace-in-new-tab', 'echo.bandsReadonly', true, 'echo.bands');
   appRegistry.emit('open-namespace-in-new-tab', 'echo.artists', false);
   // appRegistry.emit('open-namespace-in-new-tab', 'thisisaverylongdatabase.andcollectionname');
 });
