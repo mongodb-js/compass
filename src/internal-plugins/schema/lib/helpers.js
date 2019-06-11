d5affe8922dda5ef90eeb044a398753fb41f9f25
Durran Jordan <durran@gmail.com>
2019-06-11 22:39:06 +0200

    Delete schema internal plugin

--- src/internal-plugins/schema/lib/helpers.js
+++
@@ -1,7 +0,0 @@
-module.exports.STRING = 'String';
-module.exports.DECIMAL_128 = 'Decimal128';
-module.exports.DOUBLE = 'Double';
-module.exports.LONG = 'Long';
-module.exports.INT_32 = 'Int32';
-module.exports.STRING = 'String';
-module.exports.NUMBER = 'Number'; // pseudo-type for all numeric types
