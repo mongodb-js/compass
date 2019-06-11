8dc85ee3b1cb25d41b0483d5e9591e771a215b7d
Durran Jordan <durran@gmail.com>
2019-06-11 14:22:28 +0200

    Remove dnd context

--- packages/compass-collection/src/components/workspace/workspace.jsx
+++ packages/compass-collection/src/components/workspace/workspace.jsx
@@ -2,7 +2,6 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import { WithDragDropContext } from 'hadron-react-components';
 import {
   createNewTab,
   closeTab,
@@ -242,5 +241,5 @@ const MappedWorkspace = connect(
   }
 )(Workspace);
 
-export default WithDragDropContext(MappedWorkspace);
+export default MappedWorkspace;
 export { Workspace };
