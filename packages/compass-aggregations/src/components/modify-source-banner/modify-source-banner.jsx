1481ab9a6418db98e17c5b40be48d01c2c75554d
Durran Jordan <durran@gmail.com>
2019-06-27 15:06:12 +0200

    JSDoc the banner component

--- packages/compass-aggregations/src/components/modify-source-banner/modify-source-banner.jsx
+++ packages/compass-aggregations/src/components/modify-source-banner/modify-source-banner.jsx
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import styles from './modify-source-banner.less';
 
+/**
+ * The blue banner displayed when modifying a source pipeline.
+ *
+ * @param {Object} props - The properties.
+ *
+ * @returns {Component} The react component.
+ */
 const ModifySourceBanner = (props) => {
   return (
     <div className={classnames(styles['modify-source-banner'])}>
