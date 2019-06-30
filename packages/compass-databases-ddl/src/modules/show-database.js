a140d4d773efde95d9997ec5415452ea5f6c05f6
Durran Jordan <durran@gmail.com>
2019-06-30 12:30:35 +0200

    Remove Collection and Namespace store usage

--- packages/compass-databases-ddl/src/modules/show-database.js
+++ packages/compass-databases-ddl/src/modules/show-database.js
@@ -1,16 +1,6 @@
 import { appRegistryEmit } from 'modules/app-registry';
 
 /**
- * Namespace store name in the app registry.
- */
-const NAMESPACE_STORE = 'App.NamespaceStore';
-
-/**
- * Collection store name in the app registry.
- */
-const COLLECTION_STORE = 'App.CollectionStore';
-
-/**
  * Show the database.
  *
  * @param {String} name - The database name.
@@ -19,15 +9,9 @@ export const showDatabase = (name) => {
   return (dispatch, getState) => {
     const appRegistry = getState().appRegistry;
     if (appRegistry) {
-      const namespaceStore = appRegistry.getStore(NAMESPACE_STORE);
-      const collectionStore = appRegistry.getStore(COLLECTION_STORE);
-      if (namespaceStore.ns !== name) {
-        const ipc = require('hadron-ipc');
-        collectionStore.setCollection({});
-        namespaceStore.ns = name;
-        dispatch(appRegistryEmit('select-database', name));
-        ipc.call('window:hide-collection-submenu');
-      }
+      const ipc = require('hadron-ipc');
+      dispatch(appRegistryEmit('select-database', name));
+      ipc.call('window:hide-collection-submenu');
     }
   };
 };
