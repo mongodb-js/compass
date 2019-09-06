5daded6e748f038af0c82cf150c6b754e4753aed
Durran Jordan <durran@gmail.com>
2019-09-06 13:39:05 +0100

    Update the root index for exports

--- packages/compass-loading/src/index.js
+++ packages/compass-loading/src/index.js
@@ -1,40 +1,5 @@
 import LoadingPlugin from './plugin';
-import LoadingStore from 'stores';
-
-/**
- * A sample role for the component.
- */
-const ROLE = {
-  name: 'Loading',
-  component: LoadingPlugin
-};
-
-/**
- * Activate all the components in the Loading package.
- * @param {Object} appRegistry - The Hadron appRegisrty to activate this plugin with.
- **/
-function activate(appRegistry) {
-  // Register the LoadingPlugin as a role in Compass
-  //
-  // Available roles are:
-  //   - Instance.Tab: { name: <String>, component: <React.Component>, order: <Number> }
-  //   - Database.Tab: { name: <String>, component: <React.Component>, order: <Number> }
-  //   - Collection.Tab: { name: <String>, component: <React.Component>, order: <Number> }
-  //   - CollectionHUD.Item: { name <String>, component: <React.Component> }
-  //   - Header.Item: { name: <String>, component: <React.Component>, alignment: <String> }
-
-  appRegistry.registerRole('Loading.Screen', ROLE);
-  appRegistry.registerStore('Loading.Store', LoadingStore);
-}
-
-/**
- * Deactivate all the components in the Loading package.
- * @param {Object} appRegistry - The Hadron appRegisrty to deactivate this plugin with.
- **/
-function deactivate(appRegistry) {
-  appRegistry.deregisterRole('Loading.Screen', ROLE);
-  appRegistry.deregisterStore('Loading.Store');
-}
+import configureStore, { CHANGE_STATUS } from 'stores';
 
 export default LoadingPlugin;
-export { activate, deactivate };
+export { configureStore, CHANGE_STATUS };
