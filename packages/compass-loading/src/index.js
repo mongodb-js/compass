4c6767f8fae1d45715a97de3f335c49223d789e9
Durran Jordan <durran@gmail.com>
2019-10-28 15:06:50 +0100

    Adding in activate/deactivate methods for load

--- packages/compass-loading/src/index.js
+++ packages/compass-loading/src/index.js
@@ -1,5 +1,19 @@
 import LoadingPlugin from './plugin';
 import configureStore, { CHANGE_STATUS } from 'stores';
 
+/**
+ * Activate all the components in the Aggregations package.
+ * @param {Object} appRegistry - The Hadron appRegisrty to activate this plugin with.
+ **/
+const activate = () => {
+};
+
+/**
+ * Deactivate all the components in the Aggregations package.
+ * @param {Object} appRegistry - The Hadron appRegisrty to deactivate this plugin with.
+ **/
+const deactivate = () => {
+};
+
 export default LoadingPlugin;
 export { configureStore, CHANGE_STATUS };
