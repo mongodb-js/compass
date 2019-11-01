f423921399c83d6eb8cf581db209e76867dbce5a
Joy Sampoonachot <Joy.sampoonachot@gmail.com>
2019-11-01 12:49:06 -0400

    connect-ui-tweaks: Cleaned up spacing between components (#76)
    * cleaned up spacing between components
    
    * added back in the banner styling for connection alerts
    
    * set min and max width on form container and atlas callout

--- packages/compass-connect/src/components/form/form-actions.jsx
+++ packages/compass-connect/src/components/form/form-actions.jsx
@@ -89,8 +89,8 @@ class FormActions extends React.Component {
     return (
       <div className={classnames(styles['unsaved-message-actions'])}>
         You have unsaved changes.
-        <a onClick={this.onFavoriteChangeDiscarded}>[discard]</a>
-        <a onClick={this.onSaveFavoriteClicked}>[save changes]</a>
+        <a onClick={this.onFavoriteChangeDiscarded}>[Discard]</a>
+        <a onClick={this.onSaveFavoriteClicked}>[Save changes]</a>
       </div>
     );
   }
