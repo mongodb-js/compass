17164252f20e2f53eb129f0f5ed00f02b031c857
Durran Jordan <durran@gmail.com>
2019-12-05 12:58:56 +0100

    COMPASS-3967: Migrate without segfault

--- src/app/migrations/connection-disk.js
+++ src/app/migrations/connection-disk.js
@@ -1,12 +1,13 @@
-const Connection = require('./legacy-connection');
-const Collection = require('ampersand-rest-collection');
+const Connection = require('mongodb-connection-model');
 const storageMixin = require('storage-mixin');
 
 let appName;
+let basepath;
 
 try {
   const electron = require('electron');
   appName = electron.remote ? electron.remote.app.getName() : undefined;
+  basepath = electron.remote ? electron.remote.app.getPath('userData') : undefined;
 } catch (e) {
   /* eslint no-console: 0 */
   console.log('Could not load electron', e.message);
@@ -15,28 +16,22 @@ try {
 /**
  * Configuration for connecting to a MongoDB Deployment.
  */
-const ConnectionIndexedDB = Connection.extend(storageMixin, {
+const ConnectionDisk = Connection.extend(storageMixin, {
   idAttribute: '_id',
   namespace: 'Connections',
   storage: {
-    backend: 'local',
-    appName: appName
+    backend: 'disk',
+    namespace: 'Connections',
+    appName: appName,
+    basepath
   },
   serialize: function() {
     return Connection.prototype.serialize.call(this, {
       all: true
     });
+  },
+  validate: function() {
   }
 });
 
-module.exports = ConnectionIndexedDB;
-
-module.exports.ConnectionIndexedDBCollection = Collection.extend(storageMixin, {
-  model: ConnectionIndexedDB,
-  namespace: 'Connections',
-  storage: {
-    backend: 'local',
-    appName: appName
-  },
-  mainIndex: '_id'
-});
+module.exports = ConnectionDisk;
