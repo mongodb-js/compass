81a497cc119bd90961260b222c895f9fa0e7b50f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2020-01-29 14:18:01 -0500

    fix: normalize webpack config against agg plugin. minify is broken (#169)

--- packages/compass-crud/config/webpack.prod.config.js
+++ packages/compass-crud/config/webpack.prod.config.js
@@ -1,20 +1,14 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const baseWebpackConfig = require('./webpack.base.config');
 const project = require('./project');
 
-const GLOBALS = {
-  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
-};
-
 const config = {
+  mode: 'production',
   target: 'electron-renderer',
   devtool: false,
   entry: {
@@ -33,35 +27,25 @@ const config = {
     rules: [
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
-        use: [{
-          loader: 'file-loader',
-          // In prod we need to go to $COMPASS_HOME/node_modules/<plugin>/lib or
-          // $USER_HOME/.mongodb/compasss(-community)/plugins
-          //
-          // @note This currently does not work in published plugin.
-          query: {
-            name: 'assets/images/[name]__[hash:base64:5].[ext]',
-            publicPath: function(file) {
-              return `../../node_modules/@mongodb-js/compass-schema/lib/${file}`;
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 500000
             }
           }
-        }]
+        ]
       },
       {
         test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-          // In prod we need to go to $COMPASS_HOME/node_modules/<plugin>/lib or
-          // $USER_HOME/.mongodb/compasss(-community)/plugins
-          //
-          // @note This currently does not work in published plugin.
-          query: {
-            name: 'assets/images/[name]__[hash:base64:5].[ext]',
-            publicPath: function(file) {
-              return path.join(__dirname, '..', 'lib', file);
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 500000
             }
           }
-        }]
+        ]
       }
     ]
   },
@@ -70,28 +54,14 @@ const config = {
     // so that the external vendor JavaScript is not part of our compiled bundle
     new PeerDepsExternalsPlugin(),
 
-    // Do not emit compiled assets that include errors
-    new webpack.NoEmitOnErrorsPlugin(),
-
     // Configure Extract Plugin for dependent global styles into a single CSS file
     new ExtractTextPlugin({
       filename: 'assets/css/index.css',
       allChunks: true,
       ignoreOrder: true // When using CSS modules import order of CSS no longer needs to be preserved
     }),
-
-    // Defines global variables
-    new webpack.DefinePlugin(GLOBALS),
-
     // Creates HTML page for us at build time
-    new HtmlWebpackPlugin(),
-
-    // An ES6+ aware minifier, results in smaller output compared to UglifyJS given that
-    // Chromium in electron supports the majority of ES6 features out of the box.
-    new MinifyPlugin()
-
-    // Uncomment to Analyze the output bundle size of the plugin. Useful for optimizing the build.
-    // new BundleAnalyzerPlugin()
+    new HtmlWebpackPlugin()
   ],
   stats: {
     colors: true,
