8442e706af04416f28ec6fc37a647df848bdf44f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2020-02-03 15:45:53 -0500

    chore: Update deps (#14)
    * chore: update deps
    
    ```
     mocha                  ^7.0.0  →    ^7.0.1
     mongodb-js-precommit   ^2.1.0  →    ^2.2.0
     babel-register         ^6.7.2  →   ^6.26.0
     hadron-app-registry    ^7.1.0  →    ^7.2.0
     less                   ^2.7.2  →   ^3.10.3
     mongodb-data-service  ^12.3.0  →   ^16.5.0
     prop-types            ^15.5.8  →   ^15.7.2
    ```
    
    * cleanup
    
    * chore: normalize travis
    
    * fixing travis

--- packages/hadron-app/test/index.test.js
+++ packages/hadron-app/test/index.test.js
@@ -1,12 +1,29 @@
 const app = require('../');
 const expect = require('chai').expect;
 
+const babel = require('babel-register');
+const appRegistry = require('hadron-app-registry');
+const less = require('less');
+const dataService = require('mongodb-data-service');
+const propTypes = require('prop-types');
+const react = require('react');
+const reactDOM = require('react-dom');
+
 
 describe('hadron-app', function() {
   describe('extend', function() {
     it('should be extenable', function() {
       app.extend({foo: 'bar'});
       expect(app).to.have.property('foo', 'bar');
+    });
+    it('should have peer deps as dev deps', function() {
+      expect(babel).to.not.be.null;
+      expect(appRegistry).to.not.be.null;
+      expect(less).to.not.be.null;
+      expect(dataService).to.not.be.null;
+      expect(propTypes).to.not.be.null;
+      expect(react).to.not.be.null;
+      expect(reactDOM).to.not.be.null;
     });
   });
 });
