6bb06024cc2e5d579e46bd14e504e5bc8d2df7f6
Durran Jordan <durran@gmail.com>
2020-03-17 19:39:04 +0100

    Fix Atlas test

--- packages/compass-deployment-awareness/src/stores/index.js
+++ packages/compass-deployment-awareness/src/stores/index.js
@@ -32,11 +32,8 @@ const DeploymentAwarenessStore = Reflux.createStore({
     this.appRegistry = appRegistry;
     appRegistry.on('data-service-initialized', this.onDataServiceInitialized.bind(this));
     appRegistry.on('instance-refreshed', (state) => {
-      console.log('instance-refreshed', state);
-      const isAtlas = !!state.instance._id.match(ATLAS);
-      console.log('isAtlas', isAtlas);
+      const isAtlas = !!state.instance._id.match(ATLAS_REGEX);
       const isDataLake = state.instance.dataLake && state.instance.dataLake.isDataLake;
-      console.log('isDataLake', isDataLake);
       if (isAtlas && !isDataLake) {
         this.setState({ isDataLake: false, env: ATLAS });
       } else if (isDataLake) {
@@ -61,7 +58,6 @@ const DeploymentAwarenessStore = Reflux.createStore({
    * @param {Event} evt - The topologyDescriptionChanged event.
    */
   topologyDescriptionChanged(evt) {
-    console.log('topology changed', evt);
     const newDescription = evt.newDescription;
     const servers = [];
     for (const desc of newDescription.servers.values()) {
@@ -72,7 +68,6 @@ const DeploymentAwarenessStore = Reflux.createStore({
       });
     }
     if (this.state.topologyType !== newDescription.type) {
-      console.log('emitting', this.state);
       this.appRegistry.emit(
         'compass:deployment-awareness:topology-changed',
         {
