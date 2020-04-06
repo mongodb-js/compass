b852d075b0371137b6766abdff5989ab9f0376c5
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2020-04-06 10:32:19 -0400

    fix: less should be a devDependency (#32)
    * fix: less should be a devDependency
    
    Currently causing compass builds on windows to fail. Appears one of less's underlying deps update introducing the below:
    
    ```
    [2020/04/03 17:54:40.550] 2020-04-03T17:54:40.550Z check-max-path 2 paths too long!
    [2020/04/03 17:54:40.550] 2020-04-03T17:54:40.550Z check-max-path paths that are too long:  [
    [2020/04/03 17:54:40.550]   'C:\\data\\mci\\f8d375254475f97179b344bb6912df63\\src\\dist\\MongoDBCompassIsolatedEditionBeta-win32-x64\\resources\\app.asar.unpacked\\node_modules\\@mongodb-js\\compass-serverstats\\node_modules\\less\\packages\\css-parser\\node_modules\\chevrotain\\diagrams\\vendor\\railroad-diagrams.js',
    [2020/04/03 17:54:40.550]   'C:\\data\\mci\\f8d375254475f97179b344bb6912df63\\src\\dist\\MongoDBCompassIsolatedEditionBeta-win32-x64\\resources\\app.asar.unpacked\\node_modules\\@mongodb-js\\compass-serverstats\\node_modules\\less\\packages\\less\\node_modules\\chevrotain\\diagrams\\vendor\\railroad-diagrams.js'
    [2020/04/03 17:54:40.550] ]
    [2020/04/03 17:54:40.551] ❌ 2 paths too long!
    [2020/04/03 17:54:40.551] - (269) C:\data\mci\f8d375254475f97179b344bb6912df63\src\dist\MongoDBCompassIsolatedEditionBeta-win32-x64\resources\app.asar.unpacked\node_modules\@mongodb-js\compass-serverstats\node_modules\less\packages\css-parser\node_modules\chevrotain\diagrams\vendor\railroad-diagrams.js C:\data\mci\f8d375254475f97179b344bb6912df63\src\dist\MongoDBCompassIsolatedEditionBeta-win32-x64\resources\app.asar.unpacked\node_modules\@mongodb-js\compass-serverstats\node_modules\less\packages\css-parser\node_modules\chevrotain\diagrams\vendor\railroad-diagrams.js
    [2020/04/03 17:54:40.551] - (263) C:\data\mci\f8d375254475f97179b344bb6912df63\src\dist\MongoDBCompassIsolatedEditionBeta-win32-x64\resources\app.asar.unpacked\node_modules\@mongodb-js\compass-serverstats\node_modules\less\packages\less\node_modules\chevrotain\diagrams\vendor\railroad-diagrams.js C:\data\mci\f8d375254475f97179b344bb6912df63\src\dist\MongoDBCompassIsolatedEditionBeta-win32-x64\resources\app.asar.unpacked\node_modules\@mongodb-js\compass-serverstats\node_modules\less\packages\less\node_modules\chevrotain\diagrams\vendor\railroad-diagrams.js
    [2020/04/03 17:54:40.563] Command failed: command encountered problem: error waiting on process 'bcdaa49c-e674-4734-8793-55d75c7c9fdf': exit status 1
    ```
    
    Including some other cleanup:
    
    - `mongodb-core` is unused
    - `mongodb-js-fmt` is deprecated
    -  update to latest travis tpl
    - `ncu -u mocha mock-require mongodb-data-service mongodb-js-precommit`
    - debug@4
    
    * fix some more stuff
    
    * Remove duplicate debug dependency
    
    Co-authored-by: Durran Jordan <durran@gmail.com>

--- packages/compass-serverstats/electron/main/index.js
+++ packages/compass-serverstats/electron/main/index.js
@@ -11,7 +11,7 @@ const SRC_DIR = path.join(ROOT_DIR, 'src');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
+  mainWindow = new BrowserWindow({ width: 1200, height: 800, webPreferences: {nodeIntegration: true }});
   mainWindow.loadURL(`file://${__dirname}/../renderer/index.html`);
   mainWindow.on('closed', () => {
     mainWindow = null;
