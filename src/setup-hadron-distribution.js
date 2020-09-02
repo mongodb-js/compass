b87e06b95a1e38282a31cb7e836508a07718bb57
Rhys <Anemy@users.noreply.github.com>
2020-09-02 15:18:41 +0200

    Cherry pick for ga 1.22.0 (#2005)
    * Bump mongosh compass-shell to 0.2.2 (#2004)
    
    * Remove shell from readonly (#1989)
    
    * COMPASS-4131: remove compass community (#1982)

--- src/setup-hadron-distribution.js
+++ src/setup-hadron-distribution.js
@@ -2,7 +2,7 @@ const pkg = require('../package.json');
 
 /**
  * @note: HADRON_DISTRIBUTION is set via command line args in dev, for example:
- * npm start compass-community
+ * npm start compass-readonly
  */
 if (!process.env.HADRON_DISTRIBUTION) {
   const distribution = pkg.distribution || pkg.config.hadron.distributions.default;
