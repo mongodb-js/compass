d33a31cd586363c873ad8395e6745b96eaa2d435
mcasimir <maurizio.cas@gmail.com>
2020-11-12 10:29:49 +0100

    add cdn urls

--- scripts/download-akzidenz.js
+++ scripts/download-akzidenz.js
@@ -1,7 +1,36 @@
 const path = require('path');
 const download = require('download');
 
-const AKZIDENZ_CDN_URLS = [];
+const AKZIDENZ_CDN_URLS = [
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdita.eot',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdita.svg',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdita.ttf',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdita.woff',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdlig.eot',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdlig.svg',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdlig.ttf',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdlig.woff',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdligcnd.eot',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdligcnd.svg',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdligcnd.ttf',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdligcnd.woff',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdligita.eot',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdligita.svg',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdligita.ttf',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdligita.woff',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdmed.eot',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdmed.svg',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdmed.ttf',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdmed.woff',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdmedita.eot',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdmedita.svg',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdmedita.ttf',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdmedita.woff',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdreg.eot',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdreg.svg',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdreg.ttf',
+  'https://d2va9gm4j17fy9.cloudfront.net/fonts/akzidgrostdreg.woff'
+];
 
 const FONTS_DIRECTORY = path.resolve(
   __dirname,
@@ -12,9 +41,8 @@ const FONTS_DIRECTORY = path.resolve(
 );
 
 (async() => {
-  console.log('Downloading akzidenz fonts.', []);
+  console.log('Downloading akzidenz fonts.', AKZIDENZ_CDN_URLS);
 
   await Promise.all(
     AKZIDENZ_CDN_URLS.map(url => download(url, FONTS_DIRECTORY)));
 })();
-
