47f084d3ef2e53cadf2fc928108bb7f8e4663537
Maurizio Casimirri <maurizio.cas@gmail.com>
2021-02-03 18:22:14 +0100

    fix: allow port input to be empty COMPASS-4607 (#261)
    * Update port-input.jsx
    
    * Update port-input.jsx
    
    * Update port-input.jsx
    
    * fix: allow empty port in input

--- packages/compass-connect/src/components/form/port-input.jsx
+++ packages/compass-connect/src/components/form/port-input.jsx
@@ -7,7 +7,14 @@ import FormInput from './form-input';
 class PortInput extends React.PureComponent {
   static displayName = 'PortInput';
 
-  static propTypes = { port: PropTypes.number, isPortChanged: PropTypes.bool };
+  static propTypes = {
+    port: PropTypes.number // initial port
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { port: props.port };
+  }
 
   /**
    * Changes port.
@@ -15,32 +22,24 @@ class PortInput extends React.PureComponent {
    * @param {Object} evt - evt.
    */
   onPortChanged(evt) {
-    Actions.onPortChanged(+evt.target.value);
-  }
-
-  /**
-   * Gets port.
-   *
-   * @returns {Number} port.
-   */
-  getPort() {
-    if (this.props.isPortChanged === false) {
-      return 27017;
-    }
-
-    return +this.props.port;
+    this.setState({ port: evt.target.value });
+    Actions.onPortChanged(evt.target.value ? +evt.target.value : 27017);
   }
 
   render() {
+    if (!this.state) {
+      return;
+    }
+
     return (
       <FormInput
         label="Port"
         name="port"
         placeholder="27017"
         changeHandler={this.onPortChanged.bind(this)}
-        value={this.getPort()}
+        value={this.state.port}
         type="number"
-        otherInputAttributes={{min: 1, max: 65536}} />
+        otherInputAttributes={{ min: 1, max: 65536 }} />
     );
   }
 }
