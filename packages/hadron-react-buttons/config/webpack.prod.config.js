7407de06a887b5d106ec9b40983ee67a17ec01a9
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-04-29 11:47:16 +0200

    test(hadron-react-buttons): Compile before running tests; Add base webpack config; Ignore lib

--- packages/hadron-react-buttons/config/webpack.prod.config.js
+++ packages/hadron-react-buttons/config/webpack.prod.config.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const path = require('path');
 const PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin');
 
-const baseWebpackConfig = require('../../../config/webpack.base.config');
+const baseWebpackConfig = require('./webpack.base.config');
 
 const config = {
   mode: 'production',
