8d61cb6b802129ba59febd8454866e3dece1061e
Anemy <rhysh@live.com>
2021-05-19 18:14:41 -0400

    remove hadron-spectron package

--- packages/hadron-spectron/test/selector.test.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('chai');
-const { selector } = require('../');
-
-describe('#selector', () => {
-  it('returns the data-test-id selector', () => {
-    expect(selector('test')).to.equal("[data-test-id='test']");
-  });
-});
