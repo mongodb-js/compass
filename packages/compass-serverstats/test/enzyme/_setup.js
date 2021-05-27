83038d64021b8443ef1a73c74277e8b619634ef8
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-05-27 12:01:02 +0200

    test(@mongodb-js/compass-serverstats): Explicitly define files processed by babel/register to work around windows cwd issue

--- packages/compass-serverstats/test/enzyme/_setup.js
+++ packages/compass-serverstats/test/enzyme/_setup.js
@@ -5,8 +5,19 @@ const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 Enzyme.configure({ adapter: new Adapter() });
 
+const root = require('path').resolve(__dirname, '..', '..');
+
 require('@babel/register')({
-  root: require('path').resolve(__dirname, '..', '..')
+  root,
+  only: [
+    // To work around https://github.com/babel/babel/issues/10232#issuecomment-678429724
+    (filepath) => {
+      return String(filepath).startsWith(root);
+    }
+  ],
+  // Adding custom `only` requires manually provide a custom `ignore` (even
+  // though it matches the default one)
+  ignore: [/node_modules/]
 });
 
 const jsdom = require('jsdom').jsdom;
