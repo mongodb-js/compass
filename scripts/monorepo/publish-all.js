c3027f35bcbf4edd2b931e8422b3a8810788b14f
Maurizio Casimirri <maurizio.cas@gmail.com>
2021-05-27 14:20:41 +0200

    chore: add package publish scripts (#2210)

---
+++ scripts/monorepo/publish-all.js
@@ -0,0 +1,73 @@
+// Publishes all the unpublished packages in topological order.
+
+const childProcess = require('child_process');
+
+function isDirty() {
+  const stdout = childProcess.execSync('git status --porcelain');
+  return stdout.toString().trim().length > 0;
+}
+
+function getCurrentBranch() {
+  const stdout = childProcess.execSync('git rev-parse --abbrev-ref HEAD');
+  return stdout.toString().trim();
+}
+
+function fail(message) {
+  console.error(message);
+  process.exit(1);
+}
+
+const MAIN_BRANCH = 'master';
+
+function main() {
+  const currentBranch = getCurrentBranch();
+
+  if (currentBranch !== MAIN_BRANCH) {
+    fail('You can only publish from the main branch');
+  }
+
+  if (isDirty()) {
+    fail('The repo is not pristine');
+  }
+
+  const packages = JSON.parse(childProcess.execSync('lerna list --json --toposort'));
+  for (const package of packages) {
+    if (package.private) {
+      continue;
+    }
+
+    publishPackage(package);
+  }
+}
+
+function publishPackage({ location: packageLocation, name: packageName, version: packageVersion }) {
+  console.log('\n');
+  const packageNameAndVersion = `${packageName}@${packageVersion}`;
+
+  if (alreadyPublished(packageLocation, packageNameAndVersion)) {
+    console.log('Already published', packageNameAndVersion, 'skipping ...');
+    return;
+  }
+
+  console.log('Publishing package', packageNameAndVersion, '...');
+
+  installAndPublish(packageLocation);
+}
+
+function installAndPublish(packageLocation) {
+  childProcess.execSync('npm install', { cwd: packageLocation });
+  childProcess.execSync('npm publish', { cwd: packageLocation });
+}
+
+function alreadyPublished(packageLocation, packageNameAndVersion) {
+  try {
+    const stdout = childProcess.execSync(`npm view ${packageNameAndVersion} --json`, { cwd: packageLocation });
+    JSON.parse(stdout.toString().trim());
+    // if this command returns a json that can be parsed then the package exists
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+main();
