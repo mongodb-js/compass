eecf27011cdc1be22eefadee59faa0cc021b112a
mcasimir <maurizio.cas@gmail.com>
2021-05-28 14:38:23 +0200

    fix also package-lock

--- scripts/monorepo/fix-dev-version.js
+++ scripts/monorepo/fix-dev-version.js
@@ -1,5 +1,13 @@
 const fsExtra = require('fs-extra');
-const packageJsonPath = 'packages/compass/package.json';
-const packageJson = fsExtra.readJSONSync(packageJsonPath);
-packageJson.version = '0.0.0-dev.0';
-fsExtra.writeJSONSync(packageJsonPath, packageJson, {spaces: 2});
+
+fixVersion('packages/compass/package.json');
+fixVersion('packages/compass/package-lock.json');
+
+function fixVersion(jsonFilePath) {
+  if (!fsExtra.existsSync(jsonFilePath)) {
+    return;
+  }
+  const content = fsExtra.readJSONSync(jsonFilePath);
+  content.version = '0.0.0-dev.0';
+  fsExtra.writeJSONSync(jsonFilePath, content, {spaces: 2});
+}
