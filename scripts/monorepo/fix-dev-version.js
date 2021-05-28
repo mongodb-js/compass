1463377233ed92084b233b169604d6c4377340c7
Maurizio Casimirri <maurizio.cas@gmail.com>
2021-05-28 14:31:17 +0200

    Update scripts/monorepo/fix-dev-version.js
    Co-authored-by: Sergey <petushkov.sergey@gmail.com>

--- scripts/monorepo/fix-dev-version.js
+++ scripts/monorepo/fix-dev-version.js
@@ -1,5 +1,5 @@
 const fsExtra = require('fs-extra');
 const packageJsonPath = 'packages/compass/package.json';
-const packageJson = fsExtra.readJSONSync();
+const packageJson = fsExtra.readJSONSync(packageJsonPath);
 packageJson.version = '0.0.0-dev.0';
 fsExtra.writeJSONSync(packageJsonPath, packageJson, {spaces: 2});
