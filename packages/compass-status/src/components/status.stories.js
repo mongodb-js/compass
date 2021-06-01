e5830b8f8d688f6cbc6c7f83053fcf04fac93dcd
Sergey <petushkov.sergey@gmail.com>
2021-06-01 14:00:39 +0200

    chore: Clean-up repo in preparation for the bootstrap (#2218)
    * chore: Replace prepublish/prepublishOnly with prepare script for every package
    
    This helps during lerna bootstrap to ensure that every package has a dist created
    
    * chore: Remove deprecated 'react-storybook-addon-chapters' package from all packages
    
    It's deprecated and has npm engine set to ^3 in package.json and this breaks bootstrap
    
    * chore: Update react to the same version everywhere
    
    Helps to efficiently hoist the dependency upwards
    
    * chore: Clean-up accidentally left over babel config

--- packages/compass-status/src/components/status.stories.js
+++ packages/compass-status/src/components/status.stories.js
@@ -3,90 +3,34 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ComponentPreview from 'storybook/decorators/componentPreview';
-import { withChaptersOptions } from 'constants/storybook';
 
 import Status from 'components/status';
 
 storiesOf('Status', module)
-  .addWithChapters('Hidden', {
-    chapters: [
-      {
-        sections: [
-          {
-            sectionFn: () => (
-              <ComponentPreview light>
-                <Status />
-              </ComponentPreview>
-            ),
-            options: withChaptersOptions
-          }
-        ]
-      }
-    ]
-  })
-  .addWithChapters('Progress Bar (50%)', {
-    chapters: [
-      {
-        sections: [
-          {
-            sectionFn: () => (
-              <ComponentPreview light>
-                <Status visible progressbar progress={50} />
-              </ComponentPreview>
-            ),
-            options: withChaptersOptions
-          }
-        ]
-      }
-    ]
-  })
-  .addWithChapters('Animation', {
-    chapters: [
-      {
-        sections: [
-          {
-            sectionFn: () => (
-              <ComponentPreview light>
-                <Status visible animation />
-              </ComponentPreview>
-            ),
-            options: withChaptersOptions
-          }
-        ]
-      }
-    ]
-  })
-  .addWithChapters('Animation w/ Message', {
-    chapters: [
-      {
-        sections: [
-          {
-            sectionFn: () => (
-              <ComponentPreview light>
-                <Status visible animation message="Loading" />
-              </ComponentPreview>
-            ),
-            options: withChaptersOptions
-          }
-        ]
-      }
-    ]
-  })
-  .addWithChapters('Animation w/ Sidebar & Message', {
-    chapters: [
-      {
-        sections: [
-          {
-            sectionFn: () => (
-              <ComponentPreview light>
-                <Status visible animation sidebar message="Loading" />
-              </ComponentPreview>
-            ),
-            options: withChaptersOptions
-          }
-        ]
-      }
-    ]
-  });
+  .add('Hidden', () => (
+    <ComponentPreview light>
+      <Status />
+    </ComponentPreview>
+  ))
+  .add('Progress Bar (50%)', () => (
+    <ComponentPreview light>
+      <Status visible progressbar progress={50} />
+    </ComponentPreview>
+  ))
+  .add('Animation', () => (
+    <ComponentPreview light>
+      <Status visible animation />
+    </ComponentPreview>
+  ))
+  .add('Animation w/ Message', () => (
+    <ComponentPreview light>
+      <Status visible animation message="Loading" />
+    </ComponentPreview>
+  ))
+  .add('Animation w/ Sidebar & Message', () => (
+    <ComponentPreview light>
+      <Status visible animation sidebar message="Loading" />
+    </ComponentPreview>
+  ));
 
 /* eslint-enable react/no-multi-comp */
