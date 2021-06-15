b57e55f255db07575c06d5bdf662fd6e62f9452f
mcasimir <maurizio.cas@gmail.com>
2021-06-15 18:30:01 +0200

    chore(monorepo): remove storybook

--- packages/compass-serverstats/stories/server-stats.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@kadira/storybook';
-import ServerStats from '../src/components/index';
-
-storiesOf('ServerStats', module)
-  .add('default', () => <ServerStats interval={10}/>);
