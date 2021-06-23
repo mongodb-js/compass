3765c1e6472d559e2dae9883f57dd34f1a37c4e4
Rhys <Anemy@users.noreply.github.com>
2021-06-23 16:12:00 -0400

    fix(compass-sidebar): Show collection icon on read only collections (that aren't views) (#2294)

---
+++ packages/compass-sidebar/src/components/collection-type-icon/collection-type-icon.jsx
@@ -0,0 +1,48 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from '@leafygreen-ui/icon';
+
+import { TIME_SERIES_COLLECTION_TYPE } from '../../modules/collection';
+
+import styles from './collection-type-icon.less';
+
+const VIEW_COLLECTION_TYPE = 'view';
+
+function CollectionTypeIcon({
+  collectionType
+}) {
+  if (collectionType === TIME_SERIES_COLLECTION_TYPE) {
+    return (
+      <Icon
+        className={styles['compass-sidebar-collection-type-icon']}
+        glyph="TimeSeries"
+        title="Time-Series Collection"
+      />
+    );
+  }
+
+  if (collectionType === VIEW_COLLECTION_TYPE) {
+    return (
+      <Icon
+        className={styles['compass-sidebar-collection-type-icon']}
+        glyph="Visibility"
+        title="Read-only View"
+      />
+    );
+  }
+
+  return (
+    <Icon
+      className={styles['compass-sidebar-collection-type-icon']}
+      glyph="Folder"
+      title="Collection"
+    />
+  );
+}
+
+CollectionTypeIcon.propTypes = {
+  collectionType: PropTypes.string.isRequired
+};
+
+export default CollectionTypeIcon;
