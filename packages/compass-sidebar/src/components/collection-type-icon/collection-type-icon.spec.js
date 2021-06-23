323ed1abbec447527f2b3a5ac7e6fcf5333a2718
Anemy <rhysh@live.com>
2021-06-23 15:23:33 -0400

    Use collection type to decide which icon to show for views

---
+++ packages/compass-sidebar/src/components/collection-type-icon/collection-type-icon.spec.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Icon from '@leafygreen-ui/icon';
+
+import CollectionTypeIcon from '../collection-type-icon';
+
+describe('CollectionTypeIcon [Component]', () => {
+  let component;
+
+  describe('default collection type', () => {
+    beforeEach(() => {
+      component = mount(<CollectionTypeIcon
+        collectionType="collection"
+      />);
+    });
+
+    it('has a collection type icon folder', () => {
+      expect(component.find(Icon).props().glyph).to.equal('Folder');
+    });
+  });
+
+  describe('time-series collection type', () => {
+    beforeEach(() => {
+      component = mount(<CollectionTypeIcon
+        collectionType="timeseries"
+      />);
+    });
+
+    it('has a time series icon', () => {
+      expect(component.find(Icon).props().glyph).to.equal('TimeSeries');
+    });
+  });
+
+  describe('view collection type', () => {
+    beforeEach(() => {
+      component = mount(<CollectionTypeIcon
+        collectionType="view"
+      />);
+    });
+
+    it('has a view icon', () => {
+      expect(component.find(Icon).props().glyph).to.equal('Visibility');
+    });
+  });
+});
