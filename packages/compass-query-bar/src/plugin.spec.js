b3bbefd0e8c7c15b40cb98b3f1555160af081af6
Anemy <rhysh@live.com>
2021-07-23 19:22:42 -0400

    Add test for query bar updating

--- packages/compass-query-bar/src/plugin.spec.js
+++ packages/compass-query-bar/src/plugin.spec.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { StoreConnector } from 'hadron-react-components';
+
 import QueryBarPlugin from './plugin';
 import configureStore from './stores';
 import configureActions from './actions';
+import OptionEditor from './components/option-editor';
 
 describe('QueryBar [Plugin]', () => {
-  let component;
   let store;
   let actions;
 
@@ -15,16 +16,52 @@ describe('QueryBar [Plugin]', () => {
     store = configureStore({
       actions: actions
     });
-    component = shallow(<QueryBarPlugin store={store} actions={actions} />);
   });
 
   afterEach(function() {
     actions = null;
     store = null;
-    component = null;
   });
 
   it('should contain a <StoreConnector /> with a store prop', function() {
+    const component = shallow(<QueryBarPlugin store={store} actions={actions} />);
     expect(component.find(StoreConnector).first().props('store')).to.be.an('object');
+  });
+
+  describe('when an input occurs and find is clicked', function() {
+    it('it calls onApply and update the store with the query', async function() {
+      store.setState({
+        valid: false,
+        filterString: 'a',
+        filterValid: false
+      });
+
+      let calledApply = false;
+      const component = mount(
+        <QueryBarPlugin
+          store={store}
+          actions={actions}
+          layout={['filter']}
+          onApply={() => {
+            calledApply = true;
+          }}
+        />
+      );
+
+      // Set the ace editor input value.
+      component.find(OptionEditor).instance().editor.session.setValue(
+        '{a: 3}'
+      );
+
+      expect(store.state.valid).to.equal(true);
+      expect(store.state.filterString).to.equal('{a: 3}');
+
+      // Click the filter button.
+      component.find(
+        {'data-test-id': 'query-bar-apply-filter-button'}
+      ).props().onClick();
+
+      expect(calledApply).to.equal(true);
+    });
   });
 });
