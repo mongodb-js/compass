a857160b65908f90b0ada892b6efe8765cb6ee27
Anemy <rhysh@live.com>
2021-07-26 08:52:45 -0700

    add react library tests, axe a11y tests

--- packages/compass-components/src/components/confirmation-modal/confirmation-modal.spec.tsx
+++ packages/compass-components/src/components/confirmation-modal/confirmation-modal.spec.tsx
@@ -1,36 +1,61 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import { act, fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { axe, toHaveNoViolations } from 'jest-axe';
 
 import ConfirmationModal from './';
 
-describe('App', () => {
-  beforeEach(function() {
-    render(
-      <ConfirmationModal
-        title="modal title"
-        buttonText="ok"
+expect.extend(toHaveNoViolations);
+
+function renderModal() {
+  return render(
+    <ConfirmationModal
+      title="Pineapples"
+      buttonText="Confirm"
+      open
+    >
+      <div
+        data-testid="testing-inner-content"
       >
-        <div
-          role="testing-inner-content"
-        >
-          inner content
-        </div>
-      </ConfirmationModal>
-    );
+        inner content
+      </div>
+    </ConfirmationModal>
+  );
+}
+
+describe('ConfirmationModal Component', () => {
+  describe('a11y', () => {
+    it('does not have basic accessibility issues', async () => {
+      const { container, getByText } = renderModal();
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+
+      let newResults;
+      act(() => {
+        fireEvent.click(getByText('Confirm'))
+      });
+      await act(async () => {
+        newResults = await axe(container);
+      });
+      expect(newResults).toHaveNoViolations();
+    });
   });
 
   it('should show the modal heading', () => {
-    expect(screen.getByRole('heading')).toHaveTextContent('modal title');
-    expect(screen.getByRole('button')).toHaveTextContent('ok')
+    renderModal();
+    expect(screen.getByRole('heading')).toHaveTextContent('Pineapples');
   });
 
   it('should show the modal button', () => {
-    expect(screen.getByRole('button')).toHaveTextContent('ok');
-    expect(screen.getByRole('button')).not.toBeDisabled();
+    renderModal();
+    const button = screen.getByText('Confirm').closest('button');
+    expect(button).not.toBeDisabled();
   });
 
   it('should show the modal content', () => {
-    expect(screen.getByRole('testing-inner-content')).toHaveTextContent('inner content')
+    renderModal();
+    const innerContent = screen.getByTestId('testing-inner-content');
+    expect(innerContent).toHaveTextContent('inner content');
+    expect(innerContent).toBeVisible();
   });
 });
