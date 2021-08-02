413087f65008b748d1f6756d71d27a80974d16ee
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-08-02 15:31:37 +0200

    feat(depalign): Refactor depalign script to allow for autofix behavior

--- scripts/depalign.js
+++ scripts/depalign.js
@@ -1,21 +1,22 @@
-const childProcess = require('child_process');
 const path = require('path');
-const fsExtra = require('fs-extra');
 const semver = require('semver');
 const chalk = require('chalk');
+const { runInDir } = require('./run-in-dir');
+const { updatePackageJson } = require('./monorepo/update-package-json');
+const { withProgress } = require('./monorepo/with-progress');
 
 const depalignrc = require('../.depalignrc.json');
 
 const USAGE = `Check for dependency alignment issues.
 
-USAGE: depalign.js [--skip-deduped|--json|--fix]
+USAGE: depalign.js [--skip-deduped|--json|--autofix]
 
 Options:
 
---skip-deduped  don't output warnings for ranges that can be resolve to a single version.
---json          output a json report
---suggest-fixes output a list of replacements to normalize ranges and align everything
-                to the highest possible range
+  --skip-deduped                     Don't output warnings and don't autofix ranges that can be resolved to a single version.
+  --json                             Output a json report
+  --autofix                          Output a list of replacements to normalize ranges and align everything to the highest possible range
+  --dangerously-include-mismatched   Include mismatched dependencies into autofix
 
 .depalignrc.json
 
@@ -24,43 +25,148 @@ Options:
 For example: {"ignore": {"async": ["^1.2.3"]}}.
 `;
 
-const LERNA_BIN = path.resolve(
-  __dirname,
-  '..',
-  'node_modules',
-  '.bin',
-  'lerna'
-);
+const ROOT_DIR = path.resolve(__dirname, '..');
 
-const ROOT_PACKAGE_JSON_PATH = path.dirname(require.resolve('../package.json'));
+const LERNA_BIN = path.join(ROOT_DIR, 'node_modules', '.bin', 'lerna');
+
+async function main(args) {
+  if (args.includes('--help')) {
+    console.log(USAGE);
+    return;
+  }
+
+  const outputJson = args.includes('--json');
+  const shouldApplyFixes = args.includes('--autofix');
+  const includeDeduped = !args.includes('--skip-deduped');
+  const includeMismatched = args.includes('--dangerously-include-mismatched');
+
+  const workspaces = await collectWorkspacesMeta();
+  const dependencies = collectWorkspacesDependencies(workspaces);
+  const report = generateReport(dependencies, depalignrc);
+
+  if (!includeDeduped) {
+    report.deduped = new Map();
+  }
+
+  if (shouldApplyFixes) {
+    if (includeMismatched) {
+      console.log();
+      console.log(
+        `%s: You are about to update mismatched dependencies which might potentially be a %s. Please make sure that everything is still working as expected after the update.`,
+        chalk.yellow('Warning'),
+        chalk.bold('breaking change')
+      );
+      console.log();
+    }
+
+    await withProgress(
+      `Applying autofixes`,
+      applyFixes,
+      report,
+      includeMismatched
+    );
+
+    console.log();
+  }
+
+  if (outputJson) {
+    console.log(
+      JSON.stringify(
+        report,
+        (_key, val) => {
+          if (val instanceof Map) {
+            return Object.fromEntries(val);
+          } else if (val instanceof Set) {
+            return Array.from(val);
+          } else {
+            return val;
+          }
+        },
+        2
+      )
+    );
+  } else {
+    prettyPrintReport(report);
+  }
+
+  process.exitCode = report.mismatched.size;
+}
+
+async function collectWorkspacesMeta() {
+  const workspaces = JSON.parse(
+    (await runInDir(`${LERNA_BIN} list --all --json --toposort`)).stdout
+  );
+
+  return new Map(
+    workspaces
+      .concat({ location: ROOT_DIR })
+      .map(({ location }) => [
+        location,
+        require(path.join(location, 'package.json'))
+      ])
+  );
+}
+
+function collectWorkspacesDependencies(workspaces) {
+  const dependencies = new Map();
+
+  for (const [location, packageJson] of workspaces) {
+    for (const [dependency, versionRange] of [
+      ...Object.entries(packageJson.dependencies || {}),
+      ...Object.entries(packageJson.devDependencies || {}),
+      ...filterOutStarDeps(Object.entries(packageJson.peerDependencies || {})),
+      ...filterOutStarDeps(
+        Object.entries(packageJson.optionalDependencies || {})
+      )
+    ]) {
+      if (dependencies.has(dependency)) {
+        dependencies
+          .get(dependency)
+          .push({ version: versionRange, from: location });
+      } else {
+        dependencies.set(dependency, [
+          { version: versionRange, from: location }
+        ]);
+      }
+    }
+  }
+
+  return dependencies;
+}
 
 function filterOutStarDeps(entries) {
   return entries.filter(([, v]) => v !== '*');
 }
 
-function collectDependencies() {
-  const packages = JSON.parse(childProcess.execSync(`${LERNA_BIN} list --all --json --toposort`));
-  const dependencies = {};
+function generateReport(dependencies, { ignore = {} } = { ignore: {} }) {
+  const report = { mismatched: new Map(), deduped: new Map() };
 
-  for (const packageInfo of [...packages, {location: ROOT_PACKAGE_JSON_PATH}]) {
-    const packageJsonPath = path.join(packageInfo.location, 'package.json');
-    const packageJson = fsExtra.readJSONSync(packageJsonPath);
-    for (const [dependency, versionRange] of [
-      ...Object.entries(packageJson.dependencies || {}),
-      ...Object.entries(packageJson.devDependencies || {}),
-      ...filterOutStarDeps(Object.entries(packageJson.peerDependencies || {})),
-      ...filterOutStarDeps(Object.entries(packageJson.optionalDependencies || {}))
-    ]) {
-      dependencies[dependency] = dependencies[dependency] || [];
-      dependencies[dependency].push(versionRange);
+  for (const [depName, versions] of dependencies) {
+    const ignoredVersions = new Set(ignore[depName] || []);
+    const notIgnoredVersions = versions.filter(
+      ({ version }) => !ignoredVersions.has(version)
+    );
+    const notIgnoredUniqueVersionsOnly = Array.from(
+      new Set(notIgnoredVersions.map(({ version }) => version))
+    );
+
+    if (notIgnoredUniqueVersionsOnly.length <= 1) {
+      continue;
+    }
+
+    const reportItem = {
+      versions: notIgnoredVersions,
+      fixes: calculateReplacements(notIgnoredUniqueVersionsOnly)
+    };
+
+    if (!intersects(notIgnoredUniqueVersionsOnly)) {
+      report.mismatched.set(depName, reportItem);
+    } else {
+      report.deduped.set(depName, reportItem);
     }
   }
 
-  for (const [dependency, versionRanges] of Object.entries(dependencies)) {
-    dependencies[dependency] = Array.from(new Set(versionRanges));
-  }
-
-  return dependencies;
+  return report;
 }
 
 function intersects(range) {
@@ -78,113 +184,156 @@ function intersects(range) {
   return true;
 }
 
-function mismatchingError(name, ranges) {
-  console.log(chalk.red('mismatching:'), chalk.bold(name), ':', ranges.join(', '));
-}
-
-function dedupedWarning(name, ranges) {
-  console.log(chalk.yellow('deduped:'), chalk.bold(name), ':', ranges.join(', '));
-}
-
-function main(args) {
-  if (args.includes('--help')) {
-    console.log(USAGE);
-    process.exit(0);
-  }
-
-  const outputDeduped = !args.includes('--skip-deduped');
-  const outputJson = args.includes('--json');
-  const suggestFixes = args.includes('--suggest-fixes');
-
-  const report = {mismatching: [], deduped: []};
-
-  const dependencies = collectDependencies();
-
-  for (const [dependency, versionRanges] of Object.entries(dependencies)) {
-    const ignoredRanges = new Set((depalignrc.ignore || {})[dependency] || []);
-
-    const notIgnoredRanges = versionRanges.filter((range) => !ignoredRanges.has(range))
-    if (notIgnoredRanges.length <= 1) {
-      continue;
-    }
-
-    if (!intersects(notIgnoredRanges)) {
-      report.mismatching.push({name: dependency, ranges: notIgnoredRanges});
-    } else {
-      report.deduped.push({name: dependency, ranges: notIgnoredRanges});
-    }
-  }
-
-  if (suggestFixes) {
-    return outputFixes(report);
-  }
-
-  if (outputJson) {
-    console.log(JSON.stringify(report, null, 2));
-
-  } else {
-    for (const { name, ranges } of report.mismatching) {
-      mismatchingError(name, ranges);
-    }
-
-    if (outputDeduped) {
-      for (const { name, ranges } of report.deduped) {
-        dedupedWarning(name, ranges);
-      }
-    }
-  }
-
-  const exitCode = report.mismatching.length === 0 ? 0 : 1;
-  process.exit(exitCode);
-}
-
-function outputFixes({mismatching, deduped}) {
-  const deps = [...mismatching, ...deduped];
-  const replacements = {};
-  for (const dep of deps) {
-    replacements[dep.name] = calculateReplacements(dep.ranges);
-  }
-
-  console.log(JSON.stringify(replacements, null, 2));
-}
-
 function calculateReplacements(ranges) {
-  const replacements = {};
-  const validRanges = ranges.filter(range => semver.validRange(range) && range !== '*');
-  const sortedRanges = validRanges.sort(
-    (v1, v2) => {
-      const res = semver.compare(semver.minVersion(v2), semver.minVersion(v1))
+  const replacements = new Map();
+  const highestRange = getHighestRange(ranges);
 
-      if (res === 1 || res === -1) {
-        return res;
-      }
-
-      if (semver.valid(v1) && !semver.valid(v2)) {
-        return 1;
-      }
-
-      if (!semver.valid(v1) && semver.valid(v2)) {
-        return -1;
-      }
-
-      return 0;
-    }
-  );
-
-  if (ranges.includes('*') && sortedRanges.length) {
-    replacements['*'] = sortedRanges[0];
-  }
-
-  let refRange;
-  for (const range of sortedRanges) {
-    if (refRange && semver.subset(refRange, range)) {
-      replacements[range] = refRange;
-    } else {
-      refRange = range;
+  for (const range of ranges) {
+    if (semver.subset(highestRange, range)) {
+      replacements.set(range, highestRange);
     }
   }
 
   return replacements;
 }
+
+function getHighestRange(ranges) {
+  const validRanges = ranges.filter(
+    (range) => semver.validRange(range) && range !== '*'
+  );
+
+  const sortedRanges = validRanges.sort((v1, v2) => {
+    const res = semver.compare(semver.minVersion(v2), semver.minVersion(v1));
+
+    if (res === 1 || res === -1) {
+      return res;
+    }
+
+    if (semver.valid(v1) && !semver.valid(v2)) {
+      return 1;
+    }
+
+    if (!semver.valid(v1) && semver.valid(v2)) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  return sortedRanges[0] || null;
+}
+
+async function applyFixes({ deduped, mismatched }, includeMismatched = false) {
+  const spinner = this;
+  const spinnerText = spinner.text;
+
+  const updates = new Map([
+    ...deduped,
+    ...(includeMismatched ? mismatched : [])
+  ]);
+
+  if (updates.size === 0) {
+    return updates.size;
+  }
+
+  spinner.text = `${spinnerText} for ${updates.size} dependencies (collecting info)`;
+
+  const updatesByPackage = new Map();
+
+  for (const [depName, { versions, fixes }] of updates) {
+    deduped.delete(depName);
+    mismatched.delete(depName);
+
+    for (const { version, from } of versions) {
+      // Deduped versions will always have a fix, for mismatched we fall back to
+      // highest version (you need to opt in into this potentially breaking
+      // update so you should know what you're doing)
+      const fixVersion = fixes.get(version) || fixes.values().next().value;
+
+      if (updatesByPackage.has(from)) {
+        updatesByPackage.get(from).set(depName, fixVersion);
+      } else {
+        updatesByPackage.set(from, new Map([[depName, fixVersion]]));
+      }
+    }
+  }
+
+  spinner.text = `${spinnerText} for ${updates.size} dependencies (updating package.json)`;
+
+  for (const [location, updates] of updatesByPackage) {
+    await updatePackageJson(location, (pkgJson) => {
+      updates.forEach((version, name) => {
+        for (const depType of [
+          'dependencies',
+          'devDependencies',
+          'peerDependencies',
+          'optionalDependencies'
+        ]) {
+          if ((pkgJson[depType] || {})[name]) {
+            pkgJson[depType][name] = version;
+          }
+        }
+      });
+      return pkgJson;
+    });
+  }
+
+  spinner.text = `${spinnerText} for ${updates.size} dependencies (updating package-lock.json)`;
+
+  await runInDir('npm install --package-lock-only');
+
+  spinner.text = `${spinnerText} for ${updates.size} dependencies`;
+
+  return updates.size;
+}
+
+function prettyPrintReport({ deduped, mismatched }) {
+  function printReportItems(items) {
+    items.forEach(({ versions }, depName) => {
+      const versionPadStart = versions.reduce((longest, { version }) => {
+        return longest > version.length ? longest : version.length;
+      }, -Infinity);
+      console.log('  %s', chalk.bold(depName));
+      console.log();
+      versions.forEach(({ version, from }) => {
+        console.log(
+          '    %s%s %s',
+          ' '.repeat(versionPadStart - version.length),
+          version,
+          chalk.dim(`at ${path.relative(process.cwd(), from)}`)
+        );
+      });
+      console.log();
+    });
+  }
+
+  if (deduped.size > 0) {
+    console.log(
+      '%s %s',
+      chalk.bold.yellow('Deduped:'),
+      chalk.dim('(can be fixed with --autofix)')
+    );
+    console.log();
+    printReportItems(deduped);
+  }
+
+  if (mismatched.size > 0) {
+    console.log(chalk.bold.red('Mismatched:'));
+    console.log();
+    printReportItems(mismatched);
+  }
+
+  if (deduped.size === 0 && mismatched.size === 0) {
+    console.log(
+      '%s',
+      chalk.green('All dependencies are aligned, nothing to report!')
+    );
+  }
+}
+
+process.on('unhandledRejection', (err) => {
+  console.error();
+  console.error(err.stack || err.message || err);
+});
 
 main(process.argv.slice(2));
