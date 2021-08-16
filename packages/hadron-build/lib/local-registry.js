8c181c853bca23d52a25d1d1eefaf3f2ec5979cb
mcasimir <maurizio.cas@gmail.com>
2021-08-16 20:02:45 +0200

    wip

--- packages/hadron-build/lib/local-registry.js
+++ packages/hadron-build/lib/local-registry.js
@@ -1,46 +1,46 @@
 const path = require('path');
 const execa = require('execa');
-const { default: startServer } = require('verdaccio');
-
+const getPort = require('get-port');
+const waitOn = require('wait-on');
 const monorepoPath = path.dirname(require.resolve('../../../package.json'));
 
-const config = {
-  'storage': './storage',
-  'uplinks': {
-    'npmjs': {
-      'url': 'https://registry.npmjs.org/'
-    }
-  },
-  'packages': {
-    '@*/*': {
-      'access': '$all',
-      'publish': '$all',
-      'proxy': 'npmjs'
-    },
-    '**': {
-      'access': '$all',
-      'publish': '$all',
-      'proxy': 'npmjs'
-    }
-  },
-  'logs': {
-    'type': 'stdout',
-    'level': 'error'
+function startServer(port) {
+  return execa('npx', [
+    'verdaccio',
+    '--config', path.resolve(__dirname, 'verdaccio.yaml'),
+    '--listen', port
+  ], { cwd: monorepoPath, stdio: 'inherit' });
+}
+
+async function lernaPublish(registryAddress) {
+  await execa('npx', [
+    'lerna', 'publish', 'from-package',
+    '--ignore-scripts',
+    '--registry', `http://${registryAddress}`
+    // , '--yes'
+  ], { cwd: monorepoPath, stdio: 'inherit' });
+}
+
+async function main() {
+  const port = await getPort();
+  const verdaccioProcess = startServer(port);
+
+  const registryAddress = `http://localhost:${port}`;
+  await waitOn({resources: [
+    registryAddress
+  ]});
+
+  console.log('verdaccio ready on', registryAddress);
+
+  await lernaPublish(registryAddress);
+
+  verdaccioProcess.cancel();
+
+  try {
+    await verdaccioProcess;
+  } catch (error) {
+    console.log('verdaccio server terminated');
   }
-};
+}
 
-startServer(config, 6000, config.storage, '1.0.0', 'verdaccio',
-  (webServer, addr, pkgName, pkgVersion) => {
-    console.log({pkgName, pkgVersion, port: addr.port, host: addr.host});
-
-    webServer.listen(addr.port || addr.path, addr.host, async() => {
-      console.log('verdaccio running');
-
-      await execa('npx', [
-        'lerna', 'publish', 'from-package',
-        '--ignore-scripts',
-        '--registry', `http://${addr.host}:${addr.port}`
-        // , '--yes'
-      ], {cwd: monorepoPath, stdio: 'inherit'});
-    });
-  });
+main();
