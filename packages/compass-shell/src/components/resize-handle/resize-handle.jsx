146c2e968a71bffab87cff28d72226b0bc8d2b0b
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-08-16 13:10:40 +0200

    chore(@mongodb-js/compass-shell): Provide data-test-ids and apply misc aria fixes

--- packages/compass-shell/src/components/resize-handle/resize-handle.jsx
+++ packages/compass-shell/src/components/resize-handle/resize-handle.jsx
@@ -4,7 +4,7 @@ import styles from './resize-handle.less';
 export default class ResizeHandle extends PureComponent {
   render() {
     return (
-      <div className={styles['resize-handle']} />
+      <div data-test-id="resize-handle" className={styles['resize-handle']} />
     );
   }
 }
