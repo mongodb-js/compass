66873fa058f9cc5ec2b558f21fcb20ed96a32f6d
Le Roux Bodenstein <lerouxb@gmail.com>
2021-08-25 12:41:38 +0100

    fix: reformat the parsed query to turn function() {} into 'function() {}'

--- packages/compass-aggregations/src/modules/stage.spec.js
+++ packages/compass-aggregations/src/modules/stage.spec.js
@@ -16,6 +16,7 @@ describe('Stage module', () => {
       it('returns an empty object', () => {
         expect(generateStage(stage)).to.deep.equal({});
       });
+
       it('returns an empty object string', () => {
         expect(generateStageAsString(stage)).to.equal('{}');
       });
@@ -34,6 +35,7 @@ describe('Stage module', () => {
       it('returns an empty object', () => {
         expect(generateStage(stage)).to.deep.equal({});
       });
+
       it('returns an empty object string', () => {
         expect(generateStageAsString(stage)).to.equal('{}');
       });
@@ -52,6 +54,7 @@ describe('Stage module', () => {
       it('returns an empty object', () => {
         expect(generateStage(stage)).to.deep.equal({});
       });
+
       it('returns an empty object string', () => {
         expect(generateStageAsString(stage)).to.equal('{}');
       });
@@ -116,6 +119,7 @@ describe('Stage module', () => {
       it('returns the decommented stage', () => {
         expect(generateStage(stage)).to.deep.equal({ '$match': { x: 1 }});
       });
+
       it('returns the decommented string', () => {
         expect(generateStageAsString(stage)).to.deep.equal('{$match: {x: 1}}');
       });
@@ -140,13 +144,9 @@ describe('Stage module', () => {
           }
         });
       });
-      it('returns the stage string', () => {
-        expect(generateStageAsString(stage)).to.deep.equal(`{$addFields: {
-       totalHomework: { $sum: "$homework" } ,
-       totalQuiz: { $sum: "$quiz" }
 
-  }}`
-        );
+      it('returns the stage string', () => {
+        expect(generateStageAsString(stage)).to.deep.equal('{$addFields: {totalHomework: {$sum: \'$homework\'},totalQuiz: {$sum: \'$quiz\'}}}');
       });
     });
 
@@ -161,6 +161,7 @@ describe('Stage module', () => {
         stage: '{_id: 0, avg_price: {$avg: "$price"}}'
       };
       const res = generateStage(stage);
+
       it('returns the stage', () => {
         expect(res).to.deep.equal({
           '$project': {
@@ -171,9 +172,11 @@ describe('Stage module', () => {
           }
         });
       });
+
       it('does not include dropped projections', () => {
         expect(stage.projections.length).to.equal(1);
       });
+
       it('detects the avg_price projection', () => {
         expect(stage.projections[0].name).to.equal('avg_price');
       });
@@ -203,16 +206,9 @@ describe('Stage module', () => {
           }
         });
       });
+
       it('returns the stage string', () => {
-        expect(generateStageAsString(stage)).to.deep.equal(`{$bucket: {
-     groupBy: "$price",
-     boundaries: [ 0, 200, 400 ],
-     default: "Other",
-     output: {
-       "count": { $sum: 1 },
-       "titles" : { $push: "$title" }
-     }
-   }}`);
+        expect(generateStageAsString(stage)).to.deep.equal('{$bucket: {groupBy: \'$price\',boundaries: [0,200,400],\'default\': \'Other\',output: {count: { $sum: 1},titles: { $push: \'$title\'}}}}');
       });
     });
 
@@ -230,8 +226,77 @@ describe('Stage module', () => {
       it('returns the stage', () => {
         expect(generateStage(stage)).to.deep.equal({'$count': 'fieldname'});
       });
+
       it('returns the stage string', () => {
-        expect(generateStageAsString(stage)).to.deep.equal('{$count: "fieldname"}');
+        expect(generateStageAsString(stage)).to.deep.equal('{$count: \'fieldname\'}');
+      });
+    });
+
+    context('when the stage contains functions', () => {
+      const stage = {
+        id: 0,
+        isEnabled: true,
+        isExpanded: true,
+        isValid: true,
+        snippet: '',
+        stageOperator: '$addFields',
+        stage: `{
+        isFound:
+            { $function:
+               {
+                  body: function(name) {
+                     return hex_md5(name) == "15b0a220baa16331e8d80e15367677ad"
+                  },
+                  args: [ "$name" ],
+                  lang: "js"
+               }
+            },
+         message:
+            { $function:
+               {
+                  body: function(name, scores) {
+                     let total = Array.sum(scores);
+                     return \`Hello \${name}.  Your total score is \${total}.\`
+                  },
+                  args: [ "$name", "$scores"],
+                  lang: "js"
+               }
+            }
+       }`
+      };
+
+      it('returns the stage', () => {
+        const generated = generateStage(stage);
+        expect(stage.isValid).to.equal(true);
+        expect(generated).to.deep.equal({
+          '$addFields': {
+            isFound: {
+              '$function': {
+                args: [
+                  '$name'
+                ],
+                body: 'function(name) {\n                     return hex_md5(name) == \'15b0a220baa16331e8d80e15367677ad\'\n                  }',
+                lang: 'js'
+              }
+            },
+            message: {
+              '$function': {
+                args: [
+                  '$name',
+                  '$scores'
+                ],
+                body: 'function(name, scores) {\n                     let total = Array.sum(scores);\n                     return `Hello ${name}.  Your total score is ${total}.`\n                  }',
+                lang: 'js'
+              }
+            }
+          }
+        });
+      });
+
+      it('returns the stage string', () => {
+        const generated = generateStageAsString(stage);
+        expect(stage.isValid).to.equal(true);
+        expect(generated).to.deep.equal('{$addFields: {isFound: {$function: { body: \'function(name) {\\n return hex_md5(name) == "15b0a220baa16331e8d80e15367677ad"\\n }\', args: [ \'$name\' ], lang: \'js\'}},message: {$function: { body: \'function(name, scores) {\\n let total = Array.sum(scores);\\n return `Hello ${name}. Your total score is ${total}.`\\n }\', args: [ \'$name\', \'$scores\' ], lang: \'js\'}}}}');
       });
     });
 
@@ -310,21 +375,9 @@ describe('Stage module', () => {
       it('generates timestamp', () => {
         expect(generated.ts.low).to.equal(-321469502);
       });
+
       it('returns the stage string', () => {
-        expect(generateStageAsString(stage)).to.deep.equal(`{$match: {
-  code: Code('some code'),
-  oid: ObjectId('5a7382114ec1f67ae445f778'),
-  bin: Binary('aakjadfjadfldksjfadf', '1'),
-  dbref: DBRef('db.coll', '1'),
-  nl: NumberLong('3'),
-  nd: NumberDecimal('5.00000001'),
-  ni: NumberInt('5'),
-  minkey: MinKey(),
-  maxkey: MaxKey(),
-  isodate: ISODate('1999-01-01'),
-  regexp: RegExp('/^[a-z0-9_-]{3,16}$/'),
-  ts: Timestamp(-321469502, 367)
-}}`);
+        expect(generateStageAsString(stage)).to.deep.equal('{$match: {code: Code(\'some code\'),oid: ObjectId(\'5a7382114ec1f67ae445f778\'),bin: BinData(1, \'YWFramFkZmphZGZsZGtzamZhZGY=\'),dbref: DBRef(\'coll\', \'1\'),nl: NumberLong(3),nd: NumberDecimal(\'5.00000001\'),ni: 5,minkey: MinKey(),maxkey: MaxKey(),isodate: ISODate(\'1999-01-01T00:00:00.000Z\'),regexp: RegExp(\'\\/^[a-z0-9_-]{3,16}$\\/\'),ts: Timestamp(-321469502, 367)}}');
       });
     });
   });
