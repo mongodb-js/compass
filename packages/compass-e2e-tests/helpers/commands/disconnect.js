7573f5a52e81087f939c33763696307a3c13c50e
Le Roux Bodenstein <lerouxb@gmail.com>
2021-09-03 12:38:14 +0100

    tweak

--- packages/compass-e2e-tests/helpers/commands/disconnect.js
+++ packages/compass-e2e-tests/helpers/commands/disconnect.js
@@ -2,23 +2,24 @@ const { delay } = require('../delay');
 const Selectors = require('../selectors');
 
 async function closeConnectionModal(app) {
-  try {
-    await app.client.clickVisible(Selectors.CancelConnectionButton);
-    await app.client.waitUntilGone(Selectors.ConnectionStatusModalContent, {
-      timeoutMsg: 'Expected connection status modal to disappear after cancelling the connection'
-    });
-  } catch (e) {
-    // If that failed, the button was probably gone before we managed to
-    // click it. Let's go through the whole disconnecting flow now
-  }
+  await app.client.clickVisible(Selectors.CancelConnectionButton);
+  await app.client.waitUntilGone(Selectors.ConnectionStatusModalContent, {
+    timeoutMsg: 'Expected connection status modal to disappear after cancelling the connection'
+  });
 }
 
 module.exports = function(app) {
   return async function () {
     // If we are still connecting, let's try cancelling the connection first
     if (await app.client.isVisible(Selectors.CancelConnectionButton)) {
-      await closeConnectionModal(app)
+      try {
+        await closeConnectionModal(app)
+      } catch (e) {
+        // If that failed, the button was probably gone before we managed to
+        // click it. Let's go through the whole disconnecting flow now
+      }
     }
+
     app.webContents.send('app:disconnect');
     await app.client.waitForVisible(Selectors.ConnectSection, 5000);
     // Show "new connection" section as if we just opened this screen
