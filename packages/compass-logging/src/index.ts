70d3557d9698ac8e2e4a7d831684630cbac40e5f
Anna Henningsen <anna@addaleax.net>
2021-09-07 14:37:05 +0200

    fixup: hadron-ipc interface change from #2428

--- packages/compass-logging/src/index.ts
+++ packages/compass-logging/src/index.ts
@@ -1,26 +1,17 @@
 import { MongoLogWriter, mongoLogId } from 'mongodb-log-writer';
 import { Writable } from 'stream';
+import ipc from 'hadron-ipc';
 
 function createLogger(component: string): {
   log: ReturnType<MongoLogWriter['bindComponent']>;
   mongoLogId: typeof mongoLogId;
 } {
-  // hadron-ipc only works inside of Electron, hence the conditional
-  let ipc: typeof import('hadron-ipc') | undefined;
-  try {
-    ipc = require('hadron-ipc');
-  } catch {
-    /* not electron */
-  }
-
   const target = new Writable({
     decodeStrings: false,
     write(line: string, encoding: unknown, callback: () => void) {
-      if (ipc) {
-        ipc.call('compass:log', { line });
-      } else {
-        (process as any).emit('compass:log', { line });
-      }
+      // ipc.call is undefined outside of Electron
+      ipc.call?.('compass:log', { line });
+      (process as any).emit('compass:log', { line });
       callback();
     },
   });
--- packages/hadron-ipc/index.js
+++ packages/hadron-ipc/index.js
@@ -2,8 +2,22 @@
 
 const isRenderer = require('is-electron-renderer');
 
-if (isRenderer) {
-  module.exports = require('./lib/renderer');
-} else {
-  module.exports = require('./lib/main');
+let isElectron = false;
+try {
+  isElectron = typeof require('electron') !== 'string';
+} catch (err) { /* not electron */ }
+
+function requireExportModule() {
+  if (!isElectron) {
+    if (process.env.NODE_ENV !== 'test') {
+      // eslint-disable-next-line no-console
+      console.warn('Unsupported environment for hadron-ipc');
+    }
+
+    return {};
+  }
+
+  return isRenderer ? require('./lib/renderer') : require('./lib/main');
 }
+
+module.exports = requireExportModule();
