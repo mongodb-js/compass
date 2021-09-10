33a8fcc054fa28abacf989fb58b5aeea7861913e
Le Roux Bodenstein <lerouxb@gmail.com>
2021-09-10 09:49:01 +0100

    always do the keychain thing

--- packages/compass-e2e-tests/tests/ttfq.test.js
+++ packages/compass-e2e-tests/tests/ttfq.test.js
@@ -5,11 +5,12 @@ const { beforeTests, afterTests } = require('../helpers/compass');
 describe('Time to first query', function () {
   this.timeout(1000 * 60 * 1);
 
+  let keychain;
   let compass;
 
   it('can open compass and connect to a database', async function () {
     // start compass inside the test so that the time is measured together
-    ({ compass } = await beforeTests(false));
+    ({ keychain, compass } = await beforeTests());
 
     await compass.client.connectWithConnectionString(
       'mongodb://localhost:27018/test'
@@ -27,6 +28,6 @@ describe('Time to first query', function () {
   after(function () {
     // cleanup outside of the test so that the time it takes to run does not
     // get added to the time it took to run the first query
-    return afterTests({ compass });
+    return afterTests({ keychain, compass });
   });
 });
