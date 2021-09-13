33e73b5865094147cb4fd3b0e3d0ec87ba8fb8a0
Le Roux Bodenstein <lerouxb@gmail.com>
2021-09-13 21:06:41 +0100

    and another

--- packages/compass-e2e-tests/helpers/commands/wait-for-connection-screen.js
+++ packages/compass-e2e-tests/helpers/commands/wait-for-connection-screen.js
@@ -4,7 +4,7 @@ const MINUTE = 1000 * 60 * 1;
 
 module.exports = function (app) {
   return async function waitForConnectionScreen() {
-    const client = app.client;
+    const client = app.wrappedClient;
     await client.waitUntil(
       async () => {
         // Compass starts with two windows (one is loading, another is main)
