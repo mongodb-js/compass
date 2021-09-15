5fc7b455f6b8d53b22ecf71a97f596a7bf133629
Le Roux Bodenstein <lerouxb@gmail.com>
2021-09-15 12:25:05 +0100

    barking up the wrong tree

--- packages/compass-e2e-tests/helpers/commands/set-value-visible.js
+++ packages/compass-e2e-tests/helpers/commands/set-value-visible.js
@@ -1,17 +1,7 @@
-const { retryWithBackoff } = require('../retry-with-backoff');
-
 module.exports = function (app) {
   return async function setValueVisible(selector, value) {
     const { client } = app;
     await client.waitForVisible(selector);
-    // In CI on macOS this can throw "element not interactable". Ideally we'd
-    // have a waitForInteractable or similar.
-    await retryWithBackoff(
-      async () => {
-        await client.setValue(selector, value);
-      },
-      3,
-      1000
-    );
+    await client.setValue(selector, value);
   };
 };
