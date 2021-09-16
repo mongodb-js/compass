896f6f159248cf1c88c26979e48f081664fb6494
Vladimir Isakov <vladimir.isakov@10gen.com>
2021-09-16 16:16:42 -0400

    option editor no longer overflows

--- packages/compass-query-bar/src/components/option-editor/option-editor.jsx
+++ packages/compass-query-bar/src/components/option-editor/option-editor.jsx
@@ -118,7 +118,6 @@ class OptionEditor extends Component {
         className={styles['option-editor']}
         mode="mongodb"
         theme="mongodb-query"
-        width="80%"
         value={this.props.value}
         onChange={this.onChangeQuery}
         editorProps={{ $blockScrolling: Infinity }}
