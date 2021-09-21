e3491f66a5412ae553c2ce387eb8ba1ce52fdbb7
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-09-21 15:03:35 +0200

    feat(webpack-config-compass): More shared features for webpack config
    - Add progress logging for builds
    - Do not externalize peer deps by default
    - Add support for loading libraries from source
    - Exclude node_modules from the babel processing to speed up things a bit
    - Add support for bundle analyzer + duplicate modules analysis

---
+++ configs/webpack-config-compass/src/args.ts
@@ -0,0 +1,78 @@
+import { Configuration, WebpackOptionsNormalized } from 'webpack';
+import { merge } from 'webpack-merge';
+import path from 'path';
+
+export type WebpackConfig = Configuration &
+  Pick<WebpackOptionsNormalized, 'devServer'>;
+
+export type WebpackCLIArgs = {
+  entry: string | string[];
+  configName?: string | string[];
+  name?: string;
+  color?: boolean;
+  merge?: boolean;
+  env: typeof process.env & {
+    WEBPACK_BUNDLE?: true;
+    WEBPACK_BUILD?: true;
+    WEBPACK_SERVE?: true;
+  };
+  nodeEnv: string;
+  progress?: boolean | string;
+  outputPath: string;
+  target?: string | string[];
+  watch?: boolean;
+  watchOptionsStdin?: boolean;
+  hot?: boolean;
+  devtool?: string | false;
+  prefetch?: boolean;
+  json?: boolean | string;
+  mode: WebpackConfig['mode'];
+  stats?: boolean | string;
+  analyze?: boolean;
+};
+
+type CompassConfigArgs = {
+  outputFilename?: string;
+  mainProcessLiveReload: boolean;
+  devServerPort: number;
+  cwd: string;
+};
+
+export type ConfigArgs = WebpackCLIArgs & CompassConfigArgs;
+
+function isMode(str?: string): str is WebpackConfig['mode'] {
+  return ['development', 'production', 'none', undefined].includes(str);
+}
+
+export function isServe(args: WebpackCLIArgs): boolean {
+  return Boolean(args?.env?.WEBPACK_SERVE);
+}
+
+export function webpackArgsWithDefaults(
+  args: Partial<ConfigArgs>,
+  ...more: Partial<ConfigArgs>[]
+): ConfigArgs {
+  const cwd = process.cwd();
+
+  return merge<ConfigArgs>(
+    {
+      entry: path.join(cwd, 'src', 'index.js'),
+      env: {},
+      nodeEnv: process.env.NODE_ENV ?? args.mode ?? 'production',
+      outputPath: path.join(cwd, 'dist'),
+      watch: false,
+      watchOptionsStdin: false,
+      hot: false,
+      mode:
+        isMode(process.env.NODE_ENV) && process.env.NODE_ENV
+          ? process.env.NODE_ENV
+          : 'production',
+      mainProcessLiveReload: true,
+      devServerPort: 4242,
+      analyze: ['1', 'true'].includes(process.env.ANALYZE as string),
+      cwd,
+    },
+    args as ConfigArgs,
+    ...(more as ConfigArgs[])
+  );
+}
---
+++ configs/webpack-config-compass/src/util.ts
@@ -0,0 +1,68 @@
+import fs from 'fs';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+export function toCommonJsExternal(deps: string[]): Record<string, string> {
+  return Object.fromEntries(
+    deps.map((depName) => [depName, `commonjs2 ${depName}`])
+  );
+}
+
+type SimpleEntry = string | string[] | Record<string, string>;
+
+export function entriesToNamedEntries(
+  entry: SimpleEntry
+): Record<string, string> {
+  return typeof entry === 'string'
+    ? { [path.basename(entry).replace(/\.(jsx?|tsx?)$/, '')]: entry }
+    : Array.isArray(entry)
+    ? Object.fromEntries(
+        entry.map((entryPath) => [
+          path.basename(entryPath).replace(/\.(jsx?|tsx?)$/, ''),
+          entryPath,
+        ])
+      )
+    : entry;
+}
+
+export function entriesToHtml(
+  entries: Record<string, string>
+): HtmlWebpackPlugin[] {
+  return Array.from(
+    Object.entries(entries).map(([name, entryPath]) => {
+      let template = 'auto';
+
+      for (const ext of ['.html', '.ejs']) {
+        try {
+          const maybeTemplatePath = entryPath.replace(/\.(jsx?|tsx?)$/, ext);
+          fs.statSync(maybeTemplatePath);
+          template = maybeTemplatePath;
+          break;
+        } catch (e) {
+          // ignore and use default template, electron renderer entry will need
+          // at least some kind of html page provided one way or the other
+        }
+      }
+
+      return new HtmlWebpackPlugin({
+        filename: `${name}.html`,
+        template,
+        chunks: [name],
+      });
+    })
+  );
+}
+
+export function camelCase(str: string): string {
+  return str
+    .split(/[\W_]+/)
+    .filter(Boolean)
+    .map(([first, ...word]: string) => first.toUpperCase() + word.join(''))
+    .join('');
+}
+
+export function getLibraryNameFromCwd(cwd: string): string {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { name, productName } = require(path.join(cwd, 'package.json'));
+  return camelCase(productName || name);
+}
