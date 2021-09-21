e83a5d8a255ab11078ed300f230ff1e0d3d51e89
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-09-21 19:08:03 +0200

    chore(compass-shell): Asyncify loading of the shell component so that it's not in the apps critical path an initial bundle

--- packages/compass-shell/src/plugin.js
+++ packages/compass-shell/src/plugin.js
@@ -1,40 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Provider } from 'react-redux';
-import CompassShell from './components/compass-shell';
 import CompassShellStore from './stores';
 import { getUserDataFilePath } from './modules/get-user-data-file-path';
 import { HistoryStorage } from './modules/history-storage';
 
 function createPlugin() {
   const store = new CompassShellStore();
-  const Plugin = class extends Component {
-    static displayName = 'CompassShellPlugin';
 
-    initializeHistoryStorage() {
+  function CompassShellPlugin() {
+    const [ShellComponent, setShellComponent] = useState(null);
+    const historyStorage = useRef(null);
+
+    if (!historyStorage.current) {
       const historyFilePath = getUserDataFilePath('shell-history.json');
-
-      if (!historyFilePath) {
-        return;
+      if (historyFilePath) {
+        historyStorage.current = new HistoryStorage(historyFilePath);
       }
-
-      return new HistoryStorage(historyFilePath);
     }
 
-    /**
-     * Connect the Plugin to the store and render.
-     *
-     * @returns {React.Component} The rendered component.
-     */
-    render() {
+    useEffect(() => {
+      let mounted = true;
+
+      import(/* webpackPreload: true */ './components/compass-shell').then(
+        ({ default: Component }) => {
+          if (mounted) {
+            setShellComponent(Component);
+          }
+        }
+      );
+
+      return () => {
+        mounted = false;
+      };
+    }, []);
+
+    if (ShellComponent) {
       return (
         <Provider store={store.reduxStore}>
-          <CompassShell historyStorage={this.initializeHistoryStorage()} />
+          <ShellComponent historyStorage={historyStorage.current} />
         </Provider>
       );
     }
-  };
 
-  return {store, Plugin};
+    return null;
+  }
+
+  return { store, Plugin: CompassShellPlugin };
 }
 
 export default createPlugin;
