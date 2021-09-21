c1d86229ca545e46fe90aadcdc6bc6b6c69136a2
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-09-21 15:19:44 +0200

    chore(mongodb-compass): Update compass webpack config to allow hmr and externalize shell runtime

--- packages/compass/webpack.config.js
+++ packages/compass/webpack.config.js
@@ -2,24 +2,44 @@
 const path = require('path');
 const {
   createElectronMainConfig,
-  createElectronRendererConfig
+  createElectronRendererConfig,
+  webpackArgsWithDefaults,
+  merge
 } = require('@mongodb-js/webpack-config-compass');
 
 module.exports = (_env, args) => {
+  const opts = {
+    ...webpackArgsWithDefaults(args),
+    outputPath: path.resolve(__dirname, 'build'),
+    hot: true
+  };
+
+  process.env.NODE_ENV = opts.nodeEnv;
+
+  const mainConfig = createElectronMainConfig({
+    ...opts,
+    entry: path.resolve(__dirname, 'src', 'main', 'index.js'),
+    outputFilename: 'main.js'
+  });
+
+  const rendererConfig = createElectronRendererConfig({
+    ...opts,
+    entry: [
+      path.resolve(__dirname, 'src', 'app', 'index.js'),
+      path.resolve(__dirname, 'src', 'app', 'loading', 'loading.js')
+    ]
+  });
+
+  const externals = {
+    // Runtime implementation depends on worker file existing near the library
+    // main import and for that reason it needs to stay external to the
+    // compass-shell
+    '@mongosh/node-runtime-worker-thread':
+      'commonjs2 @mongosh/node-runtime-worker-thread'
+  };
+
   return [
-    createElectronMainConfig({
-      ...args,
-      entry: path.resolve(__dirname, 'src', 'main', 'index.js'),
-      outputPath: path.resolve(__dirname, 'build'),
-      outputFilename: 'main.js'
-    }),
-    createElectronRendererConfig({
-      ...args,
-      entry: [
-        path.resolve(__dirname, 'src', 'app', 'index.js'),
-        path.resolve(__dirname, 'src', 'app', 'loading', 'loading.js')
-      ],
-      outputPath: path.resolve(__dirname, 'build')
-    })
+    merge(mainConfig, { externals }),
+    merge(rendererConfig, { externals })
   ];
 };
