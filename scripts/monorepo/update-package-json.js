371eeb9431b3ec2aa5fa7fec88965a8dd8067969
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-09-21 19:15:12 +0200

    feat(scripts): Allow to skip package json update; Add convenience method for inserting key at particular position in the object

--- scripts/monorepo/update-package-json.js
+++ scripts/monorepo/update-package-json.js
@@ -1,16 +1,32 @@
 const path = require('path');
 const { promises: fs } = require('fs');
 
+const skip = Symbol('skip');
+
+function insertAfter(obj, key, insertKey, insertValue) {
+  const keys = Object.keys(obj);
+  keys.splice(keys.indexOf(key) + 1, 0, insertKey);
+  return Object.fromEntries(
+    keys.map((key) => [key, key === insertKey ? insertValue : obj[key]])
+  );
+}
+
 async function updatePackageJson(packageDir, updateFn) {
   const pathToPkg = path.resolve(packageDir, 'package.json');
   const pkgJson = require(pathToPkg);
-  const updated = updateFn(pkgJson);
+  const updated = await updateFn(pkgJson, skip);
+
+  if (updated === skip) {
+    return;
+  }
+
   if (!updated || typeof updated !== 'object') {
     const updatedStr = JSON.stringify(updated);
     throw new Error(
       `updatePackageJson updateFn should return a package.json object, got ${updatedStr}`
     );
   }
+
   await fs.writeFile(
     pathToPkg,
     JSON.stringify(updated, null, 2).trim() + '\n',
@@ -18,4 +34,4 @@ async function updatePackageJson(packageDir, updateFn) {
   );
 }
 
-exports.updatePackageJson = updatePackageJson;
+module.exports = { skip, updatePackageJson, insertAfter };
