c7b6c90b932f8c154e48630dfda7ec13a1c6a2a0
mcasimir <maurizio.cas@gmail.com>
2021-09-23 22:48:59 +0200

    fix log metadata

--- packages/data-service/src/connect-mongo-client.ts
+++ packages/data-service/src/connect-mongo-client.ts
@@ -80,7 +80,7 @@ export default async function connectMongoClient(
     ])) as MongoClient; // waitForTunnel always throws, never resolves
 
     log.info(mongoLogId(1_001_000_012), 'Connect', 'Connection established', {
-      driver: mongoClient.options?.metadata,
+      driver: mongoClient.options?.metadata?.driver,
       url: redactConnectionString(urlWithSshTunnel),
     });
 
