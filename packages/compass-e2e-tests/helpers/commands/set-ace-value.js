d1865cafc568b2eb8cc816e359a5a827f3d86350
Le Roux Bodenstein <lerouxb@gmail.com>
2021-09-23 16:31:51 +0100

    Control on windows & Linux

--- packages/compass-e2e-tests/helpers/commands/set-ace-value.js
+++ packages/compass-e2e-tests/helpers/commands/set-ace-value.js
@@ -1,9 +1,11 @@
+const META = process.platform === 'darwin' ? 'Meta' : 'Control';
+
 module.exports = function (app) {
   return async function setAceValue(selector, value) {
     const { client } = app;
     await client.clickVisible(`${selector} .ace_scroller`);
-    await client.keys(['Meta', 'a']);
-    await client.keys(['Meta']); // meta a second time to release it
+    await client.keys([META, 'a']);
+    await client.keys([META]); // meta a second time to release it
     await client.keys(['Backspace']);
     await client.keys(value);
   };
