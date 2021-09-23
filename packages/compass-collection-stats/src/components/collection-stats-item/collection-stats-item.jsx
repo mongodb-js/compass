605eebe7348968c633cbf3e2bf4e4561c1afdc1f
Le Roux Bodenstein <lerouxb@gmail.com>
2021-09-23 11:16:46 +0100

    data-test-id

--- packages/compass-collection-stats/src/components/collection-stats-item/collection-stats-item.jsx
+++ packages/compass-collection-stats/src/components/collection-stats-item/collection-stats-item.jsx
@@ -49,10 +49,10 @@ class CollectionStatsItem extends Component {
   render() {
     return (
       <div className={styles[BASE_CLASS]}>
-        <div className={styles[this.props.primary ? PRIMARY_LABEL : LABEL]} dataTestId={`${this.props.dataTestId}-label`}>
+        <div className={styles[this.props.primary ? PRIMARY_LABEL : LABEL]} data-test-id={`${this.props.dataTestId}-label`}>
           {this.props.label}
         </div>
-        <div className={styles[this.props.primary ? PRIMARY_VALUE : VALUE]} dataTestId={`${this.props.dataTestId}-value`}>
+        <div className={styles[this.props.primary ? PRIMARY_VALUE : VALUE]} data-test-id={`${this.props.dataTestId}-value`}>
           {this.props.value}
         </div>
       </div>
