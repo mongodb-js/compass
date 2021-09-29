be8e2f4916d7401b729aecdca0d6ecba19f30c45
Le Roux Bodenstein <lerouxb@gmail.com>
2021-09-29 17:07:45 +0100

    assert that the ace editor becomes focused

--- packages/compass-e2e-tests/helpers/commands/set-ace-value.js
+++ packages/compass-e2e-tests/helpers/commands/set-ace-value.js
@@ -1,9 +1,28 @@
+const debug = require('debug')('compass-e2e-tests').extend('set-ace-value');
+
+const FOCUS_ELEMENT = 'ace_text-input';
+
 const META = process.platform === 'darwin' ? 'Meta' : 'Control';
 
 module.exports = function (app) {
   return async function setAceValue(selector, value) {
     const { client } = app;
     await client.clickVisible(`${selector} .ace_scroller`);
+
+    // make sure the right element is focused before we continue
+    await client.waitUntil(async () => {
+      const element = await client.elementActive();
+      const className = (
+        await client.elementIdAttribute(element.value.ELEMENT, 'class')
+      ).value;
+      if (className !== FOCUS_ELEMENT) {
+        debug(
+          `expected ${FOCUS_ELEMENT} to be focused but instead found ${className}`
+        );
+      }
+      return className === FOCUS_ELEMENT;
+    });
+
     await client.keys([META, 'a']);
     await client.keys([META]); // meta a second time to release it
     await client.keys(['Backspace']);
