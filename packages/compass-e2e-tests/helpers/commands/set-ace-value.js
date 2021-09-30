0cf743744aa163f0bee9b5bdaf076b8d4401182e
Le Roux Bodenstein <lerouxb@gmail.com>
2021-09-30 09:53:29 +0100

    more debugging info for when focusing the ace editor fails

--- packages/compass-e2e-tests/helpers/commands/set-ace-value.js
+++ packages/compass-e2e-tests/helpers/commands/set-ace-value.js
@@ -1,26 +1,49 @@
+const { identifier } = require('jscodeshift');
+
 const debug = require('debug')('compass-e2e-tests').extend('set-ace-value');
 
-const FOCUS_ELEMENT = 'ace_text-input';
+const FOCUS_TAG = 'textarea';
+const FOCUS_CLASS = 'ace_text-input';
 
 const META = process.platform === 'darwin' ? 'Meta' : 'Control';
+
+async function elementResult(client, elementId) {
+  // The API methods that operate off WebElement JSON IDs are all a bit awkward,
+  // so just try and get as much debug info as possible.
+  return {
+    tagName: (await client.elementIdName(elementId)).value,
+    className: (await client.elementIdAttribute(elementId, 'class')).value,
+    id: (await client.elementIdAttribute(elementId, 'id')).value,
+    name: (await client.elementIdAttribute(elementId, 'name')).value,
+    dataTestId: (await client.elementIdAttribute(elementId, 'data-test-id'))
+      .value,
+    text: (await client.elementIdText(elementId)).value,
+    value: (await client.elementIdAttribute(elementId, 'value')).value,
+  };
+}
 
 module.exports = function (app) {
   return async function setAceValue(selector, value) {
     const { client } = app;
-    await client.clickVisible(`${selector} .ace_scroller`);
 
     // make sure the right element is focused before we continue
     await client.waitUntil(async () => {
+      await client.clickVisible(`${selector} .ace_scroller`);
+
       const element = await client.elementActive();
-      const className = (
-        await client.elementIdAttribute(element.value.ELEMENT, 'class')
-      ).value;
-      if (className !== FOCUS_ELEMENT) {
+      const result = await elementResult(client, element.value.ELEMENT);
+      const { tagName, className } = result;
+      const focused = tagName === FOCUS_TAG && className === FOCUS_CLASS;
+
+      if (!focused) {
         debug(
-          `expected ${FOCUS_ELEMENT} to be focused but instead found ${className}`
+          `expected "${FOCUS_TAG}.${FOCUS_CLASS}" to be focused but instead found "${tagName}.${
+            className || 'undefined'
+          }" with ${JSON.stringify(result)}`
         );
       }
-      return className === FOCUS_ELEMENT;
+
+      return focused;
     });
 
     await client.keys([META, 'a']);
