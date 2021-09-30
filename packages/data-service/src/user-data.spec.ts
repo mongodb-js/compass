001e4d34417cbde2556b8cd40aad1fa961431c3e
Anna Henningsen <anna@addaleax.net>
2021-09-30 12:32:48 +0200

    fixup: use fs.promises

--- packages/data-service/src/user-data.spec.ts
+++ packages/data-service/src/user-data.spec.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 import os from 'os';
 import { UserData } from './user-data';
@@ -6,15 +6,15 @@ import { UserData } from './user-data';
 describe('UserData', function () {
   let tempDir;
 
-  beforeEach(function () {
-    tempDir = fs.mkdtempSync(
+  beforeEach(async function () {
+    tempDir = await fs.mkdtemp(
       path.join(os.tmpdir(), 'compass-user-data-tests-')
     );
   });
 
-  afterEach(function () {
+  afterEach(async function () {
     try {
-      fs.rmdirSync(tempDir, { recursive: true });
+      await fs.rmdir(tempDir, { recursive: true });
     } catch (e) {
       // windows fails to clean those up sometimes, let's just skip it and move
       // forward with runnning the tests
@@ -23,7 +23,7 @@ describe('UserData', function () {
 
   describe('read', function () {
     it('reads an existing file', async function () {
-      fs.writeFileSync(path.join(tempDir, 'file.json'), '{"x":1}');
+      await fs.writeFile(path.join(tempDir, 'file.json'), '{"x":1}');
 
       const userData = new UserData({ alternateDataRoot: tempDir });
 
