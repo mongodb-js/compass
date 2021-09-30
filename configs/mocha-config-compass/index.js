6ad2eddd21d1375efb01e83eea8e900dd0227371
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-09-30 18:51:34 +0200

    feat(mocha-config-compass): Add support for importing images and fonts in the tests
    Mocks webpack behavior for some test suites

--- configs/mocha-config-compass/index.js
+++ configs/mocha-config-compass/index.js
@@ -4,6 +4,7 @@ module.exports = {
   colors: true,
   timeout: 15000,
   require: [
+    path.resolve(__dirname, 'register', 'assets-import-register.js'),
     path.resolve(__dirname, 'register', 'tsnode-register.js'),
     path.resolve(__dirname, 'register', 'sinon-chai-register.js'),
   ],
