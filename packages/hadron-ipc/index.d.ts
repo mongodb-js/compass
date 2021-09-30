040c722cb815a7c7aac44721c4d8e80a5417d6ef
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-09-30 18:58:18 +0200

    feat(hadron-ipc): Add types and named exports to package
    Named exports are easier to use when importing hadron-ipc in TypeScript

--- packages/hadron-ipc/index.d.ts
+++ packages/hadron-ipc/index.d.ts
@@ -1,1 +1,28 @@
-export = any;
+import type { IpcMain, IpcRenderer, BrowserWindow } from 'electron';
+
+export type HadronIpcMain = IpcMain & {
+  respondTo(
+    name: string,
+    handler: (browserWindow: BrowserWindow, ...args: any[]) => any
+  ): void;
+  respondTo(
+    handlerMap: Record<
+      string,
+      (browserWindow: BrowserWindow, ...args: any[]) => any
+    >
+  ): void;
+  broadcast(name: string, ...args: any[]): void;
+  broadcastFocused(name: string, ...args: any[]): void;
+  remove: typeof ipcMain['removeListener'];
+};
+
+export type HadronIpcRenderer = IpcRenderer & {
+  call(name: string, ...args: any[]): Promise<any>;
+  callQuiet(name: string, ...args: any[]): Promise<any>;
+};
+
+export type HadronIpc = HadronIpcMain | HadronIpcRenderer;
+
+export = HadronIpc;
+export const ipcMain: HadronIpcMain;
+export const ipcRenderer: HadronIpcRenderer;
