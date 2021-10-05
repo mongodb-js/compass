301f6616b668cbdc937d1aea45dba6bdc407ca62
Anemy <rhysh@live.com>
2021-10-05 19:25:14 -0400

    lint

--- packages/compass-e2e-tests/helpers/commands/set-ace-value.js
+++ packages/compass-e2e-tests/helpers/commands/set-ace-value.js
@@ -5,23 +5,6 @@ const FOCUS_CLASS = 'ace_text-input';
 
 const META = process.platform === 'darwin' ? 'Meta' : 'Control';
 
-async function elementResult(client, elementId) {
-  // The API methods that operate off WebElement JSON IDs are all a bit awkward,
-  // so just try and get as much debug info as possible.
-
-  // TODO: Maybe needs # at start
-  const element = await client.$(elementId);
-  return {
-    tagName: await element.getAttribute('name'),
-    className: await element.getAttribute('class'),
-    id: await element.getAttribute('id'),
-    name: await element.getAttribute('name'),
-    dataTestId: await element.getAttribute('data-test-id'),
-    text: await element.getText(),
-    value: await element.getValue(),
-  };
-}
-
 module.exports = function (app) {
   return async function setAceValue(selector, value) {
     const { client } = app;
@@ -35,7 +18,7 @@ module.exports = function (app) {
 
       if (!focused) {
         debug(
-          `expected "${FOCUS_TAG}.${FOCUS_CLASS}" to be focused but was not;"`
+          `expected "${FOCUS_TAG}.${FOCUS_CLASS}" to be focused but was not"`
         );
       }
 
