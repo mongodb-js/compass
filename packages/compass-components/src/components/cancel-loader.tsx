04e272eb26844b2b65a4abd46ba4397eaa9e5904
Le Roux Bodenstein <lerouxb@gmail.com>
2021-10-14 15:16:06 +0100

    css style

--- packages/compass-components/src/components/cancel-loader.tsx
+++ packages/compass-components/src/components/cancel-loader.tsx
@@ -8,22 +8,25 @@ const cancelLoaderStyle = css({
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-  padding: '10px 0',
-  '.progress-text': {
-    margin: '0 auto 30px',
-    textAlign: 'left',
-    fontSize: '24px',
-    color: '#807f7f',
-    fontWeight: 200,
-    '.fa.fa-fw': {
-      fontSize: '20px',
-      width: '40px',
-    },
-  },
-  '.buttons': {
-    fontSize: '14px',
-  },
+  padding: '10px 0'
 });
+
+const progressTextStyle = css({
+  margin: '0 auto 30px',
+  textAlign: 'left',
+  fontSize: '24px',
+  color: '#807f7f',
+  fontWeight: 200,
+});
+
+const iconStyle = css({
+  fontSize: '20px',
+  width: '40px',
+});
+
+const buttonsStyle = css({
+  fontSize: '14px',
+})
 
 function CancelLoader({
   dataTestId,
@@ -37,12 +40,12 @@ function CancelLoader({
   cancelClicked: () => void;
 }): React.ReactElement {
   return (
-    <div className="loader" data-testid={dataTestId} css={cancelLoaderStyle}>
-      <div className="progress-text">
-        <i className="fa fa-fw fa-spin fa-circle-o-notch" />
+    <div data-testid={dataTestId} css={cancelLoaderStyle}>
+      <div css={progressTextStyle}>
+        <i className="fa fa-fw fa-spin fa-circle-o-notch" css={iconStyle} />
         {progressText}
       </div>
-      <div className="buttons">
+      <div css={buttonsStyle}>
         <button className="btn btn-sm btn-info" onClick={cancelClicked}>
           {cancelText}
         </button>
