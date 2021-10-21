2d221df5d13633a0c7aaebd711e88b5d09e7e8b4
mcasimir <maurizio.cas@gmail.com>
2021-10-21 19:08:22 +0200

    use mongosh github-repo for github releases

--- packages/hadron-build/commands/upload.js
+++ packages/hadron-build/commands/upload.js
@@ -1,169 +1,62 @@
+// eslint-disable-next-line strict
 'use strict';
 
 /**
  * Upload release assets to GitHub and S3.
  */
-const Promise = require('bluebird');
-const _ = require('lodash');
-const GitHub = require('github');
-const github = new GitHub({version: '3.0.0', 'User-Agent': 'hadron-build'});
+import { Octokit } from '@octokit/rest';
+const { GitHubRepo } = require('./../lib/github-repo');
+
 const cli = require('mongodb-js-cli')('hadron-build:upload');
 const abortIfError = cli.abortIfError.bind(cli);
 const Target = require('../lib/target');
-const verifyDistro = require('../lib/distro');
 
 const downloadCenter = require('../lib/download-center');
 
-let createGitHubRelease = (CONFIG) => {
-  const p = Promise.defer();
-  var opts = {
-    owner: 'mongodb-js',
-    repo: 'compass',
-    draft: true,
-    tag_name: `v${CONFIG.version}`,
-    name: CONFIG.version,
-    target_commitish: process.env.EVERGREEN_REVISION,
-    body: '> TODO (imlucas) Auto generate this.'
-  };
-
-  cli.debug('Creating release', opts);
-
-  github.repos.createRelease(opts, function(err, res) {
-    if (err) p.reject(err);
-
-    cli.debug('Created release', res);
-    p.resolve(res);
-  });
-  return p.promise;
-};
-
-let getOrCreateGitHubRelease = (CONFIG) => {
-  const p = Promise.defer();
-  const opts = {
-    owner: 'mongodb-js',
-    repo: 'compass'
-  };
-  github.repos.getReleases(opts, (err, releases) => {
-    if (err) {
-      return p.reject(err);
-    }
-
-    let existing = _.find(releases, (release) => release.name === CONFIG.version);
-    if (existing) {
-      cli.debug(`Found existsing release for ${CONFIG.version}`, existing);
-      return p.resolve(existing);
-    }
-
-    cli.debug(`Creating new draft release for ${CONFIG.version}`);
-    createGitHubRelease(CONFIG)
-      .then( (release) => p.resolve(release))
-      .catch( (_err) => p.reject(_err));
-  });
-  return p.promise;
-};
-
-let removeGitHubReleaseAssetIfExists = (CONFIG, release, asset) => {
-  let existing = _.chain(release.assets)
-    .filter((a) => a.name === asset.name)
-    .first()
-    .value();
-
-  if (!existing) {
-    return Promise.resolve(false);
-  }
-
-  cli.debug(`Removing existing asset ${asset.name}`);
-  const opts = {
-    owner: 'mongodb-js',
-    repo: 'compass',
-    id: existing.id
-  };
-
-  const p = Promise.defer();
-  github.repos.deleteAsset(opts, (err, res) => {
-    if (err) {
-      return p.reject(err);
-    }
-    cli.debug('Asset deleted', res);
-    return p.resolve(true);
-  });
-  return p.promise;
-};
-
-let doGitHubReleaseAssetUpload = (CONFIG, release, asset) => {
-  const opts = {
-    owner: 'mongodb-js',
-    repo: 'compass',
-    id: release.id,
-    name: asset.name,
-    filePath: asset.path
-  };
-
-  cli.spinner(`Uploading ${asset.name}`);
-  /* eslint no-console: 0, no-sync: 0 */
-  console.log(opts);
-
-  const p = Promise.defer();
-
-  github.authenticate({
-    token: process.env.GITHUB_TOKEN,
-    type: 'oauth'
-  });
-
-  github.repos.uploadAsset(opts, function(err, res) {
-    if (err) {
-      err.stack = err.stack || '<no stacktrace>';
-      cli.error(`Failed to upload ${asset.name}`);
-      console.log(err);
-      return p.reject(err);
-    }
-    cli.debug('Asset upload returned', res);
-    cli.ok(`Uploaded ${asset.name}`);
-    p.resolve(asset);
-  });
-  return p.promise;
-};
-
-let uploadGitHubReleaseAsset = (CONFIG, release, asset) => {
-  if (release.draft === true) {
-    return removeGitHubReleaseAssetIfExists(CONFIG, release, asset)
-      .then(() => doGitHubReleaseAssetUpload(CONFIG, release, asset));
-  }
-
-  let existing = _.chain(release.assets)
-    .filter((a) => a.name === asset.name)
-    .first()
-    .value();
-
-  if (existing) {
-    cli.debug('Asset already exists and release is currently not a draft.  skipping.', existing);
-    return Promise.resolve(existing);
-  }
-
-  return doGitHubReleaseAssetUpload(CONFIG, release, asset);
-};
-
-let maybePublishGitHubRelease = (CONFIG) => {
-  if (CONFIG.channel === 'dev') {
+async function maybePublishGitHubRelease(target) {
+  if (target.channel === 'dev') {
     cli.info('Skipping publish GitHub release for dev channel.');
-    return Promise.resolve();
   }
 
   if (!process.env.GITHUB_TOKEN) {
     cli.warn('Skipping publish release because process.env.GITHUB_TOKEN not set.');
-    return Promise.resolve();
   }
 
-  github.authenticate({
-    token: process.env.GITHUB_TOKEN,
-    type: 'oauth'
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
   });
 
-  return getOrCreateGitHubRelease(CONFIG)
-    .then((release) => {
-      return CONFIG.assets.map((asset) => uploadGitHubReleaseAsset(CONFIG, release, asset));
-    });
-};
+  const repo = new GitHubRepo({
+    owner: 'mongodb-js',
+    repo: 'compass'
+  }, octokit);
+
+  const releaseTag = `v${target.version}`;
+
+  const release = {
+    name: target.version,
+    tag: releaseTag,
+    notes: `Release v${target.version}`
+  };
+
+  // NOTE: this will correctly fail if not in draft, so it won't override
+  // already published releases.
+  await repo.updateDraftRelease(release);
+
+  const uploads = target.assets.map(
+    async function(asset) {
+      cli.info(`Starting upload ${asset.name} to Github (path: ${asset.path})`);
+      await repo.uploadReleaseAsset(releaseTag, {
+        name: asset.name,
+        path: asset.path
+      });
+
+      cli.info(`Upload of ${asset.name} to Github done.`);
+    }
+  );
+
+  await Promise.all(uploads);
+}
 
 exports.command = 'upload [options]';
 
@@ -187,12 +80,14 @@ exports.handler = function(argv) {
   var target = new Target(argv.dir);
 
   target.assets = target.assets.filter(function(asset) {
+    // eslint-disable-next-line no-sync
     var exists = fs.existsSync(asset.path);
     if (!exists) {
       cli.warn(`Excluding ${asset.path} from upload because it does not exist.`);
     }
     return exists;
   });
+
   maybePublishGitHubRelease(target)
     .then(() => downloadCenter.maybeUpload(target))
     .catch(abortIfError);
