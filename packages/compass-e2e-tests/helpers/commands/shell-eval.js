4a98d5502e52f15f6b6c38202226c12b30f80a2d
Le Roux Bodenstein <lerouxb@gmail.com>
2021-10-25 14:50:08 +0200

    less flakyness in shell eval

--- packages/compass-e2e-tests/helpers/commands/shell-eval.js
+++ packages/compass-e2e-tests/helpers/commands/shell-eval.js
@@ -1,36 +1,63 @@
-const { delay } = require('../delay');
 const Selectors = require('../selectors');
 
 module.exports = function (app) {
-  return async function (str, parse = false, timeout) {
+  async function getOutputText() {
+    const { client } = app;
+
+    const elements = await client.$$(Selectors.ShellOutput);
+    return Promise.all(
+      elements.map((element) => {
+        return element.getText();
+      })
+    );
+  }
+
+  return async function (str, parse = false) {
     const { client } = app;
     const shellContentElement = await client.$(Selectors.ShellContent);
     if (!(await shellContentElement.isDisplayed())) {
       await client.clickVisible(Selectors.ShellExpandButton);
     }
+
+    const numLines = (await getOutputText()).length;
+
     await client.clickVisible(Selectors.ShellInput);
     // Might be marked with a deprecation warning, but can be used
     // https://github.com/webdriverio/webdriverio/issues/2076
 
-    await client.keys(parse === true ? `JSON.stringify(${str})` : str);
+    const command = parse === true ? `JSON.stringify(${str})` : str;
+    await client.keys(command);
     await client.keys('\uE007');
+
+    // wait until more output appears
+    await client.waitUntil(async () => {
+      const lines = await getOutputText();
+      // first the command we send appears then later the response
+      return lines.length > numLines + 1;
+    });
+
+    /*
     const shellLoaderElement = await client.$(Selectors.ShellLoader);
-    if (await shellLoaderElement.isDisplayed()) {
-      await shellLoaderElement.waitForDisplayed({
-        timeout,
-        timeoutMsg: `Expected shell evaluation to finish in ${timeout}ms`,
-        reverse: true,
-        interval: 50,
-      });
-    }
-    await delay(50);
+    await shellLoaderElement.waitForDisplayed({
+      timeout,
+      timeoutMsg: `Expected shell evaluation to finish in ${timeout}ms`,
+      reverse: true,
+      interval: 50,
+    });
+    */
+
     const shellOutputElements = await client.$$(Selectors.ShellOutput);
     const output = await shellOutputElements[
       shellOutputElements.length - 1
     ].getText();
     let result = Array.isArray(output) ? output.pop() : output;
     if (parse === true) {
-      result = JSON.parse(result.replace(/(^['"]|['"]$)/g, ''));
+      try {
+        result = JSON.parse(result.replace(/(^['"]|['"]$)/g, ''));
+      } catch (err) {
+        // just leave it unparsed for now if there's a parse error because
+        // that's really helpful when debugging
+      }
     }
     return result;
   };
