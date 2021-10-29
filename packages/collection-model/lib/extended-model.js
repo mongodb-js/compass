6f6b26c14f35f31a25cc7b469ecbf0f4336803a7
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-10-29 18:19:13 +0200

    feat(collection-model): Add fetch method to the collection model and collection so it can handle it's own data loading

--- packages/collection-model/lib/extended-model.js
+++ packages/collection-model/lib/extended-model.js
@@ -1,89 +1,108 @@
-var MongoDBCollection = require('./collection-model');
-var toNS = require('mongodb-ns');
-var raf = require('raf');
+const AmpersandModel = require('ampersand-model');
+const AmpersandCollection = require('ampersand-collection');
+const { promisify } = require('util');
+const toNs = require('mongodb-ns');
 
-/**
- * Metadata for a MongoDB Collection.
- * @see http://npm.im/mongodb-collection-model
- */
-var CollectionModel = MongoDBCollection.extend({
-  namespace: 'MongoDBCollection',
-  session: {
-    selected: {
-      type: 'boolean',
-      default: false
-    }
+const CollectionModel = AmpersandModel.extend({
+  modelType: 'Collection',
+  idAttribute: '_id',
+  props: {
+    _id: 'string',
+
+    // Normalized values from listCollections command
+    name: { type: 'string', required: true },
+    database: { type: 'string', required: true },
+    type: { type: 'string', required: true },
+    system: { type: 'boolean', required: true },
+    oplog: { type: 'boolean', required: true },
+    command: { type: 'boolean', required: true },
+    special: { type: 'boolean', required: true },
+    specialish: { type: 'boolean', required: true },
+    normal: { type: 'boolean', required: true },
+    readonly: 'boolean',
+    collation: 'object',
+    pipeline: 'array',
+    validation: 'object',
+
+    // Normalized values from collStats command
+    ns: 'string',
+    is_capped: 'boolean',
+    max: 'number',
+    is_power_of_two: 'boolean',
+    index_sizes: 'object',
+    document_count: 'number',
+    document_size: 'number',
+    storage_size: 'number',
+    index_count: 'number',
+    index_size: 'number',
+    padding_factor: 'number',
+    extent_count: 'number',
+    extent_last_size: 'number',
+    flags_user: 'number',
+    max_document_size: 'number',
+    size: 'number',
+    index_details: 'object',
+    wired_tiger: 'object',
   },
-  derived: {
-    name: {
-      deps: ['_id'],
-      fn: function() {
-        return toNS(this._id).collection;
-      }
-    },
-    specialish: {
-      deps: ['_id'],
-      fn: function() {
-        return toNS(this._id).specialish;
-      }
-    },
-    url: {
-      deps: ['_id'],
-      fn: function() {
-        return `/collections/${this.getId()}`;
-      }
-    }
-  },
-  serialize: function() {
-    return this.getAttributes(
-      {
-        props: true
-      },
-      true
+  /**
+   * @param {{ dataService: import('mongodb-data-service').DataService }} dataService
+   * @returns
+   */
+  async fetch({ dataService }) {
+    const collectionStatsAsync = promisify(
+      dataService.collectionStats.bind(dataService)
     );
+    const collectionInfoAsync = promisify(
+      dataService.collectionInfo.bind(dataService)
+    );
+    const [collStats, collectionInfo] = await Promise.all([
+      collectionStatsAsync(this.getId()),
+      collectionInfoAsync(this.database, this.name),
+    ]);
+    return this.set({ ...collStats, ...collectionInfo });
   },
-  fetch: function(options) {
-    var model = this;
+});
 
-    options = options ? { ...options } : {};
-    if (!options.parse) {
-      options.parse = true;
-    }
+const CollectionCollection = AmpersandCollection.extend({
+  modelType: 'CollectionCollection',
+  mainIndex: '_id',
+  indexes: ['name'],
+  comparator: '_id',
+  model: CollectionModel,
+  /**
+   * @param {{ dataService: import('mongodb-data-service').DataService }} dataService
+   * @returns
+   */
+  fetch({ dataService }) {
+    return new Promise((resolve, reject) => {
+      const databaseName = this.parent && this.parent.getId();
 
-    var success = options.success;
-    options.success = function(resp) {
-      if (!model.set(model.parse(resp, options), options)) {
-        return false;
+      if (!databaseName) {
+        throw new Error(
+          "Trying to fetch MongoDBCollectionCollection that doesn't have the parent model"
+        );
       }
-      if (success) {
-        success(model, resp, options);
-      }
-      model.trigger('sync', model, resp, options);
-    };
 
-    var fn = options.error;
-    options.error = function(resp) {
-      if (fn) {
-        fn(this, resp, options);
-      }
-      this.trigger('error', this, resp, options);
-    };
-
-    var done = function(err, res) {
-      if (err) {
-        return options.error({}, 'error', err.message);
-      }
-      raf(function onClientSuccess() {
-        options.success(res, 'success', res);
+      dataService.listCollectionsNamesOnly(databaseName, (err, collections) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(
+          this.set(
+            collections.filter((coll) => {
+              // TODO: This is not the best place to do this kind of
+              // filtering, but for now this preserves the current behavior
+              // and changing it right away will expand the scope of the
+              // refactor significantly. We can address this in COMPASS-5211
+              return toNs(`${databaseName}.${coll.name}`).system === false;
+            })
+          )
+        );
       });
-    };
-
-    options.dataService.collection(this.getId(), options, done);
-  }
+    });
+  },
 });
 
 module.exports = CollectionModel;
-
-module.exports.Collection = MongoDBCollection.Collection.extend({
-  model: CollectionModel
-});
+module.exports.Collection = CollectionCollection;
