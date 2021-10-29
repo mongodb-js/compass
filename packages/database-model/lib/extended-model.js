b0733cea1a03f35e67aba2d6e37059806e976874
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-10-29 18:22:00 +0200

    feat(database-model): Add fetch method to the database model and collection so it can handle it's own data loading

---
+++ packages/database-model/lib/extended-model.js
@@ -0,0 +1,62 @@
+const AmpersandModel = require('ampersand-model');
+const AmpersandCollection = require('ampersand-collection');
+const {
+  Collection: MongoDbCollectionCollection
+} = require('mongodb-collection-model');
+
+const DatabaseModel = AmpersandModel.extend({
+  modelType: 'Database',
+  idAttribute: '_id',
+  props: {
+    _id: 'string',
+    name: 'string',
+    document_count: 'number',
+    storage_size: 'number',
+    index_count: 'number',
+    index_size: 'number'
+  },
+  collections: {
+    collections: MongoDbCollectionCollection
+  },
+  /**
+   * @param {{ dataService: import('mongodb-data-service').DataService }} dataService
+   * @returns
+   */
+  fetch({ dataService }) {
+    return new Promise((resolve, reject) => {
+      dataService.databaseStats(this.getId(), (err, databaseInfo) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(this.set(databaseInfo));
+      });
+    });
+  }
+});
+
+const DatabaseCollection = AmpersandCollection.extend({
+  modelType: 'DatabaseCollection',
+  mainIndex: '_id',
+  indexes: ['name'],
+  comparator: '_id',
+  model: DatabaseModel,
+  /**
+   * @param {{ dataService: import('mongodb-data-service').DataService }} dataService
+   * @returns
+   */
+  fetch({ dataService }) {
+    return new Promise((resolve, reject) => {
+      dataService.listDatabases({ nameOnly: true }, (err, databases) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(this.set(databases));
+      });
+    });
+  }
+});
+
+module.exports = DatabaseModel;
+module.exports.Collection = DatabaseCollection;
