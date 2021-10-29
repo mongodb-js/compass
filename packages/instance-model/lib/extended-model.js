5b86f7f56cfcd5d78fa09d0e64dc2da00b8ef55b
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-10-29 18:25:21 +0200

    refactor(instance-model): Refactor instance fetch method

--- packages/instance-model/lib/extended-model.js
+++ packages/instance-model/lib/extended-model.js
@@ -1,117 +1,79 @@
-var MongoDBInstance = require('./model');
-var MongoDBCollection = require('mongodb-collection-model');
-var MongoDBCollectionCollection = require('mongodb-collection-model').Collection;
-var BaseDatabaseModel = require('mongodb-database-model');
-var BaseDatabaseCollection = require('mongodb-database-model').Collection;
-var filterableMixin = require('ampersand-collection-filterable');
-var toNS = require('mongodb-ns');
-var raf = require('raf');
-var result = require('lodash.result');
+const AmpersandModel = require('ampersand-model');
 
-/**
- * A user selectable collection of `MongoDBCollection`'s with `specialish`
- * collections filtered out.
- */
-var MongoDBCollectionOnInstanceCollection = MongoDBCollectionCollection.extend({
-  namespace: 'MongoDBCollectionOnInstanceCollection',
-  model: MongoDBCollection,
-  parse: function(res) {
-    return res.filter(function(d) {
-      return !toNS(d._id).system;
-    });
+const {
+  Collection: MongoDbDatabaseCollection,
+} = require('mongodb-database-model');
+
+const HostInfo = AmpersandModel.extend({
+  props: {
+    arch: 'string',
+    cpu_cores: 'number',
+    cpu_frequency: 'number',
+    memory_bits: 'number',
+    os: 'string',
+    os_family: 'string',
+    kernel_version: 'string',
+    kernel_version_string: 'string',
+  },
+});
+
+const BuildInfo = AmpersandModel.extend({
+  props: {
+    isEnterprise: 'boolean',
+    version: 'string',
+  },
+});
+
+const GenuineMongoDB = AmpersandModel.extend({
+  props: {
+    isGenuine: 'boolean',
+    dbType: 'string',
+  },
+});
+
+const DataLake = AmpersandModel.extend({
+  props: {
+    isDataLake: 'boolean',
+    version: 'string',
+  },
+});
+
+const InstanceModel = AmpersandModel.extend({
+  modelType: 'Instance',
+  idAttribute: '_id',
+  props: {
+    _id: { type: 'string', required: true },
+    hostname: { type: 'string', required: true },
+    port: 'number',
+    status: { type: 'string', default: 'initial' },
+  },
+  children: {
+    host: HostInfo,
+    build: BuildInfo,
+    genuineMongoDB: GenuineMongoDB,
+    dataLake: DataLake,
+  },
+  collections: {
+    databases: MongoDbDatabaseCollection,
   },
   /**
-   * @param {Object} model you want to mark as selected.
-   * @return {Boolean} false if model already selected, true if incoming
-   * selected and previously selected toggled to true.
+   * @param {{ dataService: import('mongodb-data-service').DataService }} dataService
+   * @returns
    */
-  select: function(model) {
-    if (model.selected) {
-      return false;
-    }
-    raf(function selectableMarkModelSelected() {
-      var current = this.selected;
-      if (current) {
-        current.selected = false;
-      }
-      model.selected = true;
-      this.selected = model;
-      this.trigger('change:selected', this.selected);
-    }.bind(this));
-
-    return true;
-  },
-  unselectAll: function() {
-    if (this.selected) {
-      this.selected.set({
-        selected: false
+  fetch({ dataService }) {
+    const newStatus = this.status === 'initial' ? 'fetching' : 'refreshing';
+    this.set({ status: newStatus });
+    return new Promise((resolve, reject) => {
+      dataService.instance((err, instanceInfo) => {
+        if (err) {
+          this.set({ status: 'error' });
+          reject(err);
+          return;
+        }
+        resolve(this.set({ status: 'ready', ...instanceInfo }));
       });
-    }
-    this.selected = null;
-    this.trigger('change:selected', this.selected);
-  }
-}, filterableMixin);
-
-var DatabaseModel = BaseDatabaseModel.extend({
-  collections: {
-    collections: MongoDBCollectionOnInstanceCollection
-  }
+    });
+  },
 });
 
-var DatabaseCollection = BaseDatabaseCollection.extend({
-  model: DatabaseModel
-}, filterableMixin);
-
-/**
- * Metadata for a MongoDB Instance, such as a `db.hostInfo()`, `db.listDatabases()`,
- * `db.buildInfo()`, and more.
- *
- * @see http://npm.im/mongodb-instance-model
- */
-module.exports = MongoDBInstance.extend({
-  namespace: 'MongoDBInstance',
-  collections: {
-    databases: DatabaseCollection,
-    collections: MongoDBCollectionOnInstanceCollection
-  },
-  url: '/instance',
-  fetch: function(options) {
-    var model = this;
-    var url = result(model, 'url');
-
-    options = options ? { ...options } : {};
-    if (!options.parse) {
-      options.parse = true;
-    }
-
-    var success = options.success;
-    options.success = function(resp) {
-      if (!model.set(model.parse(resp, options), options)) {
-        return false;
-      }
-      if (success) {
-        success(model, resp, options);
-      }
-      model.trigger('sync', model, resp, options);
-    };
-
-    var fn = options.error;
-    options.error = function(resp) {
-      if (fn) {
-        fn(model, resp, options);
-      }
-      model.trigger('error', model, resp, options);
-    };
-
-    var done = function(err, res) {
-      if (err) {
-        return options.error({}, 'error', err.message);
-      }
-      raf(function onClientSuccess() {
-        options.success(res, 'success', res);
-      });
-    };
-
-    options.dataService.instance(done);
-  }
-});
+module.exports = InstanceModel;
