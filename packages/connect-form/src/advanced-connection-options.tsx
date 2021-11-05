41f25294e41dc640c682dc0d1b3a2533b1cd90e2
Leonardo Rossi <leonardo.rossi@gmail.com>
2021-11-05 17:25:54 +0100

    uses paragraph for accordion title

--- packages/connect-form/src/advanced-connection-options.tsx
+++ packages/connect-form/src/advanced-connection-options.tsx
@@ -1,31 +1,45 @@
 import React, { useState } from 'react';
 import { Label, spacing } from '@mongodb-js/compass-components';
 import { css } from '@emotion/css';
+import Icon from '@leafygreen-ui/icon';
 
 import AdvancedOptionsTabs from './advanced-options-tabs/advanced-options-tabs';
 
 const labelStyles = css({
   padding: 0,
   margin: 0,
-  marginTop: spacing[3],
+  fontWeight: 'bold'
 });
+
+const containerStyles = css({
+  marginTop: spacing[3],
+  display: 'flex',
+  alignContent: 'center',
+  '&:hover': {
+    cursor: 'pointer'
+  }
+})
 
 function AdvancedConnectionOptions() : React.ReactElement {
   const [open, setOpen] = useState(false)
   const chevronClass = open ? 'fa-chevron-down' : 'fa-chevron-right'
   return (
     <div>
-      <Label htmlFor={''} className={labelStyles}
+      <div 
+        className={containerStyles}
         onClick={(e) => setOpen(!open)}
       >
-        <i className={'fa ' + chevronClass}></i>&nbsp;
-        Advanced connection options</Label>
-
+        <Icon glyph={ open ? 'ChevronDown' : 'ChevronRight'} />
+        <p
+          className={labelStyles}
+          
+        >Advanced connection options</p>
+      </div>
       {open &&(
-        <AdvancedOptionsTabs />
-      )}
+          <AdvancedOptionsTabs />
+        )}
     </div>
-   
+    
 
   )
 }
