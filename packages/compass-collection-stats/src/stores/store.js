9b8a800b37730171a847b2fac66d1d831d67ef8c
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-11-10 11:35:03 +0100

    chore(collection-stats): Clarify why we are storing references to the appRegistry and instance on the store

--- packages/compass-collection-stats/src/stores/store.js
+++ packages/compass-collection-stats/src/stores/store.js
@@ -1,12 +1,10 @@
+/* eslint-disable camelcase */
 import Reflux from 'reflux';
 import StateMixin from 'reflux-state-mixin';
 import toNS from 'mongodb-ns';
 import numeral from 'numeral';
 import createDebug from 'debug';
 const debug = createDebug('compass-collection-stats:store');
-
-const kInstance = Symbol('instance');
-const kGlobalAppRegistry = Symbol('globalAppRegistry');
 
 /**
  * Invalid stats.
@@ -28,7 +26,7 @@ const store = Reflux.createStore({
       namespace: collection.ns,
       isReadonly: collection.readonly,
       isTimeSeries: collection.type === 'timeseries',
-      ...this._parseCollectionDetails(collection),
+      ...this._formatCollectionStats(collection),
     };
     this.setState(newState);
   },
@@ -59,32 +57,27 @@ const store = Reflux.createStore({
     };
   },
 
-  _parseCollectionDetails(result) {
+  _formatCollectionStats(collectionModel) {
+    const { document_count, document_size, index_count, index_size } =
+      collectionModel;
+
     return {
       documentCount:
-        result.document_count !== undefined
-          ? this._format(result.document_count)
-          : INVALID,
-      totalDocumentSize: this._format(result.document_size, 'b'),
+        document_count !== undefined ? this._format(document_count) : INVALID,
+      totalDocumentSize: this._format(document_size, 'b'),
       avgDocumentSize: this._format(
-        this._avg(result.document_size, result.document_count),
+        this._avg(document_size, document_count),
         'b'
       ),
-      indexCount: this._format(result.index_count),
-      totalIndexSize: this._format(result.index_size, 'b'),
-      avgIndexSize: this._format(
-        this._avg(result.index_size, result.index_count),
-        'b'
-      ),
-      rawDocumentCount: result.document_count,
-      rawTotalDocumentSize: result.document_size,
-      rawAvgDocumentSize: this._avg(
-        result.document_size,
-        result.document_count
-      ),
-      rawIndexCount: result.index_count,
-      rawTotalIndexSize: result.index_size,
-      rawAvgIndexSize: this._avg(result.index_size, result.index_count),
+      indexCount: this._format(index_count),
+      totalIndexSize: this._format(index_size, 'b'),
+      avgIndexSize: this._format(this._avg(index_size, index_count), 'b'),
+      rawDocumentCount: document_count,
+      rawTotalDocumentSize: document_size,
+      rawAvgDocumentSize: this._avg(document_size, document_count),
+      rawIndexCount: index_count,
+      rawTotalIndexSize: index_size,
+      rawAvgIndexSize: this._avg(index_size, index_count),
     };
   },
 
@@ -113,20 +106,26 @@ function onCollectionStatusChange(model, status) {
   }
 }
 
+// We use these symbols so that nothing from outside can access these values on
+// the store
+const kInstance = Symbol('instance');
+const kGlobalAppRegistry = Symbol('globalAppRegistry');
+
 function onInstanceDestroyed() {
+  // When instance destroyed, remove the reference so that configureStore knows
+  // that we need to re-attach status change listeners again
   store[kInstance] = null;
 }
 
 /**
  * Collection Stats store.
  */
-const configureStore = ({
-  namespace,
-  globalAppRegistry
-} = {}) => {
+const configureStore = ({ namespace, globalAppRegistry } = {}) => {
   const { instance } =
     globalAppRegistry.getStore('App.InstanceStore')?.getState() ?? {};
 
+  // We only want to attach listeners to the instance once if we haven't done it
+  // before, we use stored instances as a flag to determine if we did it already
   if (!store[kGlobalAppRegistry]) {
     store[kGlobalAppRegistry] = globalAppRegistry;
     globalAppRegistry.on('instance-destroyed', onInstanceDestroyed);
