89610529714a7743a7a3f85931c9fd3b10b63297
Basit <1305718+mabaasit@users.noreply.github.com>
2021-11-11 15:30:39 +0100

    feat(tracking): schema analysis COMPASS-5194 (#2572)

--- packages/compass-schema/src/stores/store.js
+++ packages/compass-schema/src/stores/store.js
@@ -2,6 +2,8 @@ import Reflux from 'reflux';
 import ipc from 'hadron-ipc';
 import StateMixin from 'reflux-state-mixin';
 import toNS from 'mongodb-ns';
+import { createLoggerAndTelemetry } from '@mongodb-js/compass-logging';
+import { intersection } from 'lodash';
 import { addLayer, generateGeoQuery } from '../modules/geo';
 import createSchemaAnalysis from '../modules/schema-analysis';
 import {
@@ -14,11 +16,14 @@ import {
 import { TAB_NAME } from '../constants/plugin';
 
 const debug = require('debug')('mongodb-compass:stores:schema');
+const { track } = createLoggerAndTelemetry('COMPASS-SCHEMA-UI');
 
 const DEFAULT_MAX_TIME_MS = 60000;
 const DEFAULT_SAMPLE_SIZE = 1000;
 
 const ERROR_CODE_MAX_TIME_MS_EXPIRED = 50;
+
+const MONGODB_GEO_TYPES = ['Point', 'LineString', 'Polygon', 'MultiPoint', 'MultiLineString', 'MultiPolygon', 'GeometryCollection'];
 
 function getErrorState(err) {
   const errorMessage = (err && err.message) || 'Unknown error';
@@ -217,6 +222,65 @@ const configureStore = (options = {}) => {
       }
     },
 
+    _calculateDepthByPath(input, paths) {
+      if (!input) {
+        return paths;
+      }
+      input.forEach(({types, fields, path, bsonType}) => {
+        /*
+         * Given the strucutre of schema, in case of an array,
+         * the path to an array item is still the same because indexes are not
+         * counted and for that reason we have an increment of 1 for arrays.
+         */
+        const increment = bsonType === 'Array' ? 1 : 0;
+        const score = path.split('.').length + increment;
+        if (!paths[path] || paths[path] < score) {
+          paths[path] = score;
+        }
+        this._calculateDepthByPath(types ?? fields, paths);
+      });
+
+      return paths;
+    },
+
+    calculateSchemaDepth(schema) {
+      const response = this._calculateDepthByPath(schema.fields, {});
+      const values = Object.values(response);
+      return Math.max(...values, 0);
+    },
+
+    _containsGeoData(input) {
+      let result = false;
+      if (!input) {
+        return result;
+      }
+      for (const { path, values, types, fields } of input) {
+        if (path.endsWith('.type') && intersection(MONGODB_GEO_TYPES, values).length > 0) {
+          result = true;
+        }
+        if (!result) {
+          result = this._containsGeoData(types ?? fields);
+        }
+      }
+      return result;
+    },
+
+    schemaContainsGeoData(schema) {
+      return this._containsGeoData(schema.fields);
+    },
+
+    _trackSchemaAnalyzed(analysisTime) {
+      const { schema } = this.state;
+      const trackEvent = {
+        with_filter: Object.entries(this.query.filter).length > 0,
+        schema_width: schema.fields.length,
+        schema_depth: this.calculateSchemaDepth(schema),
+        geo_data: this.schemaContainsGeoData(schema),
+        analysis_time_ms: analysisTime,
+      };
+      track('Schema Analyzed', trackEvent);
+    },
+
     startAnalysis: async function() {
       if (this.schemaAnalysis) {
         return;
@@ -257,7 +321,9 @@ const configureStore = (options = {}) => {
           schema: null
         });
 
+        const analysisStartTime = Date.now();
         const schema = await schemaAnalysis.getResult();
+        const analysisTime = Date.now() - analysisStartTime;
 
         this.setState({
           analysisState: schema ?
@@ -266,6 +332,8 @@ const configureStore = (options = {}) => {
           schema: schema,
           resultId: resultId()
         });
+
+        this._trackSchemaAnalyzed(analysisTime);
 
         this.onSchemaSampled();
       } catch (err) {
