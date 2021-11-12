92b28ba30e161e180fdc419644e1b966409883d3
Anemy <rhysh@live.com>
2021-11-12 13:46:42 -0500

    remove comment

--- packages/compass-preferences-model/index.d.ts
+++ packages/compass-preferences-model/index.d.ts
@@ -3,13 +3,6 @@ import AmpersandModel from 'ampersand-model';
 export type CompassPreferencesModel = typeof AmpersandModel;
 
 export = CompassPreferencesModel;
-// export const THEMES = {
-//   DARK: 'DARK',
-//   LIGHT: 'LIGHT',
-//   OS_THEME: 'OS_THEME'
-// };
-// export type THEME_TYPE = THESE.DARK | THEMES.LIGHT | OS_THEME;
-
 
 export enum THEMES {
   DARK = 'DARK',
