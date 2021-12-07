2142bf109a1b40b1c1a3a2d349399472714fadbd
Sergey Petushkov <petushkov.sergey@gmail.com>
2021-12-07 12:11:55 +0100

    chore(compass-components): Move placeholder to shared components; Add light mode styles and animation

---
+++ packages/compass-components/src/components/placeholder.tsx
@@ -0,0 +1,81 @@
+/* eslint-disable react/prop-types */
+import React, { CSSProperties, useMemo } from 'react';
+import { css, cx, keyframes } from '../';
+import { uiColors, spacing } from '../';
+
+// Ratio of showing a highlight passing through the placeholder to background color
+const scale = 4;
+
+const percent = (num: number): string => (num * 100).toFixed(3) + '%';
+
+const move = keyframes({
+  from: {
+    backgroundPosition: `${percent(scale)} 0`,
+  },
+  to: {
+    backgroundPosition: '0 0',
+  },
+});
+
+const placeholder = css({
+  '--gradient-start': uiColors.gray.light3,
+  '--gradient-end': 'rgba(235, 241, 239, 1)',
+  alignSelf: 'center',
+  borderRadius: 3,
+  maxWidth: '80%',
+  backgroundColor: 'var(--gradient-start)',
+  backgroundImage: `linear-gradient(
+        to right,
+        var(--gradient-start) 0%,
+        var(--gradient-end) ${percent(1 / scale / 2)},
+        var(--gradient-start) ${percent(1 / scale)},
+        var(--gradient-start) 100%
+    )`,
+  backgroundSize: `${percent(scale)} 100%`,
+  backgroundPosition: '0 0',
+  animation: `${move} ${scale}s infinite linear`,
+});
+
+const placeholderDarkMode = css({
+  '--gradient-start': 'rgba(38, 55, 66, 1)',
+  '--gradient-end': 'rgba(47, 64, 74, 1)',
+});
+
+function getBoundRandom(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
+export const Placeholder: React.FunctionComponent<
+  Omit<
+    React.HTMLProps<HTMLDivElement>,
+    'minChar' | 'maxChar' | 'width' | 'height'
+  > & {
+    minChar?: number;
+    maxChar?: number;
+    width?: CSSProperties['width'];
+    height?: CSSProperties['height'];
+    darkMode?: boolean;
+  }
+> = ({
+  className,
+  minChar = 5,
+  maxChar = 15,
+  width: propsWidth,
+  height: propsHeight = spacing[3],
+  darkMode,
+  ...props
+}) => {
+  const width = useMemo(() => {
+    return propsWidth || `${Math.round(getBoundRandom(minChar, maxChar))}ch`;
+  }, [minChar, maxChar, propsWidth]);
+
+  return (
+    <div
+      {...props}
+      role="presentation"
+      data-testid="placeholder"
+      className={cx(placeholder, className, darkMode && placeholderDarkMode)}
+      style={{ width, height: propsHeight }}
+    ></div>
+  );
+};
