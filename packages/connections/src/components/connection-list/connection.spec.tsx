e0553fc1e17bb2c92fbe0384b599e7aef3a4f9ab
Basit <basit.chonka@mongodb.com>
2021-12-20 14:54:06 +0100

    fix(connectionList): tests

--- packages/connections/src/components/connection-list/connection.spec.tsx
+++ packages/connections/src/components/connection-list/connection.spec.tsx
@@ -6,10 +6,12 @@ import sinon from 'sinon';
 import Connection from './connection';
 
 describe('Connection Component', function () {
-  let onClickSpy;
+  let onClickSpy: sinon.SinonSpy<any[], any>;
+  let onDoubleClickSpy: sinon.SinonSpy<any[], any>;
 
   beforeEach(function () {
     onClickSpy = sinon.spy();
+    onDoubleClickSpy = sinon.spy();
   });
 
   describe('when it has a lastUsed date', function () {
@@ -29,8 +31,8 @@ describe('Connection Component', function () {
     });
 
     it('shows the date as a string', function () {
-      const dateStringElement = screen.getByText('Dec 17, 1995, 12:00 AM');
-      expect(dateStringElement).to.not.equal(null);
+      const dateTime = screen.getAllByTestId('recent-connection-description')[0].textContent;
+      expect(dateTime).to.equal('17 Dec 1995, 00:00');
     });
   });
 
@@ -77,9 +79,13 @@ describe('Connection Component', function () {
         );
       });
 
-      it('there is no favorite color', function () {
-        const favoriteIndicator = screen.queryByTestId('connection-icon');
-        expect(favoriteIndicator).to.equal(null);
+      it('it uses white by default', function () {
+        const favoriteIndicator = screen.getByTestId('connection-icon');
+        expect(
+          getComputedStyle(favoriteIndicator).getPropertyValue(
+            'color'
+          )
+        ).to.equal('rgb(255, 255, 255)');
       });
     });
 
@@ -102,12 +108,11 @@ describe('Connection Component', function () {
         );
       });
 
-      it('has a favorite indicator with the favorite color', function () {
+      it('it uses favorite color', function () {
         const favoriteIndicator = screen.getByTestId('connection-icon');
-
         expect(
           getComputedStyle(favoriteIndicator).getPropertyValue(
-            'background-color'
+            'color'
           )
         ).to.equal('rgb(95, 200, 110)');
       });
@@ -129,9 +134,14 @@ describe('Connection Component', function () {
       );
     });
 
-    it('there is no favorite name', function () {
+    it('there is an icon and its white', function () {
       const favoriteIndicator = screen.queryByTestId('connection-icon');
-      expect(favoriteIndicator).to.equal(null);
+      expect(favoriteIndicator).to.not.equal(null);
+      expect(
+        getComputedStyle(favoriteIndicator).getPropertyValue(
+          'color'
+        )
+      ).to.equal('rgb(255, 255, 255)');
     });
 
     it('it shows the connection title as the name', function () {
@@ -218,6 +228,149 @@ describe('Connection Component', function () {
 
     it('calls the onclick handler', function () {
       expect(onClickSpy.called).to.equal(true);
+    });
+  });
+
+  describe('when double clicked', function () {
+    beforeEach(function () {
+      render(
+        <Connection
+          isActive={false}
+          connectionInfo={{
+            connectionOptions: {
+              connectionString: 'double-click.com',
+            },
+            favorite: {
+              name: 'double-click',
+            },
+          }}
+          onDoubleClick={onDoubleClickSpy}
+        />
+      );
+      const button = screen.getByText('double-click').closest('button');
+      expect(onDoubleClickSpy.called).to.equal(false);
+      fireEvent(
+        button,
+        new MouseEvent('dblclick', {
+          bubbles: true,
+          cancelable: true,
+        })
+      );
+    });
+
+    it('calls the ondoubleclick handler', function () {
+      expect(onDoubleClickSpy.called).to.equal(true);
+      const [connectionInfo] = onDoubleClickSpy.getCall(0).args;
+      expect(connectionInfo).to.deep.equal({
+        connectionOptions: {
+          connectionString: 'double-click.com',
+        },
+        favorite: {
+          name: 'double-click',
+        },
+      });
+    });
+  });
+
+  describe('when connection is active', function() {
+    describe('and is favorite', function() {
+      describe('and has a favorite color', function() {
+        beforeEach(function () {
+          render(
+            <Connection
+              isActive={true}
+              connectionInfo={{
+                id: '1234',
+                connectionOptions: {
+                  connectionString: '',
+                },
+                favorite: {
+                  name: 'aaa',
+                  color: 'rgb(95, 200, 110)',
+                },
+              }}
+            />
+          );
+        });
+        it('uses favorite color as background color and white as text color', function() {
+          const connection = screen.getByTestId('saved-connection-button-1234');
+          expect(connection).to.not.equal(null);
+          const styles = getComputedStyle(connection);
+          expect(
+            styles.getPropertyValue(
+              'color'
+            )
+          ).to.equal('rgb(255, 255, 255)');
+          expect(
+            styles.getPropertyValue(
+              'background-color'
+            )
+          ).to.equal('rgb(95, 200, 110)');
+        });
+      });
+      describe('and does not have a favorite color', function() {
+        beforeEach(function () {
+          render(
+            <Connection
+              isActive={true}
+              connectionInfo={{
+                id: '1234',
+                connectionOptions: {
+                  connectionString: '',
+                },
+                favorite: {
+                  name: 'aaa',
+                },
+              }}
+            />
+          );
+        });
+        it('uses dark gray as background color and white as text color', function() {
+          const connection = screen.getByTestId('saved-connection-button-1234');
+          expect(connection).to.not.equal(null);
+          const styles = getComputedStyle(connection);
+          expect(
+            styles.getPropertyValue(
+              'color'
+            )
+          ).to.equal('rgb(255, 255, 255)');
+          expect(
+            styles.getPropertyValue(
+              'background-color'
+            )
+          ).to.equal('rgb(61, 79, 88)');
+        });
+      });
+    });
+    describe('and is not favorite', function() {
+      beforeEach(function () {
+        render(
+          <Connection
+            isActive={true}
+            connectionInfo={{
+              id: '1234',
+              connectionOptions: {
+                connectionString: '',
+              },
+            }}
+          />
+        );
+      });
+      it('uses dark gray as background color and white as text color', function() {
+        const connection = screen.getByTestId('saved-connection-button-1234');
+        expect(connection).to.not.equal(null);
+        const styles = getComputedStyle(connection);
+        expect(
+          styles.getPropertyValue(
+            'color'
+          )
+        ).to.equal('rgb(255, 255, 255)');
+        expect(
+          styles.getPropertyValue(
+            'background-color'
+          )
+        ).to.equal('rgb(61, 79, 88)');
+      });
     });
   });
 });
