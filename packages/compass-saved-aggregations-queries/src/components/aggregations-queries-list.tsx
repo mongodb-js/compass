8909b26822434f90443332e750d766ab6c743581
Sergey Petushkov <petushkov.sergey@gmail.com>
2022-01-13 14:16:13 +0100

    chore(compass-saved-aggregations-queries): Bootstrap new plugin code; Register new tab role; Hide new tab behind feature flag

---
+++ packages/compass-saved-aggregations-queries/src/components/aggregations-queries-list.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const AggregationsQueriesList: React.FunctionComponent = () => {
+  return <div>Hello, world!</div>;
+};
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AggregationsQueriesList);
