8fbc6965f6c8ffbf8cf8ba2d1710d2a423939f20
mcasimir <maurizio.cas@gmail.com>
2022-01-16 11:57:15 +0100

    remove unused vars

--- packages/connections/src/components/connections.tsx
+++ packages/connections/src/components/connections.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Banner,
   BannerVariant,
-  MongoDBLogo,
   compassUIColors,
   spacing,
   css,
@@ -30,11 +29,6 @@ const connectStyles = css({
   display: 'flex',
   flexDirection: 'row',
   background: compassUIColors.gray8,
-});
-
-const logoStyles = css({
-  margin: spacing[5],
-  marginBottom: 0,
 });
 
 const connectItemContainerStyles = css({
