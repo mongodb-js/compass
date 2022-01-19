7970f929d0cba499a9f436ab970323d6f7e3277e
Basit <basit.chonka@mongodb.com>
2022-01-19 13:39:30 +0100

    chore(compass-saved-aggregations-queries): setup store

--- packages/compass-saved-aggregations-queries/src/components/aggregations-queries-list.tsx
+++ packages/compass-saved-aggregations-queries/src/components/aggregations-queries-list.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { fetchItems, State } from './../stores/aggregations-queries-items';
 
-const AggregationsQueriesList: React.FunctionComponent = () => {
-  return <div>Hello, world!</div>;
+const AggregationsQueriesList = ({
+  loading,
+  items,
+  fetchItems,
+}: PropsFromRedux) => {
+  useEffect(() => {
+    fetchItems();
+  }, [fetchItems]);
+
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (!loading && !items.length) {
+    return <p>No saved queries/aggregations.</p>;
+  }
+  return (
+    <div>{`Hello, world! We have ${items.length} saved queries/aggregations`}</div>
+  );
 };
 
-const mapStateToProps = () => ({});
+const mapState = ({ loading, items }: State) => ({
+  loading,
+  items,
+});
 
-const mapDispatchToProps = {};
+const mapDispatch = {
+  fetchItems,
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AggregationsQueriesList);
+const connector = connect(mapState, mapDispatch);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+export default connector(AggregationsQueriesList);
--- packages/compass-saved-aggregations-queries/src/stores/aggregations-queries-items.ts
+++ packages/compass-saved-aggregations-queries/src/stores/aggregations-queries-items.ts
@@ -1,5 +1,109 @@
-function reducer(state = {}): typeof state {
+import { remote } from 'electron';
+import { join } from 'path';
+import fs from 'fs';
+import { FavoriteQueryCollection } from '@mongodb-js/compass-query-history';
+interface Item {
+  id: string;
+  name: string;
+  namespace: string;
+  lastModified: number;
+}
+
+export interface Aggregation extends Item {
+  pipeline: unknown[];
+}
+
+export interface Query extends Item {
+  collation: Record<string, unknown>;
+  filter: Record<string, unknown>;
+  limit: number;
+  project: Record<string, unknown>;
+  skip: number;
+  sort: Record<string, number>;
+}
+
+export type State = {
+  loading: boolean;
+  items: Array<Aggregation | Query>;
+};
+
+type Action = {
+  type: string;
+};
+
+const INITIAL_STATE: State = {
+  loading: true,
+  items: [],
+};
+
+const FETCH_DATA = 'FETCH_DATA';
+
+function reducer(state = INITIAL_STATE, action: Action): State {
+  if (action.type === FETCH_DATA) {
+    return {
+      items: [...getAggregations(), ...getQueries()],
+      loading: false,
+    };
+  }
   return state;
 }
+
+export const fetchItems = (): Action => ({
+  type: FETCH_DATA
+});
+
+const getAggregations = () => {
+  const dir = join(remote.app.getPath('userData'), 'SavedPipelines');
+  const aggregations = fs
+    .readdirSync(dir)
+    .filter((file) => file.endsWith('.json'))
+    .map((file) => {
+      const filePath = join(dir, file);
+      const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+      data.lastModified = fs.statSync(filePath).mtimeMs;
+      return data;
+    });
+  return mapAggregations(aggregations);
+};
+
+const mapAggregations = (aggregations: unknown[]): Aggregation[] => {
+  return aggregations.map((aggregation: any) => ({
+    id: aggregation.id,
+    name: aggregation.name,
+    namespace: aggregation.namespace,
+    lastModified: aggregation.lastModified,
+
+    pipeline: aggregation.pipeline,
+  }));
+};
+
+const getQueries = (): Query[] => {
+  const collection = new FavoriteQueryCollection();
+  collection.fetch({
+    success: () => {
+      // todo: return mapped list here
+    },
+  });
+  return mapQueryModels(collection.models);
+};
+
+const mapQueryModels = (models: unknown[]): Query[] => {
+  return models.map((model: any) => {
+    const { _values } = model;
+    return {
+      id: _values._id,
+      name: _values._name,
+      namespace: _values._ns,
+      lastModified: _values.dateSaved,
+
+      collation: _values.collation,
+      filter: _values.filter,
+      limit: _values.limit,
+      project: _values.project,
+      skip: _values.skip,
+      sort: _values.sort,
+    };
+  });
+};
 
 export default reducer;
