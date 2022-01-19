63dd5fbffeb7a855f8eaf5ad5977edf6ef885e4d
Anemy <rhysh@live.com>
2022-01-19 02:06:29 -0500

    lint x2 :)

--- packages/connect-form/src/components/connection-string-input.spec.tsx
+++ packages/connect-form/src/components/connection-string-input.spec.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  render,
-  screen,
-  cleanup,
-} from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { expect } from 'chai';
 import sinon from 'sinon';
