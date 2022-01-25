beaa171b5030dac437eebbbf37ada99623f70f14
Basit <basit.chonka@mongodb.com>
2022-01-25 11:06:42 +0100

    tests

--- packages/compass-query-history/src/models/favorite-query.js
+++ packages/compass-query-history/src/models/favorite-query.js
@@ -10,7 +10,7 @@ const FavoriteQuery = Query.extend(storageMixin, {
   namespace: 'FavoriteQueries',
   storage: {
     backend: 'disk',
-    basepath: remote.app.getPath('userData')
+    basepath: process.env.MONGODB_COMPASS_STORAGE_MIXIN_TEST_BASE_PATH || remote.app.getPath('userData')
   },
   props: {
     /**
