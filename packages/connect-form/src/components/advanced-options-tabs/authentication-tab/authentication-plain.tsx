617205fabe99d9a6c783aff643e3dbfcbe012e3b
Maurizio Casimirri <maurizio.cas@gmail.com>
2022-01-26 19:39:50 +0100

    feat(connect-form): Add PLAIN and x509 auth mechanisms COMPASS-5436, COMPASS-5437 (#2729)
    * add ldap auth
    
    * add x509 auth
    
    * fix display error and add improve info for x509

--- packages/connect-form/src/components/advanced-options-tabs/authentication-tab/authentication-plain.tsx
+++ packages/connect-form/src/components/advanced-options-tabs/authentication-tab/authentication-plain.tsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import { TextInput } from '@mongodb-js/compass-components';
 
-function AuthenticationPlain(): React.ReactElement {
+import ConnectionStringUrl from 'mongodb-connection-string-url';
+import { UpdateConnectionFormField } from '../../../hooks/use-connect-form';
+import FormFieldContainer from '../../form-field-container';
+import {
+  ConnectionFormError,
+  errorMessageByFieldName,
+} from '../../../utils/validation';
+import {
+  getConnectionStringPassword,
+  getConnectionStringUsername,
+} from '../../../utils/connection-string-helpers';
+
+export const PLAIN_USERNAME_LABEL = 'Username';
+export const PLAIN_PASSWORD_LABEL = 'Password';
+
+function AuthenticationPlain({
+  connectionStringUrl,
+  updateConnectionFormField,
+  errors,
+}: {
+  connectionStringUrl: ConnectionStringUrl;
+  errors: ConnectionFormError[];
+  updateConnectionFormField: UpdateConnectionFormField;
+}): React.ReactElement {
+  const username = getConnectionStringUsername(connectionStringUrl);
+  const password = getConnectionStringPassword(connectionStringUrl);
+  const usernameError = errorMessageByFieldName(errors, 'username');
+  const passwordError = errorMessageByFieldName(errors, 'password');
+
   return (
     <>
-      <p>LDAP</p>
+      <FormFieldContainer>
+        <TextInput
+          onChange={({
+            target: { value },
+          }: React.ChangeEvent<HTMLInputElement>) => {
+            updateConnectionFormField({
+              type: 'update-username',
+              username: value,
+            });
+          }}
+          label={PLAIN_USERNAME_LABEL}
+          value={username || ''}
+          errorMessage={usernameError}
+          state={usernameError ? 'error' : undefined}
+        />
+      </FormFieldContainer>
+      <FormFieldContainer>
+        <TextInput
+          onChange={({
+            target: { value },
+          }: React.ChangeEvent<HTMLInputElement>) => {
+            updateConnectionFormField({
+              type: 'update-password',
+              password: value,
+            });
+          }}
+          label={PLAIN_PASSWORD_LABEL}
+          type="password"
+          value={password || ''}
+          errorMessage={passwordError}
+          state={passwordError ? 'error' : undefined}
+        />
+      </FormFieldContainer>
     </>
   );
 }
--- packages/connect-form/src/utils/validation.ts
+++ packages/connect-form/src/utils/validation.ts
@@ -9,8 +9,6 @@ export type FieldName =
   | 'hosts'
   | 'isSrv'
   | 'kerberosPrincipal'
-  | 'ldapPassword'
-  | 'ldapUsername'
   | 'password'
   | 'schema'
   | 'proxyHostname'
