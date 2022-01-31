77f924d2ff344233e8d8d6382cc11067c6f1f2fe
Basit <basit.chonka@mongodb.com>
2022-01-31 19:20:32 +0100

    fix(compass-saved-aggregations-queries): add os-dns to webpack and BigInteger to global scope for tests

--- packages/compass-saved-aggregations-queries/src/index.spec.ts
+++ packages/compass-saved-aggregations-queries/src/index.spec.ts
@@ -1,3 +1,7 @@
+// this is to avoid BigInteger is not defined error, which is thrown in ipv6/lib/node/bigint.js
+// import chain: compass-query-history -> mongodb-data-service -> ssh-tunnel -> socksv5 -> ipv6 (which is pretty old and deprecated).
+globalThis.BigInteger = undefined;
+
 import { expect } from 'chai';
 import * as CompassPlugin from './index';
 
