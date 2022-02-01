2d48d7d13456127cabdbad79ba83fc332e194d83
Basit <basit.chonka@mongodb.com>
2022-02-01 10:41:33 +0100

    fix(compass-saved-aggregations-queries): setup tests

--- packages/compass-saved-aggregations-queries/src/index.spec.ts
+++ packages/compass-saved-aggregations-queries/src/index.spec.ts
@@ -1,7 +1,3 @@
-// this is to avoid BigInteger is not defined error, which is thrown in ipv6/lib/node/bigint.js
-// import chain: compass-query-history -> mongodb-data-service -> ssh-tunnel -> socksv5 -> ipv6 (which is pretty old and deprecated).
-globalThis.BigInteger = undefined;
-
 import { expect } from 'chai';
 import * as CompassPlugin from './index';
 
