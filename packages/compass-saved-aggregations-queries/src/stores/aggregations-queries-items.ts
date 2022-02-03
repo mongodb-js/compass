f9a3e94cc78a05e6766e58c8a711374aaa10eb1c
Basit <basit.chonka@mongodb.com>
2022-02-03 01:01:45 +0100

    feature(compass-saved-aggregations-queries): add query & aggregation types

--- packages/compass-saved-aggregations-queries/src/stores/aggregations-queries-items.ts
+++ packages/compass-saved-aggregations-queries/src/stores/aggregations-queries-items.ts
@@ -18,14 +18,26 @@ export type Item = {
   name: string;
   database: string;
   collection: string;
-  type: 'query' | 'aggregation';
-};
+} & ({
+  type: 'query';
+  query: Query;
+} | {
+  type: 'aggregation';
+  aggregation: Aggregation;
+});
 
 interface Query {
   _id: string;
   _name: string;
   _ns: string;
   _dateSaved: number;
+
+  collation?: Record<string, unknown>;
+  filter?: Record<string, unknown>;
+  limit?: number;
+  project?: Record<string, unknown>;
+  skip?: number;
+  sort?: Record<string, number>;
 }
 
 interface Aggregation {
@@ -33,6 +45,17 @@ interface Aggregation {
   name: string;
   namespace: string;
   lastModified: number;
+
+  autoPreview?: boolean;
+  collation?: string;
+  collationString?: string;
+  comments?: boolean;
+  env?: string;
+  isReadonly?: boolean;
+  isTimeSeries?: boolean;
+  pipeline: unknown[];
+  sample?: boolean;
+  sourceName?: string;
 }
 
 export type State = {
@@ -86,6 +109,7 @@ const getAggregationItems = async (): Promise<Item[]> => {
       database,
       collection,
       type: 'aggregation',
+      aggregation,
     };
   });
 };
@@ -101,6 +125,7 @@ const getQueryItems = async (): Promise<Item[]> => {
       database,
       collection,
       type: 'query',
+      query
     };
   });
 };
