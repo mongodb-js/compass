c802845c43c3c62b75fb3ed7f0aea435c3bc6ef6
Maurizio Casimirri <maurizio.cas@gmail.com>
2022-02-03 15:10:08 +0100

    Update packages/connect-form/src/hooks/use-connection-color.ts
    Co-authored-by: Anna Henningsen <anna.henningsen@mongodb.com>

--- packages/connect-form/src/hooks/use-connection-color.ts
+++ packages/connect-form/src/hooks/use-connection-color.ts
@@ -1,16 +1,6 @@
 import { useCallback } from 'react';
 
-type ColorCode =
-  | 'color1'
-  | 'color2'
-  | 'color3'
-  | 'color4'
-  | 'color5'
-  | 'color6'
-  | 'color7'
-  | 'color8'
-  | 'color9'
-  | 'color10';
+type ColorCode = `color${1|2|3|4|5|6|7|8|9|10}`;
 
 const COLOR_CODES_TO_UI_COLORS_DARK_THEME_MAP: Record<ColorCode, string> = {
   color1: '#00A35C', // green.dark1
