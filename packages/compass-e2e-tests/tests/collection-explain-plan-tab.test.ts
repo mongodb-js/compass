94d097656bf851bd21464a1659624f1ef31b9890
Le Roux Bodenstein <lerouxb@gmail.com>
2022-02-08 17:36:52 +0000

    just the one test for the foreseeable future (#2769)

--- packages/compass-e2e-tests/tests/collection-explain-plan-tab.test.ts
+++ packages/compass-e2e-tests/tests/collection-explain-plan-tab.test.ts
@@ -27,7 +27,7 @@ describe('Collection explain plan tab', function () {
     await afterTest(compass, this.currentTest);
   });
 
-  it('supports queries not covered by an index', async function () {
+  it('shows an explain plan', async function () {
     await browser.clickVisible(Selectors.ExecuteExplainButton);
 
     const element = await browser.$(Selectors.ExplainSummary);
@@ -35,9 +35,4 @@ describe('Collection explain plan tab', function () {
     const stages = await browser.$$(Selectors.ExplainStage);
     expect(stages).to.have.lengthOf(1);
   });
-
-  it('supports queries covered by an index');
-  it('supports queries with a sort covered by an index');
-  it('supports maxTimeMS');
-  it('shows a visual explain plan for queries that result in a tree');
 });
