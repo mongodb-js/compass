cacae99f9da73a297502dcd9d4f81b2844a100fa
Basit <basit.chonka@mongodb.com>
2022-02-15 14:32:46 +0100

    feat(compass-saved-aggregations-queries): fix mock implementation

--- packages/compass-saved-aggregations-queries/test/mock.ts
+++ packages/compass-saved-aggregations-queries/test/mock.ts
@@ -1,26 +1,44 @@
-interface FavoriteQueryStorageClass<T> {
+/* eslint-disable @typescript-eslint/no-unused-vars */
+type UpdateAttributes = Record<string, unknown>;
+interface FavoriteQueryStorage<T> {
   new (): {
     loadAll(): Promise<T>;
-    delete(): Promise<void>;
+    updateAttributes(id: string, attributes: UpdateAttributes): Promise<void>;
+    delete(id: string): Promise<void>;
   };
 }
 
 interface PipelineStorageClass<T> {
   new (): {
     loadAll(): Promise<T>;
-    delete(): Promise<void>;
+    updateAttributes(id: string, attributes: UpdateAttributes): Promise<void>;
+    delete(id: string): Promise<void>;
   };
 }
 
 export function createCompassAggregationsMock(pipelines: unknown[]): {
   PipelineStorage: PipelineStorageClass<typeof pipelines>;
 } {
+  let data = [...pipelines];
   return {
     PipelineStorage: class PipelineStorageClass {
-      loadAll(): Promise<typeof pipelines> {
-        return Promise.resolve(pipelines);
+      loadAll(): Promise<typeof data> {
+        return Promise.resolve(data);
       }
-      delete(): Promise<void> {
+      updateAttributes(id: string, attributes: UpdateAttributes): Promise<void> {
+        data = data.map((x: any) => {
+          if (x.id !== id) {
+            return x;
+          };
+          return {
+            ...x,
+            ...attributes,
+          };
+        });
+        return Promise.resolve();
+      }
+      delete(id: string): Promise<void> {
+        data = data.filter((x: any) => x.id !== id);
         return Promise.resolve();
       }
     },
@@ -28,14 +46,28 @@ export function createCompassAggregationsMock(pipelines: unknown[]): {
 }
 
 export function createCompassQueryHistoryMock(queries: unknown[]): {
-  FavoriteQueryStorage: FavoriteQueryStorageClass<typeof queries>;
+  FavoriteQueryStorage: FavoriteQueryStorage<typeof queries>;
 } {
+  let data = [...queries];
   return {
     FavoriteQueryStorage: class FavoriteQueryStorage {
-      loadAll(): Promise<typeof queries> {
-        return Promise.resolve(queries);
+      loadAll(): Promise<typeof data> {
+        return Promise.resolve(data);
       }
-      delete(): Promise<void> {
+      updateAttributes(id: string, attributes: UpdateAttributes): Promise<void> {
+        data = data.map((x: any) => {
+          if (x._id !== id) {
+            return x;
+          }
+          return {
+            ...x,
+            ...attributes,
+          }
+        });
+        return Promise.resolve();
+      }
+      delete(id: string): Promise<void> {
+        data = data.filter((x: any) => x.id !== id);
         return Promise.resolve();
       }
     },
