cce1a9b44048c6c5a4f3a95f0bd99d255907d646
Basit <basit.chonka@mongodb.com>
2022-02-16 19:43:06 +0100

    feat(compass-saved-aggregations-queries): use modal

--- packages/compass-saved-aggregations-queries/src/components/edit-item-modal.tsx
+++ packages/compass-saved-aggregations-queries/src/components/edit-item-modal.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { ConfirmationModal, TextInput } from '@mongodb-js/compass-components';
+import {
+  Modal,
+  H3,
+  TextInput,
+  spacing,
+  css,
+  FormFooter,
+} from '@mongodb-js/compass-components';
 import { connect } from 'react-redux';
 import type { MapDispatchToProps, MapStateToProps } from 'react-redux';
 import type { RootState } from '../stores';
@@ -13,6 +20,14 @@ type EditItemModalProps = {
   onSubmit(id: string, attributes: UpdateItemAttributes): void;
   onCancel: () => void;
 };
+
+const modalStyles = css({
+  paddingBottom: 0,
+});
+
+const formTitleStyles = css({
+  marginBottom: spacing[3],
+});
 
 const EditItemModal: React.FunctionComponent<EditItemModalProps> = ({
   isModalOpen,
@@ -29,43 +44,55 @@ const EditItemModal: React.FunctionComponent<EditItemModalProps> = ({
     return <></>;
   }
 
-  const title = `Edit ${item.type === 'query' ? 'query' : 'aggregation'}`;
-  const onConfirm = () => {
-    return onSubmit(item.id, { name });
-  };
   const isSubmitDisabled = () => {
     return !name || name === item.name;
   };
+
+  const onSubmitForm = () => {
+    if (!isSubmitDisabled()) {
+      onSubmit(item.id, { name });
+    }
+  };
+
   return (
-    <ConfirmationModal
+    <Modal
+      className={modalStyles}
+      setOpen={onCancel}
       open={isModalOpen}
-      onCancel={onCancel}
-      onConfirm={onConfirm}
       data-testid="edit-item-modal"
-      title={title}
-      buttonText="Update"
-      submitDisabled={isSubmitDisabled()}
     >
       <form
         onSubmit={(event) => {
           event.preventDefault();
-          if (!isSubmitDisabled()) {
-            onConfirm();
-          }
+          onSubmitForm();
         }}
       >
+        <H3 className={formTitleStyles}>{`Edit ${item.type}`}</H3>
         <TextInput
           aria-label="Name"
           label="Name"
           name="name"
-          placeholder="Name"
+          value={name}
           onChange={(event) => {
             setName(event.target.value);
           }}
-          value={name}
+        />
+        <FormFooter
+          onCancel={onCancel}
+          primaryButton={{
+            disabled: isSubmitDisabled(),
+            text: 'Submit',
+            variant: 'primary',
+            onClick: (
+              event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+            ) => {
+              event.preventDefault();
+              onSubmitForm();
+            },
+          }}
         />
       </form>
-    </ConfirmationModal>
+    </Modal>
   );
 };
 
