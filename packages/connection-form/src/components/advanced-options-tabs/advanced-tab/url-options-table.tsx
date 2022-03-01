c7c8354d294e640b1f841825a462b68f30d3438e
Rhys <Anemy@users.noreply.github.com>
2022-03-01 11:23:55 -0500

    chore(connection-form): Add html form tag to connection form, handle on textarea enter submit (#2813)

--- packages/connection-form/src/components/advanced-options-tabs/advanced-tab/url-options-table.tsx
+++ packages/connection-form/src/components/advanced-options-tabs/advanced-tab/url-options-table.tsx
@@ -221,6 +221,7 @@ function UrlOptionsTable({
                     aria-label={`Delete option: ${datum.name ?? ''}`}
                     onClick={() => deleteUrlOption(datum.name)}
                     className={deleteOptionButtonStyle}
+                    type="button"
                   >
                     <Icon glyph="X" />
                   </IconButton>
