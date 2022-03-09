46ee1f418fee0034c6be3b96d9cf66d49ce7166d
Le Roux Bodenstein <lerouxb@gmail.com>
2022-03-09 12:54:13 +0000

    chore(e2e): browser.scrollToVirtualItem() COMPASS-5529 (#2872)
    * browser.scrollToVirtualItem()
    
    * try the same thing in a totally different way

---
+++ packages/compass-e2e-tests/helpers/commands/scroll-to-virtual-item.ts
@@ -0,0 +1,87 @@
+import type { CompassBrowser } from '../compass-browser';
+
+export async function scrollToVirtualItem(
+  browser: CompassBrowser,
+  containerSelector: string,
+  targetSelector: string
+): Promise<boolean> {
+  let found = false;
+
+  await browser.$(containerSelector).waitForDisplayed();
+
+  // it takes some time for the grid to initialise
+  await browser.waitUntil(async () => {
+    const rowCount = await browser
+      .$(`${containerSelector} [role="grid"]`)
+      .getAttribute('aria-rowcount');
+    const length = await browser.$$(`${containerSelector} [role="row"]`).length;
+
+    return !!(rowCount && length);
+  });
+
+  // scroll to the top and return the height of the scrollbar area and the
+  // scroll content
+  const [scrollHeight, totalHeight] = await browser.execute((selector) => {
+    const container = document.querySelector(selector);
+    const scrollContainer = container?.firstChild;
+    const heightContainer = scrollContainer?.firstChild;
+    if (!heightContainer) {
+      return [null, null];
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    scrollContainer.scrollTop = 0;
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return [scrollContainer.clientHeight, heightContainer.offsetHeight];
+  }, containerSelector);
+
+  if (scrollHeight === null || totalHeight === null) {
+    return false;
+  }
+
+  let scrollTop = 0;
+
+  await browser.waitUntil(async () => {
+    const targetElement = await browser.$(targetSelector);
+    if (await targetElement.isExisting()) {
+      await targetElement.waitForDisplayed();
+      await targetElement.scrollIntoView();
+      // the item is now visible, so stop scrolling
+      found = true;
+      return true;
+    }
+
+    // Browsers don't mind if we scroll past the last possible position. They
+    // will only scroll up to the last possible point. Which is handy, because
+    // then we don't have to try and calculate that pixel value.
+    scrollTop += scrollHeight;
+
+    if (scrollTop <= totalHeight) {
+      // scroll for another screen
+      await browser.execute(
+        (selector, nextScrollTop) => {
+          const container = document.querySelector(selector);
+          const scrollContainer = container?.firstChild;
+          if (!scrollContainer) {
+            return;
+          }
+
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          scrollContainer.scrollTop = nextScrollTop;
+        },
+        containerSelector,
+        scrollTop
+      );
+      return false;
+    } else {
+      // stop because we got to the end and never found it
+      return true;
+    }
+  });
+
+  return found;
+}
