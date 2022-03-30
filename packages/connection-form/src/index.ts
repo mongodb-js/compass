0526509d0a781b7e11d7a87fc9ff4d00b465b9b6
Anna Henningsen <anna@addaleax.net>
2022-03-30 16:33:25 +0200

    fixup: drop gen-esm-wrapper for connection-form

--- packages/connection-form/src/index.ts
+++ packages/connection-form/src/index.ts
@@ -1,6 +1,5 @@
 import ConnectionForm from './components/connect-form';
 import SaveConnectionModal from './components/save-connection-modal';
 import { useConnectionColor } from './hooks/use-connection-color';
-useConnectionColor;
 export { SaveConnectionModal, useConnectionColor };
 export default ConnectionForm;
