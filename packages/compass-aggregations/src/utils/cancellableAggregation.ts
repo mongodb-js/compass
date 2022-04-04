f9214631111bcb2e07bfb83da86a4906cdcf1709
Basit <1305718+mabaasit@users.noreply.github.com>
2022-04-04 14:26:28 +0200

    feat(aggregations): run aggregation COMPASS-5549 (#2930)
    Co-authored-by: Anna Henningsen <anna@addaleax.net>

---
+++ packages/compass-aggregations/src/utils/cancellableAggregation.ts
@@ -0,0 +1,38 @@
+import type { AggregateOptions, Document } from "mongodb";
+import type { DataService } from "mongodb-data-service";
+import createLogger from '@mongodb-js/compass-logging';
+const { log, mongoLogId } = createLogger('compass-aggregations');
+
+import { raceWithAbort, createCancelError } from './cancellablePromise';
+
+export async function aggregatePipeline(
+  dataService: DataService,
+  signal: AbortSignal,
+  namespace: string,
+  pipeline: Document[],
+  options: AggregateOptions,
+  skip: number,
+  limit: number,
+): Promise<Document[]> {
+  if (signal.aborted) {
+    return Promise.reject(createCancelError());
+  }
+  const session = dataService.startSession('CRUD');
+  const cursor = dataService.aggregate(namespace, pipeline, options).skip(skip).limit(limit);
+  const abort = () => {
+    cursor.close();
+    dataService.killSessions(session).catch(() => {
+      log.warn(mongoLogId(1001000105), 'Aggregations', 'Attempting to kill the session failed')
+    });
+  };
+  signal.addEventListener('abort', abort, { once: true });
+  let result;
+  try {
+    result = await raceWithAbort(cursor.toArray(), signal);
+  } finally {
+    signal.removeEventListener('abort', abort);
+    result = result || [];
+  }
+
+  return result;
+}
---
+++ packages/compass-aggregations/src/utils/cancellablePromise.ts
@@ -0,0 +1,61 @@
+const OPERATION_CANCELLED_MESSAGE = 'The operation was cancelled.';
+
+export const createCancelError = (): Error => {
+  return new Error(OPERATION_CANCELLED_MESSAGE);
+}
+/*
+ * Return a promise you can race (just like a timeout from timeouts/promises).
+ * It will reject if abortSignal triggers before successSignal
+*/
+function abortablePromise(abortSignal: AbortSignal, successSignal: AbortSignal) {
+  if (abortSignal.aborted) {
+    return Promise.reject(createCancelError());
+  }
+
+  let reject: (reason: unknown) => void;
+
+  const promise = new Promise<never>(function (_resolve, _reject) {
+    reject = _reject;
+  });
+
+  const abort = () => {
+    // if this task aborts it will never succeed, so clean up that event listener
+    // (abortSignal's event handler is already removed due to { once: true })
+    successSignal.removeEventListener('abort', succeed);
+
+    reject(createCancelError());
+  };
+
+  const succeed = () => {
+    // if this task succeeds it will never abort, so clean up that event listener
+    // (successSignal's event handler is already removed due to { once: true })
+    abortSignal.removeEventListener('abort', abort);
+  };
+
+  abortSignal.addEventListener('abort', abort, { once: true });
+  successSignal.addEventListener('abort', succeed, { once: true });
+
+  return promise;
+}
+
+/*
+ * We need a promise that will reject as soon as the operation is aborted since
+ * closing the cursor isn't enough to immediately make the cursor method's
+ * promise reject.
+*/
+export async function raceWithAbort<T>(promise: Promise<T>, signal: AbortSignal): Promise<T> {
+  if (signal.aborted) {
+    return Promise.reject(createCancelError());
+  }
+  const successController = new AbortController();
+  const abortPromise = abortablePromise(signal, successController.signal);
+  try {
+    return await Promise.race([abortPromise, promise]);
+  } finally {
+    if (!signal.aborted) {
+      // either the operation succeeded or it failed because of some error
+      // that's not an abort
+      successController.abort();
+    }
+  }
+}
