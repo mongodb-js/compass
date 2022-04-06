89853bb45d2632dbbc95ddfa4ac6e9298865207b
Basit <basit.chonka@mongodb.com>
2022-04-06 17:03:42 +0200

    feat(aggregations): fix import

--- packages/compass-components/src/components/cancel-loader.tsx
+++ packages/compass-components/src/components/cancel-loader.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { css, uiColors, spacing, SpinLoader, Subtitle, Button } from '..';
+import { spacing } from '@leafygreen-ui/tokens';
+import { css } from '@leafygreen-ui/emotion';
+import { uiColors } from '@leafygreen-ui/palette';
+
+import { Subtitle, Button } from './leafygreen';
+import { SpinLoader } from './spin-loader';
 
 const containerStyles = css({
   display: 'flex',
