ec0ba571e1004a602839a92129b580c7bf7976bb
Basit <basit.chonka@mongodb.com>
2022-04-06 17:13:38 +0200

    feat(aggregations): fix test

--- packages/compass-aggregations/src/components/pipeline-results-workspace/index.spec.tsx
+++ packages/compass-aggregations/src/components/pipeline-results-workspace/index.spec.tsx
@@ -59,7 +59,9 @@ describe('PipelineResultsWorkspace', function () {
     renderPipelineResultsWorkspace({ loading: true, onCancel: onCancelSpy });
     const container = screen.getByTestId('pipeline-results-workspace');
     expect(container).to.exist;
-    userEvent.click(within(container).getByText('Stop'));
+    userEvent.click(within(container).getByText('Stop'), undefined, {
+      skipPointerEventsCheck: true,
+    });
     expect(onCancelSpy.calledOnce).to.be.true;
   });
 });
