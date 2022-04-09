9e4928e15dc3e8352b21e885bee8c36f79ae55cb
mcasimir <maurizio.cas@gmail.com>
2022-04-09 19:56:22 +0200

    add tests

---
+++ packages/compass/src/app/intercom/setup-intercom.spec.ts
@@ -0,0 +1,168 @@
+import type { SinonStub } from 'sinon';
+import sinon from 'sinon';
+
+import { setupIntercom } from './setup-intercom';
+import { expect } from 'chai';
+import type { IntercomScript } from './intercom-script';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Preferences = require('compass-preferences-model');
+
+async function testRunSetupIntercom(
+  initialEnableFeedbackPanel: boolean,
+  user: {
+    id: string;
+    createdAt: Date;
+  }
+) {
+  const intercomScript = {
+    load: sinon.spy(),
+    unload: sinon.spy(),
+  };
+
+  const preferences = new Preferences({
+    _id: Date.now().toString(),
+    enableFeedbackPanel: initialEnableFeedbackPanel,
+  });
+
+  sinon.spy(preferences, 'isFeatureEnabled');
+
+  console.log(preferences.serialize());
+
+  await setupIntercom(
+    preferences,
+    user,
+    intercomScript as unknown as IntercomScript
+  );
+  return { intercomScript, preferences };
+}
+
+const mockUser = {
+  id: 'user-123',
+  createdAt: new Date(1649432549945),
+};
+
+describe('setupIntercom', function () {
+  let backupEnv;
+  let fetchMock: SinonStub;
+  beforeEach(function () {
+    backupEnv = {
+      HADRON_ISOLATED: process.env.HADRON_ISOLATED,
+      HADRON_METRICS_INTERCOM_APP_ID:
+        process.env.HADRON_METRICS_INTERCOM_APP_ID,
+      HADRON_PRODUCT_NAME: process.env.HADRON_PRODUCT_NAME,
+      HADRON_APP_VERSION: process.env.HADRON_APP_VERSION,
+      NODE_ENV: process.env.NODE_ENV,
+    };
+
+    process.env.HADRON_PRODUCT_NAME = 'My App Name';
+    process.env.HADRON_APP_VERSION = 'v0.0.0-test.123';
+    process.env.NODE_ENV = 'test';
+    process.env.HADRON_ISOLATED = 'false';
+    process.env.HADRON_METRICS_INTERCOM_APP_ID = 'appid123';
+    fetchMock = sinon.stub(window, 'fetch');
+    // NOTE: we use 301 since intercom will redirects
+    // to the actual location of the widget script
+    fetchMock.resolves(new Response('', { status: 301 }));
+  });
+
+  afterEach(function () {
+    process.env.HADRON_ISOLATED = backupEnv.HADRON_ISOLATED;
+    process.env.HADRON_METRICS_INTERCOM_APP_ID =
+      backupEnv.HADRON_METRICS_INTERCOM_APP_ID;
+    process.env.HADRON_PRODUCT_NAME = backupEnv.HADRON_PRODUCT_NAME;
+    process.env.HADRON_APP_VERSION = backupEnv.HADRON_APP_VERSION;
+    process.env.NODE_ENV = backupEnv.NODE_ENV;
+    fetchMock.restore();
+  });
+
+  describe('when it can be enabled', function () {
+    describe('when enableFeedbackPanel is initially enabled', function () {
+      it('calls intercomScript.load right away and calls intercomScript.unload when feedback gets disabled', async function () {
+        const { intercomScript, preferences } = await testRunSetupIntercom(
+          true,
+          mockUser
+        );
+
+        expect(preferences.isFeatureEnabled).to.have.been.calledWith(
+          'enableFeedbackPanel'
+        );
+
+        expect(intercomScript.load).to.have.been.calledWith({
+          app_id: 'appid123',
+          app_name: 'My App Name',
+          app_stage: 'test',
+          app_version: 'v0.0.0-test.123',
+          created_at: 1649432549,
+          user_id: 'user-123',
+        });
+
+        expect(intercomScript.unload).not.to.have.been.called;
+        preferences.set('enableFeedbackPanel', false);
+        expect(intercomScript.unload).to.have.been.called;
+      });
+    });
+
+    describe('when enableFeedbackPanel is initially disabled', function () {
+      it('does not call intercomScript.load right away and calls intercomScript.load when feedback gets enabled', async function () {
+        const { intercomScript, preferences } = await testRunSetupIntercom(
+          false,
+          mockUser
+        );
+
+        expect(preferences.isFeatureEnabled).to.have.been.calledWith(
+          'enableFeedbackPanel'
+        );
+
+        expect(intercomScript.load).to.not.have.been.called;
+
+        preferences.set('enableFeedbackPanel', true);
+        expect(intercomScript.load).to.have.been.calledWith({
+          app_id: 'appid123',
+          app_name: 'My App Name',
+          app_stage: 'test',
+          app_version: 'v0.0.0-test.123',
+          created_at: 1649432549,
+          user_id: 'user-123',
+        });
+      });
+    });
+  });
+
+  describe('when cannot be enabled', function () {
+    async function expectSetupGetsSkipped() {
+      const { intercomScript, preferences } = await testRunSetupIntercom(
+        true,
+        mockUser
+      );
+
+      expect(preferences.isFeatureEnabled).not.to.have.been.calledWith(
+        'enableFeedbackPanel'
+      );
+
+      expect(intercomScript.load).to.not.have.been.called;
+
+      preferences.set('enableFeedbackPanel', true);
+      expect(preferences.isFeatureEnabled).not.to.have.been.calledWith(
+        'enableFeedbackPanel'
+      );
+
+      expect(intercomScript.load).to.not.have.been.called;
+    }
+
+    it('will not enable the script when is compass isolated', async function () {
+      process.env.HADRON_ISOLATED = 'true';
+      await expectSetupGetsSkipped();
+    });
+
+    it('will not enable the script when INTERCOM_APP_ID is not set', async function () {
+      process.env.HADRON_METRICS_INTERCOM_APP_ID = '';
+      await expectSetupGetsSkipped();
+    });
+
+    it('will not enable the script when intercom is unreachable', async function () {
+      fetchMock.resolves(new Response('', { status: 500 }));
+      await expectSetupGetsSkipped();
+    });
+  });
+});
---
+++ packages/compass/src/app/intercom/setup-intercom.ts
@@ -0,0 +1,88 @@
+import type { EventEmitter } from 'events';
+import createLoggerAndTelemetry from '@mongodb-js/compass-logging';
+import type { IntercomMetadata } from './intercom-script';
+import { IntercomScript, buildIntercomScriptUrl } from './intercom-script';
+
+const { debug } = createLoggerAndTelemetry('COMPASS-INTERCOM');
+
+type User = {
+  id: string;
+  createdAt: Date;
+};
+
+type Preferences = Pick<EventEmitter, 'on'> & {
+  isFeatureEnabled: (feature: string & 'enableFeedbackPanel') => boolean;
+};
+
+export async function setupIntercom(
+  preferences: Preferences,
+  user: User,
+  intercomScript: IntercomScript = new IntercomScript()
+): Promise<void> {
+  if (typeof window === 'undefined') {
+    return;
+  }
+
+  if (process.env.HADRON_ISOLATED === 'true') {
+    debug('Skipping intercom setup on HADRON_ISOLATED');
+    return;
+  }
+
+  const intercomAppId = process.env.HADRON_METRICS_INTERCOM_APP_ID;
+
+  if (!intercomAppId) {
+    debug(
+      'skipping Intercom setup HADRON_METRICS_INTERCOM_APP_ID is not defined'
+    );
+    return;
+  }
+
+  const metadata: IntercomMetadata = {
+    user_id: user.id,
+    app_id: intercomAppId,
+    created_at: Math.floor(user.createdAt.getTime() / 1000),
+    app_name: process.env.HADRON_PRODUCT_NAME,
+    app_version: process.env.HADRON_APP_VERSION,
+    app_stage: process.env.NODE_ENV,
+  };
+
+  // In some environment the network can be firewalled, this is a safeguard to avoid
+  // uncaught errors when injecting the script.
+  debug('testing intercom availability');
+
+  const intercomWidgetUrl = buildIntercomScriptUrl(metadata.app_id);
+
+  const response = await fetch(intercomWidgetUrl).catch((e) => {
+    debug('fetch failed', e);
+    return null;
+  });
+
+  if (!response || response.status >= 400) {
+    debug('intercom unreachable, skipping setup');
+    return;
+  }
+
+  debug('intercom is reachable, proceeding with the setup');
+
+  if (preferences.isFeatureEnabled('enableFeedbackPanel')) {
+    debug(
+      'intercom loading enqueued since enableFeedbackPanel was initially enabled'
+    );
+    intercomScript.load(metadata);
+  } else {
+    debug('enableFeedbackPanel is disabled, skipping loading intercom for now');
+  }
+
+  preferences.on('change:enableFeedbackPanel', function () {
+    debug('enableFeedbackPanel changed');
+    // we need to re-check with isFeatureEnabled to make sure all the
+    // other settings for network usage are aligned too.
+    if (preferences.isFeatureEnabled('enableFeedbackPanel')) {
+      debug('enqueuing intercom script loading');
+      intercomScript.load(metadata);
+    } else {
+      debug('enqueuing intercom script unloading');
+      intercomScript.unload();
+    }
+  });
+}
