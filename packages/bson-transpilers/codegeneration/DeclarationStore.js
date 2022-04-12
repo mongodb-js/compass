b2822e0f382a8bc153fd7d929db75275ed8953ac
Preston Vasquez <prestonvasquez@icloud.com>
2022-04-11 18:57:12 -0600

    require template string when adding declarations

--- packages/bson-transpilers/codegeneration/DeclarationStore.js
+++ packages/bson-transpilers/codegeneration/DeclarationStore.js
@@ -11,53 +11,64 @@ class DeclarationStore {
   /**
    * Add declaration statements by a pre-incremented variable root-name
    *
+   * @param {string} template - Name/alias of the template needing the declaration
    * @param {string} varRoot - The root of the variable name to be appended by the occurance count
    * @param {function} declaration - The code block to be prepended to the driver syntax
    * @returns {string} the variable name with root and appended count
    */
-  add(varRoot, declaration) {
+  add(template, varRoot, declaration) {
     // Don't push existing declarations
-    const current = this.alreadyDeclared(varRoot, declaration);
+    const current = this.alreadyDeclared(template, varRoot, declaration);
     if (current !== undefined) {
       return current;
     }
-    const varName = this.next(varRoot);
-    const data = { varName: varName, declaration: declaration(varName) };
+    const varName = this.next(template, varRoot);
+    const data = { template: template, varName: varName, declaration: declaration(varName) };
     this.store.push(data);
     return data.varName;
   }
 
   /**
-   * Check if the varRoot + declaration combo already exists
+   * Check if the template + varRoot + declaration combo already exists
    *
+   * @param {string} template - Name/alias of the template needing the declaration
    * @param {string} varRoot - The root of the variable name to be appended by the occurance count
    * @param {function} declaration - The code block to be prepended to the driver syntax
    * @returns {string | undefined} the current variable name with root associated with the declaration, if it exists
    */
-  alreadyDeclared(varRoot, declaration) {
-    const existing = this.store.filter(h => h.varName.startsWith(varRoot));
+  alreadyDeclared(template, varRoot, declaration) {
+    const existing = this.candidates(template, varRoot);
     for (var i = 0; i < existing.length; i++) {
-      const mock = `${varRoot}${i > 0 ? i : ''}`;
-      if (this.store.find(e => e.declaration === declaration(mock)) !== undefined) {
+      const mock = `${this.varTemplateRoot(template, varRoot)}${i > 0 ? i : ''}`;
+
+      // check if the declaration and template are the same
+      const matching = (e) => e.declaration === declaration(mock) && e.template === template;
+      if (this.store.find(e => matching(e)) !== undefined) {
         return existing[i].varName;
       }
     }
     return undefined;
   }
 
+  candidates(template, varRoot) {
+    const varTemplateRoot = this.varTemplateRoot(template, varRoot);
+    return this.store.filter(h => h.varName.startsWith(varTemplateRoot));
+  }
+
   /**
    * Get the next variable name given a pre-incremented variable root-name
    *
+   * @param {string} template - Name/alias of the template needing the declaration
    * @param {string} varRoot - The root of the variable name to be appended by the occurance count
    * @returns {string} the variable name with root and appended count
    */
-  next(varRoot) {
-    const existing = this.store.filter(h => h.varName.startsWith(varRoot));
+  next(template, varRoot) {
+    const existing = this.candidates(template, varRoot);
 
     // If the data does not exist in the store, then the count should append nothing to the variable
     // name
     const count = existing.length > 0 ? existing.length : '';
-    return `${varRoot}${count}`;
+    return `${this.varTemplateRoot(template, varRoot)}${count}`;
   }
 
   /**
@@ -68,6 +79,10 @@ class DeclarationStore {
    */
   toString(sep = '\n\n') {
     return this.store.map((value) => value.declaration).join(sep);
+  }
+
+  varTemplateRoot(template, varRoot) {
+    return `${varRoot}For${template}`;
   }
 }
 
--- packages/bson-transpilers/test/declaration-store.test.js
+++ packages/bson-transpilers/test/declaration-store.test.js
@@ -5,20 +5,20 @@ describe('DeclarationStore', () => {
   it('adds data using #add', () => {
     const ds = new DeclarationStore();
 
-    ds.add('objectID', (varName) => { return `objectId${varName}`; });
+    ds.add('Temp', 'objectID', (varName) => { return `objectId${varName}`; });
     assert.strictEqual(ds.store.length, 1);
   });
   it('returns incremented variable names given the pre-incremented variable root-name', () => {
     const ds = new DeclarationStore();
 
-    ds.add('objectID', () => { return 1; });
-    assert.strictEqual(ds.next('objectID'), 'objectID1');
+    ds.add('Temp', 'objectID', () => { return 1; });
+    assert.strictEqual(ds.next('Temp', 'objectID'), 'objectIDForTemp1');
 
-    ds.add('objectID', () => { return 2; });
-    assert.strictEqual(ds.next('objectID'), 'objectID2');
+    ds.add('Temp', 'objectID', () => { return 2; });
+    assert.strictEqual(ds.next('Temp', 'objectID'), 'objectIDForTemp2');
 
-    ds.add('objectID', () => { return 3; });
-    assert.strictEqual(ds.next('objectID'), 'objectID3');
+    ds.add('Temp', 'objectID', () => { return 3; });
+    assert.strictEqual(ds.next('Temp', 'objectID'), 'objectIDForTemp3');
   });
   it('stringifies multiple variables declarations', () => {
     const ds = new DeclarationStore();
@@ -39,16 +39,16 @@ describe('DeclarationStore', () => {
         .concat('}')
         .join('\n');
     };
-    ds.add('objectID', declaration1);
-    ds.add('objectID', declaration2);
+    ds.add('Temp', 'objectID', declaration1);
+    ds.add('Temp', 'objectID', declaration2);
 
     const expected = []
-      .concat('objectID, err := primitive.ObjectIDFromHex()')
+      .concat('objectIDForTemp, err := primitive.ObjectIDFromHex()')
       .concat('if err != nil {')
       .concat('    log.Fatal(err)')
       .concat('}')
       .concat('')
-      .concat('objectID1, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")')
+      .concat('objectIDForTemp1, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")')
       .concat('if err != nil {')
       .concat('    log.Fatal(err)')
       .concat('}')
@@ -84,17 +84,17 @@ describe('DeclarationStore', () => {
         .join('\n');
     };
 
-    ds.add('objectID', declaration1);
-    ds.add('objectID', declaration2);
-    ds.add('objectID', declaration3);
+    ds.add('Temp', 'objectID', declaration1);
+    ds.add('Temp', 'objectID', declaration2);
+    ds.add('Temp', 'objectID', declaration3);
 
     const expected = []
-      .concat('objectID, err := primitive.ObjectIDFromHex()')
+      .concat('objectIDForTemp, err := primitive.ObjectIDFromHex()')
       .concat('if err != nil {')
       .concat('    log.Fatal(err)')
       .concat('}')
       .concat('')
-      .concat('objectID1, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")')
+      .concat('objectIDForTemp1, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")')
       .concat('if err != nil {')
       .concat('    log.Fatal(err)')
       .concat('}')
@@ -130,17 +130,68 @@ describe('DeclarationStore', () => {
         .join('\n');
     };
 
-    ds.add('objectID', declaration1);
-    ds.add('objectID', declaration2);
-    ds.add('objectID', declaration3);
+    ds.add('Temp', 'objectID', declaration1);
+    ds.add('Temp', 'objectID', declaration2);
+    ds.add('Temp', 'objectID', declaration3);
 
     const expected = []
-      .concat('objectID, err := primitive.ObjectIDFromHex()')
+      .concat('objectIDForTemp, err := primitive.ObjectIDFromHex()')
       .concat('if err != nil {')
       .concat('    log.Fatal(err)')
       .concat('}')
       .concat('')
-      .concat('objectID1, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")')
+      .concat('objectIDForTemp1, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")')
+      .concat('if err != nil {')
+      .concat('    log.Fatal(err)')
+      .concat('}')
+      .join('\n');
+    assert.strictEqual(ds.toString(), expected);
+  });
+  it('ignores duplications over different temps', () => {
+    const ds = new DeclarationStore();
+    const declaration1 = (varName) => {
+      return []
+        .concat(`${varName}, err := primitive.ObjectIDFromHex()`)
+        .concat('if err != nil {')
+        .concat('    log.Fatal(err)')
+        .concat('}')
+        .join('\n');
+    };
+
+    const declaration2 = (varName) => {
+      return []
+        .concat(`${varName}, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")`)
+        .concat('if err != nil {')
+        .concat('    log.Fatal(err)')
+        .concat('}')
+        .join('\n');
+    };
+
+    const declaration3 = (varName) => {
+      return []
+        .concat(`${varName}, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")`)
+        .concat('if err != nil {')
+        .concat('    log.Fatal(err)')
+        .concat('}')
+        .join('\n');
+    };
+
+    ds.add('TempA', 'objectID', declaration1);
+    ds.add('TempA', 'objectID', declaration2);
+    ds.add('TempB', 'objectID', declaration3);
+
+    const expected = []
+      .concat('objectIDForTempA, err := primitive.ObjectIDFromHex()')
+      .concat('if err != nil {')
+      .concat('    log.Fatal(err)')
+      .concat('}')
+      .concat('')
+      .concat('objectIDForTempA1, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")')
+      .concat('if err != nil {')
+      .concat('    log.Fatal(err)')
+      .concat('}')
+      .concat('')
+      .concat('objectIDForTempB, err := primitive.ObjectIDFromHex("5ab901c29ee65f5c8550c5b9")')
       .concat('if err != nil {')
       .concat('    log.Fatal(err)')
       .concat('}')
