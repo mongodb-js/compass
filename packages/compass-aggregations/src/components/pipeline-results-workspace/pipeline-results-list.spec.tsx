3f4cfe9f144eecb3abc7d28c90aa2694c90e7b98
Basit <1305718+mabaasit@users.noreply.github.com>
2022-04-14 16:15:10 +0200

    feat(aggregations): display aggregation results (#2952)
    * feat(aggregations): ux changes
    
    * feat(aggregations): better pagination
    
    * feat(aggregations): pipeline loader
    
    * feat(aggregations): tests
    
    * feat(aggregations): tests
    
    * feat(aggregations): use CancelLoader
    
    * feat(aggregations): fix import
    
    * feat(aggregations): fix test
    
    * feat(aggregations): add empty results page
    
    * feat(aggregations): show pagination text in loading state
    
    * feat(aggregations): UI fixes

---
+++ packages/compass-aggregations/src/components/pipeline-results-workspace/pipeline-results-list.spec.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { expect } from 'chai';
+
+import PipelineResultsList from './pipeline-results-list';
+
+describe('PipelineResultsList', function () {
+  it('does not render when documents are empty', function () {
+    render(<PipelineResultsList documents={[]} view="document" />);
+    expect(() => {
+      screen.getByTestId('document-list-item');
+    }).to.throw;
+  });
+
+  it('renders list view', function () {
+    render(
+      <PipelineResultsList documents={[{ id: 1 }, { id: 2 }]} view="document" />
+    );
+    expect(
+      document.querySelectorAll('[data-test-id="document-list-item"]')
+    ).to.have.lengthOf(2);
+  });
+
+  it('renders json view', function () {
+    render(
+      <PipelineResultsList
+        documents={[{ id: 3 }, { id: 4 }, { id: 5 }]}
+        view="json"
+      />
+    );
+    expect(
+      document.querySelectorAll('[data-test-id="document-json-item"]')
+    ).to.have.lengthOf(3);
+  });
+});
