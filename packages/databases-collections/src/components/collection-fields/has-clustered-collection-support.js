491d9f593f64f1582272a296a0eb768b54afee78
Le Roux Bodenstein <lerouxb@gmail.com>
2022-04-19 16:39:15 +0100

    feature flag for clustered collections

--- packages/databases-collections/src/components/collection-fields/has-clustered-collection-support.js
+++ packages/databases-collections/src/components/collection-fields/has-clustered-collection-support.js
@@ -3,6 +3,12 @@ import semver from 'semver';
 const MIN_CLUSTERED_COLLECTION_SERVER_VERSION = '5.3.0-alpha0';
 
 export default function hasClusteredCollectionSupport(serverVersion) {
+  const clusteredCollectionsFeatureFlag = process?.env?.COMPASS_CLUSTERED_COLLECTIONS === 'true';
+
+  if (!clusteredCollectionsFeatureFlag) {
+    return false;
+  }
+
   try {
     return semver.gte(serverVersion, MIN_CLUSTERED_COLLECTION_SERVER_VERSION);
   } catch (e) {
