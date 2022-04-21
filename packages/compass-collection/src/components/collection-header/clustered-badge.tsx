fe2e2429ba03aa784637d664eec9508605dcc33b
Le Roux Bodenstein <lerouxb@gmail.com>
2022-04-21 09:32:54 +0100

    spacing

--- packages/compass-collection/src/components/collection-header/clustered-badge.tsx
+++ packages/compass-collection/src/components/collection-header/clustered-badge.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Badge, BadgeVariant, css } from '@mongodb-js/compass-components';
+import {
+  Badge,
+  BadgeVariant,
+  css,
+  spacing,
+} from '@mongodb-js/compass-components';
 
 const collectionHeaderBadgeStyles = css({
   marginLeft: spacing[2],
