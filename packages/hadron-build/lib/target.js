f79526642968a3cb8107ddfda236b752c76d89bf
mcasimir <maurizio.cas@gmail.com>
2022-04-22 19:19:08 +0200

    fix: update winstaller

--- packages/hadron-build/lib/target.js
+++ packages/hadron-build/lib/target.js
@@ -375,10 +375,10 @@ class Target {
     /**
      * @see https://jira/mongodb.org/browse/BUILD-920
      */
-    const signWithParams = process.env.NOTARY_AUTH_TOKEN
-      ? 'yes'
-      : process.env.SIGNTOOL_PARAMS;
-    this.installerOptions.signWithParams = signWithParams;
+    // const signWithParams = process.env.NOTARY_AUTH_TOKEN
+    //   ? 'yes'
+    //   : process.env.SIGNTOOL_PARAMS;
+    // this.installerOptions.signWithParams = signWithParams;
 
     /**
      * The ICO file to use as the icon for the generated Setup.exe.
@@ -407,7 +407,7 @@ class Target {
         description: this.description,
         manufacturer: this.author,
         version: windowsInstallerVersion(this.installerVersion || this.version),
-        signWithParams: signWithParams,
+        // signWithParams: '',
         shortcutFolderName: this.shortcutFolderName || this.author,
         programFilesFolderName: this.programFilesFolderName || this.productName,
         appUserModelId: this.bundleId,
