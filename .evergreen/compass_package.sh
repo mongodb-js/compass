b37db421f97911f6046cfbad2b4d6f0800560d53
Maurizio Casimirri <maurizio.cas@gmail.com>
2022-04-28 13:42:57 +0200

    fix(hadron-build): update winstaller COMPASS-5447, COMPASS-5599 (#3018)
    * fix: update winstaller
    
    * use rev order id
    
    * unique upload url
    
    * use build id in artifacts url
    
    * manually sign windows artifacts
    
    * wait sign
    
    * fix config test
    
    * use mongosh signing code
    
    * replace imports with require
    
    * fix export
    
    * add signtool.exe
    
    * try signtool
    
    * fix signtool download
    
    * log signtool env
    
    * fix signed path
    
    * use filename without spaces
    
    * setup file without spaces
    
    * remove spaces from windows setup filename
    
    * try embed signtool.exe
    
    * try to sign with winstaller again
    
    * fix failing tests

--- .evergreen/compass_package.sh
+++ .evergreen/compass_package.sh
@@ -1,22 +1,5 @@
 #! /usr/bin/env bash
 if [[ "$OSTYPE" == "cygwin" ]]; then
-  # If not possible to remove this hack, we should find a better way
-  # to do this instead of directly referencing node_module paths,
-  # but first figure out what exactly was changed in our fork of
-  # electron-wix-msi
-  #
-  # TODO: https://jira.mongodb.org/browse/COMPASS-4888
-
-  echo "Fetching signtool -> notary-service hack..."
-  curl -fs \
-    -o "signtool.exe" \
-    --url "https://s3.amazonaws.com/boxes.10gen.com/build/signtool.exe"
-  rm -f node_modules/electron-winstaller/vendor/signtool.exe
-  rm -f node_modules/@mongodb-js/electron-wix-msi/vendor/signtool.exe
-  chmod +x signtool.exe
-  cp signtool.exe node_modules/@mongodb-js/electron-wix-msi/vendor/signtool.exe
-  cp signtool.exe node_modules/electron-winstaller/vendor/signtool.exe
-
   echo "Starting Installer Service..."
   net start MSIServer
 fi
