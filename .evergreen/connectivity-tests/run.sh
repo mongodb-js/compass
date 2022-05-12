dd2799367fec7d836820fbc47fc14b08bffb9283
Maurizio Casimirri <maurizio.cas@gmail.com>
2022-05-12 11:57:11 +0200

    run from monorepo root

--- .evergreen/connectivity-tests/run.sh
+++ .evergreen/connectivity-tests/run.sh
@@ -3,13 +3,12 @@
 # NOTE: do not -x here there are "env" vars in the commands
 set -e
 
-cd $(dirname "$0")
-
-echo "building connectivity tests image from ${0}"
-docker build -t devtools-connectivity-tests .
-echo "connectivity tests image built"
-
 MONOREPO_ROOT_DIR="$(cd $(dirname "$0")/../..; pwd)"
+cd $MONOREPO_ROOT_DIR
+
+echo "building connectivity tests image from ${PWD}"
+docker build -t devtools-connectivity-tests -f "./.evergreen/connectivity-tests/Dockerfile" .
+echo "connectivity tests image built"
 
 echo running connectivity tests with volume: "${MONOREPO_ROOT_DIR}"
 
@@ -27,7 +26,6 @@ docker run \
   -e E2E_TESTS_ATLAS_X509_PEM="${E2E_TESTS_ATLAS_X509_PEM}" \
   -e MONGODB_VERSION="${MONGODB_VERSION}" \
   -e COMPASS_CONNECTIVITY_TESTS_HOST="host.docker.internal" \
-  -v "${MONOREPO_ROOT_DIR}":/compass-monorepo-root:ro \
   --add-host host.docker.internal:"${DOCKER_HOST_GATEWAY}" \
   --add-host mongodb-kerberos-1.example.com:"${DOCKER_HOST_GATEWAY}" \
   --add-host mongodb-kerberos-2.example.com:"${DOCKER_HOST_GATEWAY}" \
