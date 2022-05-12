c9aadad46d0e0417593a9709cbcf17a3d596f238
Maurizio Casimirri <maurizio.cas@gmail.com>
2022-05-12 10:17:28 +0200

    replace host gateway with hardcoded ip

--- .evergreen/connectivity-tests/run.sh
+++ .evergreen/connectivity-tests/run.sh
@@ -13,6 +13,8 @@ MONOREPO_ROOT_DIR="$(cd $(dirname "$0")/../..; pwd)"
 
 echo running connectivity tests with volume: "${MONOREPO_ROOT_DIR}"
 
+DOCKER_HOST_GATEWAY=172.17.0.1
+
 docker run \
   --rm \
   -e E2E_TESTS_ATLAS_HOST="${E2E_TESTS_ATLAS_HOST}" \
@@ -26,9 +28,9 @@ docker run \
   -e MONGODB_VERSION="${MONGODB_VERSION}" \
   -e COMPASS_CONNECTIVITY_TESTS_HOST="host.docker.internal" \
   -v "${MONOREPO_ROOT_DIR}":/compass-monorepo-root:ro \
-  --add-host host.docker.internal:host-gateway \
-  --add-host mongodb-kerberos-1.example.com:host-gateway \
-  --add-host mongodb-kerberos-2.example.com:host-gateway \
-  --add-host mongodb-kerberos-3.examplecrossrealm.com:host-gateway \
+  --add-host host.docker.internal:"${DOCKER_HOST_GATEWAY}" \
+  --add-host mongodb-kerberos-1.example.com:"${DOCKER_HOST_GATEWAY}" \
+  --add-host mongodb-kerberos-2.example.com:"${DOCKER_HOST_GATEWAY}" \
+  --add-host mongodb-kerberos-3.examplecrossrealm.com:"${DOCKER_HOST_GATEWAY}" \
   devtools-connectivity-tests
 
