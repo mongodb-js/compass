f407c7a0dfdcb1bd812122423a5dfe91905a2390
Basit <basit.chonka@mongodb.com>
2022-05-19 15:55:03 +0200

    chore(explain-aggregation): test fix

--- packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
+++ packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
@@ -549,8 +549,8 @@ describe('Collection aggregations tab', function () {
       await browser.waitForAnimations(Selectors.AggregationExplainModal);
 
       const modal = await browser.$(Selectors.AggregationExplainModal);
-      await browser.waitForAnimations(Selectors.AggregationExplainModal);
       await modal.waitForDisplayed();
+      await browser.waitForAnimations(Selectors.AggregationExplainModal);
       expect(await modal.getText()).to.contain('Query Performance Summary');
     });
   });
