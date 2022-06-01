0c66801409b5096eb83290de23cf87e9ac647f4e
Anemy <rhysh@live.com>
2022-06-01 18:09:16 -0400

    move compass-crud to deps, remove react from explicit externals

--- configs/mocha-config-compass/index.js
+++ configs/mocha-config-compass/index.js
@@ -4,7 +4,7 @@ const specs = process.argv.slice(2).filter((arg) => !arg.startsWith('-'));
 // This can be written as `{src,test}/**/*.{spec,test}.*`, but this causes
 // issues for mocha on some version. It will try to parse every string in the
 // array by splitting on commas causing a wrong spec pattern to be applied. It
-// is fixed in latest, but as we are still in the process of updaing
+// is fixed in latest, but as we are still in the process of updating
 // everything to the same version and working around the issue helps with
 // migration (this can be removed when mocha is not in depalignrc anymore)
 const defaultSpecs = [
