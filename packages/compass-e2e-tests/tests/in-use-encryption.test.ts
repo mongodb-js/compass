f33245cab6970e8a5a706283ad0afbb0c04e9834
Alena Khineika <alena.khineika@gmail.com>
2022-06-02 21:08:37 +0200

    test: verify that copied encrypted field is not editable when not in FLE2 config COMPASS-5874

--- packages/compass-e2e-tests/tests/in-use-encryption.test.ts
+++ packages/compass-e2e-tests/tests/in-use-encryption.test.ts
@@ -7,17 +7,163 @@ import { MONGODB_VERSION } from '../helpers/compass';
 import * as Selectors from '../helpers/selectors';
 import { createDummyCollections } from '../helpers/insert-data';
 import { getFirstListDocument } from '../helpers/read-first-document-content';
+import { MongoClient } from 'mongodb';
+import { ClientEncryption } from 'mongodb-client-encryption';
+
+const CONNECTION_HOSTS = 'localhost:27091';
+const CONNECTION_STRING = `mongodb://${CONNECTION_HOSTS}`;
 
 describe('FLE2', function () {
   before(async function () {
     if (
-      semver.lt(MONGODB_VERSION, '6.0.0-rc0') ||
-      process.env.MONGODB_USE_ENTERPRISE !== 'yes'
+      semver.lt(MONGODB_VERSION, '6.0.0-rc0')
     ) {
       return this.skip();
     }
 
     await createDummyCollections();
+  });
+
+  describe('when no encryption options are specified while connecting', function () {
+    const databaseName = 'my-db';
+    const collectionName = 'my-collection';
+    let compass: Compass;
+    let browser: CompassBrowser;
+    let autoMongo: MongoClient;
+
+    before(async function () {
+      compass = await beforeTests();
+      browser = compass.browser;
+    });
+
+    beforeEach(async function () {
+      try {
+        const keyVaultNamespace = `${databaseName}.keyvault`;
+        const kmsProviders = { local: { key: 'A'.repeat(128) } };
+        const keyMongo = await MongoClient.connect(CONNECTION_STRING, {
+          autoEncryption: {
+            keyVaultNamespace,
+            kmsProviders,
+          },
+        });
+        const clientEncryption = new ClientEncryption(keyMongo, {
+          kmsProviders,
+          keyVaultNamespace,
+        });
+        const keyId = await clientEncryption.createDataKey('local');
+        const encryptedFields = {
+          fields: [
+            {
+              path: 'phoneNumber',
+              keyId,
+              bsonType: 'string',
+              queries: { queryType: 'equality' },
+            },
+          ],
+        };
+        autoMongo = await MongoClient.connect(CONNECTION_STRING, {
+          autoEncryption: {
+            keyVaultNamespace,
+            kmsProviders,
+            encryptedFieldsMap: encryptedFields,
+          },
+        });
+        await autoMongo.db(databaseName).createCollection(collectionName, {
+          encryptedFields,
+        });
+        await autoMongo.db(databaseName).collection(collectionName).insertOne({
+          phoneNumber: '727272',
+          name: 'Person Z',
+        });
+      } catch (error) {
+        /* ignore */
+      }
+
+      await browser.connectWithConnectionForm({
+        hosts: [CONNECTION_HOSTS],
+      });
+    });
+
+    after(async function () {
+      if (compass) {
+        await afterTests(compass, this.currentTest);
+      }
+    });
+
+    afterEach(async function () {
+      await autoMongo.db(databaseName).dropDatabase();
+    });
+
+    it('can not edit encrypted fields', async function () {
+      await browser.navigateToCollectionTab(
+        databaseName,
+        collectionName,
+        'Documents'
+      );
+
+      const result = await getFirstListDocument(browser);
+      expect(result.phoneNumber).to.be.equal('*********');
+
+      // browse to the "Insert to Collection" modal
+      await browser.clickVisible(Selectors.AddDataButton);
+      const insertDocumentOption = await browser.$(
+        Selectors.InsertDocumentOption
+      );
+      await insertDocumentOption.waitForDisplayed();
+      await browser.clickVisible(Selectors.InsertDocumentOption);
+
+      // wait for the modal to appear
+      const insertDialog = await browser.$(Selectors.InsertDialog);
+      await insertDialog.waitForDisplayed();
+
+      // set the text in the editor
+      await browser.setAceValue(
+        Selectors.InsertJSONEditor,
+        '{' +
+          '  "faxNumber": {' +
+          '    "$binary": {' +
+          '      "base64": "B0y10j06hEHblOCkZWG56BICR2xXyMD/njKep+2KWfZ3xlMze+mSk1LXNoGi+9GxQMCQKT3Uv/8SAx4+d/PyJUIvMV5k9eagWeyOiUFXb9mGHqhUbRA58InRYzf6NODBl1JTHqqrYk8yt13Qfdfpv0TvUarSrGpLtqUJagRpi9+mV0bFOPlunq6fAHWQJXplPuL+APTwP0/t4SMYOXt7pzXrDwwl9feoR6yYgeZK5S56BnFUF/3ljW+s14CoKypinndbXXOoQuA7AKo8Q5SMBpOug19ajy88TKQJf2E=",' +
+          '      "subType": "06"' +
+          '    }' +
+          '  },' +
+          '  "name": "La La",' +
+          '  "location": "Berlin"' +
+          '}'
+      );
+
+      // confirm
+      const insertConfirm = await browser.$(Selectors.InsertConfirm);
+      await insertConfirm.waitForEnabled();
+      await browser.clickVisible(Selectors.InsertConfirm);
+
+      // wait for the modal to go away
+      await insertDialog.waitForDisplayed({ reverse: true });
+      await browser.clickVisible(Selectors.SidebarInstanceRefreshButton);
+
+      await browser.runFindOperation('Documents', "{ name: 'La La' }");
+
+      const newDocumenResult = await getFirstListDocument(browser);
+      expect(newDocumenResult.faxNumber).to.be.equal('*********');
+      expect(newDocumenResult._id).to.be.not.equal(result._id);
+
+      const document = await browser.$(Selectors.DocumentListEntry);
+      const value = await document.$(
+        `${Selectors.HadronDocumentElement}:nth-child(4) ${Selectors.HadronDocumentClickableValue}`
+      );
+      await value.doubleClick();
+
+      const faxNumberEditor = await document.$(
+        `${Selectors.HadronDocumentElement}:nth-child(2) ${Selectors.HadronDocumentValueEditor}`
+      );
+      const isFaxNumberEditorExisting = await faxNumberEditor.isExisting();
+      expect(isFaxNumberEditorExisting).to.be.equal(false);
+
+      const locationEditor = await document.$(
+        `${Selectors.HadronDocumentElement}:nth-child(4) ${Selectors.HadronDocumentValueEditor}`
+      );
+      const isLocationEditorExisting = await locationEditor.isExisting();
+      expect(isLocationEditorExisting).to.be.equal(true);
+    });
   });
 
   describe('when fleEncryptedFieldsMap is not specified while connecting', function () {
@@ -31,7 +177,7 @@ describe('FLE2', function () {
       browser = compass.browser;
 
       await browser.connectWithConnectionForm({
-        hosts: ['localhost:27091'],
+        hosts: [CONNECTION_HOSTS],
         fleKeyVaultNamespace: `${databaseName}.keyvault`,
         fleKey: 'A'.repeat(128),
       });
@@ -98,7 +244,7 @@ describe('FLE2', function () {
 
     beforeEach(async function () {
       await browser.connectWithConnectionForm({
-        hosts: ['localhost:27091'],
+        hosts: [CONNECTION_HOSTS],
         fleKeyVaultNamespace: `${databaseName}.keyvault`,
         fleKey: 'A'.repeat(128),
         fleEncryptedFieldsMap: `{
@@ -304,6 +450,50 @@ describe('FLE2', function () {
       const modifiedResult = await getFirstListDocument(browser);
       expect(modifiedResult.phoneNumber).to.be.equal('"10101010"');
       expect(modifiedResult._id).to.be.equal(result._id);
+    });
+
+    it('allows editing not encrypted fields and those that listed in encryptedFields', async function () {
+      await browser.shellEval(`db.createCollection('${collectionName}')`);
+      await browser.shellEval(
+        'db["' +
+          collectionName +
+          '"].insertOne({' +
+          '"phoneNumber": "30303030",' +
+          '"faxNumber": {' +
+          '  "$binary": {' +
+          '    "base64": "B0y10j06hEHblOCkZWG56BICR2xXyMD/njKep+2KWfZ3xlMze+mSk1LXNoGi+9GxQMCQKT3Uv/8SAx4+d/PyJUIvMV5k9eagWeyOiUFXb9mGHqhUbRA58InRYzf6NODBl1JTHqqrYk8yt13Qfdfpv0TvUarSrGpLtqUJagRpi9+mV0bFOPlunq6fAHWQJXplPuL+APTwP0/t4SMYOXt7pzXrDwwl9feoR6yYgeZK5S56BnFUF/3ljW+s14CoKypinndbXXOoQuA7AKo8Q5SMBpOug19ajy88TKQJf2E=",' +
+          '    "subType": "06"' +
+          '  }' +
+          '},' +
+          '"name": "Person X",' +
+          '"location": "Paris"' +
+          '})'
+      );
+
+      await browser.clickVisible(Selectors.SidebarInstanceRefreshButton);
+      await browser.navigateToCollectionTab(
+        databaseName,
+        collectionName,
+        'Documents'
+      );
+
+      const document = await browser.$(Selectors.DocumentListEntry);
+      const value = await document.$(
+        `${Selectors.HadronDocumentElement}:nth-child(2) ${Selectors.HadronDocumentClickableValue}`
+      );
+      await value.doubleClick();
+
+      const faxNumberEditor = await document.$(
+        `${Selectors.HadronDocumentElement}:nth-child(2) ${Selectors.HadronDocumentValueEditor}`
+      );
+      const isFaxNumberEditorExisting = await faxNumberEditor.isExisting();
+      expect(isFaxNumberEditorExisting).to.be.equal(true);
+
+      const locationEditor = await document.$(
+        `${Selectors.HadronDocumentElement}:nth-child(3) ${Selectors.HadronDocumentValueEditor}`
+      );
+      const isLocationEditorExisting = await locationEditor.isExisting();
+      expect(isLocationEditorExisting).to.be.equal(false);
     });
   });
 });
