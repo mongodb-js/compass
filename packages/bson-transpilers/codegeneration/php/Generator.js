e1722afcd17fc2ccc666e5ee69043ba89fe4e7a2
Aleksandr Rudo <aleksandr.rudo@mongodb.com>
2022-06-02 16:31:51 +0300

    New templates for PHP.

---
+++ packages/bson-transpilers/codegeneration/php/Generator.js
@@ -0,0 +1,8 @@
+/*
+ * Class for handling edge cases for php code generation. Defines "emit" methods.
+ */
+module.exports = (Visitor) => class Generator extends Visitor {
+  constructor() {
+    super();
+  }
+};
