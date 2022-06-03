bd3d44b0a4d61f7a5069309033b136462d671e1c
Rhys <Anemy@users.noreply.github.com>
2022-06-03 17:17:45 -0400

    chore(hadron-document): export type explicitly (#3158)

--- packages/hadron-document/src/index.ts
+++ packages/hadron-document/src/index.ts
@@ -3,15 +3,16 @@ import Element, {
   Events as ElementEvents,
   isInternalFieldPath,
 } from './element';
-import ElementEditor, { Editor } from './editor';
+import ElementEditor from './editor';
+import type { Editor } from './editor';
 
 export default Document;
+export type { Editor };
 export {
   Document,
   DocumentEvents,
   Element,
   ElementEvents,
   ElementEditor,
-  Editor,
   isInternalFieldPath,
 };
