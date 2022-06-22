2af36f67f4e582982ee5b0b813ecc03f50e012a0
Aleksandr Rudo <aleksandr.rudo@mongodb.com>
2022-06-22 19:08:14 +0300

    Adding string checker to cover cases with "undefined" input.

--- packages/bson-transpilers/codegeneration/php/PHPUtils.js
+++ packages/bson-transpilers/codegeneration/php/PHPUtils.js
@@ -8,11 +8,24 @@ class PHPUtils {
   }
 
   /**
+   * Common string checker to be sure we work with string
+   * @param {string|undefined} str - input variable
+   * @returns {string} - Parsed string
+   */
+  convertToString(str) {
+    if (str === undefined) {
+      return '';
+    }
+    return str;
+  }
+
+  /**
    * Removing Object casting of PHP variable
-   * @param {string} str - String with PHP representation of variable
+   * @param {string|undefined} str - String with PHP representation of variable
    * @returns {string} - Parsed string
    */
   removePHPObject(str) {
+    str = this.convertToString(str);
     if (str.indexOf('(object) ') === 0) {
       str = str.slice(9);
     }
@@ -21,10 +34,11 @@ class PHPUtils {
 
   /**
    * Removing surrounding quotes of PHP string variable content
-   * @param {string} str - PHP representation of string with or without quotes to remove
+   * @param {string|undefined} str - PHP representation of string with or without quotes to remove
    * @returns {string} - Parsed string
    */
   removeStringQuotes(str) {
+    str = this.convertToString(str);
     if (
         (str.charAt(0) === '\'' && str.charAt(str.length - 1) === '\'') ||
         (str.charAt(0) === '"' && str.charAt(str.length - 1) === '"')
@@ -34,16 +48,28 @@ class PHPUtils {
     return str;
   };
 
+  /**
+   * @param {string|undefined} str
+   * @returns {string} - Parsed string
+   */
   stringify(str) {
     str = this.removeStringQuotes(str);
     return `${str.replace(/\\([\s\S])/g, '\\$1')}`;
   };
 
+  /**
+   * @param {string|undefined} str
+   * @returns {string} - Parsed string
+   */
   stringifyWithSingleQuotes(str) {
     str = this.removeStringQuotes(str);
     return `'${str.replace(/\\([\s\S])|(')/g, '\\$1$2')}'`;
   };
 
+  /**
+   * @param {string|undefined} str
+   * @returns {string} - Parsed string
+   */
   stringifyWithDoubleQuotes(str) {
     str = this.removeStringQuotes(str);
     return `"${str.replace(/\\([\s\S])|(")/g, '\\$1$2')}"`;
