1c11f0b926f6edfcf096f9912eaac11182ec1abf
Rhys <Anemy@users.noreply.github.com>
2022-06-26 20:28:07 +0200

    feat(compass-explain-plan): update explain toolbar components COMPASS-5675 (#3156)

--- packages/compass-explain-plan/src/constants/explain-states.js
+++
@@ -1,6 +0,0 @@
-// Constants for explain state.
-module.exports = {
-  INITIAL: 'initial', // A zero state that is being shown by default
-  OUTDATED: 'outdated', // When new query was set and explain plan should be fetched again
-  EXECUTED: 'executed', // When new query was executed by explain plan
-};
