607228fc67f7ab024fde9af0f8602c3d71fd8437
Rhys <Anemy@users.noreply.github.com>
2022-06-27 08:21:12 +0200

    feat(compass-schema): update schema toolbar to leafygreen components COMPASS-5674 (#3167)

---
+++ packages/compass-schema/src/constants/schema.ts
@@ -0,0 +1,11 @@
+const constants = {
+  SCHEMA_PROBABILITY_PERCENT: 'schema-probability-percent',
+  DECIMAL_128: 'Decimal128',
+  DOUBLE: 'Double',
+  LONG: 'Long',
+  INT_32: 'Int32',
+  STRING: 'String',
+  NUMBER: 'Number',
+};
+
+export default constants;
