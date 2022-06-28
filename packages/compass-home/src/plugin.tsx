261559f107853c84c650ebded0301ae78cc5615f
Basit <basit.chonka@mongodb.com>
2022-06-28 12:12:25 +0200

    feat(compass-settings): settings in home plugin

--- packages/compass-home/src/plugin.tsx
+++ packages/compass-home/src/plugin.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type AppRegistry from 'hadron-app-registry';
 import { LeafyGreenProvider } from '@mongodb-js/compass-components';
-
+import Settings from '@mongodb-js/compass-settings';
 import Home from './components/home';
 import AppRegistryContext from './contexts/app-registry-context';
 
@@ -17,6 +17,7 @@ function Plugin({
   return (
     <AppRegistryContext.Provider value={appRegistry}>
       <LeafyGreenProvider>
+        <Settings />
         <Home appName={appName} />
       </LeafyGreenProvider>
     </AppRegistryContext.Provider>
