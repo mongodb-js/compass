da97fa04fcc12d82469a006f0b590e5abf28435c
Le Roux Bodenstein <lerouxb@gmail.com>
2022-07-01 11:03:44 +0100

    remove references to the plugins

--- packages/databases-collections/test/setup.js
+++ packages/databases-collections/test/setup.js
@@ -1,6 +1,7 @@
 const hadronApp = require('hadron-app');
 const { AppRegistry } = require('hadron-app-registry');
 
+// TODO
 const { activate } = require('@mongodb-js/compass-deployment-awareness');
 const appRegistry = new AppRegistry();
 
