ed5e08dfc8f05bf0ac2fc1e2c4aa4322083d61dc
Le Roux Bodenstein <lerouxb@gmail.com>
2022-07-08 17:32:58 +0100

    rm debug

--- packages/data-service/src/data-service.ts
+++ packages/data-service/src/data-service.ts
@@ -2161,7 +2161,6 @@ export class DataServiceImpl extends EventEmitter implements DataService {
       client.on(
         'topologyDescriptionChanged',
         (evt: TopologyDescriptionChangedEvent) => {
-          debug('topologyDescriptionChanged', evt);
           this._isWritable = this._checkIsWritable(evt);
           this._topologyType = evt.newDescription.type;
           const attr = {
