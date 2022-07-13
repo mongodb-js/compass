d65090db98160764a1f56c0ee4db852d79b8748e
Rhys <Anemy@users.noreply.github.com>
2022-07-13 14:41:38 -0400

    fix(compass-explain-plan): keep query history open when changing to explain tab (#3255)

--- packages/compass-explain-plan/src/index.js
+++ packages/compass-explain-plan/src/index.js
@@ -5,6 +5,7 @@ const ROLE = {
   name: 'Explain Plan',
   component: ExplainPlanPlugin,
   order: 4,
+  hasQueryHistory: true,
   configureStore: configureStore,
   configureActions: () => {},
   storeName: 'ExplainPlan.Store',
