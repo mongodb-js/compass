73a848deaf3d7684ae37cca9e67cf7415f979f24
Maurizio Casimirri <maurizio.cas@gmail.com>
2022-07-15 16:31:08 +0200

    fix(connection-storage):  disable ampersand validations

--- packages/data-service/src/connection-storage.spec.ts
+++ packages/data-service/src/connection-storage.spec.ts
@@ -374,6 +374,32 @@ describe('ConnectionStorage', function () {
         });
       });
     });
+
+    it('saves an X509 connection (make sure old connection-model validations are not triggered)', async function () {
+      const connectionStorage = new ConnectionStorage();
+      const id = uuid();
+      await connectionStorage.save({
+        id,
+        connectionOptions: {
+          connectionString:
+            'mongodb://localhost:27017/?tls=true&tlsCertificateKeyFile=%2FUsers%2Fmaurizio%2FDownloads%2FX509-cert-2577072670755779500.pem',
+        },
+      });
+
+      await eventually(() => {
+        const { connectionInfo } = JSON.parse(
+          fs.readFileSync(getConnectionFilePath(tmpDir, id), 'utf-8')
+        );
+
+        expect(connectionInfo).to.deep.equal({
+          id,
+          connectionOptions: {
+            connectionString:
+              'mongodb://localhost:27017/?tls=true&tlsCertificateKeyFile=%2FUsers%2Fmaurizio%2FDownloads%2FX509-cert-2577072670755779500.pem',
+          },
+        });
+      });
+    });
   });
 
   describe('destroy', function () {
