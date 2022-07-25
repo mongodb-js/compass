mongodb-js/compass

commit 47e5bfae7ee7b3424901c01bf66da21a0a9096e4

fix: prefer half-relaxed EJSON mode (#2971)
Fix various EJSON handling bugs by aligning our EJSON handling
a bit more closely. This fixes regressions introduced by me
in e654e1d63977 (COMPASS-5710, COMPASS-5711 aka
https://github.com/mongodb-js/compass/issues/2960) and another
long-standing export bug (COMPASS-4694).

Specifically, use `{ relaxed: false }` when parsing values
into `HadronDocument`, and use `{ relaxed: false }` but
undo the strict conversion for `$numberInt` and `$numberDouble`.
This is a mode that we may eventually want to upstream
into the Node.js driver’s `bson` package.

packages/hadron-document/src/utils.ts
@@ -1,3 +1,4 @@
+import { EJSON } from 'bson';
 import type { TypeCastMap, TypeCastTypes } from 'hadron-type-checker';
 
 export function fieldStringLen(value: unknown): number {
@@ -8,3 +9,56 @@ export function fieldStringLen(value: unknown): number {
 export type BSONObject = TypeCastMap['Object'];
 export type BSONArray = TypeCastMap['Array'];
 export type BSONValue = TypeCastMap[TypeCastTypes];
+
+export interface HadronEJSONOptions {
+  indent?: number | string;
+}
+
+/**
+ * Turn a BSON value into what we consider idiomatic extended JSON.
+ *
+ * This differs from both the relaxed and strict mode of the 'bson'
+ * package's EJSON class: We preserve the type information for longs
+ * via $numberLong, but redact it for $numberInt and $numberDouble.
+ *
+ * This may seem inconsistent, but considering that the latter two
+ * types are exactly representable in JS and $numberLong is not,
+ * in addition to the fact that this has been historic behavior
+ * in Compass for a long time, this seems like a reasonable choice.
+ *
+ * @param value Any BSON value.
+ * @returns A serialized, human-readable and human-editable string.
+ */
+export function objectToIdiomaticEJSON(
+  value: Readonly<EJSON.SerializableTypes>,
+  options: HadronEJSONOptions = {}
+): string {
+  const serialized = EJSON.serialize(value, {
+    relaxed: false,
+  });
+
+  makeEJSONIdiomatic(serialized);
+
+  return JSON.stringify(
+    serialized,
+    null,
+    'indent' in options ? options.indent : 2
+  );
+}
+
+function makeEJSONIdiomatic(value: EJSON.SerializableTypes): void {
+  if (!value || typeof value !== 'object') return;
+
+  for (const key of Object.keys(value)) {
+    const entry = (value as any)[key];
+    if (entry.$numberInt) {
+      (value as any)[key] = +entry.$numberInt;
+      continue;
+    }
+    if (entry.$numberDouble) {
+      (value as any)[key] = +entry.$numberDouble;
+      continue;
+    }
+    makeEJSONIdiomatic(entry);
+  }
+}

packages/hadron-document/test/document.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
-import Document from '../';
+import Document from '../src/';
 import SharedExamples from './shared-examples';
-import { ObjectId } from 'bson';
+import { ObjectId, Long } from 'bson';
 
 describe('Document', function () {
   describe('#get', function () {
@@ -1557,6 +1557,64 @@ describe('Document', function () {
         it('does not increment the loaded count', function () {
           expect(elements.loaded).to.equal(3);
         });
+      });
+    });
+  });
+
+  describe('EJSON', function () {
+    describe('Document.FromJSON', function () {
+      it('parses strings in EJSON strict mode', function () {
+        const parsed = Document.FromEJSON('{"a": 1}');
+        expect(parsed.get('a').currentType).to.equal('Int32');
+      });
+    });
+
+    describe('Document.FromJSONArray', function () {
+      it('parses arrays', function () {
+        const parsed = Document.FromEJSONArray('[{"a": 1}]');
+        expect(parsed).to.have.lengthOf(1);
+        expect(parsed[0].get('a').currentType).to.equal('Int32');
+      });
+
+      it('lifts single objects to a singleton arrays', function () {
+        const parsed = Document.FromEJSONArray('{"a": 1}');
+        expect(parsed).to.have.lengthOf(1);
+        expect(parsed[0].get('a').currentType).to.equal('Int32');
+      });
+    });
+
+    describe('#toEJSON', function () {
+      it('serializes Int32/Double as relaxed but not Int64', function () {
+        const doc = new Document({
+          a: 1,
+          b: 1.5,
+          c: Long.fromNumber(2),
+        });
+        expect(doc.toEJSON('current', { indent: null })).to.equal(
+          '{"a":1,"b":1.5,"c":{"$numberLong":"2"}}'
+        );
+      });
+
+      it('optionally serializes the current or the original document', function () {
+        const doc = new Document({
+          a: 1,
+          b: 1.5,
+          c: Long.fromNumber(2),
+        });
+        doc.get('a').edit(2);
+        expect(doc.toEJSON('current', { indent: null })).to.equal(
+          '{"a":2,"b":1.5,"c":{"$numberLong":"2"}}'
+        );
+        expect(doc.toEJSON('original', { indent: null })).to.equal(
+          '{"a":1,"b":1.5,"c":{"$numberLong":"2"}}'
+        );
+      });
+
+      it('allows specifying JSON indent', function () {
+        const doc = new Document({ a: 1 });
+        expect(doc.toEJSON('current', { indent: '>' })).to.equal(
+          '{\n>"a": 1\n}'
+        );
       });
     });
   });

packages/compass-aggregations/src/components/pipeline-results-workspace/pipeline-results-list.tsx
@@ -10,7 +10,6 @@ import {
 import { DocumentListView, DocumentJsonView } from '@mongodb-js/compass-crud';
 import { useId } from '@react-aria/utils';
 import HadronDocument from 'hadron-document';
-import { EJSON } from 'bson';
 
 const viewTypeContainer = css({
   display: 'flex',
@@ -93,12 +92,7 @@ export const PipelineResultsList: React.FunctionComponent<
       docs: documents.map((doc) => new HadronDocument(doc)),
       isEditable: false,
       copyToClipboard(doc) {
-        const obj = doc.generateObject();
-        const str = EJSON.stringify(
-          obj as EJSON.SerializableTypes,
-          undefined,
-          2
-        );
+        const str = doc.toEJSON();
         void navigator.clipboard.writeText(str);
       },
     }),

packages/compass-crud/src/components/json-editor.jsx
@@ -1,8 +1,8 @@
-import { EJSON } from 'bson';
 import React from 'react';
 import PropTypes from 'prop-types';
 import jsonParse from 'fast-json-parse';
 import { DocumentList } from '@mongodb-js/compass-components';
+import HadronDocument from 'hadron-document';
 import UpdateDocumentFooter from './document-footer';
 import RemoveDocumentFooter from './remove-document-footer';
 
@@ -96,7 +96,7 @@ class EditableJson extends React.Component {
   }
 
   _getObjectAsString() {
-    return EJSON.stringify(this.props.doc.generateObject(), null, 2);
+    return this.props.doc.toEJSON();
   }
 
   /**
@@ -290,7 +290,7 @@ class EditableJson extends React.Component {
         <UpdateDocumentFooter
           doc={this.props.doc}
           replaceDocument={() => {
-            this.props.doc.apply(EJSON.parse(this.state.value));
+            this.props.doc.apply(HadronDocument.FromEJSON(this.state.value));
             this.props.replaceDocument(this.props.doc);
           }}
           cancelHandler={this.handleCancel.bind(this)}

packages/compass-crud/src/stores/crud-store.js
@@ -1,4 +1,3 @@
-import { EJSON } from 'bson';
 import Reflux from 'reflux';
 import toNS from 'mongodb-ns';
 import { findIndex, isEmpty } from 'lodash';
@@ -379,7 +378,7 @@ const configureStore = (options = {}) => {
      */
     copyToClipboard(doc) {
       track('Document Copied', { mode: this.modeForTelemetry() });
-      const documentJSON = EJSON.stringify(doc.generateObject(), null, 2);
+      const documentJSON = doc.toEJSON();
       let input = document.createElement(INPUT);
       input.type = TYPE;
       input.setAttribute(STYLES, DISPLAY);
@@ -654,7 +653,7 @@ const configureStore = (options = {}) => {
         }
       }
 
-      const jsonDoc = EJSON.stringify(hadronDoc.generateObject(), null, 2);
+      const jsonDoc = hadronDoc.toEJSON();
 
       this.setState({
         insert: {
@@ -700,7 +699,7 @@ const configureStore = (options = {}) => {
      */
     toggleInsertDocument(view) {
       if (view === 'JSON') {
-        const jsonDoc = EJSON.stringify(this.state.insert.doc.generateObject(), null, 2);
+        const jsonDoc = this.state.insert.toEJSON();
 
         this.setState({
           insert: {
@@ -719,7 +718,7 @@ const configureStore = (options = {}) => {
         if (this.state.insert.jsonDoc === '') {
           hadronDoc = this.state.insert.doc;
         } else {
-          hadronDoc = new HadronDocument(EJSON.parse(this.state.insert.jsonDoc), false);
+          hadronDoc = HadronDocument.FromEJSON(this.state.insert.jsonDoc);
         }
 
         this.setState({
@@ -780,7 +779,9 @@ const configureStore = (options = {}) => {
      * Insert a single document.
      */
     insertMany() {
-      const docs = EJSON.parse(this.state.insert.jsonDoc);
+      const docs =
+        HadronDocument.FromEJSONArray(this.state.insert.jsonDoc)
+          .map(doc => doc.generateObject());
       track('Document Inserted', {
         mode: this.state.insert.jsonView ? 'json' : 'field-by-field',
         multiple: docs.length > 1
@@ -833,7 +834,7 @@ const configureStore = (options = {}) => {
       let doc;
 
       if (this.state.insert.jsonView) {
-        doc = EJSON.parse(this.state.insert.jsonDoc);
+        doc = HadronDocument.FromEJSON(this.state.insert.jsonDoc).generateObject();
       } else {
         doc = this.state.insert.doc.generateObject();
       }

packages/compass-e2e-tests/tests/collection-documents-tab.test.ts
@@ -401,6 +401,53 @@ FindIterable<Document> result = collection.find(filter);`);
     );
   });
 
+  it('supports view/edit for Int64 values via json view', async function () {
+    await browser.runFindOperation('Documents', '{ i: 123 }');
+    await browser.clickVisible(Selectors.SelectJSONView);
+
+    const document = await browser.$(Selectors.DocumentJSONEntry);
+    await document.waitForDisplayed();
+
+    await waitForJSON(browser, document);
+
+    const json = await document.getText();
+    expect(json.replace(/\s+/g, ' ')).to.match(
+      /^\{ "_id": \{ "\$oid": "[a-f0-9]{24}" \}, "i": 123, "j": 0 \}$/
+    );
+
+    await browser.hover('[data-test-id="editable-json"]');
+    await browser.clickVisible('[data-testid="edit-document-button"]');
+
+    const newjson = JSON.stringify({
+      ...JSON.parse(json),
+      j: { $numberLong: '12345' },
+    });
+
+    await browser.setAceValue(
+      '[data-test-id="editable-json"] #ace-editor',
+      newjson
+    );
+
+    const footer = await document.$(Selectors.DocumentFooterMessage);
+    expect(await footer.getText()).to.equal('Document Modified.');
+
+    const button = await document.$('[data-test-id="update-document-button"]');
+    await button.click();
+    await footer.waitForDisplayed({ reverse: true });
+
+    await browser.runFindOperation('Documents', '{ i: 123 }');
+    await browser.clickVisible(Selectors.SelectJSONView);
+
+    const modifiedDocument = await browser.$(Selectors.DocumentJSONEntry);
+    await modifiedDocument.waitForDisplayed();
+
+    await waitForJSON(browser, modifiedDocument);
+
+    expect((await modifiedDocument.getText()).replace(/\s+/g, ' ')).to.match(
+      /^\{ "_id": \{ "\$oid": "[a-f0-9]{24}" \}, "i": 123, "j": \{\.\.\.\} \}$/
+    );
+  });
+
   it('supports view/edit via table view', async function () {
     await browser.runFindOperation('Documents', '{ i: 33 }');
     await browser.clickVisible(Selectors.SelectTableView);

packages/compass-e2e-tests/tests/collection-import.test.ts
@@ -127,7 +127,10 @@ describe('Collection import', function () {
     await insertDialog.waitForDisplayed();
 
     // set the text in the editor
-    await browser.setAceValue(Selectors.InsertJSONEditor, '{ "foo": 10 }');
+    await browser.setAceValue(
+      Selectors.InsertJSONEditor,
+      '{ "foo": 10, "long": { "$numberLong": "99" } }'
+    );
 
     // confirm
     const insertConfirm = await browser.$(Selectors.InsertConfirm);
@@ -156,6 +159,7 @@ describe('Collection import', function () {
 
     expect(result).to.deep.equal({
       foo: '10',
+      long: '99',
     });
   });
 

packages/compass-import-export/src/utils/formatters.js
@@ -3,7 +3,7 @@
 /* eslint-disable complexity */
 
 import * as csv from 'fast-csv';
-import { EJSON } from 'bson';
+import HadronDocument from 'hadron-document';
 import { serialize as flatten } from './bson-csv';
 import { Transform } from 'stream';
 import { EOL } from 'os';
@@ -22,7 +22,9 @@ export const createJSONFormatter = function ({ brackets = true } = {}) {
           this.push(EOL);
         }
       }
-      const s = EJSON.stringify(doc, null, brackets ? 2 : null);
+      const s = new HadronDocument(doc).toEJSON('current', {
+        indent: brackets ? 2 : undefined,
+      });
       if (this._counter === undefined) {
         this._counter = 0;
         if (brackets) {

commit 0ac97eacbfc89699b85f004ac51d34360cd900d5

fix(hadron-document): handle oddball fp numbers well in `.toEJSON()` COMPASS-5974
Bug introduced by yours truly in 47e5bfae7ee7b.

packages/hadron-document/src/utils.ts
@@ -60,7 +60,14 @@ function makeEJSONIdiomatic(value: EJSON.SerializableTypes): void {
       continue;
     }
     if (entry.$numberDouble) {
-      (value as any)[key] = +entry.$numberDouble;
+      if (
+        Number.isFinite(+entry.$numberDouble) &&
+        !Object.is(+entry.$numberDouble, -0)
+      ) {
+        // EJSON can represent +/-Infinity or NaN values but JSON can't
+        // (and -0 can be parsed from JSON but not serialized by JSON.stringify).
+        (value as any)[key] = +entry.$numberDouble;
+      }
       continue;
     }
     makeEJSONIdiomatic(entry);

packages/hadron-document/test/document.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import Document from '../src/';
 import SharedExamples from './shared-examples';
-import { ObjectId, Long, Int32 } from 'bson';
+import { ObjectId, Long, Int32, Double } from 'bson';
 
 describe('Document', function () {
   describe('#get', function () {
@@ -2056,6 +2056,27 @@ describe('Document', function () {
         const doc = new Document({ a: 1 });
         expect(doc.toEJSON('current', { indent: '>' })).to.equal(
           '{\n>"a": 1\n}'
+        );
+      });
+
+      it('handles oddball floating point values', function () {
+        const doc = new Document({
+          negzero: new Double(-0),
+          int: new Int32(1),
+          inf: Infinity,
+          ninf: -Infinity,
+          nan: NaN,
+        });
+        expect(doc.toEJSON('current', { indent: undefined })).to.equal(
+          '{' +
+            [
+              '"negzero":{"$numberDouble":"-0.0"},',
+              '"int":1,',
+              '"inf":{"$numberDouble":"Infinity"},',
+              '"ninf":{"$numberDouble":"-Infinity"},',
+              '"nan":{"$numberDouble":"NaN"}',
+            ].join('') +
+            '}'
         );
       });
     });