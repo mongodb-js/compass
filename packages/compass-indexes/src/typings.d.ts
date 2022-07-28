91534145cdfea6ef914e88d25fb6974b0d20b041
Alena Khineika <alena.khineika@gmail.com>
2022-07-28 13:18:48 +0200

    feat: declare module mongodb-query-parser

---
+++ packages/compass-indexes/src/typings.d.ts
@@ -0,0 +1,5 @@
+declare module 'mongodb-query-parser' {
+    import type { CollationOptions } from 'mongodb';
+    const isCollationValid: (collationString: string) => false | null | CollationOptions;
+    export { isCollationValid };
+}
