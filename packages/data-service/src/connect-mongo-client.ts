23ada91090c8c8a097db1680aac19989544e62ba
Anna Henningsen <anna.henningsen@mongodb.com>
2022-07-29 18:54:41 +0200

    chore(deps): bump driver, shell, libmongocrypt (#3301)
    Re-do of https://github.com/mongodb-js/compass/pull/3266 with
    the latest versions of driver, mongosh and mongodb-client-encryption.
    Resolves COMPASS-5767 and prepares us for the next driver release
    expected to drop early next week.

--- packages/data-service/src/connect-mongo-client.ts
+++ packages/data-service/src/connect-mongo-client.ts
@@ -45,7 +45,6 @@ export default async function connectMongoClientCompass(
       ...options.autoEncryption,
       extraOptions: {
         ...options.autoEncryption?.extraOptions,
-        // @ts-expect-error next driver release has types
         cryptSharedLibPath: process.env.COMPASS_CRYPT_LIBRARY_PATH,
       },
     };
