391a33f0c437335ce387f0043a81d9a1494f648c
Basit <baasit121@icloud.com>
2022-08-26 13:53:31 +0200

    reformat

--- packages/compass-indexes/src/modules/create-index/is-sparse.ts
+++ packages/compass-indexes/src/modules/create-index/is-sparse.ts
@@ -1,17 +1,20 @@
 enum ActionTypes {
   ToggleIsSparse = 'indexes/create-indexes/is-sparse/ToggleIsSparse',
-};
+}
 
 type ToggleIsSparseAction = {
   type: ActionTypes.ToggleIsSparse;
   isSparse: boolean;
-}
+};
 
 type State = boolean;
 
 export const INITIAL_STATE: State = false;
 
-export default function reducer(state: State = INITIAL_STATE, action: ToggleIsSparseAction) {
+export default function reducer(
+  state: State = INITIAL_STATE,
+  action: ToggleIsSparseAction
+) {
   if (action.type === ActionTypes.ToggleIsSparse) {
     return action.isSparse;
   }
