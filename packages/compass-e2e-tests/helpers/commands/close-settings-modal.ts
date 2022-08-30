88afd3fe9fe60cc6e969e097b635c8217760cb2f
Basit <baasit121@icloud.com>
2022-08-30 18:57:52 +0200

    remove log

--- packages/compass-e2e-tests/helpers/commands/close-settings-modal.ts
+++ packages/compass-e2e-tests/helpers/commands/close-settings-modal.ts
@@ -5,19 +5,14 @@ export async function closeSettingsModal(
   browser: CompassBrowser
 ): Promise<void> {
   if (!(await browser.existsEventually(Selectors.SettingsModal))) {
-    console.log('in the first run. modal non-existant');
     return;
   }
 
-  console.log('in the first run. modal found');
-  const settingsModalElement = await browser.$(
-    Selectors.SettingsModal
-  );
+  const settingsModalElement = await browser.$(Selectors.SettingsModal);
 
   await settingsModalElement.waitForDisplayed();
   await browser.clickVisible(Selectors.CloseSettingsModalButton);
   await settingsModalElement.waitForDisplayed({
     reverse: true,
   });
-  console.log('in the first run. modal closed');
 }
