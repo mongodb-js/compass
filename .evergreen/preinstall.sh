b302e62d627a832cdbc9561345bfdfa8bd6be84f
Anna Henningsen <anna.henningsen@mongodb.com>
2022-09-05 16:19:06 +0200

    chore(ci): package and test on macos-1100-arm64-gui (#3429)

--- .evergreen/preinstall.sh
+++ .evergreen/preinstall.sh
@@ -6,12 +6,12 @@ echo "========================="
 echo "Important Environment Variables"
 echo "========================="
 echo "PLATFORM: $PLATFORM"
+echo "ARCH: $ARCH"
 echo "NODE_JS_VERSION: $NODE_JS_VERSION"
 echo "NPM_VERSION: $NPM_VERSION"
 echo "APPDATA: $APPDATA"
 echo "PATH: $PATH"
 
-echo "PLATFORM: $PLATFORM"
 echo "IS_OSX: $IS_OSX"
 echo "IS_LINUX: $IS_LINUX"
 echo "IS_WINDOWS: $IS_WINDOWS"
@@ -23,13 +23,13 @@ SCRIPTDIR="$(cd $(dirname "$0"); pwd)"
 if [ -n "$IS_WINDOWS" ]; then
     echo "Installing nodejs v$NODE_JS_VERSION for windows..."
     bash "${SCRIPTDIR}/retry-with-backoff.sh" curl -fs \
-    -o ".deps/node-v$NODE_JS_VERSION-win-x64.zip" \
-    --url "https://nodejs.org/download/release/v$NODE_JS_VERSION/node-v$NODE_JS_VERSION-win-x64.zip"
+    -o ".deps/node-v$NODE_JS_VERSION-win-$ARCH.zip" \
+    --url "https://nodejs.org/download/release/v$NODE_JS_VERSION/node-v$NODE_JS_VERSION-win-$ARCH.zip"
     cd .deps
     ls -alh
-    unzip -q node-v$NODE_JS_VERSION-win-x64.zip
-    mv node-v$NODE_JS_VERSION-win-x64/* .
-    rm -rf node-v$NODE_JS_VERSION-win-x64
+    unzip -q node-v$NODE_JS_VERSION-win-$ARCH.zip
+    mv node-v$NODE_JS_VERSION-win-$ARCH/* .
+    rm -rf node-v$NODE_JS_VERSION-win-$ARCH
 
     echo "Installing npm@$NPM_VERSION..."
     rm -rf npm npx npm.cmd npx.cmd
@@ -43,13 +43,13 @@ if [ -n "$IS_WINDOWS" ]; then
     cd ..
     .evergreen/node-gyp-bug-workaround.sh
 else
-    echo "Installing nodejs v${NODE_JS_VERSION} for ${PLATFORM}..."
+    echo "Installing nodejs v${NODE_JS_VERSION} for ${PLATFORM} on ${ARCH}..."
 
     bash "${SCRIPTDIR}/retry-with-backoff.sh" curl -fs \
-        -o ".deps/node-v${NODE_JS_VERSION}-${PLATFORM}-x64.tar.gz" \
-        --url "https://nodejs.org/download/release/v${NODE_JS_VERSION}/node-v${NODE_JS_VERSION}-${PLATFORM}-x64.tar.gz"
+        -o ".deps/node-v${NODE_JS_VERSION}-${PLATFORM}-$ARCH.tar.gz" \
+        --url "https://nodejs.org/download/release/v${NODE_JS_VERSION}/node-v${NODE_JS_VERSION}-${PLATFORM}-$ARCH.tar.gz"
     cd .deps
-    tar xzf node-v$NODE_JS_VERSION-$PLATFORM-x64.tar.gz --strip-components=1
+    tar xzf node-v$NODE_JS_VERSION-$PLATFORM-$ARCH.tar.gz --strip-components=1
 
     echo "Installing latest npm..."
     rm -rf npm npx
