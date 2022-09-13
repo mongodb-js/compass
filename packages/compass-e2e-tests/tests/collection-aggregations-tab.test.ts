c6493b2f157e36eef5096d7b607ef1e186ead423
Alena Khineika <alena.khineika@gmail.com>
2022-09-13 22:18:59 +0200

    test: remove only

--- packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
+++ packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
@@ -61,7 +61,7 @@ async function getDocuments(browser: CompassBrowser) {
   return parsed;
 }
 
-describe.only('Collection aggregations tab', function () {
+describe('Collection aggregations tab', function () {
   let compass: Compass;
   let browser: CompassBrowser;
 
