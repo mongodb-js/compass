d329c7c9f48ed4dc27b53f398e38e2bcf989147b
Le Roux Bodenstein <lerouxb@gmail.com>
2022-09-23 11:46:14 +0100

    Change the stage disable checkbox to xsmall, remove tooltip

--- packages/compass-aggregations/src/components/stage-editor-toolbar/toggle-stage.jsx
+++ packages/compass-aggregations/src/components/stage-editor-toolbar/toggle-stage.jsx
@@ -1,9 +1,10 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Tooltip } from 'hadron-react-components';
-import { Toggle } from '@mongodb-js/compass-components';
+import { Toggle, css, spacing } from '@mongodb-js/compass-components';
 
-import styles from './toggle-stage.module.less';
+const toggleStyle = css({
+  marginLeft: spacing[1]
+});
 
 /**
  * The toggle stage button.
@@ -38,22 +39,14 @@ class ToggleStage extends PureComponent {
       ? 'Exclude stage from pipeline'
       : 'Include stage in pipeline';
     return (
-      <div
-        className={styles['toggle-stage']}
-        data-for="toggle-stage"
-        data-tip={TOOLTIP}
-        data-place="top"
-      >
-        <Toggle
-          id="toggle-stage-button"
-          checked={this.props.isEnabled}
-          onChange={this.onStageToggled}
-          className={styles['toggle-stage-button']}
-          aria-label={TOOLTIP}
-          size="small"
-        />
-        <Tooltip id="toggle-stage" />
-      </div>
+      <Toggle
+        id="toggle-stage-button"
+        checked={this.props.isEnabled}
+        onChange={this.onStageToggled}
+        aria-label={TOOLTIP}
+        size="xsmall"
+        className={toggleStyle}
+      />
     );
   }
 }
