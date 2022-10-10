6e8479d5d8e2d42558256149bd9931767496088e
Basit <baasit121@icloud.com>
2022-10-10 18:24:44 +0200

    parser

---
+++ packages/compass-aggregations/src/modules/pipeline-builder/pipeline-parser/pipeline-parser.spec.ts
@@ -0,0 +1,106 @@
+import { expect } from 'chai';
+import PipelineParser from './pipeline-parser';
+
+const pipelines = [
+  {
+    input: `[{$unwind: "users"}]`,
+    output: [
+      '[',
+      '  {',
+      '    $unwind: "users",',
+      '  },',
+      ']'
+    ].join('\n')
+  },
+  {
+    input: `[\n//{$unwind: "users"},\n // {$limit: 20}\n]`,
+    output: [
+      '[',
+      '  // {',
+      '  //   $unwind: "users",',
+      '  // }',
+      '  // {',
+      '  //   $limit: 20,',
+      '  // }',
+      ']'
+    ].join('\n')
+  },
+  {
+    input: `[{$unwind: "users"},\n // {$limit: 20},\n {$sort: {name: -1}}]`,
+    output: [
+      '[',
+      '  {',
+      '    $unwind: "users",',
+      '  }',
+      '  // {',
+      '  //   $limit: 20,',
+      '  // }',
+      '  {',
+      '    $sort: {',
+      '      name: -1,',
+      '    },',
+      '  },',
+      ']'
+    ].join('\n')
+  }
+];
+
+describe('PipelineParser', function () {
+  describe('parses text', function () {
+    it('throws if pipeline is not a valid array', function () {
+      [``, '{}', 'hello', '20'].forEach(expression => {
+        expect(() => {
+          PipelineParser.parse(expression);
+        }).to.throw;
+      });
+      expect(() => {
+        PipelineParser.parse(`[]`);
+      }).to.not.throw;
+    });
+    it('parses commented out pipeline', function () {
+      const pipeline = `[
+        // {$unwind: "users"},
+        // {$limit: 20},
+        // {$skip: 100},
+      ]`;
+      const { stages } = PipelineParser.parse(pipeline);
+      expect(stages.length).to.equal(3);
+    });
+    it('parses a pipeline', function () {
+      const pipeline = `[
+        {$match: {name: /berlin/i}},
+        {$unwind: "users"},
+        // {$limit: 20},
+        // {$skip: 100},
+      ]`;
+      const { stages } = PipelineParser.parse(pipeline);
+      expect(stages.length).to.equal(4);
+    });
+  });
+  describe('validates text', function () {
+    it('throws if pipeline is not a valid array', function () {
+      [``, '{}', 'hello', '20'].forEach(expression => {
+        const { errors } = PipelineParser.validate(expression);
+        expect(errors[0]).to.be.instanceOf(SyntaxError);
+      });
+    });
+    it('validates array stages', function () {
+      const pipeline = `[
+        {$match: {name: /berlin/i}},
+        {limit: 20},
+        {$skip: 20, $limit: 30},
+      ]`;
+
+      const { errors } = PipelineParser.validate(pipeline);
+      expect(errors.length).to.equal(2);
+      errors.forEach(x => expect(x).to.be.instanceOf(SyntaxError));
+    });
+  });
+  it('generates pipeline string', function () {
+    pipelines.forEach(({ input, output }) => {
+      const { root, stages } = PipelineParser.parse(input);
+      const generatedPipelineString = PipelineParser.generate(root, stages);
+      expect(generatedPipelineString).to.equal(output);
+    });
+  });
+});
