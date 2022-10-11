3f3d14fd9a2b4f7125c6150aecad24e90f6ee151
Rhys Howell <rhysh@live.com>
2022-10-11 16:46:54 -0400

    remove unused ModalCheckbox component

--- packages/hadron-react-components/src/index.js
+++ packages/hadron-react-components/src/index.js
@@ -1,6 +1,5 @@
 import FormInput from './form-input';
 import ModalInput from './modal-input';
-import ModalCheckbox from './modal-checkbox';
 import InfoSprinkle from './info-sprinkle';
 import ModalStatusMessage from './modal-status-message';
 import StatusRow from './status-row';
@@ -13,7 +12,6 @@ import ZeroState from './zero-state';
 export {
   FormInput,
   ModalInput,
-  ModalCheckbox,
   InfoSprinkle,
   ModalStatusMessage,
   StatusRow,
