2af3ac82e4a7a2bcd1ce5172587293c52fc8b4ea
Basit <1305718+mabaasit@users.noreply.github.com>
2022-10-19 09:44:07 +0200

    feat(pipeline-builder): pipeline builder toggle COMPASS-6169 (#3605)

--- packages/compass-aggregations/src/components/pipeline-toolbar/pipeline-settings/pipeline-extra-settings.spec.tsx
+++ packages/compass-aggregations/src/components/pipeline-toolbar/pipeline-settings/pipeline-extra-settings.spec.tsx
@@ -1,46 +1,64 @@
 import React from 'react';
+import type { ComponentProps } from 'react';
 import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { expect } from 'chai';
 import { spy } from 'sinon';
-import type { SinonSpy } from 'sinon';
 
 import { PipelineExtraSettings } from './pipeline-extra-settings';
 
-describe('PipelineExtraSettings', function () {
-  let container: HTMLElement;
-  let onToggleAutoPreviewSpy: SinonSpy;
-  let onToggleSettingsSpy: SinonSpy;
-  beforeEach(function () {
-    onToggleAutoPreviewSpy = spy();
-    onToggleSettingsSpy = spy();
-    render(
-      <PipelineExtraSettings
-        isAutoPreview={true}
-        onToggleAutoPreview={onToggleAutoPreviewSpy}
-        onToggleSettings={onToggleSettingsSpy}
-      />
-    );
-    container = screen.getByTestId('pipeline-toolbar-extra-settings');
-  });
+const renderPipelineExtraSettings = (
+  props: Partial<ComponentProps<typeof PipelineExtraSettings>> = {}
+) => {
+  return render(
+    <PipelineExtraSettings
+      isAutoPreview={true}
+      pipelineMode={'builder-ui'}
+      onToggleAutoPreview={() => {}}
+      onChangePipelineMode={() => {}}
+      onToggleSettings={() => {}}
+      {...props}
+    />
+  );
+};
 
+describe('PipelineExtraSettings', function () {
   it('calls onToggleAutoPreview when clicked', function () {
+    const onToggleAutoPreviewSpy = spy();
+    renderPipelineExtraSettings({
+      onToggleAutoPreview: onToggleAutoPreviewSpy,
+    });
+    const container = screen.getByTestId('pipeline-toolbar-extra-settings');
     const toggle = within(container).getByTestId(
       'pipeline-toolbar-preview-toggle'
     );
     expect(toggle).to.exist;
     userEvent.click(toggle);
-
     expect(onToggleAutoPreviewSpy.calledOnce).to.be.true;
   });
 
   it('calls onToggleSettings when clicked', function () {
+    const onToggleSettingsSpy = spy();
+    renderPipelineExtraSettings({ onToggleSettings: onToggleSettingsSpy });
+    const container = screen.getByTestId('pipeline-toolbar-extra-settings');
     const button = within(container).getByTestId(
       'pipeline-toolbar-settings-button'
     );
     expect(button).to.exist;
     userEvent.click(button);
-
     expect(onToggleSettingsSpy.calledOnce).to.be.true;
+  });
+
+  it('does not show pipeline builder mode when feature flag is not enabled', function () {
+    renderPipelineExtraSettings();
+    expect(() => {
+      screen.getByTestId('pipeline-builder-toggle');
+    }).to.throw;
+  });
+  it('shows pipeline builder mode when feature flag is enabled', function () {
+    process.env.COMPASS_ENABLE_AS_TEXT_PIPELINE = 'true';
+    renderPipelineExtraSettings();
+    const container = screen.getByTestId('pipeline-toolbar-extra-settings');
+    expect(within(container).getByTestId('pipeline-builder-toggle')).to.exist;
   });
 });
