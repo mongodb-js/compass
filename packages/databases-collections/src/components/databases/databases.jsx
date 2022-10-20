95ad1eaaf88c14975da5ac6a5f86382746f6faf4
Rhys Howell <rhysh@live.com>
2022-10-20 15:04:50 -0400

    update empty state graphics

--- packages/databases-collections/src/components/databases/databases.jsx
+++ packages/databases-collections/src/components/databases/databases.jsx
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import { Banner, BannerVariant, EmptyContent, Link, css, spacing } from '@mongodb-js/compass-components';
 import { DatabasesList } from '@mongodb-js/databases-collections-list';
 
+import { ZeroGraphic } from '../zero-graphic';
+
 const errorContainerStyles = css({
   padding: spacing[3],
 });
@@ -23,26 +25,11 @@ const DOCUMENTATION_LINK = 'https://www.mongodb.com/cloud/atlas';
 
 const ERROR_WARNING = 'An error occurred while loading databases';
 
-const DatabaseErrorZeroGraphic = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="51" viewBox="0 0 48 51">
-    <g fill="none" fillRule="evenodd" transform="translate(1 1)">
-      <g transform="translate(23.125 26.25)">
-        <polygon stroke="#15CC62" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.75" points=".313 22.813 22.813 22.813 11.563 .313"/>
-        <path stroke="#116149" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.75" d="M11.5625,9.6875 L11.5625,16.25"/>
-        <path fill="#116149" d="M11.5625,18.125 C11.045,18.125 10.625,18.545 10.625,19.0625 C10.625,19.58 11.045,20 11.5625,20 C12.08,20 12.5,19.58 12.5,19.0625 C12.5,18.545 12.08,18.125 11.5625,18.125 Z"/>
-      </g>
-      <g stroke="#116149" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.75">
-        <path d="M6.25 1.25C5.07716795 1.64190062 4.26189876 2.66207556 4.1764842 3.8439516L4.1764842 15.758164C4.23244618 18.0107406 2.36333618 19.8804049 0 19.935434L0 20.0701891C2.36097956 20.1224005 4.23126787 21.9892594 4.1764842 24.2395943 4.1764842 24.2418486 4.1764842 24.2446539 4.1764842 24.2474592L4.1764842 36.1560522C4.26189876 37.3379269 5.07716795 38.3581081 6.25 38.75M38.75 1.25C39.9224317 1.64077724 40.7375907 2.6615145 40.8217582 3.8439516L40.8217582 15.758164C40.7687921 18.0118677 42.6380836 19.8804049 45 19.935434L45 20.0701891C42.6374898 20.1224005 40.7670235 21.9915012 40.8217582 24.2452174 40.8217582 26.4594297 40.8217582 28.1197153 40.8217582 29.2260742M10 38.75C10 38.75 15.8403771 39.4564507 23.2208907 39.4600749M10 20C10 20 23.2085236 21.5971375 35 20M10 2.5C10 2.5 23.2085236 4.09713625 35 2.5M10 .777980134C14.203048.262413509 24.428028-.679055991 35 .777980134"/>
-      </g>
-    </g>
-  </svg>
-);
-
 function NonGenuineZeroState() {
   return (
     <div className={nonGenuineErrorContainerStyles} data-testid="databases-non-genuine-warning">
       <EmptyContent
-        icon={DatabaseErrorZeroGraphic}
+        icon={ZeroGraphic}
         title="Unable to display databases and collections"
         subTitle={NON_GENUINE_SUBTEXT}
         callToActionLink={
