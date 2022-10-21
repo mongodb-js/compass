43faf065b18f2690358fa04ef0ad0a10864bb9e6
Le Roux Bodenstein <lerouxb@gmail.com>
2022-10-21 19:40:33 +0100

    chore(e2e): Modal e2e tests (#3626)
    * shell info modal test
    
    * e2e tests for the Edit Connection String confirmation modal
    
    * open, rename, delete saved item modal tests
    
    * rename rather
    
    * e2e tests for duplicate view and modify view
    
    * focusing to try and debug windows
    
    * try the same hover retry code from elsewhere
    
    * shorter wait timeout
    
    * debug copy to clipboard on windows
    
    * fix, remove focus
    
    * increase wait timeout again

---
+++ packages/compass-e2e-tests/helpers/commands/show-shell.ts
@@ -0,0 +1,15 @@
+import type { CompassBrowser } from '../compass-browser';
+import retryWithBackoff from '../retry-with-backoff';
+import * as Selectors from '../selectors';
+
+export async function showShell(browser: CompassBrowser): Promise<void> {
+  // Expand the shVdell
+  await retryWithBackoff(async function () {
+    const shellContentElement = await browser.$(Selectors.ShellContent);
+    if (!(await shellContentElement.isDisplayed())) {
+      await browser.clickVisible(Selectors.ShellExpandButton);
+    }
+
+    await browser.clickVisible(Selectors.ShellInput);
+  });
+}
