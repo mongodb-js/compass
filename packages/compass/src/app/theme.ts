e113ab657c5dadbf9375262f39f1d26b46aa6ea8
Anna Henningsen <anna.henningsen@mongodb.com>
2022-10-26 17:12:53 +0200

    fixup: move electron nativeTheme management to main process

--- packages/compass/src/app/theme.ts
+++ packages/compass/src/app/theme.ts
@@ -1,15 +1,8 @@
-import type { THEMES } from 'compass-preferences-model';
 import preferences from 'compass-preferences-model';
 import * as darkreader from 'darkreader';
 import * as remote from '@electron/remote';
 
 const darkreaderOptions = { brightness: 100, contrast: 90, sepia: 10 };
-
-const compassThemeToElectronTheme: Record<THEMES, typeof remote.nativeTheme.themeSource> = {
-  DARK: 'dark',
-  LIGHT: 'light',
-  OS_THEME: 'system'
-} as const;
 
 function onNativeThemeUpdated () {
   if (!preferences.getPreferences().lgDarkmode) {
@@ -22,11 +15,6 @@ function onNativeThemeUpdated () {
 }
 
 export function setupTheme() {
-  preferences.onPreferenceValueChanged('theme', value => {
-    const electronThemeSource = compassThemeToElectronTheme[value];
-    remote.nativeTheme.themeSource = electronThemeSource;
-  });
-
   remote.nativeTheme.on('updated', onNativeThemeUpdated);
   onNativeThemeUpdated();
 }
