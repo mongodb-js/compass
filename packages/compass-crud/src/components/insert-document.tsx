44b3331ef9395ef266f815358079f3851867cf6b
Rhys Howell <rhysh@live.com>
2022-10-31 16:21:06 -0400

    use leafygreen view switch, remove hadron-react-components one

---
+++ packages/compass-crud/src/components/insert-document.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { css, DocumentList, spacing } from '@mongodb-js/compass-components';
+import type HadronDocumentType from 'hadron-document';
+
+const insertDocumentStyles = css({
+  // We give it a good amount of spacing for dropdown menus.
+  paddingBottom: spacing[7],
+});
+
+type InsertDocumentProps = {
+  doc: HadronDocumentType;
+};
+
+function InsertDocument({ doc }: InsertDocumentProps) {
+  return (
+    <div className={insertDocumentStyles} data-testid="insert-document-modal">
+      <DocumentList.Document value={doc} editable editing />
+    </div>
+  );
+}
+
+export default InsertDocument;
