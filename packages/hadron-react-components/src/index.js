d0d9102be8e62317525bc412873ae2bdee1957a3
Rhys <Anemy@users.noreply.github.com>
2022-10-31 14:34:18 -0400

    chore(hadron-react-components): remove unused OptionSelector component (#3665)

--- packages/hadron-react-components/src/index.js
+++ packages/hadron-react-components/src/index.js
@@ -2,7 +2,6 @@ import FormInput from './form-input';
 import ModalInput from './modal-input';
 import InfoSprinkle from './info-sprinkle';
 import ModalStatusMessage from './modal-status-message';
-import OptionSelector from './option-selector';
 import StoreConnector from './store-connector';
 import Tooltip from './tooltip';
 import ViewSwitcher from './view-switcher';
@@ -12,7 +11,6 @@ export {
   ModalInput,
   InfoSprinkle,
   ModalStatusMessage,
-  OptionSelector,
   StoreConnector,
   Tooltip,
   ViewSwitcher
