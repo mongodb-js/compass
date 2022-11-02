08e75ee089a12b4380b039825fcd84652bd9612a
Rhys <Anemy@users.noreply.github.com>
2022-11-02 11:16:27 -0400

    chore(crud): use LeafyGreen SegmentedControl component remove ViewSwitcher (#3672)

--- packages/hadron-react-components/src/index.js
+++ packages/hadron-react-components/src/index.js
@@ -4,7 +4,6 @@ import InfoSprinkle from './info-sprinkle';
 import ModalStatusMessage from './modal-status-message';
 import StoreConnector from './store-connector';
 import Tooltip from './tooltip';
-import ViewSwitcher from './view-switcher';
 
 export {
   FormInput,
@@ -12,6 +11,5 @@ export {
   InfoSprinkle,
   ModalStatusMessage,
   StoreConnector,
-  Tooltip,
-  ViewSwitcher
+  Tooltip
 };
