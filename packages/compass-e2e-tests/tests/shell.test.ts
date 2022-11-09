6b865ede9fea53ef81748883e599ffc5421f9164
Alena Khineika <alena.khineika@gmail.com>
2022-11-09 13:57:01 +0100

    test: try readOnly false for shell e2e

--- packages/compass-e2e-tests/tests/shell.test.ts
+++ packages/compass-e2e-tests/tests/shell.test.ts
@@ -27,6 +27,7 @@ describe('Shell', function () {
   });
 
   it('has an info modal', async function () {
+    await browser.setFeature('readOnly', false);
     await browser.setFeature('enableShell', true);
     await browser.connectWithConnectionString('mongodb://localhost:27091/test');
 
@@ -43,6 +44,7 @@ describe('Shell', function () {
   });
 
   it('shows and hides shell based on settings', async function () {
+    await browser.setFeature('readOnly', false);
     await browser.setFeature('enableShell', true);
     await browser.connectWithConnectionString('mongodb://localhost:27091/test');
 
