0f6d0e7812bef6a14c706aca346e397e818d2467
Rhys <Anemy@users.noreply.github.com>
2022-11-14 20:02:56 -0500

    chore(compass-schema): Update schema text components to leafygreen COMPASS-6235 (#3750)

--- packages/compass-schema/src/constants/schema.ts
+++ packages/compass-schema/src/constants/schema.ts
@@ -1,11 +1,10 @@
 const constants = {
-  SCHEMA_PROBABILITY_PERCENT: 'schema-probability-percent',
   DECIMAL_128: 'Decimal128',
   DOUBLE: 'Double',
   LONG: 'Long',
   INT_32: 'Int32',
   STRING: 'String',
   NUMBER: 'Number',
-};
+} as const;
 
 export default constants;
