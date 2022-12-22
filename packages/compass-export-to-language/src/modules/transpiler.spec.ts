ee90702c2164df67d567f3815fe596b059513dbd
Alena Khineika <alena.khineika@gmail.com>
2022-12-22 17:08:47 +0100

    test: move position of the imported class

--- packages/compass-export-to-language/src/modules/transpiler.spec.ts
+++ packages/compass-export-to-language/src/modules/transpiler.spec.ts
@@ -148,12 +148,12 @@ FindIterable<Document> result = collection.find(filter);`);
         .equal(`import static com.mongodb.client.model.Filters.eq;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.conversions.Bson;
 import java.util.concurrent.TimeUnit;
 import org.bson.Document;
+import com.mongodb.client.FindIterable;
 /*
  * Requires the MongoDB Java Driver.
  * https://mongodb.github.io/mongo-java-driver
