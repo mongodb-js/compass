34d6f9730592c4185eeb1161ba31a9835abfe7a9
Alena Khineika <alena.khineika@gmail.com>
2022-12-22 19:03:02 +0100

    fix: export to Language (Java) has incorrect class name COMPASS-6159 (#3914)
    * fix: export to Language (Java) has incorrect class name COMPASS-6159
    
    * refactor: remove bootstrap
    
    * fix: use imports depending on mode
    
    * docs: update readme
    
    * refactor: reformat
    
    * refactor: make code more readable
    
    * refactor: remove extra check
    
    * refactor: eventually for of
    
    * test: move position of the imported class

--- packages/compass-e2e-tests/tests/collection-documents-tab.test.ts
+++ packages/compass-e2e-tests/tests/collection-documents-tab.test.ts
@@ -321,12 +321,12 @@ describe('Collection documents tab', function () {
     expect(text).to.equal(`import static com.mongodb.client.model.Filters.eq;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.conversions.Bson;
 import java.util.concurrent.TimeUnit;
 import org.bson.Document;
+import com.mongodb.client.FindIterable;
 
 /*
  * Requires the MongoDB Java Driver.
