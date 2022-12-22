abe82572a39339c62f28bc3d6b3b628f24c74247
Alena Khineika <alena.khineika@gmail.com>
2022-12-22 15:26:57 +0100

    refactor: reformat

--- packages/compass-export-to-language/src/modules/transpiler.ts
+++ packages/compass-export-to-language/src/modules/transpiler.ts
@@ -84,7 +84,10 @@ export function runTranspiler({
 
   if (includeImports) {
     // bson-transpilers appears to be stateful, so getImports() has to be called after compile()
-    const imports = compiler.shell[outputLanguage].getImports(mode, includeDrivers);
+    const imports = compiler.shell[outputLanguage].getImports(
+      mode,
+      includeDrivers
+    );
     output = `${imports as string}\n\n${output}`;
   }
 
