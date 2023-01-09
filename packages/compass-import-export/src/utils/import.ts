d992e285ae46ec95cbd030806c2e22bdd8a31ed0
Le Roux Bodenstein <lerouxb@gmail.com>
2023-01-09 18:25:16 +0000

    stubs for import and export functions

---
+++ packages/compass-import-export/src/utils/import.ts
@@ -0,0 +1,123 @@
+import type { DataService } from 'mongodb-data-service';
+
+type AnalyzeOptions = {
+  filename: string,
+  abortSignal: AbortSignal,
+  progressCallback: (index: number) => void,
+};
+
+type AnalyzedCSVFieldType = {
+  // How many rows in the file matched this type.
+  count: number,
+
+  // The line in the file where this field was first detected to be of this
+  // type. This is so the field type selector can immediately present that line
+  // or document as a counter example if the user selects an incompatible field
+  // type.
+  firstRowIndex: number,
+
+  // Keep the position that the field name was in the header row so we can
+  // easily pull out the data for each row and also so we can later insert the
+  // data for each document in the same order it was in the file
+  columnIndex?: number,
+
+  // For objects only
+  children?: AnalyzedCSVFields
+
+  // For arrays only
+  length?: number
+};
+
+type AnalyzedCSVField = Record<string, AnalyzedCSVFieldType>;
+
+// Each top-level field in the file ie. each one from the CSV header row and the
+// superset of all fields in the JSON documents will make up the keys and then
+// information about those fields make up the values. This structure then
+// recurses through the children property for detected CSV objects and real JSON
+// objects. This information is used by the field type selection step to
+// preselect the narrowest possible type (ie. auto-detect the field type) and
+// also to show a warning if the user picks an incompatible type.
+type AnalyzedCSVFields = Record<string, AnalyzedCSVField>;
+
+type AnalyzeResult = {
+  type: 'json'|'csv',
+  total: number,
+  delimiter?: string, // CSV only
+  fields?: AnalyzedCSVFields, // CSV only
+};
+
+export function analyze({
+  filename,
+  abortSignal,
+  progressCallback
+}: AnalyzeOptions): Promise<AnalyzeResult> {
+  // Steps:
+  // 1. guess the type (csv or json)
+  // 2. if csv, guess the delimiter
+  // 3. stream the whole file, counting the docs. For CSV also build up the
+  //    analyzed fields object along the way (see importer spike/prototype)
+
+  // TODO
+}
+
+type ImportJSONOptions = {
+  dataService: DataService,
+  ns: string,
+  filename: string,
+  abortSignal: AbortSignal,
+  progressCallback: (index: number) => void,
+  stopOnErrors?: boolean,
+};
+
+type ImportJSONResult = Error[];
+
+export function importJSON({
+  dataService,
+  ns,
+  filename,
+  abortSignal,
+  progressCallback,
+  stopOnErrors,
+}: ImportJSONOptions): Promise<ImportJSONResult> {
+  // walk the file, build each doc, insert
+
+  // TODO: Just silencing TypeScript for now
+  console.log(dataService, ns, filename, abortSignal, progressCallback, stopOnErrors);
+  return Promise.resolve([]);
+}
+
+type ImportCSVOptions = {
+  dataService: DataService,
+  ns: string,
+  filename: string,
+  abortSignal: AbortSignal,
+  progressCallback: (index: number) => void,
+  delimiter: string,
+  ignoreEmptyStrings?: boolean,
+  stopOnErrors?: boolean,
+  // pass in the info returned from analyze because all the column indexes,
+  // children and lengths will be used again to build the docs to insert
+  analyzedFields: AnalyzedCSVFields,
+  fields: Record<string, string> // the type chosen by the user to make each field
+};
+
+type ImportCSVResult = Error[];
+
+export function importCSV({
+  dataService,
+  ns,
+  filename,
+  abortSignal,
+  progressCallback,
+  delimiter,
+  ignoreEmptyStrings,
+  stopOnErrors,
+  analyzedFields,
+  fields
+}: ImportCSVOptions): Promise<ImportCSVResult> {
+  // walk the file, build each doc, insert
+
+  // TODO: Just silencing TypeScript for now
+  console.log(dataService, ns, filename, abortSignal, progressCallback, delimiter, ignoreEmptyStrings, stopOnErrors, analyzedFields, fields);
+  return Promise.resolve([]);
+}
