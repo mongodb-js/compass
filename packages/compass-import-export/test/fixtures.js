b782da1bff85952862db29ed448ef55e321e0f26
Le Roux Bodenstein <lerouxb@gmail.com>
2023-01-19 10:49:55 +0000

    structure test fixtures, add some more from investigations

--- packages/compass-import-export/test/fixtures.js
+++ packages/compass-import-export/test/fixtures.js
@@ -1,25 +1,36 @@
 import path from 'path';
 
 const FIXTURES = {
-  GOOD_COMMAS_CSV: path.join(__dirname, 'good-commas.csv'),
-  GOOD_TABS_CSV: path.join(__dirname, 'good-tabs.csv'),
-  BAD_CSV: path.join(__dirname, 'test_bad.csv'),
-  JS_I_THINK_IS_JSON: path.join(__dirname, 'js-i-think-is-json'),
-  GOOD_JSON: path.join(__dirname, 'docs.json'),
-  LINE_DELIMITED_JSON: path.join(__dirname, 'docs.jsonl'),
+  // other
+  JS_I_THINK_IS_JSON: path.join(__dirname, 'other', 'js-i-think-is-json'),
+
+  // csv
+  GOOD_COMMAS_CSV: path.join(__dirname, 'csv', 'good-commas.csv'),
+  GOOD_TABS_CSV: path.join(__dirname, 'csv', 'good-tabs.csv'), // tsv?
+  BAD_CSV: path.join(__dirname, 'csv', 'test_bad.csv'),
+  NUMBER_TRANSFORM_CSV: path.join(__dirname, 'csv', 'number-transform.csv'),
+  CSV_FLAT_HEADERS: path.join(__dirname, 'csv', 'export-flat-headers.csv'),
+  SPARSE_CSV: path.join(__dirname, 'csv', 'sparse.csv'),
+
+  // json
+  JSON_SINGLE_DOC: path.join(__dirname, 'json', 'export-single-doc.json'),
+  JSON_MULTI_SMALL_DOCS: path.join(__dirname, 'json', 'export-multi-small-docs.json'),
+  GOOD_JSON: path.join(__dirname, 'json', 'docs.json'),
+  JSON_ARRAY: path.join(__dirname, 'json', 'docs.json'),
+  JSON_WITH_CSV_FILEEXT: path.join(__dirname, 'json', 'json-with-a.csv'),
+  COMPLEX_JSON: path.join(__dirname, 'json', 'complex.json'),
+  SINGLE_JSON: path.join(__dirname, 'json', 'single-doc.json'),
+
+  // jsonl
+  JSONL: path.join(__dirname, 'jsonl', 'export-two-docs.jsonl'),
+  NDJSON: path.join(__dirname, 'jsonl', 'docs.jsonl'),
+  NDJSON_EXTRA_LINE: path.join(__dirname, 'jsonl', 'docs-with-newline-ending.jsonl'),
+  LINE_DELIMITED_JSON: path.join(__dirname, 'jsonl', 'docs.jsonl'),
   LINE_DELIMITED_JSON_EXTRA_LINE: path.join(
     __dirname,
+    'jsonl',
     'docs-with-newline-ending.jsonl'
   ),
-  NUMBER_TRANSFORM_CSV: path.join(__dirname, 'number-transform.csv'),
-  JSON_ARRAY: path.join(__dirname, 'docs.json'),
-  NDJSON: path.join(__dirname, 'docs.jsonl'),
-  NDJSON_EXTRA_LINE: path.join(__dirname, 'docs-with-newline-ending.jsonl'),
-  JSON_WITH_CSV_FILEEXT: path.join(__dirname, 'json-with-a.csv'),
-  JSON_SINGLE_DOC: path.join(__dirname, 'export-single-doc.json'),
-  JSON_MULTI_SMALL_DOCS: path.join(__dirname, 'export-multi-small-docs.json'),
-  JSONL: path.join(__dirname, 'export-two-docs.jsonl'),
-  CSV_FLAT_HEADERS: path.join(__dirname, 'export-flat-headers.csv'),
 };
 
 export { FIXTURES };
