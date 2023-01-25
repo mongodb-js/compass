bd22d47c3daf37d30d42f03c8fe498d51c0b5474
Le Roux Bodenstein <lerouxb@gmail.com>
2023-01-25 11:15:24 +0000

    include preview rows in the listCSVFields() result

--- packages/compass-import-export/src/import/list-csv-fields.ts
+++ packages/compass-import-export/src/import/list-csv-fields.ts
@@ -6,12 +6,18 @@ import type { Delimiter } from '../utils/constants';
 
 const debug = createDebug('list-csv-fields');
 
+const NUM_PREVIEW_FIELDS = 5;
+
 type ListCSVFieldsOptions = {
   input: Readable;
   delimiter: Delimiter;
 };
 
-type ListCSVFieldsResult = string[];
+type ListCSVFieldsResult = {
+  uniqueFields: string[];
+  headerFields: string[];
+  preview: string[][];
+};
 
 export async function listCSVFields({
   input,
@@ -19,8 +25,11 @@ export async function listCSVFields({
 }: ListCSVFieldsOptions): Promise<ListCSVFieldsResult> {
   let lines = 0;
 
-  const fields: string[] = [];
-  const fieldMap: Record<string, true> = {};
+  const result: ListCSVFieldsResult = {
+    uniqueFields: [],
+    headerFields: [],
+    preview: [],
+  };
 
   return new Promise(function (resolve, reject) {
     Papa.parse(input, {
@@ -28,30 +37,38 @@ export async function listCSVFields({
       step: function (results: Papa.ParseStepResult<string[]>, parser) {
         ++lines;
         debug('listCSVFields:step', lines, results);
-        if (lines !== 1) {
+
+        if (lines === 1) {
+          result.headerFields = results.data;
+
+          // remove array indexes so that foo[0], foo[1] becomes foo
+          // and bar[0].a, bar[1].a becomes bar.a
+          // ie. the whole array counts as one field
+          const flattened = results.data.map((name) => {
+            return name.replace(/\[\d+\]/, '');
+          });
+
+          const fieldMap: Record<string, true> = {};
+
+          // make sure that each array field is only included once
+          for (const name of flattened) {
+            if (!fieldMap[name]) {
+              fieldMap[name] = true;
+              result.uniqueFields.push(name);
+            }
+          }
+
           return;
         }
 
-        // remove array indexes so that foo[0], foo[1] becomes foo
-        // and bar[0].a, bar[1].a becomes bar.a
-        // ie. the whole array counts as one field
-        const flattened = results.data.map((name) => {
-          return name.replace(/\[\d+\]/, '');
-        });
-
-        // make sure that each array field is only included once
-        for (const name of flattened) {
-          if (!fieldMap[name]) {
-            fieldMap[name] = true;
-            fields.push(name);
-          }
+        result.preview.push(results.data);
+        if (lines === NUM_PREVIEW_FIELDS + 1) {
+          parser.abort();
         }
-
-        parser.abort();
       },
       complete: function () {
         debug('listCSVFields:complete');
-        resolve(fields);
+        resolve(result);
       },
       error: function (err) {
         debug('listCSVFields:error', err);
