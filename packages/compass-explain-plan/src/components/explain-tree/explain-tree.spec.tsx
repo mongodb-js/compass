1e3f06fa80c2c5a3f6990aa27494f84b23f9105f
Maurizio Casimirri <maurizio.cas@gmail.com>
2023-01-25 23:47:26 +0100

    add darkmode to the tree links

--- packages/compass-explain-plan/src/components/explain-tree/explain-tree.spec.tsx
+++ packages/compass-explain-plan/src/components/explain-tree/explain-tree.spec.tsx
@@ -2,20 +2,21 @@ import { render, screen } from '@testing-library/react';
 import { expect } from 'chai';
 import React from 'react';
 
+import ExplainTree from '.';
 
-import ExplainTree from '../explain-tree';
-
-describe('ExplainTree [Component]', function() {
+describe('ExplainTree [Component]', function () {
   const nodes = [];
   const links = [];
   const width = 100;
   const height = 100;
 
-  beforeEach(function() {
-    render(<ExplainTree nodes={nodes} links={links} width={width} height={height} />);
+  beforeEach(function () {
+    render(
+      <ExplainTree nodes={nodes} links={links} width={width} height={height} />
+    );
   });
 
-  it('renders', function() {
+  it('renders', function () {
     expect(screen.getByTestId('explain-tree')).to.exist;
   });
 });
