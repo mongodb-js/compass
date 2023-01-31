5c8f6222dcb2d66da2b6d47cd18dea6b89b3697a
Basit <1305718+mabaasit@users.noreply.github.com>
2023-01-31 19:21:42 +0100

    Update packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
    Co-authored-by: Sergey Petushkov <petushkov.sergey@gmail.com>

--- packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
+++ packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
@@ -316,7 +316,7 @@ describe('Collection aggregations tab', function () {
 
   it('shows $merge stage preview', async function () {
     // $merge operator is supported from 4.2.0
-    if (semver.lt(MONGODB_VERSION, '4.2.0')) {
+    if (serverSatisfies('< 4.2.0')) {
       return this.skip();
     }
 
