72084ee0f00051d669edba6427251b13dc3b8096
Basit <baasit121@icloud.com>
2023-02-07 13:58:17 +0100

    remove only

--- packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
+++ packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
@@ -181,7 +181,7 @@ async function deleteStage(
   await browser.clickVisible(Selectors.StageDelete);
 }
 
-describe.only('Collection aggregations tab', function () {
+describe('Collection aggregations tab', function () {
   let compass: Compass;
   let browser: CompassBrowser;
 
