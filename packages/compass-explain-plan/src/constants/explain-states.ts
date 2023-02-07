1dbe8415e2b24017c9e6eb04b5254ceaaed0f93e
Himanshu Singh <himanshu.singhs@outlook.in>
2023-02-07 15:53:57 +0100

    feat(compass-explain-plan): adds a cancellable loader to explain (#3995)
    Co-authored-by: Sergey Petushkov <petushkov.sergey@gmail.com>

--- packages/compass-explain-plan/src/constants/explain-states.ts
+++ packages/compass-explain-plan/src/constants/explain-states.ts
@@ -2,5 +2,6 @@
 export default {
   INITIAL: 'initial', // A zero state that is being shown by default
   OUTDATED: 'outdated', // When new query was set and explain plan should be fetched again
+  REQUESTED: 'requested', // When request for a query explain is dispatched
   EXECUTED: 'executed', // When new query was executed by explain plan
 };
