dffa686d00cda7becbc1ba0338d4e5d0e18b0361
Le Roux Bodenstein <lerouxb@gmail.com>
2023-02-15 15:21:55 +0000

    feat(collections): when dropping a collection or database, redirect to either the database or databases view COMPASS-6018, COMPASS-6434  (#3972)
    * when deleting a collection, redirect to either the database or databases view
    
    * e2e test for the redirect
    
    * redirect to databases when dropping a database
    
    * active database/collection dropped only
    
    * e2e test tweaks
    
    * Update packages/compass-e2e-tests/tests/instance-sidebar.test.ts
    
    Co-authored-by: Anna Henningsen <anna.henningsen@mongodb.com>
    
    * linting
    
    * fix selector used in e2e tests
    
    * WIP test outline
    
    * e2e tests for redirects after dropping collections and databases
    
    ---------
    
    Co-authored-by: Anna Henningsen <anna.henningsen@mongodb.com>

---
+++ packages/compass-e2e-tests/helpers/commands/drop-collection-from-sidebar.ts
@@ -0,0 +1,45 @@
+import type { CompassBrowser } from '../compass-browser';
+import * as Selectors from '../selectors';
+
+export async function dropCollectionFromSidebar(
+  browser: CompassBrowser,
+  dbName: string,
+  collectionName: string
+): Promise<void> {
+  // search for the collecton in the sidebar filter
+  await browser.clickVisible(Selectors.SidebarFilterInput);
+  const sidebarFilterInputElement = await browser.$(
+    Selectors.SidebarFilterInput
+  );
+  await sidebarFilterInputElement.setValue(collectionName);
+  const dbElement = await browser.$(Selectors.sidebarDatabase(dbName));
+  await dbElement.waitForDisplayed();
+
+  // wait for the collection to become displayed
+  const collectionSelector = Selectors.sidebarCollection(
+    dbName,
+    collectionName
+  );
+  await browser.scrollToVirtualItem(
+    Selectors.SidebarDatabaseAndCollectionList,
+    collectionSelector,
+    'tree'
+  );
+  const collectionElement = await browser.$(collectionSelector);
+  await collectionElement.waitForDisplayed();
+
+  // open the drop collection modal from the sidebar
+  await browser.hover(collectionSelector);
+
+  // NOTE: if the menu was already open for another collection this could get
+  // confusing. Also this selector is just for the actions button and it is
+  // assumed that at this point it is the only one. But the drop confirmation
+  // usually catches that.
+  await browser.clickVisible(Selectors.CollectionShowActionsButton);
+  await browser.clickVisible(Selectors.DropCollectionButton);
+
+  await browser.dropCollection(collectionName);
+
+  // wait for it to be gone
+  await collectionElement.waitForExist({ reverse: true });
+}
