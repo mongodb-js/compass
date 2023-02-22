c685470b0e8803dcb8b0b99b914a826871ecc883
Maurizio Casimirri <maurizio.cas@gmail.com>
2023-02-22 22:49:52 +0100

    use different tiles for darkmode

--- packages/compass-schema/src/components/coordinates-minichart/constants.js
+++ packages/compass-schema/src/components/coordinates-minichart/constants.js
@@ -1,8 +1,9 @@
-// If no tile server is provided, use this url instead.
-const DEFAULT_TILE_URL =
-  'https://compass-maps.mongodb.com/compass/maptile/{z}/{x}/{y}/512';
+const LIGHTMODE_TILE_URL =
+  'https://compass-maps.mongodb.com/compass/maptile/reduced.day/{z}/{x}/{y}/512';
+const DARKMODE_TILE_URL =
+  'https://compass-maps.mongodb.com/compass/maptile/reduced.night/{z}/{x}/{y}/512';
 
 // The copyright url for HERE maps, if we're using the default tile url
 const COPYRIGHT_URL = 'https://compass-maps.mongodb.com/compass/copyright';
 
-export { DEFAULT_TILE_URL, COPYRIGHT_URL };
+export { LIGHTMODE_TILE_URL, DARKMODE_TILE_URL, COPYRIGHT_URL };
