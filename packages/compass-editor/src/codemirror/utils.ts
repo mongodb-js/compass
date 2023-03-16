604496fa137ab328f42bda0f2db009504a2a23aa
Basit <baasit121@icloud.com>
2023-03-16 15:16:49 +0100

    parse js tree to and suggest better completions

--- packages/compass-editor/src/codemirror/utils.ts
+++ packages/compass-editor/src/codemirror/utils.ts
@@ -5,6 +5,8 @@ import {
 } from '@codemirror/autocomplete';
 import { syntaxTree } from '@codemirror/language';
 
+export const ARRAY_ITEM_REGEX = /(\[(\d)+\])/;
+
 export const completeWordsInString = ifIn(['String'], completeAnyWord);
 
 export function resolveTokenAtCursor(context: CompletionContext) {
@@ -12,3 +14,63 @@ export function resolveTokenAtCursor(context: CompletionContext) {
 }
 
 export type Token = ReturnType<typeof resolveTokenAtCursor>;
+
+/**
+ *
+ * Returns the list of ancestors of the token.
+ */
+const getAncestorList = (node: Token | null, doc: string): string[] => {
+  let ancestors: string[] = [];
+
+  if (!node?.parent) {
+    return ancestors;
+  }
+
+  // In order to find the index of an node in an array, we have to check for the parent.
+  // And then use siblings to find the index. We can't use the node.parent as it does not
+  // contain all the childern and reference to the current node.
+  if (['ArrayExpression', 'Array'].includes(node.parent.name)) {
+    let prevSibling = node.prevSibling;
+    let index = 0;
+    while (prevSibling) {
+      if (![':', ',', '['].includes(prevSibling.name)) {
+        index++;
+      }
+      prevSibling = prevSibling.prevSibling;
+    }
+    ancestors.push(`[${index}]`);
+  }
+  ancestors.push(doc.slice(0, node.from));
+  return ancestors.concat(getAncestorList(node.parent, doc));
+};
+
+/**
+ * Filter out the invalid property names.
+ * Property names always end with a `:` and can be either unquoted or quoted (single or double).
+ * 1. Unquoted property names: [^\s\"\']*
+ * 2. Double quoted property names: \"[^"]*\"
+ * 3. Single quoted property names: \'[^\']*\'
+ * 4. String ending with a colon: (?=:$)
+ */
+const normalizeAncestorList = (parent: string[]): string[] => {
+  const propertyRegex = /([^\s\"\']*|\"[^"]*\"|\'[^']*\')(?=:$)/;
+  return parent
+    .reverse()
+    .map((x) => {
+      if (x.match(ARRAY_ITEM_REGEX)) {
+        return x;
+      }
+      return (x.trim().match(propertyRegex)?.[0] ?? '').replace(/\"/g, '');
+    })
+    .filter(Boolean) as string[];
+};
+
+/**
+ * Walks the syntax tree from the token and returns a list
+ * of all the ancestors of the token. The array indexes are
+ * represented as `[0-9]`.
+ */
+export function getAncestryOfToken(token: Token, document: string): string[] {
+  const list = getAncestorList(token, document);
+  return normalizeAncestorList(list);
+}
--- packages/compass-editor/src/codemirror/validation-autocompleter.ts
+++ packages/compass-editor/src/codemirror/validation-autocompleter.ts
@@ -2,15 +2,18 @@ import type { CompletionSource } from '@codemirror/autocomplete';
 import { snippetCompletion } from '@codemirror/autocomplete';
 import { completer, CompletionResult } from '../autocompleter';
 
-import type { Token } from './utils';
-import { resolveTokenAtCursor, completeWordsInString } from './utils';
+import {
+  resolveTokenAtCursor,
+  getAncestryOfToken,
+  ARRAY_ITEM_REGEX,
+} from './utils';
 
 const rootJsonSchemaCompletion = (from: number, to: number) => ({
   filter: false,
   from,
   to,
   options: [
-    snippetCompletion('{\n\t"$jsonSchema": {\n\t\t${}\n\t}\n}', {
+    snippetCompletion('{\n\t$jsonSchema: {\n\t\t${}\n\t}\n}', {
       label: '$jsonSchema',
       info: 'json-schema validation object',
     }),
@@ -20,7 +23,8 @@ const rootJsonSchemaCompletion = (from: number, to: number) => ({
 const createCompletions = (
   completions: CompletionResult[],
   text: string,
-  from: number
+  from: number,
+  detail?: string
 ) => {
   return {
     from,
@@ -33,32 +37,95 @@ const createCompletions = (
         ...completion,
         label: completion.value,
         info: completion.description,
-        detail: completion.meta,
+        detail: detail ?? completion.meta,
       })),
   };
 };
 
-const isCompletingStringArray = (token: Token): boolean => {
-  return token.name === 'String' && token.parent?.name === 'Array';
+const isCompletingRequired = (ancestors: string[]) => {
+  // Required is always an array.
+  return (
+    (ancestors[ancestors.length - 1] ?? '').match(ARRAY_ITEM_REGEX) &&
+    ancestors[ancestors.length - 2] === 'required'
+  );
+};
+
+const isCompletingProperties = (ancestors: string[]) => {
+  return ancestors[ancestors.length - 1] === 'properties';
+};
+
+const isCompletingBsonType = (ancestors: string[]) => {
+  // bsonType can either be a string or an array.
+  const immediateParent = ancestors[ancestors.length - 1] ?? '';
+  return (
+    immediateParent === 'bsonType' ||
+    (immediateParent.match(ARRAY_ITEM_REGEX) &&
+      ancestors[ancestors.length - 2] === 'bsonType')
+  );
+};
+
+/**
+ * The $jsonSchema's required and properties are nested, so we need to
+ * get the ancestor to get the correct field name.
+ */
+const getNestingAncestor = (ancestors: string[]): string => {
+  return (
+    ancestors
+      .filter((x) => !['$jsonSchema'].includes(x))
+      .map((x, i, a) => {
+        if (['required', 'properties'].includes(x)) {
+          return a[i - 1];
+        }
+      })
+      .filter(Boolean)
+      // because field names are represented in dot notation
+      .join('.')
+  );
+};
+
+/**
+ * As the $jsonSchema's required and properties are nested, we need to filter
+ * the fields based on the nesting level (ancestor).
+ */
+const filterFieldsByParent = (fields: CompletionResult[], ancestor: string) => {
+  if (ancestor === '') {
+    return fields;
+  }
+
+  return fields
+    .filter((x) => x.value !== ancestor && x.value.startsWith(ancestor))
+    .map((x) => {
+      const value = x.value.replace(`${ancestor}.`, '');
+      return {
+        ...x,
+        value,
+      };
+    })
+    .filter((x) => x.value);
 };
 
 export const createValidationAutocompleter = (
   fields: string[],
   serverVersion?: string
 ): CompletionSource => {
+  const queryCompletions = completer('', { serverVersion, meta: ['query'] });
+  const jsonSchemaCompletions = completer('', {
+    serverVersion,
+    meta: ['json-schema'],
+  });
   const fieldCompletions = completer('', {
     fields,
     serverVersion,
     meta: ['field:identifier'],
   });
-  const bsonCompletions = completer('', {
+  const bsonTypeCompletions = completer('', {
     serverVersion,
     meta: ['bson-type-aliases'],
   });
 
-  const jsonSchemaCompletions = completer('', {
+  const bsonCompletions = completer('', {
     serverVersion,
-    meta: ['json-schema', 'query', 'bson'],
+    meta: ['bson'],
   });
 
   return (context) => {
@@ -68,24 +135,34 @@ export const createValidationAutocompleter = (
       .slice(token.from, context.pos)
       .replace(/^("|')/, '');
 
-    if (token.type.isTop) {
+    // At the root leve
+    if (token.type.isTop || token.parent?.type.isTop) {
       return rootJsonSchemaCompletion(0, document.length);
     }
 
-    if (isCompletingStringArray(token)) {
+    const ancestors = getAncestryOfToken(token, document);
+
+    if (isCompletingRequired(ancestors) || isCompletingProperties(ancestors)) {
+      const ancestor = getNestingAncestor(ancestors);
       return createCompletions(
-        [...fieldCompletions, ...bsonCompletions],
+        filterFieldsByParent(fieldCompletions, ancestor),
         textBefore,
-        token.from + 1
+        context.pos - textBefore.length,
+        'Field'
       );
     }
 
-    if (token.name === 'String') {
-      return completeWordsInString(context);
+    if (isCompletingBsonType(ancestors)) {
+      return createCompletions(
+        bsonTypeCompletions,
+        textBefore,
+        context.pos - textBefore.length,
+        'BSON Type'
+      );
     }
 
     return createCompletions(
-      jsonSchemaCompletions,
+      [...jsonSchemaCompletions, ...bsonCompletions, ...queryCompletions],
       textBefore,
       context.pos - textBefore.length
     );
