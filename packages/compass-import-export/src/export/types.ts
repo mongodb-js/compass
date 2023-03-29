213cf19ed36db22354599beb0ac4cb3bea0e09d2
Le Roux Bodenstein <lerouxb@gmail.com>
2023-03-29 10:02:14 +0100

    factor out common export types

---
+++ packages/compass-import-export/src/export/types.ts
@@ -0,0 +1,19 @@
+import type { AggregateOptions, Document, Sort } from 'mongodb';
+
+export type ExportAggregation = {
+  stages: Document[];
+  options: AggregateOptions;
+};
+
+export type ExportQuery = {
+  filter: Document;
+  sort?: Sort;
+  limit?: number;
+  skip?: number;
+  projection?: Document;
+};
+
+export type ExportResult = {
+  docsWritten: number;
+  aborted: boolean;
+};
