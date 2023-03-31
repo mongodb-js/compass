da0d016fdae6992001ca3f3347c293c36815df43
Sergey Petushkov <petushkov.sergey@gmail.com>
2023-03-31 16:01:29 +0200

    chore(mocha-config-compass): fix logging failures in mocha-electron (#4217)
    * chore(mocha-config-compass): fix logging failures in mocha-electron
    
    * chore(app-stores, field-store): re-enable electron tests for plugins
    
    * chore(mocha-config-compass): explicitly override debug.log

---
+++ configs/mocha-config-compass/register/electron-renderer-register.js
@@ -0,0 +1,46 @@
+const isElectronRenderer = require('is-electron-renderer');
+
+// We are overriding default mocha-electron handling of console as it can throw
+// trying to log non-seializable values which should not lead to test failure
+if (isElectronRenderer) {
+  const { ipcRenderer } = require('electron');
+  const { inspect } = require('util');
+
+  const console = globalThis.console;
+
+  const ipcConsole = {
+    assert(assertion, ...args) {
+      if (!assertion) ipcConsole.log(...args);
+    },
+  };
+
+  for (const k in console) {
+    if (Object.prototype.hasOwnProperty.call(console, k) && k !== 'assert') {
+      ipcConsole[k] = (...args) => {
+        // This mirrorrs mocha-electron implementation with the only difference
+        // being that we inspect values before sending them through ipc to avoid
+        // "Object can't be cloned" errors
+        //
+        // @see {@link https://github.com/jprichardson/electron-mocha/blob/master/renderer/run.js}
+        ipcRenderer.send(
+          'console-call',
+          k,
+          args.map((arg) => {
+            return inspect(arg);
+          })
+        );
+      };
+    }
+  }
+
+  // Packages that are using `@mongodb-js/compass-logging` might write to debug
+  // which holds a reference to original console.log that we don't override.
+  // This is why we explicitly do it here
+  require('debug').log = ipcConsole.log;
+
+  Object.defineProperty(global, 'console', {
+    get() {
+      return ipcConsole;
+    },
+  });
+}
