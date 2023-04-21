b4df533ad594da22551122f3c5918ab9800477da
Le Roux Bodenstein <lerouxb@gmail.com>
2023-04-21 09:40:42 +0100

    log the full error message if it doesn't match

--- packages/compass-e2e-tests/tests/collection-import.test.ts
+++ packages/compass-e2e-tests/tests/collection-import.test.ts
@@ -651,32 +651,6 @@ describe('Collection import', function () {
     });
   });
 
-  it('displays an error for a CSV file with incompatible encoding', async function () {
-    const csvPath = path.resolve(__dirname, '..', 'fixtures', 'latin1.csv');
-
-    await browser.navigateToCollectionTab('test', 'latin1', 'Documents');
-
-    // open the import modal
-    await browser.clickVisible(Selectors.AddDataButton);
-    const insertDocumentOption = await browser.$(Selectors.ImportFileOption);
-    await insertDocumentOption.waitForDisplayed();
-    await browser.clickVisible(Selectors.ImportFileOption);
-
-    // Select the file.
-    await browser.selectFile(Selectors.ImportFileInput, csvPath);
-
-    // Wait for the modal to appear.
-    const importModal = await browser.$(Selectors.ImportModal);
-    await importModal.waitForDisplayed();
-
-    // make sure that there's an error
-    const errorElement = await browser.$(Selectors.ImportAnalyzeError);
-    await errorElement.waitForDisplayed();
-    expect(await errorElement.getText()).to.equal(
-      'The encoded data was not valid for encoding utf-8'
-    );
-  });
-
   it('displays an error if an incompatible type is chosen for a column', async function () {
     const csvPath = path.resolve(__dirname, '..', 'fixtures', 'listings.csv');
 
@@ -1032,7 +1006,12 @@ describe('Collection import', function () {
       const toastElement = await browser.$(Selectors.ImportToast);
       // Check it displays that the import was aborted.
       const toastText = await toastElement.getText();
-      expect(toastText).to.include('Import aborted.');
+      try {
+        expect(toastText).to.include('Import aborted.');
+      } catch (err) {
+        console.log(toastText);
+        throw err;
+      }
 
       // Close toast.
       await browser.clickVisible(
