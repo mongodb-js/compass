2750af852a1b686fc98a61bde5406611b710c9d7
Rhys <Anemy@users.noreply.github.com>
2023-04-25 09:24:53 -0400

    feat(compass-import-export): show an add more fields button when more fields to show COMPASS-6762 (#4288)

--- packages/compass-import-export/src/components/export-select-fields.spec.tsx
+++ packages/compass-import-export/src/components/export-select-fields.spec.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { expect } from 'chai';
+import userEvent from '@testing-library/user-event';
 
 import { UnconnectedExportSelectFields as ExportSelectFields } from './export-select-fields';
 import { getIdForSchemaPath } from '../modules/export';
+import type { FieldsToExport } from '../modules/export';
 
 const retryButtonText = 'Retry';
 const addNewFieldButtonId = 'export-add-new-field-button';
+const testAddFieldsNumber = 100;
 
 const noop = () => {
   /* noop */
@@ -110,6 +113,61 @@ describe('ExportSelectFields [Component]', function () {
 
     it('should render a nested field when its parent is not selected', function () {
       expect(screen.getByText('name.inner')).to.be.visible;
+    });
+
+    it('should not render the show more fields button when no more fields to show', function () {
+      expect(screen.queryByText(`Show ${testAddFieldsNumber} more fields`)).to
+        .not.exist;
+    });
+  });
+
+  describe('with 100+ documents', function () {
+    beforeEach(function () {
+      const countFieldsToRender = 220;
+      const fields: FieldsToExport = {};
+      for (let i = 0; i < countFieldsToRender; i++) {
+        fields[getIdForSchemaPath([`field-${i}`])] = {
+          path: [`field-${i}`],
+          selected: false,
+        };
+      }
+
+      renderExportSelectFields({
+        isLoading: false,
+        fields,
+      });
+    });
+
+    it('should render the show more fields button', function () {
+      expect(screen.getByText(`Show ${testAddFieldsNumber} more fields`)).to.be
+        .visible;
+    });
+
+    it('should allow showing more fields', function () {
+      expect(
+        screen
+          .getByTestId('export-fields-table-container')
+          .textContent?.match(/field-/g)?.length
+      ).to.equal(100);
+
+      userEvent.click(screen.getByTestId('show-more-fields-export-button'));
+      expect(
+        screen
+          .getByTestId('export-fields-table-container')
+          .textContent?.match(/field-/g)?.length
+      ).to.equal(200);
+
+      userEvent.click(screen.getByTestId('show-more-fields-export-button'));
+      expect(
+        screen
+          .getByTestId('export-fields-table-container')
+          .textContent?.match(/field-/g)?.length
+      ).to.equal(220);
+
+      expect(screen.queryByText(`Show ${testAddFieldsNumber} more fields`)).to
+        .not.exist;
+      expect(screen.queryByTestId('show-more-fields-export-button')).to.not
+        .exist;
     });
   });
 });
