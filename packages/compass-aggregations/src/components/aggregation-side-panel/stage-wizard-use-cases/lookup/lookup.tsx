f31d1d68c57db23947a41fd1089eb11ed125af14
Basit <baasit121@icloud.com>
2023-05-08 15:29:52 +0200

    lookup changes

--- packages/compass-aggregations/src/components/aggregation-side-panel/stage-wizard-use-cases/lookup/lookup.tsx
+++ packages/compass-aggregations/src/components/aggregation-side-panel/stage-wizard-use-cases/lookup/lookup.tsx
@@ -14,7 +14,7 @@ import { fetchCollectionFields } from '../../../../modules/collections-fields';
 import type { CollectionData } from '../../../../modules/collections-fields';
 import type { WizardComponentProps } from '..';
 
-const LOOKUP_TITLE = 'Join documents in';
+const LOOKUP_TITLE = 'Join documents from';
 
 type LookupFormState = {
   from: string;
@@ -144,7 +144,7 @@ export const LookupForm = ({
           })()}
           searchLoadingMessage="Fetching fields ..."
           searchErrorMessage={
-            collectionInfo?.error?.message ?? 'Count not fetch fields'
+            'Failed to fetch the fields. Type the field name manually.'
           }
           searchEmptyMessage={
             !formData.from ? 'Select a collection first.' : undefined
@@ -169,7 +169,7 @@ export const LookupForm = ({
         />
       </div>
       <div className={formGroup}>
-        <Body className={titleStyles}>in</Body>
+        <Body className={titleStyles}>as</Body>
         <div className={inputFieldStyles}>
           <TextInput
             value={formData.as}
