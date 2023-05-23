2fff62e68b30df1e07bb95089d957259314181be
Sergey Petushkov <petushkov.sergey@gmail.com>
2023-05-23 15:58:44 +0200

    chore(compass-component): add a separate component for spin loader with label

--- packages/compass-explain-plan/src/components/explain-plan-modal.tsx
+++ packages/compass-explain-plan/src/components/explain-plan-modal.tsx
@@ -4,7 +4,7 @@ import React, { useMemo } from 'react';
 import {
   Banner,
   Button,
-  CancelLoader,
+  SpinLoaderWithLabel,
   KeylineCard,
   Modal,
   ModalBody,
@@ -68,7 +68,7 @@ const editorStyles = css({
 const Loader: React.FunctionComponent = () => {
   return (
     <div className={explainPlanModalLoadingStyles}>
-      <CancelLoader progressText="Running explain"></CancelLoader>
+      <SpinLoaderWithLabel progressText="Running explain"></SpinLoaderWithLabel>
     </div>
   );
 };
