59e4093a137748ac9e63756a901f2eac1448df02
Himanshu Singh <himanshu.singhs@outlook.in>
2023-05-30 10:15:59 +0200

    fixes the vuln in stage wizard search

--- packages/compass-aggregations/src/components/aggregation-side-panel/index.tsx
+++ packages/compass-aggregations/src/components/aggregation-side-panel/index.tsx
@@ -85,7 +85,7 @@ export const AggregationSidePanel = ({
 
   const filteredUseCases = useMemo(() => {
     return STAGE_WIZARD_USE_CASES.filter(({ title, stageOperator }) => {
-      const escapedSearchText = searchText.replace('$', '');
+      const escapedSearchText = searchText.replace(/\$/g, '\\$');
       const matchRegex = new RegExp(escapedSearchText, 'gi');
       return title.match(matchRegex) || stageOperator.match(matchRegex);
     });
