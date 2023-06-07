ab26110a1e02971e4fc620c95c421d8386f17aff
Sergey Petushkov <petushkov.sergey@gmail.com>
2023-06-07 15:32:49 +0200

    chore(explain-plan): use actual buttons for zoom buttons

--- packages/compass-explain-plan/src/components/zoom-control.tsx
+++ packages/compass-explain-plan/src/components/zoom-control.tsx
@@ -6,6 +6,7 @@ import {
   useDarkMode,
   cx,
   KeylineCard,
+  Button,
   Icon,
   HorizontalRule,
 } from '@mongodb-js/compass-components';
@@ -14,9 +15,6 @@ const controlsContainerStyle = css({
   display: 'flex',
   flexDirection: 'column',
   overflow: 'hidden',
-  borderRadius: spacing[2] - 2,
-  border: `1px solid ${palette.gray.base}`,
-  userSelect: 'none',
 });
 
 const controlsDividerStyle = css({
@@ -49,6 +47,28 @@ const controlStyleDarkMode = css({
   },
 });
 
+// We don't have a good enough leafygreen alternative, so we are modifying
+// Button component styles here to match this sort of group button
+const buttonStyles = css({
+  // This box shadow effect doesn't stack well when two buttons are so close to
+  // each other
+  boxShadow: 'none !important',
+  '&:first-child': {
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    borderBottom: 'none',
+  },
+  '&:last-child': {
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+  },
+  '& > div:last-child': {
+    // No matching spacing exists to make them sqare when the icon is inside
+    paddingLeft: 6,
+    paddingRight: 6,
+  },
+});
+
 const DEFAULT_ZOOM_STEP = 0.1;
 
 const MIN_SCALE_VALUE = 0.2;
@@ -64,29 +84,26 @@ export const ZoomControl: React.FunctionComponent<{
   minValue = MIN_SCALE_VALUE,
   onZoomChange,
 }) => {
-  const isDarkMode = useDarkMode();
-  const controlStyles = cx(controlStyle, {
-    [controlStyleDarkMode]: isDarkMode,
-  });
   return (
     <div className={controlsContainerStyle}>
-      <KeylineCard
+      <Button
+        className={buttonStyles}
+        leftGlyph={<Icon glyph="Plus" />}
         onClick={() => {
           onZoomChange(Math.max(minValue, value + step));
         }}
-        className={controlStyles}
-      >
-        <Icon glyph="Plus" size="default" />
-      </KeylineCard>
-      <HorizontalRule className={controlsDividerStyle} />
-      <KeylineCard
+        size="small"
+        aria-label="Zoom in"
+      ></Button>
+      <Button
+        className={buttonStyles}
+        leftGlyph={<Icon glyph="Minus" />}
         onClick={() => {
           onZoomChange(Math.max(minValue, value - step));
         }}
-        className={controlStyles}
-      >
-        <Icon glyph="Minus" size="default" />
-      </KeylineCard>
+        size="small"
+        aria-label="Zoom out"
+      ></Button>
     </div>
   );
 };
