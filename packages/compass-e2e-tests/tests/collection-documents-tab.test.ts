c0db15a5c77e8b0ad05bfcfdfc9d65cf1ef774f4
Maurizio Casimirri <maurizio.cas@gmail.com>
2023-06-09 06:19:42 +0200

    missing assertion

--- packages/compass-e2e-tests/tests/collection-documents-tab.test.ts
+++ packages/compass-e2e-tests/tests/collection-documents-tab.test.ts
@@ -134,6 +134,7 @@ describe('Collection documents tab', function () {
       has_limit: false,
       has_projection: false,
       has_skip: false,
+      has_sort: false,
       used_regex: false,
     });
 
