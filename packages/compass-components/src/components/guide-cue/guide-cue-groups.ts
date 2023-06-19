e92692f0fa60731355929247634a49fae404b17c
Basit <1305718+mabaasit@users.noreply.github.com>
2023-06-19 10:53:24 +0200

    Update packages/compass-components/src/components/guide-cue/guide-cue-groups.ts
    Co-authored-by: Sergey Petushkov <petushkov.sergey@gmail.com>

--- packages/compass-components/src/components/guide-cue/guide-cue-groups.ts
+++ packages/compass-components/src/components/guide-cue/guide-cue-groups.ts
@@ -3,7 +3,8 @@
 // const GROUPS = [] as const; // of type {id: string, steps: number}
 const GROUPS: Array<{ id: string; steps: number }> = [];
 
-export const GROUP_STEPS_MAP = new Map<GroupName, number>();
-GROUPS.forEach(({ id, steps }) => GROUP_STEPS_MAP.set(id, steps));
+export const GROUP_STEPS_MAP = new Map<GroupName, number>(
+  GROUPS.map(({ id, steps }) => [id, steps])
+);
 
 export type GroupName = typeof GROUPS[number]['id'];
