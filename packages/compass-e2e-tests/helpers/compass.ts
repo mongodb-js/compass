1898ebbd6696772ba3fda421c2053fa4cf3e7813
Basit <1305718+mabaasit@users.noreply.github.com>
2023-06-21 11:50:09 +0200

    Update packages/compass-e2e-tests/helpers/compass.ts
    Co-authored-by: Sergey Petushkov <petushkov.sergey@gmail.com>

--- packages/compass-e2e-tests/helpers/compass.ts
+++ packages/compass-e2e-tests/helpers/compass.ts
@@ -579,7 +579,9 @@ async function startCompass(opts: StartCompassOptions = {}): Promise<Compass> {
   process.env.MONGODB_COMPASS_TEST_LOG_DIR = path.join(LOG_PATH, 'app');
   process.env.CHROME_LOG_FILE = chromedriverLogPath;
 
-  // Skip showing Guide Cues
+  // Guide cues might affect too many tests in a way where the auto showing of the cue prevents
+  // clicks from working on elements. Dealing with this case-by-case is way too much work, so 
+  // we disable the cues completely for the e2e tests
   process.env.DISABLE_GUIDE_CUES = 'true';
 
   const options = {
