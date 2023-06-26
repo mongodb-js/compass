040572024be7e270d9e5bcce80d1ae782896fe69
Basit <baasit121@icloud.com>
2023-06-26 15:03:25 +0200

    remove package

--- packages/compass-query-history/src/utils/index.js
+++
@@ -1,5 +0,0 @@
-import formatQuery from './format-query';
-import comparableQuery from './comparable-query';
-import { FavoriteQueryStorage } from './favorite-query-storage';
-
-export { formatQuery, comparableQuery, FavoriteQueryStorage };
