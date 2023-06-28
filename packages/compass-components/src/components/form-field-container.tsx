5ad4920b59d1e5fbc63df06bd1df66d3f2ff256d
Le Roux Bodenstein <lerouxb@gmail.com>
2023-06-28 10:30:55 +0100

    Make sure form fields' dropdown menus are positioned above buttons in other form fields when the window loses focus

--- packages/compass-components/src/components/form-field-container.tsx
+++ packages/compass-components/src/components/form-field-container.tsx
@@ -4,6 +4,10 @@ import React from 'react';
 
 const formFieldContainerStyles = css({
   margin: `${spacing[4]}px 0`,
+  // This is to prevent buttons from other form fields being positioned on top
+  // of a select dropdown menu in this form field when the window loses focus.
+  position: 'relative',
+  zIndex: 1,
 });
 
 function FormFieldContainer({
