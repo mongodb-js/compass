a7eb730693b63501028444f9ef6b634620af6e37
Rhys <Anemy@users.noreply.github.com>
2023-07-03 11:39:00 -0400

    chore(compass-query-bar): update ai button styles, cancel on unmount, fix thunk typing (#4610)

--- packages/compass-query-bar/src/stores/query-bar-reducer.ts
+++ packages/compass-query-bar/src/stores/query-bar-reducer.ts
@@ -1,4 +1,4 @@
-import type { Reducer, AnyAction } from 'redux';
+import type { Reducer } from 'redux';
 import { cloneDeep, isEqual } from 'lodash';
 import _ from 'lodash';
 import { UUID } from 'bson';
@@ -30,17 +30,11 @@ import {
   type RecentQuery,
   type FavoriteQuery,
   getQueryAttributes,
+  isAction,
 } from '../utils';
 const { debug } = createLoggerAndTelemetry('COMPASS-QUERY-BAR-UI');
 
 const TOTAL_RECENTS_ALLOWED = 30;
-
-function isAction<A extends AnyAction>(
-  action: AnyAction,
-  type: A['type']
-): action is A {
-  return action.type === type;
-}
 
 type QueryBarState = {
   fields: QueryFormFields;
