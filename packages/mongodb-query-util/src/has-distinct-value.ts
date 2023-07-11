974098cb736420aaa1227409c870007c15f7834a
Maurizio Casimirri <maurizio.cas@gmail.com>
2023-07-11 10:35:27 +0200

    remove unused method

--- packages/mongodb-query-util/src/has-distinct-value.ts
+++ packages/mongodb-query-util/src/has-distinct-value.ts
@@ -2,27 +2,6 @@ import { isPlainObject, some, has, isEqualWith } from 'lodash';
 import { bsonEqual } from './bson-equal';
 
 /**
- * returns an array of all distinct values in a field (equality or $in)
- *
- * @param  {Any}  field   the right-hand side of a document field
- * @return {Boolean}      array of values for this field
- */
-export const getDistinctValues = (field: any | undefined) => {
-  // field not present, return empty array
-  if (field === undefined) {
-    return [];
-  }
-  // field is object, could be a $in clause or a primitive value
-  if (isPlainObject(field)) {
-    if (has(field, '$in')) {
-      return field.$in;
-    }
-  }
-  // it is not a $in operator, return single value as array
-  return [field];
-};
-
-/**
  * Determines if a field in the query has a distinct value (equality or $in).
  *
  * @param  {Any}  field   the right-hand side of a document field
