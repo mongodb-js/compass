866995f77b21b3f69265eef26c283e5628ff82d1
Basit <1305718+mabaasit@users.noreply.github.com>
2023-08-31 13:12:52 +0200

    Update packages/compass-query-bar/src/utils/query-storage.ts
    Co-authored-by: Anna Henningsen <anna.henningsen@mongodb.com>

--- packages/compass-query-bar/src/utils/query-storage.ts
+++ packages/compass-query-bar/src/utils/query-storage.ts
@@ -31,7 +31,7 @@ const FavoriteQuerySchema = RecentQuerySchema.and(
     _dateModified: z
       .union([z.coerce.date(), z.number()])
       .optional()
-      .transform((x) => (x ? new Date(x) : x)),
+      .transform((x) => (x !== undefined ? new Date(x) : x)),
     _dateSaved: z
       .union([z.coerce.date(), z.number()])
       .transform((x) => new Date(x)),
