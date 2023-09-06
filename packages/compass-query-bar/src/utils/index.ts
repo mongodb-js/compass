b53158d1a4cfaa42b802a8e10059a81778b5ea8b
Sergey Petushkov <petushkov.sergey@gmail.com>
2023-09-06 17:27:27 +0200

    fix(query-bar): account for changed key order in query COMPASS-7194 (#4808)
    * fix(query-bar): account for changed key order in query
    
    * chore(query-bar): compare serialised buffers instead of objects

--- packages/compass-query-bar/src/utils/index.ts
+++ packages/compass-query-bar/src/utils/index.ts
@@ -1,3 +1,6 @@
+import type { Document } from 'bson';
+import { BSON } from 'bson';
+import { isEqual } from 'lodash';
 import type { AnyAction } from 'redux';
 
 export { copyToClipboard } from './copy-to-clipboard';
@@ -10,4 +13,11 @@ export function isAction<A extends AnyAction>(
   type: A['type']
 ): action is A {
   return action.type === type;
+}
+
+/**
+ * Same as _.isEqual, except it takes key order into account
+ */
+export function isQueryEqual(value: Document, other: Document): boolean {
+  return isEqual(BSON.serialize(value), BSON.serialize(other));
 }
