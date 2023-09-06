edc57c2ce3b4451989961793d3ea3c8320e9b7c3
Sergey Petushkov <petushkov.sergey@gmail.com>
2023-09-06 09:01:28 +0200

    chore(atlas-service): allow to override account portal url for dev environment testing (#4800)
    * chore(atlas-service): allow to override account portal url for dev environment testing
    
    * chore(atlas-service): make base url just url
    
    Co-authored-by: Anna Henningsen <anna@addaleax.net>
    
    ---------
    
    Co-authored-by: Anna Henningsen <anna@addaleax.net>

--- packages/atlas-service/src/main.ts
+++ packages/atlas-service/src/main.ts
@@ -169,6 +169,16 @@ export class AtlasService {
     return apiBaseUrl;
   }
 
+  private static get authPortalUrl() {
+    const authPortalUrl =
+      process.env.COMPASS_ATLAS_AUTH_PORTAL_URL_OVERRIDE ||
+      process.env.COMPASS_ATLAS_AUTH_PORTAL_URL;
+    if (!authPortalUrl) {
+      throw new Error('COMPASS_ATLAS_AUTH_PORTAL_URL is required');
+    }
+    return authPortalUrl;
+  }
+
   private static openExternal(...args: Parameters<typeof shell.openExternal>) {
     return shell?.openExternal(...args);
   }
@@ -186,13 +196,11 @@ export class AtlasService {
 
   private static setupPlugin(serializedState?: string) {
     this.plugin = this.createMongoDBOIDCPlugin({
-      redirectServerRequestHandler(data) {
+      redirectServerRequestHandler: (data) => {
         if (data.result === 'redirecting') {
           const { res, status, location } = data;
           res.statusCode = status;
-          const redirectUrl = new URL(
-            'https://account.mongodb.com/account/login'
-          );
+          const redirectUrl = new URL(this.authPortalUrl);
           redirectUrl.searchParams.set('fromURI', location);
           res.setHeader('Location', redirectUrl.toString());
           res.end();
