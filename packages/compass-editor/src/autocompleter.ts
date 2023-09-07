1e0cd95230fd8dd9098b6d6c19e015be21202632
Sergey Petushkov <petushkov.sergey@gmail.com>
2023-09-07 17:53:15 +0200

    chore(compass-editor): update mongodb-constants; use types from package

--- packages/compass-editor/src/autocompleter.ts
+++ packages/compass-editor/src/autocompleter.ts
@@ -1,22 +1,18 @@
+import type {
+  CompletionFilterOptions,
+  Completion,
+} from '@mongodb-js/mongodb-constants';
 import { getFilteredCompletions } from '@mongodb-js/mongodb-constants';
 
 export { wrapField } from '@mongodb-js/mongodb-constants';
 
-// TODO: add type exports from the package
-export type CompletionResult = ReturnType<
-  typeof getFilteredCompletions
->[number];
-export type CompletionOptions = NonNullable<
-  Parameters<typeof getFilteredCompletions>[0]
->;
-export type Completions = NonNullable<
-  Parameters<typeof getFilteredCompletions>[1]
->;
+export type CompletionResult = Completion;
+export type CompletionOptions = CompletionFilterOptions;
 
 export function completer(
   prefix = '',
   options?: CompletionOptions,
-  completions?: Completions
+  completions?: Completion[]
 ): CompletionResult[] {
   const res = getFilteredCompletions(options, completions);
   return res.filter(({ value }) => {
