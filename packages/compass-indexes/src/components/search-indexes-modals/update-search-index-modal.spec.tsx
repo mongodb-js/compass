b7686649aa08eee6f60799d49b19627761306bd9
Basit <1305718+mabaasit@users.noreply.github.com>
2023-09-21 11:28:49 +0200

    fix(search-indexes): clean up the modal state and do not update on no change (#4887)

--- packages/compass-indexes/src/components/search-indexes-modals/update-search-index-modal.spec.tsx
+++ packages/compass-indexes/src/components/search-indexes-modals/update-search-index-modal.spec.tsx
@@ -6,7 +6,10 @@ import { Provider } from 'react-redux';
 import { render, screen, cleanup } from '@testing-library/react';
 
 import React from 'react';
-import { getCodemirrorEditorValue } from '@mongodb-js/compass-editor';
+import {
+  getCodemirrorEditorValue,
+  setCodemirrorEditorValue,
+} from '@mongodb-js/compass-editor';
 import {
   refreshSearchIndexes as fetchSearchIndexes,
   showUpdateModal,
@@ -35,42 +38,45 @@ const renderModal = async (
   return store;
 };
 
+const UPDATE_INDEX = searchIndexes[0];
+
 describe('Update Search Index Modal', function () {
   afterEach(cleanup);
 
   describe('default behaviour', function () {
     it('renders the modal title', async function () {
-      await renderModal('cars_index');
-      expect(screen.getByText('Edit "cars_index" index')).to.exist;
+      await renderModal(UPDATE_INDEX.name);
+      expect(screen.getByText(`Edit "${UPDATE_INDEX.name}" index`)).to.exist;
     });
 
     it('does not show the input that changes the name of the index', async function () {
-      await renderModal('default');
+      await renderModal(UPDATE_INDEX.name);
       expect(() => screen.getByTestId('name-of-search-index')).to.throw;
     });
 
     it('uses the provided index definition', async function () {
-      await renderModal('default');
+      await renderModal(UPDATE_INDEX.name);
       const defaultIndexDef = getCodemirrorEditorValue(
         'definition-of-search-index'
       );
-      expect(JSON.parse(defaultIndexDef)).to.deep.equal({
-        mappings: {
-          dynamic: false,
-        },
-      });
+      expect(JSON.parse(defaultIndexDef)).to.deep.equal(
+        UPDATE_INDEX.latestDefinition
+      );
     });
   });
 
   describe('form validation', function () {
     it('shows server errors', async function () {
       const store = await renderModal(
-        'default',
+        UPDATE_INDEX.name,
         sinon.spy(() => {
           throw new Error('InvalidIndexSpecificationOption');
         })
       );
-
+      await setCodemirrorEditorValue(
+        'definition-of-search-index',
+        `{something: "else"}`
+      );
       screen.getByTestId('search-index-submit-button').click();
 
       expect(store.getState().searchIndexes.updateIndex).to.have.property(
@@ -85,7 +91,7 @@ describe('Update Search Index Modal', function () {
   describe('form behaviour', function () {
     it('closes the modal on cancel', async function () {
       const store = await renderModal(
-        'default',
+        UPDATE_INDEX.name,
         sinon.spy(() => {
           throw new Error('InvalidIndexSpecificationOption');
         })
@@ -99,15 +105,17 @@ describe('Update Search Index Modal', function () {
       const updateSearchIndexSpy = sinon.spy(() => {
         throw new Error('InvalidIndexSpecificationOption');
       });
-      await renderModal('default', updateSearchIndexSpy);
+      await renderModal(UPDATE_INDEX.name, updateSearchIndexSpy);
+      await setCodemirrorEditorValue(
+        'definition-of-search-index',
+        `{something: "else"}`
+      );
       screen.getByTestId('search-index-submit-button').click();
       expect(updateSearchIndexSpy).to.have.been.calledOnceWithExactly(
         'test.test',
-        'default',
+        UPDATE_INDEX.name,
         {
-          mappings: {
-            dynamic: false,
-          },
+          something: 'else',
         }
       );
     });
