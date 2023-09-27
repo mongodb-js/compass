bc6750513ab57403a9fc698e71b58dda43479259
Alena Khineika <alena.khineika@gmail.com>
2023-09-27 16:18:28 +0200

    feat: show a confirmation toast when feedback is submitted COMPASS-7251

--- packages/compass-query-bar/src/components/query-ai.tsx
+++ packages/compass-query-bar/src/components/query-ai.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GenerativeAIInput } from '@mongodb-js/compass-components';
+import { GenerativeAIInput, openToast } from '@mongodb-js/compass-components';
 import { connect } from 'react-redux';
 import createLoggerAndTelemetry from '@mongodb-js/compass-logging';
 import { usePreference } from 'compass-preferences-model';
@@ -23,6 +23,12 @@ const onSubmitFeedback = (feedback: 'positive' | 'negative', text: string) => {
     feedback,
     text,
   }));
+
+  openToast('query-ai-feedback-submitted', {
+    variant: 'success',
+    title: 'Your feedback has been submitted.',
+    timeout: 10_000,
+  });
 };
 
 type QueryAIProps = Omit<
