a6ea785456911a1d9f1f660ad8687f8ba1f283d2
Le Roux Bodenstein <lerouxb@gmail.com>
2023-10-09 16:33:08 +0100

    Clear values if 'With Field Values' was clicked

--- packages/compass-crud/src/components/insert-json-document.tsx
+++ packages/compass-crud/src/components/insert-json-document.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-import { css, cx, palette, withDarkMode } from '@mongodb-js/compass-components';
+import {
+  Checkbox,
+  css,
+  cx,
+  palette,
+  spacing,
+  withDarkMode,
+} from '@mongodb-js/compass-components';
 import { CodemirrorMultilineEditor } from '@mongodb-js/compass-editor';
 
 const editorContainerStylesLight = css({
@@ -8,6 +15,10 @@ const editorContainerStylesLight = css({
 
 const editorContainerStylesDark = css({
   borderLeft: `3px solid ${palette.gray.dark2}`,
+});
+
+const checkboxStyles = css({
+  marginTop: spacing[3],
 });
 
 /**
@@ -23,6 +34,22 @@ type InsertJsonDocumentProps = {
   updateJsonDoc: (value: string) => void;
 };
 
+function blankLeafValues(jsonDoc: string) {
+  return (
+    jsonDoc
+      // strings
+      .replace(/: "[^"]+"/g, ': ""')
+      // numbers
+      .replace(/: -?\d+(\.\d+)?/g, ': ')
+      // booleans
+      .replace(/: (true|false)/g, ': ')
+  );
+}
+
+function getInitialText(jsonDoc: string, isCommentNeeded: boolean) {
+  return isCommentNeeded ? `${EDITOR_COMMENT}${jsonDoc}` : jsonDoc;
+}
+
 const InsertJsonDocument: React.FunctionComponent<InsertJsonDocumentProps> = ({
   darkMode,
   jsonDoc,
@@ -30,27 +57,63 @@ const InsertJsonDocument: React.FunctionComponent<InsertJsonDocumentProps> = ({
   updateJsonDoc,
 }) => {
   const [text, setText] = useState(() => {
-    return isCommentNeeded ? `${EDITOR_COMMENT}${jsonDoc}` : jsonDoc;
+    return getInitialText(jsonDoc, isCommentNeeded);
   });
 
+  const [isBlank, setIsBlank] = useState(false);
+  const [blankText, setBlankText] = useState(() => {
+    return blankLeafValues(getInitialText(jsonDoc, isCommentNeeded));
+  });
+  const [isChanged, setIsChanged] = useState(false);
+
   const onChangeText = (value: string) => {
-    setText(value);
     updateJsonDoc(value.split('*/\n').pop() ?? '');
+
+    // Don't disable the With Field Values checkbox unless the user changed the
+    // text.
+    if (value === blankText || value === text) {
+      return;
+    }
+
+    setIsChanged(true);
+    setText(value);
   };
 
+  const onChangeBlank = () => {
+    if (isBlank) {
+      setIsBlank(false);
+    } else {
+      setBlankText(blankLeafValues(text));
+      setIsBlank(true);
+    }
+  };
+
+  const isDisabled = isChanged;
+
   return (
-    <div
-      className={cx(
-        darkMode ? editorContainerStylesDark : editorContainerStylesLight
-      )}
-    >
-      <CodemirrorMultilineEditor
-        data-testid="insert-document-json-editor"
-        language="json"
-        text={text}
-        onChangeText={onChangeText}
-        initialJSONFoldAll={false}
-        minLines={18}
+    <div>
+      <div
+        className={cx(
+          darkMode ? editorContainerStylesDark : editorContainerStylesLight
+        )}
+      >
+        <CodemirrorMultilineEditor
+          data-testid="insert-document-json-editor"
+          language="json"
+          text={isBlank ? blankText : text}
+          onChangeText={onChangeText}
+          initialJSONFoldAll={false}
+          minLines={18}
+        />
+      </div>
+      <Checkbox
+        className={checkboxStyles}
+        data-testid="with-field-values"
+        onChange={onChangeBlank}
+        label="With Field Values"
+        checked={!isBlank || isDisabled}
+        disabled={isDisabled}
+        bold={false}
       />
     </div>
   );
