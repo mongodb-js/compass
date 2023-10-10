32a77bccfe29c42b60c000a8013aa9b0c33167be
Basit <baasit121@icloud.com>
2023-10-10 16:41:49 +0200

    mocha timeout

--- packages/compass-e2e-tests/tests/search-indexes.test.ts
+++ packages/compass-e2e-tests/tests/search-indexes.test.ts
@@ -43,7 +43,8 @@ const INDEX_DEFINITION = JSON.stringify({
 
 // The current timeout (2mins) is not enough for the search indexes to be created
 // and be queryable on Atlas. So we are increasing the timeout to 4mins.
-// This can not be more than 4mins as the mocha timeout is 4mins.
+// This can not be more than mocha timeout.
+
 const WAIT_TIMEOUT = 240_000;
 
 function getRandomNumber() {
@@ -234,6 +235,8 @@ describe('Search Indexes', function () {
 
   for (const { name, connectionString } of connectionsWithSearchSupport) {
     context(`supports search indexes in ${name}`, function () {
+      // Set the mocha timeout to 6mins to accomodate the 4mins wait timeout
+      this.timeout(360_000);
       before(function () {
         if (!connectionString) {
           return this.skip();
