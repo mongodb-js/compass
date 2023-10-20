a7a83ffa6af263d67becd22a6240fba3e8d2c5bc
Kevin Mas Ruiz <kevin.mas@hey.com>
2023-10-20 14:28:17 +0200

    chore: duplicated log id

--- packages/compass-crud/src/stores/crud-store.ts
+++ packages/compass-crud/src/stores/crud-store.ts
@@ -1528,7 +1528,7 @@ class CrudStoreImpl
       );
     } catch (error) {
       log.error(
-        mongoLogId(1_001_000_074),
+        mongoLogId(1_001_000_267),
         'Documents',
         'Failed to refresh documents',
         error
