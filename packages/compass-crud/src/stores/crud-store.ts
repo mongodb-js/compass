b296a117dc116b4da02aa24ed9e87e554e48af59
Kevin Mas Ruiz <kevin.mas@hey.com>
2023-10-20 13:36:59 +0200

    feat: delete bulk operation modal COMPASS-7326 (#5005)
    * chore: e2e flow
    
    * chore: add bulk state in a new substate
    
    * chore: add todo markers
    
    * chore: styling
    
    * chore: more styling 💅
    
    * chore: linting
    
    * chore: add dependency to query-parser
    
    * chore: more styling
    
    * chore: fix height of preview
    
    * chore: linting
    
    * chore: fix package-lock.json
    
    * chore: fix package-lock
    
    * chore: fix tests
    
    * chore: test modal
    
    * chore: oops i did it again, remove the .only
    
    * chore: remove unused imports
    
    * chore: test bulk dialog
    
    * chore: last minute design changes
    
    * chore: add ts-ignore and clean up

--- packages/compass-crud/src/stores/crud-store.ts
+++ packages/compass-crud/src/stores/crud-store.ts
@@ -39,6 +39,7 @@ import configureGridStore from './grid-store';
 import type { TypeCastMap } from 'hadron-type-checker';
 import type AppRegistry from 'hadron-app-registry';
 import { BaseRefluxStore } from './base-reflux-store';
+import { showConfirmation } from '@mongodb-js/compass-components';
 export type BSONObject = TypeCastMap['Object'];
 export type BSONArray = TypeCastMap['Array'];
 type Mutable<T> = { -readonly [P in keyof T]: T[P] };
@@ -57,6 +58,9 @@ export type CrudActions = {
   replaceDocument(doc: Document): void;
   openInsertDocumentDialog(doc: BSONObject, cloned: boolean): void;
   copyToClipboard(doc: Document): void; //XXX
+  openBulkDeleteDialog(): void;
+  closeBulkDeleteDialog(): void;
+  runBulkDelete(): void;
 };
 
 export type DocumentView = 'List' | 'JSON' | 'Table';
@@ -366,6 +370,12 @@ export type QueryState = {
   collation: null | BSONObject;
 };
 
+export type BulkDeleteState = {
+  previews: Document[];
+  status: 'open' | 'closed' | 'in-progress';
+  affected?: number;
+};
+
 type CrudState = {
   ns: string;
   collection: string;
@@ -396,6 +406,7 @@ type CrudState = {
   fields: string[];
   isCollectionScan?: boolean;
   isSearchIndexesSupported: boolean;
+  bulkDelete: BulkDeleteState;
 };
 
 class CrudStoreImpl
@@ -455,6 +466,11 @@ class CrudStoreImpl
       fields: [],
       isCollectionScan: false,
       isSearchIndexesSupported: false,
+      bulkDelete: {
+        previews: [],
+        status: 'closed',
+        affected: 0,
+      },
     };
   }
 
@@ -1583,6 +1599,71 @@ class CrudStoreImpl
 
   openCreateSearchIndexModal() {
     this.localAppRegistry.emit('open-create-search-index-modal');
+  }
+
+  openBulkDeleteDialog() {
+    const PREVIEW_DOCS = 5;
+
+    this.setState({
+      bulkDelete: {
+        previews: this.state.docs?.slice(0, PREVIEW_DOCS) || [],
+        status: 'open',
+        affected: this.state.count || 0,
+      },
+    });
+  }
+
+  bulkDeleteInProgress() {
+    this.setState({
+      bulkDelete: {
+        ...this.state.bulkDelete,
+        status: 'in-progress',
+      },
+    }); // TODO: COMPASS-7328
+  }
+
+  bulkDeleteFailed(ex: Error) {
+    return ex; // TODO: COMPASS-7328
+  }
+
+  bulkDeleteSuccess() {
+    // TODO: COMPASS-7328
+  }
+
+  closeBulkDeleteDialog() {
+    this.setState({
+      bulkDelete: {
+        ...this.state.bulkDelete,
+        status: 'closed',
+      },
+    });
+  }
+
+  async runBulkDelete() {
+    const { affected } = this.state.bulkDelete;
+    this.closeBulkDeleteDialog();
+
+    const confirmation = await showConfirmation({
+      title: 'Are you absolutely sure?',
+      buttonText: 'Delete',
+      description: `This action can not be undone. This will permanently delete ${
+        affected || 0
+      } documents.`,
+      variant: 'danger',
+    });
+
+    if (confirmation) {
+      this.bulkDeleteInProgress();
+      try {
+        await this.dataService.deleteMany(
+          this.state.ns,
+          this.state.query.filter
+        );
+        this.bulkDeleteSuccess();
+      } catch (ex) {
+        this.bulkDeleteFailed(ex as Error);
+      }
+    }
   }
 }
 
