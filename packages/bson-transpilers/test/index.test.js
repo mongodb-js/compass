fd2e54357ea37cad2246113da20ae32f0ff01ef7
Kevin Mas Ruiz <kevin.mas@hey.com>
2023-11-02 11:05:05 +0100

    chore: test exportMode exclusion in compilation

---
+++ packages/bson-transpilers/test/index.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const transpilers = require('../index');
+
+const SAMPLE = transpilers.javascript.java;
+
+const VALID_OPTIONS = {
+    uri: 'mongodb://localhost',
+    database: 'test',
+    collection: 'webscale'
+};
+
+const INVALID_JS = '{ ... }';
+const VALID_JS = '({ a : 1 })';
+
+describe('bson transpiler', function () {
+    describe('#compileWithDriver', function () {
+        it('does not compile internal options like "options"', function () {
+            const result = SAMPLE.compileWithDriver({ options: VALID_OPTIONS, filter: VALID_JS});
+            assert.ok(result.includes('webscale'));
+        });
+
+        it('does not compile internal options like "exportMode"', function () {
+            const result = SAMPLE.compileWithDriver({ options: VALID_OPTIONS, exportMode: INVALID_JS, filter: VALID_JS});
+            assert.ok(result.includes('webscale'));
+        });
+    });
+})
