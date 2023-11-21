ce1ef9f7b69beb6bb21106881d2fbe34e2ac7a65
Anna Henningsen <anna.henningsen@mongodb.com>
2023-11-21 15:03:50 +0100

    chore(compass-database): convert to new plugin interface COMPASS-7401 (#5138)

--- packages/compass-home/src/components/workspace-content.tsx
+++ packages/compass-home/src/components/workspace-content.tsx
@@ -7,6 +7,10 @@ import {
 import InstanceWorkspacePlugin, {
   InstanceTabsProvider,
 } from '@mongodb-js/compass-instance';
+import {
+  CompassDatabasePlugin,
+  DatabaseTabsProvider,
+} from '@mongodb-js/compass-database';
 import CompassSavedAggregationsQueriesPlugin from '@mongodb-js/compass-saved-aggregations-queries';
 import { InstanceTab as DatabasesPlugin } from '@mongodb-js/compass-databases-collections';
 import type Namespace from '../types/namespace';
@@ -17,18 +21,21 @@ const WorkspaceContent: React.FunctionComponent<{ namespace: Namespace }> = ({
   namespace,
 }) => {
   const instanceTabs = useAppRegistryRole('Instance.Tab');
+  const databaseTabs = useAppRegistryRole('Database.Tab');
 
   const Collection =
     useAppRegistryComponent('Collection.Workspace') ?? EmptyComponent;
-  const Database =
-    useAppRegistryComponent('Database.Workspace') ?? EmptyComponent;
 
   if (namespace.collection) {
     return <Collection></Collection>;
   }
 
   if (namespace.database) {
-    return <Database></Database>;
+    return (
+      <DatabaseTabsProvider tabs={databaseTabs ?? []}>
+        <CompassDatabasePlugin />
+      </DatabaseTabsProvider>
+    );
   }
 
   return (
