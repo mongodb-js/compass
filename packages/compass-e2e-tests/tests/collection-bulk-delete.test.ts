4559f44ad85a3e383238c74a848afb893c259a98
Kevin Mas Ruiz <kevin.mas@hey.com>
2023-11-23 10:21:01 +0100

    chore: add e2e tests to delete modal (#5153)
    * chore: add e2e tests to delete modal
    
    * chore: remove unnecessary todos
    
    * chore: check modal visibility before confirmation

---
+++ packages/compass-e2e-tests/tests/collection-bulk-delete.test.ts
@@ -0,0 +1,112 @@
+import { expect } from 'chai';
+import type { CompassBrowser } from '../helpers/compass-browser';
+import { startTelemetryServer } from '../helpers/telemetry';
+import type { Telemetry } from '../helpers/telemetry';
+import { beforeTests, afterTests, afterTest } from '../helpers/compass';
+import type { Compass } from '../helpers/compass';
+import * as Selectors from '../helpers/selectors';
+import { createNumbersCollection } from '../helpers/insert-data';
+
+describe('Bulk Delete', () => {
+  let compass: Compass;
+  let browser: CompassBrowser;
+  let telemetry: Telemetry;
+
+  before(async function () {
+    telemetry = await startTelemetryServer();
+    compass = await beforeTests({
+      extraSpawnArgs: ['--enableBulkDeleteOperations'],
+    });
+    browser = compass.browser;
+  });
+
+  after(async function () {
+    await afterTests(compass, this.currentTest);
+    await telemetry.stop();
+  });
+
+  beforeEach(async function () {
+    await createNumbersCollection();
+    await browser.connectWithConnectionString();
+    await browser.navigateToCollectionTab('test', 'numbers', 'Documents');
+  });
+
+  afterEach(async function () {
+    await afterTest(compass, this.currentTest);
+  });
+
+  it('deletes documents matching a filter', async function () {
+    //const telemetryEntry = await browser.listenForTelemetryEvents(telemetry);
+
+    // Set a query that we'll use.
+    await browser.runFindOperation('Documents', '{ i: 5 }');
+
+    // Open the modal.
+    await browser.clickVisible(Selectors.OpenBulkDeleteButton);
+    await browser.$(Selectors.BulkDeleteModal).waitForDisplayed();
+
+    // Check that it will update the expected number of documents
+    expect(await browser.$(Selectors.BulkDeleteModalTitle).getText()).to.equal(
+      'Delete 1 document'
+    );
+
+    expect(
+      await browser.$(Selectors.BulkDeleteModalDeleteButton).getText()
+    ).to.equal('Delete 1 document');
+
+    // Press delete
+    await browser.clickVisible(Selectors.BulkDeleteModalDeleteButton);
+
+    // The modal should go away
+    await browser
+      .$(Selectors.BulkDeleteModal)
+      .waitForDisplayed({ reverse: true });
+
+    // Press delete in the confirmation modal
+    await browser.clickVisible(Selectors.ConfirmationModalConfirmButton());
+    await browser.runFindOperation('Documents', '{ i: 5 }');
+
+    // the document is deleted
+    expect(
+      await browser.$(Selectors.DocumentListActionBarMessage).getText()
+    ).to.equal('0 – 0 of 0');
+  });
+
+  it('does not delete documents when cancelled', async function () {
+    //const telemetryEntry = await browser.listenForTelemetryEvents(telemetry);
+
+    // Set a query that we'll use.
+    await browser.runFindOperation('Documents', '{ i: 5 }');
+
+    // Open the modal.
+    await browser.clickVisible(Selectors.OpenBulkDeleteButton);
+    await browser.$(Selectors.BulkDeleteModal).waitForDisplayed();
+
+    // Check that it will update the expected number of documents
+    expect(await browser.$(Selectors.BulkDeleteModalTitle).getText()).to.equal(
+      'Delete 1 document'
+    );
+
+    expect(
+      await browser.$(Selectors.BulkDeleteModalDeleteButton).getText()
+    ).to.equal('Delete 1 document');
+
+    // Press delete
+    await browser.clickVisible(Selectors.BulkDeleteModalDeleteButton);
+
+    // The modal should go away
+    await browser
+      .$(Selectors.BulkDeleteModal)
+      .waitForDisplayed({ reverse: true });
+
+    // Press cancel in the confirmation modal
+    await browser.clickVisible(Selectors.ConfirmationModalCancelButton());
+
+    await browser.runFindOperation('Documents', '{ i: 5 }');
+
+    // the document is not deleted
+    expect(
+      await browser.$(Selectors.DocumentListActionBarMessage).getText()
+    ).to.equal('1 – 1 of 1');
+  });
+});
