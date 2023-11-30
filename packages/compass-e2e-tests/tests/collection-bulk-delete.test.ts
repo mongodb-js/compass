6d39ac82784f5e25dd18edfef8733fa6467d27ca
Le Roux Bodenstein <lerouxb@gmail.com>
2023-11-30 14:31:16 +0000

    telemetry for bulk operations

--- packages/compass-e2e-tests/tests/collection-bulk-delete.test.ts
+++ packages/compass-e2e-tests/tests/collection-bulk-delete.test.ts
@@ -7,7 +7,7 @@ import type { Compass } from '../helpers/compass';
 import * as Selectors from '../helpers/selectors';
 import { createNumbersCollection } from '../helpers/insert-data';
 
-describe('Bulk Delete', () => {
+describe.only('Bulk Delete', () => {
   let compass: Compass;
   let browser: CompassBrowser;
   let telemetry: Telemetry;
@@ -36,7 +36,7 @@ describe('Bulk Delete', () => {
   });
 
   it('deletes documents matching a filter', async function () {
-    //const telemetryEntry = await browser.listenForTelemetryEvents(telemetry);
+    const telemetryEntry = await browser.listenForTelemetryEvents(telemetry);
 
     // Set a query that we'll use.
     await browser.runFindOperation('Documents', '{ i: 5 }');
@@ -44,6 +44,10 @@ describe('Bulk Delete', () => {
     // Open the modal.
     await browser.clickVisible(Selectors.OpenBulkDeleteButton);
     await browser.$(Selectors.BulkDeleteModal).waitForDisplayed();
+
+    // Check the telemetry
+    const openedEvent = await telemetryEntry('Bulk Delete Opened');
+    expect(openedEvent).to.deep.equal({});
 
     // Make sure the query is shown in the modal.
     expect(
@@ -71,6 +75,10 @@ describe('Bulk Delete', () => {
     await browser.clickVisible(Selectors.ConfirmationModalConfirmButton());
     await browser.runFindOperation('Documents', '{ i: 5 }');
 
+    // Check the telemetry
+    const executedEvent = await telemetryEntry('Bulk Delete Executed');
+    expect(executedEvent).to.deep.equal({});
+
     // The success toast is displayed
     await browser.$(Selectors.BulkDeleteSuccessToast).waitForDisplayed();
 
@@ -93,8 +101,6 @@ describe('Bulk Delete', () => {
   });
 
   it('does not delete documents when cancelled', async function () {
-    //const telemetryEntry = await browser.listenForTelemetryEvents(telemetry);
-
     // Set a query that we'll use.
     await browser.runFindOperation('Documents', '{ i: 5 }');
 
@@ -128,5 +134,49 @@ describe('Bulk Delete', () => {
     expect(
       await browser.$(Selectors.DocumentListActionBarMessage).getText()
     ).to.equal('1 – 1 of 1');
+  });
+
+  it('can export a delete query', async function () {
+    const telemetryEntry = await browser.listenForTelemetryEvents(telemetry);
+
+    // Set a query that we'll use.
+    await browser.runFindOperation('Documents', '{ i: 5 }');
+
+    // Open the modal.
+    await browser.clickVisible(Selectors.OpenBulkDeleteButton);
+    await browser.$(Selectors.BulkDeleteModal).waitForDisplayed();
+
+    // Click the export button
+    await browser.clickVisible(Selectors.BulkDeleteModalExportButton);
+
+    const openedEvent = await telemetryEntry('Delete Export Opened');
+    expect(openedEvent).to.deep.equal({});
+
+    const text = await browser.exportToLanguage('Python', {
+      includeImportStatements: true,
+      includeDriverSyntax: true,
+      useBuilders: false,
+    });
+    expect(text).to.equal(`from pymongo import MongoClient
+
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+client = MongoClient('mongodb://localhost:27091/test')
+filter={
+    'i': 5
+}
+
+result = client['test']['numbers'].delete_many(
+  filter=filter
+)`);
+
+    const exportedEvent = await telemetryEntry('Delete Exported');
+    expect(exportedEvent).to.deep.equal({
+      language: 'python',
+      with_builders: false,
+      with_drivers_syntax: true,
+      with_import_statements: true,
+    });
   });
 });
