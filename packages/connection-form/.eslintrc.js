ab235191b233d39a57c2388112270c5cb06df626
Himanshu Singh <himanshu.singhs@outlook.in>
2023-12-07 16:38:58 +0100

    chore: allow only type imports for mongodb package in connection-form

--- packages/connection-form/.eslintrc.js
+++ packages/connection-form/.eslintrc.js
@@ -5,4 +5,35 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig-lint.json'],
   },
+  overrides: [
+    {
+      files: ['./src/**/*.ts', './src/**/*.tsx'],
+      rules: {
+        // 'mongodb' package has been moved to devDependencies in this package, to
+        // aid in the usage of this package inside the VSCode extension's webview.
+        // Otherwise it would have been necessary to provide several polyfills for
+        // the dependencies of 'mongodb' package when webpacking it for an
+        // environment other than node. Hence we are restricting the usage of this
+        // package to type only imports.
+        '@typescript-eslint/no-restricted-imports': [
+          'error',
+          {
+            paths: [
+              {
+                name: 'mongodb',
+                allowTypeImports: true,
+                message: "Only type imports allowed from 'mongodb'",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      files: ['./src/**/*.spec.ts'],
+      rules: {
+        '@typescript-eslint/no-restricted-imports': ['off'],
+      },
+    },
+  ],
 };
