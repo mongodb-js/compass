51b61efbca99ff787d9cd38d0a2a84937ac8e3a5
Himanshu Singh <himanshu.singhs@outlook.in>
2023-12-08 11:05:47 +0100

    chore: remove unnecessary comment

--- packages/connection-form/.eslintrc.js
+++ packages/connection-form/.eslintrc.js
@@ -12,12 +12,6 @@ module.exports = {
         // See -
         // https://typescript-eslint.io/rules/no-restricted-imports/#how-to-use
         'no-restricted-imports': 'off',
-        // 'mongodb' package has been moved to devDependencies in this package, to
-        // aid in the usage of this package inside the VSCode extension's webview.
-        // Otherwise it would have been necessary to provide several polyfills for
-        // the dependencies of 'mongodb' package when webpacking it for an
-        // environment other than node. Hence we are restricting the usage of this
-        // package to type only imports.
         '@typescript-eslint/no-restricted-imports': [
           'error',
           {
