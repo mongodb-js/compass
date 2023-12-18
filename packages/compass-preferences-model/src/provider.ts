a123b8fb181683efeab16b7103dc9dbe1b836039
Anna Henningsen <anna@addaleax.net>
2023-12-18 15:18:25 +0100

    fixup: avoid circular dependency

--- packages/compass-preferences-model/src/provider.ts
+++ packages/compass-preferences-model/src/provider.ts
@@ -4,12 +4,14 @@ import { defaultPreferencesInstance } from '.';
 export { usePreference, withPreferences } from './react';
 
 const PreferencesContext = createContext<PreferencesAccess>(
-  defaultPreferencesInstance
+  // should be `defaultPreferencesInstance`, only using undefined here
+  // to avoid the circular dependency with index.ts
+  undefined as unknown as PreferencesAccess
 );
 
 export const PreferencesProvider = PreferencesContext.Provider;
 
 export function preferencesLocator(): PreferencesAccess {
-  return useContext(PreferencesContext);
+  return useContext(PreferencesContext) ?? defaultPreferencesInstance;
 }
 export type { PreferencesAccess };
