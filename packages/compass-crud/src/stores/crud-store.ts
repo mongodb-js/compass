4d451647f54548588f4b87347c771d9412f4819e
Sergey Petushkov <petushkov.sergey@gmail.com>
2024-01-03 12:03:11 +0100

    chore(crud): fix log id

--- packages/compass-crud/src/stores/crud-store.ts
+++ packages/compass-crud/src/stores/crud-store.ts
@@ -1764,7 +1764,7 @@ class CrudStoreImpl
       countOptions,
       (err: any) => {
         this.logger.log.warn(
-          mongoLogId(1_001_000_270),
+          mongoLogId(1_001_000_275),
           'Documents',
           'Failed to count documents',
           err
