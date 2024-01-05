80dcdee15f23c8f5e44b8561d7a21fc16077c37b
Anna Henningsen <anna@addaleax.net>
2024-01-05 13:35:10 +0100

    chore(compass-web): stub out devtools-connect entirely COMPASS-7552
    devtools-connect mostly just contains logic that applies to desktop application
    scenarios, and not so much the web use case. Instead of manually stubbing out
    the transitive dependencies that come with it, stub out the entire devtools-connect
    dependency with one that connects to the driver fairly directly.

--- packages/compass-web/polyfills/os-dns-native/index.ts
+++
@@ -1,5 +0,0 @@
-import { resolveSrv, resolveTxt } from 'dns';
-export const withNodeFallback = { resolveSrv, resolveTxt };
-export function wasNativelyLookedUp() {
-  return false;
-}
--- packages/compass-web/polyfills/os/index.ts
+++
@@ -1,4 +0,0 @@
-export function tmpdir() {
-  return '';
-}
-export default { tmpdir };
