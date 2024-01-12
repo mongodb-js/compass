11ed6bca8efa5a9558d5f1fd3c72bccaf764e5bb
Rhys <Anemy@users.noreply.github.com>
2024-01-12 12:03:01 -0500

    chore(compass-web): fix type export to unblock package update publishes (#5337)

--- packages/connection-form/src/index.ts
+++ packages/connection-form/src/index.ts
@@ -2,6 +2,7 @@ import ConnectionForm from './components/connection-form';
 import SaveConnectionModal from './components/save-connection-modal';
 import { useConnectionColor } from './hooks/use-connection-color';
 import { adjustConnectionOptionsBeforeConnect } from './hooks/use-connect-form';
+
 export {
   SaveConnectionModal,
   useConnectionColor,
