594f9818c09e82b3a343f0b89f35a9f2398c53c6
Basit <baasit121@icloud.com>
2024-01-16 16:00:07 +0100

    use abs path

--- packages/hadron-build/lib/signtool.js
+++ packages/hadron-build/lib/signtool.js
@@ -22,18 +22,21 @@ function getSignedFilename(filename) {
 
 /**
  * Currently, windows and macos zip are created from `zip` step
- * of the release process and we sign them here. For linux, we
- * create and sign the archive when creating corresponding deb/rpm file.
+ * of the release process. For linux, we create the archive when
+ * creating corresponding deb/rpm file. And here, we sign all of
+ * them togther.
  *
  * @param {import('./Target')} target
  */
 function signArchive(target, cb) {
   const { app_archive_name, platform } = target;
   if (platform === 'linux') {
-    debug('linux archive is signed when creating deb/rpm');
+    debug('Skipping archive signing for %s', platform);
     return cb();
   }
-  sign(app_archive_name).then(cb).catch(cb);
+  const file = target.dest(app_archive_name);
+  debug('Signing archive %s', file);
+  sign(file).then(cb).catch(cb);
 }
 
 /**
