ad17887e90091bb5d63ca2d6c32b8f5491f67b62
Basit <baasit121@icloud.com>
2024-01-19 19:08:45 +0100

    use function

--- .evergreen/verify-artifacts.sh
+++ .evergreen/verify-artifacts.sh
@@ -19,16 +19,27 @@ trap_handler() {
   exit $code
 }
 
-
 trap trap_handler ERR EXIT
+
+verify_using_gpg() {
+  echo "Verifying $1 using gpg"
+  gpg --verify $ARTIFACTS_DIR/$1.sig $ARTIFACTS_DIR/$1 > "$TMP_FILE" 2>&1
+}
+
+verify_using_powershell() {
+  echo "Verifying $1 using powershell"
+  powershell Get-AuthenticodeSignature -FilePath $ARTIFACTS_DIR/$1 > "$TMP_FILE" 2>&1
+}
+
+verify_using_codesign() {
+  echo "Verifying $1 using codesign"
+  codesign -dv --verbose=4 $ARTIFACTS_DIR/$1 > "$TMP_FILE" 2>&1
+}
 
 # For Windows
 if [ "$IS_WINDOWS" = true ]; then
-  echo "Verifying $WINDOWS_EXE_NAME"
-  powershell Get-AuthenticodeSignature -FilePath $ARTIFACTS_DIR/$WINDOWS_EXE_NAME > "$TMP_FILE" 2>&1
-
-  echo "Verifying $WINDOWS_MSI_NAME"
-  powershell Get-AuthenticodeSignature -FilePath $ARTIFACTS_DIR/$WINDOWS_MSI_NAME > "$TMP_FILE" 2>&1
+  verify_using_powershell $WINDOWS_EXE_NAME
+  verify_using_powershell $WINDOWS_MSI_NAME
 
   echo "Skipping verification for Windows artifacts using gpg: $WINDOWS_ZIP_NAME, $WINDOWS_NUPKG_NAME"
   exit 0
@@ -39,25 +50,16 @@ echo "Importing Compass public key"
 curl https://pgp.mongodb.com/compass.asc | gpg --import > "$TMP_FILE" 2>&1
 
 if [ "$IS_UBUNTU" = true ]; then
-  echo "Verifying $LINUX_DEB_NAME using gpg"
-  gpg --verify $ARTIFACTS_DIR/$LINUX_DEB_NAME.sig $ARTIFACTS_DIR/$LINUX_DEB_NAME > "$TMP_FILE" 2>&1
-
-  echo "Verifying $LINUX_TAR_NAME using gpg"
-  gpg --verify $ARTIFACTS_DIR/$LINUX_TAR_NAME.sig $ARTIFACTS_DIR/$LINUX_TAR_NAME > "$TMP_FILE" 2>&1
+  verify_using_gpg $LINUX_DEB_NAME
+  verify_using_gpg $LINUX_TAR_NAME
 fi
 
 if [ "$IS_RHEL" = true ]; then
-  echo "Verifying $RHEL_RPM_NAME using gpg"
-  gpg --verify $ARTIFACTS_DIR/$RHEL_RPM_NAME.sig $ARTIFACTS_DIR/$RHEL_RPM_NAME > "$TMP_FILE" 2>&1
-
-  echo "Verifying $RHEL_TAR_NAME using gpg"
-  gpg --verify $ARTIFACTS_DIR/$RHEL_TAR_NAME.sig $ARTIFACTS_DIR/$RHEL_TAR_NAME > "$TMP_FILE" 2>&1
+  verify_using_gpg $RHEL_RPM_NAME
+  verify_using_gpg $RHEL_TAR_NAME
 fi
 
 if [ "$IS_OSX" = true ]; then
-  echo "Verifying $OSX_ZIP_NAME using gpg"
-  gpg --verify $ARTIFACTS_DIR/$OSX_ZIP_NAME.sig $ARTIFACTS_DIR/$OSX_ZIP_NAME > "$TMP_FILE" 2>&1
-
-  echo "Verifying $OSX_DMG_NAME using codesign"
-  codesign -dv --verbose=4 $ARTIFACTS_DIR/$OSX_DMG_NAME > "$TMP_FILE" 2>&1
+  verify_using_gpg $OSX_ZIP_NAME
+  verify_using_codesign $OSX_DMG_NAME
 fi
