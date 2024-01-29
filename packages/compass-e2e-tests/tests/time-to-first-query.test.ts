0a329e92189fadd50c381a1f3256b4d262232691
Le Roux Bodenstein <lerouxb@gmail.com>
2024-01-29 11:16:38 +0000

    yolo

--- packages/compass-e2e-tests/tests/time-to-first-query.test.ts
+++ packages/compass-e2e-tests/tests/time-to-first-query.test.ts
@@ -3,7 +3,7 @@ import { init, cleanup, screenshotIfFailed } from '../helpers/compass';
 import type { Compass } from '../helpers/compass';
 import { createNumbersCollection } from '../helpers/insert-data';
 
-describe.only('Time to first query', function () {
+describe('Time to first query', function () {
   let compass: Compass | undefined;
 
   beforeEach(async function () {
