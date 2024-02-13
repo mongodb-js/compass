e79c8893ec8cced16e54dc584e682535ffe228e7
Himanshu Singh <himanshu.singhs@outlook.in>
2024-02-13 11:49:17 +0100

    fix for lint

--- packages/compass-crud/src/stores/crud-store.spec.ts
+++ packages/compass-crud/src/stores/crud-store.spec.ts
@@ -92,7 +92,7 @@ function waitForState(store, cb, timeout?: number) {
   return waitForStates(store, [cb], timeout);
 }
 
-describe.only('store', function () {
+describe('store', function () {
   this.timeout(5000);
 
   const cluster = mochaTestServer({
