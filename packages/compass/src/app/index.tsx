f38dc6524609316b68bfdde0a55b22b91fe5404b
Le Roux Bodenstein <lerouxb@gmail.com>
2024-02-14 15:59:16 +0000

    make the two work the same

--- packages/compass/src/app/index.tsx
+++ packages/compass/src/app/index.tsx
@@ -22,17 +22,17 @@ if (!process.env.NODE_OPTIONS.includes('--dns-result-order')) {
 // Setup error reporting to main process before anything else.
 window.addEventListener('error', (event: ErrorEvent) => {
   event.preventDefault();
-  void ipcRenderer?.call(
-    'compass:error:fatal',
-    event.error
-      ? { message: event.error.message, stack: event.error.stack }
-      : { message: event.message, stack: '<no stack available>' }
-  );
+  const error = ensureError(event.error);
+  void ipcRenderer?.call('compass:error:fatal', {
+    message: error.message,
+    stack: error.stack,
+  });
 });
 
 window.addEventListener(
   'unhandledrejection',
   (event: PromiseRejectionEvent) => {
+    event.preventDefault();
     const error = ensureError(event.reason);
     void ipcRenderer?.call('compass:rejection:fatal', {
       message: error.message,
