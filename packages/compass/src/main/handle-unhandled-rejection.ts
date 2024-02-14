5c7e734f191ab1e3f5603326e86963bb1963029c
Le Roux Bodenstein <lerouxb@gmail.com>
2024-02-14 14:25:46 +0000

    Revert "chore: add main process unhandled rejection handler COMPASS-7268 (#5445)"
    This reverts commit dac719225f5d3ef7678da9b360c1aa392e9a37e1.

--- packages/compass/src/main/handle-unhandled-rejection.ts
+++
@@ -1,9 +0,0 @@
-import { handleError } from './handle-error';
-
-async function handleUnhandledRejection(err: Error): Promise<void> {
-  // eslint-disable-next-line no-console
-  console.error('handling unhandledRejection', err);
-  await handleError(err);
-}
-
-export { handleUnhandledRejection };
