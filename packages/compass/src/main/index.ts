4cbfac9168be0ec5eb696e9eb94a633e98a67589
Le Roux Bodenstein <lerouxb@gmail.com>
2024-02-14 10:59:01 +0000

    Revert "add unhandled rejection handler"
    This reverts commit 855371b8e175de26033793330ebdb367747c7b2f.

--- packages/compass/src/main/index.ts
+++ packages/compass/src/main/index.ts
@@ -3,7 +3,6 @@ import '../setup-hadron-distribution';
 
 import { app, dialog, crashReporter } from 'electron';
 import { handleUncaughtException } from './handle-uncaught-exception';
-import { handleUnhandledRejection } from './handle-unhandled-rejection';
 import { initialize as initializeElectronRemote } from '@electron/remote/main';
 import {
   doImportConnections,
@@ -101,8 +100,6 @@ async function main(): Promise<void> {
     }
     // eslint-disable-next-line @typescript-eslint/no-misused-promises
     process.on('uncaughtException', handleUncaughtException);
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    process.on('unhandledRejection', handleUnhandledRejection);
   } else {
     if (errorOutDueToAdditionalCommandLineFlags) {
       process.stderr.write(
