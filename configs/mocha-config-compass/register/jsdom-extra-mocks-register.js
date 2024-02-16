e7da7f2b43d02fab594ddee1b0d95d5d7baa89f3
Rhys Howell <rhys.howell@mongodb.com>
2024-02-16 12:32:56 -0500

    chore: add custom event mock for jsdom eventtarget check

--- configs/mocha-config-compass/register/jsdom-extra-mocks-register.js
+++ configs/mocha-config-compass/register/jsdom-extra-mocks-register.js
@@ -12,3 +12,18 @@ if (!window.matchMedia) {
     };
   };
 }
+
+// Hack: `EventTarget` in jsdom checks for the `Event` symbol,
+// which new `CustomEvent` does not provide (outside of the dom).
+// We override the global CustomEvent with a modified event so that
+// the check succeeds. This might go away with newer "jsdom" versions.
+(function () {
+  function CustomEvent(eventName, params) {
+    const evt = document.createEvent('HTMLEvents');
+    evt.detail = params.detail;
+    evt.initEvent(eventName);
+    return evt;
+  }
+
+  global.CustomEvent = CustomEvent;
+})();
