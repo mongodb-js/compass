d139936bf58fe8a6d7099cc77dfa128b0be348ca
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-02-19 18:03:02 +0100

    chore: fix clipboard tests

--- packages/compass-e2e-tests/index.ts
+++ packages/compass-e2e-tests/index.ts
@@ -20,6 +20,9 @@ import {
 } from './helpers/compass';
 import ResultLogger from './helpers/result-logger';
 
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 const debug = Debug('compass-e2e-tests');
 
 const allowedArgs = [
