2cb2be70ab2c395038ee4ebf85112ada1df0639b
Basit <1305718+mabaasit@users.noreply.github.com>
2024-02-29 14:48:23 +0100

    chore(atlas-service): convert to plugin (#5512)
    * Revert "breadcrumbs ui"
    
    This reverts commit 8b0b3d947579d19347a28451c3655f522cec4c75.
    
    * setup provider
    
    * convert atlas-sign-in to a plugin
    
    * rename
    
    * bootstrap
    
    * logger component
    
    * clean up
    
    * use refs
    
    * bootstrap

--- packages/atlas-service/src/renderer.ts
+++ packages/atlas-service/src/renderer.ts
@@ -1,5 +1,19 @@
-export { AtlasSignIn } from './components/atlas-signin';
+import { registerHadronPlugin } from 'hadron-app-registry';
+import { activatePlugin } from './store/atlas-signin-store';
+import { atlasAuthServiceLocator } from './provider';
+import { AtlasSignIn } from './components';
 
+export const AtlasAuthPlugin = registerHadronPlugin(
+  {
+    name: 'AtlasAuth',
+    component: AtlasSignIn,
+    activate: activatePlugin,
+  },
+  {
+    atlasAuthService: atlasAuthServiceLocator,
+  }
+);
+export default AtlasAuthPlugin;
 export { AtlasServiceError } from './util';
 export type { AtlasUserConfig } from './user-config-store';
 export type { AtlasUserInfo } from './util';
