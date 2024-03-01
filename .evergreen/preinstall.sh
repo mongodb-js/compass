967d7dd00f966d7c7f2e2a1b3a464d2a6104c601
Le Roux Bodenstein <lerouxb@gmail.com>
2024-03-01 10:09:42 +0000

    chore(ci): node 18, npm 10 in CI (#5479)
    * node 18, npm 10 in CI
    
    * connect straight to 127.0.0.1 rather than localhost

--- .evergreen/preinstall.sh
+++ .evergreen/preinstall.sh
@@ -43,11 +43,20 @@ if [ -n "$IS_WINDOWS" ]; then
     cd ..
     .evergreen/node-gyp-bug-workaround.sh
 else
-    echo "Installing nodejs v${NODE_JS_VERSION} for ${PLATFORM} on ${ARCH}..."
+    if command -v ldd &> /dev/null && `ldd $(which bash) | grep 'libc.so' | awk '{print $3}'` | grep -Eq 'release version 2.(1|2[0-7])'; then
+        echo "Installing unofficial nodejs compiled for glibc 2.17 v${NODE_JS_VERSION} for ${PLATFORM} on ${ARCH}..."
 
-    bash "${SCRIPTDIR}/retry-with-backoff.sh" curl -fs \
-        -o ".deps/node-v${NODE_JS_VERSION}-${PLATFORM}-$ARCH.tar.gz" \
-        --url "https://nodejs.org/download/release/v${NODE_JS_VERSION}/node-v${NODE_JS_VERSION}-${PLATFORM}-$ARCH.tar.gz"
+        bash "${SCRIPTDIR}/retry-with-backoff.sh" curl -fs \
+            -o ".deps/node-v${NODE_JS_VERSION}-${PLATFORM}-$ARCH.tar.gz" \
+            --url "https://unofficial-builds.nodejs.org/download/release/v${NODE_JS_VERSION}/node-v${NODE_JS_VERSION}-${PLATFORM}-$ARCH-glibc-217.tar.gz"
+    else
+        echo "Installing nodejs v${NODE_JS_VERSION} for ${PLATFORM} on ${ARCH}..."
+
+        bash "${SCRIPTDIR}/retry-with-backoff.sh" curl -fs \
+            -o ".deps/node-v${NODE_JS_VERSION}-${PLATFORM}-$ARCH.tar.gz" \
+            --url "https://nodejs.org/download/release/v${NODE_JS_VERSION}/node-v${NODE_JS_VERSION}-${PLATFORM}-$ARCH.tar.gz"
+    fi
+
     cd .deps
     tar xzf node-v$NODE_JS_VERSION-$PLATFORM-$ARCH.tar.gz --strip-components=1
 
