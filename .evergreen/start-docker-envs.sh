8ab40c0ba00336ac56d28dbf800765cbafccbb17
Sergey Petushkov <petushkov.sergey@gmail.com>
2024-03-11 11:03:35 +0100

    chore: remove shebang from the sourced file
    Co-authored-by: Anna Henningsen <anna.henningsen@mongodb.com>

--- .evergreen/start-docker-envs.sh
+++ .evergreen/start-docker-envs.sh
@@ -1,5 +1,3 @@
-#! /usr/bin/env bash
-
 #########################################################
 ###                 !!! IMPORTANT !!!                 ###
 ###  THIS SCRIPT IS EXECUTED WITH THE SOURCE COMMAND  ###
