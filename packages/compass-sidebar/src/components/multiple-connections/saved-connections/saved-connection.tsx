204afb4501451d0fbc25362f1211504f5f882141
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-03-13 18:01:44 +0100

    chore: draft

--- packages/compass-sidebar/src/components/multiple-connections/saved-connections/saved-connection.tsx
+++ packages/compass-sidebar/src/components/multiple-connections/saved-connections/saved-connection.tsx
@@ -118,13 +118,6 @@ type SavedConnectionProps = {
   onToggleFavoriteConnection(connectionInfo: ConnectionInfo): void;
 };
 
-const useConnectionStatus = (
-  connectionInfo: ConnectionInfo
-): ConnectionStatus => {
-  // polyfill
-  return 'failed';
-};
-
 export function SavedConnection({
   connectionInfo,
   onConnect,
