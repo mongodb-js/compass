081f1cc576e02153ed15bf487bac596df6583743
Sergey Petushkov <petushkov.sergey@gmail.com>
2024-03-15 16:23:41 +0100

    fix: update mocha config imports

--- configs/eslint-plugin-compass/.mocharc.js
+++ configs/eslint-plugin-compass/.mocharc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ...require('@mongodb-js/mocha-config-compass'),
+  ...require('@cloud-mongodb-js/mocha-config-compass'),
   spec: ['rules/**/*.test.js'],
   watchFiles: ['rules/**/*.js'],
 };
--- packages/data-service/.mocharc.js
+++ packages/data-service/.mocharc.js
@@ -1,1 +1,1 @@
-module.exports = require('@mongodb-js/mocha-config-compass');
+module.exports = require('@cloud-mongodb-js/mocha-config-compass');
