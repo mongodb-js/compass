d79e1fc0bf865bd18173cca86dafb9b9bf453ff7
Alena Khineika <alena.khineika@gmail.com>
2024-03-22 15:34:16 +0100

    feat: update atlas login screen flow COMPASS-7755 (#5606)
    * feat: update the atlas login screen flow COMPASS-7755
    
    * test: remove TOS test cases

--- packages/atlas-service/src/oidc-plugin-logger.ts
+++ packages/atlas-service/src/oidc-plugin-logger.ts
@@ -1,6 +1,5 @@
 import { EventEmitter } from 'events';
 import type { MongoDBOIDCPluginOptions } from '@mongodb-js/oidc-plugin';
-import type { AtlasUserConfig } from './user-config-store';
 
 type MongoDBOIDCPluginLogger = Required<MongoDBOIDCPluginOptions>['logger'];
 
@@ -54,24 +53,13 @@ type OidcPluginLogger = MongoDBOIDCPluginLogger & {
   on(evt: 'atlas-service-token-refreshed', fn: () => void): void;
   on(evt: 'atlas-service-token-refresh-failed', fn: () => void): void;
   on(evt: 'atlas-service-signed-out', fn: () => void): void;
-  on(
-    evt: 'atlas-service-user-config-changed',
-    fn: (newConfig: AtlasUserConfig) => void
-  ): void;
+
   once(evt: 'atlas-service-token-refreshed', fn: () => void): void;
   once(evt: 'atlas-service-token-refresh-failed', fn: () => void): void;
   once(evt: 'atlas-service-signed-out', fn: () => void): void;
-  once(
-    evt: 'atlas-service-user-config-changed',
-    fn: (newConfig: AtlasUserConfig) => void
-  ): void;
   emit(evt: 'atlas-service-token-refreshed'): void;
   emit(evt: 'atlas-service-token-refresh-failed'): void;
   emit(evt: 'atlas-service-signed-out'): void;
-  emit(
-    evt: 'atlas-service-user-config-changed',
-    newConfig: AtlasUserConfig
-  ): void;
 } & Pick<EventEmitter, 'removeAllListeners'>;
 
 export const OidcPluginLogger: { new (): OidcPluginLogger } = EventEmitter;
