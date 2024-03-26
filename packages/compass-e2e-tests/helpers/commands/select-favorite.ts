381252ae55ed27c4c612de5a6549ed95526ac438
Le Roux Bodenstein <lerouxb@gmail.com>
2024-03-26 20:49:26 +0000

    pause before selecting favourite

--- packages/compass-e2e-tests/helpers/commands/select-favorite.ts
+++ packages/compass-e2e-tests/helpers/commands/select-favorite.ts
@@ -5,6 +5,11 @@ export async function selectFavorite(
   browser: CompassBrowser,
   favoriteName: string
 ): Promise<void> {
+  // Usually we use this after disconnecting. I'm just trying to see where the
+  // race condition is - are we clicking too quickly or is the act of clicking
+  // causing something to re-render and the element to go stale?
+  await browser.pause(1000);
+
   await browser.clickVisible(Selectors.sidebarFavoriteButton(favoriteName));
   await browser.waitUntil(async () => {
     const text = await browser.$(Selectors.ConnectionTitle).getText();
