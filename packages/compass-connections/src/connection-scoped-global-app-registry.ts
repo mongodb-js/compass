69bb8d8cc8266bedc840e9c82a2f7cb3259b3fdb
Himanshu Singh <himanshu.singhs@outlook.in>
2024-03-28 11:17:57 +0100

    feedback changes:
    - use generic type for events
    - use refs instead of memo for registry
    - subscrible only to changes in connection info id

--- packages/compass-connections/src/connection-scoped-global-app-registry.ts
+++ packages/compass-connections/src/connection-scoped-global-app-registry.ts
@@ -5,62 +5,68 @@ import {
   useGlobalAppRegistry,
 } from 'hadron-app-registry';
 import { useConnectionInfo } from '@mongodb-js/connection-storage/provider';
-import { useMemo } from 'react';
+import { useEffect, useRef } from 'react';
 
-type ConnectionScopedGlobalAppRegistryEvents =
-  | 'schema-analyzed'
-  | 'documents-refreshed'
-  | 'documents-paginated'
-  | 'document-inserted';
+type ConnectionScopedGlobalAppRegistryImplKeys<T extends string> = Omit<
+  ConnectionScopedGlobalAppRegistryImpl<T>,
+  'updateConnectionInfoId'
+>;
 
 export type ConnectionScopedGlobalAppRegistryLocator<
-  T extends ConnectionScopedGlobalAppRegistryEvents,
-  K extends keyof ConnectionScopedGlobalAppRegistryImpl<T> = keyof ConnectionScopedGlobalAppRegistryImpl<T>,
-  L extends keyof ConnectionScopedGlobalAppRegistryImpl<T> = K
+  T extends string,
+  K extends keyof ConnectionScopedGlobalAppRegistryImplKeys<T> = keyof ConnectionScopedGlobalAppRegistryImplKeys<T>,
+  L extends keyof ConnectionScopedGlobalAppRegistryImplKeys<T> = K
 > = () => ConnectionScopedGlobalAppRegistry<T, K, L>;
 
 export type ConnectionScopedGlobalAppRegistry<
-  T extends ConnectionScopedGlobalAppRegistryEvents,
-  K extends keyof ConnectionScopedGlobalAppRegistryImpl<T> = keyof ConnectionScopedGlobalAppRegistryImpl<T>,
-  L extends keyof ConnectionScopedGlobalAppRegistryImpl<T> = K
+  T extends string,
+  K extends keyof ConnectionScopedGlobalAppRegistryImplKeys<T> = keyof ConnectionScopedGlobalAppRegistryImplKeys<T>,
+  L extends keyof ConnectionScopedGlobalAppRegistryImplKeys<T> = K
 > = Pick<ConnectionScopedGlobalAppRegistryImpl<T>, K> &
   Partial<Pick<ConnectionScopedGlobalAppRegistryImpl<T>, L>>;
 
-interface EventForwarder<T extends ConnectionScopedGlobalAppRegistryEvents> {
+interface EventForwarder<T extends string> {
   emit(event: T, payload?: Record<string, any>): void;
 }
 
-export class ConnectionScopedGlobalAppRegistryImpl<
-  T extends ConnectionScopedGlobalAppRegistryEvents
-> implements EventForwarder<T>
+export class ConnectionScopedGlobalAppRegistryImpl<T extends string>
+  implements EventForwarder<T>
 {
   constructor(
     private readonly appRegistryEmitter: AppRegistry['emit'],
-    private readonly sourceConnectionInfo: ConnectionInfo
+    private sourceConnectionInfoId: ConnectionInfo['id']
   ) {}
 
+  updateConnectionInfoId(connectionInfoId: ConnectionInfo['id']) {
+    this.sourceConnectionInfoId = connectionInfoId;
+  }
+
   emit(event: T, payload: Record<string, any> = {}): void {
-    payload.sourceConnectionInfo = this.sourceConnectionInfo;
+    payload.sourceConnectionInfo = this.sourceConnectionInfoId;
     this.appRegistryEmitter(event, payload);
   }
 }
 
 export const connectionScopedGlobalAppRegistryLocator = createServiceLocator(
   function useConnectionScopedGlobalAppRegistry<
-    T extends ConnectionScopedGlobalAppRegistryEvents,
-    K extends keyof ConnectionScopedGlobalAppRegistryImpl<T> = keyof ConnectionScopedGlobalAppRegistryImpl<T>,
-    L extends keyof ConnectionScopedGlobalAppRegistryImpl<T> = K
+    T extends string,
+    K extends keyof ConnectionScopedGlobalAppRegistryImplKeys<T> = keyof ConnectionScopedGlobalAppRegistryImplKeys<T>,
+    L extends keyof ConnectionScopedGlobalAppRegistryImplKeys<T> = K
   >(): ConnectionScopedGlobalAppRegistry<T, K, L> {
     const appRegistry = useGlobalAppRegistry();
     const connectionInfo = useConnectionInfo();
 
-    const registry = useMemo(() => {
-      return new ConnectionScopedGlobalAppRegistryImpl<T>(
+    const registry = useRef(
+      new ConnectionScopedGlobalAppRegistryImpl<T>(
         appRegistry.emit.bind(appRegistry),
-        connectionInfo
-      );
-    }, [appRegistry, connectionInfo]);
+        connectionInfo.id
+      )
+    );
 
-    return registry;
+    useEffect(() => {
+      registry.current.updateConnectionInfoId(connectionInfo.id);
+    }, [connectionInfo.id]);
+
+    return registry.current;
   }
 );
