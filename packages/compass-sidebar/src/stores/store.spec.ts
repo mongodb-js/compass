7b045071ab008ff72628bf91a3bdeb3b229e9c77
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-04-10 15:06:51 +0200

    chore: keep fixing tests

--- packages/compass-sidebar/src/stores/store.spec.ts
+++ packages/compass-sidebar/src/stores/store.spec.ts
@@ -1,49 +1,50 @@
 import AppRegistry from 'hadron-app-registry';
 import { expect } from 'chai';
+import { stub, type SinonStub, spy, type SinonSpy } from 'sinon';
 import { createSidebarStore } from '.';
 import { createInstance } from '../../test/helpers';
 import type { Database } from '../modules/databases';
 import type { MongoDBInstance } from 'mongodb-instance-model';
+import { DataService } from 'mongodb-data-service';
 
-const instance = createInstance();
-
-function getDatabases(_instance: MongoDBInstance) {
-  return _instance.databases.map((db: Database) => {
-    return {
-      _id: db._id,
-      name: db.name,
-      collectionsStatus: db.collectionsStatus,
-      collectionsLength: db.collectionsLength,
-      collections: db.collections.map((coll) => {
-        return {
-          _id: coll._id,
-          name: coll.name,
-          type: coll.type,
-          pipeline: coll.pipeline,
-          sourceName: coll.sourceName,
-        };
-      }),
-    };
-  });
-}
+const CONNECTION_ID = 'webscale';
 
 describe('SidebarStore [Store]', function () {
-  const globalAppRegistry = new AppRegistry();
+  const instance = createInstance();
+  const globalAppRegistry = {};
+
   let store: ReturnType<typeof createSidebarStore>['store'];
   let deactivate: () => void;
+  let listMongoDBInstancesStub: SinonStub;
 
   beforeEach(function () {
+    instance.on = spy();
+
+    listMongoDBInstancesStub = stub().returns(
+      new Map([[CONNECTION_ID, instance]])
+    );
+
     ({ store, deactivate } = createSidebarStore(
       {
         globalAppRegistry,
-        dataService: {
-          getConnectionOptions() {
-            return {};
+        connectionsManager: {
+          getDataServiceForConnection(id: string) {
+            return {
+              getConnectionOptions() {
+                return {};
+              },
+              currentOp() {
+                return Promise.resolve(null);
+              },
+              top() {
+                return Promise.resolve(null);
+              },
+            } as unknown as DataService;
           },
-          currentOp() {},
-          top() {},
+        } as any,
+        instancesManager: {
+          listMongoDBInstances: listMongoDBInstancesStub,
         },
-        instance,
         logger: { log: { warn() {} }, mongoLogId() {} },
       } as any,
       { on() {}, cleanup() {}, addCleanup() {} } as any
@@ -54,47 +55,27 @@ describe('SidebarStore [Store]', function () {
     deactivate();
   });
 
-  context('when instance created', function () {
-    it('updates the instance and databases state', function () {
-      const state = store.getState();
-
-      expect(state)
-        .to.have.property('instance')
-        .deep.equal({
-          build: {
-            isEnterprise: undefined,
-            version: undefined,
-          },
-          csfleMode: 'unavailable',
-          dataLake: {
-            isDataLake: false,
-            version: undefined,
-          },
-          databasesStatus: 'initial',
-          env: 'on-prem',
-          genuineMongoDB: {
-            dbType: undefined,
-            isGenuine: true,
-          },
-          isAtlas: false,
-          isLocalAtlas: false,
-          isWritable: false,
-          refreshingStatus: 'initial',
-          status: 'initial',
-          topologyDescription: {
-            servers: [],
-            setName: 'foo',
-            type: 'Unknown',
-          },
-        });
-      expect(state)
-        .to.have.property('databases')
-        .deep.equal({
-          databases: getDatabases(instance),
-          filteredDatabases: getDatabases(instance),
-          expandedDbList: {},
-          filterRegex: null,
-        });
+  for (const event of [
+    'change:status',
+    'change:refreshingStatus',
+    'change:databasesStatus',
+    'change:csfleMode',
+    'change:topologyDescription',
+    'change:isWritable',
+    'change:env',
+    'change:databasesStatus',
+    'add:databases',
+    'remove:databases',
+    'change:databases',
+    'change:databases.collectionsStatus',
+    'add:collections',
+    'remove:collections',
+    'change:collections._id',
+    'change:collections.status',
+    'change:genuineMongoDB.isGenuine',
+  ]) {
+    it(`subscribes to an existing instance event ${event}`, function () {
+      expect(instance.on).to.have.been.calledWith(event);
     });
-  });
+  }
 });
