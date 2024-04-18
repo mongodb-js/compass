bf68783e3db426cca1f36f112f9fd483db146752
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-04-18 12:52:54 +0200

    chore: fix missing tests

--- packages/compass-sidebar/src/stores/store.spec.ts
+++ packages/compass-sidebar/src/stores/store.spec.ts
@@ -1,3 +1,4 @@
+import { EventEmitter } from 'events';
 import { expect } from 'chai';
 import { stub, type SinonStub, spy, type SinonSpy } from 'sinon';
 import { createSidebarStore } from '.';
@@ -5,24 +6,54 @@ import { createInstance } from '../../test/helpers';
 import { createNoopLoggerAndTelemetry } from '@mongodb-js/compass-logging/provider';
 import type { DataService } from '@mongodb-js/compass-connections/provider';
 import { TEST_CONNECTION_INFO } from '@mongodb-js/compass-connections/provider';
+import {
+  MongoDBInstancesManagerEvents,
+  type MongoDBInstancesManager,
+} from '@mongodb-js/compass-app-stores/provider';
 
 const CONNECTION_ID = 'webscale';
+const ALL_EVENTS = [
+  'change:status',
+  'change:refreshingStatus',
+  'change:databasesStatus',
+  'change:csfleMode',
+  'change:topologyDescription',
+  'change:isWritable',
+  'change:env',
+  'change:databasesStatus',
+  'add:databases',
+  'remove:databases',
+  'change:databases',
+  'change:databases.collectionsStatus',
+  'add:collections',
+  'remove:collections',
+  'change:collections._id',
+  'change:collections.status',
+  'change:genuineMongoDB.isGenuine',
+];
 
 describe('SidebarStore [Store]', function () {
   const instance = createInstance();
   const globalAppRegistry = {} as any;
   let instanceOnSpy: SinonSpy;
+  let instanceOffSpy: SinonSpy;
 
   let deactivate: () => void;
   let listMongoDBInstancesStub: SinonStub;
+  let instancesManager: MongoDBInstancesManager;
 
   beforeEach(function () {
     instanceOnSpy = spy();
+    instanceOffSpy = spy();
     instance.on = instanceOnSpy;
+    (instance as any).off = instanceOffSpy;
 
     listMongoDBInstancesStub = stub().returns(
       new Map([[CONNECTION_ID, instance]])
     );
+
+    instancesManager = new EventEmitter() as MongoDBInstancesManager;
+    instancesManager.listMongoDBInstances = listMongoDBInstancesStub;
 
     ({ deactivate } = createSidebarStore(
       {
@@ -42,9 +73,7 @@ describe('SidebarStore [Store]', function () {
             } as unknown as DataService;
           },
         } as any,
-        instancesManager: {
-          listMongoDBInstances: listMongoDBInstancesStub,
-        } as any,
+        instancesManager: instancesManager,
         logger: createNoopLoggerAndTelemetry(),
         initialConnectionInfo: TEST_CONNECTION_INFO,
       },
@@ -56,27 +85,25 @@ describe('SidebarStore [Store]', function () {
     deactivate();
   });
 
-  for (const event of [
-    'change:status',
-    'change:refreshingStatus',
-    'change:databasesStatus',
-    'change:csfleMode',
-    'change:topologyDescription',
-    'change:isWritable',
-    'change:env',
-    'change:databasesStatus',
-    'add:databases',
-    'remove:databases',
-    'change:databases',
-    'change:databases.collectionsStatus',
-    'add:collections',
-    'remove:collections',
-    'change:collections._id',
-    'change:collections.status',
-    'change:genuineMongoDB.isGenuine',
-  ]) {
+  for (const event of ALL_EVENTS) {
     it(`subscribes to an existing instance event ${event}`, function () {
       expect(instanceOnSpy).to.have.been.calledWith(event);
     });
   }
+
+  describe('when a new instance is created', function () {
+    beforeEach(function () {
+      instancesManager.emit(
+        MongoDBInstancesManagerEvents.InstanceCreated,
+        'newConnection',
+        createInstance()
+      );
+    });
+
+    for (const event of ALL_EVENTS) {
+      it(`subscribes to an existing instance event ${event}`, function () {
+        expect(instanceOnSpy).to.have.been.calledWith(event);
+      });
+    }
+  });
 });
