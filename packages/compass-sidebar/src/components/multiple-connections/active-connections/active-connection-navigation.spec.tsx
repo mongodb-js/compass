5297eb6633af537e49635282426b826357419863
Paula Stachova <paula.stachova@tutanota.com>
2024-04-19 11:18:59 +0200

    test

--- packages/compass-sidebar/src/components/multiple-connections/active-connections/active-connection-navigation.spec.tsx
+++ packages/compass-sidebar/src/components/multiple-connections/active-connections/active-connection-navigation.spec.tsx
@@ -12,6 +12,10 @@ import { Provider } from 'react-redux';
 import AppRegistry from 'hadron-app-registry';
 import { type ConnectionInfo } from '@mongodb-js/connection-info';
 import { createNoopLoggerAndTelemetry } from '@mongodb-js/compass-logging/provider';
+import userEvent from '@testing-library/user-event';
+import sinon from 'sinon';
+import { createSandboxFromDefaultPreferences } from 'compass-preferences-model';
+import { PreferencesProvider } from 'compass-preferences-model/provider';
 
 const mockConnections: ConnectionInfo[] = [
   {
@@ -38,8 +42,9 @@ describe('<ActiveConnectionNavigation />', function () {
   let store: ReturnType<typeof createSidebarStore>['store'];
   let deactivate: () => void;
   const globalAppRegistry = new AppRegistry();
+  const onOpenConnectionInfoStub = sinon.stub();
 
-  beforeEach(() => {
+  beforeEach(async () => {
     connectionsManager = new ConnectionsManager({} as any);
     (connectionsManager as any).connectionStatuses.set('turtle', 'connected');
     (connectionsManager as any).connectionStatuses.set('oranges', 'connected');
@@ -58,25 +63,55 @@ describe('<ActiveConnectionNavigation />', function () {
       { on() {}, cleanup() {}, addCleanup() {} } as any
     ));
 
+    const preferences = await createSandboxFromDefaultPreferences();
+    await preferences.savePreferences({
+      enableRenameCollectionModal: true,
+      enableNewMultipleConnectionSystem: true,
+    });
+
     render(
-      <ConnectionsManagerProvider value={connectionsManager}>
-        <Provider store={store}>
-          <ActiveConnectionNavigation
-            activeConnections={mockConnections}
-            activeWorkspace={{ type: 'connection' }}
-          />
-        </Provider>
-      </ConnectionsManagerProvider>
+      <PreferencesProvider value={preferences}>
+        <ConnectionsManagerProvider value={connectionsManager}>
+          <Provider store={store}>
+            <ActiveConnectionNavigation
+              activeConnections={mockConnections}
+              activeWorkspace={{ id: 'turtle', type: 'Databases' }}
+              onOpenConnectionInfo={onOpenConnectionInfoStub}
+            />
+          </Provider>
+        </ConnectionsManagerProvider>
+      </PreferencesProvider>
     );
   });
 
   afterEach(() => {
     deactivate();
+    sinon.resetHistory();
   });
 
   it('Should render the number of connections', async function () {
     await waitFor(() => {
       expect(screen.queryByText('(2)')).to.be.visible;
+    });
+  });
+
+  describe('Connection actions', () => {
+    it('Calls onOpenConnectionInfo', async () => {
+      userEvent.hover(screen.getByText('turtle'));
+
+      const connectionActionsBtn = screen.getAllByTitle('Show actions')[0]; // TODO: This will be a single element once we fix the workspaces
+      expect(connectionActionsBtn).to.be.visible;
+
+      userEvent.click(connectionActionsBtn);
+
+      const openConnectionInfoBtn = await screen.findByText(
+        'Show connection info'
+      );
+      expect(openConnectionInfoBtn).to.be.visible;
+
+      userEvent.click(openConnectionInfoBtn);
+
+      expect(onOpenConnectionInfoStub).to.have.been.calledWith('turtle');
     });
   });
 });
