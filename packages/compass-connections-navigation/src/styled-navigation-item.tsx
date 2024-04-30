c8e4b15ed4584baf2bfe68b1373cf40ecb106c32
Kevin Mas Ruiz <kevin.mas@hey.com>
2024-04-30 15:00:53 +0200

    chore: memo styled component styles

--- packages/compass-connections-navigation/src/styled-navigation-item.tsx
+++ packages/compass-connections-navigation/src/styled-navigation-item.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
   useConnectionColor,
   DefaultColorCode,
@@ -15,18 +15,32 @@ export default function StyledNavigationItem({
 }): React.ReactElement {
   const { connectionColorToHex, connectionColorToHexActive } =
     useConnectionColor();
+
   const style: React.CSSProperties & {
     '--item-bg-color'?: string;
     '--item-bg-color-hover'?: string;
     '--item-bg-color-active'?: string;
-  } = {};
+  } = useMemo(() => {
+    const style: {
+      '--item-bg-color'?: string;
+      '--item-bg-color-hover'?: string;
+      '--item-bg-color-active'?: string;
+    } = {};
 
-  if (!isSingleConnection) {
-    if (colorCode && colorCode !== DefaultColorCode) {
-      style['--item-bg-color'] = connectionColorToHex(colorCode);
-      style['--item-bg-color-hover'] = connectionColorToHexActive(colorCode);
-      style['--item-bg-color-active'] = connectionColorToHexActive(colorCode);
+    if (!isSingleConnection) {
+      if (colorCode && colorCode !== DefaultColorCode) {
+        style['--item-bg-color'] = connectionColorToHex(colorCode);
+        style['--item-bg-color-hover'] = connectionColorToHexActive(colorCode);
+        style['--item-bg-color-active'] = connectionColorToHexActive(colorCode);
+      }
     }
-  }
+    return style;
+  }, [
+    isSingleConnection,
+    colorCode,
+    connectionColorToHex,
+    connectionColorToHexActive,
+  ]);
+
   return <div style={style}>{children}</div>;
 }
