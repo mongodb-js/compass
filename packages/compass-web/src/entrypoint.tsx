1c41ed61ef6e4cbde0d5a4dabf6330c4f6252741
Himanshu Singh <himanshu.singhs@outlook.in>
2024-05-13 20:25:26 +0200

    chore: compass-web check fixes

--- packages/compass-web/src/entrypoint.tsx
+++ packages/compass-web/src/entrypoint.tsx
@@ -156,7 +156,6 @@ function CompassWorkspace({
               return (
                 <CompassSidebarPlugin
                   showConnectionInfo={false}
-                  singleConnectionConnectionInfo={connectionInfo}
                 ></CompassSidebarPlugin>
               );
             }}
