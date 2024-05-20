83f720767f326982349cfebbf451809a69c0b409
Maurizio Casimirri <maurizio.cas@gmail.com>
2024-05-20 19:01:10 +0200

    chore(ci): fix snyk test hanging COMPASS-7947

--- scripts/snyk-test.js
+++ scripts/snyk-test.js
@@ -1,106 +1,93 @@
 'use strict';
-const childProcess = require('child_process');
-const path = require('path');
 const { promises: fs } = require('fs');
-const os = require('os');
-const { glob } = require('glob');
-const { promisify } = require('util');
-const execFile = promisify(childProcess.execFile);
+const path = require('path');
+const fetch = require('node-fetch');
 
-async function fileExists(filePath) {
-  try {
-    await fs.access(filePath);
-    return true;
-  } catch {
-    return false;
-  }
-}
+const DEPENDENCY_TEST_RETRIES = 3;
+const DEPENDENCY_TEST_TIMEOUT = 10000;
 
-async function snykTest(cwd) {
-  const tmpPath = path.join(os.tmpdir(), 'tempfile-' + Date.now());
+async function snykTest(dependency) {
+  const { name, version } = dependency;
+  const encodedName = encodeURIComponent(name);
+  const apiUrl = `https://api.snyk.io/v1/test/npm/${encodedName}/${version}`;
 
-  let execErr;
+  let attempts = 0;
 
-  try {
-    if (!(await fileExists(path.join(cwd, `package.json`)))) {
-      return;
-    }
-
-    console.info(`testing ${cwd} ...`);
-    await fs.mkdir(path.join(cwd, `node_modules`), { recursive: true });
+  while (attempts < DEPENDENCY_TEST_RETRIES) {
+    attempts++;
 
     try {
-      await execFile(
-        'npx',
-        [
-          'snyk@latest',
-          'test',
-          '--all-projects',
-          '--severity-threshold=low',
-          '--dev',
-          `--json-file-output=${tmpPath}`,
-        ],
-        {
-          cwd,
-          maxBuffer: 50 /* MB */ * 1024 * 1024, // default is 1 MB
-        }
+      const abortController = new AbortController();
+      const timeoutId = setTimeout(
+        () => abortController.abort(),
+        DEPENDENCY_TEST_TIMEOUT
       );
-    } catch (err) {
-      execErr = err;
-    }
 
-    const res = JSON.parse(await fs.readFile(tmpPath));
-    console.info(`testing ${cwd} done.`);
-    return res;
-  } catch (err) {
-    console.error(`Snyk failed to create a json report for ${cwd}:`, execErr);
-    throw new Error(`Testing ${cwd} failed.`);
-  } finally {
-    try {
-      await fs.rm(tmpPath);
-    } catch (error) {
-      //
+      process.stdout.write(`Testing ${name}@${version} ... `);
+
+      const response = await fetch(apiUrl, {
+        headers: {
+          Authorization: `token ${process.env.SNYK_API_KEY}`,
+        },
+        signal: abortController.signal,
+      });
+
+      clearTimeout(timeoutId);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      process.stdout.write(`Done\n`);
+      return result.issues.vulnerabilities.map((v) => {
+        // for some reason the api doesn't add this property unlike `snyk test`
+        return { ...v, name: v.package, fixedIn: v.upgradePath ?? [] };
+      });
+    } catch (err) {
+      if (err.name === 'AbortError') {
+        console.error(
+          `Request timeout for ${name}@${version} (attempt ${attempts})`
+        );
+      } else {
+        console.error(
+          `Snyk API request failed for ${name}@${version} (attempt ${attempts}):`,
+          err
+        );
+      }
+
+      if (attempts === DEPENDENCY_TEST_RETRIES) {
+        console.error(`Max retries reached for ${name}@${version}`);
+        throw new Error(`Testing ${name}@${version} failed.`);
+      }
     }
   }
 }
 
 async function main() {
+  if (!process.env.SNYK_API_KEY) {
+    throw new Error('process.env.SNYK_API_KEY is missing.');
+  }
+
   const rootPath = path.resolve(__dirname, '..');
-
-  const { workspaces } = JSON.parse(
-    await fs.readFile(path.join(rootPath, 'package.json'))
-  );
-
-  const packages = (await Promise.all(workspaces.map((w) => glob(w)))).flat();
+  const dependenciesFiles = [path.join(rootPath, '.sbom', 'dependencies.json')];
 
   const results = [];
 
-  results.push(await snykTest(rootPath));
+  for (const file of dependenciesFiles) {
+    const dependencies = JSON.parse(await fs.readFile(file, 'utf-8'));
 
-  for (const location of packages) {
-    const result = await snykTest(location);
-    if (result) {
-      results.push(result);
+    for (const dependency of dependencies) {
+      const vulnerabilities = await snykTest(dependency);
+      if (vulnerabilities && vulnerabilities.length) {
+        results.push({ vulnerabilities });
+      }
     }
   }
 
-  await fs.mkdir(path.join(rootPath, `.sbom`), { recursive: true });
-
   await fs.writeFile(
     path.join(rootPath, `.sbom/snyk-test-result.json`),
-    JSON.stringify(results.flat(), null, 2)
-  );
-
-  await execFile(
-    'npx',
-    [
-      'snyk-to-html',
-      '-i',
-      path.join(rootPath, '.sbom/snyk-test-result.json'),
-      '-o',
-      path.join(rootPath, `.sbom/snyk-test-result.html`),
-    ],
-    { cwd: rootPath }
+    JSON.stringify(results, null, 2)
   );
 }
 
