904140644998e1303f24083ad33d4e57abff63f7
Basit Chonka <basit.chonka@mongodb.com>
2024-06-03 14:23:38 +0200

    better curl

--- .evergreen/publish-dev-release.sh
+++ .evergreen/publish-dev-release.sh
@@ -14,14 +14,7 @@ if [[ "${EVERGREEN_BRANCH_NAME}" != "main" ]]; then
 fi
 
 URL="https://mciuploads.s3.amazonaws.com/${EVERGREEN_PROJECT}/compass/dev/$1"
-TMP_FILE=$(mktemp)
-CURRENT_VERSION="0.0.0-dev.0"
-
-if [ "$(curl -s -w "%{http_code}" -o "$TMP_FILE" "$URL" | tail -n1)" -eq 200 ]; then
-  CURRENT_VERSION=$(xargs < "$TMP_FILE")
-fi
-
-npm install -g semver
+CURRENT_VERSION=$(curl -sf "${URL}" || echo "0.0.0-dev.0")
 
 echo "Comparing versions: $CURRENT_VERSION and $DEV_VERSION_IDENTIFIER"
 PUBLISH_VERSION=$(npx semver "$CURRENT_VERSION" "$DEV_VERSION_IDENTIFIER" | tail -n1 | xargs)
