74c8ab98274172f381dd37f05100e5efc5244ce2
Himanshu Singh <himanshu.singhs@outlook.in>
2024-06-12 22:17:53 +0200

    chore: export required type

--- packages/compass-connections-navigation/src/index.ts
+++ packages/compass-connections-navigation/src/index.ts
@@ -2,6 +2,7 @@ export type { Actions } from './constants';
 export type {
   Connection as SidebarConnection,
   ConnectedConnection as SidebarConnectedConnection,
+  NotConnectedConnection as SidebarNotConnectedConnection,
   Database as SidebarDatabase,
   Collection as SidebarCollection,
   SidebarActionableItem as SidebarItem,
