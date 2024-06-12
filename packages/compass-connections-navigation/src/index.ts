b341f694d71138178f8f2946868c5831fdd4ce4d
Himanshu Singh <himanshu.singhs@outlook.in>
2024-06-12 11:26:55 +0200

    chore: pr feedback - remove exports that are not used in this PR

--- packages/compass-connections-navigation/src/index.ts
+++ packages/compass-connections-navigation/src/index.ts
@@ -2,14 +2,9 @@ export type { Actions } from './constants';
 export type {
   Connection as SidebarConnection,
   ConnectedConnection as SidebarConnectedConnection,
-  NotConnectedConnection as SidebarNotConnectedConnection,
   Database as SidebarDatabase,
   Collection as SidebarCollection,
   SidebarActionableItem as SidebarItem,
-  NotConnectedConnectionTreeItem as SidebarNotConnectedConnectionTreeItem,
-  ConnectedConnectionTreeItem as SidebarConnectedConnectionTreeItem,
-  DatabaseTreeItem as SidebarDatabaseTreeItem,
-  CollectionTreeItem as SidebarCollectionTreeItem,
 } from './tree-data';
 export {
   ConnectionsNavigationTree,
