ad99901ee2f128443856b3e0d4162910893d24d5
Le Roux Bodenstein <lerouxb@gmail.com>
2024-06-14 17:01:19 +0100

    use executeAsync rather than execute for async code

--- packages/compass-e2e-tests/helpers/commands/set-feature.ts
+++ packages/compass-e2e-tests/helpers/commands/set-feature.ts
@@ -6,12 +6,24 @@ export async function setFeature<K extends keyof UserPreferences>(
   name: K,
   value: UserPreferences[K]
 ): Promise<void> {
-  await browser.execute(
-    async (_name, _value) => {
+  // Using executeAsync because even though browser.execute does seem to resolve
+  // promises it looks like the script timeout only applies to executeAsync. So
+  // this can hang indefinitely until the mocha test or hook timeout is hit.
+  await browser.executeAsync(
+    (_name, _value, done) => {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
-      await require('electron').ipcRenderer.invoke('compass:save-preferences', {
-        [_name]: _value === null ? undefined : _value,
-      });
+      require('electron')
+        .ipcRenderer.invoke('compass:save-preferences', {
+          [_name]: _value === null ? undefined : _value,
+        })
+        .then((result: any) => {
+          done(result);
+        })
+        .catch((err: any) => {
+          // unfortunately done() only takes one parameter with no way to
+          // differentiate between success and error
+          done(err);
+        });
     },
     name,
     value
