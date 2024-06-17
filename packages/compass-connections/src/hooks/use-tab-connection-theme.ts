63510739bc320f344019f9b1696efec9f50491b8
Le Roux Bodenstein <lerouxb@gmail.com>
2024-06-17 17:23:31 +0100

    match the selected colour used in the sidebar

--- packages/compass-connections/src/hooks/use-tab-connection-theme.ts
+++ packages/compass-connections/src/hooks/use-tab-connection-theme.ts
@@ -52,7 +52,7 @@ export function useTabConnectionTheme(): ThemeProvider {
         '--workspace-tab-selected-border-color': activeBgColor,
         '--workspace-tab-selected-color': darkTheme
           ? palette.gray.light2
-          : palette.gray.dark2,
+          : palette.gray.dark3,
         '&:focus-visible': {
           '--workspace-tab-border-color': darkTheme
             ? palette.blue.light1
