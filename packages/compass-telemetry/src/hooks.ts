8b527d56162448ad90fea68018455348862bf66c
Paula Stachova <paula.stachova@tutanota.com>
2024-06-18 12:29:19 +0200

    Update packages/compass-telemetry/src/hooks.ts
    Co-authored-by: Maurizio Casimirri <maurizio.cas@gmail.com>

--- packages/compass-telemetry/src/hooks.ts
+++ packages/compass-telemetry/src/hooks.ts
@@ -2,6 +2,19 @@ import React from 'react';
 import type { TrackFunction } from './track';
 import { useTelemetry } from './provider';
 
+/**
+ * Hook that allows to track telemetry events as a side effect of dependencies changing.
+ *
+ * @param {function(TrackFunction): void} onChange - Function to be called when dependencies change. Receives the current track as an argument.
+ * @param {unknown[]} dependencies - Array of dependencies to watch for changes.
+ * @param {Object} [options]
+ * @param {boolean} [options.skipOnMount=false] - If true, the onChange function is skipped on the initial mount.
+ *
+ * @example
+ * useTrackOnChange((track) => {
+ *   if (isShellOpen) { track('Shell Opened') }
+ * }, [isShellOpen], { skipOnMount: true });
+ */
 export function useTrackOnChange(
   onChange: (track: TrackFunction) => void,
   dependencies: unknown[],
