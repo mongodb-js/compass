c2490fed6b7a8efac6502597a7027ff66516b224
Rhys <Anemy@users.noreply.github.com>
2024-07-02 11:21:00 -0400

    fix(serverstats): use telemetry hook in component COMPASS-8056 (#5988)

--- packages/compass-serverstats/src/components/server-stats-lists-component.jsx
+++ packages/compass-serverstats/src/components/server-stats-lists-component.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const PropTypes = require('prop-types');
-const DetailViewComponent = require('./detailview-component');
+const { DetailViewComponent } = require('./detailview-component');
 const CurrentOpComponent = require('./current-op-component');
 const TopComponent = require('./top-component');
 const CurrentOpStore = require('../stores/current-op-store');
