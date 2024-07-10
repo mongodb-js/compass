02f6c93e86f130584f494d9fb2aa1121e957ffaf
Vivian Xiao <vivian.xiao@mongodb.com>
2024-07-10 16:37:44 -0400

    autocompleter with sections flipped, codemirror/autocomplete dependency updated

--- packages/compass-editor/src/codemirror/query-autocompleter-with-history.ts
+++ packages/compass-editor/src/codemirror/query-autocompleter-with-history.ts
@@ -3,13 +3,11 @@ import {
   createQueryHistoryAutocompleter,
 } from './query-history-autocompleter';
 import { createQueryAutocompleter } from './query-autocompleter';
-
 import type {
   CompletionSource,
   CompletionContext,
 } from '@codemirror/autocomplete';
 import type { CompletionOptions } from '../autocompleter';
-
 export const createQueryWithHistoryAutocompleter = (
   recentQueries: SavedQuery[],
   options: Pick<CompletionOptions, 'fields' | 'serverVersion'> = {},
@@ -19,12 +17,41 @@ export const createQueryWithHistoryAutocompleter = (
     recentQueries,
     onApply
   );
-
   const originalQueryAutocompleter = createQueryAutocompleter(options);
+  const originalSection = { name: '', rank: 0 };
+  const historySection = { name: 'Query History', rank: 1 };
 
-  return function fullCompletions(context: CompletionContext) {
-    if (context.state.doc.toString() !== '{}')
-      return originalQueryAutocompleter(context);
-    return queryHistoryAutocompleter(context);
+  return async function fullCompletions(context: CompletionContext) {
+    const combinedOptions = [];
+    const originalCompletions = await originalQueryAutocompleter(context);
+    const historyCompletions = await queryHistoryAutocompleter(context);
+    if (originalCompletions) {
+      combinedOptions.push(
+        ...originalCompletions.options.map((option) => ({
+          ...option,
+          section: originalSection,
+        }))
+      );
+    }
+    if (historyCompletions) {
+      combinedOptions.push(
+        ...historyCompletions.options.map((option) => ({
+          ...option,
+          section: historySection,
+        }))
+      );
+    }
+
+    const nonSectionOptions = combinedOptions.filter(
+      (option) => !option.section
+    );
+    const sectionOptions = combinedOptions.filter((option) => option.section);
+    return {
+      from: Math.min(
+        historyCompletions?.from ?? context.pos,
+        originalCompletions?.from ?? context.pos
+      ),
+      options: [...nonSectionOptions, ...sectionOptions],
+    };
   };
 };
