854df6472bd98a3036cb7bc2206bd5cbe9ae91cc
Vivian Xiao <vivian.xiao@mongodb.com>
2024-07-23 17:09:03 -0400

    added completion info panel basic code highlighting

--- packages/compass-editor/src/codemirror/query-history-autocompleter.ts
+++ packages/compass-editor/src/codemirror/query-history-autocompleter.ts
@@ -2,9 +2,88 @@ import type {
   CompletionContext,
   CompletionSource,
 } from '@codemirror/autocomplete';
-import { formatDate, spacing } from '@mongodb-js/compass-components';
+import {
+  codePalette,
+  fontFamilies,
+  formatDate,
+  spacing,
+} from '@mongodb-js/compass-components';
 import { toJSString } from 'mongodb-query-parser';
 import { css } from '@mongodb-js/compass-components';
+import { parser } from '@lezer/javascript';
+import { highlightCode, tagHighlighter, tags } from '@lezer/highlight';
+
+const customHighlighter = tagHighlighter([
+  { tag: tags.number, class: 'cm-number' },
+  { tag: tags.string, class: 'cm-string' },
+  { tag: tags.name, class: 'cm-name' },
+]);
+
+const styleElement = document.createElement('style');
+styleElement.textContent = `
+  .cm-number { color: ${codePalette['light'][9]};}
+  .cm-string { color: ${codePalette['light'][7]}; }
+  .cm-name { color: ${codePalette['light'][5]}; }
+`;
+document.head.appendChild(styleElement);
+
+function createInfo2(query: SavedQuery): {
+  dom: Node;
+  destroy?: () => void;
+} {
+  const container = document.createElement('div');
+  container.className = completionInfoStyles;
+  Object.entries(query.queryProperties).forEach(([key, value]) => {
+    const formattedQuery = toJSString(value);
+    const codeDiv = document.createElement('div');
+
+    const label = document.createElement('label');
+    label.className = queryLabelStyles;
+    label.textContent = key;
+
+    const code = document.createElement('pre');
+    code.className = queryCodeStyles;
+
+    function emit(text: string, classes: string | null) {
+      const node = document.createTextNode(text);
+      if (classes) {
+        const span = document.createElement('span');
+        span.appendChild(node);
+        span.className = classes;
+        code.appendChild(span);
+      } else {
+        code.appendChild(node);
+      }
+    }
+
+    function emitBreak() {
+      code.appendChild(document.createTextNode('\n'));
+    }
+
+    if (formattedQuery) {
+      highlightCode(
+        formattedQuery,
+        parser.parse(formattedQuery),
+        customHighlighter,
+        emit,
+        emitBreak
+      );
+    }
+
+    codeDiv.append(label);
+    codeDiv.appendChild(code);
+    container.appendChild(codeDiv);
+  });
+
+  return {
+    dom: container,
+    destroy: () => {
+      while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    },
+  };
+}
 
 export type SavedQuery = {
   lastExecuted: Date;
@@ -30,7 +109,7 @@ export const createQueryHistoryAutocompleter = (
       label: createQuery(query),
       type: 'text',
       detail: formatDate(query.lastExecuted.getTime()),
-      info: () => createInfo(query).dom,
+      info: () => createInfo2(query).dom,
       apply: () => {
         onApply(query.queryProperties);
       },
@@ -59,6 +138,7 @@ const queryLabelStyles = css({
 
 const queryCodeStyles = css({
   maxHeight: '30vh',
+  fontFamily: fontFamilies.code,
 });
 
 const completionInfoStyles = css({
@@ -73,39 +153,6 @@ export function createQuery(query: SavedQuery): string {
     res += formattedQuery ? `, ${noFilterKey}${formattedQuery}` : '';
   });
   return res.slice(2, res.length);
-}
-
-function createInfo(query: SavedQuery): {
-  dom: Node;
-  destroy?: () => void;
-} {
-  const container = document.createElement('div');
-  container.className = completionInfoStyles;
-  Object.entries(query.queryProperties).forEach(([key, value]) => {
-    const formattedQuery = toJSString(value);
-    const codeDiv = document.createElement('div');
-
-    const label = document.createElement('label');
-    label.className = queryLabelStyles;
-    label.textContent = key;
-
-    const code = document.createElement('pre');
-    code.className = queryCodeStyles;
-    if (formattedQuery) code.textContent = formattedQuery;
-
-    codeDiv.append(label);
-    codeDiv.appendChild(code);
-    container.appendChild(codeDiv);
-  });
-
-  return {
-    dom: container,
-    destroy: () => {
-      while (container.firstChild) {
-        container.removeChild(container.firstChild);
-      }
-    },
-  };
 }
 
 // scales a number unscaledNum between [newScaleMin, newScaleMax]
