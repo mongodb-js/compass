bacb02e26bf51f694d04c87fb62661dff082c699
Basit Chonka <basit.chonka@mongodb.com>
2024-07-31 09:36:20 +0200

    remove todo

--- packages/explain-plan-helper/src/utils.ts
+++ packages/explain-plan-helper/src/utils.ts
@@ -1,12 +1,14 @@
 import parse, { ParseMode } from '@mongodb-js/shell-bson-parser';
 import type { Stage } from '.';
-
-export function getStageIndexFields(stage: Stage): Record<string, string> {
+//github.com/mongodb-js/compass/pull/6071/commits/5f47eebb58485a0f7cc697c317f4d5e2890ae17c
+https: export function getStageIndexFields(
+  stage: Stage
+): Record<string, string> {
   if (
     stage.stage === 'EXPRESS_IXSCAN' &&
     typeof stage.keyPattern === 'string'
   ) {
-    // TODO (SERVER-92981): For EXPRESS stages, the keyPattern is a string, which is not valid json.
+    // For EXPRESS stages, the keyPattern is a string, which is not valid json.
     try {
       const result = parse(stage.keyPattern, { mode: ParseMode.Loose });
       return typeof result === 'string' ? {} : result;
