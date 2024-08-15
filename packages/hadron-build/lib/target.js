8b195a5c51674eb8c41ddc80e1786cf52242a0fa
Anna Henningsen <anna@addaleax.net>
2024-08-15 16:08:39 +0200

    Revert "fix(build): use an older ffmpeg version when packaging (#6070)"
    This reverts commit 4da646a12b2e0ba41483ebf5a98cd74907a22e10.

--- packages/hadron-build/lib/target.js
+++ packages/hadron-build/lib/target.js
@@ -196,14 +196,7 @@ class Target {
       arch: this.arch,
       electronVersion: this.electronVersion,
       sign: null,
-      afterExtract: [(buildPath, electronVersion, platform, arch, done) => {
-        // TODO(https://github.com/electron/electron/issues/43076): electron
-        // releases are pointing to a wrong version of ffmpeg codecs right now
-        // (platform mismatch), there is a fix in progress and we should switch
-        // asap when it's available, for now just use the ffmpeg from an older
-        // version
-        ffmpegAfterExtract(buildPath, '29.4.3', platform, arch, done)
-      }]
+      afterExtract: [ffmpegAfterExtract]
     };
 
     validateBuildConfig(this.platform, this.pkg.config.hadron.build[this.platform]);
