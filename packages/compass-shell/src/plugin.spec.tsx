ebd42f1f4baa8d8e268c46e84dc290912afda918
Sergey Petushkov <petushkov.sergey@gmail.com>
2024-08-23 10:23:16 +0200

    chore(shell): convert to new helpers

--- packages/compass-shell/src/plugin.spec.tsx
+++ packages/compass-shell/src/plugin.spec.tsx
@@ -1,90 +1,31 @@
-import sinon from 'sinon';
 import React from 'react';
-import type { ReactWrapper } from 'enzyme';
-import { mount } from 'enzyme';
 import { expect } from 'chai';
-
-import { CompassShell } from './components/compass-shell';
 import { CompassShellPlugin } from './index';
-import { AppRegistryProvider } from 'hadron-app-registry';
 import {
-  ConnectionsManager,
-  ConnectionsManagerProvider,
-  ConnectionInfoProvider,
-} from '@mongodb-js/compass-connections/provider';
-import { createNoopLogger } from '@mongodb-js/compass-logging/provider';
-import {
-  ConnectionStorageProvider,
-  InMemoryConnectionStorage,
-} from '@mongodb-js/connection-storage/provider';
-
-// Wait until a component is present that is rendered in a limited number
-// of microtask queue iterations. In particular, this does *not* wait for the
-// event loop itself to progress.
-async function waitForAsyncComponent(wrapper, Component, attempts = 10) {
-  let current = 0;
-  let result;
-  while (current++ < attempts) {
-    wrapper.update();
-    result = wrapper.find(Component);
-    // Return immediately if we found something
-    if (result.length > 0 && result.exists()) {
-      return result;
-    }
-    await new Promise((r) => setTimeout(r)); // wait a microtask queue iteration
-  }
-  return result;
-}
+  cleanup,
+  renderWithActiveConnection,
+  screen,
+  waitFor,
+} from '@mongodb-js/compass-connections/test';
 
 describe('CompassShellPlugin', function () {
-  const dummyConnectionInfo = {
-    id: '1',
-    connectionOptions: {
-      connectionString: 'mongodb://localhost:27017',
-    },
-  };
-
-  const fakeDataService = {
-    getMongoClientConnectionOptions() {},
-  } as any;
-
-  const connectionsManager = new ConnectionsManager({
-    logger: createNoopLogger().log.unbound,
-  });
-
-  sinon.replace(connectionsManager, 'getDataServiceForConnection', () => {
-    return fakeDataService;
-  });
-
-  let wrapper: ReactWrapper | null;
-
   afterEach(() => {
-    wrapper?.unmount();
-    wrapper = null;
+    cleanup();
   });
 
   it('returns a renderable plugin', async function () {
-    connectionsManager['connectionStatuses'].set('1', 'connected');
-    wrapper = mount(
-      <AppRegistryProvider>
-        {/* global */}
-        <AppRegistryProvider>
-          {/* local */}
-          <ConnectionStorageProvider
-            value={new InMemoryConnectionStorage([dummyConnectionInfo])}
-          >
-            <ConnectionsManagerProvider value={connectionsManager}>
-              <ConnectionInfoProvider connectionInfoId={dummyConnectionInfo.id}>
-                <CompassShellPlugin />
-              </ConnectionInfoProvider>
-            </ConnectionsManagerProvider>
-          </ConnectionStorageProvider>
-        </AppRegistryProvider>
-      </AppRegistryProvider>
-    );
+    await renderWithActiveConnection(<CompassShellPlugin />, undefined, {
+      connectFn() {
+        return {
+          getMongoClientConnectionOptions() {
+            return { url: '', options: {} };
+          },
+        };
+      },
+    });
 
-    const component = await waitForAsyncComponent(wrapper, CompassShell);
-
-    expect(component?.exists()).to.equal(true);
+    await waitFor(() => {
+      expect(screen.getByTestId('shell-section')).to.exist;
+    });
   });
 });
