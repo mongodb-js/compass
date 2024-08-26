29701e9ddbcf31ac4b3d37111b507299677af16d
Le Roux Bodenstein <lerouxb@gmail.com>
2024-08-26 09:37:43 +0100

    deal with the fact that the modal might already be closed

--- packages/compass-e2e-tests/helpers/commands/disconnect.ts
+++ packages/compass-e2e-tests/helpers/commands/disconnect.ts
@@ -3,6 +3,10 @@ import type { CompassBrowser } from '../compass-browser';
 import delay from '../delay';
 import * as Selectors from '../selectors';
 
+import Debug from 'debug';
+
+const debug = Debug('compass-e2e-tests');
+
 async function disconnectAllWeb(browser: CompassBrowser): Promise<void> {
   const url = new URL(await browser.getUrl());
   url.pathname = '/';
@@ -50,8 +54,17 @@ async function resetForDisconnect(
 ) {
   if (await browser.$(Selectors.LGModal).isDisplayed()) {
     // close any modals that might be in the way
-    await browser.clickVisible(Selectors.LGModalClose);
-    await browser.$(Selectors.LGModal).waitForDisplayed({ reverse: true });
+    // If there's some race condition where something else is closing the modal at
+    // the same time we're trying to close the modal, then make it error out
+    // quickly so it can be ignored and we move on.
+    const waitOptions = { timeout: 2_000 };
+    try {
+      await browser.clickVisible(Selectors.LGModalClose, waitOptions);
+      await browser.$(Selectors.LGModal).waitForDisplayed({ reverse: true });
+    } catch (err) {
+      // if the modal disappears by itself in the meantime, that's fine
+      debug('ignoring', err);
+    }
   }
 
   // Collapse all the connections so that they will all hopefully fit on screen
