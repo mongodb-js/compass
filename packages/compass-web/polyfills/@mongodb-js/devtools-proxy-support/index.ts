9621025f18d00561a4b7dd96460608d88337aa67
Anna Henningsen <anna@addaleax.net>
2024-08-27 15:36:22 +0200

    fixup: lint

--- packages/compass-web/polyfills/@mongodb-js/devtools-proxy-support/index.ts
+++ packages/compass-web/polyfills/@mongodb-js/devtools-proxy-support/index.ts
@@ -1,13 +1,13 @@
-export function createAgent(): undefined {
+export function createAgent(): void {
   // The original can return 'undefined' as well
 }
-export function useOrCreateAgent(): undefined {
+export function useOrCreateAgent(): void {
   // The original can return 'undefined' as well
 }
-export function createSocks5Tunnel(): undefined {
+export function createSocks5Tunnel(): void {
   // The original can return 'undefined' as well
 }
-export function hookLogger(): undefined {
+export function hookLogger(): void {
   // no-op
 }
 export function createFetch(): never {
@@ -21,4 +21,6 @@ export function resetSystemCACache(): never {
 }
 
 // Explicitly web-compatible
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error TS resolution doesn't match webpack resolution
 export * from '@mongodb-js/devtools-proxy-support/proxy-options';
