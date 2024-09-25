1cef989dc493e43fef8d6cca2cc440a7a03b907e
Basit Chonka <basit.chonka@mongodb.com>
2024-09-25 15:52:47 +0200

    title tooltip

--- packages/compass-global-writes/src/plugin-title.tsx
+++ packages/compass-global-writes/src/plugin-title.tsx
@@ -2,10 +2,13 @@ import { connect } from 'react-redux';
 import React from 'react';
 import { type RootState, ShardingStatuses } from './store/reducer';
 import {
+  Body,
   css,
+  cx,
   Icon,
   palette,
   spacing,
+  Tooltip,
   useDarkMode,
 } from '@mongodb-js/compass-components';
 
@@ -13,6 +16,10 @@ const containerStyles = css({
   display: 'flex',
   gap: spacing[200],
   alignItems: 'center',
+});
+
+const warningIconStyles = css({
+  display: 'flex',
 });
 
 const iconStylesLight = css({
@@ -29,10 +36,36 @@ const PluginTitle = ({ showWarning }: { showWarning: boolean }) => {
     <div data-testid="global-writes-tab-title" className={containerStyles}>
       Global Writes{' '}
       {showWarning && (
-        <Icon
-          glyph="ImportantWithCircle"
-          className={darkMode ? iconStylesDark : iconStylesLight}
-        />
+        <Tooltip
+          data-testid="collection-stats-tooltip"
+          align="bottom"
+          justify="middle"
+          trigger={
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+            <span
+              onClick={() => {
+                // LG does not bubble up the click event to the parent component,
+                // so we add noop onClick and let it bubble up.
+              }}
+            >
+              <Icon
+                glyph="ImportantWithCircle"
+                className={cx(
+                  warningIconStyles,
+                  iconStylesLight,
+                  darkMode && iconStylesDark
+                )}
+              />
+            </span>
+          }
+        >
+          <Body>
+            Collections in Atlas Global Clusters with Atlas-managed sharding
+            must be configured with a compound shard key made up of both a
+            ‘location’ field and an identifier field that you provide. Please
+            configure sharding here.
+          </Body>
+        </Tooltip>
       )}
     </div>
   );
