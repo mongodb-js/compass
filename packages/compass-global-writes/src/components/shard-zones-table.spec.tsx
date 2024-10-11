3a97f006b479c9460b6b9e4352954f139c617649
Paula Stachova <paula.stachova@tutanota.com>
2024-10-11 12:48:27 +0200

    feat(global-writes): zones list expandable rows

--- packages/compass-global-writes/src/components/shard-zones-table.spec.tsx
+++ packages/compass-global-writes/src/components/shard-zones-table.spec.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { expect } from 'chai';
-import { render, screen, within } from '@mongodb-js/testing-library-compass';
+import {
+  render,
+  screen,
+  userEvent,
+  within,
+} from '@mongodb-js/testing-library-compass';
 import { ShardZonesTable } from './shard-zones-table';
 import { type ShardZoneData } from '../store/reducer';
 
@@ -26,8 +31,22 @@ describe('Compass GlobalWrites Plugin', function () {
     },
   ];
 
-  it('renders the Location name & Zone for all items', function () {
+  it('renders the Location name & Zone for top level zones', function () {
     render(<ShardZonesTable shardZones={shardZones} />);
+
+    const rows = screen.getAllByRole('row');
+    expect(rows).to.have.lengthOf(2); // 1 header, 1 item
+    expect(within(rows[1]).getByText('Germany (DE)')).to.be.visible;
+    expect(within(rows[1]).getByText('EMEA (Frankfurt)')).to.be.visible;
+  });
+
+  it('subrows can be expanded', function () {
+    render(<ShardZonesTable shardZones={shardZones} />);
+
+    const expandBtn = screen.getByRole('button', { name: 'Expand row' });
+    expect(expandBtn).to.be.visible;
+
+    userEvent.click(expandBtn);
 
     const rows = screen.getAllByRole('row');
     expect(rows).to.have.lengthOf(3); // 1 header, 2 items
--- packages/compass-global-writes/src/components/shard-zones-table.tsx
+++ packages/compass-global-writes/src/components/shard-zones-table.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useRef } from 'react';
 import {
   Table,
   TableBody,
@@ -8,6 +8,10 @@ import {
   Row,
   Cell,
   css,
+  useLeafyGreenTable,
+  type LGColumnDef,
+  type LeafyGreenTableRow,
+  flexRender,
 } from '@mongodb-js/compass-components';
 import type { ShardZoneData } from '../store/reducer';
 
@@ -16,17 +20,81 @@ const containerStyles = css({
   height: '400px',
 });
 
+interface ShardZoneRow {
+  locationName: string;
+  zone: string;
+}
+
+interface ShardZoneExpandableRow extends ShardZoneRow {
+  subRows: ShardZoneRow[];
+}
+
+const columns: Array<LGColumnDef<ShardZoneRow>> = [
+  {
+    accessorKey: 'locationName',
+    header: 'Location Name',
+  },
+  {
+    accessorKey: 'zone',
+    header: 'Zone',
+  },
+];
+
+const parseRow = ({
+  isoCode,
+  readableName,
+  zoneName,
+  zoneLocations,
+}: ShardZoneData): ShardZoneRow => ({
+  locationName: `${readableName} (${isoCode})`,
+  zone: `${zoneName} (${zoneLocations.join(', ')})`,
+});
+
 export function ShardZonesTable({
   shardZones,
 }: {
   shardZones: ShardZoneData[];
 }) {
+  const tableContainerRef = useRef<HTMLDivElement>(null);
+
+  const data = useMemo<ShardZoneExpandableRow[]>(() => {
+    const grouppedZones = shardZones.reduce<
+      Record<ShardZoneData['typeOneIsoCode'], ShardZoneExpandableRow>
+    >((groups, next) => {
+      const { typeOneIsoCode, isoCode } = next;
+      if (!groups[typeOneIsoCode]) {
+        groups[typeOneIsoCode] = { ...parseRow(next), subRows: [] };
+      }
+      if (typeOneIsoCode === isoCode) {
+        Object.assign(groups[typeOneIsoCode], next);
+      } else {
+        groups[typeOneIsoCode].subRows.push(parseRow(next));
+      }
+      return groups;
+    }, {});
+    return Object.values(grouppedZones);
+  }, [shardZones]);
+
+  const table = useLeafyGreenTable({
+    containerRef: tableContainerRef,
+    data,
+    columns,
+  });
+
+  const { rows } = table.getRowModel();
+
   return (
     // TODO(COMPASS-8336):
     // Add search
     // group zones by ShardZoneData.typeOneIsoCode
     // and display them in a single row that can be expanded
-    <Table className={containerStyles} title="Zone Mapping">
+
+    <Table
+      className={containerStyles}
+      title="Zone Mapping"
+      table={table}
+      ref={tableContainerRef}
+    >
       <TableHead isSticky>
         <HeaderRow>
           <HeaderCell>Location Name</HeaderCell>
@@ -34,20 +102,31 @@ export function ShardZonesTable({
         </HeaderRow>
       </TableHead>
       <TableBody>
-        {shardZones.map(
-          ({ readableName, zoneName, zoneLocations, isoCode }, index) => {
-            return (
-              <Row key={index}>
-                <Cell>
-                  {readableName} ({isoCode})
+        {rows.map((row: LeafyGreenTableRow<ShardZoneRow>) => (
+          <Row key={row.id} row={row}>
+            {row.getVisibleCells().map((cell) => {
+              return (
+                <Cell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </Cell>
-                <Cell>
-                  {zoneName} ({zoneLocations.join(', ')})
-                </Cell>
+              );
+            })}
+            {row.subRows.map((subRow) => (
+              <Row key={subRow.id} row={subRow}>
+                {subRow.getVisibleCells().map((cell) => {
+                  return (
+                    <Cell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </Cell>
+                  );
+                })}
               </Row>
-            );
-          }
-        )}
+            ))}
+          </Row>
+        ))}
       </TableBody>
     </Table>
   );
