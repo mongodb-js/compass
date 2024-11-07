deab82004a993b5ad112a8e7b8fdbbbc84e095cd
Paula Stachova <paula.stachova@mongodb.com>
2024-11-07 13:36:29 +0100

    add test

---
+++ packages/compass-global-writes/src/components/states/loading-error.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { expect } from 'chai';
+import { screen, userEvent } from '@mongodb-js/testing-library-compass';
+import { LoadingError } from './loading-error';
+import { renderWithStore } from '../../../tests/create-store';
+import Sinon from 'sinon';
+
+const error = 'Test failure';
+
+function renderWithProps(
+  props?: Partial<React.ComponentProps<typeof LoadingError>>
+) {
+  return renderWithStore(<LoadingError error={error} {...props} />);
+}
+
+describe('LoadingError', function () {
+  it('renders the error', async function () {
+    await renderWithProps();
+    expect(screen.getByText(error)).to.exist;
+  });
+});
