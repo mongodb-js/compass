35ca69b996279340fe308c53fbc4f903b35fa332
Daniel Echlin <daniel.echlin@mongodb.com>
2024-11-11 14:03:05 -0500

    changes

--- packages/compass-global-writes/src/plugin-title.tsx
+++ packages/compass-global-writes/src/plugin-title.tsx
@@ -30,52 +30,73 @@ const iconStylesDark = css({
   color: palette.yellow.base,
 });
 
-export const PluginTitle = ({ showWarning }: { showWarning: boolean }) => {
+export const PluginTitle = ({
+  showError,
+  showWarning,
+}: {
+  showError: boolean;
+  showWarning: boolean;
+}) => {
   const darkMode = useDarkMode();
   return (
     <div data-testid="global-writes-tab-title" className={containerStyles}>
       Global Writes{' '}
-      {showWarning && (
-        <Tooltip
-          data-testid="collection-stats-tooltip"
-          align="bottom"
-          justify="middle"
-          trigger={
-            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-            <span
-              onClick={() => {
-                // LG does not bubble up the click event to the parent component,
-                // so we add noop onClick and let it bubble up.
-              }}
-            >
-              <Icon
-                glyph="ImportantWithCircle"
-                aria-label="warning"
-                className={cx(
-                  warningIconStyles,
-                  iconStylesLight,
-                  darkMode && iconStylesDark
-                )}
-              />
-            </span>
-          }
-        >
-          <Body>
-            Collections in Atlas Global Clusters with Atlas-managed sharding
-            must be configured with a compound shard key made up of both a
-            &apos;location&apos; field and an identifier field that you provide.
-            Please configure sharding here.
-          </Body>
-        </Tooltip>
-      )}
+      {showError ||
+        (showWarning && (
+          <Tooltip
+            data-testid="collection-stats-tooltip"
+            align="bottom"
+            justify="middle"
+            trigger={
+              // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+              <span
+                onClick={() => {
+                  // LG does not bubble up the click event to the parent component,
+                  // so we add noop onClick and let it bubble up.
+                }}
+              >
+                <Icon
+                  glyph={showWarning ? 'Warning' : 'ImportantWithCircle'}
+                  aria-label="warning"
+                  className={cx(
+                    warningIconStyles,
+                    iconStylesLight,
+                    darkMode && iconStylesDark
+                  )}
+                />
+              </span>
+            }
+          >
+            <Body>
+              Collections in Atlas Global Clusters with Atlas-managed sharding
+              must be configured with a compound shard key made up of both a
+              &apos;location&apos; field and an identifier field that you
+              provide. Please configure sharding here.
+            </Body>
+          </Tooltip>
+        ))}
     </div>
   );
 };
 
 export const GlobalWritesTabTitle = connect(
-  ({ managedNamespace, status }: RootState) => ({
-    showWarning:
-      status !== ShardingStatuses.SHARD_KEY_CORRECT &&
-      status !== ShardingStatuses.NOT_READY,
-  })
+  ({ managedNamespace, status }: RootState) => {
+    const errorStatuses = [
+      ShardingStatuses.LOADING_ERROR,
+      ShardingStatuses.SHARDING_ERROR,
+      ShardingStatuses.SHARD_KEY_MISMATCH,
+      ShardingStatuses.INVALID_SHARD_KEY,
+    ];
+    const okStatuses = [
+      ShardingStatuses.NOT_READY,
+      ShardingStatuses.SHARD_KEY_CORRECT,
+    ];
+    const showError = errorStatuses.includes(status);
+    const showWarning = !showError && !okStatuses.includes(status);
+
+    return {
+      showError,
+      showWarning,
+    };
+  }
 )(PluginTitle);
