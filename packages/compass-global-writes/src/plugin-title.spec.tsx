71164096618f28cad7646dab27b5e11f0ffde0ef
djechlin-mongodb <132293929+djechlin-mongodb@users.noreply.github.com>
2024-11-19 10:31:03 -0500

    fix(compass-global-writes): show error on all invalid statuses COMPASS-8451 (#6472)
    * changes
    
    * basic button
    
    * changes
    
    * changes
    
    * spec
    
    * changes
    
    * warning error aria and color
    
    * changes
    
    * shard key invalid
    
    * coerce use dark mode
    
    * default not basic
    
    * managednamespace unused
    
    * fix test
    
    * empty
    
    * empty again
    
    * swap(red, yellow)

--- packages/compass-global-writes/src/plugin-title.spec.tsx
+++ packages/compass-global-writes/src/plugin-title.spec.tsx
@@ -4,13 +4,19 @@ import { PluginTitle } from './plugin-title';
 import { render, screen } from '@mongodb-js/testing-library-compass';
 
 describe('PluginTitle', function () {
-  it('Renders a warning', function () {
-    render(<PluginTitle showWarning={true} />);
+  it('Renders a warning when showError', function () {
+    render(<PluginTitle showError={true} showWarning={false} />);
     expect(screen.getByLabelText('warning')).to.be.visible;
   });
 
+  it("Renders an 'important' warning when showWarning", function () {
+    render(<PluginTitle showError={false} showWarning={true} />);
+    expect(screen.getByLabelText('important')).to.be.visible;
+  });
+
   it('Does not render a warning', function () {
-    render(<PluginTitle showWarning={false} />);
+    render(<PluginTitle showError={false} showWarning={false} />);
     expect(screen.queryByLabelText('warning')).not.to.exist;
+    expect(screen.queryByLabelText('important')).not.to.exist;
   });
 });
