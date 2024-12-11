5e8935ae966ae2f07f847f992ecf750d5255ced2
Kræn Hansen <kraen.hansen@mongodb.com>
2024-12-11 10:47:09 +0100

    Fix type warnings for onClick handlers

--- packages/compass-components/src/components/actions/item-action-menu.tsx
+++ packages/compass-components/src/components/actions/item-action-menu.tsx
@@ -61,7 +61,7 @@ export function ItemActionMenu<Action extends string>({
   const menuTriggerRef = useRef<HTMLButtonElement | null>(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const onClick = useCallback(
+  const onClick: React.MouseEventHandler<HTMLElement> = useCallback(
     (evt) => {
       evt.stopPropagation();
       if (evt.currentTarget.dataset.menuitem) {
@@ -69,7 +69,11 @@ export function ItemActionMenu<Action extends string>({
         // Workaround for https://jira.mongodb.org/browse/PD-1674
         menuTriggerRef.current?.focus();
       }
-      onAction(evt.currentTarget.dataset.action);
+      const actionName = evt.currentTarget.dataset.action;
+      if (typeof actionName !== 'string') {
+        throw new Error('Expected element to have a "data-action" attribute');
+      }
+      onAction(actionName as Action);
     },
     [onAction]
   );
