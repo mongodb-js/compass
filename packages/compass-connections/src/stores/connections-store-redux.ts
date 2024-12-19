59b76e5196215721cc02709c99a13f0d4143f466
Kræn Hansen <kraen.hansen@mongodb.com>
2024-12-19 23:00:04 +0100

    Propagate from connections-navigation action through main process IPC

--- packages/compass-connections/src/stores/connections-store-redux.ts
+++ packages/compass-connections/src/stores/connections-store-redux.ts
@@ -1507,6 +1507,13 @@ function isAtlasStreamsInstance(
   }
 }
 
+export const connectInNewWindow =
+  (connectionInfo: ConnectionInfo): ConnectionsThunkAction<void> =>
+  (_dispatch, _getState, { globalAppRegistry }) => {
+    // TODO: Consider building a connection string?
+    globalAppRegistry.emit('connect-in-new-window', connectionInfo.id);
+  };
+
 export const connect = (
   connectionInfo: ConnectionInfo
 ): ConnectionsThunkAction<
