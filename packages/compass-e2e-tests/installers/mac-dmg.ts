53d549b5f298219a13ac42749d847aa7c2830210
Le Roux Bodenstein <lerouxb@gmail.com>
2024-12-24 10:55:59 +0000

    more fixes and debugging

--- packages/compass-e2e-tests/installers/mac-dmg.ts
+++ packages/compass-e2e-tests/installers/mac-dmg.ts
@@ -1,4 +1,5 @@
-import { existsSync } from 'fs';
+import path from 'path';
+import { existsSync, promises as fs } from 'fs';
 import type { InstalledAppInfo, Package } from './types';
 import { execute } from './helpers';
 
@@ -23,7 +24,25 @@ export async function installMacDMG(
     await execute('hdiutil', ['detach', `/Volumes/${appName}`]);
   }
 
+  // get debug output so we can see that it copied everything with the correct
+  // permissions
   await execute('ls', ['-laR', `/Applications/${appName}.app`]);
+
+  // see if the executable will run without being quarantined or similar
+  await execute(`/Applications/${appName}.app/Contents/MacOS/${appName}`, [
+    '--version',
+  ]);
+
+  // see if the GUI works
+  const logPath = path.resolve(__dirname, '..', '.log');
+  await fs.mkdir(logPath);
+  const screenshotPath = path.resolve(
+    __dirname,
+    '..',
+    '.log',
+    'screenshot.png'
+  );
+  await execute(`screencapture`, [screenshotPath]);
 
   return Promise.resolve({
     appName,
