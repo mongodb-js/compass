4b37f6b3387dff7ddc947fb0e4298e8fffdeccc7
Kræn Hansen <kraen.hansen@mongodb.com>
2025-01-16 09:46:37 +0100

    Add sandboxing and refactored existing code

--- packages/compass-e2e-tests/installers/helpers.ts
+++ packages/compass-e2e-tests/installers/helpers.ts
@@ -1,45 +1,19 @@
-import { spawn } from 'child_process';
-import type { SpawnOptions } from 'child_process';
+import assert from 'node:assert/strict';
+import { spawnSync, type SpawnOptions } from 'node:child_process';
 
 export function execute(
   command: string,
   args: string[],
   options?: SpawnOptions
-): Promise<void> {
-  return new Promise((resolve, reject) => {
-    console.log(command, ...args);
-    const p = spawn(command, args, {
-      stdio: 'inherit',
-      ...options,
-    });
-    p.on('error', (err: any) => {
-      reject(err);
-    });
-    p.on('close', (code: number | null, signal: NodeJS.Signals | null) => {
-      if (code !== null) {
-        if (code === 0) {
-          resolve();
-        } else {
-          reject(
-            new Error(`${command} ${args.join(' ')} exited with code ${code}`)
-          );
-        }
-      } else {
-        if (signal !== null) {
-          reject(
-            new Error(
-              `${command} ${args.join(' ')} exited with signal ${signal}`
-            )
-          );
-        } else {
-          // shouldn't happen
-          reject(
-            new Error(
-              `${command} ${args.join(' ')} exited with no code or signal`
-            )
-          );
-        }
-      }
-    });
+) {
+  const { status, signal } = spawnSync(command, args, {
+    stdio: 'inherit',
+    ...options,
   });
+  assert(
+    status === 0 && signal === null,
+    `${command} ${args.join(' ')} exited with (status = ${
+      status || 'null'
+    }, signal = ${signal || 'null'})`
+  );
 }
