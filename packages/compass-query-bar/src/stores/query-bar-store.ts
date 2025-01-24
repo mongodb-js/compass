84ca2760dfcc64ef3ab60e0918fea011dd38feaa
Paula Stachova <paula.stachova@mongodb.com>
2025-01-24 16:56:11 +0100

    must've been an accident

--- packages/compass-query-bar/src/stores/query-bar-store.ts
+++ packages/compass-query-bar/src/stores/query-bar-store.ts
@@ -86,6 +86,11 @@ export type QueryBarExtraArgs = {
 export type QueryBarThunkDispatch<A extends AnyAction = AnyAction> =
   ThunkDispatch<RootState, QueryBarExtraArgs, A>;
 
+export type QueryBarThunkAction<
+  R,
+  A extends AnyAction = AnyAction
+> = ThunkAction<R, RootState, QueryBarExtraArgs, A>;
+
 export function configureStore(
   initialState: Partial<RootState['queryBar']> = {},
   services: QueryBarExtraArgs
