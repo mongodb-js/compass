db27b01711fcd86bfc72f33265ff3852e41be3cf
Daniel Echlin <daniel.echlin@mongodb.com>
2025-02-02 22:08:42 -0500

    remove a selectable values ref

--- packages/compass-preferences-model/src/provider.ts
+++ packages/compass-preferences-model/src/provider.ts
@@ -10,10 +10,6 @@ export {
 } from './utils';
 export { capMaxTimeMSAtPreferenceLimit } from './maxtimems';
 export { featureFlags } from './feature-flags';
-export {
-  getSettingDescription,
-  getSettingSelectableValues,
-  SORT_ORDER_VALUES,
-} from './preferences-schema';
+export { getSettingDescription, SORT_ORDER_VALUES } from './preferences-schema';
 export type { AllPreferences, SORT_ORDERS } from './preferences-schema';
 export type { DevtoolsProxyOptions } from '@mongodb-js/devtools-proxy-support';
