29090f47bb769e2d995f7765c8ff7621a84cd174
Le Roux Bodenstein <lerouxb@gmail.com>
2025-02-07 07:49:40 +0000

    feat(smoke-tests): test updating from the latest release to the newly packaged app COMPASS-8532 COMPASS-8535 (#6669)
    * test updating from the latest release to the newly packaged one
    
    * also run this in CI
    
    * better type assertions
    
    * auto-updating TO doesn't support windows yet because we don't have .exe installer support
    
    * the installer to use for the latest release is not necessarily the same as the package
    
    * check the text in the toasts
    
    * refactor
    
    * Update packages/compass-smoke-tests/src/releases.ts
    
    Co-authored-by: Kræn Hansen <kraen.hansen@mongodb.com>
    
    ---------
    
    Co-authored-by: Kræn Hansen <kraen.hansen@mongodb.com>

---
+++ packages/compass-smoke-tests/src/tests/auto-update-to.ts
@@ -0,0 +1,107 @@
+import assert from 'node:assert/strict';
+import fs from 'node:fs';
+import { type SmokeTestsContext } from '../context';
+import { getInstaller } from '../installers';
+import { createSandbox } from '../directories';
+import { getTestSubjectDetails } from '../test-subject';
+import { executeAsync } from '../execute';
+import { getLatestRelease, getLatestReleaseKindByKind } from '../releases';
+import { startAutoUpdateServer } from './update-server';
+
+export async function testAutoUpdateTo(context: SmokeTestsContext) {
+  assert(
+    context.bucketKeyPrefix !== undefined,
+    'Bucket key prefix is needed to download'
+  );
+
+  const sandboxPath = createSandbox();
+  const {
+    kind,
+    appName,
+    autoUpdatable,
+    buildInfo: { channel, version },
+  } = getTestSubjectDetails({ ...context, sandboxPath });
+
+  try {
+    const install = getInstaller(getLatestReleaseKindByKind(kind));
+    const filepath = await getLatestRelease(
+      channel,
+      context.arch,
+      kind,
+      context.forceDownload
+    );
+
+    const { appPath, uninstall } = install({
+      appName,
+      filepath,
+      destinationPath: sandboxPath,
+    });
+
+    try {
+      process.env.PORT = '0'; // dynamic port
+
+      if (channel === 'dev') {
+        process.env.DEV_RELEASE = JSON.stringify({
+          version: version,
+          bucket_key_prefix: context.bucketKeyPrefix,
+        });
+      } else {
+        process.env.PUBLISHED_RELEASES = JSON.stringify({
+          name: version,
+          body: version,
+          bucket_key_prefix: context.bucketKeyPrefix,
+        });
+      }
+
+      const server = await startAutoUpdateServer();
+
+      const address = server.address();
+      assert(typeof address === 'object' && address !== null);
+      const port = address.port;
+      const HADRON_AUTO_UPDATE_ENDPOINT_OVERRIDE = `http://localhost:${port}`;
+
+      try {
+        // must be async because the update server is running in the same process
+        await executeAsync(
+          'npm',
+          [
+            'run',
+            '--unsafe-perm',
+            'test-packaged',
+            '--workspace',
+            'compass-e2e-tests',
+            '--',
+            '--test-filter=auto-update',
+          ],
+          {
+            // We need to use a shell to get environment variables setup correctly
+            shell: true,
+            env: {
+              ...process.env,
+              HADRON_AUTO_UPDATE_ENDPOINT_OVERRIDE,
+              AUTO_UPDATE_UPDATABLE: (!!autoUpdatable).toString(),
+              TEST_NAME: 'auto-update-to',
+              EXPECTED_UPDATE_VERSION: version,
+              COMPASS_APP_NAME: appName,
+              COMPASS_APP_PATH: appPath,
+            },
+          }
+        );
+      } finally {
+        console.log('Stopping auto-update server');
+        server.close();
+        delete process.env.DEV_RELEASE;
+        delete process.env.PUBLISHED_RELEASES;
+      }
+    } finally {
+      await uninstall();
+    }
+  } finally {
+    if (context.skipCleanup) {
+      console.log(`Skipped cleaning up sandbox: ${sandboxPath}`);
+    } else {
+      console.log(`Cleaning up sandbox: ${sandboxPath}`);
+      fs.rmSync(sandboxPath, { recursive: true });
+    }
+  }
+}
---
+++ packages/compass-smoke-tests/src/tests/time-to-first-query.ts
@@ -0,0 +1,57 @@
+import fs from 'node:fs';
+import { type SmokeTestsContext } from '../context';
+import { execute } from '../execute';
+import { getInstaller } from '../installers';
+import { createSandbox } from '../directories';
+import { getTestSubject } from '../test-subject';
+
+export async function testTimeToFirstQuery(context: SmokeTestsContext) {
+  const sandboxPath = createSandbox();
+  const { kind, appName, filepath } = await getTestSubject({
+    ...context,
+    sandboxPath,
+  });
+
+  try {
+    const install = getInstaller(kind);
+
+    const { appPath, uninstall } = install({
+      appName,
+      filepath,
+      destinationPath: sandboxPath,
+    });
+
+    try {
+      execute(
+        'npm',
+        [
+          'run',
+          '--unsafe-perm',
+          'test-packaged',
+          '--workspace',
+          'compass-e2e-tests',
+          '--',
+          '--test-filter=time-to-first-query',
+        ],
+        {
+          // We need to use a shell to get environment variables setup correctly
+          shell: true,
+          env: {
+            ...process.env,
+            COMPASS_APP_NAME: appName,
+            COMPASS_APP_PATH: appPath,
+          },
+        }
+      );
+    } finally {
+      await uninstall();
+    }
+  } finally {
+    if (context.skipCleanup) {
+      console.log(`Skipped cleaning up sandbox: ${sandboxPath}`);
+    } else {
+      console.log(`Cleaning up sandbox: ${sandboxPath}`);
+      fs.rmSync(sandboxPath, { recursive: true });
+    }
+  }
+}
