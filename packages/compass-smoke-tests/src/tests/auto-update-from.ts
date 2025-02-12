110b4a68399465c052b914ec329bb27cca30e934
Kræn Hansen <kraen.hansen@mongodb.com>
2025-02-12 17:04:00 +0100

    Cleanup on exit

--- packages/compass-smoke-tests/src/tests/auto-update-from.ts
+++ packages/compass-smoke-tests/src/tests/auto-update-from.ts
@@ -19,66 +19,57 @@ export async function testAutoUpdateFrom(context: SmokeTestsContext) {
 
   const { kind, filepath, autoUpdatable } = subject;
 
-  try {
-    const install = getInstaller(kind);
+  const install = getInstaller(kind);
 
-    const { appName, appPath, uninstall } = install({
-      ...subject,
-      filepath,
-      sandboxPath,
-    });
+  const { appName, appPath, uninstall } = install({
+    ...subject,
+    filepath,
+    sandboxPath,
+  });
+
+  try {
+    process.env.PORT = '0'; // dynamic port
+    process.env.UPDATE_CHECKER_ALLOW_DOWNGRADES = 'true';
+
+    const server = await startAutoUpdateServer();
+
+    const address = server.address();
+    assert(typeof address === 'object' && address !== null);
+    const port = address.port;
+    const HADRON_AUTO_UPDATE_ENDPOINT_OVERRIDE = `http://localhost:${port}`;
 
     try {
-      process.env.PORT = '0'; // dynamic port
-      process.env.UPDATE_CHECKER_ALLOW_DOWNGRADES = 'true';
-
-      const server = await startAutoUpdateServer();
-
-      const address = server.address();
-      assert(typeof address === 'object' && address !== null);
-      const port = address.port;
-      const HADRON_AUTO_UPDATE_ENDPOINT_OVERRIDE = `http://localhost:${port}`;
-
-      try {
-        // must be async because the update server is running in the same process
-        await executeAsync(
-          'npm',
-          [
-            'run',
-            '--unsafe-perm',
-            'test-packaged',
-            '--workspace',
-            'compass-e2e-tests',
-            '--',
-            '--test-filter=auto-update',
-          ],
-          {
-            // We need to use a shell to get environment variables setup correctly
-            shell: true,
-            env: {
-              ...process.env,
-              HADRON_AUTO_UPDATE_ENDPOINT_OVERRIDE,
-              AUTO_UPDATE_UPDATABLE: (!!autoUpdatable).toString(),
-              TEST_NAME: 'auto-update-from',
-              COMPASS_APP_NAME: appName,
-              COMPASS_APP_PATH: appPath,
-            },
-          }
-        );
-      } finally {
-        debug('Stopping auto-update server');
-        server.close();
-        delete process.env.UPDATE_CHECKER_ALLOW_DOWNGRADES;
-      }
+      // must be async because the update server is running in the same process
+      await executeAsync(
+        'npm',
+        [
+          'run',
+          '--unsafe-perm',
+          'test-packaged',
+          '--workspace',
+          'compass-e2e-tests',
+          '--',
+          '--test-filter=auto-update',
+        ],
+        {
+          // We need to use a shell to get environment variables setup correctly
+          shell: true,
+          env: {
+            ...process.env,
+            HADRON_AUTO_UPDATE_ENDPOINT_OVERRIDE,
+            AUTO_UPDATE_UPDATABLE: (!!autoUpdatable).toString(),
+            TEST_NAME: 'auto-update-from',
+            COMPASS_APP_NAME: appName,
+            COMPASS_APP_PATH: appPath,
+          },
+        }
+      );
     } finally {
-      await uninstall();
+      debug('Stopping auto-update server');
+      server.close();
+      delete process.env.UPDATE_CHECKER_ALLOW_DOWNGRADES;
     }
   } finally {
-    if (context.skipCleanup) {
-      debug(`Skipped cleaning up sandbox: ${sandboxPath}`);
-    } else {
-      debug(`Cleaning up sandbox: ${sandboxPath}`);
-      fs.rmSync(sandboxPath, { recursive: true });
-    }
+    await uninstall();
   }
 }
