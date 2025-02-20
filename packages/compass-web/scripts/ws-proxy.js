9cb1c315898dc46cdc1ed5cf5fc3a9d0a23c48ce
Niva Vaani Sivakumar <niva.sivakumar@mongodb.com>
2025-02-20 11:35:58 -0500

    binary messages not working

--- packages/compass-web/scripts/ws-proxy.js
+++ packages/compass-web/scripts/ws-proxy.js
@@ -29,9 +29,19 @@ function createWebSocketProxy(port = 1337, logger = console) {
         socket.write(data, 'binary');
       } else {
         // First message before socket is created is with connection info
-        const { tls: useSecureConnection, ...connectOptions } = JSON.parse(
-          data.toString()
-        );
+        const dataView = new Uint8Array(data);
+        const messageByte = dataView[0];
+        if (messageByte !== 0x01) {
+          logger.log('first message should have type byte,(%s)', evt, err);
+          ws.close(evt === 'close' ? 1001 : 1011);
+        }
+        const jsonBytes = dataView.subarray(1);
+        const jsonStr = new TextDecoder('utf-8').decode(jsonBytes);
+        console.log('pre message received on backend: ', jsonStr);
+        const { tls: useSecureConnection, ...connectOptions } =
+          JSON.parse(jsonStr);
+
+        // const { tls: useSecureConnection, ...connectOptions } = JSON.parse(data.toString);
         logger.log(
           'setting up new%s connection to %s:%s',
           useSecureConnection ? ' secure' : '',
@@ -61,10 +71,22 @@ function createWebSocketProxy(port = 1337, logger = console) {
             connectOptions.port
           );
           socket.setTimeout(0);
-          ws.send(JSON.stringify({ preMessageOk: 1 }));
+          const utf8Encoder = new TextEncoder();
+          const message = JSON.stringify({ preMessageOk: 1 });
+          const utf8Array = utf8Encoder.encode(message);
+
+          const encoded = new Uint8Array(utf8Array.length + 1);
+          encoded[0] = 0x01;
+          encoded.set(utf8Array, 1);
+          ws.send(encoded);
         });
         socket.on('data', async (data) => {
-          ws.send(data);
+          const encoded = new Uint8Array(data.length + 1);
+          encoded[0] = 0x02;
+          encoded.set(data, 1);
+          // ws.send(encoded);
+          logger.log(encoded);
+          ws.send(encoded);
         });
       }
     });
