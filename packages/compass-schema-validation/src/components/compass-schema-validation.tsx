6dd70c2f488ba82da09c6691bd1a1334ec886b83
Rhys <Anemy@users.noreply.github.com>
2025-02-21 11:21:08 -0500

    fix(schema-validation): document preview overflow styles COMPASS-8997 (#6739)

--- packages/compass-schema-validation/src/components/compass-schema-validation.tsx
+++ packages/compass-schema-validation/src/components/compass-schema-validation.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import ValidationStates from './validation-states';
-import { css, WorkspaceContainer } from '@mongodb-js/compass-components';
-
-const styles = css({
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: 1,
-  height: '100%',
-});
+import { WorkspaceContainer } from '@mongodb-js/compass-components';
 
 /**
  * The core schema validation component.
  */
 export function CompassSchemaValidation() {
   return (
-    <div className={styles} data-testid="compass-schema-validation">
-      <WorkspaceContainer>
-        <ValidationStates />
-      </WorkspaceContainer>
-    </div>
+    <WorkspaceContainer data-testid="compass-schema-validation">
+      <ValidationStates />
+    </WorkspaceContainer>
   );
 }
