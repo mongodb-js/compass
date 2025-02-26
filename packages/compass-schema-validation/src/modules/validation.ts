3b82138e485e1df1c37ccf95a4c00788dbf06160
Rhys Howell <rhys.howell@mongodb.com>
2025-02-26 13:06:03 -0500

    feat(validation): add validation template

--- packages/compass-schema-validation/src/modules/validation.ts
+++ packages/compass-schema-validation/src/modules/validation.ts
@@ -16,6 +16,37 @@ export type ValidationLevel = 'off' | 'moderate' | 'strict';
  */
 const PREFIX = 'validation' as const;
 
+const VALIDATION_TEMPLATE = `/**
+ * This is a starter template for a schema validation rule for a collection.
+ * More information on schema validation rules can be found at:
+ * https://www.mongodb.com/docs/manual/core/schema-validation/
+ */
+{ 
+	$jsonSchema: {
+		title: "Library.books", 
+		bsonType: "object", 
+		required: ["fieldname1", "fieldname2"],
+		properties: {
+			fieldname1: { 
+				bsonType: "string",
+				description: "Fieldname1 must be a string",
+			},
+fieldname2: { 
+				bsonType: "int",
+				description: "Fieldname2 must be an integer",
+			}, 
+			arrayFieldName: {
+				bsonType: "array",
+				items: {
+bsonType: "string"
+},
+description: "arrayFieldName must be an array of strings"
+			},
+		}
+	}
+}
+`;
+
 /**
  * Validator changed action name.
  */
@@ -115,7 +146,7 @@ export interface ValidationState extends Validation {
  * The initial state.
  */
 export const INITIAL_STATE: ValidationState = {
-  validator: '',
+  validator: VALIDATION_TEMPLATE,
   validationAction: 'error',
   validationLevel: 'strict',
   isChanged: false,
@@ -390,7 +421,10 @@ export const fetchValidation = (namespace: {
         const validation = validationFromCollection(null, collInfo ?? {});
 
         if (!validation.validator) {
-          const newValidation = { ...validation, validator: '{}' };
+          const newValidation = {
+            ...validation,
+            validator: VALIDATION_TEMPLATE,
+          };
           dispatch(validationFetched(newValidation));
           dispatch(isLoadedChanged(true));
           return;
