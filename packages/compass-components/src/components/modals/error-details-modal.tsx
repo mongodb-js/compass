77eaa776e19f264049de697f3d63f6d327136ec3
Paula Stachova <paula.stachova@mongodb.com>
2025-02-27 11:15:37 +0100

    include file

---
+++ packages/compass-components/src/components/modals/error-details-modal.tsx
@@ -0,0 +1,58 @@
+import React, { useMemo } from 'react';
+
+import { css, cx } from '@leafygreen-ui/emotion';
+
+import { Modal } from './modal';
+import { Button, Code, ModalFooter } from '../leafygreen';
+import { ModalBody } from './modal-body';
+import { ModalHeader } from './modal-header';
+import { ModalFooterButton } from './modal-footer-button';
+
+const backButtonStyles = css({
+  float: 'left',
+});
+
+type ModalProps = React.ComponentProps<typeof Modal>;
+type ErrorDetailsModalProps = Omit<ModalProps, 'children'> & {
+  title?: string;
+  subtitle?: string;
+  details?: Record<string, unknown>;
+  closeAction: 'close' | 'back';
+  onClose: () => void;
+};
+
+function ErrorDetailsModal({
+  title = 'Error details',
+  subtitle,
+  details,
+  closeAction,
+  onClose,
+  ...modalProps
+}: ErrorDetailsModalProps) {
+  const prettyDetails = useMemo(() => {
+    return JSON.stringify(details, undefined, 2);
+  }, [details]);
+  return (
+    <Modal setOpen={onClose} {...modalProps}>
+      <ModalHeader title={title} subtitle={subtitle} />
+      <ModalBody>
+        <Code language="json">{prettyDetails}</Code>
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          data-testid="close-button"
+          onClick={() => {
+            console.log('onClick');
+            onClose();
+          }}
+          variant="default"
+          className={cx(closeAction === 'back' && backButtonStyles)}
+        >
+          {closeAction === 'back' ? 'Back' : 'Close'}
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+}
+
+export { ErrorDetailsModal };
