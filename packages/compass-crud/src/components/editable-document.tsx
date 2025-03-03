0dad9f075fe21f24aec807b36ff37ab957af5034
Paula Stachova <paula.stachova@mongodb.com>
2025-03-03 19:15:44 +0100

    wip

--- packages/compass-crud/src/components/editable-document.tsx
+++ packages/compass-crud/src/components/editable-document.tsx
@@ -279,12 +279,12 @@ class EditableDocument extends React.Component<
         onCancel={() => {
           this.handleCancel();
         }}
-        // onOpenErrorDetails{() => {
-        //   this.props.openErrorDetailsDialog?.({
-        //     details: '// TODO erro details',
-        //     closeAction: 'close',
-        //   })
-        // }}
+        onOpenErrorDetails={(details: Record<string, unknown>) => {
+          this.props.openErrorDetailsDialog?.({
+            details,
+            closeAction: 'close',
+          });
+        }}
       />
     );
   }
@@ -320,6 +320,7 @@ class EditableDocument extends React.Component<
     replaceDocument: PropTypes.func.isRequired,
     updateDocument: PropTypes.func.isRequired,
     openInsertDocumentDialog: PropTypes.func.isRequired,
+    openErrorDetailsDialog: PropTypes.func.isRequired,
     copyToClipboard: PropTypes.func.isRequired,
     showInsights: PropTypes.bool,
   };
