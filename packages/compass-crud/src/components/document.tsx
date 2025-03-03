d1cae8c62a1775eb85618150deb30faeabe114dd
Paula Stachova <paula.stachova@mongodb.com>
2025-03-03 20:10:49 +0100

    feat: error details on update COMPASS-8865

--- packages/compass-crud/src/components/document.tsx
+++ packages/compass-crud/src/components/document.tsx
@@ -43,6 +43,7 @@ const Document = (props: DocumentProps) => {
   }
 
   if (editable) {
+    console.log('PROP IS HERE', props.openErrorDetailsDialog);
     return <EditableDocument {...props} doc={doc} />;
   }
 
