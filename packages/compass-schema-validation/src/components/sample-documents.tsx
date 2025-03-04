95e406e7f70a87d32dd3f1c40ce2699bfaa3017e
Paula Stachova <paula.stachova@mongodb.com>
2025-03-04 18:02:00 +0100

    fix(compass-validation): unified preview CTA COMPASS-8862

--- packages/compass-schema-validation/src/components/sample-documents.tsx
+++ packages/compass-schema-validation/src/components/sample-documents.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
   Icon,
   Body,
@@ -7,14 +7,14 @@ import {
   spacing,
   palette,
   InlineDefinition,
-  Subtitle,
   useDarkMode,
+  Button,
+  ButtonVariant,
 } from '@mongodb-js/compass-components';
-import { SAMPLE_SIZE } from '../modules/sample-documents';
-import {
-  InvalidDocumentPreview,
-  ValidDocumentPreview,
-} from './document-preview';
+import { fetchSampleDocuments, SAMPLE_SIZE } from '../modules/sample-documents';
+import { DocumentPreview } from './document-preview';
+import { connect } from 'react-redux';
+import type { RootState } from '../modules';
 
 const SAMPLE_DEFINITION = `A sample is fetched from a sample-space of ${SAMPLE_SIZE} randomly selected documents`;
 
@@ -66,15 +66,128 @@ const documentHeadingTextStyles = css({
   color: 'inherit',
 });
 
-export function SampleDocuments() {
+const initialStateContainerStyles = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: spacing[200],
+  padding: spacing[400],
+  marginTop: spacing[400],
+  border: `1px solid ${palette.gray.light2}`,
+  borderRadius: spacing[400],
+});
+
+const initialStateContainerDarkModeStyles = css({
+  border: `1px solid ${palette.gray.dark2}`,
+});
+
+const previewHeaderStyles = css({
+  fontSize: spacing[400],
+  fontWeight: 'bold',
+  color: palette.green.dark2,
+});
+
+const previewHeaderDarkModeStyles = css({
+  color: palette.green.light2,
+});
+
+const DocumentGraphic: React.FunctionComponent = () => {
+  const darkMode = useDarkMode();
+  const fillColor = useMemo(
+    () => (darkMode ? palette.green.light1 : palette.green.base),
+    [darkMode]
+  );
+  const strokeColor = useMemo(
+    () => (darkMode ? palette.white : palette.black),
+    [darkMode]
+  );
+
+  return (
+    <svg
+      width="64"
+      height="58"
+      viewBox="0 0 64 58"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="0.5" y="1" width="63" height="56" rx="2" stroke={strokeColor} />
+      <line y1="28.5" x2="64" y2="28.5" stroke={strokeColor} />
+      <line
+        x1="8.61035"
+        y1="15.5898"
+        x2="32.9054"
+        y2="15.5898"
+        stroke={strokeColor}
+      />
+      <circle
+        cx="44.9268"
+        cy="16.0898"
+        r="2"
+        fill={fillColor}
+        stroke={strokeColor}
+      />
+      <circle
+        cx="52.8896"
+        cy="16.0898"
+        r="2"
+        fill={fillColor}
+        stroke={strokeColor}
+      />
+    </svg>
+  );
+};
+
+const InitialState: React.FC<{
+  onPreviewClick: () => void;
+}> = ({ onPreviewClick }) => {
   const darkMode = useDarkMode();
   return (
-    <div className={sampleDocumentsSectionStyles}>
-      <Subtitle>
+    <div
+      className={cx(
+        initialStateContainerStyles,
+        darkMode && initialStateContainerDarkModeStyles
+      )}
+    >
+      <DocumentGraphic />
+      <div
+        className={cx(
+          previewHeaderStyles,
+          darkMode && previewHeaderDarkModeStyles
+        )}
+      >
         <InlineDefinition definition={SAMPLE_DEFINITION}>
-          Sample documents
+          Preview your sample here
         </InlineDefinition>
-      </Subtitle>
+      </div>
+      <div>
+        This section displays one document that passed validation and one that
+        failed validation.
+      </div>
+      <Button onClick={onPreviewClick} variant={ButtonVariant.PrimaryOutline}>
+        Preview documents
+      </Button>
+    </div>
+  );
+};
+
+function SampleDocuments({
+  validDocument,
+  invalidDocument,
+  isInitialState,
+  fetchSampleDocuments,
+}: {
+  validDocument?: Record<string, unknown>;
+  invalidDocument?: Record<string, unknown>;
+  isInitialState: boolean;
+  fetchSampleDocuments: () => void;
+}) {
+  const darkMode = useDarkMode();
+
+  if (isInitialState)
+    return <InitialState onPreviewClick={fetchSampleDocuments} />;
+
+  return (
+    <div className={sampleDocumentsSectionStyles}>
       <div className={sampleDocumentsStyles}>
         {/* Documents that match the validation */}
         <div className={sampleDocumentStyles} data-testid="matching-documents">
@@ -87,7 +200,7 @@ export function SampleDocuments() {
             <Icon glyph="CheckmarkWithCircle" size="small" />
             <Body className={documentHeadingTextStyles}>Passed validation</Body>
           </div>
-          <ValidDocumentPreview />
+          <DocumentPreview document={validDocument} />
         </div>
 
         {/* Documents that do not match the validation */}
@@ -104,9 +217,22 @@ export function SampleDocuments() {
             <Icon glyph="XWithCircle" size="small" />
             <Body className={documentHeadingTextStyles}>Failed validation</Body>
           </div>
-          <InvalidDocumentPreview />
+          <DocumentPreview document={invalidDocument} />
         </div>
       </div>
     </div>
   );
 }
+
+const ConnectedSampleDocuments = connect(
+  (state: RootState) => ({
+    validDocument: state.sampleDocuments.validDocument,
+    invalidDocument: state.sampleDocuments.invalidDocument,
+    isInitialState: state.sampleDocuments.validDocumentState === 'initial',
+  }),
+  {
+    fetchSampleDocuments,
+  }
+)(SampleDocuments);
+
+export { ConnectedSampleDocuments as SampleDocuments };
