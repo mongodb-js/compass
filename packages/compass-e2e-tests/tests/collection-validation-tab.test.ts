dcdb7106af3b965d373f18dcffa7e47ba8ebc97d
Paula Stachova <paula.stachova@mongodb.com>
2025-03-07 16:45:47 +0100

    cleanup only

--- packages/compass-e2e-tests/tests/collection-validation-tab.test.ts
+++ packages/compass-e2e-tests/tests/collection-validation-tab.test.ts
@@ -58,7 +58,7 @@ describe('Collection validation tab', function () {
       await browser.setFeature('enableExportSchema', true);
     });
 
-    it.only('provides users with a button to generate rules', async function () {
+    it('provides users with a button to generate rules', async function () {
       await browser.clickVisible(Selectors.GenerateValidationRulesButton);
       const editor = browser.$(Selectors.ValidationEditor);
       await editor.waitForDisplayed();
