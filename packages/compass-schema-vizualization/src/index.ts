0f6bac520137056668a4df4cd0a53ecf24c1a1cc
Rhys Howell <rhys.howell@mongodb.com>
2025-03-26 16:47:07 -0400

    add wrapper for title

--- packages/compass-schema-vizualization/src/index.ts
+++ packages/compass-schema-vizualization/src/index.ts
@@ -1,11 +1,20 @@
-// import React from 'react';
+import React from 'react';
 import { registerHadronPlugin } from 'hadron-app-registry';
 import { PluginTitle } from './plugin-title';
 import SchemaViz from './components';
 
 export const SchemaVizualizationHadronPlugin = registerHadronPlugin({
   name: 'Schema Vizualization' as const,
-  component: SchemaViz,
+  component: function SchemaProvider({ children, ...props }) {
+    return React.createElement(
+      React.Fragment,
+      null,
+      // Cloning children with props is a workaround for reflux store.
+      React.isValidElement(children)
+        ? React.cloneElement(children, props)
+        : null
+    );
+  },
   activate(/* initialProps, services, activateHelpers */) {
     return {
       store: {},
