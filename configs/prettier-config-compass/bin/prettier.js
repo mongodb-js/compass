ee1e7ee874655b547b9400c0842aad0b44ee76bc
Sergey Petushkov <petushkov.sergey@gmail.com>
2025-03-27 13:51:52 +0100

    chore(configs): re-export eslint and prettier clis through config packages

---
+++ configs/prettier-config-compass/bin/prettier.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+'use strict';
+const path = require('path');
+if (process.argv.some((arg) => /^--(no-)?config/.test(arg))) {
+  throw new Error('--config option is not allowed');
+}
+process.argv.push('--config', path.resolve(__dirname, '..', 'index.js'));
+require('prettier/cli.js');
