bdb4e433c0e5c15b3acfc9295a47b6532b60e8b8
Kræn Hansen <kraen.hansen@mongodb.com>
2025-04-07 11:43:43 +0200

    Fix typo in test suite titles

--- packages/compass/src/main/auto-update-manager.spec.ts
+++ packages/compass/src/main/auto-update-manager.spec.ts
@@ -191,7 +191,7 @@ describe('CompassAutoUpdateManager', function () {
       sandbox.stub(autoUpdater);
     });
 
-    describe('when electron does not support plaform updates', function () {
+    describe('when electron does not support platform updates', function () {
       before(function () {
         if (supportsAutoupdates) {
           // eslint-disable-next-line no-console
@@ -233,7 +233,7 @@ describe('CompassAutoUpdateManager', function () {
       });
     });
 
-    describe('when electron supports plaform updates', function () {
+    describe('when electron supports platform updates', function () {
       before(function () {
         if (!supportsAutoupdates) {
           // eslint-disable-next-line no-console
