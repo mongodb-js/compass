548139d0ee0a1cf8081d888eae4d7513707c798d
Le Roux Bodenstein <lerouxb@gmail.com>
2025-04-22 15:38:36 +0100

    only sign when doing a dev release

--- packages/hadron-build/lib/mac-notary-service.js
+++ packages/hadron-build/lib/mac-notary-service.js
@@ -67,7 +67,7 @@ async function notarize(src, notarizeOptions) {
         '-t',
         'app',
         '-m',
-        'notarizeAndSign',
+        process.env.DEV_VERSION_IDENTIFIER ? 'sign' : 'notarizeAndSign',
         '-u',
         process.env.MACOS_NOTARY_API_URL,
         '-b',
