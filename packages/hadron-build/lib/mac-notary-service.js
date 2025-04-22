4a571779db9abb02fb11a27dd02a3d3857bf572b
Le Roux Bodenstein <lerouxb@gmail.com>
2025-04-22 15:46:42 +0100

    requester expansion rather

--- packages/hadron-build/lib/mac-notary-service.js
+++ packages/hadron-build/lib/mac-notary-service.js
@@ -67,7 +67,7 @@ async function notarize(src, notarizeOptions) {
         '-t',
         'app',
         '-m',
-        process.env.DEV_VERSION_IDENTIFIER ? 'sign' : 'notarizeAndSign',
+        process.env.REQUESTER === 'github_pr' ? 'sign' : 'notarizeAndSign',
         '-u',
         process.env.MACOS_NOTARY_API_URL,
         '-b',
