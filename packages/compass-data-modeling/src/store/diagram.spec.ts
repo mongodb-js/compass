c2792e5cc9e36ec1d1fb946e4bf669bfcd4a36c7
Paula Stachova <paula.stachova@mongodb.com>
2025-05-20 16:55:29 +0200

    finish tests

--- packages/compass-data-modeling/src/store/diagram.spec.ts
+++ packages/compass-data-modeling/src/store/diagram.spec.ts
@@ -1,6 +1,12 @@
 import { expect } from 'chai';
 import { setupStore } from '../../test/setup-store';
-import { applyEdit, openDiagram } from './diagram';
+import {
+  applyEdit,
+  getCurrentDiagramFromState,
+  openDiagram,
+  redoEdit,
+  undoEdit,
+} from './diagram';
 
 describe('Data Modeling store', function () {
   let store;
@@ -10,73 +16,55 @@ describe('Data Modeling store', function () {
   });
 
   it('openDiagram', function () {
-    const diagram = {
+    const loadedDiagram = {
       id: 'diagram-id',
       name: 'diagram-name',
       connectionId: 'connection-id',
       edits: [{ type: 'SetModel', model: {} }],
     };
 
-    store.dispatch(openDiagram(diagram));
+    store.dispatch(openDiagram(loadedDiagram));
 
-    expect(store.getState().diagram.id).to.equal(diagram.id);
-    expect(store.getState().diagram.name).to.equal(diagram.name);
-    expect(store.getState().diagram.connectionId).to.equal(
-      diagram.connectionId
-    );
-    expect(store.getState().diagram.edits.current).to.deep.equal(diagram.edits);
-  });
-
-  it('openDiagram', function () {
-    const diagram = {
-      id: 'diagram-id',
-      name: 'diagram-name',
-      connectionId: 'connection-id',
-      edits: [{ type: 'SetModel', model: {} }],
-    };
-
-    store.dispatch(openDiagram(diagram));
-
-    expect(store.getState().diagram.id).to.equal(diagram.id);
-    expect(store.getState().diagram.name).to.equal(diagram.name);
-    expect(store.getState().diagram.connectionId).to.equal(
-      diagram.connectionId
-    );
-    expect(store.getState().diagram.edits.current).to.deep.equal(diagram.edits);
+    const diagram = getCurrentDiagramFromState(store.getState());
+    expect(diagram.id).to.equal(loadedDiagram.id);
+    expect(diagram.name).to.equal(loadedDiagram.name);
+    expect(diagram.connectionId).to.equal(loadedDiagram.connectionId);
+    expect(diagram.edits).to.deep.equal(loadedDiagram.edits);
   });
 
   describe('applyEdit', function () {
     it('should apply a valid SetModel edit', function () {
-      const diagram = {
+      const loadedDiagram = {
         id: 'diagram-id',
         name: 'diagram-name',
         connectionId: 'connection-id',
         edits: [{ type: 'SetModel', model: {} }],
       };
 
-      store.dispatch(openDiagram(diagram));
+      store.dispatch(openDiagram(loadedDiagram));
 
       const newModel = { collections: [], relationships: [] };
       store.dispatch(applyEdit({ type: 'SetModel', model: newModel }));
 
-      const state = store.getState();
-      expect(state.diagram.editErrors).to.be.undefined;
-      expect(state.diagram.edits.current[0]).to.deep.equal(diagram.edits[0]);
-      expect(state.diagram.edits.current[1]).to.deep.include({
+      const diagram = getCurrentDiagramFromState(store.getState());
+      expect(diagram.editErrors).to.be.undefined;
+      expect(diagram.edits).to.have.length(2);
+      expect(diagram.edits[0]).to.deep.equal(diagram.edits[0]);
+      expect(diagram.edits[1]).to.deep.include({
         type: 'SetModel',
         model: newModel,
       });
     });
 
     it('should apply a valid AddRelationship edit', function () {
-      const diagram = {
+      const loadedDiagram = {
         id: 'diagram-id',
         name: 'diagram-name',
         connectionId: 'connection-id',
         edits: [{ type: 'SetModel', model: {} }],
       };
 
-      store.dispatch(openDiagram(diagram));
+      store.dispatch(openDiagram(loadedDiagram));
 
       const newRelationship = {
         id: 'relationship1',
@@ -98,39 +86,75 @@ describe('Data Modeling store', function () {
         applyEdit({ type: 'AddRelationship', relationship: newRelationship })
       );
 
-      const state = store.getState();
-      expect(state.diagram.editErrors).to.be.undefined;
-      expect(state.diagram.edits.current[0]).to.deep.equal(diagram.edits[0]);
-      expect(state.diagram.edits.current[1]).to.deep.include({
+      const diagram = getCurrentDiagramFromState(store.getState());
+      expect(diagram.editErrors).to.be.undefined;
+      expect(diagram.edits).to.have.length(2);
+      expect(diagram.edits[0]).to.deep.equal(diagram.edits[0]);
+      expect(diagram.edits[1]).to.deep.include({
         type: 'AddRelationship',
         relationship: newRelationship,
       });
     });
 
     it.only('should not apply invalid AddRelationship edit', function () {
-      const diagram = {
+      const loadedDiagram = {
         id: 'diagram-id',
         name: 'diagram-name',
         connectionId: 'connection-id',
         edits: [{ type: 'SetModel', model: {} }],
       };
 
-      store.dispatch(openDiagram(diagram));
+      store.dispatch(openDiagram(loadedDiagram));
 
-      const newRelationship = {
-        id: 'relationship1',
-        isInferred: false,
+      const edit = {
+        type: 'AddRelationship',
+        relationship: {
+          id: 'relationship1',
+          isInferred: false,
+        },
       };
-      store.dispatch(
-        applyEdit({ type: 'AddRelationship', relationship: newRelationship })
-      );
+      store.dispatch(applyEdit(edit));
 
-      const state = store.getState();
-      expect(state.diagram.editErrors).to.have.length(1);
-      expect(state.diagram.editErrors[0]).to.equal(
-        "'relationship,relationship' is required"
-      );
-      expect(state.diagram.edits.current).to.deep.equal(diagram.edits);
+      const editErrors = store.getState().diagram.editErrors;
+      expect(editErrors).to.have.length(1);
+      expect(editErrors[0]).to.equal("'relationship,relationship' is required");
+      const diagram = getCurrentDiagramFromState(store.getState());
+      expect(diagram.edits).to.deep.equal(diagram.edits);
     });
+  });
+
+  it('undo/redo', function () {
+    const loadedDiagram = {
+      id: 'diagram-id',
+      name: 'diagram-name',
+      connectionId: 'connection-id',
+      edits: [{ type: 'SetModel', model: {} }],
+    };
+
+    store.dispatch(openDiagram(loadedDiagram));
+
+    const edit = {
+      type: 'SetModel',
+      model: { collections: [], relationships: [] },
+    };
+    store.dispatch(applyEdit(edit));
+
+    const diagramAfterEdit = getCurrentDiagramFromState(store.getState());
+    expect(diagramAfterEdit.edits).to.have.length(2);
+    expect(diagramAfterEdit.edits[0]).to.deep.include(loadedDiagram.edits[0]);
+    expect(diagramAfterEdit.edits[1]).to.deep.include(edit);
+
+    store.dispatch(undoEdit());
+
+    const diagramAfterUndo = getCurrentDiagramFromState(store.getState());
+    expect(diagramAfterUndo.edits).to.have.length(1);
+    expect(diagramAfterUndo.edits[0]).to.deep.include(loadedDiagram.edits[0]);
+
+    store.dispatch(redoEdit());
+
+    const diagramAfterRedo = getCurrentDiagramFromState(store.getState());
+    expect(diagramAfterRedo.edits).to.have.length(2);
+    expect(diagramAfterRedo.edits[0]).to.deep.include(loadedDiagram.edits[0]);
+    expect(diagramAfterRedo.edits[1]).to.deep.include(edit);
   });
 });
