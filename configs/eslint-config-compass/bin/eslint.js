8013236ac720042d2b0da21a0ad571f4f8b31726
Sergey Petushkov <petushkov.sergey@gmail.com>
2025-05-21 17:10:49 +0200

    chore(eslint-config): update eslint to 8

--- configs/eslint-config-compass/bin/eslint.js
+++ configs/eslint-config-compass/bin/eslint.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 'use strict';
-require('eslint/bin/eslint.js');
+const path = require('path');
+const eslintPkgJson = require('eslint/package.json');
+const eslintBinPath = path.resolve(
+  path.dirname(require.resolve('eslint/package.json')),
+  eslintPkgJson.bin.eslint
+);
+const sharedEslintConfigPath = path.resolve(__dirname, '...');
+if (!process.argv.includes('--resolve-plugins-relative-to')) {
+  process.argv.push('--resolve-plugins-relative-to', sharedEslintConfigPath);
+}
+require(eslintBinPath);
