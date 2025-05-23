87aeb40c3afc70a03c2f64e953668e0a86611b60
Sergey Petushkov <petushkov.sergey@gmail.com>
2025-05-23 10:56:13 +0200

    chore(eslint): hoist all eslint deps to monorepo root so that vscode integration can resolve them

--- configs/eslint-config-compass/bin/eslint.js
+++ configs/eslint-config-compass/bin/eslint.js
@@ -6,8 +6,4 @@ const eslintBinPath = path.resolve(
   path.dirname(require.resolve('eslint/package.json')),
   eslintPkgJson.bin.eslint
 );
-const sharedEslintConfigPath = path.resolve(__dirname, '..');
-if (!process.argv.includes('--resolve-plugins-relative-to')) {
-  process.argv.push('--resolve-plugins-relative-to', sharedEslintConfigPath);
-}
 require(eslintBinPath);
