1b53f5355cb1bcf8e46ba89da6800a69a61d2f07
Niva Sivakumar <nvs119@users.noreply.github.com>
2025-06-25 11:22:49 -0400

    refactor(storage): create an abstract QueryStorageBackend interface COMPASS-9480 (#7051)
    * create an abstract QueryStorageBackend interface
    
    * more specific data type
    
    * type import
    
    * comment

--- packages/my-queries-storage/src/compass-query-storage.ts
+++ packages/my-queries-storage/src/compass-query-storage.ts
@@ -1,16 +1,32 @@
 import { UUID, EJSON } from 'bson';
 import { UserData, type z } from '@mongodb-js/compass-user-data';
-import { RecentQuerySchema, FavoriteQuerySchema } from './query-storage-schema';
+import {
+  RecentQuerySchema,
+  FavoriteQuerySchema,
+  type RecentQuery,
+  type FavoriteQuery,
+} from './query-storage-schema';
 import type { FavoriteQueryStorage, RecentQueryStorage } from './query-storage';
 
 export type QueryStorageOptions = {
   basepath?: string;
 };
 
-export abstract class CompassQueryStorage<T extends typeof RecentQuerySchema> {
-  protected readonly userData: UserData<T>;
+export interface QueryStorageBackend<TData> {
+  loadAll(namespace?: string): Promise<TData[]>;
+  updateAttributes(id: string, data: Partial<TData>): Promise<TData>;
+  delete(id: string): Promise<boolean>;
+  saveQuery(data: Omit<TData, '_id' | '_lastExecuted'>): Promise<void>;
+}
+
+export abstract class CompassQueryStorage<
+  TSchema extends z.Schema,
+  TData extends z.output<TSchema> = z.output<TSchema>
+> implements QueryStorageBackend<TData>
+{
+  protected readonly userData: UserData<TSchema>;
   constructor(
-    schemaValidator: T,
+    schemaValidator: TSchema,
     protected readonly folder: string,
     protected readonly options: QueryStorageOptions
   ) {
@@ -22,7 +38,7 @@ export abstract class CompassQueryStorage<T extends typeof RecentQuerySchema> {
     });
   }
 
-  async loadAll(namespace?: string): Promise<z.output<T>[]> {
+  async loadAll(namespace?: string): Promise<TData[]> {
     try {
       const { data } = await this.userData.readAll();
       const sortedData = data
@@ -36,10 +52,7 @@ export abstract class CompassQueryStorage<T extends typeof RecentQuerySchema> {
     }
   }
 
-  async updateAttributes(
-    id: string,
-    data: Partial<z.input<T>>
-  ): Promise<z.output<T>> {
+  async updateAttributes(id: string, data: Partial<TData>): Promise<TData> {
     await this.userData.write(id, {
       ...((await this.userData.readOne(id)) ?? {}),
       ...data,
@@ -50,10 +63,12 @@ export abstract class CompassQueryStorage<T extends typeof RecentQuerySchema> {
   async delete(id: string) {
     return await this.userData.delete(id);
   }
+
+  abstract saveQuery(data: any): Promise<void>;
 }
 
 export class CompassRecentQueryStorage
-  extends CompassQueryStorage<typeof RecentQuerySchema>
+  extends CompassQueryStorage<typeof RecentQuerySchema, RecentQuery>
   implements RecentQueryStorage
 {
   private readonly maxAllowedQueries = 30;
@@ -83,7 +98,7 @@ export class CompassRecentQueryStorage
 }
 
 export class CompassFavoriteQueryStorage
-  extends CompassQueryStorage<typeof FavoriteQuerySchema>
+  extends CompassQueryStorage<typeof FavoriteQuerySchema, FavoriteQuery>
   implements FavoriteQueryStorage
 {
   constructor(options: QueryStorageOptions = {}) {
