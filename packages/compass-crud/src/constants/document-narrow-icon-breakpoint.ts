b7da39dcfa14f0b9e2a40e16fcbd16b2bdd96bbb
Ruby Dong <ruby.dong@mongodb.com>
2025-07-03 10:36:24 -0400

    lowered the breakpoint and updated comment format

--- packages/compass-crud/src/constants/document-narrow-icon-breakpoint.ts
+++ packages/compass-crud/src/constants/document-narrow-icon-breakpoint.ts
@@ -1,1 +1,4 @@
-export const DOCUMENT_NARROW_ICON_BREAKPOINT = '1264px'; // This is the width at which the icons switch to narrow mode in the Documents tab.
+/**
+ * This is the width at which the icons switch to narrow mode in the Documents tab.
+ */
+export const DOCUMENT_NARROW_ICON_BREAKPOINT = '960px';
