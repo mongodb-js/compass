fcc4778b7541d97c9bd2c6c8b42549b470d48c01
Le Roux Bodenstein <lerouxb@gmail.com>
2025-07-14 15:12:06 +0100

    pull the electron version from the binary, not the monorepo when testing the packaged app

--- packages/compass-e2e-tests/helpers/compass.ts
+++ packages/compass-e2e-tests/helpers/compass.ts
@@ -32,7 +32,7 @@ import {
   isTestingAtlasCloudExternal,
 } from './test-runner-context';
 import {
-  ELECTRON_CHROMIUM_VERSION,
+  MONOREPO_ELECTRON_CHROMIUM_VERSION,
   LOG_PATH,
   LOG_COVERAGE_PATH,
   COMPASS_DESKTOP_PATH,
@@ -43,6 +43,8 @@ import {
 import treeKill from 'tree-kill';
 import { downloadPath } from './downloads';
 import path from 'path';
+// @ts-expect-error no types for this package
+import { electronToChromium } from 'electron-to-chromium';
 
 const killAsync = async (pid: number, signal?: string) => {
   return new Promise<void>((resolve, reject) => {
@@ -473,6 +475,11 @@ function execFileIgnoreError(
   });
 }
 
+async function getChromiumVersionFromBinary(path: string) {
+  const { stdout } = await execFileIgnoreError(path, ['--versions'], {});
+  return electronToChromium(JSON.parse(stdout).electron);
+}
+
 export async function runCompassOnce(args: string[], timeout = 30_000) {
   const { binary } = await getCompassExecutionParameters();
   debug('spawning compass...', {
@@ -654,7 +661,9 @@ async function startCompassElectron(
     automationProtocol: 'webdriver' as const,
     capabilities: {
       browserName: 'chromium',
-      browserVersion: ELECTRON_CHROMIUM_VERSION,
+      browserVersion: testPackagedApp
+        ? await getChromiumVersionFromBinary(binary)
+        : MONOREPO_ELECTRON_CHROMIUM_VERSION,
       // https://chromedriver.chromium.org/capabilities#h.p_ID_106
       'goog:chromeOptions': {
         binary: maybeWrappedBinary,
