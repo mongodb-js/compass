e1fa9017e486a8bdaf7e212db66973d9660c1461
Paula Stachova <paula.stachova@mongodb.com>
2025-07-18 11:47:56 +0200

    revert data modeling changes

--- packages/compass-data-modeling/src/components/data-modeling.tsx
+++ packages/compass-data-modeling/src/components/data-modeling.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import DiagramEditor from './diagram-editor';
 import SavedDiagramsList from './saved-diagrams-list';
@@ -6,63 +6,30 @@ import NewDiagramFormModal from './new-diagram-form';
 import type { DataModelingState } from '../store/reducer';
 import { DiagramProvider } from '@mongodb-js/diagramming';
 import DiagramEditorSidePanel from './diagram-editor-side-panel';
-import {
-  DrawerDisplayMode,
-  DrawerLayout,
-  type DrawerLayoutProps,
-} from '@mongodb-js/compass-components';
-import { closeSidePanel } from '../store/side-panel';
 type DataModelingPluginInitialProps = {
   showList: boolean;
-  isSidePanelOpen: boolean;
-  onSidePanelClose: () => void;
 };
 
 const DataModeling: React.FunctionComponent<DataModelingPluginInitialProps> = ({
   showList,
-  onSidePanelClose,
 }) => {
-  const drawerToolbarData = useMemo<DrawerLayoutProps['toolbarData']>(
-    () => [
-      {
-        id: 'DiagramEditor',
-        label: 'Data Model',
-        content: <DiagramEditorSidePanel />,
-        title: 'Data Model',
-        glyph: 'Diagram',
-        onClose: onSidePanelClose,
-      },
-    ],
-    [onSidePanelClose]
-  );
-
   return (
     <>
       {showList ? (
         <SavedDiagramsList></SavedDiagramsList>
       ) : (
-        <DrawerLayout
-          displayMode={DrawerDisplayMode.Overlay}
-          toolbarData={drawerToolbarData}
-        >
-          <DiagramProvider fitView>
-            <DiagramEditor />
-          </DiagramProvider>
-        </DrawerLayout>
+        <DiagramProvider fitView>
+          <DiagramEditor />
+          <DiagramEditorSidePanel />
+        </DiagramProvider>
       )}
       <NewDiagramFormModal></NewDiagramFormModal>
     </>
   );
 };
 
-export default connect(
-  (state: DataModelingState) => {
-    return {
-      showList: state.step === 'NO_DIAGRAM_SELECTED',
-      isSidePanelOpen: state.sidePanel.isOpen,
-    };
-  },
-  {
-    onSidePanelClose: closeSidePanel,
-  }
-)(DataModeling);
+export default connect((state: DataModelingState) => {
+  return {
+    showList: state.step === 'NO_DIAGRAM_SELECTED',
+  };
+})(DataModeling);
--- packages/compass-data-modeling/src/components/diagram-editor-side-panel.tsx
+++ packages/compass-data-modeling/src/components/diagram-editor-side-panel.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import type { DataModelingState } from '../store/reducer';
+import { closeSidePanel } from '../store/side-panel';
 import {
+  Button,
   css,
   cx,
+  Body,
   spacing,
   palette,
   useDarkMode,
@@ -18,27 +21,44 @@ const containerStyles = css({
   alignItems: 'center',
   justifyContent: 'center',
   gap: spacing[400],
+  borderLeft: `1px solid ${palette.gray.light2}`,
 });
 
 const darkModeContainerStyles = css({
   borderLeftColor: palette.gray.dark2,
 });
 
-function DiagramEditorSidePanel() {
+type DiagramEditorSidePanelProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+function DiagmramEditorSidePanel({
+  isOpen,
+  onClose,
+}: DiagramEditorSidePanelProps) {
   const isDarkMode = useDarkMode();
-
-  // TODO: this is a placeholder for the side panel content.
-
+  if (!isOpen) {
+    return null;
+  }
   return (
     <div className={cx(containerStyles, isDarkMode && darkModeContainerStyles)}>
-      This feature is under development.
+      <Body>This feature is under development.</Body>
+      <Button onClick={onClose} variant="primary" size="small">
+        Close Side Panel
+      </Button>
     </div>
   );
 }
 
-export default connect((state: DataModelingState) => {
-  const { sidePanel } = state;
-  return {
-    isOpen: sidePanel.isOpen,
-  };
-})(DiagramEditorSidePanel);
+export default connect(
+  (state: DataModelingState) => {
+    const { sidePanel } = state;
+    return {
+      isOpen: sidePanel.isOpen,
+    };
+  },
+  {
+    onClose: closeSidePanel,
+  }
+)(DiagmramEditorSidePanel);
