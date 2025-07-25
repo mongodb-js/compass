81a3156e7ce26ea714b2bd41037e36b19807fb9b
Sergey Petushkov <petushkov.sergey@gmail.com>
2025-07-25 15:24:45 +0200

    chore(data-modeling): refactor data modeling to layout as part of the analysis

--- packages/compass-data-modeling/src/components/diagram-editor.tsx
+++ packages/compass-data-modeling/src/components/diagram-editor.tsx
@@ -1,16 +1,7 @@
-import React, {
-  useCallback,
-  useMemo,
-  useRef,
-  useEffect,
-  useState,
-} from 'react';
+import React, { useCallback, useEffect, useMemo, useRef } from 'react';
 import { connect } from 'react-redux';
-import toNS from 'mongodb-ns';
-import type { MongoDBJSONSchema } from 'mongodb-schema';
 import type { DataModelingState } from '../store/reducer';
 import {
-  applyInitialLayout,
   moveCollection,
   getCurrentDiagramFromState,
   selectCurrentModel,
@@ -21,15 +12,14 @@ import {
 } from '../store/diagram';
 import {
   Banner,
-  Body,
   CancelLoader,
   WorkspaceContainer,
   css,
   spacing,
   Button,
   useDarkMode,
-  InlineDefinition,
   useDrawerActions,
+  rafraf,
 } from '@mongodb-js/compass-components';
 import { cancelAnalysis, retryAnalysis } from '../store/analysis-process';
 import {
@@ -37,13 +27,16 @@ import {
   type NodeProps,
   type EdgeProps,
   useDiagram,
-  applyLayout,
 } from '@mongodb-js/diagramming';
-import type { Relationship, StaticModel } from '../services/data-model-storage';
+import type { StaticModel } from '../services/data-model-storage';
 import DiagramEditorToolbar from './diagram-editor-toolbar';
 import ExportDiagramModal from './export-diagram-modal';
-import { useLogger } from '@mongodb-js/compass-logging/provider';
 import { DATA_MODELING_DRAWER_ID } from './diagram-editor-side-panel';
+import {
+  collectionToDiagramNode,
+  getSelectedFields,
+  relationshipToDiagramEdge,
+} from '../utils/nodes-and-edges';
 
 const loadingContainerStyles = css({
   width: '100%',
@@ -66,12 +59,6 @@ const bannerButtonStyles = css({
   marginLeft: 'auto',
 });
 
-const mixedTypeTooltipContentStyles = css({
-  overflowWrap: 'anywhere',
-  textWrap: 'wrap',
-  textAlign: 'left',
-});
-
 const ErrorBannerWithRetry: React.FunctionComponent<{
   onRetryClick: () => void;
 }> = ({ children, onRetryClick }) => {
@@ -89,121 +76,6 @@ const ErrorBannerWithRetry: React.FunctionComponent<{
   );
 };
 
-function getBsonTypeName(bsonType: string) {
-  switch (bsonType) {
-    case 'array':
-      return '[]';
-    default:
-      return bsonType;
-  }
-}
-
-function getFieldTypeDisplay(bsonTypes: string[]) {
-  if (bsonTypes.length === 0) {
-    return 'unknown';
-  }
-
-  if (bsonTypes.length === 1) {
-    return getBsonTypeName(bsonTypes[0]);
-  }
-
-  const typesString = bsonTypes
-    .map((bsonType) => getBsonTypeName(bsonType))
-    .join(', ');
-
-  // We show `mixed` with a tooltip when multiple bsonTypes were found.
-  return (
-    <InlineDefinition
-      definition={
-        <Body className={mixedTypeTooltipContentStyles}>
-          Multiple types found in sample: {typesString}
-        </Body>
-      }
-    >
-      <div>(mixed)</div>
-    </InlineDefinition>
-  );
-}
-
-export const getFieldsFromSchema = (
-  jsonSchema: MongoDBJSONSchema,
-  highlightedFields: string[] = [],
-  depth = 0
-): NodeProps['fields'] => {
-  if (!jsonSchema || !jsonSchema.properties) {
-    return [];
-  }
-  let fields: NodeProps['fields'] = [];
-  for (const [name, field] of Object.entries(jsonSchema.properties)) {
-    // field has types, properties and (optional) children
-    // types are either direct, or from anyof
-    // children are either direct (properties), from anyOf, items or items.anyOf
-    const types: (string | string[])[] = [];
-    const children: (MongoDBJSONSchema | MongoDBJSONSchema[])[] = [];
-    if (field.bsonType) types.push(field.bsonType);
-    if (field.properties) children.push(field);
-    if (field.items)
-      children.push((field.items as MongoDBJSONSchema).anyOf || field.items);
-    if (field.anyOf) {
-      for (const variant of field.anyOf) {
-        if (variant.bsonType) types.push(variant.bsonType);
-        if (variant.properties) {
-          children.push(variant);
-        }
-        if (variant.items) children.push(variant.items);
-      }
-    }
-
-    fields.push({
-      name,
-      type: getFieldTypeDisplay(types.flat()),
-      depth: depth,
-      glyphs: types.length === 1 && types[0] === 'objectId' ? ['key'] : [],
-      variant:
-        highlightedFields.length &&
-        highlightedFields[highlightedFields.length - 1] === name
-          ? 'preview'
-          : undefined,
-    });
-
-    if (children.length > 0) {
-      fields = [
-        ...fields,
-        ...children
-          .flat()
-          .flatMap((child) =>
-            getFieldsFromSchema(
-              child,
-              name === highlightedFields[0] ? highlightedFields.slice(1) : [],
-              depth + 1
-            )
-          ),
-      ];
-    }
-  }
-
-  return fields;
-};
-
-const getSelectedFields = (
-  selectedItems: SelectedItems | null,
-  relationships?: Relationship[]
-) => {
-  if (!selectedItems || selectedItems.type !== 'relationship') return {};
-  const { id } = selectedItems;
-  const relationship = relationships?.find((rel) => rel.id === id);
-  if (
-    !relationship ||
-    !relationship.relationship[0].ns ||
-    !relationship.relationship[1].ns
-  )
-    return {};
-  return {
-    [relationship.relationship[0].ns]: relationship.relationship[0].fields,
-    [relationship.relationship[1].ns]: relationship.relationship[1].fields,
-  };
-};
-
 const modelPreviewContainerStyles = css({
   display: 'grid',
   gridTemplateColumns: '100%',
@@ -218,14 +90,10 @@ const modelPreviewStyles = css({
 
 type SelectedItems = NonNullable<DiagramState>['selectedItems'];
 
-const DiagramEditor: React.FunctionComponent<{
+const DiagramContent: React.FunctionComponent<{
   diagramLabel: string;
-  step: DataModelingState['step'];
   model: StaticModel | null;
   editErrors?: string[];
-  onRetryClick: () => void;
-  onCancelClick: () => void;
-  onApplyInitialLayout: (positions: Record<string, [number, number]>) => void;
   onMoveCollection: (ns: string, newPosition: [number, number]) => void;
   onCollectionSelect: (namespace: string) => void;
   onRelationshipSelect: (rId: string) => void;
@@ -233,22 +101,16 @@ const DiagramEditor: React.FunctionComponent<{
   selectedItems: SelectedItems;
 }> = ({
   diagramLabel,
-  step,
   model,
-  onRetryClick,
-  onCancelClick,
-  onApplyInitialLayout,
   onMoveCollection,
   onCollectionSelect,
   onRelationshipSelect,
   onDiagramBackgroundClicked,
   selectedItems,
 }) => {
-  const { log, mongoLogId } = useLogger('COMPASS-DATA-MODELING-DIAGRAM-EDITOR');
   const isDarkMode = useDarkMode();
   const diagramContainerRef = useRef<HTMLDivElement | null>(null);
-  const diagram = useDiagram();
-  const [areNodesReady, setAreNodesReady] = useState(false);
+  const diagram = useRef(useDiagram());
   const { openDrawer } = useDrawerActions();
 
   const setDiagramContainerRef = useCallback(
@@ -262,20 +124,13 @@ const DiagramEditor: React.FunctionComponent<{
     [diagram]
   );
 
-  const edges = useMemo(() => {
-    return (model?.relationships ?? []).map((relationship): EdgeProps => {
-      const [source, target] = relationship.relationship;
-      return {
-        id: relationship.id,
-        source: source.ns ?? '',
-        target: target.ns ?? '',
-        markerStart: source.cardinality === 1 ? 'one' : 'many',
-        markerEnd: target.cardinality === 1 ? 'one' : 'many',
-        selected:
-          !!selectedItems &&
-          selectedItems.type === 'relationship' &&
-          selectedItems.id === relationship.id,
-      };
+  const edges = useMemo<EdgeProps[]>(() => {
+    return (model?.relationships ?? []).map((relationship) => {
+      const selected =
+        !!selectedItems &&
+        selectedItems.type === 'relationship' &&
+        selectedItems.id === relationship.id;
+      return relationshipToDiagramEdge(relationship, selected);
     });
   }, [model?.relationships, selectedItems]);
 
@@ -284,70 +139,95 @@ const DiagramEditor: React.FunctionComponent<{
       selectedItems,
       model?.relationships
     );
-    return (model?.collections ?? []).map(
-      (coll): NodeProps => ({
-        id: coll.ns,
-        type: 'collection',
-        position: {
-          x: coll.displayPosition[0],
-          y: coll.displayPosition[1],
-        },
-        title: toNS(coll.ns).collection,
-        fields: getFieldsFromSchema(
-          coll.jsonSchema,
-          selectedFields[coll.ns] || undefined,
-          0
-        ),
-        selected:
-          !!selectedItems &&
-          selectedItems.type === 'collection' &&
-          selectedItems.id === coll.ns,
-      })
-    );
+    return (model?.collections ?? []).map((coll) => {
+      const selected =
+        !!selectedItems &&
+        selectedItems.type === 'collection' &&
+        selectedItems.id === coll.ns;
+      return collectionToDiagramNode(coll, selectedFields, selected);
+    });
   }, [model?.collections, model?.relationships, selectedItems]);
 
-  const applyInitialLayout = useCallback(async () => {
-    try {
-      const { nodes: positionedNodes } = await applyLayout(
-        nodes,
-        edges,
-        'LEFT_RIGHT'
-      );
-      onApplyInitialLayout(
-        Object.fromEntries(
-          positionedNodes.map((node) => [
-            node.id,
-            [node.position.x, node.position.y],
-          ])
-        )
-      );
-    } catch (err) {
-      log.error(
-        mongoLogId(1_001_000_361),
-        'DiagramEditor',
-        'Error applying layout:',
-        err
-      );
-    }
-  }, [edges, log, nodes, mongoLogId, onApplyInitialLayout]);
-
+  // Fit to view on initial mount
   useEffect(() => {
-    if (nodes.length === 0) return;
-    const isInitialState = nodes.some(
-      (node) => isNaN(node.position.x) || isNaN(node.position.y)
-    );
-    if (isInitialState) {
-      void applyInitialLayout();
-      return;
-    }
-    if (!areNodesReady) {
-      setAreNodesReady(true);
-      setTimeout(() => {
-        void diagram.fitView();
-      });
-    }
-  }, [areNodesReady, nodes, diagram, applyInitialLayout]);
+    // Schedule the fitView call to make sure that diagramming package had a
+    // chance to set initial nodes, edges state
+    // TODO: react-flow documentation suggests that we should be able to do this
+    // without unrelyable scheduling by calling the fitView after initial state
+    // is set, but for this we will need to make some changes to the diagramming
+    // package first
+    return rafraf(() => {
+      void diagram.current.fitView();
+    });
+  }, []);
 
+  return (
+    <div
+      ref={setDiagramContainerRef}
+      className={modelPreviewContainerStyles}
+      data-testid="diagram-editor-container"
+    >
+      <div className={modelPreviewStyles} data-testid="model-preview">
+        <Diagram
+          isDarkMode={isDarkMode}
+          title={diagramLabel}
+          edges={edges}
+          nodes={nodes}
+          // With threshold too low clicking sometimes gets confused with
+          // dragging
+          // @ts-expect-error expose this prop from the component
+          nodeDragThreshold={5}
+          // @ts-expect-error expose this prop from the component
+          onNodeClick={(_evt, node) => {
+            if (node.type !== 'collection') {
+              return;
+            }
+            onCollectionSelect(node.id);
+            openDrawer(DATA_MODELING_DRAWER_ID);
+          }}
+          onPaneClick={onDiagramBackgroundClicked}
+          onEdgeClick={(_evt, edge) => {
+            onRelationshipSelect(edge.id);
+            openDrawer(DATA_MODELING_DRAWER_ID);
+          }}
+          fitViewOptions={{
+            maxZoom: 1,
+            minZoom: 0.25,
+          }}
+          onNodeDragStop={(evt, node) => {
+            onMoveCollection(node.id, [node.position.x, node.position.y]);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+const ConnectedDiagramContent = connect(
+  (state: DataModelingState) => {
+    const { diagram } = state;
+    return {
+      model: diagram
+        ? selectCurrentModel(getCurrentDiagramFromState(state).edits)
+        : null,
+      diagramLabel: diagram?.name || 'Schema Preview',
+      selectedItems: state.diagram?.selectedItems ?? null,
+    };
+  },
+  {
+    onMoveCollection: moveCollection,
+    onCollectionSelect: selectCollection,
+    onRelationshipSelect: selectRelationship,
+    onDiagramBackgroundClicked: selectBackground,
+  }
+)(DiagramContent);
+
+const DiagramEditor: React.FunctionComponent<{
+  step: DataModelingState['step'];
+  diagramId?: string;
+  onRetryClick: () => void;
+  onCancelClick: () => void;
+}> = ({ step, diagramId, onRetryClick, onCancelClick }) => {
   let content;
 
   if (step === 'NO_DIAGRAM_SELECTED') {
@@ -383,46 +263,9 @@ const DiagramEditor: React.FunctionComponent<{
     );
   }
 
-  if (step === 'EDITING') {
+  if (step === 'EDITING' && diagramId) {
     content = (
-      <div
-        ref={setDiagramContainerRef}
-        className={modelPreviewContainerStyles}
-        data-testid="diagram-editor-container"
-      >
-        <div className={modelPreviewStyles} data-testid="model-preview">
-          <Diagram
-            isDarkMode={isDarkMode}
-            title={diagramLabel}
-            edges={edges}
-            nodes={areNodesReady ? nodes : []}
-            // With threshold too low clicking sometimes gets confused with
-            // dragging
-            // @ts-expect-error expose this prop from the component
-            nodeDragThreshold={3}
-            // @ts-expect-error expose this prop from the component
-            onNodeClick={(_evt, node) => {
-              if (node.type !== 'collection') {
-                return;
-              }
-              onCollectionSelect(node.id);
-              openDrawer(DATA_MODELING_DRAWER_ID);
-            }}
-            onPaneClick={onDiagramBackgroundClicked}
-            onEdgeClick={(_evt, edge) => {
-              onRelationshipSelect(edge.id);
-              openDrawer(DATA_MODELING_DRAWER_ID);
-            }}
-            fitViewOptions={{
-              maxZoom: 1,
-              minZoom: 0.25,
-            }}
-            onNodeDragStop={(evt, node) => {
-              onMoveCollection(node.id, [node.position.x, node.position.y]);
-            }}
-          />
-        </div>
-      </div>
+      <ConnectedDiagramContent key={diagramId}></ConnectedDiagramContent>
     );
   }
 
@@ -439,21 +282,12 @@ export default connect(
     const { diagram, step } = state;
     return {
       step: step,
-      model: diagram
-        ? selectCurrentModel(getCurrentDiagramFromState(state).edits)
-        : null,
       editErrors: diagram?.editErrors,
-      diagramLabel: diagram?.name || 'Schema Preview',
-      selectedItems: state.diagram?.selectedItems ?? null,
+      diagramId: diagram?.id,
     };
   },
   {
     onRetryClick: retryAnalysis,
     onCancelClick: cancelAnalysis,
-    onApplyInitialLayout: applyInitialLayout,
-    onMoveCollection: moveCollection,
-    onCollectionSelect: selectCollection,
-    onRelationshipSelect: selectRelationship,
-    onDiagramBackgroundClicked: selectBackground,
   }
 )(DiagramEditor);
