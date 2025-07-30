9a06339234c0df03823b2ff267918154eb982ed0
Paula Stachova <paula.stachova@mongodb.com>
2025-07-30 17:56:34 +0200

    more cleanup

--- packages/compass-data-modeling/src/components/drawer/collection-drawer-content.tsx
+++ packages/compass-data-modeling/src/components/drawer/collection-drawer-content.tsx
@@ -97,7 +97,6 @@ const CollectionDrawerContent: React.FunctionComponent<
             <Badge>{relationships.length}</Badge>
             <Button
               className={titleBtnStyles}
-              aria-label="Add relationship"
               size="xsmall"
               onClick={() => {
                 onCreateNewRelationshipClick(namespace);
