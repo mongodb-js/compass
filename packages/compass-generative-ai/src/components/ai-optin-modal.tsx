b3f2aea092ca7ead33ad120542035ff6a9046772
gagik <me@gagik.co>
2025-08-04 16:26:38 +0200

    feat(compass-generative-ai): update Gen AI opt-in modal COMPASS-9593

--- packages/compass-generative-ai/src/components/ai-optin-modal.tsx
+++ packages/compass-generative-ai/src/components/ai-optin-modal.tsx
@@ -4,12 +4,12 @@ import {
   Banner,
   Body,
   Link,
-  ConfirmationModal,
-  SpinLoader,
+  MarketingModal,
   css,
   spacing,
-  H3,
   palette,
+  useDarkMode,
+  SpinLoader,
 } from '@mongodb-js/compass-components';
 import { AiImageBanner } from './ai-image-banner';
 import { closeOptInModal, optIn } from '../store/atlas-optin-reducer';
@@ -22,50 +22,44 @@ const GEN_AI_FAQ_LINK = 'https://www.mongodb.com/docs/generative-ai-faq/';
 type OptInModalProps = {
   isOptInModalVisible: boolean;
   isOptInInProgress: boolean;
+  isCloudOptIn: boolean;
   onOptInModalClose: () => void;
   onOptInClick: () => void;
   projectId?: string;
 };
 
-const titleStyles = css({
-  marginBottom: spacing[400],
-  marginTop: spacing[400],
-  marginLeft: spacing[500],
-  marginRight: spacing[500],
-  textAlign: 'center',
-});
-
-const bodyStyles = css({
-  marginBottom: spacing[400],
-  marginTop: spacing[400],
-  marginLeft: spacing[300],
-  marginRight: spacing[300],
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  textAlign: 'center',
-});
-
 const disclaimerStyles = css({
   color: palette.gray.dark1,
-  marginTop: spacing[400],
-  marginLeft: spacing[800],
-  marginRight: spacing[800],
+  paddingLeft: spacing[800],
+  paddingRight: spacing[800],
 });
 
-const bannerStyles = css({
-  padding: spacing[400],
-  marginTop: spacing[400],
-  textAlign: 'left',
+// TODO: The LG MarketingModal does not provide a way to disable a button
+// so this is a temporary workaround to make the button look disabled.
+const disableOptInButtonStyles = css({
+  button: {
+    opacity: 0.5,
+    pointerEvents: 'none',
+    cursor: 'not-allowed',
+  },
 });
-const getButtonText = (isOptInInProgress: boolean) => {
+
+const getButtonText = ({
+  isOptInInProgress,
+  isCloudOptIn,
+  darkMode,
+}: {
+  isOptInInProgress: boolean;
+  isCloudOptIn: boolean;
+  darkMode: boolean | undefined;
+}) => {
   return (
     <>
-      &nbsp;Use Natural Language
-      {isOptInInProgress && (
+      Opt-in AI features
+      {isOptInInProgress && isCloudOptIn && (
         <>
           &nbsp;
-          <SpinLoader darkMode={true}></SpinLoader>
+          <SpinLoader darkMode={darkMode}></SpinLoader>
         </>
       )}
     </>
@@ -75,12 +69,14 @@ const getButtonText = (isOptInInProgress: boolean) => {
 export const AIOptInModal: React.FunctionComponent<OptInModalProps> = ({
   isOptInModalVisible,
   isOptInInProgress,
+  isCloudOptIn,
   onOptInModalClose,
   onOptInClick,
   projectId,
 }) => {
   const isProjectAIEnabled = usePreference('enableGenAIFeaturesAtlasProject');
   const track = useTelemetry();
+  const darkMode = useDarkMode();
   const PROJECT_SETTINGS_LINK = projectId
     ? window.location.origin + '/v2/' + projectId + '#/settings/groupSettings'
     : null;
@@ -92,7 +88,7 @@ export const AIOptInModal: React.FunctionComponent<OptInModalProps> = ({
   }, [isOptInModalVisible, track]);
 
   const onConfirmClick = () => {
-    if (isOptInInProgress) {
+    if ((isOptInInProgress && isCloudOptIn) || !isProjectAIEnabled) {
       return;
     }
     onOptInClick();
@@ -104,43 +100,23 @@ export const AIOptInModal: React.FunctionComponent<OptInModalProps> = ({
   }, [track, onOptInModalClose]);
 
   return (
-    <ConfirmationModal
-      open={isOptInModalVisible}
-      title=""
-      confirmButtonProps={{
-        children: getButtonText(isOptInInProgress),
-        disabled: !isProjectAIEnabled,
-        onClick: onConfirmClick,
-      }}
-      cancelButtonProps={{
-        onClick: handleModalClose,
-      }}
-    >
-      <Body className={bodyStyles}>
-        <AiImageBanner></AiImageBanner>
-        <H3 className={titleStyles}>
-          Use natural language to generate queries and pipelines
-        </H3>
-        Atlas users can now quickly create queries and aggregations with
-        MongoDB&apos;s&nbsp; intelligent AI-powered feature, available today.
-        <Banner
-          variant={isProjectAIEnabled ? 'info' : 'warning'}
-          className={bannerStyles}
-        >
-          {isProjectAIEnabled
-            ? 'AI features are enabled for project users with data access.'
-            : 'AI features are disabled for project users.'}{' '}
-          Project Owners can {isProjectAIEnabled ? 'disable' : 'enable'} Data
-          Explorer AI features in the{' '}
-          {PROJECT_SETTINGS_LINK !== null ? (
-            <Link href={PROJECT_SETTINGS_LINK} target="_blank">
-              Project Settings
-            </Link>
-          ) : (
-            'Project Settings'
-          )}
-          .
-        </Banner>
+    <MarketingModal
+      open={true}
+      onClose={handleModalClose}
+      className={isCloudOptIn ? disableOptInButtonStyles : undefined}
+      onButtonClick={onConfirmClick}
+      title="Opt-in Gen AI-Powered features"
+      // @ts-expect-error - buttonText expects a string but supports ReactNode as well.
+      buttonText={getButtonText({
+        isOptInInProgress,
+        isCloudOptIn,
+        darkMode,
+      })}
+      linkText="Not now"
+      onLinkClick={handleModalClose}
+      graphic={<AiImageBanner />}
+      darkMode={darkMode}
+      disclaimer={
         <div className={disclaimerStyles}>
           This is a feature powered by generative AI, and may give inaccurate
           responses. Please see our{' '}
@@ -149,8 +125,49 @@ export const AIOptInModal: React.FunctionComponent<OptInModalProps> = ({
           </Link>{' '}
           for more information.
         </div>
+      }
+    >
+      <Body
+        style={{
+          textAlign: 'left',
+          marginTop: spacing[300],
+        }}
+      >
+        Opt in now MongoDB’s intelligent AI-powered features:
+        <ul
+          style={{
+            textAlign: 'left',
+            marginTop: spacing[100],
+            listStyle: 'disc',
+          }}
+        >
+          <li>AI Assistant allows you to ask questions across connections</li>
+          <li>Natural Language Bar to create queries and aggregations</li>
+          {/* <li>Mock Data Generator to create AI powered sample data</li> */}
+        </ul>
+        {isCloudOptIn && (
+          <Banner
+            data-testid="ai-optin-cloud-banner"
+            variant={isProjectAIEnabled ? 'info' : 'warning'}
+            style={{ marginTop: spacing[300] }}
+          >
+            {isProjectAIEnabled
+              ? 'AI features are enabled for project users with data access.'
+              : 'AI features are disabled for project users.'}{' '}
+            Project Owners can {isProjectAIEnabled ? 'disable' : 'enable'} Data
+            Explorer AI features in the{' '}
+            {PROJECT_SETTINGS_LINK !== null ? (
+              <Link href={PROJECT_SETTINGS_LINK} target="_blank">
+                Project Settings
+              </Link>
+            ) : (
+              'Project Settings'
+            )}
+            .
+          </Banner>
+        )}
       </Body>
-    </ConfirmationModal>
+    </MarketingModal>
   );
 };
 
