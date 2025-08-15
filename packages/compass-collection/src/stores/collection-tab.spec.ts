ce3552de554cceaa87ea64d5e63b737de2f818b8
Sergey Petushkov <petushkov.sergey@gmail.com>
2025-08-15 12:57:43 +0200

    Revert "feat(compass-collection):  Schema Analysis Redux Integration for Collection Plugin CLOUDP-333846" (#7201)
    Revert "feat(compass-collection):  Schema Analysis Redux Integration for Coll…"
    
    This reverts commit e3092d6a442ad7d836f87495976af1b1a8f39a49.

--- packages/compass-collection/src/stores/collection-tab.spec.ts
+++ packages/compass-collection/src/stores/collection-tab.spec.ts
@@ -1,7 +1,6 @@
 import type { CollectionTabOptions } from './collection-tab';
 import { activatePlugin } from './collection-tab';
 import { selectTab } from '../modules/collection-tab';
-import * as collectionTabModule from '../modules/collection-tab';
 import { waitFor } from '@mongodb-js/testing-library-compass';
 import Sinon from 'sinon';
 import AppRegistry from '@mongodb-js/compass-app-registry';
@@ -12,7 +11,6 @@ import type { connectionInfoRefLocator } from '@mongodb-js/compass-connections/p
 import { createNoopLogger } from '@mongodb-js/compass-logging/provider';
 import { ReadOnlyPreferenceAccess } from 'compass-preferences-model/provider';
 import { ExperimentTestName } from '@mongodb-js/compass-telemetry/provider';
-import { type CollectionMetadata } from 'mongodb-collection-model';
 
 const defaultMetadata = {
   namespace: 'test.foo',
@@ -27,6 +25,16 @@ const defaultMetadata = {
 const defaultTabOptions = {
   tabId: 'workspace-tab-id',
   namespace: defaultMetadata.namespace,
+};
+
+const mockCollection = {
+  _id: defaultMetadata.namespace,
+  fetchMetadata() {
+    return Promise.resolve(defaultMetadata);
+  },
+  toJSON() {
+    return this;
+  },
 };
 
 const mockAtlasConnectionInfo = {
@@ -59,9 +67,6 @@ describe('Collection Tab Content store', function () {
   const sandbox = Sinon.createSandbox();
 
   const localAppRegistry = sandbox.spy(new AppRegistry());
-  const analyzeCollectionSchemaStub = sandbox
-    .stub(collectionTabModule, 'analyzeCollectionSchema')
-    .returns(async () => {});
   const dataService = {} as any;
   let store: ReturnType<typeof activatePlugin>['store'];
   let deactivate: ReturnType<typeof activatePlugin>['deactivate'];
@@ -80,18 +85,8 @@ describe('Collection Tab Content store', function () {
       enableGenAIFeatures: true,
       enableGenAIFeaturesAtlasOrg: true,
       cloudFeatureRolloutAccess: { GEN_AI_COMPASS: true },
-    }),
-    collectionMetadata: Partial<CollectionMetadata> = defaultMetadata
+    })
   ) => {
-    const mockCollection = {
-      _id: collectionMetadata.namespace,
-      fetchMetadata() {
-        return Promise.resolve(collectionMetadata);
-      },
-      toJSON() {
-        return this;
-      },
-    };
     ({ store, deactivate } = activatePlugin(
       {
         ...defaultTabOptions,
@@ -112,7 +107,7 @@ describe('Collection Tab Content store', function () {
     await waitFor(() => {
       expect(store.getState())
         .to.have.property('metadata')
-        .deep.eq(collectionMetadata);
+        .deep.eq(defaultMetadata);
     });
     return store;
   };
@@ -234,42 +229,6 @@ describe('Collection Tab Content store', function () {
           .to.have.property('metadata')
           .deep.eq(defaultMetadata);
       });
-    });
-  });
-
-  describe('schema analysis on collection load', function () {
-    it('should start schema analysis if collection is not read-only and not time-series', async function () {
-      await configureStore();
-
-      expect(analyzeCollectionSchemaStub).to.have.been.calledOnce;
-    });
-
-    it('should not start schema analysis if collection is read-only', async function () {
-      await configureStore(
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        { ...defaultMetadata, isReadonly: true }
-      );
-
-      expect(analyzeCollectionSchemaStub).to.not.have.been.called;
-    });
-
-    it('should not start schema analysis if collection is time-series', async function () {
-      await configureStore(
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        { ...defaultMetadata, isTimeSeries: true }
-      );
-
-      expect(analyzeCollectionSchemaStub).to.not.have.been.called;
     });
   });
 });
