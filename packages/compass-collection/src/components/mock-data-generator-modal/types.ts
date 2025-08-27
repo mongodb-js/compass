0de12bcbbf4ed49e35b5c1994516399fcf120c87
Kevin Pamaran <kevpamaran@gmail.com>
2025-08-27 13:30:42 -0400

    type state machine of mock data generator request

--- packages/compass-collection/src/components/mock-data-generator-modal/types.ts
+++ packages/compass-collection/src/components/mock-data-generator-modal/types.ts
@@ -1,3 +1,5 @@
+import type { MockDataSchemaResponse } from '@mongodb-js/compass-generative-ai';
+
 export enum MockDataGeneratorStep {
   AI_DISCLAIMER = 'AI_DISCLAIMER',
   SCHEMA_CONFIRMATION = 'SCHEMA_CONFIRMATION',
@@ -6,3 +8,35 @@ export enum MockDataGeneratorStep {
   PREVIEW_DATA = 'PREVIEW_DATA',
   GENERATE_DATA = 'GENERATE_DATA',
 }
+
+export const MOCK_DATA_GENERATOR_STATE_IDLE = 'idle';
+export const MOCK_DATA_GENERATOR_STATE_GENERATING = 'generating';
+export const MOCK_DATA_GENERATOR_STATE_COMPLETED = 'completed';
+export const MOCK_DATA_GENERATOR_STATE_ERROR = 'error';
+
+type MockDataGeneratorIdleState = {
+  status: typeof MOCK_DATA_GENERATOR_STATE_IDLE;
+};
+
+type MockDataGeneratorGeneratingState = {
+  status: typeof MOCK_DATA_GENERATOR_STATE_GENERATING;
+  requestId: string;
+};
+
+type MockDataGeneratorCompletedState = {
+  status: typeof MOCK_DATA_GENERATOR_STATE_COMPLETED;
+  fakerSchema: MockDataSchemaResponse;
+  requestId: string;
+};
+
+type MockDataGeneratorErrorState = {
+  status: typeof MOCK_DATA_GENERATOR_STATE_ERROR;
+  error: unknown;
+  requestId: string;
+};
+
+export type MockDataGeneratorState =
+  | MockDataGeneratorIdleState
+  | MockDataGeneratorGeneratingState
+  | MockDataGeneratorCompletedState
+  | MockDataGeneratorErrorState;
