f2ca6c23524aad6ac33ab71e9bd4d08e8addfcba
Le Roux Bodenstein <leroux.bodenstein@mongodb.com>
2025-09-04 13:38:15 +0100

    hide the performance insight entrypoints behind its own feature flag

--- packages/compass-assistant/src/compass-assistant-provider.tsx
+++ packages/compass-assistant/src/compass-assistant-provider.tsx
@@ -91,6 +91,10 @@ export function useAssistantActions(): AssistantActionsType {
   const actions = useContext(AssistantActionsContext);
   const isAIFeatureEnabled = useIsAIFeatureEnabled();
   const isAssistantFlagEnabled = usePreference('enableAIAssistant');
+  const isPerformanceInsightEntrypointsEnabled = usePreference(
+    'enablePerformanceInsightsEntrypoints'
+  );
+
   if (!isAIFeatureEnabled || !isAssistantFlagEnabled) {
     return {
       getIsAssistantEnabled: () => false,
@@ -106,7 +110,9 @@ export function useAssistantActions(): AssistantActionsType {
   return {
     interpretExplainPlan,
     interpretConnectionError,
-    tellMoreAboutInsight,
+    tellMoreAboutInsight: isPerformanceInsightEntrypointsEnabled
+      ? tellMoreAboutInsight
+      : undefined,
     getIsAssistantEnabled: () => true,
   };
 }
