dbecfae3087dff50acb8c66159b3d23026321bae
Nataly Carbonell <nataly.carbonell@mongodb.com>
2025-09-10 07:07:32 -0400

    add fakerSchemaState to memo dependency list

--- packages/compass-collection/src/components/mock-data-generator-modal/mock-data-generator-modal.tsx
+++ packages/compass-collection/src/components/mock-data-generator-modal/mock-data-generator-modal.tsx
@@ -99,7 +99,7 @@ const MockDataGeneratorModal = ({
       case MockDataGeneratorStep.GENERATE_DATA:
         return <ScriptScreen />;
     }
-  }, [currentStep, fakerSchemaGenerationState.status]);
+  }, [currentStep, fakerSchemaGenerationState]);
 
   const isNextButtonDisabled =
     currentStep === MockDataGeneratorStep.SCHEMA_EDITOR &&
