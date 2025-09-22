d0d7ea833ca431ba4fba655501f149d659e0d913
Le Roux Bodenstein <lerouxb@gmail.com>
2025-09-22 19:25:34 +0100

    fix(e2e): use a larger test fixture for the abort import test COMPASS-9858 (#7352)
    * use a larger test fixture for the abort import tests
    
    * make sure the button is enabled

--- packages/compass-e2e-tests/helpers/selectors.ts
+++ packages/compass-e2e-tests/helpers/selectors.ts
@@ -659,7 +659,7 @@ export const ImportSkipAnalyze = '[data-testid="skip-csv-analyze-button"]';
 export const ImportAnalyzeError =
   '[data-testid="import-modal"] [data-testid="analyze-error"]';
 export const ImportConfirm =
-  '[data-testid="import-modal"] [data-testid="import-button"]';
+  '[data-testid="import-modal"] [data-testid="import-button"][aria-disabled="false"]';
 export const ImportToast = '[data-testid="toast-import-toast"]';
 export const ImportToastErrorDetailsBtn =
   '[data-testid="toast-import-toast"] [data-testid="import-error-details-button"]';
