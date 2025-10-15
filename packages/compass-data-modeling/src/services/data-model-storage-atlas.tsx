ed88fb95f424edc2cc67a0ca76eea5defc013597
Sergey Petushkov <petushkov.sergey@gmail.com>
2025-10-15 15:05:49 +0200

    feat(data-modeling): implement data model storage that uses atlas user data

---
+++ packages/compass-data-modeling/src/services/data-model-storage-atlas.tsx
@@ -0,0 +1,119 @@
+import { AtlasUserData } from '@mongodb-js/compass-user-data';
+import type {
+  DataModelStorage,
+  MongoDBDataModelDescription,
+} from './data-model-storage';
+import { MongoDBDataModelDescriptionSchema } from './data-model-storage';
+import dataModelStorageInMemory from './data-model-storage-in-memory';
+import {
+  atlasServiceLocator,
+  type AtlasService,
+} from '@mongodb-js/atlas-service/provider';
+import { createServiceProvider } from '@mongodb-js/compass-app-registry';
+import { DataModelStorageServiceProvider } from '../provider';
+import React, { useRef } from 'react';
+import { mongoLogId, useLogger } from '@mongodb-js/compass-logging/provider';
+
+class DataModelStorageAtlas implements DataModelStorage {
+  private readonly userData: AtlasUserData<
+    typeof MongoDBDataModelDescriptionSchema
+  >;
+  constructor(orgId: string, projectId: string, atlasService: AtlasService) {
+    this.userData = new AtlasUserData(
+      MongoDBDataModelDescriptionSchema,
+      'dataModelDescriptions',
+      {
+        orgId,
+        projectId,
+        getResourceUrl(path) {
+          // TODO(COMPASS-9960): this is copied from compass-web entrypoint for
+          // brevity, but shouldn't be defined outside of AtlasUserData, this
+          // logic is literally the same between all user data instances and can
+          // be encapsulated inside of the AtlasUserData class implementation
+          const [type, pathOrgId, pathProjectId, id] =
+            path?.split('/').filter(Boolean) || [];
+
+          if (
+            !type ||
+            !pathOrgId ||
+            !pathProjectId ||
+            type !== 'dataModelDescriptions'
+          ) {
+            throw new Error(
+              'DataModelStorageAtlas is used outside of Atlas Cloud context'
+            );
+          }
+
+          return atlasService.userDataEndpoint(
+            pathOrgId,
+            pathProjectId,
+            type,
+            id
+          );
+        },
+        authenticatedFetch: atlasService.authenticatedFetch.bind(atlasService),
+      }
+    );
+  }
+  save(description: MongoDBDataModelDescription) {
+    return this.userData.write(description.id, description);
+  }
+  delete(id: MongoDBDataModelDescription['id']) {
+    return this.userData.delete(id);
+  }
+  async loadAll(): Promise<MongoDBDataModelDescription[]> {
+    try {
+      const res = await this.userData.readAll();
+      return res.data;
+    } catch {
+      return [];
+    }
+  }
+  async load(id: string): Promise<MongoDBDataModelDescription | null> {
+    return (
+      (await this.loadAll()).find((item) => {
+        return item.id === id;
+      }) ?? null
+    );
+  }
+}
+
+export const DataModelStorageServiceProviderAtlas = createServiceProvider(
+  function DataModelStorageServiceProviderAtlas({
+    children,
+    orgId,
+    projectId,
+  }: {
+    children?: React.ReactNode;
+    orgId?: string;
+    projectId?: string;
+  }) {
+    const storageRef = useRef<DataModelStorage>();
+    const atlasService = atlasServiceLocator();
+    const logger = useLogger('DATA-MODEL-STORAGE');
+
+    if (!storageRef.current) {
+      if (orgId && projectId) {
+        storageRef.current = new DataModelStorageAtlas(
+          orgId,
+          projectId,
+          atlasService
+        );
+      } else {
+        logger.log.warn(
+          mongoLogId(1_001_000_378),
+          'DataModelStorageServiceProviderAtlas',
+          'Falling back to in memory storage because orgId or projectId is missing'
+        );
+        // Fallback to in-memory if we're outside of Atlas Cloud
+        storageRef.current = dataModelStorageInMemory;
+      }
+    }
+
+    return (
+      <DataModelStorageServiceProvider storage={storageRef.current}>
+        {children}
+      </DataModelStorageServiceProvider>
+    );
+  }
+);
