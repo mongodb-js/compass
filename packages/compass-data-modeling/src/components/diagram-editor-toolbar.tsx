54bc9cc448e86e0e8fe00f47375c1b10a9c82296
Anna Henningsen <anna.henningsen@mongodb.com>
2025-10-21 11:08:03 +0200

    feat(data-modeling): add keyboard controls for DM experience COMPASS-9779 (#7474)
    Add support for keyboard shortcuts for deleting items, blurring items
    (i.e. removing focus), confirming edits in inputs with Enter, and
    undo/redo.

--- packages/compass-data-modeling/src/components/diagram-editor-toolbar.tsx
+++ packages/compass-data-modeling/src/components/diagram-editor-toolbar.tsx
@@ -16,6 +16,7 @@ import {
   Tooltip,
   Breadcrumbs,
   type BreadcrumbItem,
+  useHotkeys,
 } from '@mongodb-js/compass-components';
 import AddCollection from './icons/add-collection';
 import { useOpenWorkspace } from '@mongodb-js/compass-workspaces/provider';
@@ -85,6 +86,19 @@ export const DiagramEditorToolbar: React.FunctionComponent<{
     ],
     [diagramName, openDataModelingWorkspace]
   );
+
+  // TODO(COMPASS-9976): Integrate with application menu
+  // macOS: Cmd+Shift+Z = Redo, Cmd+Z = Undo
+  // Windows/Linux: Ctrl+Z = Undo, Ctrl+Y = Redo
+  useHotkeys('mod+z', onUndoClick, { enabled: step === 'EDITING' }, [
+    onUndoClick,
+  ]);
+  useHotkeys('mod+shift+z', onRedoClick, { enabled: step === 'EDITING' }, [
+    onRedoClick,
+  ]);
+  useHotkeys('mod+y', onRedoClick, { enabled: step === 'EDITING' }, [
+    onRedoClick,
+  ]);
 
   if (step !== 'EDITING') {
     return null;
