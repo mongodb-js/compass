dec70407fd47cac7ea3ed17d4def0b1b971a9195
Sergey Petushkov <petushkov.sergey@gmail.com>
2025-10-27 18:27:27 +0100

    chore(web): remove unused dep

--- packages/compass-web/src/preferences.tsx
+++ packages/compass-web/src/preferences.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { useCallback, useContext, useEffect, useRef } from 'react';
 import type {
   AllPreferences,
   AtlasCloudFeatureFlags,
