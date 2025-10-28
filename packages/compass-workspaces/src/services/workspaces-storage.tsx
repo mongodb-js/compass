22d71c466ec9de29800d893036e06f86683d24d9
Simon Zhu <simon.zhu@mongodb.com>
2025-10-28 14:21:48 -0400

    fix types

--- packages/compass-workspaces/src/services/workspaces-storage.tsx
+++ packages/compass-workspaces/src/services/workspaces-storage.tsx
@@ -1,73 +1,11 @@
 import { createServiceLocator } from '@mongodb-js/compass-app-registry';
 import {
+  type z,
   IUserData,
   type ReadAllResult,
-  z,
 } from '@mongodb-js/compass-user-data';
 import React, { useContext } from 'react';
-import { collectionSubtabValues } from '../types';
-
-const CollectionSubtabSchema = z.enum(collectionSubtabValues);
-
-export const WorkspaceTabSchema = z
-  .discriminatedUnion('type', [
-    z.object({
-      type: z.literal('Welcome'),
-    }),
-    z.object({
-      type: z.literal('My Queries'),
-    }),
-    z.object({
-      type: z.literal('Data Modeling'),
-    }),
-    z.object({
-      type: z.literal('Databases'),
-      connectionId: z.string(),
-    }),
-    z.object({
-      type: z.literal('Performance'),
-      connectionId: z.string(),
-    }),
-    z.object({
-      type: z.literal('Shell'),
-      connectionId: z.string(),
-      initialEvaluate: z.union([z.string(), z.array(z.string())]).optional(),
-      initialInput: z.string().optional(),
-    }),
-    z.object({
-      type: z.literal('Collections'),
-      connectionId: z.string(),
-      namespace: z.string(),
-      inferredFromPrivileges: z.boolean().optional(),
-    }),
-    z.object({
-      type: z.literal('Collection'),
-      subTab: CollectionSubtabSchema,
-      initialQuery: z.record(z.any()).optional(),
-      initialPipeline: z.array(z.record(z.any())).optional(),
-      initialPipelineText: z.string().optional(),
-      initialAggregation: z.record(z.any()).optional(),
-      editViewName: z.string().optional(),
-      connectionId: z.string(),
-      namespace: z.string(),
-      inferredFromPrivileges: z.boolean().optional(),
-    }),
-  ])
-  .and(
-    z.object({
-      id: z.string(),
-    })
-  );
-
-export const WorkspacesStateSchema = z.object({
-  tabs: z.array(WorkspaceTabSchema),
-  activeTabId: z.string().nullable(),
-  timestamp: z.number(),
-});
-
-// TypeScript types derived from the schemas
-export type WorkspaceTabData = z.output<typeof WorkspaceTabSchema>;
-export type WorkspacesStateData = z.output<typeof WorkspacesStateSchema>;
+import { WorkspacesStateSchema } from '../types';
 
 const throwIfNotTestEnv = () => {
   if (process.env.NODE_ENV !== 'test') {
