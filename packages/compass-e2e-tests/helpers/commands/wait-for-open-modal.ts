0a7e159e6275e72ba04b445b45aef13f631bc37f
Kræn Hansen <kraen.hansen@mongodb.com>
2025-11-11 15:34:40 +0100

    Add new modal specific commands

---
+++ packages/compass-e2e-tests/helpers/commands/wait-for-open-modal.ts
@@ -0,0 +1,36 @@
+import { inspect } from 'node:util';
+import type { WaitForOptions } from 'webdriverio';
+import type { CompassBrowser } from '../compass-browser';
+
+export async function waitForOpenModal(
+  browser: CompassBrowser,
+  selector: string,
+  { reverse = false, ...options }: WaitForOptions = {}
+): Promise<void> {
+  await browser.waitUntil(
+    async () => {
+      const modals = await browser.getOpenModals(selector);
+      const count = await modals.length;
+      if (reverse) {
+        return count === 0;
+      } else if (count === 0) {
+        return false;
+      } else {
+        for (const modal of modals) {
+          // Ensure any modals are interactable if open
+          await modal.waitForClickable({
+            timeoutMsg: 'Timeout waiting for open modal to become clickable',
+            ...options,
+          });
+        }
+        return true;
+      }
+    },
+    {
+      timeoutMsg: `Timeout waiting for modal '${inspect(selector)}' to ${
+        reverse ? 'close' : 'open'
+      }`,
+      ...options,
+    }
+  );
+}
