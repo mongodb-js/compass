8b1de0a9fb3fb038f1f67a6257890b565a2e6739
Neal Beeken <neal.beeken@mongodb.com>
2025-11-21 20:36:12 -0500

    chore(compass-web): build devtools-connect into compass web COMPASS-9793

--- packages/compass-web/polyfills/@mongodb-js/devtools-proxy-support/index.ts
+++ packages/compass-web/polyfills/@mongodb-js/devtools-proxy-support/index.ts
@@ -10,14 +10,20 @@ export function createSocks5Tunnel(): void {
 export function hookLogger(): void {
   // no-op
 }
-export function createFetch(): never {
-  throw new Error('node-fetch like-API not available in compass-web');
+export function createFetch(): typeof fetch {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  return async () => {
+    throw new Error('node-fetch not available in compass web');
+  };
 }
-export function systemCA(): never {
-  throw new Error('system CA access not available in compass-web');
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function systemCA(): Promise<never> {
+  const error = new Error('system CA access not available in compass-web');
+  (error as unknown as { code: 'OUT_OF_MEM' }).code = 'OUT_OF_MEM'; // This is a "tls" error code that makes devtools-connect not use the systemCA
+  throw error;
 }
 export function resetSystemCACache(): never {
-  throw new Error('system CA access not available in compass-web');
+  throw new Error('reset system CA access not available in compass-web');
 }
 
 // Explicitly web-compatible
