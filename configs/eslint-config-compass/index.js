bb79d84d475d45347a86771c2ffab15eceae8937
Sergey Petushkov <petushkov.sergey@gmail.com>
2025-11-26 13:41:47 +0100

    fix(eslint): resolve no-redundant-type-constituents issues

--- configs/eslint-config-compass/index.js
+++ configs/eslint-config-compass/index.js
@@ -14,7 +14,6 @@ const tempNewEslintRulesDisabled = {
   'react-hooks/globals': 'off',
   'react-hooks/static-components': 'off',
   'react-hooks/purity': 'off',
-  '@typescript-eslint/no-redundant-type-constituents': 'off',
   '@typescript-eslint/await-thenable': 'off',
   '@typescript-eslint/no-unsafe-enum-comparison': 'off',
 };
