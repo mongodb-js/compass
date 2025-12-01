60f0051db44214097b5ed8a3bc3f83307c528e97
Ivan Medina <ivan.medina@mongodb.com>
2025-12-01 20:56:59 +0100

    fix: e2e test for export data button

--- packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
+++ packages/compass-e2e-tests/tests/collection-aggregations-tab.test.ts
@@ -993,6 +993,10 @@ describe('Collection aggregations tab', function () {
 
     // Open the modal.
     await browser.clickVisible(Selectors.ExportAggregationResultsButton);
+    // Click the "Export pipeline results" menu item
+    await browser.clickVisible(
+      '[data-testid="pipeline-toolbar-export-data-button-export-query-action"]'
+    );
     const exportModal = browser.$(Selectors.ExportModal);
     await exportModal.waitForDisplayed();
 
