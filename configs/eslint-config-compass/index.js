736f235cf99c04b1e4fd2930e80c2f509d655a2e
Sergey Petushkov <petushkov.sergey@gmail.com>
2025-12-01 10:07:06 +0100

    fix(eslint): fix await-thenable issues (#7606)

--- configs/eslint-config-compass/index.js
+++ configs/eslint-config-compass/index.js
@@ -15,7 +15,6 @@ const tempNewEslintRulesDisabled = {
   'react-hooks/static-components': 'off',
   'react-hooks/purity': 'off',
   '@typescript-eslint/no-redundant-type-constituents': 'off',
-  '@typescript-eslint/await-thenable': 'off',
   '@typescript-eslint/no-unsafe-enum-comparison': 'off',
 };
 
