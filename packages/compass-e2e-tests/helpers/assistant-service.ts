8190219d73c54561fc91b84cccbd40ea9e81dbef
Basit Chonka <basit.chonka@mongodb.com>
2025-12-04 10:38:20 +0300

    fix cors issue on e2e tests

--- packages/compass-e2e-tests/helpers/assistant-service.ts
+++ packages/compass-e2e-tests/helpers/assistant-service.ts
@@ -170,12 +170,13 @@ export async function startMockAssistantServer(
   let response = _response;
   const server = http
     .createServer((req, res) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
       res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
       res.setHeader(
         'Access-Control-Allow-Headers',
-        'Content-Type, Authorization, X-Request-Origin, User-Agent'
+        'Content-Type, Authorization, X-Request-Origin, User-Agent, X-CSRF-Token, X-CSRF-Time'
       );
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
 
       // Handle preflight requests
       if (req.method === 'OPTIONS') {
