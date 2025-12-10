79e99107cce460ec77144a3782cabed3af08fe9a
Basit Chonka <basit.chonka@mongodb.com>
2025-12-11 00:33:16 +0300

    clean up scorer

--- packages/compass-generative-ai/tests/evals/scorers.ts
+++ packages/compass-generative-ai/tests/evals/scorers.ts
@@ -1,10 +1,6 @@
 import type { ConversationEvalScorer } from './types';
 import { Factuality as _Factuality } from 'autoevals';
 import { allText } from './utils';
-import { createNoopLogger } from '@mongodb-js/compass-logging/provider';
-import { parseXmlToMmsJsonResponse } from '../../src/utils/xml-to-mms-response';
-
-const logger = createNoopLogger();
 
 export const Factuality: ConversationEvalScorer = ({
   input,
@@ -13,12 +9,8 @@ export const Factuality: ConversationEvalScorer = ({
 }) => {
   return _Factuality({
     input: allText(input.messages),
-    output: JSON.stringify(
-      parseXmlToMmsJsonResponse(allText(output.messages), logger)
-    ),
-    expected: JSON.stringify(
-      parseXmlToMmsJsonResponse(allText(expected.messages), logger)
-    ),
+    output: allText(output.messages),
+    expected: allText(expected.messages),
     model: 'gpt-4.1',
     temperature: undefined,
   });
