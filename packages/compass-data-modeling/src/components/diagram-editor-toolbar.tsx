aa61be8b3758c27abde9bcdacf72ed202140915e
Paula Stachova <paula.stachova@mongodb.com>
2025-12-10 15:39:21 +0100

    cleanup

--- packages/compass-data-modeling/src/components/diagram-editor-toolbar.tsx
+++ packages/compass-data-modeling/src/components/diagram-editor-toolbar.tsx
@@ -215,7 +215,7 @@ export default connect(
   (state: DataModelingState) => {
     const { diagram, step } = state;
     return {
-      step: 'ANALYZING',
+      step,
       hasUndo: (diagram?.edits.prev.length ?? 0) > 0,
       hasRedo: (diagram?.edits.next.length ?? 0) > 0,
       diagramName: diagram?.name,
