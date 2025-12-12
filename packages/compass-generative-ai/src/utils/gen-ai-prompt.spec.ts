6cc6c4519e5da6cc9d145b6ebe9be20285151d71
Basit Chonka <basit.chonka@mongodb.com>
2025-12-12 13:58:49 +0300

    PR feedback

--- packages/compass-generative-ai/src/utils/gen-ai-prompt.spec.ts
+++ packages/compass-generative-ai/src/utils/gen-ai-prompt.spec.ts
@@ -35,10 +35,20 @@ const OPTIONS: PromptContextOptions = {
   ],
 };
 
-const expectedSchema = {
+const expectedSampleDocuments = `[
+  {
+    _id: ObjectId('68a2dfe93d5adb16ebf4c866'),
+    userId: ObjectId('68a2dfe93d5adb16ebf4c865')
+  }
+]
+`;
+
+const expectedSchema = `
+{
   _id: 'ObjectId',
-  userId: 'ObjectId',
-};
+  userId: 'ObjectId'
+}
+`;
 
 describe('GenAI Prompts', function () {
   it('buildFindQueryPrompt', function () {
@@ -70,16 +80,13 @@ describe('GenAI Prompts', function () {
       'Schema from a sample of documents from the collection:',
       'includes schema text'
     );
-    expect(prompt).to.include(
-      toJSString(expectedSchema),
-      'includes actual schema'
-    );
+    expect(prompt).to.include(expectedSchema, 'includes actual schema');
     expect(prompt).to.include(
       'Sample documents from the collection:',
       'includes sample documents text'
     );
     expect(prompt).to.include(
-      toJSString(OPTIONS.sampleDocuments),
+      expectedSampleDocuments,
       'includes actual sample documents'
     );
   });
@@ -113,16 +120,13 @@ describe('GenAI Prompts', function () {
       'Schema from a sample of documents from the collection:',
       'includes schema text'
     );
-    expect(prompt).to.include(
-      toJSString(expectedSchema),
-      'includes actual schema'
-    );
+    expect(prompt).to.include(expectedSchema, 'includes actual schema');
     expect(prompt).to.include(
       'Sample documents from the collection:',
       'includes sample documents text'
     );
     expect(prompt).to.include(
-      toJSString(OPTIONS.sampleDocuments),
+      expectedSampleDocuments,
       'includes actual sample documents'
     );
   });
