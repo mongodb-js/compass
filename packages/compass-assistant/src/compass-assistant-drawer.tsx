394067ed6b84bd2b944e3c76a50524b248ac461c
Le Roux Bodenstein <leroux.bodenstein@mongodb.com>
2026-01-09 16:28:30 +0000

    remove preview badge

--- packages/compass-assistant/src/compass-assistant-drawer.tsx
+++ packages/compass-assistant/src/compass-assistant-drawer.tsx
@@ -78,7 +78,6 @@ export const CompassAssistantDrawer: React.FunctionComponent<{
         <div className={assistantTitleStyles}>
           <div className={assistantTitleTextWrapperStyles}>
             <span className={assistantTitleTextStyles}>MongoDB Assistant</span>
-            <Badge variant="blue">Preview</Badge>
           </div>
           <ClearChatButton chat={chat} />
         </div>
