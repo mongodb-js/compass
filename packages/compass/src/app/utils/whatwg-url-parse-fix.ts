0f521123cb69f691ffbf1cb32b82b3776a11837f
Anna Henningsen <anna@addaleax.net>
2026-02-17 18:25:42 +0100

    fixup! feat(deps): bump mongosh to 2.7.0, driver to 7.x COMPASS-10356

--- packages/compass/src/app/utils/whatwg-url-parse-fix.ts
+++ packages/compass/src/app/utils/whatwg-url-parse-fix.ts
@@ -1,10 +1,4 @@
 function fixWhatwgURLParse(): void {
-  console.log('fixWhatwgURLParse', {
-    URL,
-    C1: typeof URL.parse === 'function',
-    C2: Object.prototype.toString.call(URL.parse?.('http://example.com')),
-    C3: Object.prototype.toString.call(new URL('http://example.com')),
-  });
   // https://github.com/jsdom/whatwg-url/issues/315
   if (
     typeof URL.parse === 'function' &&
@@ -13,7 +7,6 @@ function fixWhatwgURLParse(): void {
     Object.prototype.toString.call(new URL('http://example.com')) ===
       '[object URL]'
   ) {
-    console.warn('fixWhatwgURLParse fixing');
     URL.parse = function (url, base) {
       try {
         return new URL(url, base);
