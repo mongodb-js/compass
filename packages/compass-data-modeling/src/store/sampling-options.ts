174e6d2f5b88ccbf7fdbaea7bda6aaa1d558db7b
Paula Stachova <paula.stachova@mongodb.com>
2026-02-27 15:57:09 +0100

    fix validation

--- packages/compass-data-modeling/src/store/sampling-options.ts
+++ packages/compass-data-modeling/src/store/sampling-options.ts
@@ -7,3 +7,8 @@ export const DEFAULT_SAMPLING_OPTIONS: SamplingOptions = {
   sampleSize: 100,
   allDocuments: false,
 };
+
+export function areSamplingOptionsValid(options: SamplingOptions): boolean {
+  if (options.allDocuments) return true;
+  return !isNaN(options.sampleSize) && options.sampleSize > 0;
+}
