fa186d6d2d81c63a25106fabea05c1a1c0ee79da
Sergey Petushkov <petushkov.sergey@gmail.com>
2026-03-27 12:25:15 +0100

    chore(web): remove electron proxy for atlas sign-in from compass-web sandbox COMPASS-10507 (#7894)
    * chore(web): remove electron proxy for atlas sign-in from compass-web sandbox
    
    * chore(web): use html plugin to make sure resources are autoinjected in the template
    
    * chore(webpack): apply disable overlay to react-refresh settings also
    
    * chore(web): add redirect configs; add instructions
    
    * chore(web): adjust test for new sandbox connection storage; remove unused deps
    
    * chore(web, scripts, e2e): remove sync script; replace serve-dist with watch
    
    * chore(web): add favicon; fix sandbox container styles
    
    * chore(web): force hide the toast when in e2e
    
    * chore(web): better comment
    
    * chore(sidebar, web): show commit hash in header to make it easier to identify local compass-web
    
    * chore(web): mention the mms localhost redirect rule
    
    * chore(web): explicit devserver ws url and allow all hosts
    
    * chore(web): copy tweaks
    
    Co-authored-by: Rhys <Anemy@users.noreply.github.com>
    
    * chore(web): reformat
    
    ---------
    
    Co-authored-by: Rhys <Anemy@users.noreply.github.com>

---
+++ packages/compass-web/sandbox/open-in-atlas-toast.tsx
@@ -0,0 +1,114 @@
+import React, { useEffect } from 'react';
+import {
+  Badge,
+  Link,
+  css,
+  openToast,
+  showConfirmation,
+  spacing,
+} from '@mongodb-js/compass-components';
+
+const listStyles = css({
+  listStyle: 'decimal',
+  marginLeft: spacing[500],
+});
+
+function Instructions() {
+  return (
+    <>
+      <p>
+        By going through the following steps you can embed your local build of
+        compass-web in any Atlas Cloud environment of your choice for testing
+        purposes:
+      </p>
+      <ol className={listStyles}>
+        <li>
+          <p>
+            Install the &quot;Header Editor Lite&quot; browser extension (
+            <Link
+              href="https://chromewebstore.google.com/detail/header-editor-lite/eningockdidmgiojffjmkdblpjocbhgh"
+              target="_blank"
+            >
+              Chrome
+            </Link>
+            &nbsp;|&nbsp;
+            <Link
+              href="https://addons.mozilla.org/en-US/firefox/addon/header-editor-lite/"
+              target="_blank"
+            >
+              Firefox
+            </Link>
+            )
+          </p>
+        </li>
+        <li>
+          <p>
+            In the extension, import the configuration file from{' '}
+            <code>
+              packages/compass-web/scripts/redirect-extension-config.json
+            </code>{' '}
+            (redirects will be enabled by default)
+          </p>
+        </li>
+        <li>
+          <p>Navigate to your Atlas Cloud environment of choice</p>
+          <p>
+            If everything was configured correctly, you should see a commit hash
+            in the Compass sidebar header that would not be visible otherwise
+          </p>
+        </li>
+        <li>
+          <p>
+            <Badge>Optional</Badge>&nbsp;You can also apply your local build of
+            mms to the environment by activating the <code>mms localhost</code>{' '}
+            redirect in the extension
+          </p>
+        </li>
+        <li>
+          <p>
+            When you are done with testing, remember to disable the redirects
+            from the extension UI
+          </p>
+        </li>
+      </ol>
+    </>
+  );
+}
+
+const IS_E2E = process.env.APP_ENV === 'webdriverio';
+
+function showOpenInAtlasToast() {
+  if (IS_E2E) {
+    return;
+  }
+
+  openToast('open-in-atlas', {
+    title: (
+      <>
+        Open local compass-web{' '}
+        <Link
+          as="button"
+          onClick={() => {
+            void showConfirmation({
+              title: 'Test local compass-web in Atlas Cloud environment',
+              description: <Instructions></Instructions>,
+              hideCancelButton: true,
+              confirmButtonProps: {
+                children: 'OK',
+              },
+            });
+          }}
+        >
+          in Atlas Cloud
+        </Link>
+      </>
+    ),
+  });
+}
+
+export function OpenInAtlasToast() {
+  useEffect(() => {
+    showOpenInAtlasToast();
+  }, []);
+  return null;
+}
