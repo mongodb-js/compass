541e9be5cb10c4dfa835e4fee3b6c6ad3ec78eaf
Le Roux Bodenstein <leroux.bodenstein@mongodb.com>
2026-04-21 19:26:09 +0100

    add comment

--- packages/compass-generative-ai/src/index.ts
+++ packages/compass-generative-ai/src/index.ts
@@ -39,5 +39,11 @@ export { mockDataSchemaToolSchema } from './atlas-ai-service';
 
 export { READ_ONLY_DATABASE_TOOLS, AVAILABLE_TOOLS } from './available-tools';
 
+// Exporting these in one place so we can track the same versions across the app
+// and tests. If we just track latest, then future models could become the
+// latest model and they could break backwards compatibility which will break
+// released versions of Compass. By pinning to specific versions here, we can
+// control when we want to update to newer models and we can make sure that
+// we're using versions that work with the bundled versions of ai sdk libraries.
 export const AI_MODEL_CHAT_VERSION = 'mongodb-chat-2.1-mini-reasoning';
 export const AI_MODEL_SLIM_VERSION = 'mongodb-slim-2.1-mini';
