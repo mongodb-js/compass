e9b9fc8020509d521599f22dd62dcb08b5b41156
Jacob Samuel Lu <j.lu@mongodb.com>
2026-04-30 17:29:19 -0400

    Link

--- packages/compass-collection/src/components/mock-data-generator-modal/script-screen.tsx
+++ packages/compass-collection/src/components/mock-data-generator-modal/script-screen.tsx
@@ -256,7 +256,8 @@ const ScriptScreen = ({
             connectionInfo.atlasMetadata.projectId && (
               <li>
                 <Link
-                  href={`/v2/${connectionInfo.atlasMetadata.projectId}#/security/database/users`}
+                  href={`${window.location.origin}/v2/${connectionInfo.atlasMetadata.projectId}#/security/database/users`}
+                  target="_blank"
                 >
                   Access your Database Users
                 </Link>
