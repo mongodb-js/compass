479646114c3a448f840971725f61be5f0273cbc5
Sergey Petushkov <petushkov.sergey@gmail.com>
2026-05-19 12:33:57 +0200

    fix: use stricter shell bson parsing everywhere

--- packages/compass-crud/src/stores/crud-store.ts
+++ packages/compass-crud/src/stores/crud-store.ts
@@ -2273,7 +2273,11 @@ export async function findAndModifyWithFLEFallback(
 
 // Copied from packages/compass-aggregations/src/modules/pipeline-builder/pipeline-parser/utils.ts
 export function parseShellBSON(source: string): BSONObject | BSONObject[] {
-  const parsed = _parseShellBSON(source, { mode: ParseMode.Loose });
+  const parsed = _parseShellBSON(source, {
+    mode: ParseMode.Strict,
+    allowComments: true,
+    allowMethods: true,
+  });
   if (!parsed || typeof parsed !== 'object') {
     // XXX(COMPASS-5689): We've hit the condition in
     // https://github.com/mongodb-js/ejson-shell-parser/blob/c9c0145ababae52536ccd2244ac2ad01a4bbdef3/src/index.ts#L36
