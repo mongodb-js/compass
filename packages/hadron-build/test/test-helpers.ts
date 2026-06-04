b693ceff831eea11b94607c06ea0e24ead82ea15
Basit Chonka <basit.chonka@mongodb.com>
2026-06-04 22:36:33 +0200

    clean up

--- packages/hadron-build/test/test-helpers.ts
+++ packages/hadron-build/test/test-helpers.ts
@@ -1,18 +1,8 @@
 import path from 'path';
 import Target from '../src/lib/target';
 
-/**
- * Currently when creating a new Target.ts instance, it takes two args: dir and argv.
- * The dir is where it expects to find package.json, and argv can be used to:
- * 1. override package.json values - version
- * 2. define argv values - platform, arch, distribution
- *
- * This helper will create a Target instance for the current platform and architecture
- * using the package.json from the mongodb-compass package. We are using that within
- * tests, so that anything that changes on that side, our tests catch it.
- */
-
 export function getTarget(argv?: Record<string, any>): Target {
-  const dir = path.dirname(require.resolve('mongodb-compass/package.json'));
+  // <monorepo>/packages/compass
+  const dir = path.resolve(__dirname, '..', '..', 'compass');
   return new Target(dir, { distribution: 'compass', ...argv });
 }
