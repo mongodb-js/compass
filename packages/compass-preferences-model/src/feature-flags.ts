5e3a1e464558c6f3983a2affd0e8e620caf70044
Rhys Howell <rhysh@live.com>
2026-06-17 09:00:52 -0700

    chore(preferences): set project scope on web feature flags explicitly

--- packages/compass-preferences-model/src/feature-flags.ts
+++ packages/compass-preferences-model/src/feature-flags.ts
@@ -95,6 +95,7 @@ export const FEATURE_FLAG_DEFINITIONS = [
   {
     name: 'enableRollingIndexes',
     stage: 'released',
+    atlasCloudFeatureScope: 'group',
     description: {
       short: 'Enable creating indexes with the rolling build in Atlas Cloud',
     },
@@ -103,6 +104,7 @@ export const FEATURE_FLAG_DEFINITIONS = [
   {
     name: 'enableGlobalWrites',
     stage: 'released',
+    atlasCloudFeatureScope: 'group',
     description: {
       short: 'Enable Global Writes tab in Atlas Cloud',
     },
@@ -185,6 +187,7 @@ export const FEATURE_FLAG_DEFINITIONS = [
   {
     name: 'enableAIAssistant',
     stage: 'released',
+    atlasCloudFeatureScope: 'group',
     description: {
       short: 'Enable AI Assistant',
     },
@@ -196,6 +199,7 @@ export const FEATURE_FLAG_DEFINITIONS = [
   {
     name: 'enableToolCalling',
     stage: 'released',
+    atlasCloudFeatureScope: 'group',
     description: {
       short: 'Enable tool calling in the AI Assistant',
     },
