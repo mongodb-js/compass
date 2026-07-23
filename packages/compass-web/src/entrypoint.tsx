80ef906493ef0891197fdb4c9af324c1d98d2e43
Derek Cho <dkwncho@gmail.com>
2026-07-23 10:25:01 -0400

    refactor(compass-web): add enableCompassWebSettings feature flag COMPASS-10867 (#8248)

--- packages/compass-web/src/entrypoint.tsx
+++ packages/compass-web/src/entrypoint.tsx
@@ -43,6 +43,7 @@ import { CompassIndexesPlugin } from '@mongodb-js/compass-indexes';
 import { CompassSchemaValidationPlugin } from '@mongodb-js/compass-schema-validation';
 import { CompassGlobalWritesPlugin } from '@mongodb-js/compass-global-writes';
 import { CompassGenerativeAIPlugin } from '@mongodb-js/compass-generative-ai';
+import { CompassSettingsPlugin } from '@mongodb-js/compass-settings';
 import ExplainPlanCollectionTabModal from '@mongodb-js/compass-explain-plan';
 import ExportToLanguageCollectionTabModal from '@mongodb-js/compass-export-to-language';
 import {
@@ -494,6 +495,13 @@ const CompassComponentsProviderWeb: React.FunctionComponent<{
   );
 };
 
+const CompassSettingsPluginWithPreferences = () => {
+  const { enableCompassWebSettings } = usePreferences([
+    'enableCompassWebSettings',
+  ]);
+  return enableCompassWebSettings ? <CompassSettingsPlugin /> : null;
+};
+
 const CompassWebWithPreferences = ({
   appName,
   orgId,
@@ -607,6 +615,7 @@ const CompassWebWithPreferences = ({
                                     projectId={projectId}
                                     isCloudOptIn={true}
                                   />
+                                  <CompassSettingsPluginWithPreferences />
                                 </CompassInstanceStorePlugin>
                               </CompassConnections>
                             </CompassAssistantProvider>
