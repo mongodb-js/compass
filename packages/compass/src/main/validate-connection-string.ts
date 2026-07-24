ef6e4ceb0630b219310fb99387ce48bc685106dd
Neal Beeken <neal.beeken@mongodb.com>
2026-07-24 00:03:16 -0400

    chore: add 'maxAdaptiveRetries' to allowed connection string options

--- packages/compass/src/main/validate-connection-string.ts
+++ packages/compass/src/main/validate-connection-string.ts
@@ -35,6 +35,7 @@ const allowedConnectionStringOptions = [
   'localAddress',
   'localPort',
   'localThresholdMS',
+  'maxAdaptiveRetries',
   'maxConnecting',
   'maxIdleTimeMS',
   'maxPoolSize',
