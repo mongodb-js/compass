afed39f52fa38e463483d789729eec646f95a5e9
Basit <1305718+mabaasit@users.noreply.github.com>
2026-07-28 10:48:08 +0200

    chore(feature-flags): update comments for atlasCloudFeatureScope field (#8287)
    * update comment
    
    * Update packages/compass-preferences-model/src/feature-flags.ts
    
    Co-authored-by: Edjan Michiles <caesar.ed@hotmail.com>
    
    ---------
    
    Co-authored-by: Edjan Michiles <caesar.ed@hotmail.com>

--- packages/compass-preferences-model/src/feature-flags.ts
+++ packages/compass-preferences-model/src/feature-flags.ts
@@ -18,9 +18,12 @@ export type FeatureFlagDefinition = {
    */
   stage: 'development' | 'preview' | 'released';
   /**
-   * Optional field that is used to specify the scope of the
-   * feature flag for Atlas Cloud. Supply this when the feature flag is intended
-   * to be set in Atlas and not scoped to Compass.
+   * This field is used to specify the scope of the feature flag for
+   * Atlas Cloud. When adding a new feature flag, the `feature-flag-mms-pr.yml`
+   * Github Action will create a PR in the MMS repository to add the corresponding config.
+   *
+   * *Note*: If the feature flag is only intended to be used in Compass,
+   * close that PR.
    */
   atlasCloudFeatureScope: 'group' | 'organization';
   description: {
