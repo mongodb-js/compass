9daee6a07346b630c86b0c7f7b5f0c5f0eb6e1e3
Basit Chonka <basit.chonka@mongodb.com>
2026-07-29 21:48:42 +0200

    clean up after merge

--- packages/compass-editor/src/index.ts
+++ packages/compass-editor/src/index.ts
@@ -19,7 +19,6 @@ export { createDocumentAutocompleter } from './codemirror/document-autocompleter
 export { createValidationAutocompleter } from './codemirror/validation-autocompleter';
 export { createQueryAutocompleter } from './codemirror/query-autocompleter';
 export { createStageAutocompleter } from './codemirror/stage-autocompleter';
-export { createSafeIntegerLinter } from './codemirror/safe-integer-linter';
 export { createAggregationAutocompleter } from './codemirror/aggregation-autocompleter';
 export { createSearchIndexAutocompleter } from './codemirror/search-index-autocompleter';
 export {
