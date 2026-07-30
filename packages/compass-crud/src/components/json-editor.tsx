1dc3f3b9c422c78f078a6e60c1c2898f1a7e05cf
Basit Chonka <basit.chonka@mongodb.com>
2026-07-30 11:36:31 +0200

    in json editor copy as shell syntax

--- packages/compass-crud/src/components/json-editor.tsx
+++ packages/compass-crud/src/components/json-editor.tsx
@@ -108,7 +108,7 @@ const JSONEditor: React.FunctionComponent<JSONEditorProps> = ({
   }, [value, editing, setModifiedEJSONStringRef]);
 
   const handleCopy = useCallback(() => {
-    copyToClipboard?.(doc, 'shell-syntax');
+    copyToClipboard?.(doc, 'ejson');
   }, [copyToClipboard, doc]);
 
   const handleClone = useCallback(() => {
