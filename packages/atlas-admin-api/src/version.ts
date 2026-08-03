42794376f2dd27531dabafafc17ac622b49f46c1
Paula Stachova <paula.stachova@mongodb.com>
2026-08-03 16:48:42 +0200

    link docs

--- packages/atlas-admin-api/src/version.ts
+++ packages/atlas-admin-api/src/version.ts
@@ -1,5 +1,6 @@
 /**
  * Note: The endpoints are versioned independently, so you might need to override the default version for a specific request.
+ * See [Atlas API versioning overview](https://www.mongodb.com/docs/atlas/api/versioned-api-overview/#std-label-api-versioning-overview)
  */
 export const ATLAS_ADMIN_API_DEFAULT_VERSION = '2025-03-12';
 
