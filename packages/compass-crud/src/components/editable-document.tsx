af74acab97ae77d07ca31243b124a7fd8afa0172
Basit <1305718+mabaasit@users.noreply.github.com>
2026-08-04 00:32:13 +0200

    feat(right-click): copy element and document as shell syntax COMPASS-10041 (#8245)
    * Add copy value and copy as json for document
    
    * handle copy document as shell syntax
    
    * fix menu width
    
    * shell syntax for element values
    
    * fix tests
    
    * remove unused type
    
    * copilot comments
    
    * copy document as shell syntax
    
    * fix test
    
    * bootstrap
    
    * in json editor copy as shell syntax
    
    * add more tests
    
    * add bson value

--- packages/compass-crud/src/components/editable-document.tsx
+++ packages/compass-crud/src/components/editable-document.tsx
@@ -124,7 +124,7 @@ class EditableDocument extends React.Component<
    * Handle copying JSON to clipboard of the document.
    */
   handleCopy() {
-    this.props.copyToClipboard?.(this.props.doc);
+    this.props.copyToClipboard?.(this.props.doc, 'shell-syntax');
   }
 
   /**
