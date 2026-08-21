20012018738048bd5f8423e5779b1a96b97a3eb8
Basit Chonka <basit.chonka@mongodb.com>
2026-08-21 15:18:11 +0200

    fix tests

--- packages/compass-e2e-tests/tests/collection-import.test.ts
+++ packages/compass-e2e-tests/tests/collection-import.test.ts
@@ -201,7 +201,7 @@ describe('Collection import', function () {
 
     expect(result).to.deep.equal({
       foo: '10',
-      long: '99',
+      long: "Long('99')",
     });
 
     // make sure document count also updated
@@ -469,14 +469,14 @@ describe('Collection import', function () {
       dateBefore1970: "ISODate('1920-01-01T00:00:00.000+00:00')",
       dateField_canonical: "ISODate('2019-08-11T17:54:14.692+00:00')",
       dateField_relaxed: "ISODate('2019-08-11T17:54:14.692+00:00')",
-      decimal128Field: '10.99',
+      decimal128Field: "Decimal128('10.99')",
       documentField: 'Object (1)',
       doubleField_canonical: '10.5',
       doubleField_relaxed: '10.5',
       infiniteNumber: 'Infinity',
       int32field_canonical: '10',
       int32field_relaxed: '10',
-      int64Field_canonical: '50',
+      int64Field_canonical: "Long('50')",
       int64Field_relaxed: '50',
       maxKeyField: 'MaxKey()',
       minKeyField: 'MinKey()',
